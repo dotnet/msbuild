f63e49b7cf93ecd5bd593764bf8053bd84cababf
Rainer Sigwald <raines@microsoft.com>
2015-09-02 13:22:25 -0500

    Create msbuild test infrastructure
    This introduces Test targets into our projects and changes build.cmd to
    execute tests.  The first xunit-converted project, Framework.UnitTests, is
    the only project that's wired into these tests at the moment.

--- build.cmd
+++ build.cmd
@@ -30,8 +30,8 @@ echo ** MSBuild Path: %MSBUILDCUSTOMPATH%
 echo ** Building all sources
 
 :: Call MSBuild
-echo ** "%MSBUILDCUSTOMPATH%" "%~dp0build.proj" /maxcpucount /verbosity:minimal /nodeReuse:false /fileloggerparameters:Verbosity=diag;LogFile="%~dp0msbuild.log" %*
-"%MSBUILDCUSTOMPATH%" "%~dp0build.proj" /maxcpucount /verbosity:minimal /nodeReuse:false /fileloggerparameters:Verbosity=diag;LogFile="%~dp0msbuild.log" %*
+echo ** "%MSBUILDCUSTOMPATH%" "%~dp0build.proj" /maxcpucount /verbosity:minimal /nodeReuse:false /fileloggerparameters:Verbosity=diag;LogFile="%~dp0msbuild.log" /t:BuildAndTest %*
+"%MSBUILDCUSTOMPATH%" "%~dp0build.proj" /maxcpucount /verbosity:minimal /nodeReuse:false /fileloggerparameters:Verbosity=diag;LogFile="%~dp0msbuild.log" /t:BuildAndTest %*
 set BUILDERRORLEVEL=%ERRORLEVEL%
 echo.
 
