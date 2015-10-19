77d97f66f3c73c39809d01b5ce41c0aa5cbd59ca
David Fowler <davidfowl@gmail.com>
2015-10-18 23:35:43 -0700

    Fixed the *nix build

--- build.sh
+++ build.sh
@@ -1,8 +1,3 @@
 #!/usr/bin/env bash
 
-if ! type dnvm > /dev/null 2>&1; then
-    curl -sSL https://raw.githubusercontent.com/aspnet/Home/dev/dnvminstall.sh | DNX_BRANCH=dev sh && source ~/.dnx/dnvm/dnvm.sh
-fi
-
-# bootstrap!
 ./scripts/bootstrap
