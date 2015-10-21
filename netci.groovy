8b4df2d31fe4e235b82e75f59d56e52b54d1a9c3
Sridhar Periyasamy <sridhper@microsoft.com>
2015-10-21 13:47:53 -0700

    Address PR feedback

--- netci.groovy
+++ netci.groovy
@@ -27,11 +27,9 @@ def static getBuildJobName(def configuration, def os) {
 
         // Calculate the build command
         if (os == 'Windows_NT') {
-            // On Windows we build the mscorlibs too.
             buildCommand = ".\\scripts\\ci_build.cmd ${lowerConfiguration}"
         }
         else {
-            // On other OS's we skipmscorlib but run the pal tests
             buildCommand = "./scripts/ci_build.sh ${lowerConfiguration}"
         }
 
--- scripts/ci_build.sh
+++ scripts/ci_build.sh
@@ -2,7 +2,7 @@
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-source $SCRIPT_DIR/../build.sh $@
+$SCRIPT_DIR/../build.sh $@
 
 ret_code=$?
 exit $ret_code
