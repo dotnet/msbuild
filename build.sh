85d4f888eaa9ecba5f58c9cf263879343ad2f330
Bryan Thornbury <brthor@microsoft.com>
2015-10-22 15:56:12 -0700

    fix relative paths in build

--- build.sh
+++ build.sh
@@ -2,6 +2,14 @@
 #
 # $1 is passed to package to enable deb or pkg packaging
 
-./scripts/bootstrap.sh
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
-./scripts/package.sh $1
+$DIR/scripts/bootstrap.sh
+
+$DIR/scripts/package.sh $1
