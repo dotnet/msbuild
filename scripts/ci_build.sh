cdf8b9049445b0ded0c9506c3d031677aed481f5
Bryan <brthor@microsoft.com>
2015-11-10 16:18:00 -0800

    Refactor docker entrypoint to reduce potential for out of sync build and ci_build scripts

--- scripts/ci_build.sh
+++ scripts/ci_build.sh
@@ -33,14 +33,9 @@ if [[ "$(uname)" == "Linux" ]]; then
     [ ! -z "$BUILD_BUILDID" ] && container_name="$BUILD_BUILDID"
 
     export DOTNET_BUILD_CONTAINER_NAME="$container_name"
+    export PACKAGE_IN_DOCKER="true"
     
-    # Build Binaries outside of Docker
-    $SCRIPT_DIR/bootstrap.sh $@
-    
-    # Change Docker Build command to packaging only
-    export BUILD_COMMAND="/opt/code/scripts/package.sh"
-    $SCRIPT_DIR/dockerbuild.sh
-    
+    $SCRIPT_DIR/../build.sh $@
 else
     $SCRIPT_DIR/../build.sh $@
 fi
