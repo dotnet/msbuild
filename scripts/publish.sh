d7bae750b25dfec11e39a0c62a15c36716bccb3a
Andrew Stanton-Nurse <andrew@andrewnurse.net>
2015-11-11 15:07:56 -0800

    how do I bash?

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -144,7 +144,7 @@ upload_binaries_to_blob_storage(){
     update_file_in_blob_storage $index_URL $indexfile $indexContent
 
     # update the version file
-    local versionContent = $(cat $REPOROOT/artifacts/$RID/stage2/.version)
+    local versionContent=$(cat $REPOROOT/artifacts/$RID/stage2/.version)
     local versionfile="latest.$OSNAME.version"
     local version_URL="https://$STORAGE_ACCOUNT.blob.core.windows.net/$STORAGE_CONTAINER/$CHANNEL/dnvm/$versionfile$SASTOKEN"
     update_file_in_blob_storage $version_URL $versionfile $versionContent
