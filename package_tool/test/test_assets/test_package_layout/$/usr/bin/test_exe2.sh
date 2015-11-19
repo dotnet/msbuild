cefd1cbcfa4b8876b7edb242e50e60e93d0b1e79
Bryan Thornbury <brthor@microsoft.com>
2015-11-19 14:45:20 -0800

    Move Debian Package Tools to packaging folder

--- package_tool/test/test_assets/test_package_layout/$/usr/bin/test_exe2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#
-# Copyright (c) .NET Foundation and contributors. All rights reserved.
-# Licensed under the MIT license. See LICENSE file in the project root for full license information.
-#
-
-# Example of something you shouldn't do, but is good for a testing scenario
-exec bash /usr/share/packagetooltest/test_called.sh
