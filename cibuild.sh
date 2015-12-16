fe16e365f3184cc0fb3614ef6e30cfc55f650588
Mihai Codoban <micodoba@microsoft.com>
2015-12-15 20:58:49 -0800

    update scripts for CI
    cibuild:
    - set HOME if not set (CI machines do not have HOME)
    - use unzip instead of tar
    
    LinuxDeployDependencies:
    - reuse variable

--- cibuild.sh
+++ cibuild.sh
@@ -8,20 +8,32 @@ usage()
     echo "  --scope <scope>                Scope of the build (Compile / Test)"
 }
 
+setHome(){
+    if [ -z ${HOME+x}]
+    then
+        MONO_COMMAND="( export HOME=$HOME_DEFAULT ; $MONO_COMMAND )"
+        mkdir -p $HOME_DEFAULT
+    fi
+}
+
 downloadMSBuild(){
     if [ ! -e "$MSBUILD_EXE" ]
     then
         mkdir -p $PACKAGES_DIR # Create packages dir if it doesn't exist.
+
         echo "Downloading MSBUILD from $MSBUILD_DOWNLOAD_URL"
-        curl -sL "$MSBUILD_DOWNLOAD_URL" | tar xz -C "$PACKAGES_DIR"
+        curl -sL -o $MSBUILD_ZIP "$MSBUILD_DOWNLOAD_URL"
+
+        unzip -q $MSBUILD_ZIP -d $PACKAGES_DIR
+        rm $MSBUILD_ZIP
     fi
 }
 
 build()
 {
-	echo Build Command: "mono $MONO_ARGS"
+	echo Build Command: "$MONO_COMMAND"
 
-	mono $MONO_ARGS
+  eval "$MONO_COMMAND"
 
 	echo Build completed. Exit code: $?
 	egrep "Warning\(s\)|Error\(s\)|Time Elapsed" "$LOG_PATH_ARG"
@@ -32,11 +44,15 @@ THIS_SCRIPT_PATH="`dirname \"$0\"`"
 PACKAGES_DIR="$THIS_SCRIPT_PATH/packages"
 MSBUILD_EXE="$PACKAGES_DIR/mono-msbuild/bin/Unix/Debug-MONO/MSBuild.exe"
 MSBUILD_DOWNLOAD_URL="https://github.com/Microsoft/msbuild/releases/download/mono-hosted-msbuild-v0.1/mono-msbuild.zip"
+MSBUILD_ZIP="$PACKAGES_DIR/msbuild.zip"
+HOME_DEFAULT="/tmp/msbuild-CI-home"
+
 
 #Default build arguments
 TARGET_ARG="Build"
 LOG_PATH_ARG="$THIS_SCRIPT_PATH"/"msbuild.log"
 PROJECT_FILE_ARG="$THIS_SCRIPT_PATH"/"build.proj"
+
 
 #parse command line args
 while [[ $# > 0 ]]
@@ -85,7 +101,10 @@ fi
 
 MSBUILD_ARGS="$PROJECT_FILE_ARG /t:$TARGET_ARG /p:OS=$OS_ARG /p:Configuration=Debug-Netcore /verbosity:minimal"' "'"/fileloggerparameters:Verbosity=diag;LogFile=$LOG_PATH_ARG"'"'
 
-MONO_ARGS="$MSBUILD_EXE $MSBUILD_ARGS"
+MONO_COMMAND="mono $MSBUILD_EXE $MSBUILD_ARGS"
+
+#home is not defined on CI machines
+setHome
 
 downloadMSBuild
 
