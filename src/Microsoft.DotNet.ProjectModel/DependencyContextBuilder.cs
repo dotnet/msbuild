e39d30ba9c03465dcbbaccb9aafb64cfcdc21a17
Pavel Krymets <pavel@krymets.com>
2015-12-21 10:36:20 -0800

    Add compilation options to dependency context

--- src/Microsoft.DotNet.ProjectModel/DependencyContextBuilder.cs
+++ src/Microsoft.DotNet.ProjectModel/DependencyContextBuilder.cs
@@ -5,49 +5,99 @@ using System.Threading.Tasks;
 using Microsoft.DotNet.ProjectModel;
 using Microsoft.DotNet.ProjectModel.Compilation;
 using Microsoft.DotNet.ProjectModel.Graph;
+using NuGet.Frameworks;
 
 namespace Microsoft.Extensions.DependencyModel
 {
     public static class DependencyContextBuilder
     {
-        public static DependencyContext FromLibraryExporter(LibraryExporter libraryExporter, string target, string runtime)
+        public static DependencyContext Build(CommonCompilerOptions compilerOptions, LibraryExporter libraryExporter, string configuration, NuGetFramework target, string runtime)
         {
-            var dependencies = libraryExporter.GetAllExports();
+            var dependencies = libraryExporter.GetAllExports().Where(export => export.Library.Framework.Equals(target)).ToList();
 
-            return new DependencyContext(target, runtime,
-                GetLibraries(dependencies, export => export.CompilationAssemblies),
-                GetLibraries(dependencies, export => export.RuntimeAssemblies));
+            // Sometimes we have package and reference assembly with the same name (System.Runtime for example) thats why we
+            // deduplicating them prefering reference assembly
+            var dependencyLookup = dependencies
+                .OrderBy(export => export.Library.Identity.Type == LibraryType.ReferenceAssembly)
+                .GroupBy(export => export.Library.Identity.Name)
+                .Select(exports => exports.First())
+                .Select(export => new Dependency(export.Library.Identity.Name, export.Library.Identity.Version.ToString()))
+                .ToDictionary(dependency => dependency.Name);
+
+            return new DependencyContext(target.DotNetFrameworkName, runtime,
+                GetCompilationOptions(compilerOptions),
+                GetLibraries(dependencies, dependencyLookup, target, configuration, export => export.CompilationAssemblies),
+                GetLibraries(dependencies, dependencyLookup, target, configuration, export => export.RuntimeAssemblies));
         }
 
-        private static Library[] GetLibraries(IEnumerable<LibraryExport> dependencies, Func<LibraryExport, IEnumerable<LibraryAsset>> assemblySelector)
+        private static CompilationOptions GetCompilationOptions(CommonCompilerOptions compilerOptions)
         {
-            return dependencies.Select(export => GetLibrary(export, assemblySelector(export), dependencies)).ToArray();
+            return new CompilationOptions(compilerOptions.Defines,
+                compilerOptions.LanguageVersion,
+                compilerOptions.Platform,
+                compilerOptions.AllowUnsafe,
+                compilerOptions.WarningsAsErrors,
+                compilerOptions.Optimize,
+                compilerOptions.KeyFile,
+                compilerOptions.DelaySign,
+                compilerOptions.PublicSign,
+                compilerOptions.EmitEntryPoint);
         }
 
-        private static Library GetLibrary(LibraryExport export, IEnumerable<LibraryAsset> libraryAssets, IEnumerable<LibraryExport> dependencies)
+        private static Library[] GetLibraries(IEnumerable<LibraryExport> dependencies,
+            IDictionary<string, Dependency> dependencyLookup,
+            NuGetFramework target,
+            string configuration,
+            Func<LibraryExport, IEnumerable<LibraryAsset>> assemblySelector)
         {
+            return dependencies.Select(export => GetLibrary(export, target, configuration, assemblySelector(export), dependencyLookup)).ToArray();
+        }
+
+        private static Library GetLibrary(LibraryExport export,
+            NuGetFramework target,
+            string configuration,
+            IEnumerable<LibraryAsset> libraryAssets,
+            IDictionary<string, Dependency> dependencyLookup)
+        {
+            var type = export.Library.Identity.Type.Value.ToLowerInvariant();
+
             var serviceable = (export.Library as PackageDescription)?.Library.IsServiceable ?? false;
-            var version = dependencies.Where(dependency => dependency.Library.Identity == export.Library.Identity);
+            var libraryDependencies = new List<Dependency>();
 
-            var libraryDependencies = export.Library.Dependencies.Select(libraryRange => GetDependency(libraryRange, dependencies)).ToArray();
+            foreach (var libraryDependency in export.Library.Dependencies)
+            {
+                Dependency dependency;
+                if (dependencyLookup.TryGetValue(libraryDependency.Name, out dependency))
+                {
+                    libraryDependencies.Add(dependency);
+                }
+            }
+
+            string[] assemblies;
+            if (type == "project")
+            {
+                var isExe = ((ProjectDescription) export.Library)
+                    .Project
+                    .GetCompilerOptions(target, configuration)
+                    .EmitEntryPoint
+                    .GetValueOrDefault(false);
+
+                assemblies = new[] { export.Library.Identity.Name + (isExe ? ".exe": ".dll") };
+            }
+            else
+            {
+                assemblies = libraryAssets.Select(libraryAsset => libraryAsset.RelativePath).ToArray();
+            }
 
             return new Library(
-                export.Library.Identity.Type.ToString().ToLowerInvariant(),
+                type,
                 export.Library.Identity.Name,
                 export.Library.Identity.Version.ToString(),
                 export.Library.Hash,
-                libraryAssets.Select(libraryAsset => libraryAsset.RelativePath).ToArray(),
-                libraryDependencies,
+                assemblies,
+                libraryDependencies.ToArray(),
                 serviceable
                 );
-        }
-
-        private static Dependency GetDependency(LibraryRange libraryRange, IEnumerable<LibraryExport> dependencies)
-        {
-            var version =
-                dependencies.First(d => d.Library.Identity.Name == libraryRange.Name)
-                    .Library.Identity.Version.ToString();
-            return new Dependency(libraryRange.Name, version);
         }
     }
 }
--- src/Microsoft.DotNet.Tools.Compiler/Program.cs
+++ src/Microsoft.DotNet.Tools.Compiler/Program.cs
@@ -255,8 +255,11 @@ namespace Microsoft.DotNet.Tools.Compiler
 
             if (compilationOptions.PreserveCompilationContext == true)
             {
-                var dependencyContext = DependencyContextBuilder.FromLibraryExporter(
-                    libraryExporter, context.TargetFramework.DotNetFrameworkName, context.RuntimeIdentifier);
+                var dependencyContext = DependencyContextBuilder.Build(compilationOptions,
+                    libraryExporter,
+                    args.ConfigValue,
+                    context.TargetFramework,
+                    context.RuntimeIdentifier);
 
                 var writer = new DependencyContextWriter();
                 var depsJsonFile = Path.Combine(intermediateOutputPath, context.ProjectFile.Name + "dotnet-compile.deps.json");
