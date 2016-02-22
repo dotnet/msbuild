33ba99bb51399d169e5d38ab9852f60c870724b1
Livar Cunha <livar@live.com>
2016-02-22 13:40:45 -0800

    Adding a TestRunner.TestCompleted message that the runner can send to dotnet test.

--- src/Microsoft.Extensions.Testing.Abstractions/ITestSink.cs
+++ src/Microsoft.Extensions.Testing.Abstractions/ITestSink.cs
@@ -1,10 +1,10 @@
-// Copyright (c) .NET Foundation and contributors. All rights reserved.
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 namespace Microsoft.Extensions.Testing.Abstractions
 {
-    public interface ITestDiscoverySink : ITestSink
+    public interface ITestSink
     {
-        void SendTestFound(Test test);
+        void SendTestCompleted();
     }
 }
---
+++ src/Microsoft.Extensions.Testing.Abstractions/StreamingTestSink.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.IO;
+
+namespace Microsoft.Extensions.Testing.Abstractions
+{
+    public abstract class StreamingTestSink : ITestSink
+    {
+        protected LineDelimitedJsonStream Stream { get; }
+
+        protected StreamingTestSink(Stream stream)
+        {
+            Stream = new LineDelimitedJsonStream(stream);
+        }
+
+        public void SendTestCompleted()
+        {
+            Stream.Send(new Message
+            {
+                MessageType = "TestRunner.TestCompleted"
+            });
+        }
+    }
+}
