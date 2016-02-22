f0c8428925b3be56689ef57c814b525a3a9dee28
discostu105 <c.neumueller@gmail.com>
2016-02-22 11:25:02 +0100

    cleanup of AssemblyInfoOptions

--- src/Microsoft.DotNet.Compiler.Common/AssemblyInfoOptions.cs
+++ src/Microsoft.DotNet.Compiler.Common/AssemblyInfoOptions.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using System;
 using Microsoft.DotNet.ProjectModel;
 using System.Collections.Generic;
 using System.CommandLine;
