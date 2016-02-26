82d45c65da98990ab6e6b91eb25fd27fa68bf7b7
Daniel Plaisted <daplaist@microsoft.com>
2016-02-26 13:13:05 -0800

    Add simple test project for multiprocess build

---
+++ Samples/PortableTask/Sleep.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PortableTask
+{
+    public class Sleep : Microsoft.Build.Utilities.Task
+    {
+        public double Seconds { get; set; }
+
+        public override bool Execute()
+        {
+            Task.Delay(TimeSpan.FromSeconds(Seconds)).Wait();
+            return true;
+        }
+    }
+}
