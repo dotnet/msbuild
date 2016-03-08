a0db5086f6283ca91bc676fdc36d509488c8b8c9
Pavel Krymets <pavel@krymets.com>
2016-03-08 09:22:17 -0800

    Fix dependency context builder regression

--- src/Microsoft.DotNet.ProjectModel/DependencyContextBuilder.cs
+++ src/Microsoft.DotNet.ProjectModel/DependencyContextBuilder.cs
@@ -84,7 +84,7 @@ namespace Microsoft.Extensions.DependencyModel
             var type = export.Library.Identity.Type;
 
             var serviceable = (export.Library as PackageDescription)?.Library.IsServiceable ?? false;
-            var libraryDependencies = new List<Dependency>();
+            var libraryDependencies = new HashSet<Dependency>();
 
             var libraryAssets = runtime ? export.RuntimeAssemblies : export.CompilationAssemblies;
 
