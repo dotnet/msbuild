9cec3e31521cc32128874149717d99f911313e30
Troy Dai <troy.dai@outlook.com>
2016-04-03 19:29:23 -0700

    Add two tests for project model server

---
+++ TestAssets/ProjectModelServer/DthTestProjects/src/EmptyNetCoreApp/Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Misc.DthTestProjects.SimpleConsoleApp
+{
+    public class Program
+    {
+        public int Main(string[] args)
+        {
+            Console.WriteLine("Hello, world.");
+            return 0;
+        }
+    }
+}
