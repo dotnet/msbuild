c7b7ef5458ab0b1ab18c51be81ec571c9a7f0333
Ankit Jain <ankit.jain@xamarin.com>
2016-04-08 13:24:48 -0400

    Fix regression in use of PipeSecurity
    - commit 583a73e21ac36093e285d473eefec701d9f60d5f was incorrect,
      see https://github.com/Microsoft/msbuild/pull/560#discussion_r59051335
    
      Essentially, FEATURE_PIPE_SECURITY means that `PipeSecurity` is
      available in the framework. Revert the earlier fix.
    
    - this patch also enables FEATURE_PIPE_SECURITY for mono builds
    
    - Fixes windows build broken by the regression

--- src/XMakeBuildEngine/BackEnd/Components/Communications/NodeProviderOutOfProcBase.cs
+++ src/XMakeBuildEngine/BackEnd/Components/Communications/NodeProviderOutOfProcBase.cs
@@ -317,7 +317,7 @@ namespace Microsoft.Build.BackEnd
         private void ValidateRemotePipeSecurityOnWindows(NamedPipeClientStream nodeStream)
         {
             SecurityIdentifier identifier = WindowsIdentity.GetCurrent().Owner;
-#if !FEATURE_PIPE_SECURITY
+#if FEATURE_PIPE_SECURITY
             PipeSecurity remoteSecurity = nodeStream.GetAccessControl();
 #else
             var remoteSecurity = new PipeSecurity(nodeStream.SafePipeHandle, System.Security.AccessControl.AccessControlSections.Access |
