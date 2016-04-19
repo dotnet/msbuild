f4f05656a10e953a75d085b1efc2591d2689cbdb
Bryan <brthor@microsoft.com>
2016-04-19 14:38:24 -0700

    Take into account Code Review Feedback

--- test/Microsoft.DotNet.Cli.Utils.Tests/GivenADesktopAppWhichUsesCommandCreateDotnet.cs
+++ test/Microsoft.DotNet.Cli.Utils.Tests/GivenADesktopAppWhichUsesCommandCreateDotnet.cs
@@ -2,20 +2,9 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Linq;
-using Xunit;
-using Moq;
-using Microsoft.DotNet.Cli.Utils;
-using Microsoft.DotNet.ProjectModel;
 using Microsoft.DotNet.Tools.Test.Utilities;
-using Microsoft.Extensions.PlatformAbstractions;
-using System.Threading;
 using FluentAssertions;
-using NuGet.Frameworks;
 
 namespace Microsoft.DotNet.Cli.Utils.Tests
 {
