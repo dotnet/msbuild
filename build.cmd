0e4ec7c38361c57b1389ecec725a6d313e029db3
Mihai Codoban <micodoba@microsoft.com>
2016-05-12 14:45:35 -0700

    Update build scripts for bootstrap build

--- build.cmd
+++ build.cmd
@@ -1,12 +1,21 @@
 @echo off
 setlocal
 
-set MSBUILD_ARGS="%~dp0build.proj" /m /verbosity:minimal /fileloggerparameters:Verbosity=diag;LogFile="%~dp0msbuild.log" %*
+if not defined MSBUILDLOGPATH (
+    set MSBUILDLOGPATH=%~dp0msbuild.log
+)
 
 :: Check for a custom MSBuild path. If not defined, default to the one in your path.
 if not defined MSBUILD_CUSTOM_PATH (
     set MSBUILD_CUSTOM_PATH=MSBuild.exe
 )
+
+if not defined MSBUILD_ARGS (
+    set MSBUILD_ARGS="%~dp0build.proj" /m /verbosity:minimal %*
+)
+
+:: Add a the file logger with diagnostic verbosity to the msbuild args
+set MSBUILD_ARGS=%MSBUILD_ARGS% /fileloggerparameters:Verbosity=diag;LogFile="%MSBUILDLOGPATH%"
 
 :: Check for a runtime host. If not defined, do not use a host
 if not defined RUNTIME_HOST (
@@ -14,7 +23,7 @@ if not defined RUNTIME_HOST (
 
     :: Check prerequisites for full framework build
  	if not "%VisualStudioVersion%" == "14.0" (
-	    echo Error: build.cmd should be run from a Visual Studio 2015 Command Prompt.  
+	    echo Error: build.cmd should be run from a Visual Studio 2015 Command when RUNTIME_HOST is not defined. Prompt.  
 	    echo        Please see https://github.com/Microsoft/msbuild/wiki/Building-Testing-and-Debugging for build instructions.
 	    exit /b 1
 	)
@@ -22,12 +31,9 @@ if not defined RUNTIME_HOST (
 	set BUILD_COMMAND= "%RUNTIME_HOST%" "%MSBUILD_CUSTOM_PATH%" %MSBUILD_ARGS%
 )
 
-echo ** MSBuild Path: %MSBUILD_CUSTOM_PATH%
-echo ** Runtime Host Path: %RUNTIME_HOST%
-echo ** Building all sources
-
-:: Restore build tools
-call %~dp0init-tools.cmd
+echo.
+echo ** Using the MSBuild in path: %MSBUILD_CUSTOM_PATH%
+echo ** Using runtime host in path: %RUNTIME_HOST%
 
 :: Call MSBuild
 echo ** %BUILD_COMMAND%
@@ -36,7 +42,8 @@ set BUILDERRORLEVEL=%ERRORLEVEL%
 echo.
 
 :: Pull the build summary from the log file
-findstr /ir /c:".*Warning(s)" /c:".*Error(s)" /c:"Time Elapsed.*" "%~dp0msbuild.log"
+findstr /ir /c:".*Warning(s)" /c:".*Error(s)" /c:"Time Elapsed.*" "%MSBUILDLOGPATH%"
+echo.
 echo ** Build completed. Exit code: %BUILDERRORLEVEL%
 
 exit /b %BUILDERRORLEVEL%
