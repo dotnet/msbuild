JohnChen0/cli

Bring Host build into separate project

build_projects/dotnet-host-build/PrepareTargets.cs
@@ -1,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using Microsoft.DotNet.Cli.Build.Framework;
+using Microsoft.DotNet.InternalAbstractions;
+using Newtonsoft.Json.Linq;
+using static Microsoft.DotNet.Cli.Build.Framework.BuildHelpers;
+using static Microsoft.DotNet.Cli.Build.FS;
+using static Microsoft.DotNet.Cli.Build.Utils;
+
+namespace Microsoft.DotNet.Cli.Build
+{
+    public class PrepareTargets
+    {
+        [Target(nameof(Init))]
+        public static BuildTargetResult Prepare(BuildTargetContext c) => c.Success();
+
+        [Target(nameof(CheckPrereqCmakePresent), nameof(CheckPlatformDependencies))]
+        public static BuildTargetResult CheckPrereqs(BuildTargetContext c) => c.Success();
+
+        [Target(nameof(CheckCoreclrPlatformDependencies))]
+        public static BuildTargetResult CheckPlatformDependencies(BuildTargetContext c) => c.Success();
+
+        [Target(nameof(CheckUbuntuCoreclrAndCoreFxDependencies), nameof(CheckCentOSCoreclrAndCoreFxDependencies))]
+        public static BuildTargetResult CheckCoreclrPlatformDependencies(BuildTargetContext c) => c.Success();
+
+        // All major targets will depend on this in order to ensure variables are set up right if they are run independently
+        [Target(nameof(GenerateVersions), nameof(CheckPrereqs), nameof(LocateStage0), nameof(ExpectedBuildArtifacts))]
+        public static BuildTargetResult Init(BuildTargetContext c)
+        {
+            var configEnv = Environment.GetEnvironmentVariable("CONFIGURATION");
+
+            if (string.IsNullOrEmpty(configEnv))
+            {
+                configEnv = "Debug";
+            }
+
+            c.BuildContext["Configuration"] = configEnv;
+            c.BuildContext["Channel"] = Environment.GetEnvironmentVariable("CHANNEL");
+
+            c.Info($"Building {c.BuildContext["Configuration"]} to: {Dirs.Output}");
+            c.Info("Build Environment:");
+            c.Info($" Operating System: {RuntimeEnvironment.OperatingSystem} {RuntimeEnvironment.OperatingSystemVersion}");
+            c.Info($" Platform: {RuntimeEnvironment.OperatingSystemPlatform}");
+
+            return c.Success();
+        }
+
+        [Target]
+        public static BuildTargetResult GenerateVersions(BuildTargetContext c)
+        {
+            var gitResult = Cmd("git", "rev-list", "--count", "HEAD")
+                .CaptureStdOut()
+                .Execute();
+            gitResult.EnsureSuccessful();
+            var commitCount = int.Parse(gitResult.StdOut);
+
+            gitResult = Cmd("git", "rev-parse", "HEAD")
+                .CaptureStdOut()
+                .Execute();
+            gitResult.EnsureSuccessful();
+            var commitHash = gitResult.StdOut.Trim();
+
+            var hostVersion = new HostVersion()
+            {
+                CommitCount = commitCount
+            };
+
+            c.BuildContext["HostVersion"] = hostVersion;
+            c.BuildContext["CommitHash"] = commitHash;
+
+            c.Info($"Building Version: {hostVersion.LatestHostVersionNoSuffix} (NuGet Packages: {hostVersion.LatestHostVersion})");
+            c.Info($"From Commit: {commitHash}");
+
+            return c.Success();
+        }
+
+        [Target]
+        public static BuildTargetResult LocateStage0(BuildTargetContext c)
+        {
+            // We should have been run in the repo root, so locate the stage 0 relative to current directory
+            var stage0 = DotNetCli.Stage0.BinPath;
+
+            if (!Directory.Exists(stage0))
+            {
+                return c.Failed($"Stage 0 directory does not exist: {stage0}");
+            }
+
+            // Identify the version
+            string versionFile = Directory.GetFiles(stage0, ".version", SearchOption.AllDirectories).FirstOrDefault();
+
+            if (string.IsNullOrEmpty(versionFile))
+            {
+                throw new Exception($"'.version' file not found in '{stage0}' folder");
+            }
+
+            var version = File.ReadAllLines(versionFile);
+            c.Info($"Using Stage 0 Version: {version[1]}");
+
+            return c.Success();
+        }
+
+        [Target]
+        public static BuildTargetResult ExpectedBuildArtifacts(BuildTargetContext c)
+        {
+            var config = Environment.GetEnvironmentVariable("CONFIGURATION");
+            var versionBadgeName = $"{CurrentPlatform.Current}_{CurrentArchitecture.Current}_{config}_version_badge.svg";
+            c.BuildContext["VersionBadge"] = Path.Combine(Dirs.Output, versionBadgeName);
+
+            var hostVersion = c.BuildContext.Get<HostVersion>("HostVersion").LockedHostVersion;
+            return c.Success();
+        }
+
+        [Target]
+        [BuildPlatforms(BuildPlatform.Ubuntu)]
+        public static BuildTargetResult CheckUbuntuCoreclrAndCoreFxDependencies(BuildTargetContext c)
+        {
+            var errorMessageBuilder = new StringBuilder();
+            var stage0 = DotNetCli.Stage0.BinPath;
+
+            foreach (var package in PackageDependencies.UbuntuCoreclrAndCoreFxDependencies)
+            {
+                if (!AptDependencyUtility.PackageIsInstalled(package))
+                {
+                    errorMessageBuilder.Append($"Error: Coreclr package dependency {package} missing.");
+                    errorMessageBuilder.Append(Environment.NewLine);
+                    errorMessageBuilder.Append($"-> install with apt-get install {package}");
+                    errorMessageBuilder.Append(Environment.NewLine);
+                }
+            }
+
+            if (errorMessageBuilder.Length == 0)
+            {
+                return c.Success();
+            }
+            else
+            {
+                return c.Failed(errorMessageBuilder.ToString());
+            }
+        }
+
+        [Target]
+        [BuildPlatforms(BuildPlatform.CentOS)]
+        public static BuildTargetResult CheckCentOSCoreclrAndCoreFxDependencies(BuildTargetContext c)
+        {
+            var errorMessageBuilder = new StringBuilder();
+
+            foreach (var package in PackageDependencies.CentosCoreclrAndCoreFxDependencies)
+            {
+                if (!YumDependencyUtility.PackageIsInstalled(package))
+                {
+                    errorMessageBuilder.Append($"Error: Coreclr package dependency {package} missing.");
+                    errorMessageBuilder.Append(Environment.NewLine);
+                    errorMessageBuilder.Append($"-> install with yum install {package}");
+                    errorMessageBuilder.Append(Environment.NewLine);
+                }
+            }
+
+            if (errorMessageBuilder.Length == 0)
+            {
+                return c.Success();
+            }
+            else
+            {
+                return c.Failed(errorMessageBuilder.ToString());
+            }
+        }
+
+        [Target]
+        public static BuildTargetResult CheckPrereqCmakePresent(BuildTargetContext c)
+        {
+            try
+            {
+                Command.Create("cmake", "--version")
+                    .CaptureStdOut()
+                    .CaptureStdErr()
+                    .Execute();
+            }
+            catch (Exception ex)
+            {
+                string message = $@"Error running cmake: {ex.Message}
+cmake is required to build the native host 'corehost'";
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                {
+                    message += Environment.NewLine + "Download it from https://www.cmake.org";
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                {
+                    message += Environment.NewLine + "Ubuntu: 'sudo apt-get install cmake'";
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+                {
+                    message += Environment.NewLine + "OS X w/Homebrew: 'brew install cmake'";
+                }
+                return c.Failed(message);
+            }
+
+            return c.Success();
+        }
+    }
+}

build_projects/dotnet-host-build/Program.cs
@@ -1,0 +1,17 @@
+﻿using Microsoft.DotNet.Cli.Build.Framework;
+
+namespace Microsoft.DotNet.Cli.Build
+{
+    public class Program
+    {
+        public static int Main(string[] args)
+        {
+            DebugHelper.HandleDebugSwitch(ref args);
+
+            return BuildSetup.Create(".NET Core Host")
+                .UseStandardGoals()
+                .UseAllTargetsFromAssembly<Program>()
+                .Run(args);
+        } 
+    }
+}

build_projects/shared-build-targets-utils/Utils/StubPackageBuilder.cs
@@ -1,0 +1,101 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using Microsoft.DotNet.Cli.Build.Framework;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+using static Microsoft.DotNet.Cli.Build.Framework.BuildHelpers;
+
+namespace Microsoft.DotNet.Cli.Build
+{
+    public class StubPackageBuilder
+    {
+        private DotNetCli _dotnet;
+        private string _intermediateDirectory;
+        private string _outputDirectory;
+
+        private bool _stubBitsBuilt = false;
+
+        public StubPackageBuilder(DotNetCli dotnet, string intermediateDirectory, string outputDirectory)
+        {
+            _dotnet = dotnet;
+            _intermediateDirectory = intermediateDirectory;
+            _outputDirectory = outputDirectory;
+        }
+
+        public void GeneratePackage(string packageId, string version)
+        {
+            if (! _stubBitsBuilt)
+            {
+                BuildStubBits(_dotnet, _intermediateDirectory);
+            }
+
+            CreateStubPackage(_dotnet, packageId, version, _intermediateDirectory, _outputDirectory);
+        }
+
+        private void BuildStubBits(DotNetCli dotnet, string intermediateDirectory)
+        {
+            var projectJson = new StringBuilder();
+            projectJson.Append("{");
+            projectJson.Append("  \"dependencies\": { \"NETStandard.Library\": \"1.5.0-rc2-24008\" },");
+            projectJson.Append("  \"frameworks\": { \"netcoreapp1.0\": { \"imports\": [\"netstandard1.5\", \"dnxcore50\"] } },");
+            projectJson.Append("  \"runtimes\": { \"win7-x64\": { } },");
+            projectJson.Append("}");
+
+            var programCs = "using System; namespace ConsoleApplication { public class Program { public static void Main(string[] args) { Console.WriteLine(\"Hello World!\"); } } }";
+
+            var tempPjDirectory = Path.Combine(intermediateDirectory, "dummyNuGetPackageIntermediate");
+            FS.Rmdir(tempPjDirectory);
+
+            Directory.CreateDirectory(tempPjDirectory);
+
+            var tempPjFile = Path.Combine(tempPjDirectory, "project.json");
+            var tempSourceFile = Path.Combine(tempPjDirectory, "Program.cs");
+
+            File.WriteAllText(tempPjFile, projectJson.ToString());
+            File.WriteAllText(tempSourceFile, programCs.ToString());
+
+            dotnet.Restore("--verbosity", "verbose", "--disable-parallel")
+                .WorkingDirectory(tempPjDirectory)
+                .Execute()
+                .EnsureSuccessful();
+            dotnet.Build(tempPjFile, "--runtime", "win7-x64")
+                .WorkingDirectory(tempPjDirectory)
+                .Execute()
+                .EnsureSuccessful();
+
+            _stubBitsBuilt = true;
+        }
+
+        private static void CreateStubPackage(DotNetCli dotnet, 
+            string packageId, 
+            string version,
+            string intermediateDirectory, 
+            string outputDirectory)
+        {
+            var projectJson = new StringBuilder();
+            projectJson.Append("{");
+            projectJson.Append($"  \"version\": \"{version}\",");
+            projectJson.Append($"  \"name\": \"{packageId}\",");
+            projectJson.Append("  \"dependencies\": { \"NETStandard.Library\": \"1.5.0-rc2-24008\" },");
+            projectJson.Append("  \"frameworks\": { \"netcoreapp1.0\": { \"imports\": [\"netstandard1.5\", \"dnxcore50\"] } },");
+            projectJson.Append("}");
+
+            var tempPjDirectory = Path.Combine(intermediateDirectory, "dummyNuGetPackageIntermediate");
+            var tempPjFile = Path.Combine(tempPjDirectory, "project.json");
+
+            File.WriteAllText(tempPjFile, projectJson.ToString());
+
+            dotnet.Pack(
+                tempPjFile, "--no-build",
+                "--output", outputDirectory)
+                .WorkingDirectory(tempPjDirectory)
+                .Execute()
+                .EnsureSuccessful();
+        }
+    }
+}

dotnet/msbuild

build_projects/dotnet-host-build/PrepareTargets.cs
@@ -1,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using Microsoft.DotNet.Cli.Build.Framework;
+using Microsoft.DotNet.InternalAbstractions;
+using Newtonsoft.Json.Linq;
+using Microsoft.DotNet.Cli.Build;
+using static Microsoft.DotNet.Cli.Build.Framework.BuildHelpers;
+using static Microsoft.DotNet.Cli.Build.FS;
+using static Microsoft.DotNet.Cli.Build.Utils;
+
+namespace Microsoft.DotNet.Host.Build
+{
+    public class PrepareTargets
+    {
+        [Target(nameof(Init))]
+        public static BuildTargetResult Prepare(BuildTargetContext c) => c.Success();
+
+        [Target(nameof(CheckPrereqCmakePresent), nameof(CheckPlatformDependencies))]
+        public static BuildTargetResult CheckPrereqs(BuildTargetContext c) => c.Success();
+
+        [Target(nameof(CheckCoreclrPlatformDependencies))]
+        public static BuildTargetResult CheckPlatformDependencies(BuildTargetContext c) => c.Success();
+
+        [Target(nameof(CheckUbuntuCoreclrAndCoreFxDependencies), nameof(CheckCentOSCoreclrAndCoreFxDependencies))]
+        public static BuildTargetResult CheckCoreclrPlatformDependencies(BuildTargetContext c) => c.Success();
+
+        // All major targets will depend on this in order to ensure variables are set up right if they are run independently
+        [Target(nameof(GenerateVersions), nameof(CheckPrereqs), nameof(LocateStage0), nameof(ExpectedBuildArtifacts))]
+        public static BuildTargetResult Init(BuildTargetContext c)
+        {
+            var configEnv = Environment.GetEnvironmentVariable("CONFIGURATION");
+
+            if (string.IsNullOrEmpty(configEnv))
+            {
+                configEnv = "Debug";
+            }
+
+            c.BuildContext["Configuration"] = configEnv;
+            c.BuildContext["Channel"] = Environment.GetEnvironmentVariable("CHANNEL");
+
+            c.Info($"Building {c.BuildContext["Configuration"]} to: {Dirs.Output}");
+            c.Info("Build Environment:");
+            c.Info($" Operating System: {RuntimeEnvironment.OperatingSystem} {RuntimeEnvironment.OperatingSystemVersion}");
+            c.Info($" Platform: {RuntimeEnvironment.OperatingSystemPlatform}");
+
+            return c.Success();
+        }
+
+        [Target]
+        public static BuildTargetResult GenerateVersions(BuildTargetContext c)
+        {
+            var gitResult = Cmd("git", "rev-list", "--count", "HEAD")
+                .CaptureStdOut()
+                .Execute();
+            gitResult.EnsureSuccessful();
+            var commitCount = int.Parse(gitResult.StdOut);
+
+            gitResult = Cmd("git", "rev-parse", "HEAD")
+                .CaptureStdOut()
+                .Execute();
+            gitResult.EnsureSuccessful();
+            var commitHash = gitResult.StdOut.Trim();
+
+            var hostVersion = new HostVersion()
+            {
+                CommitCount = commitCount
+            };
+
+            c.BuildContext["HostVersion"] = hostVersion;
+            c.BuildContext["CommitHash"] = commitHash;
+
+            c.Info($"Building Version: {hostVersion.LatestHostVersionNoSuffix} (NuGet Packages: {hostVersion.LatestHostVersion})");
+            c.Info($"From Commit: {commitHash}");
+
+            return c.Success();
+        }
+
+        [Target]
+        public static BuildTargetResult LocateStage0(BuildTargetContext c)
+        {
+            // We should have been run in the repo root, so locate the stage 0 relative to current directory
+            var stage0 = DotNetCli.Stage0.BinPath;
+
+            if (!Directory.Exists(stage0))
+            {
+                return c.Failed($"Stage 0 directory does not exist: {stage0}");
+            }
+
+            // Identify the version
+            string versionFile = Directory.GetFiles(stage0, ".version", SearchOption.AllDirectories).FirstOrDefault();
+
+            if (string.IsNullOrEmpty(versionFile))
+            {
+                throw new Exception($"'.version' file not found in '{stage0}' folder");
+            }
+
+            var version = File.ReadAllLines(versionFile);
+            c.Info($"Using Stage 0 Version: {version[1]}");
+
+            return c.Success();
+        }
+
+        [Target]
+        public static BuildTargetResult ExpectedBuildArtifacts(BuildTargetContext c)
+        {
+            var config = Environment.GetEnvironmentVariable("CONFIGURATION");
+            var versionBadgeName = $"{CurrentPlatform.Current}_{CurrentArchitecture.Current}_{config}_version_badge.svg";
+            c.BuildContext["VersionBadge"] = Path.Combine(Dirs.Output, versionBadgeName);
+
+            var hostVersion = c.BuildContext.Get<HostVersion>("HostVersion").LockedHostVersion;
+            return c.Success();
+        }
+
+        [Target]
+        [BuildPlatforms(BuildPlatform.Ubuntu)]
+        public static BuildTargetResult CheckUbuntuCoreclrAndCoreFxDependencies(BuildTargetContext c)
+        {
+            var errorMessageBuilder = new StringBuilder();
+            var stage0 = DotNetCli.Stage0.BinPath;
+
+            foreach (var package in PackageDependencies.UbuntuCoreclrAndCoreFxDependencies)
+            {
+                if (!AptDependencyUtility.PackageIsInstalled(package))
+                {
+                    errorMessageBuilder.Append($"Error: Coreclr package dependency {package} missing.");
+                    errorMessageBuilder.Append(Environment.NewLine);
+                    errorMessageBuilder.Append($"-> install with apt-get install {package}");
+                    errorMessageBuilder.Append(Environment.NewLine);
+                }
+            }
+
+            if (errorMessageBuilder.Length == 0)
+            {
+                return c.Success();
+            }
+            else
+            {
+                return c.Failed(errorMessageBuilder.ToString());
+            }
+        }
+
+        [Target]
+        [BuildPlatforms(BuildPlatform.CentOS)]
+        public static BuildTargetResult CheckCentOSCoreclrAndCoreFxDependencies(BuildTargetContext c)
+        {
+            var errorMessageBuilder = new StringBuilder();
+
+            foreach (var package in PackageDependencies.CentosCoreclrAndCoreFxDependencies)
+            {
+                if (!YumDependencyUtility.PackageIsInstalled(package))
+                {
+                    errorMessageBuilder.Append($"Error: Coreclr package dependency {package} missing.");
+                    errorMessageBuilder.Append(Environment.NewLine);
+                    errorMessageBuilder.Append($"-> install with yum install {package}");
+                    errorMessageBuilder.Append(Environment.NewLine);
+                }
+            }
+
+            if (errorMessageBuilder.Length == 0)
+            {
+                return c.Success();
+            }
+            else
+            {
+                return c.Failed(errorMessageBuilder.ToString());
+            }
+        }
+
+        [Target]
+        public static BuildTargetResult CheckPrereqCmakePresent(BuildTargetContext c)
+        {
+            try
+            {
+                Command.Create("cmake", "--version")
+                    .CaptureStdOut()
+                    .CaptureStdErr()
+                    .Execute();
+            }
+            catch (Exception ex)
+            {
+                string message = $@"Error running cmake: {ex.Message}
+cmake is required to build the native host 'corehost'";
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                {
+                    message += Environment.NewLine + "Download it from https://www.cmake.org";
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                {
+                    message += Environment.NewLine + "Ubuntu: 'sudo apt-get install cmake'";
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+                {
+                    message += Environment.NewLine + "OS X w/Homebrew: 'brew install cmake'";
+                }
+                return c.Failed(message);
+            }
+
+            return c.Success();
+        }
+    }
+}

build_projects/dotnet-host-build/Program.cs
@@ -1,0 +1,17 @@
+﻿using Microsoft.DotNet.Cli.Build.Framework;
+
+namespace Microsoft.DotNet.Host.Build
+{
+    public class Program
+    {
+        public static int Main(string[] args)
+        {
+            DebugHelper.HandleDebugSwitch(ref args);
+
+            return BuildSetup.Create(".NET Core Host")
+                .UseStandardGoals()
+                .UseAllTargetsFromAssembly<Program>()
+                .Run(args);
+        } 
+    }
+}

build_projects/dotnet-host-build/StubPackageBuilder.cs
@@ -1,0 +1,102 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using Microsoft.DotNet.Cli.Build.Framework;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.DotNet.Cli.Build;
+
+using static Microsoft.DotNet.Cli.Build.Framework.BuildHelpers;
+
+namespace Microsoft.DotNet.Host.Build
+{
+    public class StubPackageBuilder
+    {
+        private DotNetCli _dotnet;
+        private string _intermediateDirectory;
+        private string _outputDirectory;
+
+        private bool _stubBitsBuilt = false;
+
+        public StubPackageBuilder(DotNetCli dotnet, string intermediateDirectory, string outputDirectory)
+        {
+            _dotnet = dotnet;
+            _intermediateDirectory = intermediateDirectory;
+            _outputDirectory = outputDirectory;
+        }
+
+        public void GeneratePackage(string packageId, string version)
+        {
+            if (! _stubBitsBuilt)
+            {
+                BuildStubBits(_dotnet, _intermediateDirectory);
+            }
+
+            CreateStubPackage(_dotnet, packageId, version, _intermediateDirectory, _outputDirectory);
+        }
+
+        private void BuildStubBits(DotNetCli dotnet, string intermediateDirectory)
+        {
+            var projectJson = new StringBuilder();
+            projectJson.Append("{");
+            projectJson.Append("  \"dependencies\": { \"NETStandard.Library\": \"1.5.0-rc2-24008\" },");
+            projectJson.Append("  \"frameworks\": { \"netcoreapp1.0\": { \"imports\": [\"netstandard1.5\", \"dnxcore50\"] } },");
+            projectJson.Append("  \"runtimes\": { \"win7-x64\": { } },");
+            projectJson.Append("}");
+
+            var programCs = "using System; namespace ConsoleApplication { public class Program { public static void Main(string[] args) { Console.WriteLine(\"Hello World!\"); } } }";
+
+            var tempPjDirectory = Path.Combine(intermediateDirectory, "dummyNuGetPackageIntermediate");
+            FS.Rmdir(tempPjDirectory);
+
+            Directory.CreateDirectory(tempPjDirectory);
+
+            var tempPjFile = Path.Combine(tempPjDirectory, "project.json");
+            var tempSourceFile = Path.Combine(tempPjDirectory, "Program.cs");
+
+            File.WriteAllText(tempPjFile, projectJson.ToString());
+            File.WriteAllText(tempSourceFile, programCs.ToString());
+
+            dotnet.Restore("--verbosity", "verbose", "--disable-parallel")
+                .WorkingDirectory(tempPjDirectory)
+                .Execute()
+                .EnsureSuccessful();
+            dotnet.Build(tempPjFile, "--runtime", "win7-x64")
+                .WorkingDirectory(tempPjDirectory)
+                .Execute()
+                .EnsureSuccessful();
+
+            _stubBitsBuilt = true;
+        }
+
+        private static void CreateStubPackage(DotNetCli dotnet, 
+            string packageId, 
+            string version,
+            string intermediateDirectory, 
+            string outputDirectory)
+        {
+            var projectJson = new StringBuilder();
+            projectJson.Append("{");
+            projectJson.Append($"  \"version\": \"{version}\",");
+            projectJson.Append($"  \"name\": \"{packageId}\",");
+            projectJson.Append("  \"dependencies\": { \"NETStandard.Library\": \"1.5.0-rc2-24008\" },");
+            projectJson.Append("  \"frameworks\": { \"netcoreapp1.0\": { \"imports\": [\"netstandard1.5\", \"dnxcore50\"] } },");
+            projectJson.Append("}");
+
+            var tempPjDirectory = Path.Combine(intermediateDirectory, "dummyNuGetPackageIntermediate");
+            var tempPjFile = Path.Combine(tempPjDirectory, "project.json");
+
+            File.WriteAllText(tempPjFile, projectJson.ToString());
+
+            dotnet.Pack(
+                tempPjFile, "--no-build",
+                "--output", outputDirectory)
+                .WorkingDirectory(tempPjDirectory)
+                .Execute()
+                .EnsureSuccessful();
+        }
+    }
+}