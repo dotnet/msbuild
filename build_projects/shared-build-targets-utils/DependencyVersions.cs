e9e4427b42f4a8db052d300862b0969206dc84b2
Senthil <schellap@microsoft.com>
2016-05-25 23:12:19 -0700

    Update core-setup dependency versions (#3231)
    Pick up host with JIT_PATH property

--- build_projects/shared-build-targets-utils/DependencyVersions.cs
+++ build_projects/shared-build-targets-utils/DependencyVersions.cs
@@ -9,8 +9,8 @@ namespace Microsoft.DotNet.Cli.Build
     {
         public static readonly string CoreCLRVersion = "1.0.2-rc3-24123-01";
 
-        public static readonly string SharedFrameworkVersion = "1.0.0-rc3-004306";
-        public static readonly string SharedHostVersion = "1.0.1-rc3-004306-00";
+        public static readonly string SharedFrameworkVersion = "1.0.0-rc3-004308";
+        public static readonly string SharedHostVersion = "1.0.1-rc3-004308-00";
 
         public static readonly string SharedFrameworkChannel = "preview";
         public static readonly string SharedHostChannel = "preview";
