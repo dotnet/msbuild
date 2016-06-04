40bf17900a7c692733e3da3dd684b45267c4b31e
Eric St. John <ericstj@microsoft.com>
2016-06-04 01:13:36 -0700

    Add IndexedArchive
    This type will archive multiple files de-duplicated in a zip with a
    central manifest that describes how to recreate the actual layout.
    In addition, any other zip file will be expanded and deduplicated
    so that we can further reduce the size of those zips/nupkgs.
    All of these are placed in a zip with no-compression, essentially
    using the zip as only a container.  We then LZMA compress that
    container to achieve maximum compression.

---
+++ src/Microsoft.DotNet.Archive/ProgressReport.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.DotNet.Archive
+{
+    public struct ProgressReport
+    {
+        public string Phase;
+        public long Ticks;
+        public long Total;
+    }
+
+    public static class ProgressReportExtensions
+    {
+        public static void Report(this IProgress<ProgressReport> progress, string phase, long ticks, long total)
+        {
+            progress.Report(new ProgressReport()
+            {
+                Phase = phase,
+                Ticks = ticks,
+                Total = total
+            });
+        }
+    }
+
+}
---
+++ src/Microsoft.DotNet.Archive/ThreadLocalZipArchive.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microsoft.DotNet.Archive
+{
+    // wraps ThreadLocal<ZipArchive> and exposes Dispose semantics that dispose all archives
+    class ThreadLocalZipArchive : IDisposable
+    {
+        private ThreadLocal<ZipArchive> _archive;
+        private bool disposed = false;
+
+        public ThreadLocalZipArchive(string archivePath, ZipArchive local = null)
+        {
+            _archive = new ThreadLocal<ZipArchive>(() =>
+                         new ZipArchive(File.Open(archivePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite | FileShare.Delete), ZipArchiveMode.Read),
+                         trackAllValues:true);
+
+            if (local != null)
+            {
+                // reuse provided one for current thread
+                _archive.Value = local;
+            }
+        }
+
+        public ZipArchive Archive { get { return _archive.Value; } }
+
+        public void Dispose()
+        {
+            if (!disposed)
+            {
+                if (_archive != null)
+                {
+                    // dispose all archives
+                    if (_archive.Values != null)
+                    {
+                        foreach (var value in _archive.Values)
+                        {
+                            if (value != null)
+                            {
+                                value.Dispose();
+                            }
+                        }
+                    }
+
+                    // dispose ThreadLocal
+                    _archive.Dispose();
+                    _archive = null;
+                }
+            }
+        }
+    }
+}
