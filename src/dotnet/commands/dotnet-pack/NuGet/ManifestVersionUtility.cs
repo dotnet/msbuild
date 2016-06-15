f0380cfd9c1c9d7a864e3176fe7b56a0f79e6457
Bryan Thornbury <brthor@microsoft.com>
2016-06-15 10:23:16 -0700

    replicate https://github.com/dotnet/cli/pull/3492

--- src/dotnet/commands/dotnet-pack/NuGet/ManifestVersionUtility.cs
+++ src/dotnet/commands/dotnet-pack/NuGet/ManifestVersionUtility.cs
@@ -15,9 +15,6 @@ namespace NuGet
         public const int TargetFrameworkSupportForDependencyContentsAndToolsVersion = 4;
         public const int TargetFrameworkSupportForReferencesVersion = 5;
         public const int XdtTransformationVersion = 6;
-        // Note that this version should change from 7 to 8 when the PackageType
-        // schema is merged into here
-        public const int ServiceableVersion = 7;
 
         public static int GetManifestVersion(ManifestMetadata metadata)
         {
@@ -26,6 +23,8 @@ namespace NuGet
 
         private static int GetMaxVersionFromMetadata(ManifestMetadata metadata)
         {
+            // Important: always add newer version checks at the top
+            
             bool referencesHasTargetFramework =
               metadata.PackageAssemblyReferences != null &&
               metadata.PackageAssemblyReferences.Any(r => r.TargetFramework != null);
