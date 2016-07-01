3c789c8230e05f7f245e6dd77c41d12de461c37b
Livar Cunha <livar@live.com>
2016-07-01 11:02:26 -0700

    Addressing more code review comments

--- build_projects/dotnet-cli-build/FixModeFlags.cs
+++ build_projects/dotnet-cli-build/FixModeFlags.cs
@@ -4,11 +4,10 @@ using System.IO;
 using System.IO.Compression;
 using System.Runtime.InteropServices;
 using System.Net.Http;
+using Microsoft.Build.Framework;
 using Microsoft.Build.Utilities;
 using Microsoft.DotNet.Cli.Build.Framework;
 using Microsoft.DotNet.InternalAbstractions;
-using Microsoft.Build.Framework;
-using Microsoft.Build.Utilities;
 
 using static Microsoft.DotNet.Cli.Build.Framework.BuildHelpers;
 
