2084a099b8323efdceaa3bd81fb9fb307b57eded
Dennis Fricke <github@dennis-fricke.de>
2016-07-06 16:55:35 +0200

    Update LibraryExporterExtensions.cs
    tabs -> spaces

--- src/Microsoft.DotNet.Compiler.Common/LibraryExporterExtensions.cs
+++ src/Microsoft.DotNet.Compiler.Common/LibraryExporterExtensions.cs
@@ -10,77 +10,77 @@ using Microsoft.DotNet.ProjectModel.Graph;
 
 namespace Microsoft.DotNet.Cli.Compiler.Common
 {
-	public static class LibraryExporterExtensions
-	{
-		public static IEnumerable<LibraryExport> GetAllProjectTypeDependencies(this LibraryExporter exporter)
-		{
-			return
-				exporter.GetDependencies(LibraryType.Project)
-					.Concat(exporter.GetDependencies(LibraryType.MSBuildProject));
-		}
+    public static class LibraryExporterExtensions
+    {
+        public static IEnumerable<LibraryExport> GetAllProjectTypeDependencies(this LibraryExporter exporter)
+        {
+            return
+                exporter.GetDependencies(LibraryType.Project)
+                    .Concat(exporter.GetDependencies(LibraryType.MSBuildProject));
+        }
+        
+        public static void CopyTo(this IEnumerable<LibraryAsset> assets, string destinationPath)
+        {
+            if (!Directory.Exists(destinationPath))
+            {
+                Directory.CreateDirectory(destinationPath);
+            }
 
-		public static void CopyTo(this IEnumerable<LibraryAsset> assets, string destinationPath)
-		{
-			if (!Directory.Exists(destinationPath))
-			{
-				Directory.CreateDirectory(destinationPath);
-			}
+            foreach (var asset in assets)
+            {
+                var file = Path.Combine(destinationPath, Path.GetFileName(asset.ResolvedPath));
+                File.Copy(asset.ResolvedPath, file, overwrite: true);
+                RemoveFileAttribute(file, FileAttributes.ReadOnly);
+            }
+        }
 
-			foreach (var asset in assets)
-			{
-				var file = Path.Combine(destinationPath, Path.GetFileName(asset.ResolvedPath));
-				File.Copy(asset.ResolvedPath, file, overwrite: true);
-				RemoveFileAttribute(file, FileAttributes.ReadOnly);
-			}
-		}
+        private static void RemoveFileAttribute(String file, FileAttributes attribute)
+        {
+            if (File.Exists(file))
+            {
+                var fileAttributes = File.GetAttributes(file);
+                if ((fileAttributes & attribute) == attribute)
+                {
+                    File.SetAttributes(file, fileAttributes & ~attribute);
+                }
+            }
+        }
 
-		private static void RemoveFileAttribute(String file, FileAttributes attribute)
-		{
-			if (File.Exists(file))
-			{
-				var fileAttributes = File.GetAttributes(file);
-				if ((fileAttributes & attribute) == attribute)
-				{
-					File.SetAttributes(file, fileAttributes & ~attribute);
-				}
-			}
-		}
+        public static void StructuredCopyTo(this IEnumerable<LibraryAsset> assets, string destinationPath, string tempLocation)
+        {
+            if (!Directory.Exists(destinationPath))
+            {
+                Directory.CreateDirectory(destinationPath);
+            }
 
-		public static void StructuredCopyTo(this IEnumerable<LibraryAsset> assets, string destinationPath, string tempLocation)
-		{
-			if (!Directory.Exists(destinationPath))
-			{
-				Directory.CreateDirectory(destinationPath);
-			}
+            foreach (var asset in assets)
+            {
+                var targetName = ResolveTargetName(destinationPath, asset);
+                var transformedFile = asset.GetTransformedFile(tempLocation);
 
-			foreach (var asset in assets)
-			{
-				var targetName = ResolveTargetName(destinationPath, asset);
-				var transformedFile = asset.GetTransformedFile(tempLocation);
+                File.Copy(transformedFile, targetName, overwrite: true);
+                RemoveFileAttribute(targetName, FileAttributes.ReadOnly);
+            }
+        }
 
-				File.Copy(transformedFile, targetName, overwrite: true);
-				RemoveFileAttribute(targetName, FileAttributes.ReadOnly);
-			}
-		}
+        private static string ResolveTargetName(string destinationPath, LibraryAsset asset)
+        {
+            string targetName;
+            if (!string.IsNullOrEmpty(asset.RelativePath))
+            {
+                targetName = Path.Combine(destinationPath, asset.RelativePath);
+                var destinationAssetPath = Path.GetDirectoryName(targetName);
 
-		private static string ResolveTargetName(string destinationPath, LibraryAsset asset)
-		{
-			string targetName;
-			if (!string.IsNullOrEmpty(asset.RelativePath))
-			{
-				targetName = Path.Combine(destinationPath, asset.RelativePath);
-				var destinationAssetPath = Path.GetDirectoryName(targetName);
-
-				if (!Directory.Exists(destinationAssetPath))
-				{
-					Directory.CreateDirectory(destinationAssetPath);
-				}
-			}
-			else
-			{
-				targetName = Path.Combine(destinationPath, Path.GetFileName(asset.ResolvedPath));
-			}
-			return targetName;
-		}
-	}
+                if (!Directory.Exists(destinationAssetPath))
+                {
+                    Directory.CreateDirectory(destinationAssetPath);
+                }
+            }
+            else
+            {
+                targetName = Path.Combine(destinationPath, Path.GetFileName(asset.ResolvedPath));
+            }
+            return targetName;
+        }
+    }
 }
