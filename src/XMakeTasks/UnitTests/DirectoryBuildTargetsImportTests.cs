da283a7bea8a023c47cea73630fa3126c57b49a1
Jeff Kluge <jeffkl@microsoft.com>
2016-07-06 14:29:04 -0700

    Implementation of ImportDirectoryBuildProps and ImportDirectoryBuildTargets (#751)
    Implementation of ImportDirectoryBuildProps and ImportDirectoryBuildTargets
    
    This imports a file named Directory.Build.props and Directory.Build.targets in the directory structure of the project being built.

---
+++ src/XMakeTasks/UnitTests/DirectoryBuildTargetsImportTests.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Microsoft.Build.UnitTests
+{
+    /// <summary>
+    /// Tests that Microsoft.Common.targets successfully imports a directory build project in the directory tree of the project being built.
+    /// </summary>
+    sealed public class DirectoryBuildTargetsImportTests : DirectoryBuildProjectImportTestBase
+    {
+        protected override string DirectoryBuildProjectFile => "Directory.Build.targets";
+
+        protected override string CustomBuildProjectFile => "customBuild.targets";
+
+        protected override string DirectoryBuildProjectPathPropertyName => "DirectoryBuildTargetsPath";
+
+        protected override string ImportDirectoryBuildProjectPropertyName => "ImportDirectoryBuildTargets";
+
+        protected override string DirectoryBuildProjectFilePropertyName => "_DirectoryBuildTargetsFile";
+
+        protected override string DirectoryBuildProjectBasePathPropertyName => "_DirectoryBuildTargetsBasePath";
+    }
+}
