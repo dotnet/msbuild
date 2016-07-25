571d1a313193853c681e2032e2be5a58d7e7af0e
Nat Ayewah <nayewah@microsoft.com>
2016-07-25 12:39:57 -0700

    Checking Basic Task to represent project.lock.json as MSBuild items, and some sample targets to use these items

---
+++ src/Tasks/Microsoft.MSBuild.LockFile.Tasks/MetadataKeys.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.MSBuild.LockFile.Tasks
+{
+    internal static class MetadataKeys
+    {
+        // General Metadata
+        internal const string Name = "Name";
+        internal const string Type = "Type";
+        internal const string Version = "Version";
+        internal const string FileGroup = "FileGroup";
+
+        // Target Metadata
+        internal const string RuntimeIdentifier = "RuntimeIdentifier";
+        internal const string TargetFramework = "TargetFramework";
+        internal const string FrameworkName = "FrameworkName";
+        internal const string FrameworkVersion = "FrameworkVersion";
+
+        // Foreign Keys
+        internal const string ParentTarget = "ParentTarget";
+        internal const string ParentTargetLibrary = "ParentTargetLibrary";
+    }
+}
