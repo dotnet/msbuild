4ec15ca91d11b285b56d1e1f36b31e51d52dc27a
Fredric Silberberg <frsilb@microsoft.com>
2016-08-02 11:02:29 -0700

    Added netci for jenkins

---
+++ netci.groovy
@@ -0,0 +1,33 @@
+import jobs.generation.Utilities
+import jobs.generation.InternalUtilities
+
+def project = GithubProject
+def branch = GithubBranchName
+
+// Generate a PR/nonPR job for debug (test only), which just does testing.
+[true, false].each { isPR ->
+    ['Debug', 'Release'].each { config ->
+        def lowerCaseConfig = config.toLowerCase()
+
+        def newJobName = InternalUtilities.getFullJobName(project, "windows_$lowerCaseConfig", isPR)
+
+        def newJob = job(newJobName) {
+            steps {
+                batchFile("build.cmd /$lowerCaseConfig")
+            }
+        }
+
+        // TODO: For when we actually have unit tests in this repo
+        // Utilities.addXUnitDotNETResults(myJob, '**/xUnitResults/*.xml')
+
+        Utilities.setMachineAffinity(newJob, 'Windows_NT')
+        InternalUtilities.standardJobSetup(newJob, project, isPR, "*/${branch}")
+        Utilities.addHtmlPublisher(newJob, "TestResults", "Unit Test Results", "index.html")
+
+        if (isPR) {
+            Utilities.addGithubPRTriggerForBranch(newJob, branch, "Windows $config")
+        } else {
+            Utilities.addGithubPushTrigger(newJob)
+        }
+    }
+}
