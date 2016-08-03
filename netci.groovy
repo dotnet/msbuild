99c0e60fbff480ea59ac7b66c6650498a748fec4
Fredric Silberberg <frsilb@microsoft.com>
2016-08-03 11:35:20 -0700

    Removed non-pr jobs

--- netci.groovy
+++ netci.groovy
@@ -5,29 +5,23 @@ def project = GithubProject
 def branch = GithubBranchName
 
 // Generate a PR/nonPR job for debug (test only), which just does testing.
-[true, false].each { isPR ->
-    ['Debug', 'Release'].each { config ->
-        def lowerCaseConfig = config.toLowerCase()
+['Debug', 'Release'].each { config ->
+    def lowerCaseConfig = config.toLowerCase()
 
-        def newJobName = InternalUtilities.getFullJobName(project, "windows_$lowerCaseConfig", isPR)
+    def newJobName = InternalUtilities.getFullJobName(project, "windows_$lowerCaseConfig", true /* isPR */)
 
-        def newJob = job(newJobName) {
-            steps {
-                batchFile("build.cmd -Configuration $config")
-            }
-        }
-
-        // TODO: For when we actually have unit tests in this repo
-        // Utilities.addXUnitDotNETResults(myJob, '**/xUnitResults/*.xml')
-
-        Utilities.setMachineAffinity(newJob, 'Windows_NT', 'latest-or-auto-internal')
-        InternalUtilities.standardJobSetup(newJob, project, isPR, "*/${branch}")
-        Utilities.addHtmlPublisher(newJob, "TestResults", "Unit Test Results", "index.html")
-
-        if (isPR) {
-            Utilities.addGithubPRTriggerForBranch(newJob, branch, "Windows $config")
-        } else {
-            Utilities.addGithubPushTrigger(newJob)
+    def newJob = job(newJobName) {
+        steps {
+            batchFile("build.cmd -Configuration $config")
         }
     }
+
+    // TODO: For when we actually have unit tests in this repo
+    // Utilities.addXUnitDotNETResults(myJob, '**/xUnitResults/*.xml')
+
+    Utilities.setMachineAffinity(newJob, 'Windows_NT', 'latest-or-auto-internal')
+    InternalUtilities.standardJobSetup(newJob, project, true /* isPR */, "*/${branch}")
+    Utilities.addHtmlPublisher(newJob, "TestResults", "Unit Test Results", "index.html")
+
+    Utilities.addGithubPRTriggerForBranch(newJob, branch, "Windows $config")
 }
