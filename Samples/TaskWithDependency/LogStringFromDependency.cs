eb5eb3ec0cc9a075f6d7ef1491ca5c612c523424
Tom Meschter <tom.meschter@microsoft.com>
2016-08-03 14:57:58 -0700

    Add a couple of tests around loading assembly dependencies
    Related to #658.
    
    This change introduced a couple of basic tests to verify that we can find a
    task assembly's dependencies when they are located in the same directory. In
    order to do this, it adds a couple of projects to produce assemblies just for
    this test: TaskWithDependency.dll, and Dependency.dll.

---
+++ Samples/TaskWithDependency/LogStringFromDependency.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.Build.Framework;
+using System.Reflection;
+using Dependency;
+
+namespace TaskWithDependency
+{
+    public class LogStringFromDependency : Microsoft.Build.Utilities.Task
+    {
+        public override bool Execute()
+        {
+            Log.LogMessage($"Message from dependency: {Alpha.GetString()}");
+
+            return true;
+        }
+    }
+}
