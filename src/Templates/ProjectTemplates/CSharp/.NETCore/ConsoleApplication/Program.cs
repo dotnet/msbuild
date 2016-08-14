922cfe2703d992ee34880ea65d874ef89c1ed338
Nat Ayewah <nayewah@ntdev.microsoft.com>
2016-08-14 12:06:11 -0700

    Move Templates to core sdk repos

---
+++ src/Templates/ProjectTemplates/CSharp/.NETCore/ConsoleApplication/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace $safeprojectname$
+{
+    internal class Program
+    {
+        internal static void Main(string[] args)
+        {
+        }
+    }
+}
