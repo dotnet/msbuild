e08da40a1fc3d650552efaf0813e76cf5d265dec
Rohit Agrawal <ragrawal@microsoft.com>
2016-08-18 16:53:55 -0700

    added pack3 command for dotnet (#4058)

--- src/dotnet/Program.cs
+++ src/dotnet/Program.cs
@@ -15,6 +15,7 @@ using Microsoft.DotNet.Tools.Compiler;
 using Microsoft.DotNet.Tools.Compiler.Csc;
 using Microsoft.DotNet.Tools.Help;
 using Microsoft.DotNet.Tools.New;
+using Microsoft.DotNet.Tools.Pack3;
 using Microsoft.DotNet.Tools.Publish;
 using Microsoft.DotNet.Tools.Restore;
 using Microsoft.DotNet.Tools.Restore3;
@@ -40,6 +41,7 @@ namespace Microsoft.DotNet.Cli
             ["build3"] = Build3Command.Run,
             ["run3"] = Run3Command.Run,
             ["restore3"] = Restore3Command.Run,
+            ["pack3"] = Pack3Command.Run,
         };
 
         public static int Main(string[] args)
