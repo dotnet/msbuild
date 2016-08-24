b91af4630b8fbb6114a5c23650c5a1206b7985ed
Andy Gerlicher <angerlic@microsoft.com>
2016-08-24 15:36:26 -0700

    Update AssemblyInfo to match shipping product.  - Add assembly title, description, etc.  - Specify icon for MSBuild.exe and MSBuildTaskHost.exe  - Update MSBuild.exe.manifest (values were very old)  - Add CLSCompliant(true)  - Removed some STANDALONEBUILD properties from AssemblyInfo, they should    not longer be needed
    The primary motivation for this is the CLSCompliant. This was previously
    set by our internal build system and was lost when we went open source.
    This was causing build warnings for other teams.

--- src/XMakeCommandLine/AssemblyInfo.cs
+++ src/XMakeCommandLine/AssemblyInfo.cs
@@ -1,18 +1,14 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System;
 using System.Reflection;
 using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-#if (STANDALONEBUILD)
 [assembly: InternalsVisibleTo("Microsoft.Build.CommandLine.UnitTests, PublicKey=002400000480000094000000060200000024000052534131000400000100010015c01ae1f50e8cc09ba9eac9147cf8fd9fce2cfe9f8dce4f7301c4132ca9fb50ce8cbf1df4dc18dd4d210e4345c744ecb3365ed327efdbc52603faa5e21daa11234c8c4a73e51f03bf192544581ebe107adee3a34928e39d04e524a9ce729d5090bfd7dad9d10c722c0def9ccc08ff0a03790e48bcd1f9b6c476063e1966a1c4")]
 [assembly: InternalsVisibleTo("Microsoft.Build.Utilities.UnitTests, PublicKey=002400000480000094000000060200000024000052534131000400000100010015c01ae1f50e8cc09ba9eac9147cf8fd9fce2cfe9f8dce4f7301c4132ca9fb50ce8cbf1df4dc18dd4d210e4345c744ecb3365ed327efdbc52603faa5e21daa11234c8c4a73e51f03bf192544581ebe107adee3a34928e39d04e524a9ce729d5090bfd7dad9d10c722c0def9ccc08ff0a03790e48bcd1f9b6c476063e1966a1c4")]
-#else
-[assembly: InternalsVisibleTo("MSBuild.Unittest, PublicKey=002400000480000094000000060200000024000052534131000400000100010015c01ae1f50e8cc09ba9eac9147cf8fd9fce2cfe9f8dce4f7301c4132ca9fb50ce8cbf1df4dc18dd4d210e4345c744ecb3365ed327efdbc52603faa5e21daa11234c8c4a73e51f03bf192544581ebe107adee3a34928e39d04e524a9ce729d5090bfd7dad9d10c722c0def9ccc08ff0a03790e48bcd1f9b6c476063e1966a1c4")]
-[assembly: InternalsVisibleTo("MSBuild.Whidbey.Unittest, PublicKey=002400000480000094000000060200000024000052534131000400000100010015c01ae1f50e8cc09ba9eac9147cf8fd9fce2cfe9f8dce4f7301c4132ca9fb50ce8cbf1df4dc18dd4d210e4345c744ecb3365ed327efdbc52603faa5e21daa11234c8c4a73e51f03bf192544581ebe107adee3a34928e39d04e524a9ce729d5090bfd7dad9d10c722c0def9ccc08ff0a03790e48bcd1f9b6c476063e1966a1c4")]
-#endif
 
 // This will enable passing the SafeDirectories flag to any P/Invoke calls/implementations within the assembly, 
 // so that we don't run into known security issues with loading libraries from unsafe locations 
@@ -22,3 +18,11 @@ using System.Runtime.InteropServices;
 // Needed for the "hub-and-spoke model to locate and retrieve localized resources": https://msdn.microsoft.com/en-us/library/21a15yht(v=vs.110).aspx
 [assembly: NeutralResourcesLanguage("en", UltimateResourceFallbackLocation.Satellite)]
 #endif
+
+[assembly: CLSCompliant(true)]
+
+[assembly: AssemblyTitle("MSBuild.exe")]
+[assembly: AssemblyDescription("MSBuild.exe")]
+[assembly: AssemblyCompany("Microsoft Corporation")]
+[assembly: AssemblyProduct("MSBuild")]
+[assembly: AssemblyCopyright("Microsoft Corporation. All rights reserved.")]
---
+++ src/XMakeCommandLine/MSBuildTaskHost/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8974eb2a-7ea3-4d5d-b33b-149738025f36")]
+
+[assembly: CLSCompliant(true)]
+
+[assembly: AssemblyTitle("MSBuildTaskHost.exe")]
+[assembly: AssemblyDescription("MSBuildTaskHost.exe")]
+[assembly: AssemblyCompany("Microsoft Corporation")]
+[assembly: AssemblyProduct("MSBuild")]
+[assembly: AssemblyCopyright("Microsoft Corporation. All rights reserved.")]
