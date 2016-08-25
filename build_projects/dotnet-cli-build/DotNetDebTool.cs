ed66c70106a1cc0a85cc673147c6aa09d82cbed5
PiotrP <piotrp@microsoft.com>
2016-08-25 15:53:15 -0700

    wIp

--- build_projects/dotnet-cli-build/DotNetDebTool.cs
+++ build_projects/dotnet-cli-build/DotNetDebTool.cs
@@ -1,6 +1,9 @@
 // Copyright (c) .NET Foundation and contributors. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
 namespace Microsoft.DotNet.Cli.Build
 {
     public class DotNetDebTool : DotNetTool
