9f5f8cfc7c5973de92ecbb90a4be8a7320b33dba
Sridhar Periyasamy <sridhper@microsoft.com>
2016-09-02 16:31:51 -0700

    Generate Satellite assemblies
    Ported over code from dotnet-resgen to create a new task called "GenerateSatelliteAssemblies'.
    Some more fixes required to make this work
    - 'ComputeIntermediateSatelliteAssemblies' Target in xplat msbuild is disabled for .net core (https://github.com/Microsoft/msbuild/blob/xplat/src/XMakeTasks/Microsoft.Common.CurrentVersion.targets#L3332). It needs to be enabled to copy the satellite assemblies from obj to bin folder.
    - The resource manifest name does not have the Root Namespace as described here - https://github.com/Microsoft/msbuild/blob/35860e4ffb0d8016779bc45f41fc6049af4db2ce/src/XMakeTasks/Microsoft.CSharp.CurrentVersion.targets#L77-L83.
    - Need to figure out a way to pass the assembly version to the 'GenerateSatelliteAssemblies'. Currently the satellite assembies have version '0.0.0.0'.

---
+++ src/Tasks/Microsoft.NETCore.Build.Tasks/GenerateSatelliteAssemblies.cs
@@ -0,0 +1,83 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.Extensions.DependencyModel;
+
+namespace Microsoft.NETCore.Build.Tasks
+{
+    /// <summary>
+    /// Generates the $(project).deps.json file.
+    /// </summary>
+    public class GenerateSatelliteAssemblies : Task
+    {
+        [Required]
+        public ITaskItem[] EmbedResources { get; set; }
+
+        [Output]
+        [Required]
+        public ITaskItem OutputAssembly { get; set; }
+
+        [Required]
+        public string Culture { get; set; }
+
+        [Required]
+        public string[] References { get; set; }
+
+        public string Version { get; set; }
+
+        public override bool Execute()
+        {
+            var resourceDescriptions = new List<ResourceDescription>();
+
+            foreach (var input in EmbedResources)
+            {
+                var fileName = input.GetMetadata("Filename");
+                var fullPath = input.GetMetadata("FullPath");
+                var fileInfo = new FileInfo(fullPath);
+                resourceDescriptions.Add(new ResourceDescription(fileName, () => fileInfo.OpenRead(), true));
+            }
+
+            var compilationOptions = new CSharpCompilationOptions(outputKind: OutputKind.DynamicallyLinkedLibrary);
+            var compilation = CSharpCompilation.Create(OutputAssembly.GetMetadata("Filename"),
+                references: References.Select(reference => MetadataReference.CreateFromFile(reference)),
+                options: compilationOptions);
+
+            var metadata = new AssemblyInfoOptions
+            {
+                Culture = Culture,
+                AssemblyVersion = Version,
+            };
+
+            var cs = AssemblyInfoFileGenerator.GenerateCSharp(metadata, Enumerable.Empty<string>());            
+            compilation = compilation.AddSyntaxTrees(new[]
+            {
+                CSharpSyntaxTree.ParseText(cs)
+            });
+
+            var satelliteAssembly = OutputAssembly.GetMetadata("FullPath");
+            using (var outputStream = new FileInfo(satelliteAssembly).Create())
+            {
+                var result = compilation.Emit(outputStream, manifestResources: resourceDescriptions);
+
+                if (!result.Success)
+                {
+                    Log.LogError($"Errors ocurred when emitting satellite assembly - {satelliteAssembly}");
+                    foreach (var diagnostic in result.Diagnostics)
+                    {
+                        Log.LogError(diagnostic.ToString());
+                    }                    
+                }
+            }
+
+            return !Log.HasLoggedErrors;
+        }
+    }
+}
