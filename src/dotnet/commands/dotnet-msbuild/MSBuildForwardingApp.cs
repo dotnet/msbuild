4019fdca164b9c089c21a901bb85025a086419e5
Livar Cunha <livar@live.com>
2016-09-21 15:24:54 -0700

    Removing DotnetHostPath and setting CscToolExe to determine which script to use to run csc.exe using dotnet. Need to add the script counterpart for windows still. Doing that next.

--- src/dotnet/commands/dotnet-msbuild/MSBuildForwardingApp.cs
+++ src/dotnet/commands/dotnet-msbuild/MSBuildForwardingApp.cs
@@ -32,13 +32,8 @@ namespace Microsoft.DotNet.Tools.MSBuild
             return new Dictionary<string, string>
             {
                 { "MSBuildExtensionsPath", AppContext.BaseDirectory },
-                { "DotnetHostPath", GetHostPath() },
+                { "CscToolExe", GetRunCscPath() }
             };
-        }
-
-        private static string GetHostPath()
-        {
-            return new Muxer().MuxerPath;
         }
 
         private static string GetMSBuildExePath()
@@ -46,6 +41,12 @@ namespace Microsoft.DotNet.Tools.MSBuild
             return Path.Combine(
                 AppContext.BaseDirectory,
                 s_msbuildExeName);
+        }
+
+        private static string GetRunCscPath()
+        {
+            var scriptExtension = RuntimeEnvironment.OperatingSystemPlatform == Platform.Windows ? ".cmd" : ".sh";
+            return Path.Combine(AppContext.BaseDirectory, $"RunCsc{scriptExtension}");
         }
     }
 }
