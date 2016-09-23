d1cea1bef24d24c02452f821b91a2ed28fedd157
Livar Cunha <livar@live.com>
2016-09-22 20:53:47 -0700

    Addressing code review comments.

--- src/redist/RunCsc.sh
+++ src/redist/RunCsc.sh
@@ -14,4 +14,4 @@ while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symli
 done
 DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
-$DIR/../../dotnet $DIR/csc.exe $@
+"$DIR/../../dotnet" "$DIR/csc.exe" "$@"
