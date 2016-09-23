09a369530caa33a7eecfaf179c6ea1f932df9d47
Livar Cunha <livar@live.com>
2016-09-23 12:49:24 -0700

    Adding tests for run3.

---
+++ test/dotnet-run3.Tests/GivenDotnetRun3RunsCsProj.cs
@@ -0,0 +1,93 @@
+// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.DotNet.Tools.Test.Utilities;
+using Xunit;
+
+namespace Microsoft.DotNet.Cli.Run3.Tests
+{
+    public class GivenDotnetRun3BuildsCsproj : TestBase
+    {
+        [Fact]
+        public void ItCanRunAMSBuildProject()
+        {
+            var testAppName = "MSBuildTestApp";
+            var testInstance = TestAssetsManager
+                .CreateTestInstance(testAppName)
+                .WithLockFiles();
+
+            var testProjectDirectory = testInstance.TestRoot;
+
+            new Build3Command()
+                .WithWorkingDirectory(testProjectDirectory)
+                .Execute()
+                .Should()
+                .Pass();
+
+            new Run3Command()
+                .WithWorkingDirectory(testProjectDirectory)
+                .ExecuteWithCapturedOutput()
+                .Should()
+                .Pass()
+                .And
+                .HaveStdOutContaining("Hello World!");
+        }
+
+        [Fact]
+        public void ItBuildsTheProjectBeforeRunning()
+        {
+            var testAppName = "MSBuildTestApp";
+            var testInstance = TestAssetsManager
+                .CreateTestInstance(testAppName)
+                .WithLockFiles();
+
+            var testProjectDirectory = testInstance.TestRoot;
+
+            new Run3Command()
+                .WithWorkingDirectory(testProjectDirectory)
+                .ExecuteWithCapturedOutput()
+                .Should()
+                .Pass()
+                .And
+                .HaveStdOutContaining("Hello World!");
+        }
+
+        [Fact]
+        public void ItCanRunAMSBuildProjectWhenSpecifyingAFramework()
+        {
+            var testAppName = "MSBuildTestApp";
+            var testInstance = TestAssetsManager
+                .CreateTestInstance(testAppName)
+                .WithLockFiles();
+
+            var testProjectDirectory = testInstance.TestRoot;
+
+            new Run3Command()
+                .WithWorkingDirectory(testProjectDirectory)
+                .ExecuteWithCapturedOutput("--framework netcoreapp1.0")
+                .Should()
+                .Pass()
+                .And
+                .HaveStdOutContaining("Hello World!");            
+        }
+
+        [Fact]
+        public void ItFailsToRunAMSBuildProjectWhenTheSpecifiedFrameworkIsNotInTheProject()
+        {
+            var testAppName = "MSBuildTestApp";
+            var testInstance = TestAssetsManager
+                .CreateTestInstance(testAppName)
+                .WithLockFiles();
+
+            var testProjectDirectory = testInstance.TestRoot;
+
+            new Run3Command()
+                .WithWorkingDirectory(testProjectDirectory)
+                .ExecuteWithCapturedOutput("--framework my-framework")
+                .Should()
+                .Pass()
+                .And
+                .HaveStdOutContaining("Hello World!");
+        }
+    }
+}
