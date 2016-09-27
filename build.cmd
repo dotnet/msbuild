0f5d8d732f2b0dacab9eab8cb5886056ceb45977
Vijay Ramakrishnan <vramak@microsoft.com>
2016-09-27 16:10:19 -0700

    Initial commit for Publish task targetting NetCore

--- build.cmd
+++ build.cmd
@@ -1,2 +1,7 @@
 @ECHO OFF
+
+PowerShell -NoProfile -NoLogo -ExecutionPolicy unrestricted -Command "[System.Threading.Thread]::CurrentThread.CurrentCulture = ''; [System.Threading.Thread]::CurrentThread.CurrentUICulture = '';& '%~dp0dotnet-install.ps1' %*; exit $LASTEXITCODE" 
+SET PATH=%PATH%;%localappdata%\Microsoft\dotnet
+dotnet restore3 src\Microsoft.DotNetCore.Publish.Tasks\Microsoft.DotNetCore.Publish.Tasks.csproj
+
 tools\build.cmd
