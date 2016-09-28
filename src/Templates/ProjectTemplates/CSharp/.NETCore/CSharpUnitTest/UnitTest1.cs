26763c185ee293497caf00aaa43b780179737e88
Sarabjot Singh <sasin@microsoft.com>
2016-09-28 16:13:17 +0530

    Adding the template for Unit Test project for .NET core.

---
+++ src/Templates/ProjectTemplates/CSharp/.NETCore/CSharpUnitTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace $safeprojectname$
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
