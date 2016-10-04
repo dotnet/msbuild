c868719684ff43689da519ac3bb0e7ecd711ee2a
Vijay Ramakrishnan <vramak@microsoft.com>
2016-10-04 16:59:05 -0700

    Adding PublishOnBuild condition for importing publish targets

--- build.cmd
+++ build.cmd
@@ -1,4 +1,4 @@
-@ECHO ON
+@ECHO OFF
 
 PowerShell -NoProfile -NoLogo -ExecutionPolicy unrestricted -Command "[System.Threading.Thread]::CurrentThread.CurrentCulture = ''; [System.Threading.Thread]::CurrentThread.CurrentUICulture = '';& '%~dp0dotnet-install.ps1' %*; exit $LASTEXITCODE" 
 SET PATH=%localappdata%\Microsoft\dotnet;%PATH%
