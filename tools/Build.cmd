0786ad8406cce78cfcae287f242876fe7eb22707
Vijay Ramakrishnan <vramak@microsoft.com>
2016-10-04 15:02:50 -0700

    Adding SkipInvalidConfigurations=true to dotnet build3

--- tools/Build.cmd
+++ tools/Build.cmd
@@ -6,4 +6,4 @@ copy \\aspnetci\share\tools\Microsoft.Web.MsBuildTasks2.dll %PublishTools% /y
 copy \\aspnetci\share\tools\7za.exe %PublishTools% /y
 
 dotnet restore3 %PublishRoot%\src\Microsoft.DotNetCore.Publish.Tasks\Microsoft.DotNetCore.Publish.Tasks.csproj /p:SkipInvalidConfigurations=true
-msbuild %PublishRoot%\dirs.proj /p:configuration=Release /t:Build;Sign
+msbuild %PublishRoot%\dirs.proj /p:configuration=Release;SkipInvalidConfigurations=true /t:Build;Sign
