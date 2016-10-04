925f816eb56454dbff72ae0391878d1d5a63e67e
Vijay Ramakrishnan <vramak@microsoft.com>
2016-10-04 10:07:22 -0700

    Adding more logging to find the build issue

--- build.cmd
+++ build.cmd
@@ -1,6 +1,5 @@
 @ECHO ON
 
 PowerShell -NoProfile -NoLogo -ExecutionPolicy unrestricted -Command "[System.Threading.Thread]::CurrentThread.CurrentCulture = ''; [System.Threading.Thread]::CurrentThread.CurrentUICulture = '';& '%~dp0dotnet-install.ps1' %*; exit $LASTEXITCODE" 
-SET PATH=%localappdata%\Microsoft\dotnet;%PATH%;
-dotnet restore3 src\Microsoft.DotNetCore.Publish.Tasks\Microsoft.DotNetCore.Publish.Tasks.csproj
+SET PATH=%localappdata%\Microsoft\dotnet;%PATH%
 tools\build.cmd
