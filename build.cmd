fc94e4eb2a9495ce4188932190799b41d33caaa5
Vijay Ramakrishnan <vramak@microsoft.com>
2016-10-03 22:51:31 -0700

    Adding additional logging for restore issue

--- build.cmd
+++ build.cmd
@@ -1,7 +1,6 @@
-@ECHO OFF
+@ECHO ON
 
 PowerShell -NoProfile -NoLogo -ExecutionPolicy unrestricted -Command "[System.Threading.Thread]::CurrentThread.CurrentCulture = ''; [System.Threading.Thread]::CurrentThread.CurrentUICulture = '';& '%~dp0dotnet-install.ps1' %*; exit $LASTEXITCODE" 
 SET PATH=%PATH%;%localappdata%\Microsoft\dotnet
-dotnet restore3 src\Microsoft.DotNetCore.Publish.Tasks\Microsoft.DotNetCore.Publish.Tasks.csproj
 
 tools\build.cmd
