f624ed7eba37d0cbaac3122a407cf1dd82697314
Vijay Ramakrishnan <vramak@microsoft.com>
2016-10-06 10:32:03 -0700

    Renaming the package to Microsoft.NET.Sdk.Publish

--- tools/Build.cmd
+++ tools/Build.cmd
@@ -5,6 +5,6 @@ call %~dp0\EnsurePublishEnv.cmd
 copy \\aspnetci\share\tools\Microsoft.Web.MsBuildTasks2.dll %PublishTools% /y
 copy \\aspnetci\share\tools\7za.exe %PublishTools% /y
 
-dotnet restore3 %PublishRoot%\src\Microsoft.DotNetCore.Publish.Tasks\Microsoft.DotNetCore.Publish.Tasks.csproj /p:SkipInvalidConfigurations=true
-dotnet build3 %PublishRoot%\src\Microsoft.DotNetCore.Publish.Tasks\Microsoft.DotNetCore.Publish.Tasks.csproj /p:SkipInvalidConfigurations=true;configuration=Release
+dotnet restore3 %PublishRoot%\src\Microsoft.NETCore.Sdk.Publish.Tasks\Microsoft.NETCore.Sdk.Publish.Tasks.csproj /p:SkipInvalidConfigurations=true;configuration=Release
+dotnet build3 %PublishRoot%\src\Microsoft.NETCore.Sdk.Publish.Tasks\Microsoft.NETCore.Sdk.Publish.Tasks.csproj /p:SkipInvalidConfigurations=true;configuration=Release
 msbuild %PublishRoot%\dirs.proj /p:configuration=Release;SkipInvalidConfigurations=true /t:Build;Sign
