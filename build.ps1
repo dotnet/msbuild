01fa7cd2b5095391d4d7f6595e45ae3d3f31e355
Nat Ayewah <nayewah@microsoft.com>
2016-10-06 16:45:04 -0700

    Clean out unnecessary properties and targets; move to common build.proj and sign template files separately; add manifest version variable

--- build.ps1
+++ build.ps1
@@ -60,7 +60,6 @@ if ($RealSign) {
 }
 
 $commonBuildArgs = echo $RepoRoot\build\build.proj /m /nologo /p:Configuration=$Configuration /p:Platform=$Platform /p:SignType=$signType 
-$commonBuildArgsVsMan = echo $RepoRoot\build\buildWithVSMan.proj /m /nologo /p:Configuration=$Configuration /p:Platform=$Platform /p:SignType=$signType 
 
 # Template Build
 $msbuildSummaryLog = Join-Path -path $logPath -childPath "templates.log"
@@ -76,7 +75,4 @@ $msbuildWarningLog = Join-Path -path $logPath -childPath "sdk.wrn"
 $msbuildFailureLog = Join-Path -path $logPath -childPath "sdk.err"
 
 dotnet build3 $commonBuildArgs /flp1:Summary`;Verbosity=diagnostic`;Encoding=UTF-8`;LogFile=$msbuildSummaryLog /flp2:WarningsOnly`;Verbosity=diagnostic`;Encoding=UTF-8`;LogFile=$msbuildWarningLog /flp3:ErrorsOnly`;Verbosity=diagnostic`;Encoding=UTF-8`;LogFile=$msbuildFailureLog
-if($LASTEXITCODE -ne 0) { throw "Failed to build" }
-
-msbuild $commonBuildArgsVsMan /nr:false /p:BuildTemplates=true /flp1:Summary`;Verbosity=diagnostic`;Encoding=UTF-8`;LogFile=$msbuildSummaryLog /flp2:WarningsOnly`;Verbosity=diagnostic`;Encoding=UTF-8`;LogFile=$msbuildWarningLog /flp3:ErrorsOnly`;Verbosity=diagnostic`;Encoding=UTF-8`;LogFile=$msbuildFailureLog
 if($LASTEXITCODE -ne 0) { throw "Failed to build" }
