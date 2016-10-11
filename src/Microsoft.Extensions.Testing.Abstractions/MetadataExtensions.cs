38753d42b0a0387828164019a8c6e027b3e20f54
Eric Erhardt <eric.erhardt@microsoft.com>
2016-10-11 16:01:03 -0500

    Fix the tests for targeting netstandard1.3.

--- src/Microsoft.Extensions.Testing.Abstractions/MetadataExtensions.cs
+++ src/Microsoft.Extensions.Testing.Abstractions/MetadataExtensions.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System;
 using System.Reflection;
 using System.Reflection.Metadata;
 using System.Reflection.Metadata.Ecma335;
@@ -9,11 +10,11 @@ namespace Microsoft.Extensions.Testing.Abstractions
 {
     internal static class MetadataExtensions
     {
+        private static PropertyInfo s_methodInfoMethodTokenProperty = typeof(MethodInfo).GetProperty("MethodToken");
+
         internal static int GetMethodToken(this MethodInfo methodInfo)
         {
-            var methodToken = methodInfo.MetadataToken;
-
-            return methodToken;
+            return (int)s_methodInfoMethodTokenProperty.GetValue(methodInfo);
         }
 
         internal static MethodDebugInformationHandle GetMethodDebugInformationHandle(this MethodInfo methodInfo)
