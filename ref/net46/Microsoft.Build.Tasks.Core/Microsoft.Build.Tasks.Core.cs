a4e3d2aba4fd605e9b91f896e2d20a99378166bc
Andy Gerlicher <angerlic@microsoft.com>
2016-10-19 21:45:23 -0700

    Enable VisualStudioSetup Feature - This was missed when the feature was merged to xplat. The feature flag   was added to the code but not defined in the build.

--- ref/net46/Microsoft.Build.Tasks.Core/Microsoft.Build.Tasks.Core.cs
+++ ref/net46/Microsoft.Build.Tasks.Core/Microsoft.Build.Tasks.Core.cs
@@ -2503,6 +2503,71 @@ namespace Microsoft.Build.Tasks.Xaml
         protected override bool ValidateParameters() { throw null; }
     }
 }
+namespace Microsoft.VisualStudio.Setup.Configuration
+{
+    [System.Runtime.CompilerServices.CompilerGeneratedAttribute]
+    [System.Runtime.InteropServices.GuidAttribute("6380BCFF-41D3-4B2E-8B2E-BF8A6810C848")]
+    [System.Runtime.InteropServices.InterfaceTypeAttribute((System.Runtime.InteropServices.ComInterfaceType)(1))]
+    [System.Runtime.InteropServices.TypeIdentifierAttribute]
+    public partial interface IEnumSetupInstances
+    {
+        void Next(int celt, Microsoft.VisualStudio.Setup.Configuration.ISetupInstance[] rgelt, out int pceltFetched);
+    }
+    [System.FlagsAttribute]
+    [System.Runtime.CompilerServices.CompilerGeneratedAttribute]
+    [System.Runtime.InteropServices.TypeIdentifierAttribute("310100ba-5f84-4103-abe0-e8132ae862d9", "Microsoft.VisualStudio.Setup.Configuration.InstanceState")]
+    public enum InstanceState : uint
+    {
+        Complete = (uint)4294967295,
+        Local = (uint)1,
+        None = (uint)0,
+        NoRebootRequired = (uint)4,
+        Registered = (uint)2,
+    }
+    [System.Runtime.CompilerServices.CompilerGeneratedAttribute]
+    [System.Runtime.InteropServices.GuidAttribute("42843719-DB4C-46C2-8E7C-64F1816EFD5B")]
+    [System.Runtime.InteropServices.InterfaceTypeAttribute((System.Runtime.InteropServices.ComInterfaceType)(1))]
+    [System.Runtime.InteropServices.TypeIdentifierAttribute]
+    public partial interface ISetupConfiguration
+    {
+    }
+    [System.Runtime.CompilerServices.CompilerGeneratedAttribute]
+    [System.Runtime.InteropServices.GuidAttribute("26AAB78C-4A60-49D6-AF3B-3C35BC93365D")]
+    [System.Runtime.InteropServices.InterfaceTypeAttribute((System.Runtime.InteropServices.ComInterfaceType)(1))]
+    [System.Runtime.InteropServices.TypeIdentifierAttribute]
+    public partial interface ISetupConfiguration2 : Microsoft.VisualStudio.Setup.Configuration.ISetupConfiguration
+    {
+        void _VtblGap1_3() { }
+        Microsoft.VisualStudio.Setup.Configuration.IEnumSetupInstances EnumAllInstances();
+    }
+    [System.Runtime.CompilerServices.CompilerGeneratedAttribute]
+    [System.Runtime.InteropServices.GuidAttribute("B41463C3-8866-43B5-BC33-2B0676F7F42E")]
+    [System.Runtime.InteropServices.InterfaceTypeAttribute((System.Runtime.InteropServices.ComInterfaceType)(1))]
+    [System.Runtime.InteropServices.TypeIdentifierAttribute]
+    public partial interface ISetupInstance
+    {
+        void _VtblGap1_3() { }
+        string GetDisplayName(int lcid=0);
+        string GetInstallationPath();
+        string GetInstallationVersion();
+    }
+    [System.Runtime.CompilerServices.CompilerGeneratedAttribute]
+    [System.Runtime.InteropServices.GuidAttribute("89143C9A-05AF-49B0-B717-72E218A2185C")]
+    [System.Runtime.InteropServices.InterfaceTypeAttribute((System.Runtime.InteropServices.ComInterfaceType)(1))]
+    [System.Runtime.InteropServices.TypeIdentifierAttribute]
+    public partial interface ISetupInstance2 : Microsoft.VisualStudio.Setup.Configuration.ISetupInstance
+    {
+        void _VtblGap1_8() { }
+        Microsoft.VisualStudio.Setup.Configuration.InstanceState GetState();
+    }
+    [System.Runtime.CompilerServices.CompilerGeneratedAttribute]
+    [System.Runtime.InteropServices.CoClassAttribute(typeof(object))]
+    [System.Runtime.InteropServices.GuidAttribute("42843719-DB4C-46C2-8E7C-64F1816EFD5B")]
+    [System.Runtime.InteropServices.TypeIdentifierAttribute]
+    public partial interface SetupConfiguration : Microsoft.VisualStudio.Setup.Configuration.ISetupConfiguration, Microsoft.VisualStudio.Setup.Configuration.ISetupConfiguration2
+    {
+    }
+}
 namespace System.Deployment.Internal.CodeSigning
 {
     public sealed partial class RSAPKCS1SHA256SignatureDescription : System.Security.Cryptography.SignatureDescription
