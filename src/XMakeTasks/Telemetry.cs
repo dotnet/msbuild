3aacaf242318e4d63e0a7b8b04659117f72d659f
Jeff Kluge <jeffkl@microsoft.com>
2016-10-25 09:12:21 -0700

    Forgot the copyright header in new files

--- src/XMakeTasks/Telemetry.cs
+++ src/XMakeTasks/Telemetry.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Build.Framework;
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.Build.Framework;
 using System;
 using System.Collections.Generic;
 
