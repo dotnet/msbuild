ad3ffba0782e1385bff3a9f36246a07d4038992b
Ankit Jain <ankit.jain@xamarin.com>
2016-10-24 22:56:46 -0400

    [tests] TaskItem_Tests.Escaping{2,3} - Disable Task Host tests for ..
    .. mono/osx.

--- src/XMakeBuildEngine/UnitTests/Instance/TaskItem_Tests.cs
+++ src/XMakeBuildEngine/UnitTests/Instance/TaskItem_Tests.cs
@@ -217,7 +217,7 @@ namespace Microsoft.Build.UnitTests.OM.Instance
         /// <summary>
         /// Flushing an item through a task run in the task host also should not mess up special characters on the metadata. 
         /// </summary>
-#if RUNTIME_TYPE_NETCORE
+#if RUNTIME_TYPE_NETCORE || MONO
         [Fact(Skip = "FEATURE: TASKHOST")]
 #else
         [Fact]
@@ -278,7 +278,7 @@ namespace Microsoft.Build.UnitTests.OM.Instance
         /// <summary>
         /// Flushing an item through a task run in the task host also should not mess up the escaping of the itemspec either. 
         /// </summary>
-#if RUNTIME_TYPE_NETCORE
+#if RUNTIME_TYPE_NETCORE || MONO
         [Fact(Skip = "FEATURE: TASKHOST")]
 #else
         [Fact]
