dc4af3c5e49792ea4d18fe9dc42d8f1a20e5682e
Jeff Kluge <jeffkl@microsoft.com>
2016-11-01 12:27:32 -0700

    Fix some build warnings, update ref assemblies (#1292)

--- ref/netstandard1.3/Microsoft.Build.Utilities.Core/Microsoft.Build.Utilities.Core.cs
+++ ref/netstandard1.3/Microsoft.Build.Utilities.Core/Microsoft.Build.Utilities.Core.cs
@@ -184,6 +184,7 @@ namespace Microsoft.Build.Utilities
         public Microsoft.Build.Framework.IBuildEngine2 BuildEngine2 { get { throw null; } }
         public Microsoft.Build.Framework.IBuildEngine3 BuildEngine3 { get { throw null; } }
         public Microsoft.Build.Framework.IBuildEngine4 BuildEngine4 { get { throw null; } }
+        public Microsoft.Build.Framework.IBuildEngine5 BuildEngine5 { get { throw null; } }
         protected string HelpKeywordPrefix { get { throw null; } set { } }
         public Microsoft.Build.Framework.ITaskHost HostObject { get { throw null; } set { } }
         public Microsoft.Build.Utilities.TaskLoggingHelper Log { get { throw null; } }
