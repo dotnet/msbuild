499579ddb8e396b77a7773321ef1d407591d46d0
Justin Goshi <jgoshi@microsoft.com>
2016-11-02 13:44:28 -1000

    Rename test templates for casing consistency (#4587)

--- src/dotnet/commands/dotnet-new/Program.cs
+++ src/dotnet/commands/dotnet-new/Program.cs
@@ -138,8 +138,8 @@ namespace Microsoft.DotNet.Tools.New
                                    new { Name = "Console", isMsBuild = true }, 
                                    new { Name = "Web", isMsBuild = true }, 
                                    new { Name = "Lib", isMsBuild = true },
-                                   new { Name = "mstest", isMsBuild = true },
-                                   new { Name = "xunittest", isMsBuild = true }
+                                   new { Name = "Mstest", isMsBuild = true },
+                                   new { Name = "Xunittest", isMsBuild = true }
                                }
             };
 
