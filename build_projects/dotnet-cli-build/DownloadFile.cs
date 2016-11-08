087852b1b8df5fa8735b01e5e49c7e06e904f7c6
Livar Cunha <livar@live.com>
2016-11-08 15:14:47 -0800

    Changing the lzma generation to first trying to download it from blob. If that fails, generate it. And if we generate it and we are uploading things to azure, upload it as well.

--- build_projects/dotnet-cli-build/DownloadFile.cs
+++ build_projects/dotnet-cli-build/DownloadFile.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System;
 using System.IO;
 using System.Net.Http;
 using Microsoft.Build.Framework;
@@ -35,7 +36,15 @@ namespace Microsoft.DotNet.Cli.Build
 
                 using (var outStream = File.Create(DestinationPath))
                 {
-                    getTask.Result.CopyTo(outStream);
+                    try
+                    {
+                        getTask.Result.CopyTo(outStream);
+                    }
+                    catch (Exception)
+                    {
+                        File.Delete(DestinationPath);
+                        throw;
+                    }
                 }
             }
 
