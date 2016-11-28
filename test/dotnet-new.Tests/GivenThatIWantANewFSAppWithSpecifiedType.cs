7b0117ac8520b8fc79e73e0ea08f61a0e439836a
Enrico Sada <enrico@sada.io>
2016-11-28 09:45:38 +0100

    Update fsharp templates for preview4 with latest msbuild style (#4836)
    * update web template
    
    * add test build of fsharp/web template
    
    * align fsharp/xunittest template
    
    * add test for all template projects
    
    * align fsharp/console template to #4821
    
    * align fsharp/lib template to #4821
    
    * align fsharp/mstest template to #4821
    
    * align fsharp/xunittest template to #4821
    
    * fix shortened test directory path (was same for c# test)
    
    * try fix ci failure

---
+++ test/dotnet-new.Tests/GivenThatIWantANewFSAppWithSpecifiedType.cs
@@ -0,0 +1,50 @@
+// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.DotNet.Tools.Test.Utilities;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Xunit;
+using FluentAssertions;
+
+namespace Microsoft.DotNet.New.Tests
+{
+    public class GivenThatIWantANewFSAppWithSpecifiedType : TestBase
+    {
+        [Theory]
+        [InlineData("Console", false)]
+        [InlineData("Lib", false)]
+        [InlineData("Web", true)]
+        [InlineData("Mstest", false)]
+        [InlineData("XUnittest", false)]
+        public void When_dotnet_build_is_invoked_then_project_restores_and_builds_without_warnings_fs(
+            string projectType,
+            bool useNuGetConfigForAspNet)
+        {
+            var rootPath = TestAssetsManager.CreateTestDirectory(callingMethod: "fs").Path;
+
+            new TestCommand("dotnet") { WorkingDirectory = rootPath }
+                .Execute($"new --lang fsharp --type {projectType}")
+                .Should().Pass();
+
+            if (useNuGetConfigForAspNet)
+            {
+                File.Copy("NuGet.tempaspnetpatch.config", Path.Combine(rootPath, "NuGet.Config"));
+            }
+
+            new TestCommand("dotnet") { WorkingDirectory = rootPath }
+                .Execute($"restore /p:SkipInvalidConfigurations=true")
+                .Should().Pass();
+
+            var buildResult = new TestCommand("dotnet")
+                .WithWorkingDirectory(rootPath)
+                .ExecuteWithCapturedOutput("build")
+                .Should().Pass()
+                .And.NotHaveStdErr();
+        }
+    }
+}
