47eb1ff6d874565ee34eb9cac584cf2d72a51705
Daniel Plaisted <daplaist@microsoft.com>
2016-12-01 01:36:13 -0800

    Use lowercase sdk path in Sdk path

--- src/XMakeBuildEngine/Evaluation/ProjectParser.cs
+++ src/XMakeBuildEngine/Evaluation/ProjectParser.cs
@@ -210,9 +210,9 @@ namespace Microsoft.Build.Construction
                     // TODO: paths should just be Sdk.props/targets; Sdk-aware imports should do the rest of the path.
                     //  Use lower case version of Sdk name when constructing the path so that it can be case-insensitive even on case-sensitive file systems
                     var initialImportPath = Path.Combine(BuildEnvironmentHelper.Instance.MSBuildSdksPath,
-                        sdkName.ToLowerInvariant(), "Sdk", "Sdk.props");
+                        sdkName.ToLowerInvariant(), "sdk", "Sdk.props");
                     var finalImportPath = Path.Combine(BuildEnvironmentHelper.Instance.MSBuildSdksPath,
-                        sdkName.ToLowerInvariant(), "Sdk", "Sdk.targets");
+                        sdkName.ToLowerInvariant(), "sdk", "Sdk.targets");
 
                     // TODO: don't require all SDKs to have both props and targets
                     // if (File.Exists(initialImportPath))
