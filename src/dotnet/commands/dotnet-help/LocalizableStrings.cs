e3cfe1d13dbd8277009e8695e29b12df3f298de7
Scott Carlton <v-sccarl@microsoft.com>
2016-12-04 20:24:02 -0800

    Localization for the Help Command (#4891)
    * Localization for the Help Command
    
    * Updated ProjModificationCommands
    
    * Added RemoveDefinition
    
    * Make UsageText `static readonly`

---
+++ src/dotnet/commands/dotnet-help/LocalizableStrings.cs
@@ -0,0 +1,61 @@
+﻿namespace Microsoft.DotNet.Tools.Help
+{
+    internal class LocalizableStrings
+    {
+        public const string Usage = "Usage";
+
+        public const string Arguments = "Arguments";
+
+        public const string CommandDefinition = "The command to execute";
+
+        public const string ArgumentsDefinition = "Arguments to pass to the command";
+
+        public const string HostOptionsDefinition = "Options specific to dotnet (host)";
+
+        public const string OptionsDescription = "Options common to all commands";
+
+        public const string CommonOptions = "Common options";
+
+        public const string VerboseDefinition = "Enable verbose output";
+
+        public const string HelpDefinition = "Show help";
+
+        public const string HostOptions = "Host options (passed before the command)";
+
+        public const string VersionDescription = "Display .NET CLI Version Number";
+
+        public const string InfoDescription = "Display .NET CLI Info";
+
+        public const string Commands = "Commands";
+
+        public const string NewDefinition = "Initialize a basic .NET project";
+
+        public const string RestoreDefinition = "Restore dependencies specified in the.NET project";
+
+        public const string BuildDefinition = "Builds a .NET project";
+
+        public const string PublishDefinition = "Publishes a .NET project for deployment (including the runtime)";
+
+        public const string RunDefinition = "Compiles and immediately executes a.NET project";
+
+        public const string TestDefinition = "Runs unit tests using the test runner specified in the project";
+
+        public const string PackDefinition = "Creates a NuGet package";
+
+        public const string MigrateDefinition = "Migrates a project.json based project to a msbuild based project";
+
+        public const string ProjectModificationCommands = "Project modification commands";
+
+        public const string AddDefinition = "Add items to the project";
+        
+        public const string RemoveDefinition = "Remove items from the project";
+
+        public const string AdvancedCommands = "Advanced Commands";
+
+        public const string NugetDefinition = "Provides additional NuGet commands";
+
+        public const string MsBuildDefinition = "msbuilds a project and all of its dependencies";
+
+        public const string VsTestDefinition = "Runs tests from the specified files";
+    }
+}
