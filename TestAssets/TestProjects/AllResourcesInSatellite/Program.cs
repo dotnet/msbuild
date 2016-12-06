0ed792aafa8e6c57636087bb09f7878c7020b490
Nick Guerrera <nicholg@microsoft.com>
2016-12-06 15:52:58 -0800

    Stop defaulting NeutralLanguage
    This can cause MissingManifestException if project doesn't have
    English neutral resources in main assembly, or incorrectly displaying
    non-English neutral resources to Enlish end-user.

---
+++ TestAssets/TestProjects/AllResourcesInSatellite/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Reflection;
+using System.Resources;
+
+namespace AllResourcesInSatellite
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CultureInfo.CurrentUICulture = new CultureInfo("en-US");
+
+            var resources = new ResourceManager("AllResourcesInSatellite.Strings", typeof(Program).GetTypeInfo().Assembly);
+
+            Console.WriteLine(resources.GetString("HelloWorld"));
+        }
+    }
+}
