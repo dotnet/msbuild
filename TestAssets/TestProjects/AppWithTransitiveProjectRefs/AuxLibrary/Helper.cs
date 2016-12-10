69eabef35f94fce3bf1bb7ec16dc83d0aa86a3fb
Nat Ayewah <nayewah@microsoft.com>
2016-12-10 01:50:48 -0800

    Add assets and scaffolding for test to build App With Transitive ProjectRefs

---
+++ TestAssets/TestProjects/AppWithTransitiveProjectRefs/AuxLibrary/Helper.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace AuxLibrary
+{
+    public static class Helper
+    {
+        public static string GetMessage()
+        {
+            return "This string came from AuxLibrary!";
+        }
+    }
+}
