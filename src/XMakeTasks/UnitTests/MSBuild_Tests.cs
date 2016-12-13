3bfcf96f5506eb0b5c500d354348602a0f623431
Ankit Jain <ankit.jain@xamarin.com>
2016-12-13 16:44:55 -0500

    [tests] Disable failing MSBuildTask_Tests.PropertyOverridesContainSemicolon
    .. on mono/osx.
    
    - Fix the path issues in test
    - the remaining problem is that is it trying to invoke `csc.exe` from
      mono's framework directory. The correct fix would be the mono specific
      targets file which overrides `CscTool*` properties.

--- src/XMakeTasks/UnitTests/MSBuild_Tests.cs
+++ src/XMakeTasks/UnitTests/MSBuild_Tests.cs
@@ -329,6 +329,7 @@ namespace Microsoft.Build.UnitTests
 #else
         [Fact]
 #endif
+        [Trait("Category", "mono-osx-failing")]
         public void PropertyOverridesContainSemicolon()
         {
             ObjectModelHelpers.DeleteTempProjectDirectory();
@@ -339,7 +340,7 @@ namespace Microsoft.Build.UnitTests
 
             // Just a normal console application project.
             ObjectModelHelpers.CreateFileInTempProjectDirectory(
-                @"bug'533'369\Sub;Dir\ConsoleApplication1\ConsoleApplication1.csproj", @"
+                Path.Combine("bug'533'369", "Sub;Dir", "ConsoleApplication1", "ConsoleApplication1.csproj"), @"
 
                 <Project DefaultTargets=`Build` ToolsVersion=`msbuilddefaulttoolsversion` xmlns=`msbuildnamespace`>
                   <PropertyGroup>
@@ -377,7 +378,7 @@ namespace Microsoft.Build.UnitTests
 
             // Just a normal console application project.
             ObjectModelHelpers.CreateFileInTempProjectDirectory(
-                @"bug'533'369\Sub;Dir\ConsoleApplication1\Program.cs", @"
+                Path.Combine("bug'533'369", "Sub;Dir", "ConsoleApplication1", "Program.cs"), @"
                 using System;
 
                 namespace ConsoleApplication32
@@ -401,7 +402,7 @@ namespace Microsoft.Build.UnitTests
             // is coming from another property which is produced by CreateProperty and has
             // some special characters in it.
             ObjectModelHelpers.CreateFileInTempProjectDirectory(
-                @"bug'533'369\Sub;Dir\TeamBuild.proj", @"
+                Path.Combine("bug'533'369", "Sub;Dir", "TeamBuild.proj"), @"
                 <Project ToolsVersion=`msbuilddefaulttoolsversion` xmlns=`msbuildnamespace`>
 
                     <Target Name=`Build`>
@@ -418,9 +419,9 @@ namespace Microsoft.Build.UnitTests
                 </Project>
                 ");
 
-            ObjectModelHelpers.BuildTempProjectFileExpectSuccess(@"bug'533'369\Sub;Dir\TeamBuild.proj");
+            ObjectModelHelpers.BuildTempProjectFileExpectSuccess(Path.Combine("bug'533'369", "Sub;Dir", "TeamBuild.proj"));
 
-            ObjectModelHelpers.AssertFileExistsInTempProjectDirectory(@"bug'533'369\Sub;Dir\binaries\ConsoleApplication1.exe");
+            ObjectModelHelpers.AssertFileExistsInTempProjectDirectory(Path.Combine("bug'533'369", "Sub;Dir", "binaries", "ConsoleApplication1.exe"));
         }
 
         /// <summary>
