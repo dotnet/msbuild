eee0602b1a752abde5c52d63e59d0e16e46ab139
Mike Lorbetske <mlorbe@microsoft.com>
2016-12-20 23:14:25 -0800

    Installer & build cleanup
    Add nuget package installer based on #119
    Make failing builds fail faster
    Apply fix to debug:attach from #119
    Add packages folder
    Rename .netnew folder to .templateengine

---
+++ src/dotnet-new3/Package.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace dotnet_new3
+{
+    internal class Package
+    {
+        public string Name { get; }
+
+        public string Version { get; }
+
+        public Package(string name, string version)
+        {
+            Name = name;
+            Version = version;
+        }
+
+        public static bool TryParse(string spec, out Package package)
+        {
+            int index = 0;
+            if (string.IsNullOrEmpty(spec) || (index = spec.IndexOf("::", StringComparison.Ordinal)) < 0 || index == spec.Length - 1)
+            {
+                package = null;
+                return false;
+            }
+
+            string name = spec.Substring(0, index);
+            string version = spec.Substring(index + 2);
+
+            package = new Package(name, version);
+            return true;
+        }
+    }
+}
