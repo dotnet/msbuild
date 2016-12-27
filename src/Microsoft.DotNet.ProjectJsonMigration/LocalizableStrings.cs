e9dc08eafa1e1792b2410999cfc149f49be615c1
Krzysztof Wicher <mordotymoja@gmail.com>
2016-12-27 14:13:44 -0800

    make localizablestrings internal again

--- src/Microsoft.DotNet.ProjectJsonMigration/LocalizableStrings.cs
+++ src/Microsoft.DotNet.ProjectJsonMigration/LocalizableStrings.cs
@@ -3,7 +3,7 @@
 
 namespace Microsoft.DotNet.ProjectJsonMigration
 {
-    public class LocalizableStrings
+    internal class LocalizableStrings
     {
         public const string DoubleMigrationError = "Detected double project migration: {0}";
 
