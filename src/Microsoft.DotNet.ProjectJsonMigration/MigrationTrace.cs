35bc8e1cc11c80cfc79e6017f9e38a75ada31464
Krzysztof Wicher <mordotymoja@gmail.com>
2016-12-27 13:19:40 -0800

    remove cross project reference from dotnet-add-p2p tests

--- src/Microsoft.DotNet.ProjectJsonMigration/MigrationTrace.cs
+++ src/Microsoft.DotNet.ProjectJsonMigration/MigrationTrace.cs
@@ -2,37 +2,47 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
+using System.IO;
+using System.Text;
 
 namespace Microsoft.DotNet.ProjectJsonMigration
 {
-    internal class MigrationTrace
+    internal class MigrationTrace : TextWriter
     {
         public static MigrationTrace Instance { get; set; }
+
+        public string EnableEnvironmentVariable => "DOTNET_MIGRATION_TRACE";
+        public bool IsEnabled { get; private set; }
+
+        private TextWriter _underlyingWriter;
 
         static MigrationTrace ()
         {
             Instance = new MigrationTrace();
         }
 
-        public string EnableEnvironmentVariable => "DOTNET_MIGRATION_TRACE";
-
-        public bool IsEnabled
+        public MigrationTrace()
         {
-            get
-            {
-#if DEBUG
-                return true;
-#else
-                return Environment.GetEnvironmentVariable(EnableEnvironmentVariable) != null;
-#endif
-            }
+            _underlyingWriter = Console.Out;
+            IsEnabled = IsEnabledValue();
         }
 
-        public void WriteLine(string message)
+        private bool IsEnabledValue()
+        {
+#if DEBUG
+            return true;
+#else
+            return Environment.GetEnvironmentVariable(EnableEnvironmentVariable) != null;
+#endif
+        }
+
+        public override Encoding Encoding => _underlyingWriter.Encoding;
+
+        public override void Write(char value)
         {
             if (IsEnabled)
             {
-                Console.WriteLine(message);
+                _underlyingWriter.Write(value);
             }
         }
     }
