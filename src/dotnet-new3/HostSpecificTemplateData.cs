44466c41a2983510c25aa9754182088c28cec7a1
seancpeters <sepeters@microsoft.com>
2017-01-03 19:59:06 -0800

    reworked cli specific config to allow more parameter overrides (#156)
    * reworked cli specifc config to allow more parameter overrides
    
    * replaced tabs with spaces

--- src/dotnet-new3/HostSpecificTemplateData.cs
+++ src/dotnet-new3/HostSpecificTemplateData.cs
@@ -9,18 +9,84 @@ namespace dotnet_new3
 {
     internal class HostSpecificTemplateData
     {
+        private static readonly string IsHiddenKey = "isHidden";
+        private static readonly string LongNameKey = "longName";
+        private static readonly string ShortNameKey = "shortName";
+
         public static HostSpecificTemplateData Default { get; } = new HostSpecificTemplateData();
 
         public HostSpecificTemplateData()
         {
-            HiddenParameters = new HashSet<string>(StringComparer.OrdinalIgnoreCase);
-            ParameterMap = new Dictionary<string, string>();
+            SymbolInfo = new Dictionary<string, Dictionary<string, string>>();
         }
 
         [JsonProperty]
-        public HashSet<string> HiddenParameters { get; }
+        public Dictionary<string, Dictionary<string, string>> SymbolInfo { get; }
 
-        [JsonProperty]
-        public Dictionary<string, string> ParameterMap { get; }
+        public HashSet<string> HiddenParameterNames
+        {
+            get
+            {
+                HashSet<string> hiddenNames = new HashSet<string>();
+                foreach (KeyValuePair<string, Dictionary<string, string>> paramInfo in SymbolInfo)
+                {
+                    if (paramInfo.Value.TryGetValue(IsHiddenKey, out string hiddenStringValue)
+                        && bool.TryParse(hiddenStringValue, out bool hiddenBoolValue)
+                        && hiddenBoolValue)
+                    {
+                        hiddenNames.Add(paramInfo.Key);
+                    }
+                }
+
+                return hiddenNames;
+            }
+        }
+
+        public Dictionary<string, string> LongNameOverrides
+        {
+            get
+            {
+                Dictionary<string, string> map = new Dictionary<string, string>();
+
+                foreach (KeyValuePair<string, Dictionary<string, string>> paramInfo in SymbolInfo)
+                {
+                    if (paramInfo.Value.TryGetValue(LongNameKey, out string longNameOverride))
+                    {
+                        map.Add(paramInfo.Key, longNameOverride);
+                    }
+                }
+
+                return map;
+            }
+        }
+
+        public Dictionary<string, string> ShortNameOverrides
+        {
+            get
+            {
+                Dictionary<string, string> map = new Dictionary<string, string>();
+
+                foreach (KeyValuePair<string, Dictionary<string, string>> paramInfo in SymbolInfo)
+                {
+                    if (paramInfo.Value.TryGetValue(ShortNameKey, out string shortNameOverride))
+                    {
+                        map.Add(paramInfo.Key, shortNameOverride);
+                    }
+                }
+
+                return map;
+            }
+        }
+
+        public string DisplayNameForParameter(string parameterName)
+        {
+            if (SymbolInfo.TryGetValue(parameterName, out Dictionary<string, string> configForParam)
+                && configForParam.TryGetValue(LongNameKey, out string longName))
+            {
+                return longName;
+            }
+
+            return parameterName;
+        }
     }
 }
