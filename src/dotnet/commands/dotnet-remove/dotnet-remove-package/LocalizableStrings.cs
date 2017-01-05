a461cfbb2b73201ae3d3dbc0f27e7a33f5e81850
Ankit Mishra <anmishr@microsoft.com>
2017-01-04 18:48:14 -0800

    Adding dotnet remove package sub command

---
+++ src/dotnet/commands/dotnet-remove/dotnet-remove-package/LocalizableStrings.cs
@@ -0,0 +1,17 @@
+// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Microsoft.DotNet.Tools.Remove.PackageReference
+{
+    internal class LocalizableStrings
+    {
+        public const string AppFullName = ".NET Remove Package reference Command";
+
+        public const string AppDescription = "Command to remove package reference";
+
+        public const string AppHelpText = "Package references to remove";
+
+        public const string SpecifyExactlyOnePackageReference = "Please specify one package reference to remove";
+
+    }
+}
