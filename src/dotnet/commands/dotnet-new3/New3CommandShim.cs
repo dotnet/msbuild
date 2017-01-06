decec5f8dafaef2e9ba5de80ff7af1671d0e2b42
Mike Lorbetske <mike.lorbetske@n3-p.com>
2017-01-06 14:48:27 -0800

    First step to ingest template engine (#5065)
    * First step to ingest template engine
    
    Please do not merge yet
    @piotrp @seancpeters @livarocc
    
    * Localization
    
    * Attempt to get a build going
    
    Rename program.cs to New3Command.cs
    Move TableFormatter into its own file
    Consume template engine packages version 1.0.0-beta1-20161218-24
    Temporarily add MyGet templating feed so that package restore will work
    
    * Update ExtendedCommandParser
    
    Make short form args prefer longer runs of characters in the source
    parameter before falling back to p:shortname syntax
    Change scoping for a few methods to get rid of inconsistent visiblity
    errors
    
    * Fix package installation wildcards
    
    * Cleanup New3Command, fix review issue, bump version, sorting for template list
    
    * Installation, loc fix, help formatting
    
    * Use latest TemplateEngine packages
    
    * New3 unit tests
    
    * Fixed formatting on csproj files
    
    * Add the build steps to add templates to the layout
    
    * Change tests slightly to make comparisons easier
    
    Also fixes the wrong flag getting passed to set language
    
    * Fixes for 127, 128, 130, 131 - help display
    
    * Sync to latest TemplateEngine version
    
    Absorbs new search logic
    
    * All tests passing
    
    Cleaner New3Command
    Support for project/item template contexts
    
    * Try to make tests more durable
    
    * Disable test parallelization for dotnet-new tests
    
    * Update web SDK and template engine versions
    
    * Remove AI package feed
    
    * Simplify CLI interface to Template Engine
    
    * Fix host identifier and update to latest packages
    
    * Update template engine & Web SDK versions
    
    * Fix template engine version
    
    * Remove UsingTask and redeclaration of property

---
+++ src/dotnet/commands/dotnet-new3/New3CommandShim.cs
@@ -0,0 +1,71 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using Microsoft.TemplateEngine.Abstractions;
+using Microsoft.TemplateEngine.Cli;
+using Microsoft.TemplateEngine.Edge;
+using Microsoft.TemplateEngine.Orchestrator.RunnableProjects;
+using Microsoft.TemplateEngine.Orchestrator.RunnableProjects.Config;
+using Microsoft.TemplateEngine.Orchestrator.RunnableProjects.Macros;
+using Microsoft.TemplateEngine.Utils;
+
+namespace Microsoft.DotNet.Tools.New3
+{
+    internal class New3CommandShim
+    {
+        private const string HostIdentifier = "dotnetcli";
+        private const string HostVersion = "1.0.0";
+        private const string CommandName = "new3";
+
+        public static int Run(string[] args)
+        {
+            return New3Command.Run(CommandName, CreateHost(), FirstRun, args);
+        }
+
+        private static ITemplateEngineHost CreateHost()
+        {
+            var builtIns = new Dictionary<Guid, Func<Type>>
+            {
+                { new Guid("0C434DF7-E2CB-4DEE-B216-D7C58C8EB4B3"), () => typeof(RunnableProjectGenerator) },
+                { new Guid("3147965A-08E5-4523-B869-02C8E9A8AAA1"), () => typeof(BalancedNestingConfig) },
+                { new Guid("3E8BCBF0-D631-45BA-A12D-FBF1DE03AA38"), () => typeof(ConditionalConfig) },
+                { new Guid("A1E27A4B-9608-47F1-B3B8-F70DF62DC521"), () => typeof(FlagsConfig) },
+                { new Guid("3FAE1942-7257-4247-B44D-2DDE07CB4A4A"), () => typeof(IncludeConfig) },
+                { new Guid("3D33B3BF-F40E-43EB-A14D-F40516F880CD"), () => typeof(RegionConfig) },
+                { new Guid("62DB7F1F-A10E-46F0-953F-A28A03A81CD1"), () => typeof(ReplacementConfig) },
+                { new Guid("370996FE-2943-4AED-B2F6-EC03F0B75B4A"), () => typeof(ConstantMacro) },
+                { new Guid("BB625F71-6404-4550-98AF-B2E546F46C5F"), () => typeof(EvaluateMacro) },
+                { new Guid("10919008-4E13-4FA8-825C-3B4DA855578E"), () => typeof(GuidMacro) },
+                { new Guid("F2B423D7-3C23-4489-816A-41D8D2A98596"), () => typeof(NowMacro) },
+                { new Guid("011E8DC1-8544-4360-9B40-65FD916049B7"), () => typeof(RandomMacro) },
+                { new Guid("8A4D4937-E23F-426D-8398-3BDBD1873ADB"), () => typeof(RegexMacro) },
+                { new Guid("B57D64E0-9B4F-4ABE-9366-711170FD5294"), () => typeof(SwitchMacro) },
+                { new Guid("10919118-4E13-4FA9-825C-3B4DA855578E"), () => typeof(CaseChangeMacro) }
+            }.ToList();
+
+            var preferences = new Dictionary<string, string>
+            {
+                { "prefs:language", "C#" }
+            };
+
+            return new DefaultTemplateEngineHost(HostIdentifier, HostVersion, CultureInfo.CurrentCulture.Name, preferences, builtIns);
+        }
+
+        private static void FirstRun(ITemplateEngineHost host, IInstaller installer)
+        { 
+            var templatesDir = Path.Combine(Paths.Global.BaseDir, "Templates");
+
+            if (templatesDir.Exists())
+            {
+                var layoutIncludedPackages = host.FileSystem.EnumerateFiles(templatesDir, "*.nupkg", SearchOption.TopDirectoryOnly);
+                installer.InstallPackages(layoutIncludedPackages);
+            }
+        }
+    }
+}
