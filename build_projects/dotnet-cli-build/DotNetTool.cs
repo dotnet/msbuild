19ed5a558b14d7b542498b9541dd572cd18d10f2
Piotr Puszkiewicz <piotrp@microsoft.com>
2017-01-24 21:59:18 -0800

    WiP

--- build_projects/dotnet-cli-build/DotNetTool.cs
+++ build_projects/dotnet-cli-build/DotNetTool.cs
@@ -1,5 +1,6 @@
 ﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System.Linq;
 
 using Microsoft.Build.Framework;
 using Microsoft.Build.Utilities;
@@ -12,6 +13,10 @@ namespace Microsoft.DotNet.Cli.Build
     {
         public DotNetTool()
         {
+            EnvironmentVariables = new EnvironmentFilter()
+                .GetEnvironmentVariableNamesToRemove()
+                .Select(e => $"{e}=")
+                .ToArray();
         }
 
         protected abstract string Command { get; }
