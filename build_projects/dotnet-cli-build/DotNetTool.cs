c25abfb7c7fb559ddeeaeff633ad6942abd1db8e
Piotr Puszkiewicz <piotrp@microsoft.com>
2017-01-25 10:34:50 -0800

    WiP

--- build_projects/dotnet-cli-build/DotNetTool.cs
+++ build_projects/dotnet-cli-build/DotNetTool.cs
@@ -1,5 +1,6 @@
 ﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System;
 using System.Linq;
 
 using Microsoft.Build.Framework;
@@ -13,10 +14,20 @@ namespace Microsoft.DotNet.Cli.Build
     {
         public DotNetTool()
         {
+Log.LogMessage(MessageImportance.High, "STARTING "); 
             EnvironmentVariables = new EnvironmentFilter()
                 .GetEnvironmentVariableNamesToRemove()
                 .Select(e => $"{e}=")
                 .ToArray();
+            
+Log.LogMessage(MessageImportance.High, "OVERRIDING "); 
+
+            foreach (var ev in EnvironmentVariables)
+            {
+                Log.LogMessage(MessageImportance.High, $"{ev}");
+            }
+
+            throw new Exception($"{EnvironmentVariables.Count()}");
         }
 
         protected abstract string Command { get; }
