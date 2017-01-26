d3ba728fd111ad32c21819bb129284d19ad9ad5b
Daniel Plaisted <daplaist@microsoft.com>
2017-01-26 10:01:08 -0800

    Set VS Install location environment variables in netci.groovy

--- netci.groovy
+++ netci.groovy
@@ -41,7 +41,10 @@ osList.each { os ->
             steps {
                 if (osBase == 'Windows_NT') {
                     // Batch
-                    batchFile(buildCommand)
+                    batchFile("""SET VS150COMNTOOLS=%ProgramFiles(x86)%\\Microsoft Visual Studio\\2017\\Enterprise\\Common7\\Tools\\
+SET VSSDK150Install=%ProgramFiles(x86)%\\Microsoft Visual Studio\\2017\\Enterprise\\VSSDK\\
+SET VSSDKInstall=%ProgramFiles(x86)%\\Microsoft Visual Studio\\2017\\Enterprise\\VSSDK\\
+${buildCommand}""")
                 }
                 else {
                     // Shell
