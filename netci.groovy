0d3cffc1c4a4b79d1b12d1d05ffe26bab20c552b
Daniel Plaisted <daplaist@microsoft.com>
2017-01-26 11:30:18 -0800

    Don't set environment variables for VS SDK during CI runs
    The VS SDK isn't installed on CI machines

--- netci.groovy
+++ netci.groovy
@@ -44,8 +44,6 @@ osList.each { os ->
                 if (osBase == 'Windows_NT') {
                     // Batch
                     batchFile("""SET VS150COMNTOOLS=%ProgramFiles(x86)%\\Microsoft Visual Studio\\2017\\Enterprise\\Common7\\Tools\\
-SET VSSDK150Install=%ProgramFiles(x86)%\\Microsoft Visual Studio\\2017\\Enterprise\\VSSDK\\
-SET VSSDKInstall=%ProgramFiles(x86)%\\Microsoft Visual Studio\\2017\\Enterprise\\VSSDK\\
 ${buildCommand}""")
                 }
                 else {
