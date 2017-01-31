f3c9a53e5a4ae278db3dd4aed53ccdb1af38430a
Nick Guerrera <nicholg@microsoft.com>
2017-01-31 13:31:20 -0800

    Reapply reverted #626 to generate satellite assemblies
    This reverts commit 1ac0ffde855d0ca856c7aebe81443c252174f577.
    
    Conflicts:
     src/Tasks/Microsoft.NET.Build.Tasks/Resources/xlf/Strings.xlf
     src/VsixV3/Microsoft.Net.Sdk.swr

--- build.ps1
+++ build.ps1
@@ -80,7 +80,7 @@ if ($FullMSBuild)
     $env:DOTNET_SDK_TEST_MSBUILD_PATH = join-path $env:VSInstallDir "MSBuild\15.0\bin\MSBuild.exe"
 }
 
-$commonBuildArgs = echo $RepoRoot\build\build.proj /t:$buildTarget /m /nologo /p:Configuration=$Configuration /p:Platform=$Platform /p:SignType=$signType /verbosity:$Verbosity
+$commonBuildArgs = echo $RepoRoot\build\build.proj /t:$buildTarget /m:1 /nologo /p:Configuration=$Configuration /p:Platform=$Platform /p:SignType=$signType /verbosity:$Verbosity
 
 # NET Core Build 
 $msbuildSummaryLog = Join-Path -path $logPath -childPath "sdk.log"
