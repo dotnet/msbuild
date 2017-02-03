e342ee6ba290d1be073a172ce457787671a72e43
Daniel Plaisted <daplaist@microsoft.com>
2017-02-03 14:53:56 -0800

    Use case insensitive comparison for HashSet of NuGet package names

--- src/Tasks/Microsoft.NET.Build.Tasks/LockFileExtensions.cs
+++ src/Tasks/Microsoft.NET.Build.Tasks/LockFileExtensions.cs
@@ -82,7 +82,7 @@ namespace Microsoft.NET.Build.Tasks
             LockFileTargetLibrary platformLibrary,
             IDictionary<string, LockFileTargetLibrary> libraryLookup)
         {
-            var exclusionList = new HashSet<string>();
+            var exclusionList = new HashSet<string>(StringComparer.OrdinalIgnoreCase);
 
             exclusionList.Add(platformLibrary.Name);
             CollectDependencies(libraryLookup, platformLibrary.Dependencies, exclusionList);
