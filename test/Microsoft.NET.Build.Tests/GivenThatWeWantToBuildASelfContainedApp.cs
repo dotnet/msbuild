77e71bc46a525d36204f2f961d89594472deb586
Eric Erhardt <eric.erhardt@microsoft.com>
2017-02-03 18:31:12 -0600

    Respond to PR feedback.
    Add options for appending TFM and RID to the output path.  Default the RID option to 'false', since it would be a breaking change at this point.
    
    Add extra comments explaining the difference between build and publish outputs in .NET Core projects.

--- test/Microsoft.NET.Build.Tests/GivenThatWeWantToBuildASelfContainedApp.cs
+++ test/Microsoft.NET.Build.Tests/GivenThatWeWantToBuildASelfContainedApp.cs
@@ -31,6 +31,8 @@ namespace Microsoft.NET.Build.Tests
                     var ns = project.Root.Name.Namespace;
                     var propertyGroup = project.Root.Elements(ns + "PropertyGroup").First();
                     propertyGroup.Add(new XElement(ns + "RuntimeIdentifier", runtimeIdentifier));
+
+                    propertyGroup.Add(new XElement(ns + "AppendRuntimeIdentifierToOutputPath", "true"));
                 })
                 .Restore();
 
