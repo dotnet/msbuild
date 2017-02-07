64c393f8e69d1e884329ef651cfb7416bb93cdd8
Eric Erhardt <eric.erhardt@microsoft.com>
2017-02-07 15:50:45 -0600

    Fixing self-contained tests on OSX Sierra
    Our self-contained tests use netcoreapp1.0 and the current machine's RID.  When building/running on OSX Sierra this is a problem because netcoreapp1.0 only supported the previous 2 OSX versions.
    
    The fix is to check if we are running on a newer OSX machine than what was supported, and downgrade the RID to a supported RID.
    
    Fix #834

---
+++ test/Microsoft.NET.TestFramework/EnvironmentInfo.cs
@@ -0,0 +1,34 @@
+// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using Microsoft.DotNet.InternalAbstractions;
+
+namespace Microsoft.NET.TestFramework
+{
+    public static class EnvironmentInfo
+    {
+        public static string GetCompatibleRid(string targetFramework)
+        {
+            string rid = RuntimeEnvironment.GetRuntimeIdentifier();
+
+            if (string.Equals(targetFramework, "netcoreapp1.0", StringComparison.OrdinalIgnoreCase))
+            {
+                // netcoreapp1.0 only supports osx.10.10 and osx.10.11
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+                {
+                    Version osVersion;
+                    if (Version.TryParse(RuntimeEnvironment.OperatingSystemVersion, out osVersion) &&
+                        osVersion > new Version(10, 11))
+                    {
+                        rid = "osx.10.11-x64";
+                    }
+                }
+            }
+
+            return rid;
+        }
+    }
+}
