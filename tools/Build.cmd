7b238e14560406b35ec1f62ca86edc1e8b022338
Vijay Ramakrishnan <vramak@microsoft.com>
2017-02-16 11:10:31 -0800

    Updating the build script to copy Web & ProjectSystem targets

--- tools/Build.cmd
+++ tools/Build.cmd
@@ -18,12 +18,23 @@ call dotnet build %WebSdkRoot%\src\Publish\Microsoft.NET.Sdk.Publish.Tasks\Micro
 if errorlevel 1 GOTO ERROR
 
 
-REM Copy the built output to the CLI folder for testing
+REM Copy Publish Sdks
 xcopy %WebSdkSource%\Publish\Microsoft.NET.Sdk.Publish.Targets\Sdk.props  %DOTNET_INSTALL_DIR%\Sdk\%DOTNET_VERSION%\Sdks\Microsoft.NET.Sdk.Publish\Sdk\ /y /C
 xcopy %WebSdkSource%\Publish\Microsoft.NET.Sdk.Publish.Targets\Sdk.targets  %DOTNET_INSTALL_DIR%\Sdk\%DOTNET_VERSION%\Sdks\Microsoft.NET.Sdk.Publish\Sdk\ /y /C
 
-xcopy %WebSdkSource%\Publish\Microsoft.NET.Sdk.Publish.Targets\netstandard1.0\*  %DOTNET_INSTALL_DIR%\Sdk\%DOTNET_VERSION%\Sdks\Microsoft.NET.Sdk.Publish\build\netstandard1.0\* /y /C /e /s /f
+REM Copy Project System Sdks
+xcopy %WebSdkSource%\Web\Microsoft.NET.Sdk.Web.ProjectSystem.Targets\Sdk.props  %DOTNET_INSTALL_DIR%\Sdk\%DOTNET_VERSION%\Sdks\Microsoft.NET.Sdk.Web.ProjectSystem\Sdk\ /y /C
+xcopy %WebSdkSource%\Web\Microsoft.NET.Sdk.Web.ProjectSystem.Targets\Sdk.targets  %DOTNET_INSTALL_DIR%\Sdk\%DOTNET_VERSION%\Sdks\Microsoft.NET.Sdk.Web.ProjectSystem\Sdk\ /y /C
 
+REM Copy Web Sdks
+xcopy %WebSdkSource%\Web\Microsoft.NET.Sdk.Web.Targets\Sdk.props  %DOTNET_INSTALL_DIR%\Sdk\%DOTNET_VERSION%\Sdks\Microsoft.NET.Sdk.Web\Sdk\ /y /C
+xcopy %WebSdkSource%\Web\Microsoft.NET.Sdk.Web.Targets\Sdk.targets  %DOTNET_INSTALL_DIR%\Sdk\%DOTNET_VERSION%\Sdks\Microsoft.NET.Sdk.Web\Sdk\ /y /C
+
+REM Copy Targets
+xcopy %WebSdkSource%\Publish\Microsoft.NET.Sdk.Publish.Targets\netstandard1.0\*  %DOTNET_INSTALL_DIR%\Sdk\%DOTNET_VERSION%\Sdks\Microsoft.NET.Sdk.Publish\build\netstandard1.0\* /y /C /e /s /f
+xcopy %WebSdkSource%\Web\Microsoft.NET.Sdk.Web.ProjectSystem.Targets\netstandard1.0\*  %DOTNET_INSTALL_DIR%\Sdk\%DOTNET_VERSION%\Sdks\Microsoft.NET.Sdk.Web.ProjectSystem\build\netstandard1.0\* /y /C /e /s /f
+
+REM Copy Tasks
 xcopy %WebSdkbin%\Release\net46\Microsoft.NET.Sdk.Publish.Tasks.dll %DOTNET_INSTALL_DIR%\Sdk\%DOTNET_VERSION%\Sdks\Microsoft.NET.Sdk.Publish\tools\net46\ /y /C
 xcopy %WebSdkbin%\Release\netstandard1.3\Microsoft.NET.Sdk.Publish.Tasks.dll %DOTNET_INSTALL_DIR%\Sdk\%DOTNET_VERSION%\Sdks\Microsoft.NET.Sdk.Publish\tools\netcoreapp1.0\ /y /C
 
