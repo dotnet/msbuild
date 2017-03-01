b64068291bd035f0a689640df59dfd73dec35182
Eric Erhardt <eric.erhardt@microsoft.com>
2017-03-01 15:12:16 -0600

    Remove OpenSUSE13 and Fedora23 since they are end of lifed.

--- build_projects/dotnet-cli-build/GetCurrentRuntimeInformation.cs
+++ build_projects/dotnet-cli-build/GetCurrentRuntimeInformation.cs
@@ -3,6 +3,7 @@
 
 using Microsoft.Build.Framework;
 using Microsoft.Build.Utilities;
+using Microsoft.DotNet.Cli.Build.Framework;
 using Microsoft.DotNet.PlatformAbstractions;
 
 namespace Microsoft.DotNet.Cli.Build
@@ -22,9 +23,25 @@ namespace Microsoft.DotNet.Cli.Build
         {
             Rid = RuntimeEnvironment.GetRuntimeIdentifier();
             Architecture = RuntimeEnvironment.RuntimeArchitecture;
-            OSName = Monikers.GetOSShortName();
+            OSName = GetOSShortName();
 
             return true;
+        }
+
+        private static string GetOSShortName()
+        {
+            string osname = "";
+            switch (CurrentPlatform.Current)
+            {
+                case BuildPlatform.Windows:
+                    osname = "win";
+                    break;
+                default:
+                    osname = CurrentPlatform.Current.ToString().ToLower();
+                    break;
+            }
+
+            return osname;
         }
     }
 }
