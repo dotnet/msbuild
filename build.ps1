689a05c306d48244b0817576646a12d5f9e6b038
Daniel Plaisted <daplaist@microsoft.com>
2017-03-03 16:56:55 -0800

    Update build scripts to explicitly install shared framework for 1.1.1, as the 2.0 CLI no longer does this

--- build.ps1
+++ build.ps1
@@ -54,6 +54,12 @@ Invoke-WebRequest $DOTNET_INSTALL_SCRIPT_URL -OutFile "$env:DOTNET_INSTALL_DIR\d
 & "$env:DOTNET_INSTALL_DIR\dotnet-install.ps1" -Version $DotnetCLIVersion $dotnetInstallVerbosity
 if($LASTEXITCODE -ne 0) { throw "Failed to install stage0" }
 
+if (!(Test-Path "$env:DOTNET_INSTALL_DIR\shared\Microsoft.NETCore.App\1.1.1"))
+{
+    & "$env:DOTNET_INSTALL_DIR\dotnet-install.ps1" -Channel "Release/1.1.0" -Version 1.1.1 -SharedRuntime
+    if($LASTEXITCODE -ne 0) { throw "Failed to install stage0" }
+}
+
 # Put the stage0 on the path
 $env:PATH = "$env:DOTNET_INSTALL_DIR;$env:PATH"
 
