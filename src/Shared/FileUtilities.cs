7fb752c2c17ceeee15d5868e339c7947f14f7709
Mihai Codoban <micodoba@microsoft.com>
2017-03-06 17:27:16 -0800

    Globbing APIS
    Expose msbuild's globbing logic via public APIs
    
    Addresses #1794

--- src/Shared/FileUtilities.cs
+++ src/Shared/FileUtilities.cs
@@ -1016,6 +1016,11 @@ namespace Microsoft.Build.Shared
             return s.Replace('\\', '/');
         }
 
+        internal static string WithTrailingSlash(this string s)
+        {
+            return EnsureTrailingSlash(s);
+        }
+
         internal static string NormalizeForPathComparison(this string s) => s.ToSlash().TrimTrailingSlashes();
 
         internal static bool PathsEqual(string path1, string path2)
