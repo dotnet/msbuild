120d28a78dc66f770c4d798b82905cc3a37397c2
Piotr Puszkiewicz <piotrp@microsoft.com>
2017-03-10 01:11:16 -0800

    Migrate dotnet-pack

--- src/dotnet/commands/dotnet-pack/PackCommandParser.cs
+++ src/dotnet/commands/dotnet-pack/PackCommandParser.cs
@@ -1,38 +1,40 @@
 // Copyright (c) .NET Foundation and contributors. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System.Linq;
 using Microsoft.DotNet.Cli.CommandLine;
+using LocalizableStrings = Microsoft.DotNet.Tools.Pack.LocalizableStrings;
 
 namespace Microsoft.DotNet.Cli
 {
     internal static class PackCommandParser
     {
         public static Command Pack() =>
-            Create.Command("pack",
-                           ".NET Core NuGet Package Packer",
-                           CommonOptions.HelpOption(),
-                           Create.Option("-o|--output",
-                                         "Directory in which to place built packages.",
-                                         Accept.ExactlyOneArgument
-                                               .With(name: "OUTPUT_DIR")),
-                           Create.Option("--no-build",
-                                         "Skip building the project prior to packing. By default, the project will be built."),
-                           Create.Option("--include-symbols",
-                                         "Include packages with symbols in addition to regular packages in output directory."),
-                           Create.Option("--include-source",
-                                         "Include PDBs and source files. Source files go into the src folder in the resulting nuget package"),
-                           Create.Option("-c|--configuration",
-                                         "Configuration to use for building the project.  Default for most projects is  \"Debug\".",
-                                         Accept.ExactlyOneArgument
-                                               .With(name: "CONFIGURATION")
-                                               .WithSuggestionsFrom("DEBUG",
-                                                                    "RELEASE")),
-                           Create.Option("--version-suffix",
-                                         "Defines the value for the $(VersionSuffix) property in the project.",
-                                         Accept.ExactlyOneArgument
-                                               .With(name: "VERSION_SUFFIX")),
-                           Create.Option("-s|--serviceable",
-                                         "Set the serviceable flag in the package. For more information, please see https://aka.ms/nupkgservicing."),
-                           CommonOptions.VerbosityOption());
+            Create.Command(
+                "pack",
+                LocalizableStrings.AppDescription,
+                Accept.ZeroOrMoreArguments,
+                CommonOptions.HelpOption(),
+                Create.Option(
+                    "-o|--output",
+                    LocalizableStrings.CmdOutputDirDescription,
+                    Accept.ExactlyOneArgument
+                        .With(name: LocalizableStrings.CmdOutputDir)
+                        .ForwardAs(o => $"/p:PackageOutputPath={o.Arguments.Single()}")),
+                Create.Option("--no-build",
+                                LocalizableStrings.CmdNoBuildOptionDescription,
+                                Accept.NoArguments.ForwardAs("/p:NoBuild=true")),
+                Create.Option("--include-symbols",
+                                LocalizableStrings.CmdIncludeSymbolsDescription,
+                                Accept.NoArguments.ForwardAs("/p:IncludeSymbols=true")),
+                Create.Option("--include-source",
+                                LocalizableStrings.CmdIncludeSourceDescription,
+                                Accept.NoArguments.ForwardAs("/p:IncludeSource=true")),
+                CommonOptions.ConfigurationOption(),
+                CommonOptions.VersionSuffixOption(),
+                Create.Option("-s|--serviceable",
+                              LocalizableStrings.CmdServiceableDescription,
+                              Accept.NoArguments.ForwardAs("/p:Serviceable=true")),
+                CommonOptions.VerbosityOption());
     }
 }
