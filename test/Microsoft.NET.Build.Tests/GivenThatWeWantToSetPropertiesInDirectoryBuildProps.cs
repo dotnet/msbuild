ea991d74c8f5593de5d7782d7e24be6090e4e3ea
Daniel Plaisted <daplaist@microsoft.com>
2017-03-13 18:21:27 -0700

    Add test for setting Configuration in Directory.Build.props

---
+++ test/Microsoft.NET.Build.Tests/GivenThatWeWantToSetPropertiesInDirectoryBuildProps.cs
@@ -0,0 +1,82 @@
+﻿using FluentAssertions;
+using Microsoft.NET.TestFramework;
+using Microsoft.NET.TestFramework.Assertions;
+using Microsoft.NET.TestFramework.ProjectConstruction;
+using Microsoft.NET.TestFramework.Commands;
+using static Microsoft.NET.TestFramework.Commands.MSBuildTest;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Xml.Linq;
+using Xunit;
+
+namespace Microsoft.NET.Build.Tests
+{
+    public class GivenThatWeWantToSetPropertiesInDirectoryBuildProps : SdkTest
+    {
+
+        [Fact]
+        public void The_default_configuration_can_be_set_to_release()
+        {
+            TestProject project = new TestProject()
+            {
+                Name = "DirectoryBuildPropsTest",
+                TargetFrameworks = "netstandard1.4",
+                IsSdkProject = true
+            };
+
+            var testAsset = _testAssetsManager.CreateTestProject(project);
+
+            string directoryBuildPropsPath = Path.Combine(testAsset.Path, "Directory.Build.props");
+            
+            var directoryBuildPropsContent = @"
+<Project>
+  <PropertyGroup>
+    <Configuration Condition="" '$(Configuration)' == '' "">Release</Configuration>
+  </PropertyGroup>
+</Project>
+";
+
+            File.WriteAllText(directoryBuildPropsPath, directoryBuildPropsContent);
+
+            var restoreCommand = testAsset.GetRestoreCommand(project.Name);
+
+            restoreCommand
+                .Execute()
+                .Should()
+                .Pass();
+
+            string projectFolder = Path.Combine(testAsset.Path, project.Name);
+
+            var buildCommand = new BuildCommand(Stage0MSBuild, projectFolder);
+
+            buildCommand
+                .Execute()
+                .Should()
+                .Pass();
+
+            string GetPropertyValue(string propertyName)
+            {
+                var getValuesCommand = new GetValuesCommand(Stage0MSBuild, projectFolder,
+                    project.TargetFrameworks, propertyName, GetValuesCommand.ValueType.Property)
+                {
+                    Configuration = "Release"
+                };
+
+                getValuesCommand
+                    .Execute()
+                    .Should()
+                    .Pass();
+
+                var values = getValuesCommand.GetValues();
+                values.Count.Should().Be(1);
+                return values[0];
+            }
+
+            GetPropertyValue("Configuration").Should().Be("Release");
+            GetPropertyValue("Optimize").Should().Be("true");
+        }
+
+    }
+}
