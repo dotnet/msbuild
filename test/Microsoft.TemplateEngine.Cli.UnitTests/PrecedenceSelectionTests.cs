c2cef3fc2fb67fd4bc06c3932c0e6936b251d1e3
Mike Lorbetske <mike.lorbetske@n3-p.com>
2017-03-15 13:04:43 -0700

    Support running multiple test batteries during creation tests

--- test/Microsoft.TemplateEngine.Cli.UnitTests/PrecedenceSelectionTests.cs
+++ test/Microsoft.TemplateEngine.Cli.UnitTests/PrecedenceSelectionTests.cs
@@ -2,6 +2,7 @@ using System;
 using System.Diagnostics;
 using System.IO;
 using System.Reflection;
+using System.Text;
 using Microsoft.TemplateEngine.Utils;
 using Xunit;
 
@@ -10,26 +11,18 @@ namespace Microsoft.TemplateEngine.Cli.UnitTests
     public class PrecedenceSelectionTests
     {
         [Theory(DisplayName = nameof(MvcCorrectlyDisambiguatesPrecedenceTest))]
-        [InlineData("MvcNoAuthTest.json", "mvc")]
-        [InlineData("MvcFramework20Test.json", "mvc")]
-        [InlineData("MvcIndAuthTest.json", "mvc -au individual")]
-        [InlineData("MvcFramework20Test.json", "mvc -au individual")]
+        [InlineData("mvc", "MvcNoAuthTest.json", "MvcFramework20Test.json")]
+        [InlineData("mvc -au individual", "MvcIndAuthTest.json", "MvcFramework20Test.json")]
 
-        [InlineData("MvcNoAuthTest.json", "mvc -f netcoreapp1.0")]
-        [InlineData("MvcFramework10Test.json", "mvc -f netcoreapp1.0")]
-        [InlineData("MvcIndAuthTest.json", "mvc -au individual -f netcoreapp1.0")]
-        [InlineData("MvcFramework10Test.json", "mvc -au individual -f netcoreapp1.0")]
+        [InlineData("mvc -f netcoreapp1.0", "MvcNoAuthTest.json", "MvcFramework10Test.json")]
+        [InlineData("mvc -au individual -f netcoreapp1.0", "MvcIndAuthTest.json", "MvcFramework10Test.json")]
 
-        [InlineData("MvcNoAuthTest.json", "mvc -f netcoreapp1.1")]
-        [InlineData("MvcFramework11Test.json", "mvc -f netcoreapp1.1")]
-        [InlineData("MvcIndAuthTest.json", "mvc -au individual -f netcoreapp1.1")]
-        [InlineData("MvcFramework11Test.json", "mvc -au individual -f netcoreapp1.1")]
+        [InlineData("mvc -f netcoreapp1.1", "MvcNoAuthTest.json", "MvcFramework11Test.json")]
+        [InlineData("mvc -au individual -f netcoreapp1.1", "MvcIndAuthTest.json", "MvcFramework11Test.json")]
 
-        [InlineData("MvcNoAuthTest.json", "mvc -f netcoreapp2.0")]
-        [InlineData("MvcFramework20Test.json", "mvc -f netcoreapp2.0")]
-        [InlineData("MvcIndAuthTest.json", "mvc -au individual -f netcoreapp2.0")]
-        [InlineData("MvcFramework20Test.json", "mvc -au individual -f netcoreapp2.0")]
-        public void MvcCorrectlyDisambiguatesPrecedenceTest(string script, string args)
+        [InlineData("mvc -f netcoreapp2.0", "MvcNoAuthTest.json", "MvcFramework20Test.json")]
+        [InlineData("mvc -au individual -f netcoreapp2.0", "MvcIndAuthTest.json", "MvcFramework20Test.json")]
+        public void MvcCorrectlyDisambiguatesPrecedenceTest(string args, params string[] scripts)
         {
             string codebase = typeof(PrecedenceSelectionTests).GetTypeInfo().Assembly.CodeBase;
             Uri cb = new Uri(codebase);
@@ -37,7 +30,17 @@ namespace Microsoft.TemplateEngine.Cli.UnitTests
             string dir = Path.GetDirectoryName(asmPath);
 
             string harnessPath = Path.Combine(dir, "..", "..", "..", "..", "Microsoft.TemplateEngine.EndToEndTestHarness");
-            string testScript = Path.Combine(dir, script);
+            int scriptCount = scripts.Length;
+            StringBuilder builder = new StringBuilder();
+            builder.Append(scriptCount);
+            builder.Append(" ");
+
+            foreach (string script in scripts)
+            {
+                string testScript = Path.Combine(dir, script);
+                builder.Append($"\"{testScript}\" ");
+            }
+
             string outputPath = Path.Combine(Directory.GetCurrentDirectory(), "temp");
 
             Process p = Process.Start(new ProcessStartInfo
@@ -48,7 +51,7 @@ namespace Microsoft.TemplateEngine.Cli.UnitTests
                 CreateNoWindow = false,
                 WorkingDirectory = harnessPath,
                 FileName = "dotnet",
-                Arguments = $"run \"{testScript}\" \"{outputPath}\" {args} -o \"{outputPath}\""
+                Arguments = $"run {builder} \"{outputPath}\" {args} -o \"{outputPath}\""
             });
 
             p.WaitForExit();
