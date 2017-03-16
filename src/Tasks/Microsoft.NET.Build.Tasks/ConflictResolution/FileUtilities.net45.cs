7543da3fa8d231818873608729d18a952456f30e
Daniel Plaisted <daplaist@microsoft.com>
2017-03-15 19:24:49 -0700

    Add conflict resolution tasks to Microsoft.NET.Build.Tasks project

--- src/Tasks/Microsoft.NET.Build.Tasks/ConflictResolution/FileUtilities.net45.cs
+++ src/Tasks/Microsoft.NET.Build.Tasks/ConflictResolution/FileUtilities.net45.cs
@@ -2,6 +2,8 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+#if NET46
+
 using System;
 using System.Reflection;
 
@@ -22,3 +24,5 @@ namespace Microsoft.DotNet.Build.Tasks
         }
     }
 }
+
+#endif
