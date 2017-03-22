09b6f33ff3fcf87c5e70c08401e319a4e4ab4a34
Mihai Codoban <micodoba@microsoft.com>
2017-03-21 17:27:09 -0700

    remove weird reference

--- src/Build.UnitTests/Evaluation/ItemEvaluation_Tests.cs
+++ src/Build.UnitTests/Evaluation/ItemEvaluation_Tests.cs
@@ -9,7 +9,6 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Runtime.Remoting.Channels;
 using Microsoft.Build.Evaluation;
 using Xunit;
 using System.Text;
