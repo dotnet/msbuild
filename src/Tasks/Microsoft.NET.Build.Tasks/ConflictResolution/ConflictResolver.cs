9c7dae0c828e51a2ae58445db232b7f0d413c332
Daniel Plaisted <daplaist@microsoft.com>
2017-04-03 10:59:17 -0700

    Fix incorrect logic (probably copy/paste error) in conflict resolver item type logic

--- src/Tasks/Microsoft.NET.Build.Tasks/ConflictResolution/ConflictResolver.cs
+++ src/Tasks/Microsoft.NET.Build.Tasks/ConflictResolution/ConflictResolver.cs
@@ -179,7 +179,7 @@ namespace Microsoft.NET.Build.Tasks.ConflictResolution
             if (!isPlatform1 && isPlatform2)
             {
                 log.LogMessage($"{conflictMessage}.  Choosing {item2.DisplayName} because it is a platform item.");
-                return item1;
+                return item2;
             }
 
             log.LogMessage($"{conflictMessage}.  Could not determine winner due to equal file and assembly versions.");
