347505dd1ef838ed5d4b0df248d90a2862b47547
Eric Erhardt <eric.erhardt@microsoft.com>
2017-04-04 15:55:23 -0500

    Fix SelfContained error on libraries.
    When publishing an app with a library and using --self-contained, the libraries are throwing an error saying it is not supported to have SelfContained without setting a RID.  We need to skip this error for libraries, since it doesn't make any sense.

--- test/Microsoft.NET.Publish.Tests/GivenThatWeWantToPublishAnAppWithLibrariesAndRid.cs
+++ test/Microsoft.NET.Publish.Tests/GivenThatWeWantToPublishAnAppWithLibrariesAndRid.cs
@@ -16,6 +16,49 @@ namespace Microsoft.NET.Publish.Tests
     public class GivenThatWeWantToPublishAnAppWithLibrariesAndRid : SdkTest
     {
         [Fact]
+        public void It_publishes_a_self_contained_runnable_output()
+        {
+            if (UsingFullFrameworkMSBuild)
+            {
+                //  Disable this test on full framework, as the current build won't have access to 
+                //  https://github.com/Microsoft/msbuild/pull/1674
+                //  See https://github.com/dotnet/sdk/issues/877
+                return;
+            }
+
+            PublishAppWithLibraryAndRid(true,
+                out var publishDirectory,
+                out var runtimeIdentifier);
+
+            var selfContainedExecutable = $"App{Constants.ExeSuffix}";
+
+            publishDirectory.Should().NotHaveSubDirectories();
+            publishDirectory.Should().HaveFiles(new[] {
+                selfContainedExecutable,
+                "App.dll",
+                "App.pdb",
+                "App.deps.json",
+                "App.runtimeconfig.json",
+                "LibraryWithoutRid.dll",
+                "LibraryWithoutRid.pdb",
+                "LibraryWithRid.dll",
+                "LibraryWithRid.pdb",
+                "LibraryWithRids.dll",
+                "LibraryWithRids.pdb",
+                $"{FileConstants.DynamicLibPrefix}sqlite3{Constants.DynamicLibSuffix}",
+                $"{FileConstants.DynamicLibPrefix}coreclr{Constants.DynamicLibSuffix}",
+                $"{FileConstants.DynamicLibPrefix}hostfxr{Constants.DynamicLibSuffix}",
+                $"{FileConstants.DynamicLibPrefix}hostpolicy{Constants.DynamicLibSuffix}",
+            });
+
+            Command.Create(Path.Combine(publishDirectory.FullName, selfContainedExecutable), new string[] { })
+                .CaptureStdOut()
+                .Execute()
+                .Should().Pass()
+                .And.HaveStdOutContaining($"3.13.0 '{runtimeIdentifier}' 3.13.0 '{runtimeIdentifier}' Hello World");
+        }
+
+        [Fact]
         public void It_publishes_a_framework_dependent_RID_specific_runnable_output()
         {
             if (UsingFullFrameworkMSBuild)
@@ -26,26 +69,9 @@ namespace Microsoft.NET.Publish.Tests
                 return;
             }
 
-            var runtimeIdentifier = RuntimeEnvironment.GetRuntimeIdentifier();
-            var testAsset = _testAssetsManager
-                .CopyTestAsset("AppWithLibraryAndRid", "PublishFrameworkDependentRIDSpecific")
-                .WithSource();
-
-            var projectPath = Path.Combine(testAsset.TestRoot, "App");
-
-            var restoreCommand = new RestoreCommand(Stage0MSBuild, projectPath, "App.csproj");
-            restoreCommand
-                .Execute($"/p:TestRuntimeIdentifier={runtimeIdentifier}")
-                .Should()
-                .Pass();
-
-            var publishCommand = new PublishCommand(Stage0MSBuild, projectPath);
-
-            publishCommand
-                .Execute($"/p:RuntimeIdentifier={runtimeIdentifier}", $"/p:TestRuntimeIdentifier={runtimeIdentifier}", "/p:SelfContained=false")
-                .Should().Pass();
-
-            var publishDirectory = publishCommand.GetOutputDirectory("netcoreapp1.1", runtimeIdentifier: runtimeIdentifier);
+            PublishAppWithLibraryAndRid(false,
+                out var publishDirectory,
+                out var runtimeIdentifier);
 
             publishDirectory.Should().NotHaveSubDirectories();
             publishDirectory.Should().OnlyHaveFiles(new[] {
@@ -67,6 +93,33 @@ namespace Microsoft.NET.Publish.Tests
                 .Execute()
                 .Should().Pass()
                 .And.HaveStdOutContaining($"3.13.0 '{runtimeIdentifier}' 3.13.0 '{runtimeIdentifier}' Hello World");
+        }
+
+        private void PublishAppWithLibraryAndRid(bool selfContained, out DirectoryInfo publishDirectory, out string runtimeIdentifier)
+        {
+            runtimeIdentifier = RuntimeEnvironment.GetRuntimeIdentifier();
+            var testAsset = _testAssetsManager
+                .CopyTestAsset("AppWithLibraryAndRid", $"PublishAppWithLibraryAndRid{selfContained}")
+                .WithSource();
+
+            var projectPath = Path.Combine(testAsset.TestRoot, "App");
+
+            var restoreCommand = new RestoreCommand(Stage0MSBuild, projectPath, "App.csproj");
+            restoreCommand
+                .Execute($"/p:TestRuntimeIdentifier={runtimeIdentifier}")
+                .Should()
+                .Pass();
+
+            var publishCommand = new PublishCommand(Stage0MSBuild, projectPath);
+
+            publishCommand
+                .Execute(
+                    $"/p:RuntimeIdentifier={runtimeIdentifier}", 
+                    $"/p:TestRuntimeIdentifier={runtimeIdentifier}", 
+                    $"/p:SelfContained={selfContained}")
+                .Should().Pass();
+
+            publishDirectory = publishCommand.GetOutputDirectory("netcoreapp1.1", runtimeIdentifier: runtimeIdentifier);
         }
     }
 }
