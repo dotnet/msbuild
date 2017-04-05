b8a9fcefd17a71bcc13360e00503cb9d2c45f142
Mike Lorbetske <mike.lorbetske@n3-p.com>
2017-04-05 13:29:53 -0700

    Get the basic read of the forms working
    Wire up replacing all forms of the value with the user supplied (non-transformed) value

---
+++ test/Microsoft.TemplateEngine.Cli.UnitTests/ValueFormsTests.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+
+namespace Microsoft.TemplateEngine.Cli.UnitTests
+{
+    public class ValueFormsTests : EndToEndTestBase
+    {
+        [Theory(DisplayName = nameof(ValueFormsTest))]
+        [InlineData("TestAssets.TemplateWithValueForms --foo Test.Value6", "TemplateWithValueForms.json")]
+        public void ValueFormsTest(string args, params string[] scripts)
+        {
+            Run(args, scripts);
+        }
+    }
+}
