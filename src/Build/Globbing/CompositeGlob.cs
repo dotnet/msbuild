b06a3d024208d2304818476cacfdcbfdd2feae00
Mihai Codoban <micodoba@microsoft.com>
2017-04-05 11:24:34 -0700

    Revert "A better GetAllGlobs (#1871)"
    This reverts commit 1b2c9de504835fe0705ed8b31c60c9febd04c52f.

--- src/Build/Globbing/CompositeGlob.cs
+++ src/Build/Globbing/CompositeGlob.cs
@@ -4,7 +4,6 @@
 
 
 using System.Collections.Generic;
-using System.Collections.Immutable;
 using System.Linq;
 
 namespace Microsoft.Build.Globbing
@@ -22,23 +21,17 @@ namespace Microsoft.Build.Globbing
         /// <summary>
         ///     Constructor
         /// </summary>
-        /// <param name="globs">Children globs. Input gets shallow cloned</param>
+        /// <param name="globs">Children globs</param>
         public CompositeGlob(IEnumerable<IMSBuildGlob> globs)
         {
-            // ImmutableArray also does this check, but copied it here just in case they remove it
-            if (globs is ImmutableArray<IMSBuildGlob>)
-            {
-                Globs = (ImmutableArray<IMSBuildGlob>)globs;
-            }
-
-            Globs = globs.ToImmutableArray();
+            Globs = globs;
         }
 
         /// <summary>
         ///     Constructor
         /// </summary>
-        /// <param name="globs">Children globs. Input gets shallow cloned</param>
-        public CompositeGlob(params IMSBuildGlob[] globs) : this(globs.ToImmutableArray())
+        /// <param name="globs">children globs</param>
+        public CompositeGlob(params IMSBuildGlob[] globs) : this(globs.AsEnumerable())
         {}
 
         /// <inheritdoc />
