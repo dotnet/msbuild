88412b4689a0c764e591508b4972c43982e5d4e2
Livar Cunha <livar@live.com>
2017-04-08 00:32:34 -0700

    Fixing the backwards compat test to use the stage2 with backwards compatible runtimes.

--- test/dotnet-back-compat.Tests/GivenThatWeWantToBeBackwardsCompatibleWith1xProjects.cs
+++ test/dotnet-back-compat.Tests/GivenThatWeWantToBeBackwardsCompatibleWith1xProjects.cs
@@ -42,7 +42,7 @@ namespace Microsoft.DotNet.Cli.Build.Tests
             var outputDll = testInstance.Root.GetDirectory("bin", configuration, target)
                 .GetFile($"{testAppName}.dll");
 
-            new DotnetCommand()
+            new DotnetCommand(DotnetUnderTest.WithBackwardsCompatibleRuntimes)
                 .ExecuteWithCapturedOutput(outputDll.FullName)
                 .Should().Pass()
                 .And.HaveStdOutContaining("Hello World");
