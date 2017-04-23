9253a9626a87bd50240c782ba0c856fd21b6da23
Mike Lorbetske <mike.lorbetske@n3-p.com>
2017-04-23 09:02:31 -0700

    Add tests for templates with an unspecified sourceName

--- test/Microsoft.TemplateEngine.Cli.UnitTests/FileRenameTests.cs
+++ test/Microsoft.TemplateEngine.Cli.UnitTests/FileRenameTests.cs
@@ -7,6 +7,7 @@ namespace Microsoft.TemplateEngine.Cli.UnitTests
         [Theory(DisplayName = nameof(VerifyTemplateContentRenames))]
         [InlineData("TestAssets.TemplateWithRenames --foo baz", "FileRenamesTest.json")]
         [InlineData("TestAssets.TemplateWithSourceName --name baz", "FileRenamesTest.json")]
+        [InlineData("TestAssets.TemplateWithUnspecifiedSourceName --name baz", "NegativeFileRenamesTest.json")]
         public void VerifyTemplateContentRenames(string args, params string[] scripts)
         {
             Run(args, scripts);
