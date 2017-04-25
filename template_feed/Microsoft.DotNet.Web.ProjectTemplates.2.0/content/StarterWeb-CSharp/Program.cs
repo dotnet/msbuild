9d844e3f3b7853fa006d43fe492be8df750c318e
Daniel Roth <daroth@microsoft.com>
2017-04-25 11:07:54 -0700

    Move Main method to the top

--- template_feed/Microsoft.DotNet.Web.ProjectTemplates.2.0/content/StarterWeb-CSharp/Program.cs
+++ template_feed/Microsoft.DotNet.Web.ProjectTemplates.2.0/content/StarterWeb-CSharp/Program.cs
@@ -12,14 +12,14 @@ namespace Company.WebApplication1
 {
     public class Program
     {
-        public static IWebHost BuildWebHost(string[] args) => WebHost
-            .CreateDefaultBuilder(args)
-            .UseStartup<Startup>()
-            .Build();
-
         public static void Main(string[] args)
         {
             BuildWebHost(args).Run();
         }
+
+        public static IWebHost BuildWebHost(string[] args) => WebHost
+            .CreateDefaultBuilder(args)
+            .UseStartup<Startup>()
+            .Build();
     }
 }
