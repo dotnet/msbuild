2ab4ac5454277f63ab3ae47ca6e391b96836dd92
William Li <wul@microsoft.com>
2017-04-25 10:18:10 -0700

    Adjust for using cli-migrate package reference
    Remove ProjectJsonMigration and its test from solution file
    
    Change dependecy by package reference instead of project 2 project
    
    Construct the sln and dotnet new with wrapper that is used in migrate
    package
    
    Add reference to Nuget ProjectModel etc since CrossGen need it and it
    cannot automatically get the reference

--- src/dotnet/commands/dotnet-migrate/DotnetNewRedirector.cs
+++ src/dotnet/commands/dotnet-migrate/DotnetNewRedirector.cs
@@ -1,9 +1,10 @@
 ﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using Microsoft.DotNet.Cli;
 using Microsoft.DotNet.Cli.Utils;
 using Microsoft.DotNet.Tools.MigrateCommand;
-using Microsoft.DotNet.Tools.New;
+using System.Collections.Generic;
 
 namespace Microsoft.DotNet.Tools.Migrate
 {
@@ -14,15 +15,26 @@ namespace Microsoft.DotNet.Tools.Migrate
             string outputDirectory,
             string workingDirectory)
         {
-            var newCommandArgs = new[] {templateName, "-o", "outputDirectory"};
-            var result = NewCommandShim.Run(newCommandArgs);
+            RunCommand("new", new string[] { "console", "-o", workingDirectory, "--debug:ephemeral-hive", "--no-restore" }, workingDirectory);
+        }
+        private void RunCommand(string commandToExecute, IEnumerable<string> args, string workingDirectory)
+        {
+            var command = new DotNetCommandFactory()
+                .Create(commandToExecute, args)
+                .WorkingDirectory(workingDirectory)
+                .CaptureStdOut()
+                .CaptureStdErr();
 
-            if (result != 0)
+            var commandResult = command.Execute();
+
+            if (commandResult.ExitCode != 0)
             {
-                throw new GracefulException(
-                    $"{nameof(NewCommandShim)} " +
-                    "failed to execuate given args " +
-                    $"{string.Join(" ", newCommandArgs)}");
+                string argList = string.Join(", ", args);
+                throw new GracefulException($"Failed to run {commandToExecute} with " +
+                    $"args: {argList} ... " +
+                    $"workingDirectory = {workingDirectory}, " +
+                    $"StdOut = {commandResult.StdOut}, " +
+                    $"StdErr = {commandResult.StdErr}");
             }
         }
     }
