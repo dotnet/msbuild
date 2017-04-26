99340d470d5fc97292ff06bd20bc6de29fcd8209
Andy Gerlicher <angerlic@microsoft.com>
2017-04-26 13:04:53 -0700

    Use AssemblyName.Clone when possible. AssemblyName.Clone is not available in early versions of .NET Core. Until we can upgrade MSBuild to use .NET Core 2.0, we can't call the Clone method directly. This change will attempt to call .Clone using reflection and if not available fallback to the previous .NET Core behavior.
    Potential fix for #2015 (when running on .NET Core 2.0).

--- src/Shared/AssemblyUtilities.cs
+++ src/Shared/AssemblyUtilities.cs
@@ -11,6 +11,13 @@ namespace Microsoft.Build.Shared
     /// </summary>
     internal static class AssemblyUtilities
     {
+        // True when the cached method info objects have been set.
+        private static bool s_initialized;
+
+        // Cached method info
+        private static MethodInfo s_assemblyNameCloneMethod;
+        private static PropertyInfo s_assemblylocationProperty;
+
         public static string GetAssemblyLocation(Assembly assembly)
         {
 #if FEATURE_ASSEMBLY_LOCATION
@@ -18,15 +25,14 @@ namespace Microsoft.Build.Shared
 #else
             // Assembly.Location is only available in .netstandard1.5, but MSBuild needs to target 1.3.
             // use reflection to access the property
+            Initialize();
 
-            var locationProperty = assembly.GetType().GetProperty("Location", typeof(string));
-
-            if (locationProperty == null)
+            if (s_assemblylocationProperty == null)
             {
                 throw new NotSupportedException("Type Assembly does not have the Location property");
             }
 
-            return (string)locationProperty.GetValue(assembly);
+            return (string)s_assemblylocationProperty.GetValue(assembly);
 #endif
         }
 
@@ -39,5 +45,37 @@ namespace Microsoft.Build.Shared
             return t;
         }
 #endif
+
+        public static AssemblyName CloneIfPossible(this AssemblyName assemblyNameToClone)
+        {
+#if FEATURE_ASSEMBLYNAME_CLONE
+            return (AssemblyName) assemblyNameToClone.Clone();
+#else
+
+            Initialize();
+
+            if (s_assemblyNameCloneMethod == null)
+            {
+                return new AssemblyName(assemblyNameToClone.FullName);
+            }
+
+            // Try to Invoke the Clone method via reflection. If the method exists (it will on .NET
+            // Core 2.0 or later) use that result, otherwise use new AssemblyName(FullName).
+            return (AssemblyName) s_assemblyNameCloneMethod.Invoke(assemblyNameToClone, null) ??
+                   new AssemblyName(assemblyNameToClone.FullName);
+#endif
+        }
+
+        /// <summary>
+        /// Initialize static fields. Doesn't need to be thread safe.
+        /// </summary>
+        private static void Initialize()
+        {
+            if (s_initialized) return;
+
+            s_assemblyNameCloneMethod = typeof(AssemblyName).GetMethod("Clone");
+            s_assemblylocationProperty = typeof(Assembly).GetProperty("Location", typeof(string));
+            s_initialized = true;
+        }
     }
 }
