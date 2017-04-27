8f5be100fb8cd71c8070e5f053c6f2fdf6a5c9cd
Ankit Jain <ankit.jain@xamarin.com>
2017-04-27 01:03:12 -0400

    Fix file path in item metadata, when CWD != project directory
    If a project A has:
    
        <Reference Include="foo">
            <HintPath>lib\foo.dll</HintPath>
        </Reference>
    
    .. we want to convert the HintPath metadata to unix style. And that
    happens in Expander.ExpandInto* methods.
    
    But if the solution is building A, then at evaluation time the CWD may not
    be set to the project A's directory, which would cause the
    `LooksLikeUnixFilePath` test to fail for `lib\foo.dll`, since `lib` does
    not exist in the solution's directory. And Expander methods don't have
    the context to know about this.
    
    So, instead we do this conversion in LazyItemOperation and attempt to
    convert the path, as if we were in that project A's directory.
    
        #/abc/foo/foo.sln
        abc/ $ msbuild foo/foo.sln
    
    Or basically any case where the CWD doesn't match project A's directory.
    
    The test project fails in ResolveAssemblyReference with:
    
    warning MSB3246: Resolved file has a bad image, no metadata, or is otherwise inaccessible. Could not find file "/Users/ankit/Projects/tests/nuget-tests/ConsoleApp2/ConsoleApp2/lib\fsharpFailsToEmbedResources.exe"
    .. or ..
    warning MSB3245: Could not resolve this reference. Could not locate the assembly "fsharpFailsToEmbedResources, Version=1.0.0.0". Check to make sure the assembly exists on disk. If this reference is required by your code, you may get compilation errors.
    
    Fixes: https://bugzilla.xamarin.com/show_bug.cgi?id=55629

---
+++ src/Build.UnitTests/FixPathOnUnix_Tests.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.IO;
+using Microsoft.Build.Evaluation;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using Xunit;
+
+namespace Microsoft.Build.UnitTests
+{
+    [PlatformSpecific(Xunit.PlatformID.AnyUnix)]
+    public  class FixPathOnUnixTests
+    {
+        string _assemblyPath;
+
+        public FixPathOnUnixTests()
+        {
+            _assemblyPath = System.Reflection.Assembly.GetExecutingAssembly().Location;
+        }
+
+        [Fact]
+        public void TestPathFixupInMetadata()
+        {
+            string buildProjectContents = @"
+                <Project xmlns='http://schemas.microsoft.com/developer/msbuild/2003' >
+                    <Target Name='Build'>
+                        <MSBuild Projects='projectDirectory/main.proj' />
+                    </Target>
+               </Project>";
+
+            string mainProjectContents = String.Format(@"
+                <Project xmlns='http://schemas.microsoft.com/developer/msbuild/2003' >
+                    <UsingTask TaskName='TestTask' AssemblyFile='{0}' />
+                    <ItemGroup>
+                        <Item0 Include='xyz'>
+                            <Md0>lib\foo.dll</Md0>
+                        </Item0>
+                    </ItemGroup>
+                    <Target Name='Build'>
+                        <TestTask Items='@(Item0)' />
+                    </Target>
+                </Project>", _assemblyPath);
+
+            string buildProjectPath = ObjectModelHelpers.CreateFileInTempProjectDirectory("build.proj", buildProjectContents);
+            string mainProjectPath = ObjectModelHelpers.CreateFileInTempProjectDirectory("projectDirectory/main.proj", mainProjectContents);
+            ObjectModelHelpers.CreateFileInTempProjectDirectory(Path.Combine("projectDirectory", "lib", "foo.dll"), "just a text file");
+
+            var projColln = new ProjectCollection();
+            var logger = new MockLogger();
+            projColln.RegisterLogger(logger);
+
+            var project = projColln.LoadProject(buildProjectPath);
+            var result = project.Build("Build");
+
+            Assert.True(result);
+
+            logger.AssertLogContains($"ItemMetadata: Md0 = {Path.Combine("lib", "foo.dll")}");
+        }
+    }
+
+    public class TestTask : Task
+    {
+        public ITaskItem[] Items { get; set; }
+
+        public override bool Execute()
+        {
+            if (Items != null)
+            {
+                foreach (var item in Items)
+                {
+                    Log.LogMessage ($"Item: {item.ItemSpec}");
+                    foreach (string name in item.MetadataNames)
+                    {
+                        Log.LogMessage ($"ItemMetadata: {name} = {item.GetMetadata(name)}");
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
