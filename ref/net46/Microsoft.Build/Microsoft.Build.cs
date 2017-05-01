dotnet/msbuild

commit 9e2254833ced58fd3bb55715e8d3bec8b89addc9

Add support for SDK Resolvers (#2002)
* Adds support for MSBuild to discover plug-in based resolvers for the
'SDK' import feature. A resolver assembly can be placed in:
    MSBuild\SdkResolver\(ResolverName)\(ResolverName).dll

MSBuild will create an instance of any class that implements SdkResolver
and attempt to resolve all SDK references with those resolver (sorted by
SdkResolver.Priority).

* Adds support for MSBuild to consume an Sdk reference as element (as an
alternative to a property on the Project element). Both versions of the
syntax will add an implicit Import element at the top and the bottom of
the file.

Related to #1493

ref/net46/Microsoft.Build/Microsoft.Build.cs
@@ -308,6 +308,7 @@ namespace Microsoft.Build.Construction
         public Microsoft.Build.Construction.ProjectOtherwiseElement CreateOtherwiseElement() { throw null; }
         public Microsoft.Build.Construction.ProjectOutputElement CreateOutputElement(string taskParameter, string itemType, string propertyName) { throw null; }
         public Microsoft.Build.Construction.ProjectExtensionsElement CreateProjectExtensionsElement() { throw null; }
+        public Microsoft.Build.Construction.ProjectSdkElement CreateProjectSdkElement(string sdkName, string sdkVersion) { throw null; }
         public Microsoft.Build.Construction.ProjectPropertyElement CreatePropertyElement(string name) { throw null; }
         public Microsoft.Build.Construction.ProjectPropertyGroupElement CreatePropertyGroupElement() { throw null; }
         public Microsoft.Build.Construction.ProjectTargetElement CreateTargetElement(string name) { throw null; }
@@ -333,6 +334,14 @@ namespace Microsoft.Build.Construction
         public static Microsoft.Build.Construction.ProjectRootElement TryOpen(string path) { throw null; }
         public static Microsoft.Build.Construction.ProjectRootElement TryOpen(string path, Microsoft.Build.Evaluation.ProjectCollection projectCollection) { throw null; }
         public static Microsoft.Build.Construction.ProjectRootElement TryOpen(string path, Microsoft.Build.Evaluation.ProjectCollection projectCollection, System.Nullable<bool> preserveFormatting) { throw null; }
+    }
+    public partial class ProjectSdkElement : Microsoft.Build.Construction.ProjectElementContainer
+    {
+        internal ProjectSdkElement() { }
+        public string MinimumVersion { get { throw null; } set { } }
+        public string Name { get { throw null; } set { } }
+        public string Version { get { throw null; } set { } }
+        protected override Microsoft.Build.Construction.ProjectElement CreateNewInstance(Microsoft.Build.Construction.ProjectRootElement owner) { throw null; }
     }
     [System.Diagnostics.DebuggerDisplayAttribute("Name={Name} #Children={Count} Condition={Condition}")]
     public partial class ProjectTargetElement : Microsoft.Build.Construction.ProjectElementContainer

ref/netstandard1.3/Microsoft.Build/Microsoft.Build.cs
@@ -308,6 +308,7 @@ namespace Microsoft.Build.Construction
         public Microsoft.Build.Construction.ProjectOtherwiseElement CreateOtherwiseElement() { throw null; }
         public Microsoft.Build.Construction.ProjectOutputElement CreateOutputElement(string taskParameter, string itemType, string propertyName) { throw null; }
         public Microsoft.Build.Construction.ProjectExtensionsElement CreateProjectExtensionsElement() { throw null; }
+        public Microsoft.Build.Construction.ProjectSdkElement CreateProjectSdkElement(string sdkName, string sdkVersion) { throw null; }
         public Microsoft.Build.Construction.ProjectPropertyElement CreatePropertyElement(string name) { throw null; }
         public Microsoft.Build.Construction.ProjectPropertyGroupElement CreatePropertyGroupElement() { throw null; }
         public Microsoft.Build.Construction.ProjectTargetElement CreateTargetElement(string name) { throw null; }
@@ -333,6 +334,14 @@ namespace Microsoft.Build.Construction
         public static Microsoft.Build.Construction.ProjectRootElement TryOpen(string path) { throw null; }
         public static Microsoft.Build.Construction.ProjectRootElement TryOpen(string path, Microsoft.Build.Evaluation.ProjectCollection projectCollection) { throw null; }
         public static Microsoft.Build.Construction.ProjectRootElement TryOpen(string path, Microsoft.Build.Evaluation.ProjectCollection projectCollection, System.Nullable<bool> preserveFormatting) { throw null; }
+    }
+    public partial class ProjectSdkElement : Microsoft.Build.Construction.ProjectElementContainer
+    {
+        internal ProjectSdkElement() { }
+        public string MinimumVersion { get { throw null; } set { } }
+        public string Name { get { throw null; } set { } }
+        public string Version { get { throw null; } set { } }
+        protected override Microsoft.Build.Construction.ProjectElement CreateNewInstance(Microsoft.Build.Construction.ProjectRootElement owner) { throw null; }
     }
     [System.Diagnostics.DebuggerDisplayAttribute("Name={Name} #Children={Count} Condition={Condition}")]
     public partial class ProjectTargetElement : Microsoft.Build.Construction.ProjectElementContainer

src/Build/Construction/ProjectImportElement.cs
@@ -6,7 +6,7 @@
 //-----------------------------------------------------------------------
 
 using System.Diagnostics;
-
+using Microsoft.Build.Framework;
 using Microsoft.Build.Shared;
 
 using ProjectXmlUtilities = Microsoft.Build.Internal.ProjectXmlUtilities;
@@ -31,7 +31,7 @@ namespace Microsoft.Build.Construction
         /// <summary>
         /// Initialize an unparented ProjectImportElement
         /// </summary>
-        private ProjectImportElement(XmlElementWithLocation xmlElement, ProjectRootElement containingProject)
+        internal ProjectImportElement(XmlElementWithLocation xmlElement, ProjectRootElement containingProject)
             : base(xmlElement, null, containingProject)
         {
         }
@@ -91,7 +91,12 @@ namespace Microsoft.Build.Construction
         /// added because of the <see cref="ProjectRootElement.Sdk"/> attribute and the location where the project was
         /// imported.
         /// </summary>
-        public ImplicitImportLocation ImplicitImportLocation { get; internal set; } = ImplicitImportLocation.None;
+        public ImplicitImportLocation ImplicitImportLocation { get; internal set; }
+
+        /// <summary>
+        /// <see cref="SdkReference"/> if applicable to this import element.
+        /// </summary>
+        internal SdkReference ParsedSdkReference { get; set; }
 
         /// <summary>
         /// Creates an unparented ProjectImportElement, wrapping an unparented XmlElement.
@@ -101,27 +106,23 @@ namespace Microsoft.Build.Construction
         internal static ProjectImportElement CreateDisconnected(string project, ProjectRootElement containingProject)
         {
             XmlElementWithLocation element = containingProject.CreateElement(XMakeElements.import);
-
-            ProjectImportElement import = new ProjectImportElement(element, containingProject);
-
-            import.Project = project;
-
-            return import;
+            return new ProjectImportElement(element, containingProject) {Project = project};
         }
 
         /// <summary>
         /// Creates an implicit ProjectImportElement as if it was in the project.
         /// </summary>
         /// <returns></returns>
-        internal static ProjectImportElement CreateImplicit(string project, ProjectRootElement containingProject, ImplicitImportLocation implicitImportLocation, string sdkName)
+        internal static ProjectImportElement CreateImplicit(string project, ProjectRootElement containingProject, ImplicitImportLocation implicitImportLocation, SdkReference sdkReference)
         {
-            ProjectImportElement import = CreateDisconnected(project, containingProject);
-
-            import.ImplicitImportLocation = implicitImportLocation;
-
-            import.Sdk = sdkName;
-
-            return import;
+            XmlElementWithLocation element = containingProject.CreateElement(XMakeElements.import);
+            return new ProjectImportElement(element, containingProject)
+            {
+                Project = project,
+                Sdk = sdkReference.ToString(),
+                ImplicitImportLocation = implicitImportLocation,
+                ParsedSdkReference = sdkReference
+            };
         }
 
         /// <summary>

src/Build/Evaluation/ProjectParser.cs
@@ -237,7 +237,9 @@ namespace Microsoft.Build.Construction
                     case XMakeElements.projectExtensions:
                         child = ParseProjectExtensionsElement(childElement);
                         break;
-
+                    case XMakeElements.sdk:
+                        child = ParseProjectSdkElement(childElement);
+                        break;
                     // Obsolete
                     case XMakeElements.error:
                     case XMakeElements.warning:
@@ -945,6 +947,19 @@ namespace Microsoft.Build.Construction
 
             // All children inside ProjectExtensions are ignored, since they are only part of its value
             return new ProjectExtensionsElement(element, _project, _project);
+        }
+
+        /// <summary>
+        /// Parse a ProjectExtensionsElement
+        /// </summary>
+        private ProjectSdkElement ParseProjectSdkElement(XmlElementWithLocation element)
+        {
+            if (string.IsNullOrEmpty(element.GetAttribute(XMakeAttributes.sdkName)))
+            {
+                ProjectErrorUtilities.ThrowInvalidProject(element.Location, "InvalidSdkElementName", element.Name);
+            }
+
+            return new ProjectSdkElement(element, _project, _project);
         }
     }
 }

commit 8993c2ba2f8fcf4511db3d4b6bcf7d6a4bcd908f

Fix issue importing an SDK via Import element.
* This feature was regressed in #2002
* Update unit tests to verify functionality
* Update ProjectParser to parse SDK name/version values and construct an SdkReference object to be used by the evaluator.

Closes #2034

ref/net46/Microsoft.Build/Microsoft.Build.cs
@@ -90,10 +90,12 @@ namespace Microsoft.Build.Construction
     {
         internal ProjectImportElement() { }
         public Microsoft.Build.Construction.ImplicitImportLocation ImplicitImportLocation { [System.Runtime.CompilerServices.CompilerGeneratedAttribute]get { throw null; } }
+        public string MinimumVersion { get { throw null; } set { } }
         public string Project { get { throw null; } set { } }
         public Microsoft.Build.Construction.ElementLocation ProjectLocation { get { throw null; } }
         public string Sdk { get { throw null; } set { } }
         public Microsoft.Build.Construction.ElementLocation SdkLocation { get { throw null; } }
+        public string Version { get { throw null; } set { } }
         protected override Microsoft.Build.Construction.ProjectElement CreateNewInstance(Microsoft.Build.Construction.ProjectRootElement owner) { throw null; }
     }
     [System.Diagnostics.DebuggerDisplayAttribute("#Imports={Count} Condition={Condition} Label={Label}")]

ref/netstandard1.3/Microsoft.Build/Microsoft.Build.cs
@@ -90,10 +90,12 @@ namespace Microsoft.Build.Construction
     {
         internal ProjectImportElement() { }
         public Microsoft.Build.Construction.ImplicitImportLocation ImplicitImportLocation { [System.Runtime.CompilerServices.CompilerGeneratedAttribute]get { throw null; } }
+        public string MinimumVersion { get { throw null; } set { } }
         public string Project { get { throw null; } set { } }
         public Microsoft.Build.Construction.ElementLocation ProjectLocation { get { throw null; } }
         public string Sdk { get { throw null; } set { } }
         public Microsoft.Build.Construction.ElementLocation SdkLocation { get { throw null; } }
+        public string Version { get { throw null; } set { } }
         protected override Microsoft.Build.Construction.ProjectElement CreateNewInstance(Microsoft.Build.Construction.ProjectRootElement owner) { throw null; }
     }
     [System.Diagnostics.DebuggerDisplayAttribute("#Imports={Count} Condition={Condition} Label={Label}")]

src/Build/Construction/ProjectImportElement.cs
@@ -22,10 +22,11 @@ namespace Microsoft.Build.Construction
         /// <summary>
         /// Initialize a parented ProjectImportElement
         /// </summary>
-        internal ProjectImportElement(XmlElementWithLocation xmlElement, ProjectElementContainer parent, ProjectRootElement containingProject)
+        internal ProjectImportElement(XmlElementWithLocation xmlElement, ProjectElementContainer parent, ProjectRootElement containingProject, SdkReference sdkReference = null)
             : base(xmlElement, parent, containingProject)
         {
             ErrorUtilities.VerifyThrowArgumentNull(parent, "parent");
+            ParsedSdkReference = sdkReference;
         }
 
         /// <summary>
@@ -75,9 +76,37 @@ namespace Microsoft.Build.Construction
             set
             {
                 ErrorUtilities.VerifyThrowArgumentLength(value, XMakeAttributes.sdk);
-
+                if (!CheckUpdatedSdk()) return;
                 ProjectXmlUtilities.SetOrRemoveAttribute(XmlElement, XMakeAttributes.sdk, value);
                 MarkDirty("Set Import Sdk {0}", value);
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the version associated with this SDK import
+        /// </summary>
+        public string Version
+        {
+            get { return ProjectXmlUtilities.GetAttributeValue(XmlElement, XMakeAttributes.sdkVersion); }
+            set
+            {
+                if (!CheckUpdatedSdk()) return;
+                ProjectXmlUtilities.SetOrRemoveAttribute(XmlElement, XMakeAttributes.sdkVersion, value);
+                MarkDirty("Set Import Version {0}", value);
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the minimum SDK version required by this import.
+        /// </summary>
+        public string MinimumVersion
+        {
+            get { return ProjectXmlUtilities.GetAttributeValue(XmlElement, XMakeAttributes.sdkMinimumVersion); }
+            set
+            {
+                if (!CheckUpdatedSdk()) return;
+                ProjectXmlUtilities.SetOrRemoveAttribute(XmlElement, XMakeAttributes.sdkMinimumVersion, value);
+                MarkDirty("Set Import Minimum Version {0}", value);
             }
         }
 
@@ -138,6 +167,23 @@ namespace Microsoft.Build.Construction
         protected override ProjectElement CreateNewInstance(ProjectRootElement owner)
         {
             return owner.CreateImportElement(this.Project);
+        }
+
+        private bool CheckUpdatedSdk()
+        {
+
+            SdkReference sdk = new SdkReference(
+                ProjectXmlUtilities.GetAttributeValue(XmlElement, XMakeAttributes.sdk, true),
+                ProjectXmlUtilities.GetAttributeValue(XmlElement, XMakeAttributes.sdkVersion, true),
+                ProjectXmlUtilities.GetAttributeValue(XmlElement, XMakeAttributes.sdkMinimumVersion, true));
+
+            if (sdk.Equals(ParsedSdkReference))
+            {
+                return false;
+            }
+
+            ParsedSdkReference = sdk;
+            return true;
         }
     }
 }

src/Build/Evaluation/ProjectParser.cs
@@ -11,6 +11,7 @@ using System.Xml;
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
+using Microsoft.Build.Framework;
 #if (!STANDALONEBUILD)
 using Microsoft.Internal.Performance;
 
@@ -515,12 +516,19 @@ namespace Microsoft.Build.Construction
                 );
 
             ProjectXmlUtilities.VerifyThrowProjectAttributes(element, s_validAttributesOnImport);
-
             ProjectXmlUtilities.VerifyThrowProjectRequiredAttribute(element, XMakeAttributes.project);
-
             ProjectXmlUtilities.VerifyThrowProjectNoChildElements(element);
 
-            return new ProjectImportElement(element, parent, _project);
+            SdkReference sdk = null;
+            if (element.HasAttribute(XMakeAttributes.sdk))
+            {
+                sdk = new SdkReference(
+                    element.GetAttribute(XMakeAttributes.sdk),
+                    element.GetAttribute(XMakeAttributes.sdkVersion),
+                    element.GetAttribute(XMakeAttributes.sdkMinimumVersion));
+            }
+
+            return new ProjectImportElement(element, parent, _project, sdk);
         }
 
         /// <summary>

src/Build.OM.UnitTests/Construction/ProjectSdkImplicitImport_Tests.cs
@@ -20,6 +20,24 @@ namespace Microsoft.Build.UnitTests.OM.Construction
     /// </summary>
     public class ProjectSdkImplicitImport_Tests : IDisposable
     {
+        private const string ProjectTemplateSdkAsAttribute = @"
+<Project Sdk=""{0}"">
+  {1}
+</Project>";
+
+        private const string ProjectTemplateSdkAsElement = @"
+<Project>
+  <Sdk Name=""{0}"" />
+  {1}
+</Project>";
+
+        private const string ProjectTemplateSdkAsExplicitImport = @"
+<Project>
+  <Import Project=""Sdk.props"" Sdk=""{0}"" />
+  {1}
+  <Import Project=""Sdk.targets"" Sdk=""{0}"" />
+</Project>";
+
         private const string SdkName = "MSBuildUnitTestSdk";
         private readonly string _testSdkRoot;
         private readonly string _testSdkDirectory;
@@ -37,29 +55,18 @@ namespace Microsoft.Build.UnitTests.OM.Construction
         }
 
         [Theory]
-        [InlineData(@"
-<Project Sdk=""{0}"">
-  <PropertyGroup>
-    <UsedToTestIfImplicitImportsAreInTheCorrectLocation>null</UsedToTestIfImplicitImportsAreInTheCorrectLocation>
-  </PropertyGroup>
-</Project>
-")]
-        [InlineData(@"
-<Project>
-  <Sdk Name=""{0}"" />
-  <PropertyGroup>
-    <UsedToTestIfImplicitImportsAreInTheCorrectLocation>null</UsedToTestIfImplicitImportsAreInTheCorrectLocation>
-  </PropertyGroup>
-</Project>
-")]
-        public void SdkImportsAreInLogicalProject(string projectFormatString)
+        [InlineData(ProjectTemplateSdkAsAttribute, false)]
+        [InlineData(ProjectTemplateSdkAsElement, true)]
+        [InlineData(ProjectTemplateSdkAsExplicitImport, false)]
+        public void SdkImportsAreInLogicalProject(string projectFormatString, bool expectImportInLogicalProject)
         {
+            string projectInnerContents = @"<PropertyGroup><UsedToTestIfImplicitImportsAreInTheCorrectLocation>null</UsedToTestIfImplicitImportsAreInTheCorrectLocation></PropertyGroup>";
             File.WriteAllText(_sdkPropsPath, "<Project><PropertyGroup><InitialImportProperty>Hello</InitialImportProperty></PropertyGroup></Project>");
             File.WriteAllText(_sdkTargetsPath, "<Project><PropertyGroup><FinalImportProperty>World</FinalImportProperty></PropertyGroup></Project>");
 
             using (new Helpers.TemporaryEnvironment("MSBuildSDKsPath", _testSdkRoot))
             {
-                string content = string.Format(projectFormatString, SdkName);
+                string content = string.Format(projectFormatString, SdkName, projectInnerContents);
 
                 ProjectRootElement projectRootElement = ProjectRootElement.Create(XmlReader.Create(new StringReader(content)));
 
@@ -68,42 +75,30 @@ namespace Microsoft.Build.UnitTests.OM.Construction
                 IList<ProjectElement> children = project.GetLogicalProject().ToList();
 
                 // <Sdk> style will have an extra ProjectElment.
-                var expected = projectFormatString.Contains("Sdk=") ? 6 : 7;
-                Assert.Equal(expected, children.Count);
+                Assert.Equal(expectImportInLogicalProject ? 7 : 6, children.Count);
             }
         }
 
         [Theory]
-        [InlineData(@"
-<Project Sdk=""{0}"">
-  <PropertyGroup>
-    <UsedToTestIfImplicitImportsAreInTheCorrectLocation>null</UsedToTestIfImplicitImportsAreInTheCorrectLocation>
-  </PropertyGroup>
-</Project>
-")]
-        [InlineData(@"
-<Project>
-  <Sdk Name=""{0}"" />
-  <PropertyGroup>
-    <UsedToTestIfImplicitImportsAreInTheCorrectLocation>null</UsedToTestIfImplicitImportsAreInTheCorrectLocation>
-  </PropertyGroup>
-</Project>
-")]
-        public void SdkImportsAreInImportList(string projectFormatString)
+        [InlineData(ProjectTemplateSdkAsAttribute, false)]
+        [InlineData(ProjectTemplateSdkAsElement, false)]
+        [InlineData(ProjectTemplateSdkAsExplicitImport, true)]
+        public void SdkImportsAreInImportList(string projectFormatString, bool expectImportInLogicalProject)
         {
+            string projectInnerContents = @"<PropertyGroup><UsedToTestIfImplicitImportsAreInTheCorrectLocation>null</UsedToTestIfImplicitImportsAreInTheCorrectLocation></PropertyGroup>";
             File.WriteAllText(_sdkPropsPath, "<Project><PropertyGroup><InitialImportProperty>Hello</InitialImportProperty></PropertyGroup></Project>");
             File.WriteAllText(_sdkTargetsPath, "<Project><PropertyGroup><FinalImportProperty>World</FinalImportProperty></PropertyGroup></Project>");
 
             using (new Helpers.TemporaryEnvironment("MSBuildSDKsPath", _testSdkRoot))
             {
-                string content = string.Format(projectFormatString, SdkName);
+                string content = string.Format(projectFormatString, SdkName, projectInnerContents);
 
                 ProjectRootElement projectRootElement = ProjectRootElement.Create(XmlReader.Create(new StringReader(content)));
 
                 var project = new Project(projectRootElement);
 
-                // The XML representation of the project should indicate there are no imports
-                Assert.Equal(0, projectRootElement.Imports.Count);
+                // The XML representation of the project should only indicate an import if they are not implicit.
+                Assert.Equal(expectImportInLogicalProject ? 2 : 0, projectRootElement.Imports.Count);
 
                 // The project representation should have imports
                 Assert.Equal(2, project.Imports.Count);
@@ -128,14 +123,24 @@ namespace Microsoft.Build.UnitTests.OM.Construction
         [Theory]
         [InlineData(@"
 <Project Sdk=""{0};{1};{2}"">
-</Project >")]
+</Project >", false)]
         [InlineData(@"
 <Project>
   <Sdk Name=""{0}"" />
   <Sdk Name=""{1}"" />
   <Sdk Name=""{2}"" />
-</Project>")]
-        public void SdkSupportsMultiple(string projectFormatString)
+</Project>", false)]
+        [InlineData(@"
+<Project>
+  <Import Project=""Sdk.props"" Sdk=""{0}"" />
+  <Import Project=""Sdk.props"" Sdk=""{1}"" />
+  <Import Project=""Sdk.props"" Sdk=""{2}"" />
+
+  <Import Project=""Sdk.targets"" Sdk=""{0}"" />
+  <Import Project=""Sdk.targets"" Sdk=""{1}"" />
+  <Import Project=""Sdk.targets"" Sdk=""{2}"" />
+</Project>", true)]
+        public void SdkSupportsMultiple(string projectFormatString, bool expectImportInLogicalProject)
         {
             IList<string> sdkNames = new List<string>
             {
@@ -161,7 +166,7 @@ namespace Microsoft.Build.UnitTests.OM.Construction
                 Project project = new Project(projectRootElement);
 
                 // The XML representation of the project should indicate there are no imports
-                Assert.Equal(0, projectRootElement.Imports.Count);
+                Assert.Equal(expectImportInLogicalProject ? 6 : 0, projectRootElement.Imports.Count);
 
                 // The project representation should have twice as many imports as SDKs
                 Assert.Equal(sdkNames.Count * 2, project.Imports.Count);
@@ -173,21 +178,12 @@ namespace Microsoft.Build.UnitTests.OM.Construction
         }
 
         [Theory]
-        [InlineData(@"<Project Sdk=""{0}"" ToolsVersion=""15.0"">
-")]
-        [InlineData(@"<Project ToolsVersion=""15.0"">
-  <Sdk Name=""{0}"" />
-")]
-        public void ProjectWithSdkImportsIsCloneable(string projectFileFirstLineFormat)
+        [InlineData(ProjectTemplateSdkAsAttribute)]
+        [InlineData(ProjectTemplateSdkAsElement)]
+        [InlineData(ProjectTemplateSdkAsExplicitImport)]
+        public void ProjectWithSdkImportsIsCloneable(string projectFormatString)
         {
-            File.WriteAllText(_sdkPropsPath, "<Project />");
-            File.WriteAllText(_sdkTargetsPath, "<Project />");
-
-            using (new Helpers.TemporaryEnvironment("MSBuildSDKsPath", _testSdkRoot))
-            {
-                // Based on the new-console-project CLI template (but not matching exactly
-                // should not be a deal-breaker).
-                string content = $@"{string.Format(projectFileFirstLineFormat, SdkName)}
+            string projectInnerContents = @"
   <PropertyGroup>
     <OutputType>Exe</OutputType>
     <TargetFramework>netcoreapp1.0</TargetFramework>
@@ -200,10 +196,15 @@ namespace Microsoft.Build.UnitTests.OM.Construction
 
   <ItemGroup>
     <PackageReference Include=""Microsoft.NETCore.App"" Version=""1.0.1"" />
-  </ItemGroup>
+  </ItemGroup>";
+            File.WriteAllText(_sdkPropsPath, " < Project />");
+            File.WriteAllText(_sdkTargetsPath, "<Project />");
 
-</Project>";
-
+            using (new Helpers.TemporaryEnvironment("MSBuildSDKsPath", _testSdkRoot))
+            {
+                // Based on the new-console-project CLI template (but not matching exactly
+                // should not be a deal-breaker).
+                string content = string.Format(projectFormatString, SdkName, projectInnerContents);
                 ProjectRootElement project = ProjectRootElement.Create(XmlReader.Create(new StringReader(content)));
 
                 project.DeepClone();
@@ -211,21 +212,12 @@ namespace Microsoft.Build.UnitTests.OM.Construction
         }
 
         [Theory]
-        [InlineData(@"<Project Sdk=""{0}"" ToolsVersion=""15.0"">
-")]
-        [InlineData(@"<Project ToolsVersion=""15.0"">
-  <Sdk Name=""{0}"" />
-")]
-        public void ProjectWithSdkImportsIsRemoveable(string projectFileFirstLineFormat)
+        [InlineData(ProjectTemplateSdkAsAttribute)]
+        [InlineData(ProjectTemplateSdkAsElement)]
+        [InlineData(ProjectTemplateSdkAsExplicitImport)]
+        public void ProjectWithSdkImportsIsRemoveable(string projectFormatString)
         {
-            File.WriteAllText(_sdkPropsPath, "<Project />");
-            File.WriteAllText(_sdkTargetsPath, "<Project />");
-
-            using (new Helpers.TemporaryEnvironment("MSBuildSDKsPath", _testSdkRoot))
-            {
-                // Based on the new-console-project CLI template (but not matching exactly
-                // should not be a deal-breaker).
-                string content = $@"{string.Format(projectFileFirstLineFormat, SdkName)}
+            string projectInnerContents = @"
   <PropertyGroup>
     <OutputType>Exe</OutputType>
     <TargetFramework>netcoreapp1.0</TargetFramework>
@@ -238,10 +230,15 @@ namespace Microsoft.Build.UnitTests.OM.Construction
 
   <ItemGroup>
     <PackageReference Include=""Microsoft.NETCore.App"" Version=""1.0.1"" />
-  </ItemGroup>
+  </ItemGroup>";
+            File.WriteAllText(_sdkPropsPath, " < Project />");
+            File.WriteAllText(_sdkTargetsPath, "<Project />");
 
-</Project>";
-
+            using (new Helpers.TemporaryEnvironment("MSBuildSDKsPath", _testSdkRoot))
+            {
+                // Based on the new-console-project CLI template (but not matching exactly
+                // should not be a deal-breaker).
+                string content = string.Format(projectFormatString, SdkName, projectInnerContents);
                 ProjectRootElement project = ProjectRootElement.Create(XmlReader.Create(new StringReader(content)));
                 ProjectRootElement clone = ProjectRootElement.Create(XmlReader.Create(new StringReader(content)));
 
@@ -280,21 +277,28 @@ namespace Microsoft.Build.UnitTests.OM.Construction
         /// <summary>
         /// Verifies that an empty SDK attribute works and nothing is imported.
         /// </summary>
-        [Fact]
-        public void ProjectWithEmptySdkName()
+        [Theory]
+        [InlineData(ProjectTemplateSdkAsAttribute, false)]
+        [InlineData(ProjectTemplateSdkAsElement, true)]
+        [InlineData(ProjectTemplateSdkAsExplicitImport, true)]
+        public void ProjectWithEmptySdkName(string projectFormatString, bool throwsOnEvaluate)
         {
+            string projectInnerContents =
+                @"<PropertyGroup><UsedToTestIfImplicitImportsAreInTheCorrectLocation>null</UsedToTestIfImplicitImportsAreInTheCorrectLocation></PropertyGroup>";
+
             using (new Helpers.TemporaryEnvironment("MSBuildSDKsPath", _testSdkRoot))
             {
-                string content = @"
-                    <Project Sdk="""">
-                        <PropertyGroup>
-                            <UsedToTestIfImplicitImportsAreInTheCorrectLocation>null</UsedToTestIfImplicitImportsAreInTheCorrectLocation>
-                        </PropertyGroup>
-                    </Project>";
-
-                Project project = new Project(ProjectRootElement.Create(XmlReader.Create(new StringReader(content))));
-
-                Assert.Equal(0, project.Imports.Count);
+                string content = string.Format(projectFormatString, string.Empty, projectInnerContents);
+                if (throwsOnEvaluate)
+                {
+                    Assert.Throws<InvalidProjectFileException>(
+                        () => new Project(ProjectRootElement.Create(XmlReader.Create(new StringReader(content)))));
+                }
+                else
+                {
+                    var project = new Project(ProjectRootElement.Create(XmlReader.Create(new StringReader(content))));
+                    Assert.Equal(0, project.Imports.Count);
+                }
             }
         }
 