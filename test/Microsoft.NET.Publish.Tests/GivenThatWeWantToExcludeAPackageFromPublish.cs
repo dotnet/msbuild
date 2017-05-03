7abe373fa66ee4052f94ca5249d80cbed3e11a6a
Daniel Plaisted <daplaist@microsoft.com>
2017-05-03 14:50:54 -0700

    Add tests for Publish metadata on PackageReference

--- test/Microsoft.NET.Publish.Tests/GivenThatWeWantToExcludeAPackageFromPublish.cs
+++ test/Microsoft.NET.Publish.Tests/GivenThatWeWantToExcludeAPackageFromPublish.cs
@@ -50,5 +50,76 @@ namespace Microsoft.NET.Publish.Tests
                 "HelloWorld.runtimeconfig.json"
             });
         }
+
+        [Fact]
+        public void It_does_not_publish_a_PackageReference_with_Publish_false()
+        {
+            var helloWorldAsset = _testAssetsManager
+                .CopyTestAsset("HelloWorld", "PublishPackagePublishFalse")
+                .WithSource()
+                .WithProjectChanges(project =>
+                {
+                    var ns = project.Root.Name.Namespace;
+
+                    var itemGroup = new XElement(ns + "ItemGroup");
+                    project.Root.Add(itemGroup);
+
+                    itemGroup.Add(new XElement(ns + "PackageReference", new XAttribute("Include", "Newtonsoft.Json"),
+                                                                        new XAttribute("Version", "9.0.1"),
+                                                                        new XAttribute("Publish", "false")));
+                })
+                .Restore();
+
+            var publishCommand = new PublishCommand(Stage0MSBuild, helloWorldAsset.TestRoot);
+            var publishResult = publishCommand.Execute();
+
+            publishResult.Should().Pass();
+
+            var publishDirectory = publishCommand.GetOutputDirectory();
+
+            publishDirectory.Should().OnlyHaveFiles(new[] {
+                "HelloWorld.dll",
+                "HelloWorld.pdb",
+                "HelloWorld.deps.json",
+                "HelloWorld.runtimeconfig.json"
+            });
+        }
+
+        [Fact]
+        public void It_publishes_a_PackageReference_with_PrivateAssets_All_and_Publish_true()
+        {
+            var helloWorldAsset = _testAssetsManager
+                .CopyTestAsset("HelloWorld", "PublishPrivateAssets")
+                .WithSource()
+                .WithProjectChanges(project =>
+                {
+                    var ns = project.Root.Name.Namespace;
+
+                    var itemGroup = new XElement(ns + "ItemGroup");
+                    project.Root.Add(itemGroup);
+
+                    itemGroup.Add(new XElement(ns + "PackageReference", new XAttribute("Include", "Newtonsoft.Json"),
+                                                                        new XAttribute("Version", "9.0.1"),
+                                                                        new XAttribute("PrivateAssets", "All"),
+                                                                        new XAttribute("Publish", "true")));
+                })
+                .Restore();
+
+            var publishCommand = new PublishCommand(Stage0MSBuild, helloWorldAsset.TestRoot);
+            var publishResult = publishCommand.Execute();
+
+            publishResult.Should().Pass();
+
+            var publishDirectory = publishCommand.GetOutputDirectory();
+
+            publishDirectory.Should().OnlyHaveFiles(new[] {
+                "HelloWorld.dll",
+                "HelloWorld.pdb",
+                "HelloWorld.deps.json",
+                "HelloWorld.runtimeconfig.json",
+                "Newtonsoft.Json.dll",
+                "System.Runtime.Serialization.Primitives.dll"
+            });
+        }
     }
 }
