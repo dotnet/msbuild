cb217bfdd0cdf7a4f5755281d2cec7dd6e7ed00f
Glenn Condron <glennc@microsoft.com>
2017-05-04 15:13:50 -0700

    Add correlation id to error page. (#728)
    Add a correlation id to the created error page.

--- template_feed/Microsoft.DotNet.Web.ProjectTemplates.2.0/content/RazorPagesWeb-CSharp/Pages/Index.cshtml.cs
+++ template_feed/Microsoft.DotNet.Web.ProjectTemplates.2.0/content/RazorPagesWeb-CSharp/Pages/Index.cshtml.cs
@@ -1,9 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Company.WebApplication1.Pages
 {
