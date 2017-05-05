62f5b50e9f23bfdac78d608a0192cb7985e603a7
Ankit Jain <ankit.jain@xamarin.com>
2017-05-05 15:08:07 -0400

    [mono] Add SdkResolver for .netcore 2.0
    This will help resolve SDKs for the .netcore 2.0 installation.
    
    - Microsoft.DotNet.MSBuildSdkResolver.*:
    	- from `https://github.com/dotnet/cli/` `release/2.0.0` branch
    	  with HEAD at 6e50ad963068f33edbaf5beca1940c1c86ca2def
    	  - locally built and copied
    		./src/Microsoft.DotNet.MSBuildSdkResolver/bin/Debug/netstandard1.3/Microsoft.DotNet.MSBuildSdkResolver.*
    
    - libhostfxr.dylib
    	- from nuget package `runtime.osx-x64.Microsoft.NETCore.DotNetHostResolver`
    	  version `2.0.0-preview1-002111-00`, taken from `CLI_SharedFrameworkVersion`
    	  https://github.com/dotnet/cli/blob/release/2.0.0/build/DependencyVersions.props#L4

--- install-mono-prefix.sh
+++ install-mono-prefix.sh
@@ -79,7 +79,7 @@ cp -R nuget-support/tasks-targets/* ${DESTDIR}${XBUILD_DIR}/
 
 # The directory might not exist on bockbuild when it runs this script.
 # Bockbuild will handle copying these files
-test -d ${XBUILD_DIR}/Microsoft/NuGet && for f in ${XBUILD_DIR}/Microsoft/NuGet/*; do ln -s $f ${DESTDIR}${XBUILD_DIR} ; done
+test -d ${XBUILD_DIR}/Microsoft/NuGet && for f in ${XBUILD_DIR}/Microsoft/NuGet/*; do ln -f -s $f ${DESTDIR}${XBUILD_DIR} ; done
 
 # man page
 mkdir -p ${DESTDIR}${MONO_PREFIX}/share/man/man1
@@ -91,6 +91,9 @@ SDKS_OUT_DIR=${MSBUILD_INSTALL_BIN_DIR}/Sdks
 
 cp -R ${SDKS_SRC_DIR}/ ${DESTDIR}${SDKS_OUT_DIR}
 
+SDK_RESOLVERS_OUT_DIR=${MSBUILD_INSTALL_BIN_DIR}
+mkdir -p ${DESTDIR}${SDK_RESOLVERS_OUT_DIR}
+cp -R mono/SdkResolvers ${DESTDIR}${SDK_RESOLVERS_OUT_DIR}
 
 sed -e 's,@bindir@,'$MONO_PREFIX'/bin,' -e 's,@mono_instdir@,'$MONO_PREFIX/lib/mono',' msbuild-mono-deploy.in > msbuild-mono-deploy.tmp
 chmod +x msbuild-mono-deploy.tmp
