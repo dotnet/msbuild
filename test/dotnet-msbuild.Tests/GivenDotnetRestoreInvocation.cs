357fd3daf1e55df9d1c6522c88c0bddb485113d6
Livar Cunha <livar@live.com>
2017-05-18 14:13:01 -0700

    Fixing restore so that it respects the verbosity param. The problem was that ConsoleLoggerParameters was overwritting whatever was coming through the command line.

--- test/dotnet-msbuild.Tests/GivenDotnetRestoreInvocation.cs
+++ test/dotnet-msbuild.Tests/GivenDotnetRestoreInvocation.cs
@@ -10,7 +10,11 @@ namespace Microsoft.DotNet.Cli.MSBuild.Tests
 {
     public class GivenDotnetRestoreInvocation
     {
-        const string ExpectedPrefix = "exec <msbuildpath> /m /v:m /NoLogo /t:Restore /ConsoleLoggerParameters:Verbosity=Minimal";
+        private const string ExpectedPrefix =
+            "exec <msbuildpath> /m /v:m /NoLogo /t:Restore";
+
+        private string ExpectedPrefixWithConsoleLoggerParamaters =
+            $"{ExpectedPrefix} /ConsoleLoggerParameters:Verbosity=Minimal";
 
         [Theory]
         [InlineData(new string[] { }, "")]
@@ -26,15 +30,26 @@ namespace Microsoft.DotNet.Cli.MSBuild.Tests
         [InlineData(new string[] { "--no-cache" }, "/p:RestoreNoCache=true")]
         [InlineData(new string[] { "--ignore-failed-sources" }, "/p:RestoreIgnoreFailedSources=true")]
         [InlineData(new string[] { "--no-dependencies" }, "/p:RestoreRecursive=false")]
-        [InlineData(new string[] { "-v", "minimal" }, @"/verbosity:minimal")]
-        [InlineData(new string[] { "--verbosity", "minimal" }, @"/verbosity:minimal")]
-        public void MsbuildInvocationIsCorrect(string[] args, string expectedAdditionalArgs)
+        public void MsbuildInvocationWithConsoleLoggerParametersIsCorrect(string[] args, string expectedAdditionalArgs)
         {
             expectedAdditionalArgs = (string.IsNullOrEmpty(expectedAdditionalArgs) ? "" : $" {expectedAdditionalArgs}");
 
             var msbuildPath = "<msbuildpath>";
             RestoreCommand.FromArgs(args, msbuildPath)
-                .GetProcessStartInfo().Arguments.Should().Be($"{ExpectedPrefix}{expectedAdditionalArgs}");
+                .GetProcessStartInfo().Arguments
+                .Should().Be($"{ExpectedPrefixWithConsoleLoggerParamaters}{expectedAdditionalArgs}");
+        }
+
+        [InlineData(new string[] { "-v", "minimal" }, @"/verbosity:minimal")]
+        [InlineData(new string[] { "--verbosity", "minimal" }, @"/verbosity:minimal")]
+        public void MsbuildInvocationWithVerbosityIsCorrect(string[] args, string expectedAdditionalArgs)
+        {
+            expectedAdditionalArgs = (string.IsNullOrEmpty(expectedAdditionalArgs) ? "" : $" {expectedAdditionalArgs}");
+
+            var msbuildPath = "<msbuildpath>";
+            RestoreCommand.FromArgs(args, msbuildPath)
+                .GetProcessStartInfo().Arguments
+                .Should().Be($"{ExpectedPrefix}{expectedAdditionalArgs}");
         }
     }
 }
