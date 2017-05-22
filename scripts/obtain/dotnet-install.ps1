283bff55ce5464632c484722c3bef2d20173ce46
John Beisner <jbeisner@microsoft.com>
2017-05-22 16:17:31 -0700

    Changes per manual testing on Windows-x64

--- scripts/obtain/dotnet-install.ps1
+++ scripts/obtain/dotnet-install.ps1
@@ -189,7 +189,7 @@ function Get-Latest-Version-Info([string]$AzureFeed, [string]$Channel, [string]$
 
     switch ($Response.Content.Headers.ContentType) {
         { ($_ -eq "application/octet-stream") } { $VersionText = [Text.Encoding]::UTF8.GetString($StringContent) }
-        { ($_ -eq "text/plain") } { ($_ -eq "text/plain; charset=UTF-8") } { $VersionText = $StringContent }
+        { ($_.StartsWith("text/plain")) } { $VersionText = $StringContent }
         default { throw "``$Response.Content.Headers.ContentType`` is an unknown .version file content type." }
     }
 
