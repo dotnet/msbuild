43da7acea1c8f75e12d1af1bfc771d85fd9e8035
mlorbetske <mike.lorbetske@n3-p.com>
2017-05-24 16:40:00 -0700

    Things work

--- src/dotnet-new3/Program.cs
+++ src/dotnet-new3/Program.cs
@@ -8,6 +8,9 @@ using System.Runtime.CompilerServices;
 using Microsoft.TemplateEngine.Abstractions;
 using Microsoft.TemplateEngine.Cli;
 using Microsoft.TemplateEngine.Edge;
+using Microsoft.TemplateEngine.Orchestrator.RunnableProjects;
+using Microsoft.TemplateEngine.Orchestrator.RunnableProjects.Config;
+using Microsoft.TemplateEngine.Orchestrator.RunnableProjects.Macros;
 using Microsoft.TemplateEngine.Utils;
 
 [assembly:InternalsVisibleTo("dotnet-new3.UnitTests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100f33a29044fa9d740c9b3213a93e57c84b472c84e0b8a0e1ae48e67a9f8f6de9d5f7f3d52ac23e48ac51801f1dc950abe901da34d2a9e3baadb141a17c77ef3c565dd5ee5054b91cf63bb3c6ab83f72ab3aafe93d0fc3c2348b764fafb0b1c0733de51459aeab46580384bf9d74c4e28164b7cde247f891ba07891c9d872ad2bb")]
@@ -44,7 +47,13 @@ namespace dotnet_new3
             catch
             { }
 
-            DefaultTemplateEngineHost host = new DefaultTemplateEngineHost(HostIdentifier, HostVersion, CultureInfo.CurrentCulture.Name, preferences, new[] { "dotnetcli" });
+            var builtIns = new AssemblyComponentCatalog(new[]
+            {
+                typeof(RunnableProjectGenerator).GetTypeInfo().Assembly,
+                typeof(ConditionalConfig).GetTypeInfo().Assembly
+            });
+
+            DefaultTemplateEngineHost host = new DefaultTemplateEngineHost(HostIdentifier, HostVersion, CultureInfo.CurrentCulture.Name, preferences, builtIns, new[] { "dotnetcli" });
 
             if (emitTimings)
             {
