f078aca50e817eb4b7ea8ecc3e16bb1fb950d347
William Li <wul@microsoft.com>
2017-06-01 14:19:53 -0700

    Add telemetry data points for .NET Core 2.0

---
+++ test/Microsoft.NET.Build.Tests/LogTelemetryToStdOutForTest.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using NuGet.Protocol;
+
+namespace Microsoft.NET.Build.Tests
+{
+    public sealed class LogTelemetryToStdOutForTest : Logger
+    {
+
+        public LogTelemetryToStdOutForTest()
+        {
+        }
+
+        public override void Initialize(IEventSource eventSource)
+        {
+            if (eventSource is IEventSource2 eventSource2)
+            {
+                eventSource2.TelemetryLogged += OnTelemetryLogged;
+            }
+        }
+
+        private void OnTelemetryLogged(object sender, TelemetryEventArgs args)
+        {
+            Console.WriteLine(args.ToJson());
+        }
+    }
+}
