c36975b71541a3c1648bc0394e727e0f23c7f460
Hao Kung <haok@microsoft.com>
2017-06-02 10:43:22 -0700

    PR tweaks

--- template_feed/Microsoft.DotNet.Web.ProjectTemplates.2.0/content/StarterWeb-CSharp/Extensions/AzureAdB2C/AzureAdB2CDefaults.cs
+++ template_feed/Microsoft.DotNet.Web.ProjectTemplates.2.0/content/StarterWeb-CSharp/Extensions/AzureAdB2C/AzureAdB2CDefaults.cs
@@ -2,8 +2,6 @@ namespace Microsoft.AspNetCore.Authentication.Extensions
 {
     public static class AzureAdB2CDefaults
     {
-        public const string AuthenticationScheme = "AzureAdB2C";
-        public const string BearerAuthenticationScheme = "AzureAdB2CBearer";
         public const string SignUpSignInAuthenticationScheme = "SignInSignUp";
         public const string ResetPasswordAuthenticationScheme = "ResetPassword";
         public const string EditProfileAuthenticationScheme = "EditProfile";
