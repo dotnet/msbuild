2eb671a6cc157ca68e918954d44892979cacba30
Mihai Codoban <micodoba@microsoft.com>
2017-06-05 11:11:10 -0700

    Replace RemoveAll with Remove (#2162)
    Solves #2069

--- src/Build/Evaluation/LazyItemEvaluator.RemoveOperation.cs
+++ src/Build/Evaluation/LazyItemEvaluator.RemoveOperation.cs
@@ -18,7 +18,7 @@ namespace Microsoft.Build.Evaluation
 
             protected override ICollection<I> SelectItems(ImmutableList<ItemData>.Builder listBuilder, ImmutableHashSet<string> globsToIgnore)
             {
-                return SelectItemsMatchingItemSpec(listBuilder, _itemElement.RemoveLocation).ToList();
+                return SelectItemsMatchingItemSpec(listBuilder, _itemElement.RemoveLocation).ToImmutableHashSet();
             }
 
             protected override void SaveItems(ICollection<I> items, ImmutableList<ItemData>.Builder listBuilder)
@@ -28,7 +28,28 @@ namespace Microsoft.Build.Evaluation
                     return;
                 }
 
-                listBuilder.RemoveAll(itemData => items.Contains(itemData.Item));
+                // bug in ImmutableList<T>.Builder.RemoveAll. In some cases it will remove elements for which the RemoveAll predicate is false
+                // MSBuild issue: https://github.com/Microsoft/msbuild/issues/2069
+                // corefx issue: https://github.com/dotnet/corefx/issues/20609
+                //listBuilder.RemoveAll(itemData => items.Contains(itemData.Item));
+
+                // Replacing RemoveAll with Remove fixes the above issue 
+
+                // DeLINQified for perf
+                //var itemDataToRemove = listBuilder.Where(itemData => items.Contains(itemData.Item)).ToList();
+                var itemDataToRemove = new List<ItemData>();
+                foreach (var itemData in listBuilder)
+                {
+                    if (items.Contains(itemData.Item))
+                    {
+                        itemDataToRemove.Add(itemData);
+                    }
+                }
+
+                foreach (var itemToRemove in itemDataToRemove)
+                {
+                    listBuilder.Remove(itemToRemove);
+                }
             }
 
             public ImmutableHashSet<string>.Builder GetRemovedGlobs()
