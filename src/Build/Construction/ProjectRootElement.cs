8d354522df0928c72cabf3291850af1f5f5560a8
Mihai Codoban <micodoba@microsoft.com>
2017-06-05 17:00:47 -0700

    Reloading root element from memory keeps initial file
    Closes #2159

--- src/Build/Construction/ProjectRootElement.cs
+++ src/Build/Construction/ProjectRootElement.cs
@@ -1703,6 +1703,8 @@ namespace Microsoft.Build.Construction
         /// If the new state has invalid XML or MSBuild syntax, then this method throws an <see cref="InvalidProjectFileException"/>.
         /// When this happens, the state of this object does not change.
         /// 
+        /// Reloading from an XMLReader will retain the previous root element location (<see cref="FullPath"/>, <see cref="DirectoryPath"/>, <see cref="ProjectFileLocation"/>).
+        /// 
         /// </summary>
         /// <param name="reader">Reader to read from</param>
         /// <param name="throwIfUnsavedChanges">
@@ -1714,7 +1716,15 @@ namespace Microsoft.Build.Construction
         /// </param>
         public void ReloadFrom(XmlReader reader, bool throwIfUnsavedChanges = true, bool? preserveFormatting = null)
         {
-            Func<bool, XmlDocumentWithLocation> documentProducer = shouldPreserveFormatting => LoadDocument(reader, shouldPreserveFormatting);
+            Func<bool, XmlDocumentWithLocation> documentProducer = shouldPreserveFormatting =>
+            {
+                var document =  LoadDocument(reader, shouldPreserveFormatting);
+
+                document.FullPath = this.FullPath;
+
+                return document;
+            };
+
             ReloadFrom(documentProducer, throwIfUnsavedChanges, preserveFormatting);
         }
 
