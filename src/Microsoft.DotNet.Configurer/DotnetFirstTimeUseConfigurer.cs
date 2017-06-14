34b44b999b73a47fc24335cf2afb7d1482a315fa
Omair Majid <omajid@redhat.com>
2017-06-14 12:09:06 -0400

    Avoid repeating the first-run message if lzma archive is missing
    If the LZMA archive is missing, the first-run message is printed every time.
    This commit fixes that.
    
    Split the first-run message into two pieces:
    
    - The first-run (welcome and telemetry) message is printed only once, no matter
      whether the cache was primed or not.
    
    - The cache-priming message is printed only if the cache is avaialble.
      Otherwise skip the cache introduction and the ccache priming operation.

--- src/Microsoft.DotNet.Configurer/DotnetFirstTimeUseConfigurer.cs
+++ src/Microsoft.DotNet.Configurer/DotnetFirstTimeUseConfigurer.cs
@@ -9,39 +9,74 @@ namespace Microsoft.DotNet.Configurer
 {
     public class DotnetFirstTimeUseConfigurer
     {
+        private IReporter _reporter;
         private IEnvironmentProvider _environmentProvider;
         private INuGetCachePrimer _nugetCachePrimer;
         private INuGetCacheSentinel _nugetCacheSentinel;
+        private IFirstTimeUseNoticeSentinel _firstTimeUseNoticeSentinel;
 
         public DotnetFirstTimeUseConfigurer(
             INuGetCachePrimer nugetCachePrimer,
             INuGetCacheSentinel nugetCacheSentinel,
-            IEnvironmentProvider environmentProvider)
+            IFirstTimeUseNoticeSentinel firstTimeUseNoticeSentinel,
+            IEnvironmentProvider environmentProvider,
+            IReporter reporter)
         {
             _nugetCachePrimer = nugetCachePrimer;
             _nugetCacheSentinel = nugetCacheSentinel;
+            _firstTimeUseNoticeSentinel = firstTimeUseNoticeSentinel;
             _environmentProvider = environmentProvider;
+            _reporter = reporter;
         }
 
         public void Configure()
         {
-            if(ShouldPrimeNugetCache())
+            if (ShouldPrintFirstTimeUseNotice())
             {
                 PrintFirstTimeUseNotice();
+            }
 
+            if (ShouldPrimeNugetCache())
+            {
+                PrintNugetCachePrimeMessage();
                 _nugetCachePrimer.PrimeCache();
             }
+        }
+
+        private bool ShouldPrintFirstTimeUseNotice()
+        {
+            var showFirstTimeUseNotice =
+                _environmentProvider.GetEnvironmentVariableAsBool("DOTNET_PRINT_TELEMETRY_MESSAGE", true);
+
+            return ShouldRunFirstRunExperience() &&
+                showFirstTimeUseNotice &&
+                !_firstTimeUseNoticeSentinel.Exists();
         }
 
         private void PrintFirstTimeUseNotice()
         {
             string firstTimeUseWelcomeMessage = LocalizableStrings.FirstTimeWelcomeMessage;
 
-            Reporter.Output.WriteLine();
-            Reporter.Output.WriteLine(firstTimeUseWelcomeMessage);
+            _reporter.WriteLine();
+            _reporter.WriteLine(firstTimeUseWelcomeMessage);
+
+            _firstTimeUseNoticeSentinel.CreateIfNotExists();
         }
 
         private bool ShouldPrimeNugetCache()
+        {
+            return ShouldRunFirstRunExperience() &&
+                !_nugetCachePrimer.SkipPrimingTheCache();
+        }
+
+        private void PrintNugetCachePrimeMessage()
+        {
+            string cachePrimeMessage = LocalizableStrings.NugetCachePrimeMessage;
+            _reporter.WriteLine();
+            _reporter.WriteLine(cachePrimeMessage);
+        }
+
+        private bool ShouldRunFirstRunExperience()
         {
             var skipFirstTimeExperience = 
                 _environmentProvider.GetEnvironmentVariableAsBool("DOTNET_SKIP_FIRST_TIME_EXPERIENCE", false);
