2734f6a2764d280d622091181550ba1707ab3235
Vijay Ramakrishnan <vramak@microsoft.com>
2017-06-13 22:15:47 -0700

    Adding Support for MSdeploy Package deploy script

---
+++ src/Publish/Microsoft.NET.Sdk.Publish.Tasks/Tasks/MsDeploy/CreateMSDeployScript.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using Microsoft.NET.Sdk.Publish.Tasks.Properties;
+
+namespace Microsoft.NET.Sdk.Publish.Tasks.MsDeploy
+{
+    public class CreateMSDeployScript : Task
+    {
+        [Required]
+        public string ProjectName { get; set; }
+
+        [Required]
+        public string ScriptFullPath { get; set; }
+
+        [Required]
+        public string ReadMeFullPath { get; set; }
+
+        public override bool Execute()
+        {
+            if (!File.Exists(ScriptFullPath))
+            {
+                File.Create(ScriptFullPath);
+            }
+
+            File.WriteAllLines(ScriptFullPath, GetReplacedFileContents(Resources.MsDeployBatchFile));
+
+            if (!File.Exists(ReadMeFullPath))
+            {
+                File.Create(ReadMeFullPath);
+            }
+
+            File.WriteAllLines(ReadMeFullPath, GetReplacedFileContents(Resources.MsDeployReadMe));
+
+            return true;
+        }
+
+        private string[] GetReplacedFileContents(string fileContents)
+        {
+            var lines = fileContents.Split(new string[] { Environment.NewLine, "\n" }, StringSplitOptions.None);
+            for (int i = 0; i < lines.Length; i++)
+            {
+                lines[i] = lines[i]?.Replace("$$ProjectName$$", ProjectName);
+            }
+
+            return lines;
+        }
+    }
+}
