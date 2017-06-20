dotnet/msbuild

commit 51e8cdc86cb2dec580b5010e2d201a282c14633c

Ignore missing imports (#2171)
New ProjectLoadSetting to control the feature

New code only runs after the project fails to load and the file existence is checked which should not impact performance.

Update unit test for corrected error code
Use Traits.Instance.EscapeHatches in Evaluator

Closes #1727

src/Build/Evaluation/Evaluator.cs
@@ -67,16 +67,6 @@ namespace Microsoft.Build.Evaluation
         private static readonly char[] s_splitter = new char[] { ';' };
 
         /// <summary>
-        /// Whether to write information about why we evaluate to debug output.
-        /// </summary>
-        private static readonly bool s_debugEvaluation = (Environment.GetEnvironmentVariable("MSBUILDDEBUGEVALUATION") != null);
-
-        /// <summary>
-        /// Whether to to respect the TreatAsLocalProperty parameter on the Project tag. 
-        /// </summary>
-        private static readonly bool s_ignoreTreatAsLocalProperty = (Environment.GetEnvironmentVariable("MSBUILDIGNORETREATASLOCALPROPERTY") != null);
-
-        /// <summary>
         /// Locals types names. We only have these because 'Built In' has a space,
         /// else we would use LocalsTypes enum names.
         /// Note: This should match LocalsTypes enum.
@@ -266,7 +256,7 @@ namespace Microsoft.Build.Evaluation
             _expander = new Expander<P, I>(data, data);
 
             // This setting may change after the build has started, therefore if the user has not set the property to true on the build parameters we need to check to see if it is set to true on the environment variable.
-            _expander.WarnForUninitializedProperties = BuildParameters.WarnOnUninitializedProperty || !String.IsNullOrEmpty(Environment.GetEnvironmentVariable("MSBUILDWARNONUNINITIALIZEDPROPERTY"));
+            _expander.WarnForUninitializedProperties = BuildParameters.WarnOnUninitializedProperty || Traits.Instance.EscapeHatches.WarnOnUninitializedProperty;
             _data = data;
             _itemGroupElements = new List<ProjectItemGroupElement>();
             _itemDefinitionGroupElements = new List<ProjectItemDefinitionGroupElement>();
@@ -362,14 +352,6 @@ namespace Microsoft.Build.Evaluation
             /// Items
             /// </summary>
             Items
-        }
-
-        /// <summary>
-        /// Whether to write information about why we evaluate to debug output.
-        /// </summary>
-        internal static bool DebugEvaluation
-        {
-            get { return s_debugEvaluation; }
         }
 
         /// <summary>
@@ -921,7 +903,7 @@ namespace Microsoft.Build.Evaluation
             _data.BeforeTargets = targetsWhichRunBeforeByTarget;
             _data.AfterTargets = targetsWhichRunAfterByTarget;
 
-            if (s_debugEvaluation)
+            if (Traits.Instance.EscapeHatches.DebugEvaluation)
             {
                 // This is so important for VS performance it's worth always tracing; accidentally having 
                 // inconsistent sets of global properties will cause reevaluations, which are wasteful and incorrect
@@ -973,7 +955,7 @@ namespace Microsoft.Build.Evaluation
             IList<string> initialTargets = _expander.ExpandIntoStringListLeaveEscaped(currentProjectOrImport.InitialTargets, ExpanderOptions.ExpandProperties, currentProjectOrImport.InitialTargetsLocation);
             _initialTargetsList.AddRange(initialTargets);
 
-            if (!s_ignoreTreatAsLocalProperty)
+            if (!Traits.Instance.EscapeHatches.IgnoreTreatAsLocalProperty)
             {
                 IList<string> globalPropertiesToTreatAsLocals = _expander.ExpandIntoStringListLeaveEscaped(currentProjectOrImport.TreatAsLocalProperty, ExpanderOptions.ExpandProperties, currentProjectOrImport.TreatAsLocalPropertyLocation);
 
@@ -2637,7 +2619,7 @@ namespace Microsoft.Build.Evaluation
                             }
                         }
                     }
-                    catch (InvalidProjectFileException ex) when (ExceptionHandling.IsIoRelatedException(ex.InnerException))
+                    catch (InvalidProjectFileException ex)
                     {
                         // The import couldn't be read from disk, or something similar. In that case,
                         // the error message would be more useful if it pointed to the location in the importing project file instead.
@@ -2657,6 +2639,31 @@ namespace Microsoft.Build.Evaluation
                         }
                         else
                         {
+                            // If IgnoreEmptyImports is enabled, check if the file is considered empty
+                            //
+                            if (((_loadSettings & ProjectLoadSettings.IgnoreEmptyImports) != 0 || Traits.Instance.EscapeHatches.IgnoreEmptyImports) && ProjectRootElement.IsEmptyXmlFile(importFileUnescaped))
+                            {
+                                atleastOneImportIgnored = true;
+
+                                ProjectImportedEventArgs eventArgs = new ProjectImportedEventArgs(
+                                    importElement.Location.Line,
+                                    importElement.Location.Column,
+                                    ResourceUtilities.GetResourceString("ProjectImportSkippedEmptyFile"),
+                                    importFileUnescaped,
+                                    importElement.ContainingProject.FullPath,
+                                    importElement.Location.Line,
+                                    importElement.Location.Column)
+                                {
+                                    BuildEventContext = _evaluationLoggingContext.BuildEventContext,
+                                    UnexpandedProject = importElement.Project,
+                                    ProjectFile = importElement.ContainingProject.FullPath
+                                };
+
+                                _evaluationLoggingContext.LogBuildEvent(eventArgs);
+
+                                continue;
+                            }
+
                             // Otherwise a more generic message, still pointing to the location of the import tag
                             ProjectErrorUtilities.ThrowInvalidProject(importLocationInProject, "InvalidImportedProjectFile",
                                 importFileUnescaped, ex.InnerException.Message);

ref/net46/Microsoft.Build/Microsoft.Build.cs
@@ -759,6 +759,7 @@ namespace Microsoft.Build.Evaluation
     public enum ProjectLoadSettings
     {
         Default = 0,
+        IgnoreEmptyImports = 16,
         IgnoreMissingImports = 1,
         RecordDuplicateButNotCircularImports = 2,
         RecordEvaluatedItemElements = 8,

ref/netstandard1.3/Microsoft.Build/Microsoft.Build.cs
@@ -747,6 +747,7 @@ namespace Microsoft.Build.Evaluation
     public enum ProjectLoadSettings
     {
         Default = 0,
+        IgnoreEmptyImports = 16,
         IgnoreMissingImports = 1,
         RecordDuplicateButNotCircularImports = 2,
         RecordEvaluatedItemElements = 8,

src/Build.OM.UnitTests/Definition/Project_Tests.cs
@@ -3923,6 +3923,51 @@ namespace Microsoft.Build.UnitTests.OM.Definition
         }
 
         [Fact]
+        public void ProjectImportedEventEmptyFile()
+        {
+            const string contents = @"<?xml version=""1.0"" encoding=""utf-8""?>
+";
+            string importPath = ObjectModelHelpers.CreateFileInTempProjectDirectory(Guid.NewGuid().ToString("N"), contents, Encoding.UTF8);
+            ProjectRootElement pre = ProjectRootElement.Create(FileUtilities.GetTemporaryFile());
+
+            try
+            {
+                pre.AddPropertyGroup().AddProperty("NotUsed", "");
+                var import = pre.AddImport(importPath);
+
+                pre.Save();
+                pre.Reload();
+
+                using (ProjectCollection collection = new ProjectCollection())
+                {
+                    MockLogger logger = new MockLogger();
+                    collection.RegisterLogger(logger);
+
+                    Project unused = new Project(pre, null, null, collection, ProjectLoadSettings.IgnoreEmptyImports);
+
+                    ProjectImportedEventArgs eventArgs = logger.AllBuildEvents.SingleOrDefault(i => i is ProjectImportedEventArgs) as ProjectImportedEventArgs;
+
+                    Assert.NotNull(eventArgs);
+
+                    Assert.Equal(import.Project, eventArgs.UnexpandedProject);
+
+                    Assert.Null(eventArgs.ImportedProjectFile);
+
+                    Assert.Equal(pre.FullPath, eventArgs.ProjectFile);
+
+                    Assert.Equal(6, eventArgs.LineNumber);
+                    Assert.Equal(3, eventArgs.ColumnNumber);
+
+                    logger.AssertLogContains($"Project \"{import.Project}\" was not imported by \"{pre.FullPath}\" at ({eventArgs.LineNumber},{eventArgs.ColumnNumber}), due to the file being empty.");
+                }
+            }
+            finally
+            {
+                ObjectModelHelpers.DeleteTempProjectDirectory();
+            }
+        }
+
+        [Fact]
         public void ProjectImportEvent()
         {
             ProjectRootElement pre1 = ProjectRootElement.Create(FileUtilities.GetTemporaryFile());

src/Build.UnitTests/Construction/ProjectRootElement_Tests.cs
@@ -1,0 +1,56 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+
+using System;
+using System.IO;
+using System.Text;
+using Microsoft.Build.Construction;
+using Xunit;
+
+namespace Microsoft.Build.UnitTests.Construction
+{
+    /// <summary>
+    /// Tests for the ElementLocation class
+    /// </summary>
+    public class ProjectRootElement_Tests
+    {
+        [Theory]
+        [InlineData("", true)]
+        [InlineData("", false)]
+        [InlineData(@"<?xml version=""1.0"" encoding=""utf-8""?>", true)]
+        [InlineData(@"<?xml version=""1.0"" encoding=""utf-8""?>", false)]
+        [InlineData(@"<?xml version=""1.0"" encoding=""utf-8""?>
+", true)]
+        [InlineData(@"<?xml version=""1.0"" encoding=""utf-8""?>
+", false)]
+        public void IsEmptyXmlFileReturnsTrue(string contents, bool useByteOrderMark)
+        {
+            string path = useByteOrderMark ?
+                ObjectModelHelpers.CreateFileInTempProjectDirectory(Guid.NewGuid().ToString("N"), contents, Encoding.UTF8) :
+                ObjectModelHelpers.CreateFileInTempProjectDirectory(Guid.NewGuid().ToString("N"), contents);
+
+            Assert.True(ProjectRootElement.IsEmptyXmlFile(path));
+        }
+
+        [Theory]
+        [InlineData("<Foo/>", true)]
+        [InlineData("Foo/>", false)]
+        [InlineData(@"<?xml version=""1.0"" encoding=""utf-8""?>
+<Foo/>", true)]
+        [InlineData(@"<?xml version=""1.0"" encoding=""utf-8""?>
+<Foo/>", false)]
+        [InlineData(@"<?xml version=""1.0"" encoding=""utf-8""?>
+bar", true)]
+        [InlineData(@"<?xml version=""1.0"" encoding=""utf-8""?>
+bar", false)]
+        public void IsEmptyXmlFileReturnsFalse(string contents, bool useByteOrderMark)
+        {
+            string path = useByteOrderMark ?
+                ObjectModelHelpers.CreateFileInTempProjectDirectory(Guid.NewGuid().ToString("N"), contents, Encoding.UTF8) :
+                ObjectModelHelpers.CreateFileInTempProjectDirectory(Guid.NewGuid().ToString("N"), contents);
+
+            Assert.False(ProjectRootElement.IsEmptyXmlFile(path));
+        }
+    }
+}

src/Build.UnitTests/Evaluation/ImportFromMSBuildExtensionsPath_Tests.cs
@@ -267,7 +267,7 @@ namespace Microsoft.Build.UnitTests.Evaluation
                 projColln.RegisterLogger(logger);
 
                 Assert.Throws<InvalidProjectFileException>(() => projColln.LoadProject(mainProjectPath));
-                logger.AssertLogContains("MSB4025");
+                logger.AssertLogContains("MSB4024");
             } finally {
                 if (mainProjectPath != null)
                 {

src/Build.UnitTests/Microsoft.Build.Engine.UnitTests.csproj
@@ -129,6 +129,7 @@
     <Compile Include="BackEnd\TaskHostTaskCancelled_Tests.cs" />
     <Compile Include="BackEnd\TaskHostTaskComplete_Tests.cs" />
     <Compile Include="BackEnd\TaskHost_Tests.cs" />
+    <Compile Include="Construction\ProjectRootElement_Tests.cs" />
     <Compile Include="Evaluation\EvaluationLogging_Tests.cs" />
     <Compile Include="MockLoggingContext.cs" />
     <Compile Include="TestComparers\TaskRegistryComparers.cs" />

src/Build/Construction/ProjectRootElement.cs
@@ -13,6 +13,7 @@ using System.IO;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading;
 using System.Xml;
 using Microsoft.Build.BackEnd.Logging;
@@ -63,6 +64,11 @@ namespace Microsoft.Build.Construction
         private static readonly ProjectRootElementCache.OpenProjectRootElement s_openLoaderDelegate = OpenLoader;
 
         private static readonly ProjectRootElementCache.OpenProjectRootElement s_openLoaderPreserveFormattingDelegate = OpenLoaderPreserveFormatting;
+
+        /// <summary>
+        /// Used to determine if a file is an empty XML file if it ONLY contains an XML declaration like &lt;?xml version="1.0" encoding="utf-8"?&gt;.
+        /// </summary>
+        private static readonly Lazy<Regex> XmlDeclarationRegEx = new Lazy<Regex>(() => new Regex(@"\A\s*\<\?\s*xml.*\?\>\s*\Z"), isThreadSafe: true);
 
         /// <summary>
         /// The default encoding to use / assume for a new project.
@@ -1936,6 +1942,62 @@ namespace Microsoft.Build.Construction
 
                 yield return sdkReference;
             }
+        }
+
+        /// <summary>
+        /// Determines if the specified file is an empty XML file meaning it has no contents, contains only whitespace, or
+        /// only an XML declaration.  If the file does not exist, it is not considered empty.
+        /// </summary>
+        /// <param name="path">The full path to a file to check.</param>
+        /// <returns><code>true</code> if the file is an empty XML file, otherwise <code>false</code>.</returns>
+        internal static bool IsEmptyXmlFile(string path)
+        {
+            // The maximum number of characters of the file to read to check if its empty or not.  Ideally we
+            // would only look at zero-length files but empty XML files can contain just an xml declaration:
+            //
+            //   <? xml version="1.0" encoding="utf-8" standalone="yes" ?>
+            //
+            // And this should also be treated as if the file is empty.
+            //
+            const int maxSizeToConsiderEmpty = 100;
+
+            if (!File.Exists(path))
+            {
+                // Non-existent files are not treated as empty
+                //
+                return false;
+            }
+
+            try
+            {
+                FileInfo fileInfo = new FileInfo(path);
+
+                if (fileInfo.Length == 0)
+                {
+                    // Zero length files are empty
+                    //
+                    return true;
+                }
+
+                if (fileInfo.Length > maxSizeToConsiderEmpty)
+                {
+                    // Files greater than the maximum bytes to check are not empty
+                    //
+                    return false;
+                }
+
+                string contents = File.ReadAllText(path);
+
+                // If the file is only whitespace or the XML declaration then it empty
+                //
+                return String.IsNullOrEmpty(contents) || XmlDeclarationRegEx.Value.IsMatch(contents);
+            }
+            catch (Exception)
+            {
+                // ignored
+            }
+
+            return false;
         }
 
         /// <summary>

commit b0ef7e90f2f83dbd859b488a947cc3e448d6e12a

Fix NullReferenceException in evaluator
Caused by 51e8cdc86cb2dec580b5010e2d201a282c14633c which overly caught exceptions that didn't have an InnerException.

Fixes #2228

src/Build/Evaluation/Evaluator.cs
@@ -2664,9 +2664,16 @@ namespace Microsoft.Build.Evaluation
                                 continue;
                             }
 
-                            // Otherwise a more generic message, still pointing to the location of the import tag
-                            ProjectErrorUtilities.ThrowInvalidProject(importLocationInProject, "InvalidImportedProjectFile",
-                                importFileUnescaped, ex.InnerException.Message);
+                            // If this exception is a wrapped exception (like IOException or XmlException) then wrap it as an invalid import instead
+                            if (ex.InnerException != null)
+                            {
+                                // Otherwise a more generic message, still pointing to the location of the import tag
+                                ProjectErrorUtilities.ThrowInvalidProject(importLocationInProject, "InvalidImportedProjectFile",
+                                    importFileUnescaped, ex.InnerException.Message);
+                            }
+
+                            // Throw the original InvalidProjectFileException because it has no InnerException and was not wrapping something else
+                            throw;
                         }
                     }
 

src/Build.UnitTests/Evaluation/Evaluator_Tests.cs
@@ -4328,6 +4328,63 @@ namespace Microsoft.Build.UnitTests.Evaluation
                 new BuildEventContext(1, 2, 3, 4)));
         }
 
+        /// <summary>
+        /// Test regression reported at https://github.com/Microsoft/msbuild/issues/2228
+        /// </summary>
+        [Fact]
+        public void ThrownInvalidProjectExceptionProperlyHandled()
+        {
+            string projectContents = ObjectModelHelpers.CleanupFileContents(@"
+                             <Project ToolsVersion=""msbuilddefaulttoolsversion"" xmlns='msbuildnamespace'>
+                                
+                                <Import Project=""import.proj"" />
+
+                            </Project>");
+
+            string importContents = ObjectModelHelpers.CleanupFileContents(@"
+                             <Project ToolsVersion=""msbuilddefaulttoolsversion"" xmlns='msbuildnamespace'>
+
+                                <AnInvalidTopLevelElement />
+
+                              </Project>");
+
+            string projectDirectory = Path.Combine(Path.GetTempPath(), "VerifyTreatAsLocalPropertyUnionBetweenImports");
+
+            try
+            {
+                if (Directory.Exists(projectDirectory))
+                {
+                    FileUtilities.DeleteWithoutTrailingBackslash(projectDirectory, true /* recursive delete */);
+                }
+
+                Directory.CreateDirectory(projectDirectory);
+
+                string primaryProject = Path.Combine(projectDirectory, "project.proj");
+                string import = Path.Combine(projectDirectory, "import.proj");
+
+                File.WriteAllText(primaryProject, projectContents);
+                File.WriteAllText(import, importContents);
+
+                InvalidProjectFileException ex = Assert.Throws<InvalidProjectFileException>( () =>
+                    {
+                        Project unused = new Project(primaryProject, null, null);
+                    })
+                ;
+
+                Assert.Equal($"The element <AnInvalidTopLevelElement> beneath element <Project> is unrecognized.  {import}", ex.Message);
+                Assert.Equal("MSB4067", ex.ErrorCode);
+                Assert.Equal(4, ex.LineNumber);
+                Assert.Equal(33, ex.ColumnNumber);
+            }
+            finally
+            {
+                if (Directory.Exists(projectDirectory))
+                {
+                    FileUtilities.DeleteWithoutTrailingBackslash(projectDirectory, true /* recursive delete */);
+                }
+            }
+        }
+
 #if FEATURE_HTTP_LISTENER
         /// <summary>
         /// HTTP server code running on a separate thread that expects a connection request