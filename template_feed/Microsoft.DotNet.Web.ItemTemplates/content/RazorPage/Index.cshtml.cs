0be0aabd223fe25ab1db44c9d6bee6310035252f
Mike Lorbetske <mike.lorbetske@n3-p.com>
2017-07-03 11:55:46 -0700

    Fix issue #1010

--- template_feed/Microsoft.DotNet.Web.ItemTemplates/content/RazorPage/Index.cshtml.cs
+++ template_feed/Microsoft.DotNet.Web.ItemTemplates/content/RazorPage/Index.cshtml.cs
@@ -7,7 +7,11 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace MyApp.Namespace
 {
+#if NameIsPage
+    public class IndexModel : Microsoft.AspNetCore.Mvc.RazorPages.PageModel
+#else
     public class IndexModel : PageModel
+#endif
     {
         public void OnGet()
         {
