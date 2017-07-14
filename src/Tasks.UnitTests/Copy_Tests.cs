392ad0a74945c468d8b75515b95cbcec979cc8da
Kirill Osenkov <github@osenkov.com>
2017-07-14 12:28:23 -0700

    Fix unit-tests. MockEngine was caching the text of the log the first time it saw it, so subsequent calls to AssertLogContains were incorrect. Removing _upperLog.
    This uncovered the two tests that were supposed to be failing, but are
    passing due to the incorrect log being cached. Filed issue #2305 to fix
    these tests.
    
    Thread through the ITestOutputHelper so that the failing test can output
    the full contents of the log in case of failure. ITestOutputHelper gets
    injected into the constructor by xUnit.

--- src/Tasks.UnitTests/Copy_Tests.cs
+++ src/Tasks.UnitTests/Copy_Tests.cs
@@ -15,14 +15,15 @@ using Microsoft.Build.Utilities;
 
 using System.Diagnostics;
 using System.Runtime.InteropServices;
+using System.Security.Principal;
 using System.Text;
 using Xunit;
+using Xunit.Abstractions;
 using PlatformID = Xunit.PlatformID;
-using System.Security.Principal;
 
 namespace Microsoft.Build.UnitTests
 {
-    public abstract class Copy_Tests : IDisposable
+    public class Copy_Tests : IDisposable
     {
         public bool useHardLinks = false;
 
@@ -41,12 +42,15 @@ namespace Microsoft.Build.UnitTests
         /// </summary>
         private string _alwaysRetry = null;
 
+        private readonly ITestOutputHelper _testOutputHelper;
+
         /// <summary>
         /// There are a couple of environment variables that can affect the operation of the Copy
         /// task.  Make sure none of them are set. 
         /// </summary>
-        public Copy_Tests()
+        public Copy_Tests(ITestOutputHelper testOutputHelper)
         {
+            _testOutputHelper = testOutputHelper;
             _alwaysOverwriteReadOnlyFiles = Environment.GetEnvironmentVariable("MSBUILDALWAYSOVERWRITEREADONLYFILES");
             _alwaysRetry = Environment.GetEnvironmentVariable("MSBUILDALWAYSRETRY");
 
@@ -694,7 +698,7 @@ namespace Microsoft.Build.UnitTests
                     {
                         t.UseHardlinksIfPossible = useHardLinks;
                     }
-                    MockEngine engine = new MockEngine();
+                    MockEngine engine = new MockEngine(_testOutputHelper);
                     t.BuildEngine = engine;
                     t.SourceFiles = sourceFiles;
                     t.DestinationFiles = new TaskItem[] { new TaskItem(destinationFile) };
@@ -1886,7 +1890,8 @@ namespace Microsoft.Build.UnitTests
 
     public class CopyNotHardLink_Tests : Copy_Tests
     {
-        public CopyNotHardLink_Tests()
+        public CopyNotHardLink_Tests(ITestOutputHelper testOutputHelper)
+            : base(testOutputHelper)
         {
             this.useHardLinks = false;
         }
@@ -1934,7 +1939,8 @@ namespace Microsoft.Build.UnitTests
 
     public class CopyHardLink_Tests : Copy_Tests
     {
-        public CopyHardLink_Tests()
+        public CopyHardLink_Tests(ITestOutputHelper testOutputHelper)
+            : base(testOutputHelper)
         {
             this.useHardLinks = true;
         }
@@ -2213,7 +2219,8 @@ namespace Microsoft.Build.UnitTests
 
     public class CopySymbolicLink_Tests : Copy_Tests
     {
-        public CopySymbolicLink_Tests()
+        public CopySymbolicLink_Tests(ITestOutputHelper testOutputHelper)
+            : base(testOutputHelper)
         {
             useSymbolicLinks = true;
         }
