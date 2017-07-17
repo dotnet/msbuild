371d25657ed8ff8ca5908de8a221c14eac240fc0
Ankit Jain <ankit.jain@xamarin.com>
2017-07-17 12:42:10 -0400

    [mono] install-mono-prefix.sh: Update for non-existant ..
    .. `Roslyn/System.Runtime.InteropServices.RuntimeInformation.dll` .

--- install-mono-prefix.sh
+++ install-mono-prefix.sh
@@ -58,7 +58,6 @@ rm ${DESTDIR}${MSBUILD_INSTALL_BIN_DIR}/*UnitTests*
 rm ${DESTDIR}${MSBUILD_INSTALL_BIN_DIR}/*xunit*
 rm ${DESTDIR}${MSBUILD_INSTALL_BIN_DIR}/NuGet*
 rm ${DESTDIR}${MSBUILD_INSTALL_BIN_DIR}/System.Runtime.InteropServices.RuntimeInformation.dll
-rm ${DESTDIR}${MSBUILD_INSTALL_BIN_DIR}/Roslyn/System.Runtime.InteropServices.RuntimeInformation.dll
 rm ${DESTDIR}${MSBUILD_INSTALL_BIN_DIR}/Roslyn/csc.exe*
 
 FILES="\
