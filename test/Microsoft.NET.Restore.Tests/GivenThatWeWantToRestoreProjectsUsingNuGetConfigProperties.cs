2a7fdf2a52acee24f4d215a2a68d3ada49123679
Livar Cunha <livar@live.com>
2017-07-24 15:26:29 -0700

    Changing the condition to not use fallback folder to trigger only on netcoreapp.

--- test/Microsoft.NET.Restore.Tests/GivenThatWeWantToRestoreProjectsUsingNuGetConfigProperties.cs
+++ test/Microsoft.NET.Restore.Tests/GivenThatWeWantToRestoreProjectsUsingNuGetConfigProperties.cs
@@ -24,7 +24,7 @@ namespace Microsoft.NET.Restore.Tests
 
         // https://github.com/dotnet/sdk/issues/1327
         [CoreMSBuildOnlyTheory]
-        [InlineData("netstandard1.3", "1.3", true)]
+        [InlineData("netstandard1.3", "1.3", false)]
         [InlineData("netcoreapp1.0", "1.0", true)]
         [InlineData("netcoreapp1.1", "1.1", true)]
         [InlineData("netstandard2.0", "2.0", false)]
@@ -33,6 +33,7 @@ namespace Microsoft.NET.Restore.Tests
         // https://github.com/NuGet/Home/issues/5596 and https://github.com/dotnet/project-system/issues/2605
         //[InlineData("netcoreapp2.0;net461", "multiTFM20app", false)]
         //[InlineData("netcoreapp1.0;netcoreapp2.0", "multiTFM1020app", true)]
+        //[InlineData("netcoreapp1.0;net461", "multiTFM1046app", true)]
         public void I_can_restore_a_project_with_implicit_msbuild_nuget_config(
             string frameworks,
             string projectPrefix,
