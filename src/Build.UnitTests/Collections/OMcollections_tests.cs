dotnet/msbuild

commit 6881b077c1635f1f10dbc243c774c5b4724581ab

Avoid allocations checking for global properties
When considering what to do about a property element, we avoid computing
values for global properties, since they can't be changed. That involves
querying whether the given property name is in the set of global
property names.

Prior to this commit, that code constructed a (new, used once)
`ReadOnlyConvertingCollection`, which involved allocating a
`Microsoft.Build.Collections.Contains` delegate. A trace observed that
52 MB of delegates were allocated in a NuGet scenario. None of that
allocation is necessary.

Fixes #2266 by changing `PropertyDictionary.ContainsKey()` to pass
through to its backing `RetrievableEntryHashSet` instead of going
through an intermediate `ReadOnlyConvertingCollection`, so no allocation
is required to check for the existence of a property.

There were no other callers of `RetrievableEntryHashSet.Keys`, so I
disabled it.

src/Build/Collections/RetrievableEntryHashSet/HashSet.cs
@@ -229,7 +229,7 @@ namespace Microsoft.Build.Collections
         {
             get
             {
-                return new ReadOnlyConvertingCollection<T, string>(this, delegate (T input) { return input.Key; }, delegate (string key) { return Contains(key); });
+                throw new NotImplementedException();
             }
         }
 

src/Build.UnitTests/Definition/Toolset_Tests.cs
@@ -3,7 +3,7 @@
 
 using System;
 using System.Collections.Generic;
-
+using System.Linq;
 using Microsoft.Build.BackEnd;
 using Microsoft.Build.Collections;
 using Microsoft.Build.Evaluation;
@@ -128,7 +128,7 @@ namespace Microsoft.Build.UnitTests.Definition
             Assert.Equal(t.OverrideTasksPath, t2.OverrideTasksPath);
             Assert.Equal(t.Properties.Count, t2.Properties.Count);
 
-            foreach (string key in t.Properties.Keys)
+            foreach (string key in t.Properties.Values.Select(p => p.Name))
             {
                 Assert.Equal(t.Properties[key].Name, t2.Properties[key].Name);
                 Assert.Equal(t.Properties[key].EvaluatedValue, t2.Properties[key].EvaluatedValue);
@@ -146,7 +146,7 @@ namespace Microsoft.Build.UnitTests.Definition
                     Assert.Equal(subToolset1.SubToolsetVersion, subToolset2.SubToolsetVersion);
                     Assert.Equal(subToolset1.Properties.Count, subToolset2.Properties.Count);
 
-                    foreach (string subToolsetPropertyKey in subToolset1.Properties.Keys)
+                    foreach (string subToolsetPropertyKey in subToolset1.Properties.Values.Select(p => p.Name))
                     {
                         Assert.Equal(subToolset1.Properties[subToolsetPropertyKey].Name, subToolset2.Properties[subToolsetPropertyKey].Name);
                         Assert.Equal(subToolset1.Properties[subToolsetPropertyKey].EvaluatedValue, subToolset2.Properties[subToolsetPropertyKey].EvaluatedValue);

src/Build/Collections/PropertyDictionary.cs
@@ -101,7 +101,10 @@ namespace Microsoft.Build.Collections
         {
             get
             {
-                return PropertyNames;
+                ErrorUtilities.ThrowInternalError("Keys is not supported on PropertyDictionary.");
+
+                // Show the compiler that this always throws:
+                throw new NotImplementedException();
             }
         }
 
@@ -157,21 +160,6 @@ namespace Microsoft.Build.Collections
                 lock (_properties)
                 {
                     return _properties.Count;
-                }
-            }
-        }
-
-        /// <summary>
-        /// Retrieves a collection containing the names of all the properties present in the dictionary.
-        /// </summary>
-        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
-        internal ICollection<string> PropertyNames
-        {
-            get
-            {
-                lock (_properties)
-                {
-                    return _properties.Keys;
                 }
             }
         }
@@ -370,7 +358,10 @@ namespace Microsoft.Build.Collections
         /// </summary>
         bool IDictionary<string, T>.ContainsKey(string key)
         {
-            return PropertyNames.Contains(key);
+            lock (_properties)
+            {
+                return _properties.ContainsKey(key);
+            }
         }
 
         /// <summary>

commit b7d89deb313d5dde6f1c7cb33e063a593623ec47

ReadOnlyConvertingCollection from RetrievableEntryHashSet.Keys
Removing this functionality was observable from outside MSBuild because
fields backed by a RetrievableEntryHashSet are exposed in the public
API. Specifically, 6881b07 caused a regression in TypeScript by throwing
when CPS tried to enumerate target names using `Project.Targets.Keys`.

This reverts commit 3c40d5b0a58bb2f5fca735373435839d54c8e5bc.

src/Build/Collections/ReadOnlyConvertingCollection.cs
@@ -1,0 +1,161 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+//-----------------------------------------------------------------------
+// </copyright>
+// <summary>A read-only collection wrapper which converts values as they are accessed.</summary>
+//-----------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Build.Shared;
+using Microsoft.Build.Collections;
+
+namespace Microsoft.Build.Collections
+{
+    /// <summary>
+    /// A function that can be passed in so that the converting
+    /// collection can do a "contains" operation on the backing
+    /// collection, using an object of the "converted to" type.
+    /// </summary>
+    /// <typeparam name="N">Type converted to</typeparam>
+    /// <returns>Whether the item is present</returns>
+    internal delegate bool Contains<N>(N item);
+
+    /// <summary>
+    /// Implementation of ICollection which converts values to the specified type when they are accessed.
+    /// </summary>
+    /// <typeparam name="V">The backing collection's value type.</typeparam>
+    /// <typeparam name="N">The desired value type.</typeparam>
+    internal class ReadOnlyConvertingCollection<V, N> : ICollection<N>
+    {
+        /// <summary>
+        /// The backing collection.
+        /// </summary>
+        private readonly ICollection<V> _backing;
+
+        /// <summary>
+        /// The delegate used to convert values.
+        /// </summary>
+        private readonly Func<V, N> _converter;
+
+        /// <summary>
+        /// The delegate used to satisfy contains operations, optionally
+        /// </summary>
+        private readonly Contains<N> _contains;
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        internal ReadOnlyConvertingCollection(ICollection<V> backing, Func<V, N> converter)
+            : this(backing, converter, null)
+        {
+        }
+
+        /// <summary>
+        /// Constructor, optionally taking a delegate to do a "backwards" contains operation.
+        /// </summary>
+        internal ReadOnlyConvertingCollection(ICollection<V> backing, Func<V, N> converter, Contains<N> contains)
+        {
+            ErrorUtilities.VerifyThrowArgumentNull(backing, "backing");
+            ErrorUtilities.VerifyThrowArgumentNull(converter, "converter");
+
+            _backing = backing;
+            _converter = converter;
+            _contains = contains;
+        }
+
+        #region ICollection<N> Members
+
+        /// <summary>
+        /// Return the number of items in the collection.
+        /// </summary>
+        public int Count
+        {
+            get { return _backing.Count; }
+        }
+
+        /// <summary>
+        /// Returns true if the collection is readonly.
+        /// </summary>
+        public bool IsReadOnly
+        {
+            get { return true; }
+        }
+
+        /// <summary>
+        /// Adds the specified item to the collection.
+        /// </summary>
+        public void Add(N item)
+        {
+            ErrorUtilities.ThrowInvalidOperation("OM_NotSupportedReadOnlyCollection");
+        }
+
+        /// <summary>
+        /// Clears all items from the collection.
+        /// </summary>
+        public void Clear()
+        {
+            ErrorUtilities.ThrowInvalidOperation("OM_NotSupportedReadOnlyCollection");
+        }
+
+        /// <summary>
+        /// Returns true if the collection contains the speciied item.
+        /// </summary>
+        public bool Contains(N item)
+        {
+            if (_contains == null)
+            {
+                ErrorUtilities.ThrowInvalidOperation("OM_NotSupportedConvertingCollectionValueToBacking");
+                return false;
+            }
+
+            return _contains(item);
+        }
+
+        /// <summary>
+        /// Copy the elements of the collection to the specified array.
+        /// </summary>
+        public void CopyTo(N[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Remove the specified item from the collection.
+        /// </summary>
+        public bool Remove(N item)
+        {
+            ErrorUtilities.ThrowInvalidOperation("OM_NotSupportedReadOnlyCollection");
+            return false;
+        }
+
+        #endregion
+
+        #region IEnumerable<N> Members
+
+        /// <summary>
+        /// Implementation of generic IEnumerable.GetEnumerator()
+        /// </summary>
+        public IEnumerator<N> GetEnumerator()
+        {
+            return new ConvertingEnumerable<V, N>(_backing, _converter).GetEnumerator();
+        }
+
+        #endregion
+
+        #region IEnumerable Members
+
+        /// <summary>
+        /// Implementation of IEnumerable.GetEnumerator()
+        /// </summary>
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable<N>)this).GetEnumerator();
+        }
+
+        #endregion
+    }
+}

src/Build/Collections/ReadOnlyConvertingDictionary.cs
@@ -61,13 +61,7 @@ namespace Microsoft.Build.Collections
         /// </summary>
         public ICollection<N> Values
         {
-            get
-            {
-                ErrorUtilities.ThrowInternalError("Values is not supported on ReadOnlyConvertingDictionary.");
-
-                // Show the compiler that this always throws:
-                throw new NotImplementedException();
-            }
+            get { return new ReadOnlyConvertingCollection<V, N>(_backing.Values, _converter); }
         }
 
         /// <summary>

src/Build/Collections/RetrievableEntryHashSet/HashSet.cs
@@ -229,7 +229,8 @@ namespace Microsoft.Build.Collections
         {
             get
             {
-                throw new NotImplementedException();
+                return new ReadOnlyConvertingCollection<T, string>(this, delegate(T input) { return input.Key; },
+                    delegate(string key) { return Contains(key); });
             }
         }
 

src/Build/Microsoft.Build.csproj
@@ -360,6 +360,7 @@
     <Compile Include="Collections\OrdinalIgnoreCaseKeyedComparer.cs" />
     <Compile Include="Collections\PropertyDictionary.cs" />
     <Compile Include="..\Shared\ReadOnlyCollection.cs" />
+    <Compile Include="Collections\ReadOnlyConvertingCollection.cs" />
     <Compile Include="Collections\ReadOnlyConvertingDictionary.cs" />
     <Compile Include="Collections\Triple.cs" />
     <!-- ######################## -->

src/Build.UnitTests/Collections/OMcollections_tests.cs
@@ -265,6 +265,24 @@ namespace Microsoft.Build.UnitTests.OM.Collections
         }
 
         /// <summary>
+        /// Verify that the converting collection functions.
+        /// </summary>
+        [Fact]
+        public void ReadOnlyConvertingCollection()
+        {
+            string[] source = { "1", "2", "3" };
+            ReadOnlyConvertingCollection<string, int> convertingCollection = new ReadOnlyConvertingCollection<string, int>(source, delegate (string x) { return Convert.ToInt32(x); });
+            Assert.Equal(3, convertingCollection.Count);
+            Assert.True(convertingCollection.IsReadOnly);
+
+            int index = 1;
+            foreach (int value in convertingCollection)
+            {
+                Assert.Equal(index++, value);
+            }
+        }
+
+        /// <summary>
         /// Verify that the converting dictionary functions.
         /// </summary>
         [Fact]