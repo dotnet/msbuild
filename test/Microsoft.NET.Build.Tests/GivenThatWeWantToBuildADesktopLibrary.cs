d9955fcf3440f8596f4e63aa37a8dbcc477d97f4
John Beisner <johnbeisner@users.noreply.github.com>
2017-09-26 14:19:00 -0700

    Adding P2P test coverage for targeting common PCL profiles (#1606)
    * Adding P2P test coverage for targeting common PCL profiles.
    
    * Adding copyright header; adding comments.

--- test/Microsoft.NET.Build.Tests/GivenThatWeWantToBuildADesktopLibrary.cs
+++ test/Microsoft.NET.Build.Tests/GivenThatWeWantToBuildADesktopLibrary.cs
@@ -1,4 +1,7 @@
-﻿using FluentAssertions;
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using FluentAssertions;
 using Microsoft.Build.Utilities;
 using Microsoft.Extensions.DependencyModel;
 using Microsoft.NET.TestFramework;
--- test/Microsoft.NET.Build.Tests/GivenThatWeWantToVerifyNuGetReferenceCompat.cs
+++ test/Microsoft.NET.Build.Tests/GivenThatWeWantToVerifyNuGetReferenceCompat.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.NET.TestFramework;
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.NET.TestFramework;
 using Microsoft.NET.TestFramework.Assertions;
 using Microsoft.NET.TestFramework.Commands;
 using Microsoft.NET.TestFramework.ProjectConstruction;
