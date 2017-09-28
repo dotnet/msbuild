aabcf60198b1c981196369c3aac759e7a7d277e2
Mihai Codoban <micodoba@microsoft.com>
2017-09-28 13:48:52 -0700

    Improve normalize perf
         Method |     Mean |    Error |   StdDev |  Gen 0 | Allocated |
    ----------- |---------:|---------:|---------:|-------:|----------:|
     ViaBuilder | 148.2 ns | 1.691 ns | 1.582 ns | 0.0215 |     136 B |
      ViaSplits | 895.0 ns | 5.154 ns | 4.569 ns | 0.0772 |     488 B |

--- src/Shared/FileMatcher.cs
+++ src/Shared/FileMatcher.cs
@@ -1586,43 +1586,99 @@ namespace Microsoft.Build.Shared
         }
 
         // Replace all slashes to the OS slash, collapse multiple slashes into one, trim trailing slashes
-        private static string Normalize(string aString)
+        internal static string Normalize(string aString)
         {
             if (string.IsNullOrEmpty(aString))
             {
                 return aString;
             }
 
-            var header = string.Empty;
+            var sb = new StringBuilder(aString.Length);
+            var index = 0;
 
-            // preserver meaningful root slashes
-            if (aString.StartsWith(@"\\"))
+            // preserve meaningful roots and their slashes
+            if (aString.Length >= 2 && IsValidDriveChar(aString[0]) && aString[1] == ':')
             {
-                header = @"\\";
+                sb.Append(aString[0]);
+                sb.Append(aString[1]);
+
+                var i = SkipCharacters(aString, 2, c => IsSlash(c));
+
+                if (index != i)
+                {
+                    sb.Append('\\');
+                }
+
+                index = i;
             }
-            else if (aString.StartsWith("/"))
+            else if (aString.StartsWith("/", StringComparison.Ordinal))
             {
-                header = "/";
+                sb.Append('/');
+                index = SkipCharacters(aString, 1, c => IsSlash(c));
             }
-            else if (aString.StartsWith(@"\"))
+            else if (aString.StartsWith(@"\\", StringComparison.Ordinal))
             {
-                header = @"\";
+                sb.Append(@"\\");
+                index = SkipCharacters(aString, 2, c => IsSlash(c));
+            }
+            else if (aString.StartsWith(@"\", StringComparison.Ordinal))
+            {
+                sb.Append(@"\");
+                index = SkipCharacters(aString, 1, c => IsSlash(c));
             }
 
-            var splits = aString.Split(directorySeparatorCharacters, StringSplitOptions.RemoveEmptyEntries);
-
-            if (splits.Length == 0)
+            while (index < aString.Length)
             {
-                splits = new string[0];
+                var afterSlashesIndex = SkipCharacters(aString, index, c => IsSlash(c));
+
+                // do not append separator at the end of the string
+                if (afterSlashesIndex >= aString.Length)
+                {
+                    break;
+                }
+                // replace multiple slashes with the OS separator
+                else if (afterSlashesIndex > index)
+                {
+                    sb.Append(s_directorySeparator);
+                }
+
+                var afterNonSlashIndex = SkipCharacters(aString, afterSlashesIndex, c => !IsSlash(c));
+
+                sb.Append(aString, afterSlashesIndex, afterNonSlashIndex - afterSlashesIndex);
+
+                index = afterNonSlashIndex;
             }
 
-            // do not loose the slash when the only split element is a drive letter
-            if (splits.Length == 1 && splits[0].Length == 2 && splits[0][1] == ':')
+            return sb.ToString();
+        }
+
+        private static bool IsSlash(char c) => c == '/' || c == '\\';
+
+        /// <summary>
+        /// Skips characters that satisfy the condition <param name="jumpOverCharacter"></param>
+        /// </summary>
+        /// <param name="aString">The working string</param>
+        /// <param name="startingIndex">Offset in string to start the search in</param>
+        /// <returns>First index that does not satisfy the condition. Returns the string's length if end of string is reached</returns>
+        private static int SkipCharacters(string aString, int startingIndex, Func<char, bool> jumpOverCharacter)
+        {
+            var index = startingIndex;
+
+            while (index < aString.Length && jumpOverCharacter(aString[index]))
             {
-                splits[0] = splits[0] + s_directorySeparator;
+                index++;
             }
 
-            return header + splits.DefaultIfEmpty().Aggregate((a, b) => $"{a}{s_directorySeparator}{b}");
+            return index;
+        }
+
+        // copied from https://github.com/dotnet/corefx/blob/master/src/Common/src/System/IO/PathInternal.Windows.cs#L77-L83
+        /// <summary>
+        /// Returns true if the given character is a valid drive letter
+        /// </summary>
+        internal static bool IsValidDriveChar(char value)
+        {
+            return ((value >= 'A' && value <= 'Z') || (value >= 'a' && value <= 'z'));
         }
 
         static string[] CreateArrayWithSingleItemIfNotExcluded(string filespecUnescaped, IEnumerable<string> excludeSpecsUnescaped)
