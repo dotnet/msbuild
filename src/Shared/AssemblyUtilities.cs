d2d4fcfa12d6905436ff37ea07102564ebdc09ac
Mihai Codoban <micodoba@microsoft.com>
2017-10-03 09:46:49 -0700

    Use reflection to find CultureInfo.GetCultures
    Closes #2349

--- src/Shared/AssemblyUtilities.cs
+++ src/Shared/AssemblyUtilities.cs
@@ -2,6 +2,8 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
+using System.Globalization;
+using System.Linq;
 using System.Reflection;
 
 namespace Microsoft.Build.Shared
@@ -17,6 +19,11 @@ namespace Microsoft.Build.Shared
         // Cached method info
         private static MethodInfo s_assemblyNameCloneMethod;
         private static PropertyInfo s_assemblylocationProperty;
+        private static MethodInfo s_cultureInfoGetCultureMethod;
+
+#if !FEATURE_CULTUREINFO_GETCULTURES
+        private static Lazy<CultureInfo[]> s_validCultures = new Lazy<CultureInfo[]>(() => GetValidCultures(), true);
+#endif
 
         public static string GetAssemblyLocation(Assembly assembly)
         {
@@ -66,6 +73,27 @@ namespace Microsoft.Build.Shared
 #endif
         }
 
+        public static bool CultureInfoHasGetCultures()
+        {
+            return s_cultureInfoGetCultureMethod != null;
+        }
+
+        public static CultureInfo[] GetAllCultures()
+        {
+#if FEATURE_CULTUREINFO_GETCULTURES
+            return CultureInfo.GetCultures(CultureTypes.AllCultures);
+#else
+            Initialize();
+
+            if (!CultureInfoHasGetCultures())
+            {
+                throw new NotSupportedException("CultureInfo does not have the method GetCultures");
+            }
+
+            return s_validCultures.Value;
+#endif
+        }
+
         /// <summary>
         /// Initialize static fields. Doesn't need to be thread safe.
         /// </summary>
@@ -75,7 +103,29 @@ namespace Microsoft.Build.Shared
 
             s_assemblyNameCloneMethod = typeof(AssemblyName).GetMethod("Clone");
             s_assemblylocationProperty = typeof(Assembly).GetProperty("Location", typeof(string));
+            s_cultureInfoGetCultureMethod = typeof(CultureInfo).GetMethod("GetCultures");
+
             s_initialized = true;
         }
+
+#if !FEATURE_CULTUREINFO_GETCULTURES
+        private static CultureInfo[] GetValidCultures()
+        {
+            var cultureTypesType = s_cultureInfoGetCultureMethod?.GetParameters().FirstOrDefault()?.ParameterType;
+
+            ErrorUtilities.VerifyThrow(cultureTypesType != null &&
+                                       cultureTypesType.Name == "CultureTypes" &&
+                                       Enum.IsDefined(cultureTypesType, "AllCultures"),
+                                       "GetCulture is expected to accept CultureTypes.AllCultures");
+
+            var allCulturesEnumValue = Enum.Parse(cultureTypesType, "AllCultures", true);
+
+            var cultures = s_cultureInfoGetCultureMethod.Invoke(null, new[] {allCulturesEnumValue}) as CultureInfo[];
+
+            ErrorUtilities.VerifyThrowInternalNull(cultures, "CultureInfo.GetCultures should work if all reflection checks pass");
+
+            return cultures;
+        }
+#endif
     }
 }
