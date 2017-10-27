cef9a90ad130310cfa2e1f25cf40e0fa9ba63343
William Lee <wul@microsoft.com>
2017-10-27 10:58:25 -0700

    Hashed telemetry and event name change (#7919)

--- src/dotnet/Telemetry/TelemetryFilter.cs
+++ src/dotnet/Telemetry/TelemetryFilter.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System.Collections.Generic;
+using System;
 using System.Linq;
 using Microsoft.DotNet.Cli.CommandLine;
 using Microsoft.DotNet.Cli.Utils;
@@ -11,48 +12,15 @@ namespace Microsoft.DotNet.Cli.Telemetry
     internal class TelemetryFilter : ITelemetryFilter
     {
         private const string DotnetName = "dotnet";
+        private readonly Func<string, string> _hash;
+
+        public TelemetryFilter(Func<string, string> hash)
+        {
+            _hash = hash ?? throw new ArgumentNullException(nameof(hash));
+        }
 
         public IEnumerable<ApplicationInsightsEntryFormat> Filter(object objectToFilter)
         {
-            var ruleSet = new List<IParseResultLogRule>
-            {   new AllowListToSendFirstArgument(new HashSet<string>{ "new", "help" }),
-                new AllowListToSendFirstAppliedOptions(new HashSet<string>{ "add", "remove", "list", "sln", "nuget" }),
-                new TopLevelCommandNameAndOptionToLog
-                (
-                    topLevelCommandName: new HashSet<string> { "new" },
-                    optionsToLog: new HashSet<string> { "language" }
-                ),
-                new TopLevelCommandNameAndOptionToLog
-                (
-                    topLevelCommandName: new HashSet<string> { "build", "publish" },
-                    optionsToLog: new HashSet<string> { "framework", "runtime", "configuration" }
-                ),
-                new TopLevelCommandNameAndOptionToLog
-                (
-                    topLevelCommandName: new HashSet<string> { "run", "clean", "test" },
-                    optionsToLog: new HashSet<string> { "framework", "configuration" }
-                ),
-                new TopLevelCommandNameAndOptionToLog
-                (
-                    topLevelCommandName: new HashSet<string> { "pack" },
-                    optionsToLog: new HashSet<string> { "configuration" }
-                ),
-                new TopLevelCommandNameAndOptionToLog
-                (
-                    topLevelCommandName: new HashSet<string> { "migrate" },
-                    optionsToLog: new HashSet<string> { "sdk-package-version" }
-                ),
-                new TopLevelCommandNameAndOptionToLog
-                (
-                    topLevelCommandName: new HashSet<string> { "vstest" },
-                    optionsToLog: new HashSet<string> { "platform", "framework", "logger" }
-                ),
-                new TopLevelCommandNameAndOptionToLog
-                (
-                    topLevelCommandName: new HashSet<string> { "publish" },
-                    optionsToLog: new HashSet<string> { "runtime" }
-                )
-            };
             var result = new List<ApplicationInsightsEntryFormat>();
 
             if (objectToFilter is ParseResult parseResult)
@@ -62,15 +30,72 @@ namespace Microsoft.DotNet.Cli.Telemetry
                 {
                     LogVerbosityForAllTopLevelCommand(result, parseResult, topLevelCommandName);
 
-                    foreach (IParseResultLogRule rule in ruleSet)
+                    foreach (IParseResultLogRule rule in ParseResultLogRules)
                     {
                         result.AddRange(rule.AllowList(parseResult));
                     }
                 }
             }
+            else if(objectToFilter is TopLevelCommandParserResult topLevelCommandParserResult)
+            {
+                result.Add(new ApplicationInsightsEntryFormat(
+                            "toplevelparser/command",
+                            new Dictionary<string, string>()
+                        {{ "verb", topLevelCommandParserResult.Command}}
+                ));
 
-            return result;
+            }
+            else if (objectToFilter is InstallerSuccessReport installerSuccessReport)
+            {
+                result.Add(new ApplicationInsightsEntryFormat(
+                    "install/reportsuccess",
+                    new Dictionary<string, string> {{ "exeName", installerSuccessReport.ExeName}}
+                ));
+            }
+
+            return result.Select(r => r.WithAppliedToPropertiesValue(_hash)).ToList();
         }
+
+        private static List<IParseResultLogRule> ParseResultLogRules => new List<IParseResultLogRule>
+        {
+            new AllowListToSendFirstArgument(new HashSet<string> {"new", "help"}),
+            new AllowListToSendFirstAppliedOptions(new HashSet<string> {"add", "remove", "list", "sln", "nuget"}),
+            new TopLevelCommandNameAndOptionToLog
+            (
+                topLevelCommandName: new HashSet<string> {"new"},
+                optionsToLog: new HashSet<string> {"language"}
+            ),
+            new TopLevelCommandNameAndOptionToLog
+            (
+                topLevelCommandName: new HashSet<string> {"build", "publish"},
+                optionsToLog: new HashSet<string> {"framework", "runtime", "configuration"}
+            ),
+            new TopLevelCommandNameAndOptionToLog
+            (
+                topLevelCommandName: new HashSet<string> {"run", "clean", "test"},
+                optionsToLog: new HashSet<string> {"framework", "configuration"}
+            ),
+            new TopLevelCommandNameAndOptionToLog
+            (
+                topLevelCommandName: new HashSet<string> {"pack"},
+                optionsToLog: new HashSet<string> {"configuration"}
+            ),
+            new TopLevelCommandNameAndOptionToLog
+            (
+                topLevelCommandName: new HashSet<string> {"migrate"},
+                optionsToLog: new HashSet<string> {"sdk-package-version"}
+            ),
+            new TopLevelCommandNameAndOptionToLog
+            (
+                topLevelCommandName: new HashSet<string> {"vstest"},
+                optionsToLog: new HashSet<string> {"platform", "framework", "logger"}
+            ),
+            new TopLevelCommandNameAndOptionToLog
+            (
+                topLevelCommandName: new HashSet<string> {"publish"},
+                optionsToLog: new HashSet<string> {"runtime"}
+            )
+        };
 
         private static void LogVerbosityForAllTopLevelCommand(
             ICollection<ApplicationInsightsEntryFormat> result,
@@ -84,9 +109,10 @@ namespace Microsoft.DotNet.Cli.Telemetry
                     parseResult[DotnetName][topLevelCommandName].AppliedOptions["verbosity"];
 
                 result.Add(new ApplicationInsightsEntryFormat(
-                    "dotnet-" + topLevelCommandName,
+                    "sublevelparser/command",
                     new Dictionary<string, string>()
                     {
+                        { "verb", topLevelCommandName},
                         {"verbosity", appliedOptions.Arguments.ElementAt(0)}
                     }));
             }
