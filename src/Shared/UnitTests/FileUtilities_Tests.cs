c9532bb1a55dbbfac378b41529bdd5c991fd1e8f
David Kean <davkean@microsoft.com>
2017-11-03 15:58:48 +1100

    Remove and sort usings

--- src/Shared/UnitTests/FileUtilities_Tests.cs
+++ src/Shared/UnitTests/FileUtilities_Tests.cs
@@ -2,16 +2,15 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 using System.Reflection;
 using System.Text;
-
-using Microsoft.Build.Shared;
-using System.IO;
-using System.Collections.Generic;
-using Microsoft.Build.Evaluation;
-using Xunit;
 using System.Threading;
-using System.Globalization;
+using Microsoft.Build.Evaluation;
+using Microsoft.Build.Shared;
+using Xunit;
 
 namespace Microsoft.Build.UnitTests
 {
