4b523ec5ff4954df182ca03476c0db3cb92bb648
Vijay Ramakrishnan <vramak@microsoft.com>
2017-11-03 10:43:42 -0700

    Add Support for cross plat build

--- tools/Build.cmd
+++ tools/Build.cmd
@@ -3,12 +3,10 @@
 call %~dp0\EnsureWebSdkEnv.cmd
 
 REM Copy the files required for signing
-xcopy \\aspnetci\share\tools\Microsoft.Web.MsBuildTasks2.dll %WebSdkTools% /y /C
-xcopy \\aspnetci\share\tools\7za.exe %WebSdkTools% /y /C
-xcopy \\aspnetci\share\tools\Microsoft.NET.Sdk.Web.Sign.targets %WebSdkTools% /y /C
-xcopy \\aspnetci\share\tools\WebDeploy\* %WebSdkTools%\WebDeploy\* /y /C /e /s /f
-
-call %WebSdkRoot%\tools\InstallUpdates.cmd
+xcopy \\aspnetci\share\tools\websdk\Microsoft.Web.MsBuildTasks2.dll %WebSdkTools% /y /C
+xcopy \\aspnetci\share\tools\websdk\7za.exe %WebSdkTools% /y /C
+xcopy \\aspnetci\share\tools\websdk\Microsoft.NET.Sdk.Web.Sign.targets %WebSdkTools% /y /C
+xcopy \\aspnetci\share\tools\websdk\WebDeploy\* %WebSdkTools%\WebDeploy\* /y /C /e /s /f
 
 msbuild %WebSdkRoot%\build.proj /p:configuration=Release;SkipInvalidConfigurations=true /t:Build;Sign
 if errorlevel 0 exit /b 0
