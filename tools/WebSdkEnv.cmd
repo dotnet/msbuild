87a346afbe828f2e1d9b2322844317e7288a87a2
Vijay Ramakrishnan <vramak@microsoft.com>
2017-11-04 18:31:14 -0700

    Adding msbuild to the path

--- tools/WebSdkEnv.cmd
+++ tools/WebSdkEnv.cmd
@@ -8,6 +8,20 @@ if not defined DOTNET_VERSION (
     set DOTNET_VERSION=2.0.1-servicing-006924
 )
 
+if defined ProgramFiles(x86) (
+    set "WebSdkProgramFiles=%ProgramFiles(x86)%"
+) else (
+    set "WebSdkProgramFiles=%ProgramFiles%"
+)
+
+if exist "%WebSdkProgramFiles%\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe" (
+    set "WebSdkVSVersion=15.0"
+    set WebSdkMSBuildPath="%WebSdkProgramFiles%\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin"
+) else (
+    set "WebSdkVSVersion=14.0"
+    set WebSdkMSBuildPath="%WebSdkProgramFiles%\MSBuild\14.0\Bin"
+)
+
 set "WebSdkRoot=%~dp0"
 set "WebSdkRoot=%WebSdkRoot:~0,-7%"
 set "WebSdkBin=%WebSdkRoot%\bin\"
@@ -16,4 +30,5 @@ set "WebSdkReferences=%WebSdkRoot%\references\"
 set "WebSdkSource=%WebSdkRoot%\src\"
 set "WebSdkTools=%WebSdkRoot%\tools\"
 
+set "PATH=%PATH%;%WebSdkMSBuildPath%"
 set "PATH=%PATH%;%WebSdkTools%"
