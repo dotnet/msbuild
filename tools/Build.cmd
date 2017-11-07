a910dd8c43fbdc661120d011719ab9b5ca4659e9
Vijay Ramakrishnan <vramak@microsoft.com>
2017-11-06 22:27:50 -0800

    Adding quotes for all the paths

--- tools/Build.cmd
+++ tools/Build.cmd
@@ -1,14 +1,14 @@
 @echo off
 
-call %~dp0\EnsureWebSdkEnv.cmd
+call "%~dp0\EnsureWebSdkEnv.cmd"
 
 REM Copy the files required for signing
-xcopy \\aspnetci\share\tools\websdk\Microsoft.Web.MsBuildTasks2.dll %WebSdkTools% /y /C
-xcopy \\aspnetci\share\tools\websdk\7za.exe %WebSdkTools% /y /C
-xcopy \\aspnetci\share\tools\websdk\Microsoft.NET.Sdk.Web.Sign.targets %WebSdkTools% /y /C
-xcopy \\aspnetci\share\tools\websdk\WebDeploy\* %WebSdkTools%\WebDeploy\* /y /C /e /s /f
+xcopy \\aspnetci\share\tools\websdk\Microsoft.Web.MsBuildTasks2.dll "%WebSdkTools%" /y /C
+xcopy \\aspnetci\share\tools\websdk\7za.exe "%WebSdkTools%" /y /C
+xcopy \\aspnetci\share\tools\websdk\Microsoft.NET.Sdk.Web.Sign.targets "%WebSdkTools%" /y /C
+xcopy \\aspnetci\share\tools\websdk\WebDeploy\* "%WebSdkTools%\WebDeploy\*" /y /C /e /s /f
 
-msbuild %WebSdkRoot%\build.proj /p:configuration=Release;SkipInvalidConfigurations=true /t:Build;Sign
+msbuild "%WebSdkRoot%\build.proj" /p:configuration=Release /p:SkipInvalidConfigurations=true /t:Build;Sign
 if errorlevel 0 exit /b 0
 
 :ERROR
