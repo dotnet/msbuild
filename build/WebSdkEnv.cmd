14cd91e23fbd7ea0c7f54059a8c8561a31b07cd9
Vijay Ramakrishnan <vramak@microsoft.com>
2017-11-15 15:14:49 -0800

    Adding a script to run Desktop msbuild

--- build/WebSdkEnv.cmd
+++ build/WebSdkEnv.cmd
@@ -16,3 +16,18 @@ set "WebSdkBuild=%WebSdkRoot%\build\"
 set "WebSdkPublishBin=%WebSdkRoot%\src\Publish\Microsoft.NET.Sdk.Publish.Tasks\bin\"
 
 set "PATH=%PATH%;%WebSdkBuild%"
+
+REM Only required for the signing packages
+if defined ProgramFiles(x86) (
+    set "WebSdkProgramFiles=%ProgramFiles(x86)%"
+) else (
+    set "WebSdkProgramFiles=%ProgramFiles%"
+)
+
+if exist "%WebSdkProgramFiles%\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe" (
+    set "WebSdkMSBuildPath=%WebSdkProgramFiles%\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin"
+) else (
+    set "WebSdkMSBuildPath=%WebSdkProgramFiles%\MSBuild\14.0\Bin"
+)
+
+set "PATH=%PATH%;%WebSdkMSBuildPath%"
---
+++ build_FullMsBuild.cmd
@@ -0,0 +1,15 @@
+@ECHO OFF
+
+PowerShell -NoProfile -NoLogo -ExecutionPolicy unrestricted -Command "[System.Threading.Thread]::CurrentThread.CurrentCulture = ''; [System.Threading.Thread]::CurrentThread.CurrentUICulture = '';& '%~dp0dotnet-install_1.0.ps1' %*; exit $LASTEXITCODE" 
+PowerShell -NoProfile -NoLogo -ExecutionPolicy unrestricted -Command "[System.Threading.Thread]::CurrentThread.CurrentCulture = ''; [System.Threading.Thread]::CurrentThread.CurrentUICulture = '';& '%~dp0dotnet-install_2.0.ps1' %*; exit $LASTEXITCODE" 
+SET PATH=%localappdata%\Microsoft\dotnet;%PATH%
+
+call "%~dp0\build\EnsureWebSdkEnv.cmd"
+xcopy \\aspnetci\share\tools\websdk\WebDeploy\* "%WebSdkBuild%\WebDeploy\*" /y /C /e /s /f
+
+msbuild "%WebSdkBuild%\build.proj" /p:configuration=Release /t:Build
+if errorlevel 0 exit /b 0
+
+:ERROR
+endlocal
+exit /b 1
