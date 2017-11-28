5afaf3bb8c19185020943537139efeef1aa3d0fc
Daniel Plaisted <daplaist@microsoft.com>
2017-11-28 14:05:42 -0800

    Use a project-specific temporary path
    This is so multiple test projects can be run in parallel without sharing the same temp directory

--- src/Shared/UnitTests/TestAssemblyInfo.cs
+++ src/Shared/UnitTests/TestAssemblyInfo.cs
@@ -13,12 +13,14 @@ using Xunit;
 
 [assembly: AssemblyFixture(typeof(MSBuildTestAssemblyFixture))]
 
-public class MSBuildTestAssemblyFixture
+public class MSBuildTestAssemblyFixture : IDisposable
 {
+    string _oldTempPath;
+    bool _disposed;
+
     public MSBuildTestAssemblyFixture()
     {
         //  Find correct version of "dotnet", and set DOTNET_HOST_PATH so that the Roslyn tasks will use the right host
-
         var currentFolder = System.AppContext.BaseDirectory;
 
         while (currentFolder != null)
@@ -46,6 +48,31 @@ public class MSBuildTestAssemblyFixture
             }
 
             currentFolder = Directory.GetParent(currentFolder)?.FullName;
+        }
+
+        //  Use a project-specific temporary path
+        //  This is so multiple test projects can be run in parallel without sharing the same temp directory
+        _oldTempPath = Environment.GetEnvironmentVariable("TMP");
+        string newTempPath = Path.Combine(AppContext.BaseDirectory, "Temp");
+        if (!Directory.Exists(newTempPath))
+        {
+            Directory.CreateDirectory(newTempPath);
+        }
+        Environment.SetEnvironmentVariable("TMP", newTempPath);
+    }
+
+    public void Dispose()
+    {
+        if (!_disposed)
+        {
+            
+            //  Ideally we would delete the temp directory, but this is apparently failing in some cases (possibly because
+            //  tests are failing and not being cleaned up correctly)
+            //Directory.Delete(Environment.GetEnvironmentVariable("TMP"), true);
+
+            Environment.SetEnvironmentVariable("TMP", _oldTempPath);
+
+            _disposed = true;
         }
     }
 }
