d4bb158ce24e08c5ca9fff3f830d7dd925ff1cb0
Mihai Codoban <micodoba@microsoft.com>
2017-12-15 14:55:56 -0800

    Add construction info to Project

---
+++ src/Build/Definition/ProjectConstructionInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace Microsoft.Build.Evaluation
+{
+    /// <summary>
+    ///     Common <see cref="Project" /> constructor arguments.
+    /// </summary>
+    public class ProjectConstructionInfo
+    {
+        /// <summary>
+        /// Global properties to evaluate with.
+        /// </summary>
+        public IDictionary<string, string> GlobalProperties { get; set; }
+
+        /// <summary>
+        /// Tools version to evaluate with
+        /// </summary>
+        public string ToolsVersion { get; set; }
+
+        /// <summary>
+        /// Sub-toolset version to explicitly evaluate the toolset with.
+        /// </summary>
+        public string SubToolsetVersion { get; set; }
+
+        /// <summary>
+        /// The <see cref="ProjectCollection"/> the project is added to. Default is <see cref="ProjectCollection.GlobalProjectCollection"/>/>
+        /// </summary>
+        public ProjectCollection ProjectCollection { get; set; }
+
+        /// <summary>
+        /// The <see cref="ProjectLoadSettings"/> to use for evaluation.
+        /// </summary>
+        public ProjectLoadSettings LoadSettings { get; set; } = ProjectLoadSettings.Default;
+    }
+}
