30d6089496dd54341de84424d07bb0999f89a49f
Jeff Kluge <jeffkl@microsoft.com>
2018-01-02 10:25:07 -0800

    Add new MSBuildVersion property (#2819)
    Sets a property named `MSBuildVersion` which contains the Major.Minor.Build version of MSBuild.  This allows build owners to implement errors if a user is using an older version of MSBuild that might not be able to build their source code.
    
    Closes #2350

--- src/Build/Definition/Toolset.cs
+++ src/Build/Definition/Toolset.cs
@@ -928,6 +928,7 @@ namespace Microsoft.Build.Evaluation
 
                     reservedProperties.Add(ProjectPropertyInstance.Create(ReservedPropertyNames.toolsPath, EscapingUtilities.Escape(ToolsPath), mayBeReserved: true));
                     reservedProperties.Add(ProjectPropertyInstance.Create(ReservedPropertyNames.assemblyVersion, Constants.AssemblyVersion, mayBeReserved: true));
+                    reservedProperties.Add(ProjectPropertyInstance.Create(ReservedPropertyNames.version, MSBuildAssemblyFileVersion.Instance.MajorMinorBuild, mayBeReserved: true));
 
                     // Add one for the subtoolset version property -- it may or may not be set depending on whether it has already been set by the 
                     // environment or global properties, but it's better to create a dictionary that's one too big than one that's one too small.  
