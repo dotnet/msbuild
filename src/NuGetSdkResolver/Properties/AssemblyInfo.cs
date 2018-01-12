4bc4e06459a5c775e604cde2993dcbce1850345e
Jeff Kluge <jeffkl@microsoft.com>
2018-01-12 09:30:28 -0800

    Clean up AssemblyInfo

--- src/NuGetSdkResolver/Properties/AssemblyInfo.cs
+++ src/NuGetSdkResolver/Properties/AssemblyInfo.cs
@@ -7,47 +7,14 @@
 
 using System;
 using System.Reflection;
-using System.Resources;
-#if FEATURE_SECURITY_PERMISSIONS
-using System.Security.Permissions;
-#endif
-using System.Runtime.InteropServices;
-using System.Runtime.CompilerServices;
-
-#if FEATURE_SECURITY_PERMISSIONS
-// A combination of RequestMinimum and RequestOptional causes the permissions granted to 
-// the assembly to only be the permission requested (like a PermitOnly). More generally
-// the equation for the PermissionSet granted at load time is:
-//
-//        Granted = (MaxGrant intersect (ReqMin union ReqOpt)) - ReqRefuse
-//
-// Where,
-//        MaxGrant -- the permissions granted by policy.
-//        ReqMin -- the permissions that RequestMinimum is specified for.
-//        ReqOpt -- the permissions that RequestOptional is specified for.
-//        ReqRefuse -- the permissions that Request refuse is specified for.
-//
-// Note that if ReqOpt is the empty set, then it is consider to be "FullTrust" and this 
-// equation becomes:
-//
-//        Granted = MaxGrant - ReqRefuse
-//
-// Regardless of whether ReqMin is empty or not.
-#pragma warning disable 618
-[assembly: SecurityPermission(SecurityAction.RequestMinimum, Flags = SecurityPermissionFlag.Execution)]
-#pragma warning restore 618
-#endif
 
 #if STATIC_VERSION_NUMBER
 [assembly: AssemblyVersion(Microsoft.Build.Shared.MSBuildConstants.CurrentAssemblyVersion)]
 [assembly: AssemblyFileVersion(Microsoft.Build.Shared.MSBuildConstants.CurrentAssemblyFileVersion)]
 #endif
 
-// This will enable passing the SafeDirectories flag to any P/Invoke calls/implementations within the assembly, 
-// so that we don't run into known security issues with loading libraries from unsafe locations 
-[assembly: DefaultDllImportSearchPaths(DllImportSearchPath.SafeDirectories)]
-
 #if (LOCALIZED_BUILD)
+using System.Resources;
 // Needed for the "hub-and-spoke model to locate and retrieve localized resources": https://msdn.microsoft.com/en-us/library/21a15yht(v=vs.110).aspx
 // We want "en" to require a satellite assembly for debug builds in order to flush out localization
 // issues, but we want release builds to work without it. Also, .net core does not have resource fallbacks
