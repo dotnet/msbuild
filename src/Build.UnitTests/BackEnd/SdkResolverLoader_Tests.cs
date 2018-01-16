79bb14c2d2c603225861ac151d6450c121667c42
Jeff Kluge <jeffkl@microsoft.com>
2018-01-16 15:29:11 -0800

    More unit tests

--- src/Build.UnitTests/BackEnd/SdkResolverLoader_Tests.cs
+++ src/Build.UnitTests/BackEnd/SdkResolverLoader_Tests.cs
@@ -1,12 +1,21 @@
-﻿using Shouldly;
+﻿using System;
+using System.Collections.Generic;
+using Shouldly;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using Microsoft.Build.BackEnd.Logging;
 using Microsoft.Build.BackEnd.SdkResolution;
+using Microsoft.Build.Construction;
 using Microsoft.Build.Framework;
 using Microsoft.Build.Shared;
 using Microsoft.Build.UnitTests;
 using Xunit;
+using Exception = System.Exception;
+using SdkResolverBase = Microsoft.Build.Framework.SdkResolver;
+using SdkResolverContextBase = Microsoft.Build.Framework.SdkResolverContext;
+using SdkResultBase = Microsoft.Build.Framework.SdkResult;
+using SdkResultFactoryBase = Microsoft.Build.Framework.SdkResultFactory;
 
 namespace Microsoft.Build.Engine.UnitTests.BackEnd
 {
@@ -71,6 +80,160 @@ namespace Microsoft.Build.Engine.UnitTests.BackEnd
             finally
             {
                 FileUtilities.DeleteDirectoryNoThrow(root, true);
+            }
+        }
+
+        /// <summary>
+        /// Verifies that if an SDK resolver throws while creating an instance that a warning is logged.
+        /// </summary>
+        [Fact]
+        public void VerifyWarningLoggedWhenResolverFailsToLoad()
+        {
+            SdkResolverLoader sdkResolverLoader = new MockSdkResolverLoader
+            {
+                LoadResolverAssemblyFunc = (resolverPath, loggingContext, location) => typeof(SdkResolverLoader_Tests).GetTypeInfo().Assembly,
+                FindPotentialSdkResolversFunc = rootFolder => new List<string>
+                {
+                    "myresolver.dll"
+                },
+                GetResolverTypesFunc = assembly => new[] { typeof(MockSdkResolverThatDoesNotLoad) }
+            };
+
+            sdkResolverLoader.LoadResolvers(_loggingContext, ElementLocation.EmptyLocation);
+
+            _logger.Warnings.Select(i => i.Message).ShouldBe(new []
+            {
+                $"The SDK resolver type \"{nameof(MockSdkResolverThatDoesNotLoad)}\" failed to load. A8BB8B3131D3475D881ACD3AF8D75BD6"
+            });
+            _logger.ErrorCount.ShouldBe(0);
+        }
+
+        /// <summary>
+        /// Verifies that when we attempt to create an instance of a resolver with no public constructor that a warning
+        /// is logged with the appropriate message.
+        /// </summary>
+        [Fact]
+        public void VerifyWarningLoggedResolverHasNoPublicConstructor()
+        {
+            SdkResolverLoader sdkResolverLoader = new MockSdkResolverLoader
+            {
+                LoadResolverAssemblyFunc = (resolverPath, loggingContext, location) => typeof(SdkResolverLoader_Tests).GetTypeInfo().Assembly,
+                FindPotentialSdkResolversFunc = rootFolder => new List<string>
+                {
+                    "myresolver.dll"
+                },
+                GetResolverTypesFunc = assembly => new[] { typeof(MockSdkResolverNoPublicConstructor) }
+            };
+
+            sdkResolverLoader.LoadResolvers(_loggingContext, ElementLocation.EmptyLocation);
+
+            _logger.Warnings.Select(i => i.Message).ShouldBe(new[]
+            {
+                $"The SDK resolver type \"{nameof(MockSdkResolverNoPublicConstructor)}\" failed to load. No parameterless constructor defined for this object."
+            });
+            _logger.ErrorCount.ShouldBe(0);
+        }
+
+        /// <summary>
+        /// Verifies that when a resolver assembly cannot be loaded, that a warning is logged and other resolvers are still loaded.
+        /// </summary>
+        [Fact]
+        public void VerifyWarningLoggedWhenResolverAssemblyCannotBeLoaded()
+        {
+            const string assemblyPath = @"C:\foo\bar\myresolver.dll";
+            const string expectedMessage = "91BF077D4E9646819DE7AB2CBA2637B6";
+
+            SdkResolverLoader sdkResolverLoader = new MockSdkResolverLoader
+            {
+                LoadResolverAssemblyFunc = (resolverPath, loggingContext, location) => throw new Exception(expectedMessage),
+                FindPotentialSdkResolversFunc = rootFolder => new List<string>
+                {
+                    assemblyPath,
+                }
+            };
+
+            IList<SdkResolverBase> resolvers = sdkResolverLoader.LoadResolvers(_loggingContext, ElementLocation.EmptyLocation);
+
+            resolvers.Count.ShouldBe(1);
+
+            _logger.Warnings.Select(i => i.Message).ShouldBe(new[]
+            {
+                $"The SDK resolver assembly \"{assemblyPath}\" could not be loaded. {expectedMessage}"
+            });
+            _logger.ErrorCount.ShouldBe(0);
+        }
+
+        private class MockSdkResolverThatDoesNotLoad : SdkResolverBase
+        {
+            public const string ExpectedMessage = "A8BB8B3131D3475D881ACD3AF8D75BD6";
+
+            public MockSdkResolverThatDoesNotLoad()
+            {
+                throw new Exception(ExpectedMessage);
+            }
+
+            public override string Name => nameof(MockSdkResolverThatDoesNotLoad);
+
+            public override int Priority => 0;
+
+            public override SdkResultBase Resolve(SdkReference sdkReference, SdkResolverContextBase resolverContext, SdkResultFactoryBase factory)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        private class MockSdkResolverNoPublicConstructor : SdkResolverBase
+        {
+            private MockSdkResolverNoPublicConstructor()
+            {
+            }
+
+            public override string Name => nameof(MockSdkResolverNoPublicConstructor);
+
+            public override int Priority => 0;
+
+            public override SdkResultBase Resolve(SdkReference sdkReference, SdkResolverContextBase resolverContext, SdkResultFactoryBase factory)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        private class MockSdkResolverLoader : SdkResolverLoader
+        {
+            public Func<string, LoggingContext, ElementLocation, Assembly> LoadResolverAssemblyFunc { get; set; }
+
+            public Func<string, IList<string>> FindPotentialSdkResolversFunc { get; set; }
+
+            public Func<Assembly, IEnumerable<Type>> GetResolverTypesFunc { get; set; }
+
+            protected override Assembly LoadResolverAssembly(string resolverPath, LoggingContext loggingContext, ElementLocation location)
+            {
+                if (LoadResolverAssemblyFunc != null)
+                {
+                    return LoadResolverAssemblyFunc(resolverPath, loggingContext, location);
+                }
+
+                return base.LoadResolverAssembly(resolverPath, loggingContext, location);
+            }
+
+            protected override IEnumerable<Type> GetResolverTypes(Assembly assembly)
+            {
+                if (GetResolverTypesFunc != null)
+                {
+                    return GetResolverTypesFunc(assembly);
+                }
+
+                return base.GetResolverTypes(assembly);
+            }
+
+            internal override IList<string> FindPotentialSdkResolvers(string rootFolder)
+            {
+                if (FindPotentialSdkResolversFunc != null)
+                {
+                    return FindPotentialSdkResolversFunc(rootFolder);
+                }
+
+                return base.FindPotentialSdkResolvers(rootFolder);
             }
         }
     }
