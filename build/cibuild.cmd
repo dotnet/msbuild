8ba038dbb354c7ca74ce7ad54a24de523f15039f
Daniel Plaisted <daplaist@microsoft.com>
2018-01-22 18:06:17 -0800

    Fix vsix signing configuration

--- build/cibuild.cmd
+++ build/cibuild.cmd
@@ -1,4 +1,3 @@
 @echo off
-REM -sign temporarily disabled
-powershell -ExecutionPolicy ByPass %~dp0build.ps1 -build -pack -ci -prepareMachine %*
+powershell -ExecutionPolicy ByPass %~dp0build.ps1 -build -pack -sign -ci -prepareMachine %*
 exit /b %ErrorLevel%
