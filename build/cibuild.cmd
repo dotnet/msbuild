12f8c229cbffdeae183c604e925400278f3f83df
Daniel Plaisted <daplaist@microsoft.com>
2018-01-22 18:13:52 -0800

    Quote paths to build script

--- build/cibuild.cmd
+++ build/cibuild.cmd
@@ -1,3 +1,3 @@
 @echo off
-powershell -ExecutionPolicy ByPass %~dp0build.ps1 -build -pack -sign -ci -prepareMachine %*
+powershell -ExecutionPolicy ByPass "%~dp0build.ps1" -build -pack -sign -ci -prepareMachine %*
 exit /b %ErrorLevel%
