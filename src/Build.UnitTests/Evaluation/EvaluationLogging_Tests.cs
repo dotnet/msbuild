dotnet/msbuild

commit 2567cc4dcd27ba12e0e359e71084b085c4d7b473

Fix evaluation logging for multi-proc builds (#2748)
* Fix evaluation logging

src/Build.UnitTests/BackEnd/MockLoggingService.cs
@@ -423,6 +423,21 @@ namespace Microsoft.Build.UnitTests.BackEnd
         }
 
         /// <summary>
+        /// Logs a project evaluation started event
+        /// </summary>
+        public BuildEventContext LogProjectEvaluationStarted(int nodeId, int submissionId, string projectFile)
+        {
+            return new BuildEventContext(0, 0, 0, 0, 0, 0, 0);
+        }
+
+        /// <summary>
+        /// Logs a project evaluation finished event
+        /// </summary>
+        public void LogProjectEvaluationFinished(BuildEventContext projectEvaluationEventContext, string projectFile)
+        {
+        }
+
+        /// <summary>
         /// Logs a project started event
         /// </summary>
         public BuildEventContext LogProjectStarted(BuildEventContext nodeBuildEventContext, int submissionId, int projectId, BuildEventContext parentBuildEventContext, string projectFile, string targetNames, IEnumerable<DictionaryEntry> properties, IEnumerable<DictionaryEntry> items)

src/Build/BackEnd/Components/Logging/EvaluationLoggingContext.cs
@@ -5,6 +5,7 @@
 
 using Microsoft.Build.BackEnd.Logging;
 using Microsoft.Build.Framework;
+using Microsoft.Build.Shared;
 
 namespace Microsoft.Build.BackEnd.Components.Logging
 {
@@ -13,19 +14,25 @@ namespace Microsoft.Build.BackEnd.Components.Logging
     /// </summary>
     internal class EvaluationLoggingContext : LoggingContext
     {
-        public EvaluationLoggingContext(ILoggingService loggingService, BuildEventContext eventContext, int evaluationId) : base(
-            loggingService,
-            new BuildEventContext(
-                eventContext.SubmissionId,
-                eventContext.NodeId,
-                evaluationId,
-                eventContext.ProjectInstanceId,
-                eventContext.ProjectContextId,
-                eventContext.TargetId,
-                eventContext.TaskId
-            ))
+        private readonly string _projectFile;
+
+        public EvaluationLoggingContext(ILoggingService loggingService, BuildEventContext buildEventContext, string projectFile) :
+            base(
+                loggingService,
+                loggingService.LogProjectEvaluationStarted(buildEventContext.NodeId, buildEventContext.SubmissionId, projectFile))
         {
+            _projectFile = projectFile;
             IsValid = true;
+        }
+
+        /// <summary>
+        /// Log that the project has finished
+        /// </summary>
+        internal void LogProjectEvaluationFinished()
+        {
+            ErrorUtilities.VerifyThrow(IsValid, "invalid");
+            LoggingService.LogProjectEvaluationFinished(BuildEventContext, _projectFile);
+            IsValid = false;
         }
     }
 }

src/Build/BackEnd/Components/Logging/ILoggingService.cs
@@ -368,6 +368,23 @@ namespace Microsoft.Build.BackEnd.Logging
         void LogBuildFinished(bool success);
 
         /// <summary>
+        /// Logs that a project evaluation has started
+        /// </summary>
+        /// <param name="nodeId">The id of the node which is evaluating this project.</param>
+        /// <param name="submissionId">The id of the submission.</param>
+        /// <param name="projectFile">Project file to build</param>
+        /// <returns>The evaluation event context for the project.</returns>
+        BuildEventContext LogProjectEvaluationStarted(int nodeId, int submissionId, string projectFile);
+
+        /// <summary>
+        /// Logs that a project evaluation has finished
+        /// </summary>
+        /// <param name="projectEvaluationEventContext">Event context for the project.</param>
+        /// <param name="projectFile">Project file being built</param>
+        /// <exception cref="InternalErrorException">BuildEventContext is null</exception>
+        void LogProjectEvaluationFinished(BuildEventContext projectEvaluationEventContext, string projectFile);
+
+        /// <summary>
         /// Log that a project has started
         /// </summary>
         /// <param name="nodeBuildEventContext">The logging context of the node which is building this project.</param>

src/Build/BackEnd/Components/Logging/LoggingServiceLogMethods.cs
@@ -495,6 +495,55 @@ namespace Microsoft.Build.BackEnd.Logging
         }
 
         /// <summary>
+        /// Logs that a project evaluation has started
+        /// </summary>
+        /// <param name="nodeId">The id of the node which is evaluating this project.</param>
+        /// <param name="submissionId">The id of the submission.</param>
+        /// <param name="projectFile">Project file to build</param>
+        /// <returns>The evaluation event context for the project.</returns>
+        public BuildEventContext LogProjectEvaluationStarted(int nodeId, int submissionId, string projectFile)
+        {
+            lock (_lockObject)
+            {
+                BuildEventContext projectEvaluationEventContext = new BuildEventContext(submissionId, nodeId, NextEvaluationId, BuildEventContext.InvalidProjectInstanceId, BuildEventContext.InvalidProjectContextId, BuildEventContext.InvalidTargetId, BuildEventContext.InvalidTaskId);
+
+                ProjectEvaluationStartedEventArgs evaluationEvent =
+                    new ProjectEvaluationStartedEventArgs(ResourceUtilities.GetResourceString("EvaluationStarted"),
+                        projectFile)
+                    {
+                        BuildEventContext = projectEvaluationEventContext,
+                        ProjectFile = projectFile
+                    };
+
+                ProcessLoggingEvent(evaluationEvent);
+
+                return projectEvaluationEventContext;
+            }
+        }
+
+        /// <summary>
+        /// Logs that a project evaluation has finished
+        /// </summary>
+        /// <param name="projectEvaluationEventContext">Event context for the project.</param>
+        /// <param name="projectFile">Project file being built</param>
+        /// <exception cref="InternalErrorException">BuildEventContext is null</exception>
+        public void LogProjectEvaluationFinished(BuildEventContext projectEvaluationEventContext, string projectFile)
+        {
+            lock (_lockObject)
+            {
+                ErrorUtilities.VerifyThrow(projectEvaluationEventContext != null, "projectBuildEventContext");
+
+                ProjectEvaluationFinishedEventArgs buildEvent =
+                    new ProjectEvaluationFinishedEventArgs(ResourceUtilities.GetResourceString("EvaluationFinished"), projectFile)
+                    {
+                        BuildEventContext = projectEvaluationEventContext,
+                        ProjectFile = projectFile
+                    };
+                ProcessLoggingEvent(buildEvent);
+            }
+        }
+
+        /// <summary>
         /// Logs that a project build has started
         /// </summary>
         /// <param name="nodeBuildEventContext">The event context of the node which is spawning this project.</param>

src/Build/Evaluation/Evaluator.cs
@@ -90,11 +90,6 @@ namespace Microsoft.Build.Evaluation
         };
 
         /// <summary>
-        /// Each evaluation has a unique ID.
-        /// </summary>
-        private static int s_evaluationId = BuildEventContext.InvalidEvaluationId;
-
-        /// <summary>
         /// Expander for evaluating conditions
         /// </summary>
         private readonly Expander<P, I> _expander;
@@ -734,9 +729,6 @@ namespace Microsoft.Build.Evaluation
             {
                 ErrorUtilities.VerifyThrow(_data.EvaluationId == BuildEventContext.InvalidEvaluationId, "There is no prior evaluation ID. The evaluator data needs to be reset at this point");
 
-                _data.EvaluationId = NextEvaluationId();
-                _evaluationLoggingContext = new EvaluationLoggingContext(loggingService, buildEventContext, _data.EvaluationId);
-
                 _logProjectImportedEvents = Traits.Instance.EscapeHatches.LogProjectImports;
 
 #if FEATURE_MSBUILD_DEBUGGER
@@ -799,11 +791,9 @@ namespace Microsoft.Build.Evaluation
 #endif
                 projectFile = String.IsNullOrEmpty(_projectRootElement.ProjectFileLocation.File) ? "(null)" : _projectRootElement.ProjectFileLocation.File;
 
-                _evaluationLoggingContext.LogBuildEvent(new ProjectEvaluationStartedEventArgs(ResourceUtilities.GetResourceString("EvaluationStarted"), projectFile)
-                {
-                    BuildEventContext = _evaluationLoggingContext.BuildEventContext,
-                    ProjectFile = projectFile
-                });
+                _evaluationLoggingContext = new EvaluationLoggingContext(loggingService, buildEventContext, projectFile);
+                _data.EvaluationId = _evaluationLoggingContext.BuildEventContext.EvaluationId;
+                ErrorUtilities.VerifyThrow(_data.EvaluationId != BuildEventContext.InvalidEvaluationId, "Evaluation should produce an evaluation ID");
 
 #if MSBUILDENABLEVSPROFILING
         string endPass0 = String.Format(CultureInfo.CurrentCulture, "Evaluate Project {0} - End Pass 0 (Initial properties)", projectFile);
@@ -3061,8 +3051,6 @@ namespace Microsoft.Build.Evaluation
 
             return sb.ToString();
         }
-
-        private static int NextEvaluationId() => Interlocked.Increment(ref s_evaluationId);
     }
 
     /// <summary>

src/Build.UnitTests/BackEnd/BuildManager_Tests.cs
@@ -3564,5 +3564,70 @@ namespace Microsoft.Build.UnitTests.BackEnd
                 _buildManager.EndBuild();
             }
         }
+
+        [Fact]
+        [Trait("Category", "mono-osx-failing")] // out-of-proc nodes not working on mono yet
+        public void OutOfProcEvaluationIdsUnique()
+        {
+            var mainProject =
+                @"<Project>
+
+  <Target Name=`MainTarget`>
+    <MSBuild Projects=`{0};{1}` Targets=`DummyTarget` />
+  </Target>
+
+</Project>";
+
+            var childProject =
+                @"<Project>
+
+  <Target Name=`DummyTarget`>
+    <Message Text=`Bar` Importance=`High` />
+  </Target>
+
+</Project>";
+
+            var testFiles = _env.CreateTestProjectWithFiles(string.Empty, new[] { "main", "child1", "child2" }, string.Empty);
+
+            var buildParameters = new BuildParameters(_projectCollection)
+            {
+                DisableInProcNode = true,
+                Loggers = new ILogger[] { _logger }
+            };
+
+            _buildManager.BeginBuild(buildParameters);
+
+            try
+            {
+                var child1ProjectPath = testFiles.CreatedFiles[1];
+                var child2ProjectPath = testFiles.CreatedFiles[2];
+                var cleanedUpChildContents = CleanupFileContents(childProject);
+                File.WriteAllText(child1ProjectPath, cleanedUpChildContents);
+                File.WriteAllText(child2ProjectPath, cleanedUpChildContents);
+
+                var mainProjectPath = testFiles.CreatedFiles[0];
+                var cleanedUpMainContents = CleanupFileContents(string.Format(mainProject, child1ProjectPath, child2ProjectPath));
+                File.WriteAllText(mainProjectPath, cleanedUpMainContents);
+
+                var buildRequestData = new BuildRequestData(
+                    mainProjectPath,
+                    new Dictionary<string, string>(),
+                    MSBuildConstants.CurrentToolsVersion,
+                    new[] { "MainTarget" },
+                    null
+                );
+
+                var submission = _buildManager.PendBuildRequest(buildRequestData);
+
+                var result = submission.Execute();
+
+                Assert.Equal(BuildResultCode.Success, result.OverallResult);
+                Assert.True(_logger.AllBuildEvents.OfType<ProjectEvaluationStartedEventArgs>().GroupBy(args => args.BuildEventContext.EvaluationId).All(g => g.Count() == 1));
+            }
+            finally
+            {
+                _buildManager.EndBuild();
+            }
+        }
     }
 }

src/Build/BackEnd/Components/Logging/LoggingService.cs
@@ -172,6 +172,11 @@ namespace Microsoft.Build.BackEnd.Logging
         private Lazy<IConfigCache> _configCache;
 
         /// <summary>
+        /// The next project ID to assign when a project evaluation started event is received.
+        /// </summary>
+        private int _nextEvaluationId = 1;
+
+        /// <summary>
         /// The next project ID to assign when a project started event is received.
         /// </summary>
         private int _nextProjectId = 1;
@@ -253,6 +258,7 @@ namespace Microsoft.Build.BackEnd.Logging
 
             // Start the project context id count at the nodeId
             _nextProjectId = nodeId;
+            _nextEvaluationId = nodeId;
 
             string queueCapacityEnvironment = Environment.GetEnvironmentVariable("MSBUILDLOGGINGQUEUECAPACITY");
             if (!String.IsNullOrEmpty(queueCapacityEnvironment))
@@ -325,6 +331,22 @@ namespace Microsoft.Build.BackEnd.Logging
         {
             get;
             set;
+        }
+
+        /// <summary>
+        /// Gets the next project evaluation id.
+        /// </summary>
+        /// <remarks>This property is thread-safe</remarks>
+        public int NextEvaluationId
+        {
+            get
+            {
+                lock (_lockObject)
+                {
+                    _nextEvaluationId += MaxCPUCount + 2 /* We can create one node more than the maxCPU count (this can happen if either the inproc or out of proc node has not been created yet and the project collection needs to be counted also)*/;
+                    return _nextEvaluationId;
+                }
+            }
         }
 
         /// <summary>

commit f09e0a1cf95b2cfe38c953e61b7443cfa8040b69

Project.LastEvaluationId should be valid during logging
CPS expects Project.LastEvaluationId to reflect the id of the ongoing, not yet finished evaluation.
This invariant regressed in #2748

LoggingService now creates an evaluation build event context separate from sending project started events. This allows the Evaluator to set the evaluation Id on Project before the first evaluation event gets fired.

src/Build.UnitTests/BackEnd/MockLoggingService.cs
@@ -422,12 +422,16 @@ namespace Microsoft.Build.UnitTests.BackEnd
         {
         }
 
-        /// <summary>
-        /// Logs a project evaluation started event
-        /// </summary>
-        public BuildEventContext LogProjectEvaluationStarted(int nodeId, int submissionId, string projectFile)
+
+        /// <inheritdoc />
+        public BuildEventContext CreateEvaluationBuildEventContext(int nodeId, int submissionId)
         {
             return new BuildEventContext(0, 0, 0, 0, 0, 0, 0);
+        }
+
+        /// <inheritdoc />
+        public void LogProjectEvaluationStarted(BuildEventContext eventContext, string projectFile)
+        {
         }
 
         /// <summary>

src/Build/BackEnd/Components/Logging/EvaluationLoggingContext.cs
@@ -19,10 +19,15 @@ namespace Microsoft.Build.BackEnd.Components.Logging
         public EvaluationLoggingContext(ILoggingService loggingService, BuildEventContext buildEventContext, string projectFile) :
             base(
                 loggingService,
-                loggingService.LogProjectEvaluationStarted(buildEventContext.NodeId, buildEventContext.SubmissionId, projectFile))
+                loggingService.CreateEvaluationBuildEventContext(buildEventContext.NodeId, buildEventContext.SubmissionId))
         {
             _projectFile = projectFile;
             IsValid = true;
+        }
+
+        public void LogProjectEvaluationStarted()
+        {
+            LoggingService.LogProjectEvaluationStarted(BuildEventContext, _projectFile);
         }
 
         /// <summary>

src/Build/BackEnd/Components/Logging/ILoggingService.cs
@@ -7,6 +7,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Microsoft.Build.BackEnd.Components.Logging;
 using Microsoft.Build.Framework;
 using Microsoft.Build.Shared;
 
@@ -368,13 +369,20 @@ namespace Microsoft.Build.BackEnd.Logging
         void LogBuildFinished(bool success);
 
         /// <summary>
+        /// Create an evaluation context, by generating a new evaluation id.
+        /// </summary>
+        /// <param name="nodeId">The node id</param>
+        /// <param name="submissionId">The submission id</param>
+        /// <returns></returns>
+        BuildEventContext CreateEvaluationBuildEventContext(int nodeId, int submissionId);
+
+        /// <summary>
         /// Logs that a project evaluation has started
         /// </summary>
-        /// <param name="nodeId">The id of the node which is evaluating this project.</param>
-        /// <param name="submissionId">The id of the submission.</param>
-        /// <param name="projectFile">Project file to build</param>
+        /// <param name="eventContext">The event context to use for logging</param>
+        /// <param name="projectFile">Project file being built</param>
         /// <returns>The evaluation event context for the project.</returns>
-        BuildEventContext LogProjectEvaluationStarted(int nodeId, int submissionId, string projectFile);
+        void LogProjectEvaluationStarted(BuildEventContext eventContext, string projectFile);
 
         /// <summary>
         /// Logs that a project evaluation has finished

src/Build/BackEnd/Components/Logging/LoggingServiceLogMethods.cs
@@ -13,6 +13,7 @@ using System.Reflection;
 using System.Text;
 using System.Threading;
 using Microsoft.Build.BackEnd;
+using Microsoft.Build.BackEnd.Components.Logging;
 using Microsoft.Build.Collections;
 using Microsoft.Build.Framework;
 using Microsoft.Build.Shared;
@@ -494,19 +495,17 @@ namespace Microsoft.Build.BackEnd.Logging
             }
         }
 
-        /// <summary>
-        /// Logs that a project evaluation has started
-        /// </summary>
-        /// <param name="nodeId">The id of the node which is evaluating this project.</param>
-        /// <param name="submissionId">The id of the submission.</param>
-        /// <param name="projectFile">Project file to build</param>
-        /// <returns>The evaluation event context for the project.</returns>
-        public BuildEventContext LogProjectEvaluationStarted(int nodeId, int submissionId, string projectFile)
+        /// <inheritdoc />
+        public BuildEventContext CreateEvaluationBuildEventContext(int nodeId, int submissionId)
+        {
+            return new BuildEventContext(submissionId, nodeId, NextEvaluationId, BuildEventContext.InvalidProjectInstanceId, BuildEventContext.InvalidProjectContextId, BuildEventContext.InvalidTargetId, BuildEventContext.InvalidTaskId);
+        }
+
+        /// <inheritdoc />
+        public void LogProjectEvaluationStarted(BuildEventContext projectEvaluationEventContext, string projectFile)
         {
             lock (_lockObject)
             {
-                BuildEventContext projectEvaluationEventContext = new BuildEventContext(submissionId, nodeId, NextEvaluationId, BuildEventContext.InvalidProjectInstanceId, BuildEventContext.InvalidProjectContextId, BuildEventContext.InvalidTargetId, BuildEventContext.InvalidTaskId);
-
                 ProjectEvaluationStartedEventArgs evaluationEvent =
                     new ProjectEvaluationStartedEventArgs(ResourceUtilities.GetResourceString("EvaluationStarted"),
                         projectFile)
@@ -516,8 +515,6 @@ namespace Microsoft.Build.BackEnd.Logging
                     };
 
                 ProcessLoggingEvent(evaluationEvent);
-
-                return projectEvaluationEventContext;
             }
         }
 

src/Build/Evaluation/Evaluator.cs
@@ -803,6 +803,9 @@ namespace Microsoft.Build.Evaluation
 
                 _evaluationLoggingContext = new EvaluationLoggingContext(loggingService, buildEventContext, projectFile);
                 _data.EvaluationId = _evaluationLoggingContext.BuildEventContext.EvaluationId;
+
+                _evaluationLoggingContext.LogProjectEvaluationStarted();
+
                 ErrorUtilities.VerifyThrow(_data.EvaluationId != BuildEventContext.InvalidEvaluationId, "Evaluation should produce an evaluation ID");
 
 #if MSBUILDENABLEVSPROFILING

src/Build.UnitTests/Evaluation/EvaluationLogging_Tests.cs
@@ -6,11 +6,13 @@
 //-----------------------------------------------------------------------
 
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
-using System.Linq;
 using Microsoft.Build.Engine.UnitTests;
 using Microsoft.Build.Evaluation;
 using Microsoft.Build.Framework;
+using Shouldly;
 using Xunit;
 
 namespace Microsoft.Build.UnitTests.Evaluation
@@ -38,7 +40,10 @@ namespace Microsoft.Build.UnitTests.Evaluation
             GC.Collect();
         }
 
-        private static void AssertLoggingEvents(Action<Project, MockLogger> loggingTest)
+        private static void AssertLoggingEvents(
+            Action<Project, MockLogger> loggingTest = null,
+            MockLogger firstEvaluationLogger = null,
+            Func<Project, MockLogger> reevaluationLoggerFactory = null)
         {
             var projectImportContents =
                 @"<Project>
@@ -76,18 +81,28 @@ namespace Microsoft.Build.UnitTests.Evaluation
 
                 projectContents = string.Format(projectContents, importFile);
 
-
                 var projectFile = env.CreateFile().Path;
                 File.WriteAllText(projectFile, projectContents);
 
-                var logger = new MockLogger();
-                collection.RegisterLogger(logger);
+                firstEvaluationLogger = firstEvaluationLogger ?? new MockLogger();
+                collection.RegisterLogger(firstEvaluationLogger);
 
                 var project = new Project(projectFile, null, null, collection);
 
-                Assert.NotEmpty(logger.AllBuildEvents);
+                firstEvaluationLogger.AllBuildEvents.ShouldNotBeEmpty();
 
-                loggingTest.Invoke(project, logger);
+                if (reevaluationLoggerFactory != null)
+                {
+                    var reevaluationLogger = reevaluationLoggerFactory.Invoke(project);
+                    collection.RegisterLogger(reevaluationLogger);
+
+                    project.SetProperty("aProperty", "Value");
+                    project.ReevaluateIfNecessary();
+
+                    reevaluationLogger.AllBuildEvents.ShouldNotBeEmpty();
+                }
+
+                loggingTest?.Invoke(project, firstEvaluationLogger);
             }
         }
 
@@ -95,29 +110,67 @@ namespace Microsoft.Build.UnitTests.Evaluation
         public void AllEvaluationEventsShouldHaveAnEvaluationId()
         {
             AssertLoggingEvents(
-                (project, mockLogger) =>
+                (project, firstEvaluationLogger) =>
                 {
                     var evaluationId = project.LastEvaluationId;
+                    evaluationId.ShouldNotBe(BuildEventContext.InvalidEvaluationId);
 
-                    Assert.NotEqual(BuildEventContext.InvalidEvaluationId, evaluationId);
-
-                    foreach (var buildEvent in mockLogger.AllBuildEvents)
+                    foreach (var buildEvent in firstEvaluationLogger.AllBuildEvents)
                     {
-                        Assert.Equal(evaluationId, buildEvent.BuildEventContext.EvaluationId);
+                        buildEvent.BuildEventContext.EvaluationId.ShouldBe(evaluationId);
                     }
                 });
         }
 
         [Fact]
-        public void FirstAndLastEvaluationEventsShouldBeStartedAndEnded()
+        public void GivenOneProjectThereShouldBeOneStartedAndOneEndedEvent()
         {
             AssertLoggingEvents(
-                (project, mockLogger) =>
+                (project, firstEvaluationLogger) =>
                 {
-                    Assert.True(mockLogger.AllBuildEvents.Count >= 2);
+                    var allBuildEvents = firstEvaluationLogger.AllBuildEvents;
 
-                    Assert.StartsWith("Evaluation started", mockLogger.AllBuildEvents.First().Message);
-                    Assert.StartsWith("Evaluation finished", mockLogger.AllBuildEvents.Last().Message);
+                    allBuildEvents.Count.ShouldBeGreaterThan(2);
+
+                    for (var i = 0; i < allBuildEvents.Count; i++)
+                    {
+                        var buildEvent = allBuildEvents[i];
+
+                        if (i == 0)
+                        {
+                            buildEvent.Message.ShouldStartWith("Evaluation started");
+                        }
+                        else if (i == allBuildEvents.Count - 1)
+                        {
+                            buildEvent.Message.ShouldStartWith("Evaluation finished");
+                        }
+                        else
+                        {
+                            buildEvent.Message.ShouldNotStartWith("Evaluation started");
+                            buildEvent.Message.ShouldNotStartWith("Evaluation finished");
+                        }
+                    }
+                });
+        }
+
+        [Fact]
+        public void ProjectShouldHaveValidEvaluationIdDuringEvaluation()
+        {
+            AssertLoggingEvents(
+                null,
+                null,
+                project => new MockLogger
+                {
+                    AdditionalHandlers = new List<Action<object, BuildEventArgs>>
+                    {
+                        (sender, args) =>
+                        {
+                            var eventEvaluationId = args.BuildEventContext.EvaluationId;
+
+                            eventEvaluationId.ShouldNotBe(BuildEventContext.InvalidEvaluationId);
+                            project.LastEvaluationId.ShouldBe(eventEvaluationId);
+                        }
+                    }
                 });
         }
     }

src/Shared/UnitTests/MockLogger.cs
@@ -185,8 +185,7 @@ namespace Microsoft.Build.UnitTests
          */
         public void Initialize(IEventSource eventSource)
         {
-            eventSource.AnyEventRaised +=
-                    new AnyEventHandler(LoggerEventHandler);
+            eventSource.AnyEventRaised += LoggerEventHandler;
         }
 
         /// <summary>
@@ -219,6 +218,8 @@ namespace Microsoft.Build.UnitTests
             _testOutputHelper = testOutputHelper;
         }
 
+        public List<Action<object, BuildEventArgs>> AdditionalHandlers { get; set; } = new List<Action<object, BuildEventArgs>>();
+
         /*
          * Method:  LoggerEventHandler
          *
@@ -228,6 +229,11 @@ namespace Microsoft.Build.UnitTests
         internal void LoggerEventHandler(object sender, BuildEventArgs eventArgs)
         {
             AllBuildEvents.Add(eventArgs);
+
+            foreach (var handler in AdditionalHandlers)
+            {
+                handler(sender, eventArgs);
+            }
 
             if (eventArgs is BuildWarningEventArgs)
             {