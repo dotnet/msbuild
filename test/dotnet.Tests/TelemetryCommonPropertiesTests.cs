aa90d8efd2ee6fc21c1743df4ffc53bdb7e22b24
danmosemsft <danmose@microsoft.com>
2018-02-09 17:20:50 -0800

    Port Kernel Version telemetry to preview1

--- test/dotnet.Tests/TelemetryCommonPropertiesTests.cs
+++ test/dotnet.Tests/TelemetryCommonPropertiesTests.cs
@@ -5,9 +5,11 @@ using FluentAssertions;
 using Microsoft.DotNet.Tools.Test.Utilities;
 using Xunit;
 using System;
+using System.Runtime.InteropServices;
 using Microsoft.DotNet.Cli;
 using Microsoft.DotNet.Cli.Telemetry;
 using Microsoft.DotNet.Configurer;
+using RuntimeEnvironment = Microsoft.DotNet.PlatformAbstractions.RuntimeEnvironment;
 
 namespace Microsoft.DotNet.Tests
 {
@@ -41,6 +43,13 @@ namespace Microsoft.DotNet.Tests
             var assignedMachineId = unitUnderTest.GetTelemetryCommonProperties()["Machine ID"];
 
             Guid.TryParse(assignedMachineId, out var _).Should().BeTrue("it should be a guid");
+        }
+
+        [Fact]
+        public void TelemetryCommonPropertiesShouldContainKernelVersion()
+        {
+            var unitUnderTest = new TelemetryCommonProperties(getMACAddress: () => null, userLevelCacheWriter: new NothingCache());
+            unitUnderTest.GetTelemetryCommonProperties()["Kernel Version"].Should().Be(RuntimeInformation.OSDescription);
         }
 
         private class NothingCache : IUserLevelCacheWriter
