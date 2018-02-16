08f050c012cb80bb9433cd401d9be866307e4687
William Lee <wul@microsoft.com>
2018-02-16 13:32:29 -0800

    bundled DotnetTool (#8606)
    Extract packages to DotnetTools folder under sdk/{version}
    
    Add new resolver to discover it
    
    Add test to enforce package structure. It will fail when the structure
    changed

---
+++ test/EndToEnd/GivenDotnetUsesDotnetTools.cs
@@ -0,0 +1,17 @@
+﻿using FluentAssertions;
+using Microsoft.DotNet.Tools.Test.Utilities;
+using Xunit;
+
+namespace EndToEnd
+{
+    public class GivenDotnetUsesDotnetTools : TestBase
+    {
+        [Fact]
+        public void ThenOneDotnetToolsCanBeCalled()
+        {
+            new DotnetCommand()
+                .ExecuteWithCapturedOutput("dev-certs --help")
+                    .Should().Pass();
+        }
+    }
+}
