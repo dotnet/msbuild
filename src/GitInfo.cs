5e1cfb2e3808f656a41a9a0acf225b25da47a69e
vramak <vramak@microsoft.com>
2018-02-15 16:40:36 -0800

    Checking in a version of GitInfo.cs

---
+++ src/GitInfo.cs
@@ -0,0 +1,9 @@
+namespace Microsoft.NET.Sdk.Web
+{
+    internal static class GitInfo
+    {
+        public static string PackageVersion { get; } = "";
+        public static string CommitCount { get; } = "";
+        public static string CommitHash { get; } = "";
+    }
+}
