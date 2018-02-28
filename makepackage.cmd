49b7fc80473aa28c8e36dc67c81ef7f11853c249
Rainer Sigwald <raines@microsoft.com>
2018-02-28 17:23:56 -0600

    small script for packaging

--- makepackage.cmd
+++ makepackage.cmd
@@ -1,3 +1,6 @@
+set RepoRoot=%~dp0
+set RepoRoot=%RepoRoot:~0,-1%
+
 call cibuild.cmd --scope build --config Release --bootstrap-only
 
-nuget.exe pack setup\MsBuild.Engine.Corext\MsBuild.Engine.Corext.nuspec -NonInteractive -OutputDirectory bin\Setup -Properties version=%%1;repoRoot=%~dp0;X86BinPath=bin\Release\x86\Windows_NT\Output;X64BinPath=bin\Release\x64\Windows_NT\Output -Verbosity Detailed
+nuget.exe pack setup\MsBuild.Engine.Corext\MsBuild.Engine.Corext.nuspec -NonInteractive -OutputDirectory bin\Setup -Properties version=%1;repoRoot=%RepoRoot%;X86BinPath=%~dp0bin\Release\x86\Windows_NT\Output;X64BinPath=%~dp0bin\Release\x64\Windows_NT\Output -Verbosity Detailed
