b3731e8c43e5b250dd03e982b35204b203d93bba
Rainer Sigwald <raines@microsoft.com>
2018-02-28 18:13:22 -0600

    fix bootstrap

--- makepackage.cmd
+++ makepackage.cmd
@@ -3,4 +3,6 @@ set RepoRoot=%RepoRoot:~0,-1%
 
 call cibuild.cmd --scope build --config Release --bootstrap-only
 
+rd /s /q bin\Bootstrap\MSBuild\15.0\Bin\SdkResolvers\NuGet.MSBuildSdkResolver
+
 nuget.exe pack setup\MsBuild.Engine.Corext\MsBuild.Engine.Corext.nuspec -NonInteractive -OutputDirectory bin\Setup -Properties version=%1;repoRoot=%RepoRoot%;X86BinPath=%~dp0bin\Release\x86\Windows_NT\Output;X64BinPath=%~dp0bin\Release\x64\Windows_NT\Output -Verbosity Detailed
