dotnet/msbuild

commit 9cc2b7cd2f9dc165ee3a04b1de6fa5065b142dd3

Change --source to --source-feed and make it additional (#8833)
Other than change source to source-feed and make it additional instead of exclusive. I changed source to be multiple. Because restore support multiple source https://github.com/Microsoft/dotnet/issues/361

As for mock. The offline feed and source feed is considered the same, so remove the category of “source”. I renamed source to “AdditionalFeed” because that is more accurate on implementation level.

Note:
NuGet feed don’t have order. Whichever responses the fastest, is the first.
No change on restore.

scripts/cli-test-env.sh change is due to mac 10.13 is finally added to RID graph. And it is “considered” one of the CLI supported RID

src/dotnet/ToolPackage/ToolPackageInstaller.cs
@@ -30,13 +30,12 @@ namespace Microsoft.DotNet.ToolPackage
             _offlineFeed = offlineFeed ?? new DirectoryPath(new CliFolderPathCalculator().CliFallbackFolderPath);
         }
 
-        public IToolPackage InstallPackage(
-            PackageId packageId,
+        public IToolPackage InstallPackage(PackageId packageId,
             VersionRange versionRange = null,
             string targetFramework = null,
             FilePath? nugetConfig = null,
             DirectoryPath? rootConfigDirectory = null,
-            string source = null,
+            string[] additionalFeeds = null,
             string verbosity = null)
         {
             var packageRootDirectory = _store.GetRootPackageDirectory(packageId);
@@ -55,7 +54,8 @@ namespace Microsoft.DotNet.ToolPackage
                             versionRange: versionRange,
                             targetFramework: targetFramework ?? BundledTargetFramework.GetTargetFrameworkMoniker(),
                             restoreDirectory: stageDirectory,
-                            rootConfigDirectory: rootConfigDirectory);
+                            rootConfigDirectory: rootConfigDirectory,
+                            additionalFeeds: additionalFeeds);
 
                         try
                         {
@@ -63,8 +63,7 @@ namespace Microsoft.DotNet.ToolPackage
                                 tempProject,
                                 stageDirectory,
                                 nugetConfig,
-                                source,
-                                verbosity);
+                                verbosity: verbosity);
                         }
                         finally
                         {
@@ -113,12 +112,12 @@ namespace Microsoft.DotNet.ToolPackage
                 });
         }
 
-        private FilePath CreateTempProject(
-            PackageId packageId,
+        private FilePath CreateTempProject(PackageId packageId,
             VersionRange versionRange,
             string targetFramework,
             DirectoryPath restoreDirectory,
-            DirectoryPath? rootConfigDirectory)
+            DirectoryPath? rootConfigDirectory,
+            string[] additionalFeeds)
         {
             var tempProject = _tempProject ?? new DirectoryPath(Path.GetTempPath())
                 .WithSubDirectories(Path.GetRandomFileName())
@@ -141,8 +140,7 @@ namespace Microsoft.DotNet.ToolPackage
                         new XElement("RestoreRootConfigDirectory", rootConfigDirectory?.Value ?? Directory.GetCurrentDirectory()), // config file probing start directory
                         new XElement("DisableImplicitFrameworkReferences", "true"), // no Microsoft.NETCore.App in tool folder
                         new XElement("RestoreFallbackFolders", "clear"), // do not use fallbackfolder, tool package need to be copied to tool folder
-                        new XElement("RestoreAdditionalProjectSources", // use fallbackfolder as feed to enable offline
-                            Directory.Exists(_offlineFeed.Value) ? _offlineFeed.Value : string.Empty),
+                        new XElement("RestoreAdditionalProjectSources", JoinSourceAndOfflineCache(additionalFeeds)),
                         new XElement("RestoreAdditionalProjectFallbackFolders", string.Empty), // block other
                         new XElement("RestoreAdditionalProjectFallbackFoldersExcludes", string.Empty),  // block other
                         new XElement("DisableImplicitNuGetFallbackFolder", "true")),  // disable SDK side implicit NuGetFallbackFolder
@@ -156,6 +154,23 @@ namespace Microsoft.DotNet.ToolPackage
 
             File.WriteAllText(tempProject.Value, tempProjectContent.ToString());
             return tempProject;
+        }
+
+        private string JoinSourceAndOfflineCache(string[] additionalFeeds)
+        {
+            var feeds = new List<string>();
+            if (additionalFeeds != null)
+            {
+                feeds.AddRange(additionalFeeds);
+            }
+
+            // use fallbackfolder as feed to enable offline
+            if (Directory.Exists(_offlineFeed.Value))
+            {
+                feeds.Add(_offlineFeed.ToXmlEncodeString());
+            }
+
+            return string.Join(";", feeds);
         }
     }
 }

test/Microsoft.DotNet.ToolPackage.Tests/ToolPackageInstallerTests.cs
@@ -46,7 +46,7 @@ namespace Microsoft.DotNet.ToolPackage.Tests
         [Theory]
         [InlineData(false)]
         [InlineData(true)]
-        public void GivenOfflineFeedInstallSuceeds(bool testMockBehaviorIsInSync)
+        public void GivenOfflineFeedInstallSucceeds(bool testMockBehaviorIsInSync)
         {
             var (store, installer, reporter, fileSystem) = Setup(
                 useMock: testMockBehaviorIsInSync,
@@ -57,6 +57,30 @@ namespace Microsoft.DotNet.ToolPackage.Tests
                 packageId: TestPackageId,
                 versionRange: VersionRange.Parse(TestPackageVersion),
                 targetFramework: _testTargetframework);
+
+            AssertPackageInstall(reporter, fileSystem, package, store);
+
+            package.Uninstall();
+        }
+
+        [Theory]
+        [InlineData(false)]
+        [InlineData(true)]
+        public void GivenAEmptySourceAndOfflineFeedInstallSucceeds(bool testMockBehaviorIsInSync)
+        {
+            var emptySource = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+            Directory.CreateDirectory(emptySource);
+
+            var (store, installer, reporter, fileSystem) = Setup(
+                useMock: testMockBehaviorIsInSync,
+                offlineFeed: new DirectoryPath(GetTestLocalFeedPath()),
+                feeds: GetOfflineMockFeed());
+
+            var package = installer.InstallPackage(
+                packageId: TestPackageId,
+                versionRange: VersionRange.Parse(TestPackageVersion),
+                targetFramework: _testTargetframework,
+                additionalFeeds: new[] {emptySource});
 
             AssertPackageInstall(reporter, fileSystem, package, store);
 
@@ -228,7 +252,31 @@ namespace Microsoft.DotNet.ToolPackage.Tests
                 packageId: TestPackageId,
                 versionRange: VersionRange.Parse(TestPackageVersion),
                 targetFramework: _testTargetframework,
-                source: source);
+                additionalFeeds: new[] {source});
+
+            AssertPackageInstall(reporter, fileSystem, package, store);
+
+            package.Uninstall();
+        }
+
+        [Theory]
+        [InlineData(false)]
+        [InlineData(true)]
+        public void GivenAEmptySourceAndNugetConfigInstallSucceeds(bool testMockBehaviorIsInSync)
+        {
+            var nugetConfigPath = WriteNugetConfigFileToPointToTheFeed();
+            var emptySource = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+            Directory.CreateDirectory(emptySource);
+
+            var (store, installer, reporter, fileSystem) = Setup(
+                useMock: testMockBehaviorIsInSync,
+                feeds: GetMockFeedsForSource(emptySource));
+
+            var package = installer.InstallPackage(
+                packageId: TestPackageId,
+                versionRange: VersionRange.Parse(TestPackageVersion),
+                targetFramework: _testTargetframework,
+                nugetConfig: nugetConfigPath, additionalFeeds: new[] {emptySource});
 
             AssertPackageInstall(reporter, fileSystem, package, store);
 
@@ -281,7 +329,7 @@ namespace Microsoft.DotNet.ToolPackage.Tests
                         packageId: TestPackageId,
                         versionRange: VersionRange.Parse(TestPackageVersion),
                         targetFramework: _testTargetframework,
-                        source: source);
+                        additionalFeeds: new[] {source});
 
                     FailedStepAfterSuccessRestore();
                     t.Complete();
@@ -313,7 +361,7 @@ namespace Microsoft.DotNet.ToolPackage.Tests
                         packageId: TestPackageId,
                         versionRange: VersionRange.Parse(TestPackageVersion),
                         targetFramework: _testTargetframework,
-                        source: source);
+                        additionalFeeds: new[] {source});
 
                     first.ShouldNotThrow();
 
@@ -321,7 +369,7 @@ namespace Microsoft.DotNet.ToolPackage.Tests
                         packageId: TestPackageId,
                         versionRange: VersionRange.Parse(TestPackageVersion),
                         targetFramework: _testTargetframework,
-                        source: source);
+                        additionalFeeds: new[] {source});
 
                     t.Complete();
                 }
@@ -352,7 +400,7 @@ namespace Microsoft.DotNet.ToolPackage.Tests
                 packageId: TestPackageId,
                 versionRange: VersionRange.Parse(TestPackageVersion),
                 targetFramework: _testTargetframework,
-                source: source);
+                additionalFeeds: new[] {source});
 
             AssertPackageInstall(reporter, fileSystem, package, store);
 
@@ -360,7 +408,7 @@ namespace Microsoft.DotNet.ToolPackage.Tests
                 packageId: TestPackageId,
                 versionRange: VersionRange.Parse(TestPackageVersion),
                 targetFramework: _testTargetframework,
-                source: source);
+                additionalFeeds: new[] {source});
 
             reporter.Lines.Should().BeEmpty();
 
@@ -401,7 +449,7 @@ namespace Microsoft.DotNet.ToolPackage.Tests
                 packageId: TestPackageId,
                 versionRange: VersionRange.Parse(TestPackageVersion),
                 targetFramework: _testTargetframework,
-                source: source);
+                additionalFeeds: new[] {source});
 
             AssertPackageInstall(reporter, fileSystem, package, store);
 
@@ -425,7 +473,7 @@ namespace Microsoft.DotNet.ToolPackage.Tests
                 packageId: TestPackageId,
                 versionRange: VersionRange.Parse(TestPackageVersion),
                 targetFramework: _testTargetframework,
-                source: source);
+                additionalFeeds: new[] {source});
 
             AssertPackageInstall(reporter, fileSystem, package, store);
 
@@ -458,7 +506,7 @@ namespace Microsoft.DotNet.ToolPackage.Tests
                 packageId: TestPackageId,
                 versionRange: VersionRange.Parse(TestPackageVersion),
                 targetFramework: _testTargetframework,
-                source: source);
+                additionalFeeds: new[] {source});
 
             AssertPackageInstall(reporter, fileSystem, package, store);
 
@@ -597,7 +645,7 @@ namespace Microsoft.DotNet.ToolPackage.Tests
             {
                 new MockFeed
                 {
-                    Type = MockFeedType.Source,
+                    Type = MockFeedType.ImplicitAdditionalFeed,
                     Uri = source,
                     Packages = new List<MockFeedPackage>
                     {
@@ -617,7 +665,7 @@ namespace Microsoft.DotNet.ToolPackage.Tests
             {
                 new MockFeed
                 {
-                    Type = MockFeedType.OfflineFeed,
+                    Type = MockFeedType.ImplicitAdditionalFeed,
                     Uri = GetTestLocalFeedPath(),
                     Packages = new List<MockFeedPackage>
                     {

scripts/cli-test-env.sh
@@ -16,7 +16,7 @@ REPO_ROOT="$( cd -P "$( dirname "$SOURCE" )/../" && pwd )"
 uname=$(uname)
 if [ "$(uname)" = "Darwin" ]
 then
-  RID=osx.10.13-x64
+  RID=osx-x64
 else
   RID=linux-x64
 fi

src/dotnet/ToolPackage/IProjectRestorer.cs
@@ -8,11 +8,9 @@ namespace Microsoft.DotNet.ToolPackage
 {
     internal interface IProjectRestorer
     {
-        void Restore(
-            FilePath project,
+        void Restore(FilePath project,
             DirectoryPath assetJsonOutput,
             FilePath? nugetConfig = null,
-            string source = null,
             string verbosity = null);
     }
 }

src/dotnet/ToolPackage/IToolPackageInstaller.cs
@@ -10,13 +10,12 @@ namespace Microsoft.DotNet.ToolPackage
 {
     internal interface IToolPackageInstaller
     {
-        IToolPackage InstallPackage(
-            PackageId packageId,
+        IToolPackage InstallPackage(PackageId packageId,
             VersionRange versionRange = null,
             string targetFramework = null,
             FilePath? nugetConfig = null,
             DirectoryPath? rootConfigDirectory = null,
-            string source = null,
+            string[] additionalFeeds = null,
             string verbosity = null);
     }
 }

src/dotnet/commands/dotnet-install/dotnet-install-tool/InstallToolCommand.cs
@@ -32,7 +32,7 @@ namespace Microsoft.DotNet.Tools.Install.Tool
         private readonly string _packageVersion;
         private readonly string _configFilePath;
         private readonly string _framework;
-        private readonly string _source;
+        private readonly string[] _source;
         private readonly bool _global;
         private readonly string _verbosity;
         private readonly string _toolPath;
@@ -55,7 +55,7 @@ namespace Microsoft.DotNet.Tools.Install.Tool
             _packageVersion = appliedCommand.ValueOrDefault<string>("version");
             _configFilePath = appliedCommand.ValueOrDefault<string>("configfile");
             _framework = appliedCommand.ValueOrDefault<string>("framework");
-            _source = appliedCommand.ValueOrDefault<string>("source");
+            _source = appliedCommand.ValueOrDefault<string[]>("source-feed");
             _global = appliedCommand.ValueOrDefault<bool>("global");
             _verbosity = appliedCommand.SingleArgumentOrDefault("verbosity");
             _toolPath = appliedCommand.SingleArgumentOrDefault("tool-path");
@@ -137,7 +137,7 @@ namespace Microsoft.DotNet.Tools.Install.Tool
                         versionRange: versionRange,
                         targetFramework: _framework,
                         nugetConfig: configFile,
-                        source: _source,
+                        additionalFeeds: _source,
                         verbosity: _verbosity);
 
                     foreach (var command in package.Commands)

src/dotnet/commands/dotnet-install/dotnet-install-tool/InstallToolCommandParser.cs
@@ -32,10 +32,10 @@ namespace Microsoft.DotNet.Cli
                     LocalizableStrings.ConfigFileOptionDescription,
                     Accept.ExactlyOneArgument()),
                 Create.Option(
-                    "--source",
-                    LocalizableStrings.SourceOptionDescription,
-                    Accept.ExactlyOneArgument()
-                        .With(name: LocalizableStrings.SourceOptionName)),
+                    "--source-feed",
+                    LocalizableStrings.SourceFeedOptionDescription,
+                    Accept.OneOrMoreArguments()
+                        .With(name: LocalizableStrings.SourceFeedOptionName)),
                 Create.Option(
                     "-f|--framework",
                     LocalizableStrings.FrameworkOptionDescription,

src/dotnet/commands/dotnet-install/dotnet-install-tool/LocalizableStrings.resx
@@ -129,11 +129,11 @@
   <data name="VersionOptionDescription" xml:space="preserve">
     <value>Version of the tool package in NuGet.</value>
   </data>
-  <data name="SourceOptionDescription" xml:space="preserve">
-    <value>Specifies a NuGet package source to use during installation.</value>
+  <data name="SourceFeedOptionDescription" xml:space="preserve">
+    <value>Adds an additional NuGet package source to use during installation.</value>
   </data>
-  <data name="SourceOptionName" xml:space="preserve">
-    <value>SOURCE</value>
+  <data name="SourceFeedOptionName" xml:space="preserve">
+    <value>SOURCE_FEED</value>
   </data>
   <data name="CommandDescription" xml:space="preserve">
     <value>Installs a tool for use on the command line.</value>

commit b9597913dc13fa45bb437c173bbae5569f5aca80

Fix --source-feed option for tool install and update commands.
Commit 9cc2b7cd2f9dc165ee3a04b1de6fa5065b142dd3 regressed the `--source-feed`
option so that it no longer accepted relative paths.  Because the option is now
saved to the temp project file, any relative paths specified by the
`--source-feed` option were made relative to the temp project path and not from
the current working directory of where dotnet was run.

The fix is to use `Path.GetFullPath` of the `--source-feed` option, provided
the option specified was not an absolute URI.

Fixes #9132.

src/dotnet/ToolPackage/ToolPackageInstaller.cs
@@ -161,7 +161,17 @@ namespace Microsoft.DotNet.ToolPackage
             var feeds = new List<string>();
             if (additionalFeeds != null)
             {
-                feeds.AddRange(additionalFeeds);
+                foreach (var feed in additionalFeeds)
+                {
+                    if (Uri.IsWellFormedUriString(feed, UriKind.Absolute))
+                    {
+                        feeds.Add(feed);
+                    }
+                    else
+                    {
+                        feeds.Add(Path.GetFullPath(feed));
+                    }
+                }
             }
 
             // use fallbackfolder as feed to enable offline

test/Microsoft.DotNet.ToolPackage.Tests/ToolPackageInstallerTests.cs
@@ -262,6 +262,50 @@ namespace Microsoft.DotNet.ToolPackage.Tests
         [Theory]
         [InlineData(false)]
         [InlineData(true)]
+        public void GivenARelativeSourcePathInstallSucceeds(bool testMockBehaviorIsInSync)
+        {
+            var source = GetTestLocalFeedPath();
+
+            var (store, installer, reporter, fileSystem) = Setup(
+                useMock: testMockBehaviorIsInSync,
+                feeds: GetMockFeedsForSource(source));
+
+            var package = installer.InstallPackage(
+                packageId: TestPackageId,
+                versionRange: VersionRange.Parse(TestPackageVersion),
+                targetFramework: _testTargetframework,
+                additionalFeeds: new[] { Path.GetRelativePath(Directory.GetCurrentDirectory(), source) });
+
+            AssertPackageInstall(reporter, fileSystem, package, store);
+
+            package.Uninstall();
+        }
+
+        [Theory]
+        [InlineData(false)]
+        [InlineData(true)]
+        public void GivenAUriSourceInstallSucceeds(bool testMockBehaviorIsInSync)
+        {
+            var source = GetTestLocalFeedPath();
+
+            var (store, installer, reporter, fileSystem) = Setup(
+                useMock: testMockBehaviorIsInSync,
+                feeds: GetMockFeedsForSource(source));
+
+            var package = installer.InstallPackage(
+                packageId: TestPackageId,
+                versionRange: VersionRange.Parse(TestPackageVersion),
+                targetFramework: _testTargetframework,
+                additionalFeeds: new[] { new Uri(source).AbsoluteUri });
+
+            AssertPackageInstall(reporter, fileSystem, package, store);
+
+            package.Uninstall();
+        }
+
+        [Theory]
+        [InlineData(false)]
+        [InlineData(true)]
         public void GivenAEmptySourceAndNugetConfigInstallSucceeds(bool testMockBehaviorIsInSync)
         {
             var nugetConfigPath = WriteNugetConfigFileToPointToTheFeed();