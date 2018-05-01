57d065e43d0d378e549c8fad92c99ea03ae34067
William Li <wul@microsoft.com>
2018-05-01 10:24:40 -0700

    Correct dotnet list description (#9174)
    After dotnet tool list moved out of dotnet list, the description has not been updated.

--- test/dotnet-help.Tests/GivenThatIWantToShowHelpForDotnetCommand.cs
+++ test/dotnet-help.Tests/GivenThatIWantToShowHelpForDotnetCommand.cs
@@ -34,7 +34,7 @@ SDK commands:
   sln              Modify solution (SLN) files.
   add              Add reference to the project.
   remove           Remove reference from the project.
-  list             List project references or installed tools.
+  list             List references of a .NET project.
   nuget            Provides additional NuGet commands.
   msbuild          Runs Microsoft Build Engine (MSBuild).
   vstest           Runs Microsoft Test Execution Command Line Tool.
