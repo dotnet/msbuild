fa3b49ffa85ff315dbf1cd9f3a384f07a829ac34
Ankit Jain <ankit.jain@microsoft.com>
2018-05-10 18:39:26 -0400

    Add support for fallback paths to be specified via property references
    Currently, the list of fallback paths in app.config can have a property
    reference, like:
    
    ```
     <searchPaths os="osx">
    	<property name="MSBuildExtensionsPath" value="$(FallbackPath);/x/y/z" />
    ```
    
    .. but it is assumed that such a property will expand to a single path.
    
    This commit adds support for such properties to expand to multiple ';'
    separated list of paths. The rules for expansion do not change, they are still
    expanded at the time of project import.
    
    With this, given `$(FallbackPath)="/a/b/c;/q/w/e"` and the list of fallback paths
    would become `"/a/b/c;/q/w/e;/x/yz"`.

--- src/Build/Definition/ToolsetConfigurationReader.cs
+++ src/Build/Definition/ToolsetConfigurationReader.cs
@@ -41,12 +41,6 @@ namespace Microsoft.Build.Evaluation
         private bool _configurationReadAttempted = false;
 
         /// <summary>
-        /// Character used to separate search paths specified for MSBuildExtensionsPath* in
-        /// the config file
-        /// </summary>
-        private char _separatorForExtensionsPathSearchPaths = ';';
-
-        /// <summary>
         /// Cached values of tools version -> project import search paths table
         /// </summary>
         private readonly Dictionary<string, Dictionary<string, ProjectImportPathMatch>> _projectImportSearchPathsCache;
@@ -239,13 +233,7 @@ namespace Microsoft.Build.Evaluation
                     continue;
                 }
 
-                //FIXME: handle ; in path on Unix
-                var paths = property.Value
-                    .Split(new[] {_separatorForExtensionsPathSearchPaths}, StringSplitOptions.RemoveEmptyEntries)
-                    .Distinct()
-                    .Where(path => !string.IsNullOrEmpty(path));
-
-                pathsTable.Add(property.Name, new ProjectImportPathMatch(property.Name, paths.ToList()));
+                pathsTable.Add(property.Name, new ProjectImportPathMatch(property.Name, property.Value));
             }
 
             return pathsTable;
