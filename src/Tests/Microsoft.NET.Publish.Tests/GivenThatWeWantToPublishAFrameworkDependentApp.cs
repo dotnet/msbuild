c4b2e484aee91060af860f893741e7561e381ab0
Peter Huene <pehuene@microsoft.com>
2018-05-25 13:25:27 -0700

    Implement framework-dependent application with apphost.
    This commit implements building and publishing a framework-dependent
    application with the application host.
    
    Building with `/p:UseAppHost=true` (when a `RuntimeIdentifier` is specified)
    and with `/p:SelfContained=false` will create an executable apphost that can be
    used to run the application against globally installed runtimes.
    
    Fixes #6237.

---
+++ src/Tests/Microsoft.NET.Publish.Tests/GivenThatWeWantToPublishAFrameworkDependentApp.cs
@@ -0,0 +1,136 @@
+// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using FluentAssertions;
+using Microsoft.DotNet.Cli.Utils;
+using Microsoft.DotNet.PlatformAbstractions;
+using Microsoft.NET.Build.Tasks;
+using Microsoft.NET.TestFramework;
+using Microsoft.NET.TestFramework.Assertions;
+using Microsoft.NET.TestFramework.Commands;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Microsoft.NET.Publish.Tests
+{
+    public class GivenThatWeWantToPublishAFrameworkDependentApp : SdkTest
+    {
+        private const string TestProjectName = "HelloWorld";
+        private const string TargetFramework = "netcoreapp2.1";
+
+        public GivenThatWeWantToPublishAFrameworkDependentApp(ITestOutputHelper log) : base(log)
+        {
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("true")]
+        [InlineData("false")]
+        public void It_publishes_with_or_without_apphost(string useAppHost)
+        {
+            var runtimeIdentifier = RuntimeEnvironment.GetRuntimeIdentifier();
+            var appHostName = $"{TestProjectName}{Constants.ExeSuffix}";
+
+            var testAsset = _testAssetsManager
+                .CopyTestAsset(TestProjectName, $"It_publishes_with_or_without_apphost_{(useAppHost ?? "null")}")
+                .WithSource();
+
+            var msbuildArgs = new List<string>()
+            {
+                "/restore",
+                $"/p:RuntimeIdentifier={runtimeIdentifier}",
+                $"/p:TestRuntimeIdentifier={runtimeIdentifier}",
+                "/p:SelfContained=false",
+                $"/p:TargetFramework={TargetFramework}"
+            };
+
+            if (useAppHost != null)
+            {
+                msbuildArgs.Add($"/p:UseAppHost={useAppHost}");
+            }
+
+            var publishCommand = new PublishCommand(Log, testAsset.TestRoot);
+            publishCommand
+                .Execute(msbuildArgs.ToArray())
+                .Should()
+                .Pass();
+
+            var publishDirectory = publishCommand.GetOutputDirectory(TargetFramework, runtimeIdentifier: runtimeIdentifier);
+
+            var expectedFiles = new List<string>()
+            {
+                $"{TestProjectName}.dll",
+                $"{TestProjectName}.pdb",
+                $"{TestProjectName}.deps.json",
+                $"{TestProjectName}.runtimeconfig.json",
+            };
+
+            if (useAppHost != "false")
+            {
+                expectedFiles.Add(appHostName);
+            }
+
+            publishDirectory.Should().NotHaveSubDirectories();
+            publishDirectory.Should().OnlyHaveFiles(expectedFiles);
+
+            // Run the apphost if one was generated
+            if (useAppHost != "false")
+            {
+                Command.Create(Path.Combine(publishDirectory.FullName, appHostName), Enumerable.Empty<string>())
+                    .EnvironmentVariable(
+                        Environment.Is64BitProcess ? "DOTNET_ROOT" : "DOTNET_ROOT(x86)",
+                        Path.GetDirectoryName(TestContext.Current.ToolsetUnderTest.DotNetHostPath))
+                    .CaptureStdOut()
+                    .Execute()
+                    .Should()
+                    .Pass()
+                    .And
+                    .HaveStdOutContaining("Hello World!");
+            }
+        }
+
+        [Fact]
+        public void It_errors_when_using_app_host_without_rid()
+        {
+            var testAsset = _testAssetsManager
+                .CopyTestAsset(TestProjectName)
+                .WithSource();
+
+            var publishCommand = new PublishCommand(Log, testAsset.TestRoot);
+            publishCommand
+                .Execute(
+                    "/p:SelfContained=false",
+                    "/p:UseAppHost=true",
+                    $"/p:TargetFramework={TargetFramework}")
+                .Should()
+                .Fail()
+                .And
+                .HaveStdOutContaining(Strings.CannotUseAppHostWithoutRuntimeIdentifier);
+        }
+
+        [Fact]
+        public void It_errors_when_using_app_host_with_older_target_framework()
+        {
+            var runtimeIdentifier = RuntimeEnvironment.GetRuntimeIdentifier();
+
+            var testAsset = _testAssetsManager
+                .CopyTestAsset(TestProjectName)
+                .WithSource();
+
+            var publishCommand = new PublishCommand(Log, testAsset.TestRoot);
+            publishCommand
+                .Execute(
+                    "/p:SelfContained=false",
+                    "/p:UseAppHost=true",
+                    $"/p:RuntimeIdentifier={runtimeIdentifier}")
+                .Should()
+                .Fail()
+                .And
+                .HaveStdOutContaining(Strings.FrameworkDependentAppHostRequiresVersion21);
+        }
+    }
+}
