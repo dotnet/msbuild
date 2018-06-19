939de240f9457d3833674b9b39b1ddeb51e81a30
Erik Mavrinac <erikmav@microsoft.com>
2018-06-19 13:07:12 -0700

    step

--- src/Build/Collections/WeakValueDictionary.cs
+++ src/Build/Collections/WeakValueDictionary.cs
@@ -1,9 +1,6 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 //-----------------------------------------------------------------------
-// </copyright>
-// <summary>Dictionary that does not prevent values from being garbage collected.</summary>
-//-----------------------------------------------------------------------
 
 using System;
 using System.Collections.Generic;
