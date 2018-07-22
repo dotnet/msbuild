119e4b2cfbcb4333206b25d916684c557643d8e2
Mike Lorbetske <mlorbe@microsoft.com>
2018-07-21 21:22:32 -0700

    Introduce new interfaces to preserve source to target mappings and consume them in post-actions
    This should make referring to outputs in post-actions much easier after the rest have been adapted
    
    Fixes #1623

--- src/Microsoft.TemplateEngine.Cli/PostActionProcessors/DotnetRestorePostActionProcessor.cs
+++ src/Microsoft.TemplateEngine.Cli/PostActionProcessors/DotnetRestorePostActionProcessor.cs
@@ -1,10 +1,12 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using Microsoft.TemplateEngine.Abstractions;
 
 namespace Microsoft.TemplateEngine.Cli.PostActionProcessors
 {
-    public class DotnetRestorePostActionProcessor : IPostActionProcessor
+    public class DotnetRestorePostActionProcessor : PostActionProcessor2Base, IPostActionProcessor, IPostActionProcessor2
     {
         private static readonly Guid ActionProcessorId = new Guid("210D431B-A78B-4D2F-B762-4ED3E3EA9025");
 
@@ -12,6 +14,51 @@ namespace Microsoft.TemplateEngine.Cli.PostActionProcessors
 
         public DotnetRestorePostActionProcessor()
         {
+        }
+
+        public bool Process(IEngineEnvironmentSettings environment, IPostAction actionConfig, ICreationEffects2 creationEffects, string outputBasePath)
+        {
+            bool allSucceeded = true;
+            IEnumerable<string> targetFiles;
+
+            if (actionConfig.Args.TryGetValue("files", out string specificFilesString))
+            {
+                targetFiles = specificFilesString.Split(new[] { ';' }, StringSplitOptions.RemoveEmptyEntries).SelectMany(x => GetTargetForSource(creationEffects, x));
+            }
+            else
+            {
+                targetFiles = creationEffects.FileChanges.Select(x => x.TargetRelativePath);
+            }
+
+            foreach (string targetRelativePath in targetFiles)
+            {
+                string pathToRestore = !string.IsNullOrEmpty(outputBasePath) ? Path.Combine(outputBasePath, targetRelativePath) : targetRelativePath;
+
+                if (string.IsNullOrEmpty(pathToRestore) ||
+                    (!Directory.Exists(pathToRestore)
+                        && !Path.GetExtension(pathToRestore).EndsWith("proj", StringComparison.OrdinalIgnoreCase)
+                        && !Path.GetExtension(pathToRestore).Equals(".sln", StringComparison.OrdinalIgnoreCase)
+                    ))
+                {
+                    continue;
+                }
+
+                environment.Host.LogMessage(string.Format(LocalizableStrings.RunningDotnetRestoreOn, pathToRestore));
+                Dotnet restoreCommand = Dotnet.Restore(pathToRestore).ForwardStdErr().ForwardStdOut();
+                Dotnet.Result commandResult = restoreCommand.Execute();
+
+                if (commandResult.ExitCode != 0)
+                {
+                    environment.Host.LogMessage(LocalizableStrings.RestoreFailed);
+                    allSucceeded = false;
+                }
+                else
+                {
+                    environment.Host.LogMessage(LocalizableStrings.RestoreSucceeded);
+                }
+            }
+
+            return allSucceeded;
         }
 
         public bool Process(IEngineEnvironmentSettings environment, IPostAction actionConfig, ICreationResult templateCreationResult, string outputBasePath)
