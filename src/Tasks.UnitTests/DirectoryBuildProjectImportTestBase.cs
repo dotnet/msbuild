d42d3504057ef2b88dd4f68c4bfc5591371bd6fe
Jeff Kluge <jeffkl@microsoft.com>
2018-08-14 14:27:39 -0700

    Prepend the last modified project to MSBuildAllProjects (#3605)
    During evaluation, keep track of the last modified project in the import graph.
    Prepend the path to the last modified project to `MSBuildAllProjects`
    Stop setting `MSBuildAllProjects` in our targets
    Add unit tests
    
    Closes #1299

--- src/Tasks.UnitTests/DirectoryBuildProjectImportTestBase.cs
+++ src/Tasks.UnitTests/DirectoryBuildProjectImportTestBase.cs
@@ -118,7 +118,6 @@ namespace Microsoft.Build.UnitTests
             Assert.Equal(String.Empty, project.GetPropertyValue(DirectoryBuildProjectBasePathPropertyName), StringComparer.OrdinalIgnoreCase);
             Assert.Equal(String.Empty, project.GetPropertyValue(DirectoryBuildProjectFilePropertyName), StringComparer.OrdinalIgnoreCase);
             Assert.Equal(String.Empty, project.GetPropertyValue(DirectoryBuildProjectPathPropertyName));
-            Assert.DoesNotContain(directoryBuildProjectFilePath, project.GetPropertyValue("MSBuildAllProjects"));
         }
 
         /// <summary>
@@ -147,7 +146,6 @@ namespace Microsoft.Build.UnitTests
             Assert.Equal("true", project.GetPropertyValue(ImportDirectoryBuildProjectPropertyName), StringComparer.OrdinalIgnoreCase);
             Assert.Equal("true", project.GetPropertyValue("WasDirectoryBuildProjectImported"), StringComparer.OrdinalIgnoreCase);
             Assert.Equal(customFilePath, project.GetPropertyValue(DirectoryBuildProjectPathPropertyName));
-            Assert.Contains(customFilePath, project.GetPropertyValue("MSBuildAllProjects"));
         }
 
         /// <summary>
@@ -175,7 +173,6 @@ namespace Microsoft.Build.UnitTests
             Assert.Equal(ObjectModelHelpers.TempProjectDir, project.GetPropertyValue(DirectoryBuildProjectBasePathPropertyName), StringComparer.OrdinalIgnoreCase);
             Assert.Equal(DirectoryBuildProjectFile, project.GetPropertyValue(DirectoryBuildProjectFilePropertyName), StringComparer.OrdinalIgnoreCase);
             Assert.Equal(Path.Combine(ObjectModelHelpers.TempProjectDir, DirectoryBuildProjectFile), project.GetPropertyValue(DirectoryBuildProjectPathPropertyName));
-            Assert.Contains(directoryBuildProjectFilePath, project.GetPropertyValue("MSBuildAllProjects"));
         }
     }
 }
