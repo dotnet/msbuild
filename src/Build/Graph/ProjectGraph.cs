6244c69aa4732353bbc0a2c9adfd2e5f79de5516
David Federman <david.federman@outlook.com>
2018-09-05 14:09:38 -0700

    Initial ProjectGraph interfaces/stubs (#3701)
    Closes: #3680

---
+++ src/Build/Graph/ProjectGraph.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using Microsoft.Build.Exceptions;
+
+namespace Microsoft.Build.Graph
+{
+    /// <summary>
+    /// Represents a graph of evaluated projects.
+    /// </summary>
+    internal sealed class ProjectGraph
+    {
+        /// <summary>
+        /// Constructs a graph starting from the given project file.
+        /// </summary>
+        /// <param name="entryProjectFile">The project file to use as the entry point in constructing the graph</param>
+        /// <exception cref="InvalidProjectFileException">If the evaluation of any project in the graph fails.</exception>
+        public ProjectGraph(string entryProjectFile) { }
+
+        /// <summary>
+        /// Constructs a graph starting from the given project files.
+        /// </summary>
+        /// <param name="entryProjectFiles">The project files to use as the entry points in constructing the graph</param>
+        /// <exception cref="InvalidProjectFileException">If the evaluation of any project in the graph fails.</exception>
+        public ProjectGraph(IEnumerable<string> entryProjectFiles) { }
+
+        /// <summary>
+        /// Get an unordered collection of all project nodes in the graph.
+        /// </summary>
+        public IReadOnlyCollection<ProjectGraphNode> ProjectNodes => Array.Empty<ProjectGraphNode>();
+    }
+}
