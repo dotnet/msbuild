89050fd78128d6f0c76211dc11a4630cb24e021b
William Li <wul@microsoft.com>
2018-09-11 08:43:07 -0700

    Update CreateRegexUpdater

--- build_projects/update-dependencies/Program.cs
+++ build_projects/update-dependencies/Program.cs
@@ -132,7 +132,7 @@ namespace Microsoft.DotNet.Scripts
             if (s_config.HasVersionFragment("templating"))
             {
                 yield return CreateRegexUpdater(dependencyVersionsPath, "MicrosoftDotNetCommonItemTemplatesPackageVersion", "Microsoft.DotNet.Common.ItemTemplates");
-                yield return CreateRegexUpdater(dependencyVersionsPath, "MicrosoftDotNetTestProjectTemplates22PackageVersion", "Microsoft.DotNet.Test.ProjectTemplates.2.0");
+                yield return CreateRegexUpdater(dependencyVersionsPath, "MicrosoftDotNetTestProjectTemplates22PackageVersion", "Microsoft.DotNet.Test.ProjectTemplates.2.2");
                 yield return CreateRegexUpdater(dependencyVersionsPath, "MicrosoftTemplateEngineCliPackageVersion", "Microsoft.TemplateEngine.Cli");
             }
             if (s_config.HasVersionFragment("websdk"))
