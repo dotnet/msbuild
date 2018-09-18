ee1f3a6ccfd05dbd6e4a0eda8577bf2abe7a6870
David Federman <david.federman@outlook.com>
2018-09-18 16:15:06 -0700

    Build a graph containing a single node (#3748)
    Closes #3682

--- src/Build.UnitTests/Graph/ProjectGraph_Tests.cs
+++ src/Build.UnitTests/Graph/ProjectGraph_Tests.cs
@@ -2,12 +2,11 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
-using System.IO;
 using System.Linq;
 using Microsoft.Build.Evaluation;
 using Microsoft.Build.UnitTests;
-using Xunit;
 using Shouldly;
+using Xunit;
 
 namespace Microsoft.Build.Graph.UnitTests
 {
