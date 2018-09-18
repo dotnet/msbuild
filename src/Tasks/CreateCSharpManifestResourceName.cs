ce263910d8e3266b95f48853ba988c0bc17f45a9
isiahtoe <pgmr.isiah@gmail.com>
2018-09-17 22:16:00 -0700

    Rename function to minimize differences from original names; add comments and license

--- src/Tasks/CreateCSharpManifestResourceName.cs
+++ src/Tasks/CreateCSharpManifestResourceName.cs
@@ -91,7 +91,7 @@ namespace Microsoft.Build.Tasks
             TaskLoggingHelper log
         )
         {
-            return ManifestNameCreator.CreateNameForCSharpManifestResource
+            return ManifestNameCreator.CreateCSharpManifestResourceName
             (
                 fileName,
                 linkFileName,
