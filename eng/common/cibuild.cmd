c36fd80371beb9fa6bb7668453fa6454589aefb3
Livar Cunha <livar@live.com>
2018-09-24 13:16:07 -0700

    Modifying YAML to include version, signing and publishing new parameters.

--- eng/common/cibuild.cmd
+++ eng/common/cibuild.cmd
@@ -1,3 +1,3 @@
 @echo off
-powershell -NoLogo -NoProfile -ExecutionPolicy ByPass -Command "& """%~dp0Build.ps1""" -restore -build -test -sign -pack -ci %*"
+powershell -NoLogo -NoProfile -ExecutionPolicy ByPass -Command "& """%~dp0Build.ps1""" -restore -build -test -sign -pack -publish -ci %*"
 exit /b %ErrorLevel%
