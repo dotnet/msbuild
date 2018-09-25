3af93e195e2c5ee372a1382d8a5b2d65a75c4760
Sven Boemer <sbomer@gmail.com>
2018-09-25 10:18:05 -0700

    Make ResourceUpdater internal

--- src/Tasks/Microsoft.NET.Build.Tasks/ResourceUpdater.cs
+++ src/Tasks/Microsoft.NET.Build.Tasks/ResourceUpdater.cs
@@ -13,7 +13,7 @@ namespace Microsoft.NET.Build.Tasks
     /// in a PE image. It currently only works on Windows, because it
     /// requires various kernel32 APIs.
     /// </summary>
-    public class ResourceUpdater : IDisposable
+    internal class ResourceUpdater : IDisposable
     {
         private sealed class Kernel32
         {
