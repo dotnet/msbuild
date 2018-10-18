c8d7c34cca6ef438edb8e979a3746827dcf9f98f
Mihai Codoban <micodoba@microsoft.com>
2018-10-18 15:05:30 -0700

    Minimal repro

---
+++ src/Build.UnitTests/Graph/Investigate_CI.cs
@@ -0,0 +1,52 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.Build.Execution;
+using Microsoft.Build.Shared;
+using Microsoft.Build.UnitTests;
+using Xunit;
+
+namespace Microsoft.Build.Graph.UnitTests
+{
+    public class Investigate_CI
+    {
+        [Fact]
+        public void FailingTest()
+        {
+            using (var env = TestEnvironment.Create())
+            using (var buildManager = new BuildManager())
+            {
+                var projectFile = env.CreateFile()
+                    .Path;
+
+                File.WriteAllText(projectFile, @"
+                <Project>
+                    <Target Name='SelfTarget'>
+                    </Target>
+                </Project>");
+
+                var buildParameters = new BuildParameters();
+
+                var rootRequest = new BuildRequestData(
+                    projectFile,
+                    new Dictionary<string, string>(),
+                    MSBuildConstants.CurrentToolsVersion,
+                    new[] {"SelfTarget"},
+                    null);
+
+                try
+                {
+                    buildManager.BeginBuild(buildParameters);
+
+                    buildManager.BuildRequest(rootRequest);
+                }
+                finally
+                {
+                    buildManager.EndBuild();
+                }
+            }
+        }
+    }
+}
