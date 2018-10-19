fdf922b1402937786965788d17d8754963f7fcb3
Christian Castaneda <chcasta@microsoft.com>
2018-10-19 16:53:34 -0700

    Add SystemState cache conversion utilities

---
+++ src/Tasks/SystemStateCacheHydrator.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Reflection;
+using System.Runtime.Versioning;
+
+using Microsoft.Build.Shared;
+
+namespace Microsoft.Build.Tasks
+{
+    internal sealed partial class SystemState
+    {
+        private class SystemStateCacheHydrator
+        {
+            internal static Dictionary<string, FileState> HydrateSystemStateCache(SystemStateCachePayload cachePayload)
+            {
+                var cache = new Dictionary<string, FileState>(StringComparer.OrdinalIgnoreCase);
+
+                foreach (FileStatePayload fileStatePayload in cachePayload.InstanceLocalFileStateCache)
+                {
+                    cache[fileStatePayload.Path] = HydrateFileState(fileStatePayload);
+                }
+
+                return cache;
+            }
+
+            private static FileState HydrateFileState(FileStatePayload fileStatePayload)
+            {
+                DateTime lastModified = HydrateLastModified(fileStatePayload.LastModified);
+                AssemblyNameExtension assembly = HydrateAssembly(fileStatePayload.Assembly);
+                AssemblyNameExtension[] dependencies = HydrateDependencies(fileStatePayload.Dependencies);
+                FrameworkName frameworkName = HydrateFrameworkName(fileStatePayload.FrameworkName);
+
+                return new FileState(lastModified)
+                {
+                    Assembly = assembly,
+                    dependencies = dependencies,
+                    scatterFiles = fileStatePayload.ScatterFiles,
+                    runtimeVersion = fileStatePayload.RuntimeVersion,
+                    frameworkName = frameworkName
+                };
+            }
+
+            private static DateTime HydrateLastModified(DateTimePayload lastModifiedPayload) =>
+                new DateTime(lastModifiedPayload.Ticks, lastModifiedPayload.Kind);
+
+            private static FrameworkName HydrateFrameworkName(FrameworkNamePayload frameworkNamePayload)
+            {
+                if (frameworkNamePayload == null)
+                {
+                    return null;
+                }
+
+                var version = new Version(frameworkNamePayload.Version);
+                return new FrameworkName(frameworkNamePayload.Identifier, version, frameworkNamePayload.Profile);
+            }
+
+            private static AssemblyNameExtension[] HydrateDependencies(AssemblyNameExtensionPayload[] dependenciesPayload)
+            {
+                if (dependenciesPayload == null)
+                {
+                    return null;
+                }
+
+                var dependencies = new AssemblyNameExtension[dependenciesPayload.Length];
+
+                for (int i = 0; i < dependenciesPayload.Length; i++)
+                {
+                    dependencies[i] = HydrateAssembly(dependenciesPayload[i]);
+                }
+
+                return dependencies;
+            }
+
+            private static AssemblyNameExtension HydrateAssembly(AssemblyNameExtensionPayload assemblyPayload)
+            {
+                if (assemblyPayload == null)
+                {
+                    return null;
+                }
+
+                AssemblyName asAssemblyName = HydrateAsAssemblyName(assemblyPayload.AsAssemblyName);
+                HashSet<AssemblyNameExtension> remappedFrom = HydrateRemappedFrom(assemblyPayload.RemappedFrom);
+                var assemblyNameState = new AssemblyNameExtensionState
+                {
+                    AsAssemblyName = asAssemblyName,
+                    AsString = assemblyPayload.AsString,
+                    IsSimpleName = assemblyPayload.IsSimpleName,
+                    HasProcessorArchitectureInFusionName = assemblyPayload.HasProcessorArchitectureInFusionName,
+                    Immutable = assemblyPayload.Immutable,
+                    RemappedFrom = remappedFrom
+                };
+
+                return new AssemblyNameExtension(assemblyNameState);
+            }
+
+            private static HashSet<AssemblyNameExtension> HydrateRemappedFrom(AssemblyNameExtensionPayload[] remappedFromPayload)
+            {
+                if (remappedFromPayload == null)
+                {
+                    return null;
+                }
+
+                var remappedFrom = new HashSet<AssemblyNameExtension>();
+
+                foreach (AssemblyNameExtensionPayload assemblyNameData in remappedFromPayload)
+                {
+                    AssemblyNameExtension assembly = HydrateAssembly(assemblyNameData);
+                    remappedFrom.Add(assembly);
+                }
+
+                return remappedFrom;
+            }
+
+            private static AssemblyName HydrateAsAssemblyName(AssemblyNamePayload asAssemblyNamePayload)
+            {
+                if (asAssemblyNamePayload == null)
+                {
+                    return null;
+                }
+
+                Version version = HydrateVersion(asAssemblyNamePayload.Version);
+                CultureInfo cultureInfo = HydrateCultureInfo(asAssemblyNamePayload.CultureInfo);
+                StrongNameKeyPair keyPair = HydrateKeyPairData(asAssemblyNamePayload.KeyPair);
+
+                var asAssemblyName = new AssemblyName
+                {
+                    Name = asAssemblyNamePayload.Name,
+                    Version = version,
+                    Flags = asAssemblyNamePayload.Flags,
+                    ProcessorArchitecture = asAssemblyNamePayload.ProcessorArchitecture,
+                    CultureInfo = cultureInfo,
+                    HashAlgorithm = asAssemblyNamePayload.HashAlgorithm,
+                    VersionCompatibility = asAssemblyNamePayload.VersionCompatibility,
+                    CodeBase = asAssemblyNamePayload.CodeBase,
+                    KeyPair = keyPair
+                };
+                asAssemblyName.SetPublicKey(asAssemblyNamePayload.PublicKey);
+                asAssemblyName.SetPublicKeyToken(asAssemblyNamePayload.PublicKeyToken);
+
+                return asAssemblyName;
+            }
+
+            private static Version HydrateVersion(VersionPayload versionPayload)
+            {
+                return versionPayload == null
+                    ? null
+                    : new Version
+                    (
+                        versionPayload.Major,
+                        versionPayload.Minor,
+                        versionPayload.Build,
+                        versionPayload.Revision
+                    );
+            }
+
+            private static CultureInfo HydrateCultureInfo(CultureInfoPayload infoPayload)
+            {
+                return infoPayload == null ? null : new CultureInfo(infoPayload.LCID);
+            }
+
+            private static StrongNameKeyPair HydrateKeyPairData(StrongNameKeyPairPayload keyPairPayload)
+            {
+                return keyPairPayload == null ? null : new StrongNameKeyPair(keyPairPayload.PublicKey);
+            }
+        }
+    }
+}
---
+++ src/Tasks/SystemStateCachePayloadExtractor.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Reflection;
+using System.Runtime.Versioning;
+using Microsoft.Build.Shared;
+
+namespace Microsoft.Build.Tasks
+{
+    internal sealed partial class SystemState
+    {
+        private class SystemStateCachePayloadExtractor
+        {
+            internal static SystemStateCachePayload ExtractSystemStateCachePayload(Dictionary<string, FileState> cache)
+            {
+                var cachePayload = new FileStatePayload[cache.Count];
+                int cachePayloadCount = 0;
+
+                foreach (KeyValuePair<string, FileState> pathWithFileState in cache)
+                {
+                    string path = pathWithFileState.Key;
+                    FileState fileState = pathWithFileState.Value;
+                    cachePayload[cachePayloadCount] = ExtractFileStatePayload(fileState, path);
+                    cachePayloadCount++;
+                }
+
+                return new SystemStateCachePayload { InstanceLocalFileStateCache = cachePayload };
+            }
+
+            private static FileStatePayload ExtractFileStatePayload(FileState fileState, string path)
+            {
+                DateTimePayload lastModifiedPayload = ExtractLastModifiedPayload(fileState.LastModified);
+                AssemblyNameExtensionPayload assemblyPayload = ExtractAssemblyPayload(fileState.Assembly);
+                AssemblyNameExtensionPayload[] dependenciesPayload = ExtractDependenciesPayload(fileState.dependencies);
+                FrameworkNamePayload frameworkName = ExtractFrameworkNamePayload(fileState.frameworkName);
+
+                return new FileStatePayload
+                {
+                    Path = path,
+                    LastModified = lastModifiedPayload,
+                    Assembly = assemblyPayload,
+                    Dependencies = dependenciesPayload,
+                    ScatterFiles = fileState.scatterFiles,
+                    RuntimeVersion = fileState.RuntimeVersion,
+                    FrameworkName = frameworkName
+                };
+            }
+
+            private static DateTimePayload ExtractLastModifiedPayload(DateTime lastModified) =>
+                new DateTimePayload
+                {
+                    Ticks = lastModified.Ticks,
+                    Kind = lastModified.Kind
+                };
+
+            private static FrameworkNamePayload ExtractFrameworkNamePayload(FrameworkName frameworkName)
+            {
+                return frameworkName == null
+                    ? null
+                    : new FrameworkNamePayload
+                    {
+                        Version = frameworkName.Version.ToString(),
+                        Identifier = frameworkName.Identifier,
+                        Profile = frameworkName.Profile
+                    };
+            }
+
+            private static AssemblyNameExtensionPayload[] ExtractDependenciesPayload(AssemblyNameExtension[] dependencies)
+            {
+                if (dependencies == null)
+                {
+                    return null;
+                }
+
+                var dependenciesPayload = new AssemblyNameExtensionPayload[dependencies.Length];
+
+                for (int i = 0; i < dependencies.Length; i++)
+                {
+                    dependenciesPayload[i] = ExtractAssemblyPayload(dependencies[i]);
+                }
+
+                return dependenciesPayload;
+            }
+
+            private static AssemblyNameExtensionPayload ExtractAssemblyPayload(AssemblyNameExtension assembly)
+            {
+                if (assembly == null)
+                {
+                    return null;
+                }
+
+                AssemblyNameExtensionState assemblyNameState = assembly.GetState();
+                AssemblyNamePayload asAssemblyNamePayload = ExtractAsAssemblyNamePayload(assemblyNameState.AsAssemblyName);
+                AssemblyNameExtensionPayload[] remappedFromPayload = ExtractRemappedFromPayload(assemblyNameState.RemappedFrom);
+
+                return new AssemblyNameExtensionPayload
+                {
+                    AsAssemblyName = asAssemblyNamePayload,
+                    AsString = assemblyNameState.AsString,
+                    IsSimpleName = assemblyNameState.IsSimpleName,
+                    HasProcessorArchitectureInFusionName = assemblyNameState.HasProcessorArchitectureInFusionName,
+                    Immutable = assemblyNameState.Immutable,
+                    RemappedFrom = remappedFromPayload
+                };
+            }
+
+            private static AssemblyNameExtensionPayload[] ExtractRemappedFromPayload(HashSet<AssemblyNameExtension> remappedFrom)
+            {
+                if (remappedFrom == null)
+                {
+                    return null;
+                }
+
+                var remappedFromPayload = new AssemblyNameExtensionPayload[remappedFrom.Count];
+                int remappedFromPayloadCount = 0;
+
+                foreach (AssemblyNameExtension assemblyName in remappedFrom)
+                {
+                    remappedFromPayload[remappedFromPayloadCount] = ExtractAssemblyPayload(assemblyName);
+                    remappedFromPayloadCount++;
+                }
+
+                return remappedFromPayload;
+            }
+
+            private static AssemblyNamePayload ExtractAsAssemblyNamePayload(AssemblyName asAssemblyName)
+            {
+                if (asAssemblyName == null)
+                {
+                    return null;
+                }
+
+                VersionPayload versionPayload = ExtractVersionPayload(asAssemblyName.Version);
+                CultureInfoPayload cultureInfoPayload = ExtractCultureInfoPayload(asAssemblyName.CultureInfo);
+                StrongNameKeyPairPayload keyPairPayload = ExtractKeyPairPayload(asAssemblyName.KeyPair);
+
+                return new AssemblyNamePayload
+                {
+                    Name = asAssemblyName.Name,
+                    PublicKey = asAssemblyName.GetPublicKey(),
+                    PublicKeyToken = asAssemblyName.GetPublicKeyToken(),
+                    Version = versionPayload,
+                    Flags = asAssemblyName.Flags,
+                    ProcessorArchitecture = asAssemblyName.ProcessorArchitecture,
+                    CultureInfo = cultureInfoPayload,
+                    HashAlgorithm = asAssemblyName.HashAlgorithm,
+                    VersionCompatibility = asAssemblyName.VersionCompatibility,
+                    CodeBase = asAssemblyName.CodeBase,
+                    KeyPair = keyPairPayload
+                };
+            }
+
+            private static VersionPayload ExtractVersionPayload(Version version)
+            {
+                return version == null
+                    ? null
+                    : new VersionPayload
+                    {
+                        Major = version.Major,
+                        Minor = version.Minor,
+                        Build = version.Build,
+                        Revision = version.Revision
+                    };
+            }
+
+            private static CultureInfoPayload ExtractCultureInfoPayload(CultureInfo info)
+            {
+                return info == null ? null : new CultureInfoPayload { LCID = info.LCID };
+            }
+
+            private static StrongNameKeyPairPayload ExtractKeyPairPayload(StrongNameKeyPair keyPair)
+            {
+                return keyPair == null
+                    ? null
+                    : new StrongNameKeyPairPayload
+                    {
+                        PublicKey = keyPair.PublicKey
+                    };
+            }
+        }
+
+    }
+}
