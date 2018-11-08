22c5648ea119903c639ac5de7419ba32c7270990
Christian Castaneda <chcasta@microsoft.com>
2018-11-07 19:55:33 -0800

    Add service-to-task request/response mapper

---
+++ src/Tasks/ResolveAssemblyReferences/Services/EventQueueBuildEngine.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using Microsoft.Build.Framework;
+using Microsoft.Build.Tasks.ResolveAssemblyReferences.Domain;
+
+namespace Microsoft.Build.Tasks.ResolveAssemblyReferences.Services
+{
+    internal class EventQueueBuildEngine : IBuildEngine5
+    {
+        public List<ResolveAssemblyReferenceBuildEventArgs> BuildEventArgsQueue = new List<ResolveAssemblyReferenceBuildEventArgs>();
+
+        public bool IsRunningMultipleNodes => throw new NotImplementedException();
+
+        public bool ContinueOnError => throw new NotImplementedException();
+
+        public int LineNumberOfTaskNode => 0;
+
+        public int ColumnNumberOfTaskNode => 0;
+
+        public string ProjectFileOfTaskNode => string.Empty;
+
+        public bool BuildProjectFile(string projectFileName, string[] targetNames, IDictionary globalProperties, IDictionary targetOutputs, string toolsVersion)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool BuildProjectFile(string projectFileName, string[] targetNames, IDictionary globalProperties, IDictionary targetOutputs)
+        {
+            throw new NotImplementedException();
+        }
+
+        public BuildEngineResult BuildProjectFilesInParallel(string[] projectFileNames, string[] targetNames, IDictionary[] globalProperties, IList<string>[] removeGlobalProperties, string[] toolsVersion, bool returnTargetOutputs)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool BuildProjectFilesInParallel(string[] projectFileNames, string[] targetNames, IDictionary[] globalProperties, IDictionary[] targetOutputsPerProject, string[] toolsVersion, bool useResultsCache, bool unloadProjectsOnCompletion)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object GetRegisteredTaskObject(object key, RegisteredTaskObjectLifetime lifetime)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LogCustomEvent(CustomBuildEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LogErrorEvent(BuildErrorEventArgs e)
+        {
+            long eventTimestamp = e.UtcTimestamp.ToFileTimeUtc();
+            string[] messageArgs = ParseMessageArguments(e.UnparsedArguments);
+
+            var buildEventArgs = new ResolveAssemblyReferenceBuildEventArgs
+            {
+                BuildEventArgsType = BuildEventArgsType.Error,
+                Subcategory = e.Subcategory,
+                Code = e.Code,
+                File = e.File,
+                LineNumber = e.LineNumber,
+                ColumnNumber = e.ColumnNumber,
+                EndLineNumber = e.EndLineNumber,
+                EndColumnNumber = e.EndColumnNumber,
+                Message = e.UnformattedMessage,
+                HelpKeyword = e.HelpKeyword,
+                SenderName = e.SenderName,
+                EventTimestamp = eventTimestamp,
+                MessageArgs = messageArgs
+            };
+
+            BuildEventArgsQueue.Add(buildEventArgs);
+        }
+
+        public void LogMessageEvent(BuildMessageEventArgs e)
+        {
+            long eventTimestamp = e.UtcTimestamp.ToFileTimeUtc();
+            string[] messageArgs = ParseMessageArguments(e.UnparsedArguments);
+
+            var buildEventArgs = new ResolveAssemblyReferenceBuildEventArgs
+            {
+                BuildEventArgsType = BuildEventArgsType.Message,
+                Subcategory = e.Subcategory,
+                Code = e.Code,
+                File = e.File,
+                LineNumber = e.LineNumber,
+                ColumnNumber = e.ColumnNumber,
+                EndLineNumber = e.EndLineNumber,
+                EndColumnNumber = e.EndColumnNumber,
+                Message = e.UnformattedMessage,
+                HelpKeyword = e.HelpKeyword,
+                SenderName = e.SenderName,
+                Importance = (int)e.Importance,
+                EventTimestamp = eventTimestamp,
+                MessageArgs = messageArgs
+            };
+
+            BuildEventArgsQueue.Add(buildEventArgs);
+        }
+
+        public void LogTelemetry(string eventName, IDictionary<string, string> properties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LogWarningEvent(BuildWarningEventArgs e)
+        {
+            long eventTimestamp = e.UtcTimestamp.ToFileTimeUtc();
+            string[] messageArgs = ParseMessageArguments(e.UnparsedArguments);
+
+            var buildEventArgs = new ResolveAssemblyReferenceBuildEventArgs
+            {
+                BuildEventArgsType = BuildEventArgsType.Warning,
+                Subcategory = e.Subcategory,
+                Code = e.Code,
+                File = e.File,
+                LineNumber = e.LineNumber,
+                ColumnNumber = e.ColumnNumber,
+                EndLineNumber = e.EndLineNumber,
+                EndColumnNumber = e.EndColumnNumber,
+                Message = e.UnformattedMessage,
+                HelpKeyword = e.HelpKeyword,
+                SenderName = e.SenderName,
+                EventTimestamp = eventTimestamp,
+                MessageArgs = messageArgs
+            };
+
+            BuildEventArgsQueue.Add(buildEventArgs);
+        }
+
+        public void Reacquire()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RegisterTaskObject(object key, object obj, RegisteredTaskObjectLifetime lifetime, bool allowEarlyCollection)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object UnregisterTaskObject(object key, RegisteredTaskObjectLifetime lifetime)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Yield()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static string[] ParseMessageArguments(object[] unparsedArgs)
+        {
+            int numArgs = unparsedArgs.Length;
+            var parsedArgs = new string[numArgs];
+
+            for (int i = 0; i < numArgs; i++)
+            {
+                parsedArgs[i] = Convert.ToString(unparsedArgs[i]);
+            }
+
+            return parsedArgs;
+        }
+    }
+}
---
+++ src/Tasks/ResolveAssemblyReferences/Services/ResolveAssemblyReferenceTaskGateway.cs
@@ -0,0 +1,176 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+using Microsoft.Build.Framework;
+using Microsoft.Build.Tasks.ResolveAssemblyReferences.Abstractions;
+using Microsoft.Build.Tasks.ResolveAssemblyReferences.Domain;
+
+namespace Microsoft.Build.Tasks.ResolveAssemblyReferences.Services
+{
+    internal class ResolveAssemblyReferenceTaskGateway : IResolveAssemblyReferenceService
+    {
+        private IResolveAssemblyReferenceTask RarTask { get; }
+
+        internal ResolveAssemblyReferenceTaskGateway(IResolveAssemblyReferenceTask rarTask)
+        {
+            RarTask = rarTask;
+        }
+
+        public ResolveAssemblyReferenceResponse ResolveAssemblyReferences(ResolveAssemblyReferenceRequest req)
+        {
+            var ioTracker = new ResolveAssemblyReferenceIOTracker();
+            var buildEngine = new EventQueueBuildEngine();
+
+            ResolveAssemblyReferenceTaskInput input = ConvertRequestToTaskInput(req, ioTracker, buildEngine);
+            ResolveAssemblyReferenceTaskOutput output = RarTask.Execute(input);
+
+            return ConvertTaskOutputToResponse(output, ioTracker, buildEngine);
+        }
+
+        private static ResolveAssemblyReferenceTaskInput ConvertRequestToTaskInput(
+            ResolveAssemblyReferenceRequest req,
+            ResolveAssemblyReferenceIOTracker ioTracker,
+            EventQueueBuildEngine buildEngine
+        )
+        {
+            return new ResolveAssemblyReferenceTaskInput
+            {
+                AllowedAssemblyExtensions = req.AllowedAssemblyExtensions,
+                AllowedRelatedFileExtensions = req.AllowedRelatedFileExtensions,
+                AppConfigFile = req.AppConfigFile,
+                Assemblies = req.Assemblies,
+                AssemblyFiles = req.AssemblyFiles,
+                AutoUnify = req.AutoUnify,
+                BuildEngine = buildEngine,
+                CandidateAssemblyFiles = req.CandidateAssemblyFiles,
+                CopyLocalDependenciesWhenParentReferenceInGac = req.CopyLocalDependenciesWhenParentReferenceInGac,
+                DoNotCopyLocalIfInGac = req.DoNotCopyLocalIfInGac,
+                FindDependencies = req.FindDependencies,
+                FindDependenciesOfExternallyResolvedReferences = req.FindDependenciesOfExternallyResolvedReferences,
+                FindRelatedFiles = req.FindRelatedFiles,
+                FindSatellites = req.FindSatellites,
+                FindSerializationAssemblies = req.FindSerializationAssemblies,
+                FullFrameworkAssemblyTables = req.FullFrameworkAssemblyTables,
+                FullFrameworkFolders = req.FullFrameworkFolders,
+                FullTargetFrameworkSubsetNames = req.FullTargetFrameworkSubsetNames,
+                IgnoreDefaultInstalledAssemblySubsetTables = req.IgnoreDefaultInstalledAssemblySubsetTables,
+                IgnoreDefaultInstalledAssemblyTables = req.IgnoreDefaultInstalledAssemblyTables,
+                IgnoreTargetFrameworkAttributeVersionMismatch = req.IgnoreTargetFrameworkAttributeVersionMismatch,
+                IgnoreVersionForFrameworkReferences = req.IgnoreVersionForFrameworkReferences,
+                InstalledAssemblySubsetTables = req.InstalledAssemblySubsetTables,
+                InstalledAssemblyTables = req.InstalledAssemblyTables,
+                IoTracker = ioTracker,
+                LatestTargetFrameworkDirectories = req.LatestTargetFrameworkDirectories,
+                ProfileName = req.ProfileName,
+                ResolvedSDKReferences = req.ResolvedSDKReferences,
+                SearchPaths = req.SearchPaths,
+                ShouldExecuteInProcess = true,
+                Silent = req.Silent,
+                StateFile = req.StateFile,
+                SupportsBindingRedirectGeneration = req.SupportsBindingRedirectGeneration,
+                TargetFrameworkDirectories = req.TargetFrameworkDirectories,
+                TargetFrameworkMoniker = req.TargetFrameworkMoniker,
+                TargetFrameworkMonikerDisplayName = req.TargetFrameworkMonikerDisplayName,
+                TargetFrameworkSubsets = req.TargetFrameworkSubsets,
+                TargetFrameworkVersion = req.TargetFrameworkVersion,
+                TargetProcessorArchitecture = req.TargetProcessorArchitecture,
+                TargetedRuntimeVersion = req.TargetedRuntimeVersion,
+                UnresolveFrameworkAssembliesFromHigherFrameworks = req.UnresolveFrameworkAssembliesFromHigherFrameworks,
+                WarnOrErrorOnTargetArchitectureMismatch = req.WarnOrErrorOnTargetArchitectureMismatch,
+            };
+        }
+
+        private static ResolveAssemblyReferenceResponse ConvertTaskOutputToResponse(
+            ResolveAssemblyReferenceTaskOutput taskOutput,
+            ResolveAssemblyReferenceIOTracker ioTracker,
+            EventQueueBuildEngine buildEngine
+        )
+        {
+            int estimatedTaskItemCount = EstimateTaskItemCount(taskOutput);
+            var taskItemPayloadList = new List<ReadOnlyTaskItem>(estimatedTaskItemCount);
+            var taskItemToPayload = new Dictionary<ITaskItem, ReadOnlyTaskItem>(estimatedTaskItemCount);
+
+            ExtractTaskItemPayloadList(taskItemPayloadList, taskItemToPayload, taskOutput.CopyLocalFiles, TaskItemField.CopyLocalFiles);
+            ExtractTaskItemPayloadList(taskItemPayloadList, taskItemToPayload, taskOutput.FilesWritten, TaskItemField.FilesWritten);
+            ExtractTaskItemPayloadList(taskItemPayloadList, taskItemToPayload, taskOutput.RelatedFiles, TaskItemField.RelatedFiles);
+            ExtractTaskItemPayloadList(taskItemPayloadList, taskItemToPayload, taskOutput.ResolvedDependencyFiles, TaskItemField.ResolvedDependencyFiles);
+            ExtractTaskItemPayloadList(taskItemPayloadList, taskItemToPayload, taskOutput.ResolvedFiles, TaskItemField.ResolvedFiles);
+            ExtractTaskItemPayloadList(taskItemPayloadList, taskItemToPayload, taskOutput.SatelliteFiles, TaskItemField.SatelliteFiles);
+            ExtractTaskItemPayloadList(taskItemPayloadList, taskItemToPayload, taskOutput.ScatterFiles, TaskItemField.ScatterFiles);
+            ExtractTaskItemPayloadList(taskItemPayloadList, taskItemToPayload, taskOutput.SerializationAssemblyFiles, TaskItemField.SerializationAssemblyFiles);
+            ExtractTaskItemPayloadList(taskItemPayloadList, taskItemToPayload, taskOutput.SuggestedRedirects, TaskItemField.SuggestedRedirects);
+
+            HashSet<string> trackedPaths = ioTracker.TrackedPaths;
+            var trackedDirectories = new List<string>(trackedPaths.Count);
+            var trackedFiles = new List<string>(trackedPaths.Count);
+
+            // TODO: Investigate within RAR which delegate calls intercepted by IOTracker are guaranteed
+            // to be called with a file or directory, then let IOTracker return TrackedDirectories/TrackedFiles.
+            // Currently checking for dir separators is necessary since one of either GetDirectories or DirectoryExists
+            // appear to be called with a file path at some point, causing files to appear in TrackDirectories and
+            // eventually crashing FileSystemWatcher.
+            foreach (string path in trackedPaths)
+            {
+                if (IsDirectory(path))
+                {
+                    trackedDirectories.Add(path);
+                }
+                else
+                {
+                    trackedFiles.Add(path);
+                }
+            }
+
+            return new ResolveAssemblyReferenceResponse
+            {
+                BuildEventArgsQueue = buildEngine.BuildEventArgsQueue,
+                DependsOnNETStandard = taskOutput.DependsOnNETStandard,
+                DependsOnSystemRuntime = taskOutput.DependsOnSystemRuntime,
+                TaskItems = taskItemPayloadList,
+                TrackedDirectories = trackedDirectories,
+                TrackedFiles = trackedFiles,
+            };
+        }
+
+        private static int EstimateTaskItemCount(ResolveAssemblyReferenceTaskOutput taskOutput)
+        {
+            return taskOutput.CopyLocalFiles.Length + taskOutput.FilesWritten.Length
+                                             + taskOutput.RelatedFiles.Length + taskOutput.ResolvedDependencyFiles.Length
+                                             + taskOutput.ResolvedFiles.Length + taskOutput.SatelliteFiles.Length
+                                             + taskOutput.ScatterFiles.Length + taskOutput.SerializationAssemblyFiles.Length
+                                             + taskOutput.SuggestedRedirects.Length;
+        }
+
+        private static void ExtractTaskItemPayloadList(List<ReadOnlyTaskItem> taskItemPayloadList, Dictionary<ITaskItem, ReadOnlyTaskItem> taskItemToPayload, ITaskItem[] taskItems, TaskItemField field)
+        {
+            foreach (ITaskItem taskItem in taskItems)
+            {
+                if (!taskItemToPayload.TryGetValue(taskItem, out ReadOnlyTaskItem taskItemPayload))
+                {
+                    taskItemPayload = new ReadOnlyTaskItem(taskItem.ItemSpec);
+
+                    // TODO: Perf improvement, copying metadata accounts for a significant percentage of overhead
+                    taskItem.CopyMetadataTo(taskItemPayload);
+                    taskItemToPayload[taskItem] = taskItemPayload;
+                    taskItemPayloadList.Add(taskItemPayload);
+                }
+
+                taskItemPayload.AddResponseField(field);
+            }
+        }
+
+        private static bool IsDirectory(string path)
+        {
+            if (path.Length == 0)
+            {
+                return false;
+            }
+
+            char ch = path[path.Length - 1];
+
+            return ch == Path.DirectorySeparatorChar
+                   || ch == Path.AltDirectorySeparatorChar
+                   || ch == Path.VolumeSeparatorChar;
+        }
+    }
+}
