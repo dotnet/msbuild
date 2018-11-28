9a14094428190b7e33b59f2177e032156b3abfc7
Peter Huene <pehuene@microsoft.com>
2018-11-27 19:51:01 -0800

    Rename `_HandlePackageFileConflictsForBuild` back to `_HandlePackageFileConflicts`
    This commit renames the `_HandlePackageFileConflictsForBuild` target back to
    the original name of `_HandlePackageFileConflicts`.
    
    This prevents a failure to find the target when using a previously shipping
    `Microsoft.NET.Build.Extensions.NETFramework.targets` with a 3.0 .NET Core SDK.
    
    Fixes #2695.

--- src/Tests/Microsoft.NET.Build.Tests/GivenThatWeWantToBuildADesktopExeWtihNetStandardLib.cs
+++ src/Tests/Microsoft.NET.Build.Tests/GivenThatWeWantToBuildADesktopExeWtihNetStandardLib.cs
@@ -220,7 +220,7 @@ namespace Microsoft.NET.Build.Tests
                         // Add a target to validate that no conflicts are from support libs
                         var target = new XElement(ns + "Target",
                             new XAttribute("Name", "CheckForConflicts"),
-                            new XAttribute("AfterTargets", "_HandlePackageFileConflictsForBuild"));
+                            new XAttribute("AfterTargets", "_HandlePackageFileConflicts"));
                         project.Root.Add(target);
 
                         target.Add(new XElement(ns + "FindUnderPath",
