c8b27ca67b81a64cf06a567eeda2e8a058f621dc
Kevin Ransom <codecutter.fsharp@hotmail.com>
2018-12-06 12:56:23 -0800

    Remove telemetry

--- src/dotnet/Telemetry/TelemetryFilter.cs
+++ src/dotnet/Telemetry/TelemetryFilter.cs
@@ -87,16 +87,6 @@ namespace Microsoft.DotNet.Cli.Telemetry
             ),
             new TopLevelCommandNameAndOptionToLog
             (
-                topLevelCommandName: new HashSet<string> {"fsc"},
-                optionsToLog: new HashSet<string> {""}
-            ),
-            new TopLevelCommandNameAndOptionToLog
-            (
-                topLevelCommandName: new HashSet<string> {"fsi"},
-                optionsToLog: new HashSet<string> {""}
-            ),
-            new TopLevelCommandNameAndOptionToLog
-            (
                 topLevelCommandName: new HashSet<string> {"vstest"},
                 optionsToLog: new HashSet<string> {"platform", "framework", "logger"}
             ),
