36692cfc91744944f5afdd9a34d744c3d8c13019
Vijay Ramakrishnan <vijayrkn@users.noreply.github.com>
2018-12-21 09:20:59 -0800

    Fixing the package path (#450)
    * Fixing the package path for publish targets

--- build.cmd
+++ build.cmd
@@ -1,3 +1,4 @@
 @echo off
+xcopy \\aspnetci\share\tools\websdk\WebDeploy\* "%~dp0build\WebDeploy\*" /y /C /e /s /f
 powershell -ExecutionPolicy ByPass -NoProfile -command "& """%~dp0eng\common\Build.ps1""" -build -restore -pack -test %*"
 exit /b %ErrorLevel%
