32120d819dd568ff84252e25d6e55abd80b680b1
Ankit Jain <ankit.jain@microsoft.com>
2019-01-17 15:08:12 -0500

    [mono] cibuild_*.sh - Support building for mono
    - Some bits taken from the older `build/build.sh` script

--- eng/cibuild_bootstrapped_msbuild.sh
+++ eng/cibuild_bootstrapped_msbuild.sh
@@ -44,8 +44,36 @@ while [[ $# -gt 0 ]]; do
   esac
 done
 
+function DownloadMSBuildForMono {
+  if [[ ! -e "$mono_msbuild_dir/MSBuild.dll" ]]
+  then
+    mkdir -p $artifacts_dir
+    echo "** Downloading MSBUILD from $msbuild_download_url"
+    curl -sL -o "$msbuild_zip" "$msbuild_download_url"
+
+    unzip -q "$msbuild_zip" -d "$artifacts_dir"
+    # rename just to make it obvious when reading logs!
+    mv $artifacts_dir/msbuild $mono_msbuild_dir
+    chmod +x $artifacts_dir/mono-msbuild/MSBuild.dll
+    rm "$msbuild_zip"
+  fi
+}
+
 RepoRoot="$ScriptRoot/.."
 artifacts_dir="$RepoRoot/artifacts"
+
+mono_msbuild_dir="$artifacts_dir/mono-msbuild"
+msbuild_download_url="https://github.com/mono/msbuild/releases/download/0.06/mono_msbuild_xplat-master-3c930fa8.zip"
+msbuild_zip="$artifacts_dir/msbuild.zip"
+
+if [ $host_type = "mono" ] ; then
+  DownloadMSBuildForMono
+
+  export _InitializeBuildTool="mono"
+  export _InitializeMSBuildToUse="$mono_msbuild_dir/MSBuild.dll"
+
+  configuration="$configuration-MONO"
+fi
 
 if [[ $build_stage1 == true ]];
 then
@@ -59,6 +87,16 @@ export artifacts_dir="$artifacts_dir/2"
 if [ $host_type = "core" ]
 then
 	_InitializeMSBuildToUse="$bootstrapRoot/netcoreapp2.1/MSBuild/MSBuild.dll"
+elif [ $host_type = "mono" ]
+then
+  export _InitializeBuildTool="mono"
+  export _InitializeMSBuildToUse="$bootstrapRoot/net472/MSBuild/Current/Bin/MSBuild.dll"
+
+  # FIXME: remove this once we move to a newer version of Arcade with a fix for $MonoTool
+  # https://github.com/dotnet/arcade/commit/f6f14c169ba19cd851120e0d572cd1c5619205b3
+  export MonoTool=`which mono`
+
+  properties="$properties /p:MSBuildExtensionsPath=$bootstrapRoot/net472/MSBuild "
 else
   echo "Unsupported hostType ($host_type)"
   exit 1
