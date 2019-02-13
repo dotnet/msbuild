cb575dfc35e5276d9b36ffa25eed8ef550dba2fc
Jacques Eloff <joeloff@users.noreply.github.com>
2019-02-13 14:48:56 -0800

    Pull WebDeployment dependencies through a package referece (#482)
    * Pull WebDeployment dependencies through a package referece
    
    * Add build from source condition

--- build.cmd
+++ build.cmd
@@ -1,4 +1,3 @@
 @echo off
-xcopy \\aspnetci\share\tools\websdk\WebDeploy\* "%~dp0build\WebDeploy\*" /y /C /e /s /f
 powershell -ExecutionPolicy ByPass -NoProfile -command "& """%~dp0eng\common\Build.ps1""" -build -restore -pack -test %*"
 exit /b %ErrorLevel%
