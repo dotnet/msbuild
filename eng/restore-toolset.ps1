6055574318be25abcab08888a8386035574bcb1e
Daniel Plaisted <daplaist@microsoft.com>
2019-02-20 14:03:46 -0800

    Install 2.2.2 shared framework

--- eng/restore-toolset.ps1
+++ eng/restore-toolset.ps1
@@ -18,7 +18,7 @@ function InitializeCustomSDKToolset {
   InstallDotNetSharedFramework "1.0.5"
   InstallDotNetSharedFramework "1.1.2"
   InstallDotNetSharedFramework "2.1.0"
-  InstallDotNetSharedFramework "2.2.1"
+  InstallDotNetSharedFramework "2.2.2"
 
   CreateBuildEnvScript
   InstallNuget
