17c476cf131deee7243504b97388ee4c7c843c03
Rainer Sigwald <raines@microsoft.com>
2019-04-30 14:58:17 -0500

    Skeleton for TDD

---
+++ src/Tasks.UnitTests/ResourceHandling/MSBuildResXReader_Tests.cs
@@ -0,0 +1,47 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Build.Tasks.ResourceHandling;
+using Microsoft.Build.Tasks.UnitTests.ResourceHandling;
+using Microsoft.Build.UnitTests;
+using Shouldly;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Microsoft.Build.Tasks.UnitTests.GenerateResource
+{
+    public class MSBuildResXReader_Tests
+    {
+        private readonly ITestOutputHelper _output;
+
+        public MSBuildResXReader_Tests(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        [Fact]
+        public void StringResourcesAreReadAsStrings()
+        {
+            var resxWithSingleString = new MSBuildResXReader(
+                ResXHelper.SurroundWithBoilerplate(
+                    @"<data name=""StringResource"" xml:space=""preserve"">
+    <value>StringValue</value>
+    <comment>Comment</comment>
+  </data>"));
+
+            resxWithSingleString.Resources.ShouldHaveSingleItem();
+            resxWithSingleString.Resources[0].ShouldBeOfType<StringResource>();
+
+            var loadedResource = (StringResource)resxWithSingleString.Resources[0];
+
+            loadedResource.Name.ShouldBe("StringResource");
+            loadedResource.Value.ShouldBe("StringValue");
+        }
+    }
+}
---
+++ src/Tasks/ResourceHandling/MSBuildResXReader.cs
@@ -0,0 +1,46 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace Microsoft.Build.Tasks.ResourceHandling
+{
+    internal class MSBuildResXReader
+    {
+        public IReadOnlyList<IResource> Resources { get; }
+
+        public MSBuildResXReader(Stream s, string filename)
+        {
+            // TODO: is it ok to hardcode the "shouldUseSourcePath" behavior?
+
+            var resources = new List<IResource>();
+
+            using (var xmlReader = new XmlTextReader(s))
+            {
+                xmlReader.WhitespaceHandling = WhitespaceHandling.None;
+                XDocument doc = XDocument.Load(xmlReader, LoadOptions.PreserveWhitespace);
+                foreach (XElement dataElem in doc.Element("root").Elements("data"))
+                {
+                    string name = dataElem.Attribute("name").Value;
+                    string value = dataElem.Element("value").Value;
+
+                    resources.Add(new StringResource(name, value, filename));
+                }
+            }
+
+            Resources = resources;
+        }
+
+        public MSBuildResXReader(Stream s) : this(s, null)
+        { }
+    }
+}
