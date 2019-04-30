979a0a6e6b587840e9cd741076f91a6401b689fc
Rainer Sigwald <raines@microsoft.com>
2019-04-30 17:23:04 -0500

    start thinking about resxfileref

--- src/Tasks/ResourceHandling/MSBuildResXReader.cs
+++ src/Tasks/ResourceHandling/MSBuildResXReader.cs
@@ -49,7 +49,7 @@ namespace Microsoft.Build.Tasks.ResourceHandling
             Resources = resources;
         }
 
-        private static void ParseData(string filename, List<IResource> resources, XElement elem)
+        private static void ParseData(string resxFilename, List<IResource> resources, XElement elem)
         {
             string name = elem.Attribute("name").Value;
             string value = elem.Element("value").Value;
@@ -59,11 +59,68 @@ namespace Microsoft.Build.Tasks.ResourceHandling
 
             if (typename == null && mimetype == null)
             {
-                resources.Add(new StringResource(name, value, filename));
+                resources.Add(new StringResource(name, value, resxFilename));
+            }
+            else if (typename == "System.Resources.ResXFileRef, System.Windows.Forms")
+            {
+                string[] fileRefInfo = ParseResxFileRefString(value);
+
+                string filename = fileRefInfo[0];
+                string fileReftype = fileRefInfo[1];
+                string fileRefEncoding = fileRefInfo[2];
+
+                throw new NotImplementedException();
             }
         }
 
         public MSBuildResXReader(Stream s) : this(s, null)
         { }
+
+        // From https://github.com/dotnet/winforms/blob/a88c1a73fd7298b0a5c45251771f439262016826/src/System.Windows.Forms/src/System/Resources/ResXFileRef.cs#L187-L220
+        internal static string[] ParseResxFileRefString(string stringValue)
+        {
+            string[] result = null;
+            if (stringValue != null)
+            {
+                stringValue = stringValue.Trim();
+                string fileName;
+                string remainingString;
+                if (stringValue.StartsWith("\""))
+                {
+                    int lastIndexOfQuote = stringValue.LastIndexOf("\"");
+                    if (lastIndexOfQuote - 1 < 0)
+                        throw new ArgumentException(nameof(stringValue));
+                    fileName = stringValue.Substring(1, lastIndexOfQuote - 1); // remove the quotes in" ..... "
+                    if (lastIndexOfQuote + 2 > stringValue.Length)
+                        throw new ArgumentException(nameof(stringValue));
+                    remainingString = stringValue.Substring(lastIndexOfQuote + 2);
+                }
+                else
+                {
+                    int nextSemiColumn = stringValue.IndexOf(";");
+                    if (nextSemiColumn == -1)
+                        throw new ArgumentException(nameof(stringValue));
+                    fileName = stringValue.Substring(0, nextSemiColumn);
+                    if (nextSemiColumn + 1 > stringValue.Length)
+                        throw new ArgumentException(nameof(stringValue));
+                    remainingString = stringValue.Substring(nextSemiColumn + 1);
+                }
+                string[] parts = remainingString.Split(';');
+                if (parts.Length > 1)
+                {
+                    result = new string[] { fileName, parts[0], parts[1] };
+                }
+                else if (parts.Length > 0)
+                {
+                    result = new string[] { fileName, parts[0] };
+                }
+                else
+                {
+                    result = new string[] { fileName };
+                }
+            }
+            return result;
+        }
+
     }
 }
