840186c38076f673cb4c9de3e1893b7a01cdc7c1
Phillip Carter <pcarter@fastmail.com>
2019-05-08 13:41:48 -0700

    Update Program.fs

--- template_feed/Microsoft.DotNet.Common.ProjectTemplates.3.0/content/ConsoleApplication-FSharp/Program.fs
+++ template_feed/Microsoft.DotNet.Common.ProjectTemplates.3.0/content/ConsoleApplication-FSharp/Program.fs
@@ -2,13 +2,12 @@
 
 open System
 
-
 // Define a function to print a message
-let from who =
-    sprintf "from %s" message
+let from whom =
+    sprintf "from %s" whom
 
 [<EntryPoint>]
 let main argv =
-    let nmessage = from "F#" // Call the function
-    printMessage "Hello world %s" message
+    let message = from "F#" // Call the function
+    printfn "Hello world %s" message
     0 // return an integer exit code
