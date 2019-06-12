fce5096afdec48b54dfd504f2a897933beb640d6
William Li <wul@microsoft.com>
2019-06-12 12:59:05 -0700

    Collect crash exception (#3318)

---
+++ src/Tests/Microsoft.NET.Build.Tests/GivenThatWeWantToCollectExceptionTelemetry.cs
@@ -0,0 +1,38 @@
+﻿using FluentAssertions;
+using Microsoft.NET.TestFramework;
+using Microsoft.NET.TestFramework.Assertions;
+using Microsoft.NET.TestFramework.Commands;
+using System;
+using System.IO;
+using Xunit.Abstractions;
+using System.Reflection;
+
+namespace Microsoft.NET.Build.Tests
+{
+    public class GivenThatWeWantToCollectExceptionTelemetry : SdkTest
+    {
+        public GivenThatWeWantToCollectExceptionTelemetry(ITestOutputHelper log) : base(log)
+        {
+        }
+
+        [CoreMSBuildAndWindowsOnlyFact]
+        public void It_collects_Exception()
+        {
+            Type loggerType = typeof(LogTelemetryToStdOutForTest);
+            string telemetryTestLogger = $"/Logger:{loggerType.FullName},{loggerType.GetTypeInfo().Assembly.Location}";
+
+            var testAsset = _testAssetsManager.CopyTestAsset("HelloWorld").WithSource().Restore(Log);
+
+            var mSBuildCommand = new MSBuildCommand(Log, "GenerateToolsSettingsFileFromBuildProperty", Path.Combine(testAsset.TestRoot));
+
+            string invalidPath = @"\\.\COM56";
+            string causeTaskToFail = $"/p:_ToolsSettingsFilePath={invalidPath}";
+
+            mSBuildCommand
+                .Execute(telemetryTestLogger, causeTaskToFail)
+                .StdOut.Should()
+                .Contain("\"EventName\":\"taskBaseCatchException\",\"Properties\":{\"exceptionType\":\"System.IO.FileNotFoundException\"")
+                .And.Contain("detail");
+        }
+    }
+}
