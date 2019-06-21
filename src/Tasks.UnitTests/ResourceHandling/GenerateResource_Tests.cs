dotnet/msbuild

commit f8330fdc1273efc1b9585fe3852dede1f9fc797b

Fix intermittent unit test issue

src/Tasks.UnitTests/GenerateResource_Tests.cs
@@ -20,12 +20,14 @@ namespace Microsoft.Build.UnitTests.GenerateResource_Tests.InProc
 {
     [Trait("Category", "mono-osx-failing")]
     [Trait("Category", "mono-windows-failing")]
-    sealed public class RequiredTransformations
+    public sealed class RequiredTransformations
     {
+        private readonly TestEnvironment _env;
         private readonly ITestOutputHelper _output;
 
         public RequiredTransformations(ITestOutputHelper output)
         {
+            _env = TestEnvironment.Create(output);
             _output = output;
         }
 
@@ -303,56 +305,41 @@ namespace Microsoft.Build.UnitTests.GenerateResource_Tests.InProc
         [Trait("Category", "netcore-linux-failing")]
         public void ForceOutOfDate()
         {
-            string resxFile = Utilities.WriteTestResX(false, null, null);
+            var folder = _env.CreateFolder();
+            string resxFile = Utilities.WriteTestResX(false, null, null, _env.CreateFile(folder, ".resx").Path);
 
             GenerateResource t = Utilities.CreateTask(_output);
-            t.StateFile = new TaskItem(Utilities.GetTempFileName(".cache"));
+            t.StateFile = new TaskItem(_env.GetTempFile(".cache").Path);
+            t.Sources = new ITaskItem[] {new TaskItem(resxFile)};
 
-            try
-            {
-                t.Sources = new ITaskItem[] { new TaskItem(resxFile) };
+            Utilities.ExecuteTask(t);
 
-                Utilities.ExecuteTask(t);
-
-                string resourcesFile = t.OutputResources[0].ItemSpec;
-                Assert.Equal(Path.GetExtension(resourcesFile), ".resources");
-                resourcesFile = t.FilesWritten[0].ItemSpec;
-                Assert.Equal(Path.GetExtension(resourcesFile), ".resources");
+            string resourcesFile = t.OutputResources[0].ItemSpec;
+            Assert.Equal(Path.GetExtension(resourcesFile), ".resources");
+            resourcesFile = t.FilesWritten[0].ItemSpec;
+            Assert.Equal(Path.GetExtension(resourcesFile), ".resources");
 
 #if FEATURE_RESGENCACHE
                 Utilities.AssertStateFileWasWritten(t);
 #endif
-                GenerateResource t2 = Utilities.CreateTask(_output);
-                t2.StateFile = new TaskItem(t.StateFile);
-                t2.Sources = new ITaskItem[] { new TaskItem(resxFile) };
+            GenerateResource t2 = Utilities.CreateTask(_output);
+            t2.StateFile = new TaskItem(t.StateFile);
+            t2.Sources = new ITaskItem[] {new TaskItem(resxFile)};
 
-                DateTime time = File.GetLastWriteTime(t.OutputResources[0].ItemSpec);
+            DateTime time = File.GetLastWriteTime(t.OutputResources[0].ItemSpec);
 
-                System.Threading.Thread.Sleep(200);
-                if (NativeMethodsShared.IsOSX)
-                {
-                    // Must be > 1 sec for HFS+ timestamp granularity
-                    System.Threading.Thread.Sleep(1100);
-                }
-
-                File.SetLastWriteTime(resxFile, DateTime.Now);
-
-                Utilities.ExecuteTask(t2);
-
-                Assert.True(DateTime.Compare(File.GetLastWriteTime(t2.OutputResources[0].ItemSpec), time) > 0);
-            }
-            finally
+            System.Threading.Thread.Sleep(200);
+            if (NativeMethodsShared.IsOSX)
             {
-                // Done, so clean up.
-                File.Delete(t.Sources[0].ItemSpec);
-                foreach (ITaskItem item in t.FilesWritten)
-                {
-                    if (File.Exists(item.ItemSpec))
-                    {
-                        File.Delete(item.ItemSpec);
-                    }
-                }
+                // Must be > 1 sec for HFS+ timestamp granularity
+                System.Threading.Thread.Sleep(1100);
             }
+
+            File.SetLastWriteTime(resxFile, DateTime.Now);
+
+            Utilities.ExecuteTask(t2);
+
+            Assert.True(DateTime.Compare(File.GetLastWriteTime(t2.OutputResources[0].ItemSpec), time) > 0);
         }
 
         /// <summary>
@@ -419,63 +406,50 @@ namespace Microsoft.Build.UnitTests.GenerateResource_Tests.InProc
         [Fact]
         public void ForceSomeOutOfDate()
         {
-            string firstResx = null;
-            string secondResx = null;
-            string cache = null;
+            var folder = _env.CreateFolder();
 
-            try
+            var firstResx = Utilities.WriteTestResX(false, null, null, _env.CreateFile(folder, ".resx").Path);
+            var secondResx = Utilities.WriteTestResX(false, null, null, _env.CreateFile(folder, ".resx").Path);
+            var cache = _env.GetTempFile(folder, ".cache").Path;
+
+            GenerateResource createResources = Utilities.CreateTask(_output);
+            createResources.StateFile = new TaskItem(cache);
+            createResources.Sources = new ITaskItem[] {new TaskItem(firstResx), new TaskItem(secondResx)};
+
+            _output.WriteLine("Transform both");
+            Utilities.ExecuteTask(createResources);
+
+            _output.WriteLine("Get current write times of outputs");
+            DateTime firstOutputCreationTime = File.GetLastWriteTime(createResources.OutputResources[0].ItemSpec);
+            DateTime secondOutputCreationTime = File.GetLastWriteTime(createResources.OutputResources[1].ItemSpec);
+
+            _output.WriteLine("Create a new task to transform them again");
+            GenerateResource t2 = Utilities.CreateTask(_output);
+            t2.StateFile = new TaskItem(createResources.StateFile.ItemSpec);
+            t2.Sources = new ITaskItem[] {new TaskItem(firstResx), new TaskItem(secondResx)};
+
+            System.Threading.Thread.Sleep(200);
+            if (!NativeMethodsShared.IsWindows)
             {
-                firstResx = Utilities.WriteTestResX(false, null, null);
-                secondResx = Utilities.WriteTestResX(false, null, null);
-                cache = Utilities.GetTempFileName(".cache");
-
-                GenerateResource createResources = Utilities.CreateTask(_output);
-                createResources.StateFile = new TaskItem(cache);
-                createResources.Sources = new ITaskItem[] { new TaskItem(firstResx), new TaskItem(secondResx) };
-
-                _output.WriteLine("Transform both");
-                Utilities.ExecuteTask(createResources);
-
-                _output.WriteLine("Get current write times of outputs");
-                DateTime firstOutputCreationTime = File.GetLastWriteTime(createResources.OutputResources[0].ItemSpec);
-                DateTime secondOutputCreationTime = File.GetLastWriteTime(createResources.OutputResources[1].ItemSpec);
-
-                _output.WriteLine("Create a new task to transform them again");
-                GenerateResource t2 = Utilities.CreateTask(_output);
-                t2.StateFile = new TaskItem(createResources.StateFile.ItemSpec);
-                t2.Sources = new ITaskItem[] { new TaskItem(firstResx), new TaskItem(secondResx) };
-
-                System.Threading.Thread.Sleep(200);
-                if (!NativeMethodsShared.IsWindows)
-                {
-                    // Must be > 1 sec on some file systems for proper timestamp granularity
-                    // TODO: Implement an interface for fetching deterministic timestamps rather than relying on the file
-                    System.Threading.Thread.Sleep(1000);
-                }
-
-                _output.WriteLine("Touch one input");
-                File.SetLastWriteTime(firstResx, DateTime.Now);
-
-                Utilities.ExecuteTask(t2);
-
-                _output.WriteLine("Check only one output was updated");
-                File.GetLastWriteTime(t2.OutputResources[0].ItemSpec).ShouldBeGreaterThan(firstOutputCreationTime);
-                File.GetLastWriteTime(t2.OutputResources[1].ItemSpec).ShouldBe(secondOutputCreationTime);
-
-                // Although only one file was updated, both should be in OutputResources and FilesWritten
-                t2.OutputResources[0].ItemSpec.ShouldBe(createResources.OutputResources[0].ItemSpec);
-                t2.OutputResources[1].ItemSpec.ShouldBe(createResources.OutputResources[1].ItemSpec);
-                t2.FilesWritten[0].ItemSpec.ShouldBe(createResources.FilesWritten[0].ItemSpec);
-                t2.FilesWritten[1].ItemSpec.ShouldBe(createResources.FilesWritten[1].ItemSpec);
+                // Must be > 1 sec on some file systems for proper timestamp granularity
+                // TODO: Implement an interface for fetching deterministic timestamps rather than relying on the file
+                System.Threading.Thread.Sleep(1000);
             }
-            finally
-            {
-                if (null != firstResx) File.Delete(firstResx);
-                if (null != secondResx) File.Delete(secondResx);
-                if (null != cache) File.Delete(cache);
-                if (null != firstResx) File.Delete(Path.ChangeExtension(firstResx, ".resources"));
-                if (null != secondResx) File.Delete(Path.ChangeExtension(secondResx, ".resources"));
-            }
+
+            _output.WriteLine("Touch one input");
+            File.SetLastWriteTime(firstResx, DateTime.Now);
+
+            Utilities.ExecuteTask(t2);
+
+            _output.WriteLine("Check only one output was updated");
+            File.GetLastWriteTime(t2.OutputResources[0].ItemSpec).ShouldBeGreaterThan(firstOutputCreationTime);
+            File.GetLastWriteTime(t2.OutputResources[1].ItemSpec).ShouldBe(secondOutputCreationTime);
+
+            // Although only one file was updated, both should be in OutputResources and FilesWritten
+            t2.OutputResources[0].ItemSpec.ShouldBe(createResources.OutputResources[0].ItemSpec);
+            t2.OutputResources[1].ItemSpec.ShouldBe(createResources.OutputResources[1].ItemSpec);
+            t2.FilesWritten[0].ItemSpec.ShouldBe(createResources.FilesWritten[0].ItemSpec);
+            t2.FilesWritten[1].ItemSpec.ShouldBe(createResources.FilesWritten[1].ItemSpec);
         }
 
         /// <summary>
@@ -3472,12 +3446,9 @@ namespace Microsoft.Build.UnitTests.GenerateResource_Tests
         /// <param name="useType">Indicates whether to include an enum to test type-specific resource encoding with assembly references</param>
         /// <param name="linkedBitmap">The name of a linked-in bitmap.  use 'null' for no bitmap.</param>
         /// <returns>The name of the resx file</returns>
-        public static string WriteTestResX(bool useType, string linkedBitmap, string extraToken)
+        public static string WriteTestResX(bool useType, string linkedBitmap, string extraToken, string resxFileToWrite = null)
         {
-            string resgenFile = Utilities.GetTempFileName(".resx");
-            File.Delete(resgenFile);
-            File.WriteAllText(resgenFile, GetTestResXContent(useType, linkedBitmap, extraToken, false));
-            return resgenFile;
+            return WriteTestResX(useType, linkedBitmap, extraToken, useInvalidType: false, resxFileToWrite:resxFileToWrite);
         }
 
         /// <summary>
@@ -3486,10 +3457,15 @@ namespace Microsoft.Build.UnitTests.GenerateResource_Tests
         /// <param name="useType">Indicates whether to include an enum to test type-specific resource encoding with assembly references</param>
         /// <param name="linkedBitmap">The name of a linked-in bitmap.  use 'null' for no bitmap.</param>
         /// <returns>The name of the resx file</returns>
-        public static string WriteTestResX(bool useType, string linkedBitmap, string extraToken, bool useInvalidType)
+        public static string WriteTestResX(bool useType, string linkedBitmap, string extraToken, bool useInvalidType, string resxFileToWrite = null)
         {
-            string resgenFile = Utilities.GetTempFileName(".resx");
-            File.Delete(resgenFile);
+            string resgenFile = resxFileToWrite;
+            if (string.IsNullOrEmpty(resgenFile))
+            {
+                resgenFile = GetTempFileName(".resx");
+                File.Delete(resgenFile);
+            }
+
             File.WriteAllText(resgenFile, GetTestResXContent(useType, linkedBitmap, extraToken, useInvalidType));
             return resgenFile;
         }

src/Shared/UnitTests/TestEnvironment.cs
@@ -175,7 +175,7 @@ namespace Microsoft.Build.UnitTests
         /// <param name="extension">Extensions of the file (defaults to '.tmp')</param>
         public TransientTestFile CreateFile(string extension = ".tmp")
         {
-            return WithTransientTestState(new TransientTestFile(extension));
+            return WithTransientTestState(new TransientTestFile(extension, createFile:true, expectedAsOutput:false));
         }
 
         /// <summary>
@@ -186,7 +186,52 @@ namespace Microsoft.Build.UnitTests
         /// <param name="extension">Extension of the file (defaults to '.tmp')</param>
         public TransientTestFile CreateFile(TransientTestFolder transientTestFolder, string extension = ".tmp")
         {
-            return WithTransientTestState(new TransientTestFile(transientTestFolder.FolderPath, extension));
+            return WithTransientTestState(new TransientTestFile(transientTestFolder.FolderPath, extension,
+                createFile: true, expectedAsOutput: false));
+        }
+
+
+        /// <summary>
+        ///     Gets a transient test file associated with a unique file name but does not create the file.
+        /// </summary>
+        /// <param name="extension">Extension of the file (defaults to '.tmp')</param>
+        /// <returns></returns>
+        public TransientTestFile GetTempFile(string extension = ".tmp")
+        {
+            return WithTransientTestState(new TransientTestFile(extension, createFile: false, expectedAsOutput: false));
+        }
+
+        /// <summary>
+        ///     Gets a transient test file under a specified folder associated with a unique file name but does not create the file.
+        /// </summary>
+        /// <param name="transientTestFolder">Temp folder</param>
+        /// <param name="extension">Extension of the file (defaults to '.tmp')</param>
+        /// <returns></returns>
+        public TransientTestFile GetTempFile(TransientTestFolder transientTestFolder, string extension = ".tmp")
+        {
+            return WithTransientTestState(new TransientTestFile(transientTestFolder.FolderPath, extension,
+                createFile: false, expectedAsOutput: false));
+        }
+
+        /// <summary>
+        ///     Create a temp file name that is expected to exist when the test completes.
+        /// </summary>
+        /// <param name="extension">Extension of the file (defaults to '.tmp')</param>
+        /// <returns></returns>
+        public TransientTestFile ExpectFile(string extension = ".tmp")
+        {
+            return WithTransientTestState(new TransientTestFile(extension, createFile: false, expectedAsOutput: true));
+        }
+
+        /// <summary>
+        /// Create a temp file name under a specific temporary folder. The file is expected to exist when the test completes.
+        /// </summary>
+        /// <param name="transientTestFolder">Temp folder</param>
+        /// <param name="extension">Extension of the file (defaults to '.tmp')</param>
+        /// <returns></returns>
+        public TransientTestFile ExpectFile(TransientTestFolder transientTestFolder, string extension = ".tmp")
+        {
+            return WithTransientTestState(new TransientTestFile(transientTestFolder.FolderPath, extension, createFile: false, expectedAsOutput: true));
         }
 
         /// <summary>
@@ -392,21 +437,38 @@ namespace Microsoft.Build.UnitTests
 
     public class TransientTestFile : TransientTestState
     {
-        public TransientTestFile(string extension)
+        private readonly bool _createFile;
+        private readonly bool _expectedAsOutput;
+
+        public TransientTestFile(string extension, bool createFile, bool expectedAsOutput)
         {
-            Path = FileUtilities.GetTemporaryFile(extension);
+            _createFile = createFile;
+            _expectedAsOutput = expectedAsOutput;
+            Path = FileUtilities.GetTemporaryFile(null, extension, createFile);
         }
 
-        public TransientTestFile(string rootPath, string extension)
+        public TransientTestFile(string rootPath, string extension, bool createFile, bool expectedAsOutput)
         {
-            Path = FileUtilities.GetTemporaryFile(rootPath, extension);
+            _createFile = createFile;
+            _expectedAsOutput = expectedAsOutput;
+            Path = FileUtilities.GetTemporaryFile(rootPath, extension, createFile);
         }
 
         public string Path { get; }
 
         public override void Revert()
         {
-            FileUtilities.DeleteNoThrow(Path);
+            try
+            {
+                if (_expectedAsOutput)
+                {
+                    Assert.True(File.Exists(Path), $"A file expected as an output does not exist: {Path}");
+                }
+            }
+            finally
+            {
+                FileUtilities.DeleteNoThrow(Path);
+            }
         }
     }
 

commit 8065d64c2cf4bc19524a7b18e6522742ea0cafe8

Dispose test env in RequiredTransformations
Fixes a possible test-time crash introduced in f8330fdc: there's no
specified ordering for running the finalizer of _env, so sometimes it
might run after another Dispose cleaned up the test environment, and
then attempt to throw an assertion exception at a poorly-specified time,
leading to a crash in the test process instead of a test failure.

src/Tasks.UnitTests/ResourceHandling/GenerateResource_Tests.cs
@@ -20,7 +20,7 @@ namespace Microsoft.Build.UnitTests.GenerateResource_Tests.InProc
 {
     [Trait("Category", "mono-osx-failing")]
     [Trait("Category", "mono-windows-failing")]
-    public sealed class RequiredTransformations
+    public sealed class RequiredTransformations : IDisposable
     {
         private readonly TestEnvironment _env;
         private readonly ITestOutputHelper _output;
@@ -29,6 +29,11 @@ namespace Microsoft.Build.UnitTests.GenerateResource_Tests.InProc
         {
             _env = TestEnvironment.Create(output);
             _output = output;
+        }
+
+        public void Dispose()
+        {
+            _env.Dispose();
         }
 
         /// <summary>