e00f4025fd1fb7c69450b27a6160ddcd0245ff5f
William Li <wul@microsoft.com>
2019-06-24 20:02:27 -0700

    Upload to PerfCommand (#3349)
    Instead of benchview. And changed the format of the result.
    Use a csproj and dotnet run in Helix to do convert and uploading instead of python scripts

--- build/RunPerfTestsOnHelix.cmd
+++ build/RunPerfTestsOnHelix.cmd
@@ -1,8 +1,8 @@
 @echo on
 setlocal EnableDelayedExpansion
 
-if "%BV_UPLOAD_SAS_TOKEN%" == "" (
-    echo EnvVar BV_UPLOAD_SAS_TOKEN should be set; exiting...
+if "%PERF_COMMAND_UPLOAD_TOKEN%" == "" (
+    echo EnvVar PERF_COMMAND_UPLOAD_TOKEN should be set; exiting...
     exit /b 1)
 if "%HELIX_CORRELATION_PAYLOAD%" == "" (
     echo EnvVar HELIX_CORRELATION_PAYLOAD should be set; exiting...
@@ -14,6 +14,8 @@ set GIT_COMMIT=%3
 set GIT_BRANCH=%4
 set runType=%5
 set TestFullMSBuild=%6
+set HelixTargetQueues=%7
+set BuildNumber=%8
 
 REM  Since dotnet.exe was locked; we exclude it from the helix-payload.
 REM    Run a restore to re-install the SDK.
@@ -40,15 +42,14 @@ IF %ERRORLEVEL% GTR 0 exit %ERRORLEVEL%
 ENDLOCAL
 echo "Performance tests completed"
 
-REM  Upload the performance data to BenchView.
+REM  Upload the performance data
 set perfWorkingDirectory=%HELIX_CORRELATION_PAYLOAD%\artifacts\TestResults\%configuration%\Performance
 set architecture=%PROCESSOR_ARCHITECTURE%
 if "%PROCESSOR_ARCHITECTURE%"=="AMD64" (set architecture=x64)
 
-echo "Uploading data to Benchview: uploadperftobenchview.cmd"
+echo "Uploading data: uploadperfresult.cmd"
 pushd %HELIX_CORRELATION_PAYLOAD%
-%HELIX_CORRELATION_PAYLOAD%\build\uploadperftobenchview.cmd
+%HELIX_CORRELATION_PAYLOAD%\build\uploadperfresult.cmd
 IF %ERRORLEVEL% GTR 0 exit %ERRORLEVEL%
-echo "Upload to Benchview completed"
 
 exit /b %ErrorLevel%
