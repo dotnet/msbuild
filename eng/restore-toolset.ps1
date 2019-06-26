eff71e6937da5ee6880d0a65f616403ab5559c18
Nick Guerrera <nicholg@microsoft.com>
2019-06-26 16:28:20 -0700

    Use live core-setup dependency for codeflow from framework

--- eng/restore-toolset.ps1
+++ eng/restore-toolset.ps1
@@ -28,9 +28,6 @@ function InitializeCustomSDKToolset {
   }
 
   $cli = InitializeDotnetCli -install:$true
-  InstallDotNetSharedFramework "1.1.2"
-  InstallDotNetSharedFramework "2.0.0"
-  InstallDotNetSharedFramework "2.2.0"
 
   CreateBuildEnvScript
   CreateTestEnvScript
