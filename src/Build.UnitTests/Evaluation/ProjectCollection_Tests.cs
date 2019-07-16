65cdddba758eaea4046970bf7b1b51c6697d8c0b
Christian Castaneda <chcasta@microsoft.com>
2019-07-16 16:40:25 -0700

    Add test to assert switch on environment variable

---
+++ src/Build.UnitTests/Evaluation/ProjectCollection_Tests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.Build.Evaluation;
+using Shouldly;
+using Xunit;
+
+namespace Microsoft.Build.UnitTests.Evaluation
+{
+    public class ProjectCollection_Tests
+    {
+        [Fact]
+        public void ProjectRootElementCache_IsDeterminedByEnvironmentVariable()
+        {
+            var collectionWithDefaultCache = new ProjectCollection();
+            collectionWithDefaultCache.ProjectRootElementCache.ShouldBeOfType<ProjectRootElementCache>();
+
+            const string envKey = "MsBuildUseSimpleProjectRootElementCacheConcurrency";
+            string originalEnvVar = Environment.GetEnvironmentVariable(envKey);
+
+            try
+            {
+                Environment.SetEnvironmentVariable(envKey, "true");
+
+                var collectionWithSimpleCache = new ProjectCollection();
+                collectionWithSimpleCache.ProjectRootElementCache.ShouldBeOfType<SimpleProjectRootElementCache>();
+            }
+            finally
+            {
+                Environment.SetEnvironmentVariable(envKey, originalEnvVar);
+            }
+        }
+    }
+}
