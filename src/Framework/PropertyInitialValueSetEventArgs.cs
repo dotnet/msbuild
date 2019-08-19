7150dc9643fbb2651f973e0cf7efcbfe31a8b806
Matt Neely <49730556+maneely@users.noreply.github.com>
2019-08-19 13:59:31 -0700

    Adding tracking of properties with relevant events. (#4461)
    This change introduces opt-in functionality that will track property usage and generate three events:
    
    Environment Variables Read (new)
    Uninitialized Properties Read (new)
    Property Reassigned (already exists)
    
    Opt-in for these events is via the environment variable, MSBuildLogPropertyTracking.

---
+++ src/Framework/PropertyInitialValueSetEventArgs.cs
@@ -0,0 +1,54 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+
+namespace Microsoft.Build.Framework
+{
+    /// <summary>
+    /// The argument for a property initial value set event.
+    /// </summary>
+    [Serializable]
+    public class PropertyInitialValueSetEventArgs : BuildMessageEventArgs
+    {
+        /// <summary>
+        /// Creates an instance of the <see cref="PropertyInitialValueSetEventArgs"/> class.
+        /// </summary>
+        public PropertyInitialValueSetEventArgs() { }
+
+        /// <summary>
+        /// Creates an instance of the <see cref="PropertyInitialValueSetEventArgs"/> class.
+        /// </summary>
+        /// <param name="propertyName">The name of the property.</param>
+        /// <param name="propertyValue">The value of the property.</param>
+        /// <param name="propertySource">The source of the property.</param>
+        public PropertyInitialValueSetEventArgs(
+            string propertyName,
+            string propertyValue,
+            string propertySource,
+            string message,
+            string helpKeyword = null,
+            string senderName = null,
+            MessageImportance importance = MessageImportance.Low) : base(message, helpKeyword, senderName, importance)
+        {
+            this.PropertyName = propertyName;
+            this.PropertyValue = propertyValue;
+            this.PropertySource = propertySource;
+        }
+
+        /// <summary>
+        /// The name of the property.
+        /// </summary>
+        public string PropertyName { get; set; }
+
+        /// <summary>
+        /// The value of the property.
+        /// </summary>
+        public string PropertyValue { get; set; }
+
+        /// <summary>
+        /// The source of the property.
+        /// </summary>
+        public string PropertySource { get; set; }
+    }
+}
