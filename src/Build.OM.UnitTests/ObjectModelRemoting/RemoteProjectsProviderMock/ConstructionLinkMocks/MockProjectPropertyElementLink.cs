34ef9828489aef05f6659e98b229b436f57eb6a8
Svetlozar Keremidarski <svetkere@microsoft.com>
2019-08-28 15:07:04 -0700

    Object Model Remoting: Test coverage

---
+++ src/Build.OM.UnitTests/ObjectModelRemoting/RemoteProjectsProviderMock/ConstructionLinkMocks/MockProjectPropertyElementLink.cs
@@ -0,0 +1,70 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Microsoft.Build.UnitTests.OM.ObjectModelRemoting
+{
+    using System.Collections.Generic;
+    using Microsoft.Build.Construction;
+    using Microsoft.Build.ObjectModelRemoting;
+
+    internal class MockProjectPropertyElementLinkRemoter : MockProjectElementLinkRemoter
+    {
+        public ProjectPropertyElement PropertyXml => (ProjectPropertyElement)Source;
+
+        public override ProjectElement ImportImpl(ProjectCollectionLinker remote)
+        {
+            return remote.Import<ProjectElement, MockProjectPropertyElementLinkRemoter>(this);
+        }
+
+        public override ProjectElement CreateLinkedObject(IImportHolder holder)
+        {
+            var link = new MockProjectPropertyElementLink(this, holder);
+            return holder.Linker.LinkFactory.Create(link);
+        }
+
+        // ProjectPropertyElementLink support
+        public string Value { get => PropertyXml.Value; set => PropertyXml.Value = value; }
+        public void ChangeName(string newName) { PropertyXml.Name = newName; }
+
+    }
+
+    internal class MockProjectPropertyElementLink : ProjectPropertyElementLink, ILinkMock, IProjectElementLinkHelper
+    {
+        public MockProjectPropertyElementLink(MockProjectPropertyElementLinkRemoter proxy, IImportHolder holder)
+        {
+            this.Holder = holder;
+            this.Proxy = proxy;
+        }
+
+        public IImportHolder Holder { get; }
+        public ProjectCollectionLinker Linker => this.Holder.Linker;
+        public MockProjectPropertyElementLinkRemoter Proxy { get; }
+        object ILinkMock.Remoter => this.Proxy;
+        MockProjectElementLinkRemoter IProjectElementLinkHelper.ElementProxy => this.Proxy;
+
+        public override string Value { get => this.Proxy.Value; set => this.Proxy.Value = value; }
+        public override void ChangeName(string newName)
+        {
+            this.Proxy.ChangeName(newName);
+        }
+
+        #region ProjectElementLink redirectors
+        private IProjectElementLinkHelper EImpl => (IProjectElementLinkHelper)this;
+        public override ProjectElementContainer Parent => EImpl.GetParent();
+        public override ProjectRootElement ContainingProject => EImpl.GetContainingProject();
+        public override string ElementName => EImpl.GetElementName();
+        public override string OuterElement => EImpl.GetOuterElement();
+        public override bool ExpressedAsAttribute { get => EImpl.GetExpressedAsAttribute(); set => EImpl.SetExpressedAsAttribute(value); }
+        public override ProjectElement PreviousSibling => EImpl.GetPreviousSibling();
+        public override ProjectElement NextSibling => EImpl.GetNextSibling();
+        public override ElementLocation Location => EImpl.GetLocation();
+        public override IReadOnlyCollection<XmlAttributeLink> Attributes => EImpl.GetAttributes();
+        public override string PureText => EImpl.GetPureText();
+        public override void CopyFrom(ProjectElement element) => EImpl.CopyFrom(element);
+        public override ProjectElement CreateNewInstance(ProjectRootElement owner) => EImpl.CreateNewInstance(owner);
+        public override ElementLocation GetAttributeLocation(string attributeName) => EImpl.GetAttributeLocation(attributeName);
+        public override string GetAttributeValue(string attributeName, bool nullIfNotExists) => EImpl.GetAttributeValue(attributeName, nullIfNotExists);
+        public override void SetOrRemoveAttribute(string name, string value, bool clearAttributeCache, string reason, string param) => EImpl.SetOrRemoveAttribute(name, value, clearAttributeCache, reason, param);
+        #endregion
+    }
+}
---
+++ src/Build.OM.UnitTests/ObjectModelRemoting/RemoteProjectsProviderMock/ConstructionLinkMocks/ProjectElemetExportHelper.cs
@@ -0,0 +1,136 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Microsoft.Build.UnitTests.OM.ObjectModelRemoting
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Runtime.CompilerServices;
+    using System.Xml;
+    using Microsoft.Build.Construction;
+    using Microsoft.Build.ObjectModelRemoting;
+
+
+    /// <summary>
+    /// We need to know the actual type of ProjectElements in order to do a proper remoting.
+    /// Unless we do some explicit ProjectElement.GetXMLType() thing we need to use heuristic.
+    ///
+    /// Most of the types has a single implementation, but few has a wrapper classes. They are also internal for MSbuild.
+    /// </summary>
+    internal static class ProjectElemetExportHelper
+    {
+        delegate MockProjectElementLinkRemoter ExporterFactory(ProjectCollectionLinker exporter, ProjectElement xml);
+        private class ElementInfo
+        {
+            public static ElementInfo New<T, RMock>()
+                where RMock : MockProjectElementLinkRemoter, new()
+            {
+                return new ElementInfo(typeof(T), IsOfType<T>, Export<RMock>);
+            }
+
+            public ElementInfo(Type type, Func<ProjectElement, bool> checker, ExporterFactory factory)
+            {
+                this.CanonicalType = type;
+                this.Checker = checker;
+                this.ExportFactory = factory;
+            }
+
+
+            public ElementInfo(Func<ProjectElement, bool> checker, ExporterFactory factory)
+            {
+                this.Checker = checker;
+                this.ExportFactory = factory;
+            }
+
+            public Type CanonicalType { get; }
+            public Func<ProjectElement, bool> Checker { get; }
+            public ExporterFactory ExportFactory { get; }
+        }
+
+        private static List<ElementInfo> canonicalTypes = new List<ElementInfo>()
+        {
+            ElementInfo.New<ProjectRootElement               , MockProjectRootElementLinkRemoter>(),
+            ElementInfo.New<ProjectChooseElement             , MockProjectChooseElementLinkRemoter>(),
+            ElementInfo.New<ProjectExtensionsElement         , MockProjectExtensionsElementLinkRemoter>(),
+            ElementInfo.New<ProjectImportElement             , MockProjectImportElementLinkRemoter>(),
+            ElementInfo.New<ProjectImportGroupElement        , MockProjectImportGroupElementLinkRemoter>(),
+            ElementInfo.New<ProjectItemDefinitionGroupElement, MockProjectItemDefinitionGroupElementLinkRemoter>(),
+            ElementInfo.New<ProjectItemElement               , MockProjectItemElementLinkRemoter>(),
+            ElementInfo.New<ProjectItemGroupElement          , MockProjectItemGroupElementLinkRemoter>(),
+            ElementInfo.New<ProjectMetadataElement           , MockProjectMetadataElementLinkRemoter>(),
+            ElementInfo.New<ProjectOnErrorElement            , MockProjectOnErrorElementLinkRemoter>(),
+            ElementInfo.New<ProjectOtherwiseElement          , MockProjectOtherwiseElementLinkRemoter>(),
+            ElementInfo.New<ProjectOutputElement             , MockProjectOutputElementLinkRemoter>(),
+            ElementInfo.New<ProjectPropertyElement           , MockProjectPropertyElementLinkRemoter>(),
+            ElementInfo.New<ProjectPropertyGroupElement      , MockProjectPropertyGroupElementLinkRemoter>(),
+            ElementInfo.New<ProjectSdkElement                , MockProjectSdkElementLinkRemoter>(),
+            ElementInfo.New<ProjectTargetElement             , MockProjectTargetElementLinkRemoter>(),
+            ElementInfo.New<ProjectTaskElement               , MockProjectTaskElementLinkRemoter>(),
+            ElementInfo.New<ProjectUsingTaskBodyElement      , MockProjectUsingTaskBodyElementLinkRemoter>(),
+            ElementInfo.New<ProjectItemDefinitionElement     , MockProjectItemDefinitionElementLinkRemoter>(),
+            ElementInfo.New<ProjectUsingTaskElement          , MockProjectUsingTaskElementLinkRemoter>(),
+            ElementInfo.New<ProjectUsingTaskParameterElement , MockProjectUsingTaskParameterElementLinkRemoter>(),
+            ElementInfo.New<ProjectWhenElement               , MockProjectWhenElementLinkRemoter>(),
+            ElementInfo.New<UsingTaskParameterGroupElement   , MockUsingTaskParameterGroupElementLinkRemoter>(),
+        };
+
+        private static MockProjectElementLinkRemoter Export<RMock>(ProjectCollectionLinker exporter, ProjectElement xml)
+            where RMock : MockProjectElementLinkRemoter, new()
+        {
+            return exporter.Export<ProjectElement, RMock>(xml);
+        }
+
+        private static bool IsOfType<T> (ProjectElement xml) { return xml is T; }
+
+        private static Dictionary<Type, ExporterFactory> knownTypes = new Dictionary<Type, ExporterFactory>();
+
+        static ProjectElemetExportHelper()
+        {
+            foreach (var v in canonicalTypes)
+            {
+                knownTypes.Add(v.CanonicalType, v.ExportFactory);
+            }
+        }
+
+
+        private static MockProjectElementLinkRemoter NotImplemented(ProjectCollectionLinker exporter, ProjectElement xml)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static MockProjectElementLinkRemoter ExportElement(this ProjectCollectionLinker exporter, ProjectElement xml)
+        {
+            if (xml == null)
+            {
+                return null;
+            }
+
+            var implType = xml.GetType();
+            if (knownTypes.TryGetValue(implType, out var factory))
+            {
+                return factory(exporter, xml);
+            }
+
+            factory = NotImplemented;
+
+            foreach (var t in canonicalTypes)
+            {
+                if (t.Checker(xml))
+                {
+                    factory = t.ExportFactory;
+                    break;
+                }
+            }
+
+            lock (knownTypes)
+            {
+                var newKnown = new Dictionary<Type, ExporterFactory>(knownTypes);
+                newKnown[implType] = factory;
+                knownTypes = newKnown;
+            }
+
+            return factory(exporter, xml);
+        }
+
+    }
+}
