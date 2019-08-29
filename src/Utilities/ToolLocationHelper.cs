0e1a3249fa4dfda12b5ba8ba691f2e6cdf1d0e3d
Nathan Mytelka <Forgind@users.noreply.github.com>
2019-08-28 17:57:44 -0700

    Update framework version to 4.8
    First attempt. Known to fail tests.

--- src/Utilities/ToolLocationHelper.cs
+++ src/Utilities/ToolLocationHelper.cs
@@ -100,6 +100,11 @@ namespace Microsoft.Build.Utilities
         Version472 = 13,
 
         /// <summary>
+        /// version 4.7.2
+        /// </summary>
+        Version48 = 14,
+
+        /// <summary>
         /// The latest version available at the time of major release. This
         /// value should not be updated in minor releases as it could be a
         /// breaking change. Use 'Latest' if possible, but note the
@@ -2038,6 +2043,8 @@ namespace Microsoft.Build.Utilities
                     return FrameworkLocationHelper.dotNetFrameworkVersion471;
 
                 case TargetDotNetFrameworkVersion.Version472:
+                    return FrameworkLocationHelper.dotNetFrameworkVersion472;
+                case TargetDotNetFrameworkVersion.Version48:
                 case TargetDotNetFrameworkVersion.Latest: // Latest is a special value to indicate the highest version we know about.
                     return FrameworkLocationHelper.dotNetFrameworkVersion472;
 
