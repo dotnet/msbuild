ee7655f936555a105a2722d612f395c8075e3b4e
Livar Cunha <livar@live.com>
2019-09-17 22:29:33 -0700

    Change the installed 2.2 runtime to 2.2.7.

--- eng/restore-toolset.ps1
+++ eng/restore-toolset.ps1
@@ -18,7 +18,7 @@ function InitializeCustomSDKToolset {
   InstallDotNetSharedFramework "1.0.5"
   InstallDotNetSharedFramework "1.1.2"
   InstallDotNetSharedFramework "2.1.0"
-  InstallDotNetSharedFramework "2.2.6"
+  InstallDotNetSharedFramework "2.2.7"
 
   CreateBuildEnvScript
   InstallNuget
