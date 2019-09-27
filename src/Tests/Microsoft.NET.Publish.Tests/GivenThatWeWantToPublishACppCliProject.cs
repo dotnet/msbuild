785987a3c144dd7d1bc44ef920f4797aca80a8d2
Nick Guerrera <nicholg@microsoft.com>
2019-09-26 17:16:19 -0700

    Retarget C++/CLI tests to 3.1

--- src/Tests/Microsoft.NET.Publish.Tests/GivenThatWeWantToPublishACppCliProject.cs
+++ src/Tests/Microsoft.NET.Publish.Tests/GivenThatWeWantToPublishACppCliProject.cs
@@ -35,7 +35,7 @@ namespace Microsoft.NET.Build.Tests
             var exe = Path.Combine( //find the platform directory
                 new DirectoryInfo(Path.Combine(testAsset.TestRoot, "CSConsoleApp", "bin")).GetDirectories().Single().FullName,
                 "Debug",
-                "netcoreapp3.0",
+                "netcoreapp3.1",
                 "publish",
                 "CSConsoleApp.exe");
 
