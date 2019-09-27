27d218d41c959acd1e1ac93bace15ef11e7c85fe
Nick Guerrera <nicholg@microsoft.com>
2019-09-26 17:08:38 -0700

    Remove work around that is no longer needed

--- src/Tests/Microsoft.NET.Publish.Tests/GivenThatWeWantToPublishACppCliProject.cs
+++ src/Tests/Microsoft.NET.Publish.Tests/GivenThatWeWantToPublishACppCliProject.cs
@@ -27,8 +27,6 @@ namespace Microsoft.NET.Build.Tests
                 .CopyTestAsset("NetCoreCsharpAppReferenceCppCliLib")
                 .WithSource();
 
-            WorkaroundSDKBlockOnAssetsJsonExistence(testAsset);
-
             new PublishCommand(Log, Path.Combine(testAsset.TestRoot, "CSConsoleApp"))
                 .Execute(new string[] { "-p:Platform=x64" })
                 .Should()
@@ -57,22 +55,11 @@ namespace Microsoft.NET.Build.Tests
                 .CopyTestAsset("NetCoreCsharpAppReferenceCppCliLib")
                 .WithSource();
 
-            WorkaroundSDKBlockOnAssetsJsonExistence(testAsset);
-
             new PublishCommand(Log, Path.Combine(testAsset.TestRoot, "NETCoreCppCliTest"))
                 .Execute(new string[] { "-p:Platform=x64" })
                 .Should()
                 .Fail()
                 .And.HaveStdOutContaining(Strings.NoSupportCppPublishDotnetCore);
-        }
-
-        private static void WorkaroundSDKBlockOnAssetsJsonExistence(TestAsset testAsset)
-        {
-            var lockFile = new LockFile();
-            lockFile.Targets.Add(new LockFileTarget { TargetFramework = NuGetFramework.Parse(".NETCoreApp,Version=v3.0") });
-
-            var objDirectory = Directory.CreateDirectory(Path.Combine(testAsset.TestRoot, "NETCoreCppCliTest", "obj"));
-            new LockFileFormat().Write(objDirectory.File("project.assets.json").FullName, lockFile);
         }
     }
 }
