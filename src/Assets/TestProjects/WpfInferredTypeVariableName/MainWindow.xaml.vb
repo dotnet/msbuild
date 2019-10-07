631b06faa064205944d2856d396317c0e6dd8cc4
Sarah Oslund <sfoslund@microsoft.com>
2019-10-07 13:00:40 -0700

    Added test confiming DisableImplicitNamespaceImports property solves inferred type variable name issue

---
+++ src/Assets/TestProjects/WpfInferredTypeVariableName/MainWindow.xaml.vb
@@ -0,0 +1,12 @@
+﻿Option Strict On
+Option Infer On
+Imports System.Windows
+
+Class MainWindow
+    Sub Foo()
+        Dim dataObject As DataObject
+        For Each format In dataObject.GetFormats()
+
+        Next
+    End Sub
+End Class
