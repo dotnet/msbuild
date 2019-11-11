1f4524283e4e7f0dfae9a83557bf551c8a28cab9
William Li <wul@microsoft.com>
2019-11-11 15:19:44 -0800

    Import few property in netframework props for core (#4895)
    * Update deploy script to include props and targets
    
    * Extract Microsoft.NET.props and let .netcore import it

--- scripts/Deploy-MSBuild.ps1
+++ scripts/Deploy-MSBuild.ps1
@@ -37,7 +37,31 @@ $filesToCopyToBin = @(
     "artifacts\bin\MSBuild.Bootstrap\$configuration\net472\Microsoft.Build.Tasks.Core.dll"
     "artifacts\bin\MSBuild.Bootstrap\$configuration\net472\Microsoft.Build.Utilities.Core.dll"
     "artifacts\bin\MSBuildTaskHost\$configuration\net35\MSBuildTaskHost.exe"
-    "artifacts\bin\MSBuildTaskHost\$configuration\net35\MSBuildTaskHost.pdb")
+    "artifacts\bin\MSBuildTaskHost\$configuration\net35\MSBuildTaskHost.pdb"
+
+    "artifacts\bin\MSBuild.Bootstrap\$configuration\net472\Microsoft.Common.CrossTargeting.targets"
+    "artifacts\bin\MSBuild.Bootstrap\$configuration\net472\Microsoft.Common.CurrentVersion.targets"
+    "artifacts\bin\MSBuild.Bootstrap\$configuration\net472\Microsoft.Common.targets"
+    "artifacts\bin\MSBuild.Bootstrap\$configuration\net472\Microsoft.CSharp.CrossTargeting.targets"
+    "artifacts\bin\MSBuild.Bootstrap\$configuration\net472\Microsoft.CSharp.CurrentVersion.targets"
+    "artifacts\bin\MSBuild.Bootstrap\$configuration\net472\Microsoft.CSharp.targets"
+    "artifacts\bin\MSBuild.Bootstrap\$configuration\net472\Microsoft.Data.Entity.targets"
+    "artifacts\bin\MSBuild.Bootstrap\$configuration\net472\Microsoft.Managed.targets"
+    "artifacts\bin\MSBuild.Bootstrap\$configuration\net472\Microsoft.Net.props"
+    "artifacts\bin\MSBuild.Bootstrap\$configuration\net472\Microsoft.NetFramework.CurrentVersion.props"
+    "artifacts\bin\MSBuild.Bootstrap\$configuration\net472\Microsoft.NetFramework.CurrentVersion.targets"
+    "artifacts\bin\MSBuild.Bootstrap\$configuration\net472\Microsoft.NetFramework.props"
+    "artifacts\bin\MSBuild.Bootstrap\$configuration\net472\Microsoft.NetFramework.targets"
+    "artifacts\bin\MSBuild.Bootstrap\$configuration\net472\Microsoft.ServiceModel.targets"
+    "artifacts\bin\MSBuild.Bootstrap\$configuration\net472\Microsoft.VisualBasic.CrossTargeting.targets"
+    "artifacts\bin\MSBuild.Bootstrap\$configuration\net472\Microsoft.VisualBasic.CurrentVersion.targets"
+    "artifacts\bin\MSBuild.Bootstrap\$configuration\net472\Microsoft.VisualBasic.targets"
+    "artifacts\bin\MSBuild.Bootstrap\$configuration\net472\Microsoft.WinFx.targets"
+    "artifacts\bin\MSBuild.Bootstrap\$configuration\net472\Microsoft.WorkflowBuildExtensions.targets"
+    "artifacts\bin\MSBuild.Bootstrap\$configuration\net472\Microsoft.Xaml.targets"
+    "artifacts\bin\MSBuild.Bootstrap\$configuration\net472\Workflow.targets"
+    "artifacts\bin\MSBuild.Bootstrap\$configuration\net472\Workflow.VisualBasic.targets"
+)
 
 foreach ($file in $filesToCopyToBin) {
     Copy-WithBackup $([IO.Path]::Combine($PSScriptRoot, "..", $file))
