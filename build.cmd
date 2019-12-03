cefe0ecb71d5767636d519c95e5e42e8f6735f04
Nathan Mytelka <Forgind@users.noreply.github.com>
2019-12-03 13:47:06 -0800

    Discourage building with MSBUILDDEBUGONSTART

--- build.cmd
+++ build.cmd
@@ -1,3 +1,6 @@
 @echo off
+set CACHEDMSBUILDDEBUGONSTART=%MSBUILDDEBUGONSTART%
+set MSBUILDDEBUGONSTART=0
 powershell -NoLogo -NoProfile -ExecutionPolicy ByPass -Command "& """%~dp0eng\common\build.ps1""" -build -restore %*"
+set MSBUILDDEBUGONSTART=%CACHEDMSBUILDDEBUGONSTART%
 exit /b %ErrorLevel%
