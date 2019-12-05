0e92a747960716a20b4770cb2a705454050fa5bf
Forgind <Forgind@users.noreply.github.com>
2019-12-05 08:23:56 -0800

    Update build.cmd
    Co-Authored-By: Rainer Sigwald <raines@microsoft.com>

--- build.cmd
+++ build.cmd
@@ -3,7 +3,7 @@ set CACHEDMSBUILDDEBUGONSTART=%MSBUILDDEBUGONSTART%
 if defined MSBUILDDEBUGONSTART goto build
 set /p ans=Did you mean to have MSBUILDDEBUGONSTART equal %MSBUILDDEBUGONSTART%? (y/n) 
 if %ans% == y goto build
-set MSBUILDDEBUGONSTART=0
+set MSBUILDDEBUGONSTART=
 :build
 powershell -NoLogo -NoProfile -ExecutionPolicy ByPass -Command "& """%~dp0eng\common\build.ps1""" -build -restore %*"
 set MSBUILDDEBUGONSTART=%CACHEDMSBUILDDEBUGONSTART%
