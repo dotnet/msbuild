a441c6b586ef76ed536a3a5c62fc95337b656fd3
Daniel Plaisted <daplaist@microsoft.com>
2019-12-06 14:03:59 -0800

    Port `dotnet new` tests

--- src/Tests/dotnet-new.Tests/GivenThatIWantANewApp.cs
+++ src/Tests/dotnet-new.Tests/GivenThatIWantANewApp.cs
@@ -8,26 +8,34 @@ using System.Xml.Linq;
 using FluentAssertions;
 using Microsoft.DotNet.Tools.Test.Utilities;
 using Microsoft.Extensions.DependencyModel;
+using Microsoft.NET.TestFramework;
+using Microsoft.NET.TestFramework.Assertions;
+using Microsoft.NET.TestFramework.Commands;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace Microsoft.DotNet.New.Tests
 {
-    public class GivenThatIWantANewApp : TestBase
+    public class GivenThatIWantANewApp : SdkTest
     {
+        public GivenThatIWantANewApp(ITestOutputHelper log) : base(log)
+        {
+        }
+
         [Fact]
         public void When_dotnet_new_is_invoked_multiple_times_it_should_fail()
         {
-            var rootPath = TestAssets.CreateTestDirectory().FullName;
+            var rootPath = _testAssetsManager.CreateTestDirectory().Path;
 
-            new NewCommand()
+            new DotnetCommand(Log, "new")
                 .WithWorkingDirectory(rootPath)
-                .Execute($"console --debug:ephemeral-hive --no-restore");
+                .Execute($"console", "--debug:ephemeral-hive", "--no-restore");
 
             DateTime expectedState = Directory.GetLastWriteTime(rootPath);
 
-            var result = new NewCommand()
+            var result = new DotnetCommand(Log, "new")
                 .WithWorkingDirectory(rootPath)
-                .ExecuteWithCapturedOutput($"console --debug:ephemeral-hive --no-restore");
+                .Execute($"console", "--debug:ephemeral-hive", "--no-restore");
 
             DateTime actualState = Directory.GetLastWriteTime(rootPath);
 
--- src/Tests/dotnet-new.Tests/NewCommandTests.cs
+++ src/Tests/dotnet-new.Tests/NewCommandTests.cs
@@ -3,20 +3,27 @@
 
 using FluentAssertions;
 using Microsoft.DotNet.Tools.Test.Utilities;
+using Microsoft.NET.TestFramework;
+using Microsoft.NET.TestFramework.Commands;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace Microsoft.DotNet.New.Tests
 {
-    public class NewCommandTests : TestBase
+    public class NewCommandTests : SdkTest
     {
+        public NewCommandTests(ITestOutputHelper log) : base(log)
+        {
+        }
+
         [Fact]
         public void WhenSwitchIsSkippedThenItPrintsError()
         {
-            var cmd = new DotnetCommand().Execute("new Web1.1");
+            var cmd = new DotnetCommand(Log).Execute("new", "Web1.1");
 
             cmd.ExitCode.Should().NotBe(0);
 
-            if (!DotnetUnderTest.IsLocalized())
+            if (!TestContext.IsLocalized())
             {
                 cmd.StdErr.Should().StartWith("No templates matched the input template name: Web1.1.");
             }
@@ -25,11 +32,11 @@ namespace Microsoft.DotNet.New.Tests
         [Fact(Skip = "https://github.com/dotnet/templating/issues/1971")]
         public void WhenTemplateNameIsNotUniquelyMatchedThenItIndicatesProblemToUser()
         {
-            var cmd = new DotnetCommand().Execute("new c");
+            var cmd = new DotnetCommand(Log).Execute("new", "c");
 
             cmd.ExitCode.Should().NotBe(0);
 
-            if (!DotnetUnderTest.IsLocalized())
+            if (!TestContext.IsLocalized())
             {
                 cmd.StdErr.Should().StartWith("Unable to determine the desired template from the input template name: c.");
             }
