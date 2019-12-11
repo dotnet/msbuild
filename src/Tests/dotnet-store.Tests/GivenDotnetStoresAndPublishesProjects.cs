55b05c1efbeffd48bdb757c022bf0f0074753b1e
Daniel Plaisted <daplaist@microsoft.com>
2019-12-11 13:45:41 -0800

    Port `dotnet store` tests

--- src/Tests/dotnet-store.Tests/GivenDotnetStoresAndPublishesProjects.cs
+++ src/Tests/dotnet-store.Tests/GivenDotnetStoresAndPublishesProjects.cs
@@ -3,18 +3,25 @@
 
 using FluentAssertions;
 using Microsoft.DotNet.PlatformAbstractions;
-using Microsoft.DotNet.TestFramework;
 using Microsoft.DotNet.Tools.Test.Utilities;
+using Microsoft.NET.TestFramework;
+using Microsoft.NET.TestFramework.Assertions;
+using Microsoft.NET.TestFramework.Commands;
 using System;
 using System.IO;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace Microsoft.DotNet.Cli.Publish.Tests
 {
-    public class GivenDotnetStoresAndPublishesProjects : TestBase
+    public class GivenDotnetStoresAndPublishesProjects : SdkTest
     {
         private static string _tfm = "netcoreapp3.0";
         private static string _arch = RuntimeEnvironment.RuntimeArchitecture.ToLowerInvariant();
+
+        public GivenDotnetStoresAndPublishesProjects(ITestOutputHelper log) : base(log)
+        {
+        }
 
         [Fact(Skip = "https://github.com/dotnet/cli/issues/12482")]
         public void ItPublishesARunnablePortableApp()
@@ -22,46 +29,44 @@ namespace Microsoft.DotNet.Cli.Publish.Tests
             var testAppName = "NewtonSoftDependentProject";
             var profileProjectName = "NewtonsoftProfile";
 
-            var testInstance = TestAssets.Get(testAppName)
-                .CreateInstance()
-                .WithSourceFiles();
+            var testInstance = _testAssetsManager.CopyTestAsset(testAppName)
+                .WithSource();
 
-            var testProjectDirectory = testInstance.Root.FullName;
+            var testProjectDirectory = testInstance.Path;
             var rid = EnvironmentInfo.GetCompatibleRid();
             var localAssemblyCache = Path.Combine(testProjectDirectory, "localAssemblyCache");
             var intermediateWorkingDirectory = Path.Combine(testProjectDirectory, "workingDirectory");
-            var profileProjectPath = TestAssets.Get(profileProjectName).Root.FullName;
+            var profileProjectPath = _testAssetsManager.CopyTestAsset(profileProjectName).WithSource().Path;
             var profileProject = Path.Combine(profileProjectPath, $"{profileProjectName}.xml");
 
-            new RestoreCommand()
-                .WithWorkingDirectory(testProjectDirectory)
+            new RestoreCommand(Log, testProjectDirectory)
                 .Execute()
                 .Should().Pass();
 
-            new StoreCommand()
-                .WithManifest(profileProject)
-                .WithFramework(_tfm)
-                .WithRuntime(rid)
-                .WithOutput(localAssemblyCache)
-                .WithIntermediateWorkingDirectory(intermediateWorkingDirectory)
+            new DotnetCommand(Log, "store",
+                    "--manifest", profileProject,
+                    "-f", _tfm,
+                    "-r", rid,
+                    "-o", localAssemblyCache,
+                    "-w", intermediateWorkingDirectory)
                 .Execute()
                 .Should().Pass();
 
             var configuration = Environment.GetEnvironmentVariable("CONFIGURATION") ?? "Debug";
             var profileFilter = Path.Combine(localAssemblyCache, _arch, _tfm, "artifact.xml");
 
-            new PublishCommand()
-                .WithFramework(_tfm)
+            new DotnetPublishCommand(Log,
+                    "-f", _tfm,
+                    "--manifest", profileFilter)
                 .WithWorkingDirectory(testProjectDirectory)
-                .WithTargetManifest(profileFilter)
                 .Execute()
                 .Should().Pass();
 
             var outputDll = Path.Combine(testProjectDirectory, "bin", configuration, _tfm, "publish", $"{testAppName}.dll");
 
-            new DotnetCommand()
+            new DotnetCommand(Log)
                 .WithEnvironmentVariable("DOTNET_SHARED_STORE", localAssemblyCache)
-                .ExecuteWithCapturedOutput(outputDll)
+                .Execute(outputDll)
                 .Should().Pass()
                 .And.HaveStdOutContaining("{}");
         }
@@ -73,32 +78,31 @@ namespace Microsoft.DotNet.Cli.Publish.Tests
             var profileProjectName = "NuGetConfigProfile";
             var targetManifestFileName = "NuGetConfigFilterProfile.xml";
 
-            var testInstance = TestAssets.Get(testAppName)
-                .CreateInstance()
-                .WithSourceFiles();
+            var testInstance = _testAssetsManager.CopyTestAsset(testAppName)
+                .WithSource();
 
-            var testProjectDirectory = testInstance.Root.FullName;
-            var profileProjectPath = TestAssets.Get(profileProjectName).Root.FullName;
+            var testProjectDirectory = testInstance.Path;
+            var profileProjectPath = _testAssetsManager.CopyTestAsset(profileProjectName).WithSource().Path;
             var profileFilter = Path.Combine(profileProjectPath, targetManifestFileName);
 
-            new RestoreCommand()
+            new DotnetRestoreCommand(Log)
                 .WithWorkingDirectory(testProjectDirectory)
                 .Execute()
                 .Should().Pass();
 
             var configuration = Environment.GetEnvironmentVariable("CONFIGURATION") ?? "Debug";
 
-            new PublishCommand()
-                .WithFramework(_tfm)
+            new DotnetPublishCommand(Log,
+                    "-f", _tfm,
+                    "--manifest", profileFilter)
                 .WithWorkingDirectory(testProjectDirectory)
-                .WithTargetManifest(profileFilter)
                 .Execute()
                 .Should().Pass();
 
             var outputDll = Path.Combine(testProjectDirectory, "bin", configuration, _tfm, "publish", $"{testAppName}.dll");
 
-            new DotnetCommand()
-                .ExecuteWithCapturedOutput(outputDll)
+            new DotnetCommand(Log)
+                .Execute(outputDll)
                 .Should().Fail()
                 .And.HaveStdErrContaining($"Error:{Environment.NewLine}" +
                     $"  An assembly specified in the application dependencies manifest (NuGetConfigDependentProject.deps.json) was not found:{Environment.NewLine}" +
@@ -114,53 +118,51 @@ namespace Microsoft.DotNet.Cli.Publish.Tests
             var profileProjectName = "NewtonsoftProfile";
             var profileProjectName1 = "FluentProfile";
 
-            var testInstance = TestAssets.Get(testAppName)
-                .CreateInstance()
-                .WithSourceFiles();
+            var testInstance = _testAssetsManager.CopyTestAsset(testAppName)
+                .WithSource();
 
-            var testProjectDirectory = testInstance.Root.FullName;
+            var testProjectDirectory = testInstance.Path;
             var rid = EnvironmentInfo.GetCompatibleRid();
             var localAssemblyCache = Path.Combine(testProjectDirectory, "lAC");
             var intermediateWorkingDirectory = Path.Combine(testProjectDirectory, "workingDirectory");
 
-            var profileProjectPath = TestAssets.Get(profileProjectName).Root.FullName;
+            var profileProjectPath = _testAssetsManager.CopyTestAsset(profileProjectName).WithSource().Path;
             var profileProject = Path.Combine(profileProjectPath, $"{profileProjectName}.xml");
             var profileFilter = Path.Combine(profileProjectPath, "NewtonsoftFilterProfile.xml");
 
-            var profileProjectPath1 = TestAssets.Get(profileProjectName1).Root.FullName; 
+            var profileProjectPath1 = _testAssetsManager.CopyTestAsset(profileProjectName1).WithSource().Path; 
             var profileProject1 = Path.Combine(profileProjectPath1, $"{profileProjectName1}.xml");
             var profileFilter1 = Path.Combine(profileProjectPath1, "FluentFilterProfile.xml");
 
-            new RestoreCommand()
-                .WithWorkingDirectory(testProjectDirectory)
+            new RestoreCommand(Log, testProjectDirectory)
                 .Execute()
                 .Should().Pass();
 
-            new StoreCommand()
-                .WithManifest(profileProject)
-                .WithManifest(profileProject1)
-                .WithFramework(_tfm)
-                .WithRuntime(rid)
-                .WithOutput(localAssemblyCache)
-                .WithIntermediateWorkingDirectory(intermediateWorkingDirectory)
+            new DotnetCommand(Log, "store",
+                    "--manifest", profileProject,
+                    "--manifest", profileProject1,
+                    "-f", _tfm,
+                    "-r", rid,
+                    "-o", localAssemblyCache,
+                    "-w", intermediateWorkingDirectory)
                 .Execute()
                 .Should().Pass();
 
             var configuration = Environment.GetEnvironmentVariable("CONFIGURATION") ?? "Debug";
 
-            new PublishCommand()
-                .WithFramework(_tfm)
+            new DotnetPublishCommand(Log,
+                    "-f", _tfm,
+                    "--manifest", profileFilter,
+                    "--manifest", profileFilter1)
                 .WithWorkingDirectory(testProjectDirectory)
-                .WithTargetManifest(profileFilter)
-                .WithTargetManifest(profileFilter1)
                 .Execute()
                 .Should().Pass();
 
             var outputDll = Path.Combine(testProjectDirectory, "bin", configuration, _tfm, "publish", $"{testAppName}.dll");
 
-            new DotnetCommand()
+            new DotnetCommand(Log)
                 .WithEnvironmentVariable("DOTNET_SHARED_STORE", localAssemblyCache)
-                .ExecuteWithCapturedOutput(outputDll)
+                .Execute(outputDll)
                 .Should().Pass()
                 .And.HaveStdOutContaining("{}");
         }
