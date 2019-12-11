7f6b74198513e9e5ce070f6b2c6986c7e9350b8b
Swaroop Sridhar <Swaroop.Sridhar@microsoft.com>
2019-12-11 12:49:17 -0800

    Generate Content for a Test at Run-time
    With this change, a part of the content for the test-asset HellowWorldWithSubDirs
    is not checked in, but generated during test execution in order to circumvent certain issues like:
    * Git Clone: Cannot clone files with long names on Windows if long file name support is not enabled
    * Nuget Pack: By default ignores files starting with "."

--- src/Assets/TestProjects/HelloWorldWithSubDirs/Program.cs
+++ src/Assets/TestProjects/HelloWorldWithSubDirs/Program.cs
@@ -10,6 +10,10 @@ namespace HelloWorldWithSubDirs
         {
             string baseDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
             string hello = File.ReadAllText(Path.Combine(baseDir, "SmallNameDir", "word"));
+            // The following content is not checked in to the test assets, but generated during test execution
+            // in order to circumvent certain issues like: 
+            // Git Clone: Cannot clone files with long names on Windows if long file name support is not enabled
+            // Nuget Pack: By default ignores files starting with "."
             string world = File.ReadAllText(Path.Combine(baseDir, "SmallNameDir", "This is a directory with a really long name for one that only contains a small file", ".word"));
             Console.WriteLine($"{hello} {world}");
         }
