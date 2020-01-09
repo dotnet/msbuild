85b4fe81cf7a97a87e4a1aee40d0223a27c0731c
Szali Szali <szalicil@gmail.com>
2020-01-09 13:01:19 +0100

    fix PR comment: use Shouldly

--- src/MSBuild.UnitTests/CommandLineSwitches_Tests.cs
+++ src/MSBuild.UnitTests/CommandLineSwitches_Tests.cs
@@ -736,19 +736,19 @@ namespace Microsoft.Build.UnitTests
         [InlineData("ts")]
         public void TargetsSwitchIdentificationTests(string @switch)
         {
-            Assert.True(CommandLineSwitches.IsParameterizedSwitch(
+            CommandLineSwitches.IsParameterizedSwitch(
                 @switch,
                 out var parameterizedSwitch,
                 out var duplicateSwitchErrorMessage,
                 out var multipleParametersAllowed,
                 out var missingParametersErrorMessage,
                 out var unquoteParameters,
-                out var emptyParametersAllowed));
-            Assert.Equal(CommandLineSwitches.ParameterizedSwitch.Targets, parameterizedSwitch);
-            Assert.Null(duplicateSwitchErrorMessage);
-            Assert.False(multipleParametersAllowed);
-            Assert.Null(missingParametersErrorMessage);
-            Assert.True(unquoteParameters);
+                out var emptyParametersAllowed).ShouldBeTrue();
+            parameterizedSwitch.ShouldBe(CommandLineSwitches.ParameterizedSwitch.Targets);
+            duplicateSwitchErrorMessage.ShouldBeNull();
+            multipleParametersAllowed.ShouldBeFalse();
+            missingParametersErrorMessage.ShouldBeNull();
+            unquoteParameters.ShouldBeTrue();
         }
 
         [Fact]
