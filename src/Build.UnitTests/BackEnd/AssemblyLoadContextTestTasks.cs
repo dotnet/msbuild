8973017807379e8c431b671bd51956bba57ebbd4
Rainer Sigwald <raines@microsoft.com>
2020-01-28 10:37:36 -0600

    Normalize plugin paths before loading
    Fixes #5080 by normalizing plugin assembly paths before deduplicating
    loads based on those paths.
    
    Does not fix the cross-assembly register/retrieve object problem #5084.

---
+++ src/Build.UnitTests/BackEnd/AssemblyLoadContextTestTasks.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace AssemblyLoadContextTest
+{
+    public class RegisterObject : Task
+    {
+        internal const string CacheKey = "RegressionForMSBuild#5080";
+
+        public override bool Execute()
+        {
+            BuildEngine4.RegisterTaskObject(
+                  CacheKey,
+                  new RegisterObject(),
+                  RegisteredTaskObjectLifetime.Build,
+                  allowEarlyCollection: false);
+
+            return true;
+        }
+    }
+
+    public class RetrieveObject : Task
+    {
+        public override bool Execute()
+        {
+            var entry = (RegisterObject)BuildEngine4.GetRegisteredTaskObject(RegisterObject.CacheKey, RegisteredTaskObjectLifetime.Build);
+
+            return true;
+        }
+    }
+}
