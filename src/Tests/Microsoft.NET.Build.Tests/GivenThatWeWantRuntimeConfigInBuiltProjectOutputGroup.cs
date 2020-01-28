786c4b491c248a6035d02a3957a6b1b4ee123291
sfoslund <sfoslund@microsoft.com>
2020-01-28 10:55:18 -0800

    Adding fix and test coverage for runtimeconfig.json updates not being reflected on partial builds

--- src/Tests/Microsoft.NET.Build.Tests/GivenThatWeWantRuntimeConfigInBuiltProjectOutputGroup.cs
+++ src/Tests/Microsoft.NET.Build.Tests/GivenThatWeWantRuntimeConfigInBuiltProjectOutputGroup.cs
@@ -2,11 +2,14 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Xml.Linq;
 using FluentAssertions;
 using Microsoft.NET.TestFramework;
 using Microsoft.NET.TestFramework.Assertions;
 using Microsoft.NET.TestFramework.Commands;
+using Microsoft.NET.TestFramework.ProjectConstruction;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -48,6 +51,45 @@ namespace Microsoft.NET.Build.Tests
             metadata.Count.Should().Be(2);
             metadata.Should().Contain(KeyValuePair.Create("FinalOutputPath", runtimeConfigFile.FullName));
             metadata.Should().Contain(KeyValuePair.Create("TargetPath", runtimeConfigFile.Name));
+        }
+
+        [Fact]
+        public void It_has_runtime_config_properties_after_partial_build()
+        {
+            var testProject = new TestProject()
+            {
+                Name = "RuntimeConfigPartialBuild",
+                IsSdkProject = true,
+                TargetFrameworks = "netcoreapp3.0",
+                IsExe = true,
+                RuntimeIdentifier = "win-x86"
+            };
+            var testAsset = _testAssetsManager.CreateTestProject(testProject, testProject.Name);
+
+            new BuildCommand(Log, Path.Combine(testAsset.TestRoot, testProject.Name))
+                .Execute("/property:Configuration=Release")
+                .Should()
+                .Pass();
+
+            var runtimeConfigFile = Path.Combine(testAsset.TestRoot, testProject.Name, "bin", "Release", testProject.TargetFrameworks, testProject.RuntimeIdentifier, testProject.Name + ".runtimeconfig.json");
+            File.Exists(runtimeConfigFile).Should().BeTrue();
+            File.ReadAllText(runtimeConfigFile).Should().NotContain("TieredCompilation");
+
+            testAsset = testAsset.WithProjectChanges(project =>
+            {
+                var ns = project.Root.Name.Namespace;
+                var propertyGroup = new XElement(ns + "PropertyGroup");
+                project.Root.Add(propertyGroup);
+                propertyGroup.Add(new XElement(ns + "TieredCompilation", "false"));
+            });
+
+            new BuildCommand(Log, Path.Combine(testAsset.TestRoot, testProject.Name))
+                .Execute("/property:Configuration=Release")
+                .Should()
+                .Pass();
+
+            File.Exists(runtimeConfigFile).Should().BeTrue();
+            File.ReadAllText(runtimeConfigFile).Should().Contain("TieredCompilation");
         }
     }
 }
