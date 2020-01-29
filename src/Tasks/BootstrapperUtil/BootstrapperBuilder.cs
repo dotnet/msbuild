2e5c73b3408843ac66ccca7ffd85bb6387387357
MSLukeWest <42553283+MSLukeWest@users.noreply.github.com>
2020-01-29 14:02:52 -0800

    Updating BootstrapUtil to look for packages in additional folders (#5074)
    We want teams to be able to install bootstrapper packages from outside the VS MSIs where they currently come from. Right now we only enumerate packages from a single directory that's defined in the HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\GenericBootstrapper reg key. I'm making it so if there's any additional paths specified under the AdditionalPackagePaths sub key we'll look for bootstrapper packages in those paths as well.
    
    One small issue with this change is that when a bootstrapper specified in a clickonce or setup project isn't found we give a warning like this:
    
    Item '.NETFramework,Version=v4.7.2' could not be located in 'C:\Program Files (x86)\Microsoft SDKs\ClickOnce Bootstrapper'
    
    Ideally this would be updated to also mention any possible additional paths that have been specified using this new support. But since that would require a string change and a new public interface to properly support I've decided to delay that part of the change until 16.6.

--- src/Tasks/BootstrapperUtil/BootstrapperBuilder.cs
+++ src/Tasks/BootstrapperUtil/BootstrapperBuilder.cs
@@ -442,15 +442,24 @@ namespace Microsoft.Build.Tasks.Deployment.Bootstrapper
             var files = new List<string>();
             BuildPackages(settings, null, null, files, null);
 
+            List<string> packagePaths = new List<string>() { invariantPath };
+            packagePaths.AddRange(Util.AdditionalPackagePaths.Select(p => Util.AddTrailingChar(p.ToLowerInvariant(), System.IO.Path.DirectorySeparatorChar)));
+
             foreach (string file in files)
             {
                 string folder = System.IO.Path.GetDirectoryName(file);
-                if (folder.Substring(0, invariantPath.Length).ToLowerInvariant().CompareTo(invariantPath) == 0)
+
+                foreach (string packagePath in packagePaths)
                 {
-                    string relPath = folder.Substring(invariantPath.Length);
-                    if (!folders.Contains(relPath))
+                    if (folder.Length >= packagePath.Length && folder.Substring(0, packagePath.Length).ToLowerInvariant().CompareTo(packagePath) == 0)
                     {
-                        folders.Add(relPath);
+                        string relPath = folder.Substring(packagePath.Length);
+                        if (!folders.Contains(relPath))
+                        {
+                            folders.Add(relPath);
+                        }
+
+                        break;
                     }
                 }
             }
@@ -576,18 +585,23 @@ namespace Microsoft.Build.Tasks.Deployment.Bootstrapper
             _xmlNamespaceManager.AddNamespace(BOOTSTRAPPER_PREFIX, BOOTSTRAPPER_NAMESPACE);
 
             XmlElement rootElement = _document.CreateElement("Products", BOOTSTRAPPER_NAMESPACE);
-            string packagePath = PackagePath;
 
-            if (FileSystems.Default.DirectoryExists(packagePath))
+            List<string> packagePaths = new List<string>() { PackagePath };
+            packagePaths.AddRange(Util.AdditionalPackagePaths);
+            foreach (string packagePath in packagePaths)
             {
-                foreach (string strSubDirectory in Directory.GetDirectories(packagePath))
+                if (FileSystems.Default.DirectoryExists(packagePath))
                 {
-                    int nStartIndex = packagePath.Length;
-                    if ((strSubDirectory.ToCharArray())[nStartIndex] == System.IO.Path.DirectorySeparatorChar)
+                    foreach (string strSubDirectory in Directory.GetDirectories(packagePath))
                     {
-                        nStartIndex = nStartIndex + 1;
+                        int nStartIndex = packagePath.Length;
+                        if ((strSubDirectory.ToCharArray())[nStartIndex] == System.IO.Path.DirectorySeparatorChar)
+                        {
+                            nStartIndex = nStartIndex + 1;
+                        }
+
+                        ExploreDirectory(strSubDirectory.Substring(nStartIndex), rootElement, packagePath);
                     }
-                    ExploreDirectory(strSubDirectory.Substring(nStartIndex), rootElement);
                 }
             }
 
@@ -887,11 +901,10 @@ namespace Microsoft.Build.Tasks.Deployment.Bootstrapper
             return xmlDocument;
         }
 
-        private void ExploreDirectory(string strSubDirectory, XmlElement rootElement)
+        private void ExploreDirectory(string strSubDirectory, XmlElement rootElement, string packagePath)
         {
             try
             {
-                string packagePath = PackagePath;
                 string strSubDirectoryFullPath = System.IO.Path.Combine(packagePath, strSubDirectory);
 
                 // figure out our product file paths based on the directory full path
