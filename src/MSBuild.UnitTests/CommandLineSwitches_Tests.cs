1278bcbee517118d732f2b438319bfaef0275442
Szali Szali <szalicil@gmail.com>
2020-02-02 17:58:09 +0100

    Revert "wrap parameters"
    This reverts commit 18241d5dfd0eec07dff03fc241124d8c337c485f.

--- src/MSBuild.UnitTests/CommandLineSwitches_Tests.cs
+++ src/MSBuild.UnitTests/CommandLineSwitches_Tests.cs
@@ -743,14 +743,7 @@ namespace Microsoft.Build.UnitTests
             bool unquoteParameters;
             bool emptyParametersAllowed;
 
-            Assert.True(CommandLineSwitches.IsParameterizedSwitch(
-                @switch,
-                out parameterizedSwitch,
-                out duplicateSwitchErrorMessage,
-                out multipleParametersAllowed,
-                out missingParametersErrorMessage,
-                out unquoteParameters,
-                out emptyParametersAllowed));
+            Assert.True(CommandLineSwitches.IsParameterizedSwitch(@switch, out parameterizedSwitch, out duplicateSwitchErrorMessage, out multipleParametersAllowed, out missingParametersErrorMessage, out unquoteParameters, out emptyParametersAllowed));
             Assert.Equal(CommandLineSwitches.ParameterizedSwitch.Targets, parameterizedSwitch);
             Assert.Null(duplicateSwitchErrorMessage);
             Assert.False(multipleParametersAllowed);
