2846f5e7b5f7bd48095ec835b3574df1f85f8c21
Szali Szali <szalicil@gmail.com>
2020-02-02 17:58:35 +0100

    Revert "convert duplicated test code to parametrized test"
    This reverts commit ea11ce4b7380c9b7c77ab4bdc3f50c52cf7f959c.

--- src/MSBuild.UnitTests/CommandLineSwitches_Tests.cs
+++ src/MSBuild.UnitTests/CommandLineSwitches_Tests.cs
@@ -731,10 +731,8 @@ namespace Microsoft.Build.UnitTests
             Assert.True(unquoteParameters);
         }
 
-        [Theory]
-        [InlineData("targets")]
-        [InlineData("ts")]
-        public void TargetsSwitchIdentificationTests(string @switch)
+        [Fact]
+        public void TargetsSwitchIdentificationTests()
         {
             CommandLineSwitches.ParameterizedSwitch parameterizedSwitch;
             string duplicateSwitchErrorMessage;
@@ -743,7 +741,14 @@ namespace Microsoft.Build.UnitTests
             bool unquoteParameters;
             bool emptyParametersAllowed;
 
-            Assert.True(CommandLineSwitches.IsParameterizedSwitch(@switch, out parameterizedSwitch, out duplicateSwitchErrorMessage, out multipleParametersAllowed, out missingParametersErrorMessage, out unquoteParameters, out emptyParametersAllowed));
+            Assert.True(CommandLineSwitches.IsParameterizedSwitch("targets", out parameterizedSwitch, out duplicateSwitchErrorMessage, out multipleParametersAllowed, out missingParametersErrorMessage, out unquoteParameters, out emptyParametersAllowed));
+            Assert.Equal(CommandLineSwitches.ParameterizedSwitch.Targets, parameterizedSwitch);
+            Assert.Null(duplicateSwitchErrorMessage);
+            Assert.False(multipleParametersAllowed);
+            Assert.Null(missingParametersErrorMessage);
+            Assert.True(unquoteParameters);
+
+            Assert.True(CommandLineSwitches.IsParameterizedSwitch("ts", out parameterizedSwitch, out duplicateSwitchErrorMessage, out multipleParametersAllowed, out missingParametersErrorMessage, out unquoteParameters, out emptyParametersAllowed));
             Assert.Equal(CommandLineSwitches.ParameterizedSwitch.Targets, parameterizedSwitch);
             Assert.Null(duplicateSwitchErrorMessage);
             Assert.False(multipleParametersAllowed);
