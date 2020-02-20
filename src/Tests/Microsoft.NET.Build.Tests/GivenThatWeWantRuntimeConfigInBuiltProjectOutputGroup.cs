ef114464493c777e584d21d1807466fdf3dc0cfb
sfoslund <sfoslund@microsoft.com>
2020-02-20 08:28:52 -0800

    Adding metadata to generate runtime config hash, expanding test coverage

--- src/Tests/Microsoft.NET.Build.Tests/GivenThatWeWantRuntimeConfigInBuiltProjectOutputGroup.cs
+++ src/Tests/Microsoft.NET.Build.Tests/GivenThatWeWantRuntimeConfigInBuiltProjectOutputGroup.cs
@@ -74,9 +74,9 @@ namespace Microsoft.NET.Build.Tests
             var configFile = Path.Combine(testAsset.TestRoot, testProject.Name, "bin", "Release", testProject.TargetFrameworks, testProject.RuntimeIdentifier, testProject.Name + ".runtimeconfig.json");
 
             File.Exists(configFile).Should().BeTrue();
-            File.ReadAllText(configFile).Should().NotContain("TieredCompilation");
-            File.ReadAllText(configFile).Should().NotContain("System.GC.Concurrent");
-            File.ReadAllText(configFile).Should().NotContain("System.Threading.ThreadPool.MinThreads");
+            File.ReadAllText(configFile).Should().NotContain("\"System.Runtime.TieredCompilation\"");
+            File.ReadAllText(configFile).Should().NotContain("\"System.GC.Concurrent\"");
+            File.ReadAllText(configFile).Should().NotContain("\"System.Threading.ThreadPool.MinThreads\"");
 
             testAsset = testAsset.WithProjectChanges(project =>
             {
@@ -94,9 +94,65 @@ namespace Microsoft.NET.Build.Tests
                 .Pass();
 
             File.Exists(configFile).Should().BeTrue();
-            File.ReadAllText(configFile).Should().Contain("TieredCompilation");
-            File.ReadAllText(configFile).Should().Contain("System.GC.Concurrent");
-            File.ReadAllText(configFile).Should().Contain("System.Threading.ThreadPool.MinThreads");
+            File.ReadAllText(configFile).Should().Contain("\"System.Runtime.TieredCompilation\": false");
+            File.ReadAllText(configFile).Should().Contain("\"System.GC.Concurrent\": false");
+            File.ReadAllText(configFile).Should().Contain("\"System.Threading.ThreadPool.MinThreads\": 2");
+        }
+
+        [Fact]
+        public void It_updates_runtime_config_properties_after_partial_build()
+        {
+            var testProject = new TestProject()
+            {
+                Name = "UpdateRuntimeConfigPartialBuild",
+                IsSdkProject = true,
+                TargetFrameworks = "netcoreapp3.0",
+                IsExe = true,
+                RuntimeIdentifier = "win-x86"
+            };
+            var testAsset = _testAssetsManager.CreateTestProject(testProject, testProject.Name);
+
+            testAsset = testAsset.WithProjectChanges(project =>
+            {
+                var ns = project.Root.Name.Namespace;
+                var propertyGroup = new XElement(ns + "PropertyGroup");
+                project.Root.Add(propertyGroup);
+                propertyGroup.Add(new XElement(ns + "TieredCompilation", "true"));
+                propertyGroup.Add(new XElement(ns + "ConcurrentGarbageCollection", "true"));
+                propertyGroup.Add(new XElement(ns + "ThreadPoolMinThreads", "3"));
+            });
+
+            new BuildCommand(Log, Path.Combine(testAsset.TestRoot, testProject.Name))
+                .Execute("/property:Configuration=Release")
+                .Should()
+                .Pass();
+
+            var configFile = Path.Combine(testAsset.TestRoot, testProject.Name, "bin", "Release", testProject.TargetFrameworks, testProject.RuntimeIdentifier, testProject.Name + ".runtimeconfig.json");
+
+            File.Exists(configFile).Should().BeTrue();
+            File.ReadAllText(configFile).Should().Contain("\"System.Runtime.TieredCompilation\": true");
+            File.ReadAllText(configFile).Should().Contain("\"System.GC.Concurrent\": true");
+            File.ReadAllText(configFile).Should().Contain("\"System.Threading.ThreadPool.MinThreads\": 3");
+
+            testAsset = testAsset.WithProjectChanges(project =>
+            {
+                var ns = project.Root.Name.Namespace;
+                var propertyGroup = new XElement(ns + "PropertyGroup");
+                project.Root.Add(propertyGroup);
+                propertyGroup.Add(new XElement(ns + "TieredCompilation", "false"));
+                propertyGroup.Add(new XElement(ns + "ConcurrentGarbageCollection", "false"));
+                propertyGroup.Add(new XElement(ns + "ThreadPoolMinThreads", "2"));
+            });
+
+            new BuildCommand(Log, Path.Combine(testAsset.TestRoot, testProject.Name))
+                .Execute("/property:Configuration=Release")
+                .Should()
+                .Pass();
+
+            File.Exists(configFile).Should().BeTrue();
+            File.ReadAllText(configFile).Should().Contain("\"System.Runtime.TieredCompilation\": false");
+            File.ReadAllText(configFile).Should().Contain("\"System.GC.Concurrent\": false");
+            File.ReadAllText(configFile).Should().Contain("\"System.Threading.ThreadPool.MinThreads\": 2");
         }
     }
 }
