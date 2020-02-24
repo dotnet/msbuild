6d1f6e922289102b8abd3f84a79ee9af9322542e
Ben Villalobos <villalobosb93@gmail.com>
2020-02-24 14:28:55 -0800

    Log an error when a Task returns false but didn't log an error (#4940)
    * Checking now if a task failed bug did not log info
    
    This doesn't apply to all tasks at the moment.
    
    * TaskLoggingContext now knows if it logs an error
    
    * Added error message
    
    * Added test task and unit test to verify
    
    * Don't consider msbuild tasks
    
    * notify taskloggingcontext that it has logged errors while in the taskhost
    
    * Moving & fixing unit test.
    
    * Bringing in ITestOutputHelper
    
    * Responding to comments on PR
    
    Added copyright
    Added comments explaining:
    ReturnFailureWithoutLoggingErrorTask returning false
    Why MSBuild tasks are excluded from tasks returning false.
    
    Changed the .resx string
    
    * Added test cases for ContinueOnError
    
    * Removed unused ITestOutputHelper, now checking error count in "WarnAndContinue" test.
    
    * Error will now be filtered to a warning if WarnAndContinue is set.
    
    * Simple test for LoggingContext.HasLoggedError
    
    * LoggingContest_Tests tweaks
    
    * Update src/Build/BackEnd/Components/RequestBuilder/TaskBuilder.cs
    
    Co-Authored-By: Rainer Sigwald <raines@microsoft.com>
    
    Co-authored-by: Rainer Sigwald <raines@microsoft.com>

---
+++ src/Build.UnitTests/BackEnd/ReturnFailureWithoutLoggingErrorTask.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.Build.BackEnd.Logging;
+using Microsoft.Build.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace Microsoft.Build.UnitTests
+{
+    /// <summary>
+    /// This task was created for https://github.com/microsoft/msbuild/issues/2036
+    /// </summary>
+    public class ReturnFailureWithoutLoggingErrorTask : Task
+    {
+        /// <summary>
+        /// Intentionally return false without logging an error to test proper error catching.
+        /// </summary>
+        /// <returns></returns>
+        public override bool Execute()
+        {
+            return false;
+        }
+    }
+}
--- src/Build/BackEnd/Components/Logging/LoggingContext.cs
+++ src/Build/BackEnd/Components/Logging/LoggingContext.cs
@@ -28,6 +28,8 @@ namespace Microsoft.Build.BackEnd.Logging
         /// </summary>
         private bool _isValid;
 
+        protected bool _hasLoggedErrors;
+
         /// <summary>
         /// Constructs the logging context from a logging service and an event context.
         /// </summary>
@@ -41,6 +43,7 @@ namespace Microsoft.Build.BackEnd.Logging
             _loggingService = loggingService;
             _eventContext = eventContext;
             _isValid = false;
+            _hasLoggedErrors = false;
         }
 
         /// <summary>
@@ -106,6 +109,8 @@ namespace Microsoft.Build.BackEnd.Logging
             }
         }
 
+        internal bool HasLoggedErrors { get { return _hasLoggedErrors; } set { _hasLoggedErrors = value; } }
+
         /// <summary>
         ///  Helper method to create a message build event from a string resource and some parameters
         /// </summary>
@@ -139,6 +144,7 @@ namespace Microsoft.Build.BackEnd.Logging
         {
             ErrorUtilities.VerifyThrow(_isValid, "must be valid");
             _loggingService.LogError(_eventContext, file, messageResourceName, messageArgs);
+            _hasLoggedErrors = true;
         }
 
         /// <summary>
@@ -152,6 +158,7 @@ namespace Microsoft.Build.BackEnd.Logging
         {
             ErrorUtilities.VerifyThrow(_isValid, "must be valid");
             _loggingService.LogError(_eventContext, subcategoryResourceName, file, messageResourceName, messageArgs);
+            _hasLoggedErrors = true;
         }
 
         /// <summary>
@@ -166,6 +173,7 @@ namespace Microsoft.Build.BackEnd.Logging
         {
             ErrorUtilities.VerifyThrow(_isValid, "must be valid");
             _loggingService.LogErrorFromText(_eventContext, subcategoryResourceName, errorCode, helpKeyword, file, message);
+            _hasLoggedErrors = true;
         }
 
         /// <summary>
@@ -176,6 +184,7 @@ namespace Microsoft.Build.BackEnd.Logging
         {
             ErrorUtilities.VerifyThrow(_isValid, "must be valid");
             _loggingService.LogInvalidProjectFileError(_eventContext, invalidProjectFileException);
+            _hasLoggedErrors = true;
         }
 
         /// <summary>
@@ -189,6 +198,7 @@ namespace Microsoft.Build.BackEnd.Logging
         {
             ErrorUtilities.VerifyThrow(_isValid, "must be valid");
             _loggingService.LogFatalError(_eventContext, exception, file, messageResourceName, messageArgs);
+            _hasLoggedErrors = true;
         }
 
         /// <summary>
@@ -237,6 +247,7 @@ namespace Microsoft.Build.BackEnd.Logging
         {
             ErrorUtilities.VerifyThrow(IsValid, "must be valid");
             LoggingService.LogFatalBuildError(BuildEventContext, exception, file);
+            _hasLoggedErrors = true;
         }
     }
 }
