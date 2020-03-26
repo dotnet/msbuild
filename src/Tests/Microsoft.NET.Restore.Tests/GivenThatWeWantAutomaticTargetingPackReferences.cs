c6b2f338fea64e5db572f98e2a3ed9d1ad9c8fdf
Sarah Oslund <sfoslund@microsoft.com>
2020-03-26 11:01:12 -0700

    PR feedback for automatic referencing assembly packs, adding test coverage for existing references

--- src/Tests/Microsoft.NET.Restore.Tests/GivenThatWeWantAutomaticTargetingPackReferences.cs
+++ src/Tests/Microsoft.NET.Restore.Tests/GivenThatWeWantAutomaticTargetingPackReferences.cs
@@ -1,5 +1,6 @@
 using System.IO;
 using System.Linq;
+using System.Xml.Linq;
 using FluentAssertions;
 using Microsoft.Build.Utilities;
 using Microsoft.NET.TestFramework;
@@ -93,7 +94,7 @@ namespace Microsoft.NET.Restore.Tests
                 net471FrameworkLibrary.Should().BeNull();
             }
             else
-            { 
+            {
                 net471FrameworkLibrary.Name.Should().Be("Microsoft.NETFramework.ReferenceAssemblies.net471");
                 net471FrameworkLibrary.Type.Should().Be("package");
             }
@@ -109,7 +110,47 @@ namespace Microsoft.NET.Restore.Tests
                 net472FrameworkLibrary.Name.Should().Be("Microsoft.NETFramework.ReferenceAssemblies.net472");
                 net472FrameworkLibrary.Type.Should().Be("package");
             }
+        }
 
+        [Fact]
+        public void It_restores_net_framework_project_with_existing_references()
+        {
+            var targetFramework = "net471";
+            var testProject = new TestProject()
+            {
+                Name = "ProjectWithoutTargetingPackRef",
+                TargetFrameworks = targetFramework,
+                IsSdkProject = true,
+            };
+
+            // Add explicit reference to assembly packs
+            var testAsset = _testAssetsManager.CreateTestProject(testProject).WithProjectChanges(project =>
+            {
+                var ns = project.Root.Name.Namespace;
+                var itemGroup = project.Root.Elements(ns + "ItemGroup").FirstOrDefault();
+                itemGroup.Add(new XElement(ns + "PackageReference",
+                    new XAttribute("Include", $"Microsoft.NETFramework.ReferenceAssemblies"),
+                    new XAttribute("Version", $"1.0.0-preview.2")));
+
+            });
+
+            string projectAssetsJsonPath = Path.Combine(
+                testAsset.Path,
+                testProject.Name,
+                "obj",
+                "project.assets.json");
+
+            var restoreCommand =
+                testAsset.GetRestoreCommand(Log, relativePath: testProject.Name);
+            restoreCommand.Execute().Should().Pass();
+
+            LockFile lockFile = LockFileUtilities.GetLockFile(projectAssetsJsonPath, NullLogger.Instance);
+            var netFrameworkLibrary = lockFile.GetTarget(NuGetFramework.Parse(".NETFramework,Version=v4.7.1"), null).Libraries.FirstOrDefault((file) => file.Name.Contains(targetFramework));
+
+
+            netFrameworkLibrary.Name.Should().Be("Microsoft.NETFramework.ReferenceAssemblies." + targetFramework);
+            netFrameworkLibrary.Type.Should().Be("package");
+            netFrameworkLibrary.Version.ToFullString().Should().Be("1.0.0-preview.2");
         }
     }
 }
