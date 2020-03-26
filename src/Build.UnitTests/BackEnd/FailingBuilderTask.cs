c097b14571b92d2a155cf6a19dc786732f47ec1c
Nathan Mytelka <Forgind@users.noreply.github.com>
2020-03-25 17:18:47 -0700

    Uninlined task

---
+++ src/Build.UnitTests/BackEnd/FailingBuilderTask.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace Microsoft.Build.UnitTests
+{
+    public  class FailingBuilderTask : Task
+    {
+        public FailingBuilderTask()
+            : base(null)
+        { }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1031:DoNotCatchGeneralExceptionTypes")]
+        public override bool Execute()
+        {
+            try
+            {
+                BuildEngine.BuildProjectFile(CurrentProject, new string[] { "CoreCompile" }, new Hashtable(), null);
+            }
+            catch
+            {
+            }
+            return false;
+        }
+
+        [Required]
+        public string CurrentProject { get; set; }
+    }
+}
---
+++ src/Build.UnitTests/BackEnd/GenerateTemporaryTargetAssembly_Tests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Build.UnitTests;
+using Shouldly;
+using Xunit;
+
+namespace Microsoft.Build.Engine.UnitTests.BackEnd
+{
+    sealed public class GenerateTemporaryTargetAssembly_Tests
+    {
+        [Fact]
+        public void FailsWithOnlyTargetErrors()
+        {
+            MockLogger logger = ObjectModelHelpers.BuildProjectExpectFailure(@"
+<Project>
+    <UsingTask TaskName=""FailingBuilderTask"" AssemblyName=""Microsoft.Build.Engine.UnitTests"" />
+    <Target Name=""MyTarget"">
+        <FailingBuilderTask CurrentProject="".\otherproj.csproj"" />
+    </Target>
+</Project>");
+            logger.ErrorCount.ShouldBe(1);
+        }
+    }
+}
