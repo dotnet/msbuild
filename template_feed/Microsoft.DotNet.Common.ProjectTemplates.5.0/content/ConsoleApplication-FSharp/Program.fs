65c7eea783be5c369d102eec9c0ef6ea064d82e0
Phil Henning <phenning@microsoft.com>
2020-03-27 10:54:33 -0700

    Update F# net 5 console templates to define a function

--- template_feed/Microsoft.DotNet.Common.ProjectTemplates.5.0/content/ConsoleApplication-FSharp/Program.fs
+++ template_feed/Microsoft.DotNet.Common.ProjectTemplates.5.0/content/ConsoleApplication-FSharp/Program.fs
@@ -1,8 +1,13 @@
-﻿// Learn more about F# at http://fsharp.org
+﻿// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
 
 open System
 
+// Define a function to construct a message to print
+let from whom =
+    sprintf "from %s" whom
+
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
+    let message = from "F#" // Call the function
+    printfn "Hello world %s" message
     0 // return an integer exit code
