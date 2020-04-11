dee4a5236d083dfefe11b321f40c2ef6f5842e03
Kirill Osenkov <github@osenkov.com>
2020-04-10 22:47:16 -0700

    Add more descriptive errors.

--- src/Build/Instance/ProjectItemInstance.cs
+++ src/Build/Instance/ProjectItemInstance.cs
@@ -1084,7 +1084,7 @@ namespace Microsoft.Build.Execution
                             }
                             else
                             {
-                                Debug.Fail("metadatum is null, see https://github.com/microsoft/msbuild/issues/5267");
+                                Debug.Fail($"metadatum from {_itemDefinitions[i]} is null, see https://github.com/microsoft/msbuild/issues/5267");
                             }
                         }
                     }
@@ -1100,7 +1100,7 @@ namespace Microsoft.Build.Execution
                             }
                             else
                             {
-                                Debug.Fail("metadatum is null, see https://github.com/microsoft/msbuild/issues/5267");
+                                Debug.Fail("metadatum in _directMetadata is null, see https://github.com/microsoft/msbuild/issues/5267");
                             }
                         }
                     }
