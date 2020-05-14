e8240b9e76b105e99f576fac61288e561a518986
Rainer Sigwald <raines@microsoft.com>
2020-05-14 16:49:11 -0500

    Move ItemspecContainsASingleItemReference to LazyItemOperation
    I want to use this in another subclass.

--- src/Build/Evaluation/LazyItemEvaluator.LazyItemOperation.cs
+++ src/Build/Evaluation/LazyItemEvaluator.LazyItemOperation.cs
@@ -323,6 +323,27 @@ namespace Microsoft.Build.Evaluation
 
                 return needToExpandMetadataForEachItem;
             }
+
+            protected static bool ItemspecContainsASingleItemReference(ItemSpec<P, I> itemSpec, string referencedItemType)
+            {
+                if (itemSpec.Fragments.Count != 1)
+                {
+                    return false;
+                }
+
+                var itemExpressionFragment = itemSpec.Fragments[0] as ItemSpec<P, I>.ItemExpressionFragment;
+                if (itemExpressionFragment == null)
+                {
+                    return false;
+                }
+
+                if (!itemExpressionFragment.Capture.ItemType.Equals(referencedItemType, StringComparison.OrdinalIgnoreCase))
+                {
+                    return false;
+                }
+
+                return true;
+            }
         }
     }
 }
