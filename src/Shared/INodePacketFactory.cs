3d498ca1a3e0bd75de0b82b9c3e5079eec707bca
Forgind <Forgind@users.noreply.github.com>
2020-06-26 09:38:15 -0700

    Removed some unnecessary using statements (#5457)
    Removed some using statements

--- src/Shared/INodePacketFactory.cs
+++ src/Shared/INodePacketFactory.cs
@@ -1,10 +1,6 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using System.Collections.Generic;
-using System.Text;
-using Microsoft.Build.Framework;
-
 namespace Microsoft.Build.BackEnd
 {
     /// <summary>
