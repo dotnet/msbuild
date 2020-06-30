6ddfc570bc1921d1ac8ed902d691a7e188c9cf72
Nathan Mytelka <Forgind@users.noreply.github.com>
2020-06-30 09:17:48 -0700

    Fixed build

--- src/Build/BackEnd/Components/Communications/NodeEndpointOutOfProc.cs
+++ src/Build/BackEnd/Components/Communications/NodeEndpointOutOfProc.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using Microsoft.Build.Internal;
 using Microsoft.Build.Shared;
 
 namespace Microsoft.Build.BackEnd
