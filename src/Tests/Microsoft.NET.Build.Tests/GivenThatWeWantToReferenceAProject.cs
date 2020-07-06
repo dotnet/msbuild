86bae87a15f91fa2e97392656431b0d97ae1f4e8
Sarah Oslund <sfoslund@microsoft.com>
2020-07-06 10:12:51 -0700

    Adding test coverage for copying transitive content files

--- src/Tests/Microsoft.NET.Build.Tests/GivenThatWeWantToReferenceAProject.cs
+++ src/Tests/Microsoft.NET.Build.Tests/GivenThatWeWantToReferenceAProject.cs
@@ -233,5 +233,61 @@ namespace Microsoft.NET.Build.Tests
                 project.Root.Add(target);
             }
         }
+
+        [Fact]
+        public void It_copies_content_transitively()
+        {
+            var targetFramework = "net5.0";
+            var testProjectA = new TestProject()
+            {
+                Name = "A",
+                TargetFrameworks = targetFramework,
+                IsSdkProject = true
+            };
+            var testAssetA = _testAssetsManager.CreateTestProject(testProjectA).WithProjectChanges((path, p) =>
+            {
+                var ns = p.Root.Name.Namespace;
+                p.Root.Add(new XElement(ns + "ItemGroup",
+                    new XElement(ns + "Content", new XAttribute("Include", "a.txt"), new XAttribute("CopyToOutputDirectory", "PreserveNewest"))));
+            });
+            File.WriteAllText(Path.Combine(testAssetA.Path, testProjectA.Name, "a.txt"), "A");
+
+            var testProjectB = new TestProject()
+            {
+                Name = "B",
+                TargetFrameworks = targetFramework,
+                IsSdkProject = true
+            };
+            testProjectB.AdditionalItems.Add("ProjectReference", Path.Combine(testAssetA.Path, testProjectA.Name, $"{testProjectA.Name}.csproj"));
+            var testAssetB = _testAssetsManager.CreateTestProject(testProjectB);
+
+            var testProjectC = new TestProject()
+            {
+                Name = "C",
+                TargetFrameworks = targetFramework,
+                IsSdkProject = true
+            };
+            testProjectC.AdditionalProperties.Add("DisableTransitiveProjectReferences", "true");
+            testProjectC.AdditionalItems.Add("ProjectReference", Path.Combine(testAssetB.Path, testProjectB.Name, $"{testProjectB.Name}.csproj"));
+            var testAssetC = _testAssetsManager.CreateTestProject(testProjectC);
+
+            var buildCommand = new BuildCommand(testAssetC);
+            buildCommand
+                .Execute()
+                .Should()
+                .Pass();
+
+            var contentPath = Path.Combine(testAssetC.Path, testProjectC.Name, "bin", "Debug", targetFramework, "a.txt");
+            File.Exists(contentPath).Should().BeTrue();
+            var binDir = new DirectoryInfo(Path.Combine(testAssetC.Path, testProjectC.Name, "bin"));
+            binDir.Delete(true);
+
+            buildCommand
+                .Execute("/p:BuildProjectReferences=false")
+                .Should()
+                .Pass();
+
+            File.Exists(contentPath).Should().BeTrue();
+        }
     }
 }
