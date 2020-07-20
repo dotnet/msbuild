180e7f4088bb00650940cef0999d31d40acc18b5
Rainer Sigwald <raines@microsoft.com>
2020-07-20 11:53:44 -0500

    Just log starting, don't dump environment

--- src/MSBuild/Update-MSBuildXsds.ps1
+++ src/MSBuild/Update-MSBuildXsds.ps1
@@ -1,8 +1,8 @@
 $ErrorActionPreference = 'Stop'
 
-cmd /c set
-
 $vsXsdPath = "src\xmake\XMakeCommandLine"
+
+Write-Host "Updating XSDs in $(Resolve-Path $vsXsdPath)"
 
 Copy-Item -Path (Join-Path $PSScriptRoot "Microsoft.Build.xsd") -Destination $vsXsdPath -Force
 Copy-Item -Path (Join-Path $PSScriptRoot "MSBuild\Microsoft.Build.CommonTypes.xsd") -Destination $vsXsdPath -Force
