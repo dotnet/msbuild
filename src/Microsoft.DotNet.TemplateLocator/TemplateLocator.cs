bc3e4d7495be7cfd6327ce986635659beca5e6d3
William Li <wul@microsoft.com>
2020-07-22 15:47:07 -0700

    Add mock template locator
    Check in the code, so it will be published to the feed.
    dotnet/templating can start to consume it. Once the actual
    implementation is done, we can rename the package. The only thing
    dotnet/templating need to do is to change the package reference name
    then.

---
+++ src/Microsoft.DotNet.TemplateLocator/TemplateLocator.cs
@@ -0,0 +1,73 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using NuGet.Common;
+using NuGet.Protocol;
+
+namespace Microsoft.DotNet.TemplateLocator
+{
+    public sealed class TemplateLocator
+    {
+        private DirectoryInfo _dotnetSdkTemplatesLocation;
+
+        public TemplateLocator()
+        {
+            string mockTemplateLocation = Environment.GetEnvironmentVariable("MOCKDOTNETSDKTEMPLATESLOCATION");
+            if (!string.IsNullOrWhiteSpace(mockTemplateLocation))
+            {
+                _dotnetSdkTemplatesLocation = new DirectoryInfo(mockTemplateLocation);
+            }
+        }
+
+        public IReadOnlyCollection<IOptionalSdkTemplatePackageInfo> GetDotnetSdkTemplatePackages(string sdkVersion)
+        {
+            if (_dotnetSdkTemplatesLocation == null)
+            {
+                return Array.Empty<IOptionalSdkTemplatePackageInfo>();
+            }
+
+            IEnumerable<LocalPackageInfo> packages = LocalFolderUtility
+                            .GetPackagesV2(_dotnetSdkTemplatesLocation.FullName, new NullLogger());
+
+            if (packages == null)
+            {
+                return Array.Empty<IOptionalSdkTemplatePackageInfo>();
+            }
+            else
+            {
+                return packages
+                    .Select(l => new OptionalSdkTemplatePackageInfo(l)).ToArray();
+            }
+        }
+
+        public string DotnetSdkVersionUsedInVs()
+        {
+            return "5.1.100";
+        }
+
+        internal void SetDotnetSdkTemplatesLocation(DirectoryInfo directoryInfo)
+        {
+            _dotnetSdkTemplatesLocation = directoryInfo;
+        }
+
+        private class OptionalSdkTemplatePackageInfo : IOptionalSdkTemplatePackageInfo
+        {
+            public OptionalSdkTemplatePackageInfo(LocalPackageInfo localPackageInfo)
+            {
+                TemplatePackageId = localPackageInfo.Identity.Id;
+                TemplateVersion = localPackageInfo.Identity.Version.ToNormalizedString();
+                Path = localPackageInfo.Path;
+            }
+
+            public string TemplatePackageId { get; }
+
+            public string TemplateVersion { get; }
+
+            public string Path { get; }
+        }
+    }
+}
