ae20d691be95649fa990fb828cb6159718451ce2
Ben Villalobos <bevillal@microsoft.com>
2020-07-29 17:11:16 -0700

    comments

--- src/Microsoft.Build.UnGAC/Program.cs
+++ src/Microsoft.Build.UnGAC/Program.cs
@@ -36,22 +36,23 @@ namespace Microsoft.Build.UnGAC
             input = proc.StandardInput;
 
             // use where.exe to find gacutil.exe
-            input.WriteLine("where /F /R \"C:\\Program Files (x86)\\Microsoft SDKs\\Windows\" gacutil.exe");
+            if (input.BaseStream.CanWrite)
+            {
+                input.WriteLine("where /F /R \"C:\\Program Files (x86)\\Microsoft SDKs\\Windows\" gacutil.exe");
+            }
 
             //throw away the first line - it's the command we just used
             output.ReadLine();
             string s = output.ReadLine();
 
             // store all instances of gacutil.exe
-            // doing this for now but not acting on it to acknowledge that some weird scenario may prevent us from using the
-            // first found gacutil.
+            // doing this for now "just in case"
             while (!String.IsNullOrEmpty(s))
             {
                 allInstancesOfGACUtil.Add(s);
                 s = output.ReadLine();
             }
 
-            //couldn't find gacutil, quietly fail.
             if (allInstancesOfGACUtil.Count <= 0)
             {
                 proc.Close();
@@ -64,27 +65,21 @@ namespace Microsoft.Build.UnGAC
 
             if (proc.StandardInput.BaseStream.CanWrite)
             {
-                input.WriteLine($"{gacUtilExe} /nologo /u \"MSBuild, Version=15.1.0.0\"");
-                input.WriteLine($"{gacUtilExe} /nologo /u \"Microsoft.Build.Conversion.Core, Version=15.1.0.0\"");
-                input.WriteLine($"{gacUtilExe} /nologo /u \"Microsoft.Build, Version=15.1.0.0\"");
-                input.WriteLine($"{gacUtilExe} /nologo /u \"Microsoft.Build.Engine, Version=15.1.0.0\"");
-                input.WriteLine($"{gacUtilExe} /nologo /u \"Microsoft.Build.Tasks.Core, Version=15.1.0.0\"");
-                input.WriteLine($"{gacUtilExe} /nologo /u \"Microsoft.Build.Utilities.Core, Version=15.1.0.0\"");
-                input.WriteLine($"{gacUtilExe} /nologo /u \"Microsoft.Build.Framework, Version=15.1.0.0\"");
-
+                input.WriteLine($"{gacUtilExe} /nologo /u \"MSBuild, Version=15.1.0.0\"\n" +
+                                $"{gacUtilExe} /nologo /u \"Microsoft.Build.Conversion.Core, Version=15.1.0.0\"\n" +
+                                $"{gacUtilExe} /nologo /u \"Microsoft.Build, Version=15.1.0.0\"\n" +
+                                $"{gacUtilExe} /nologo /u \"Microsoft.Build.Engine, Version=15.1.0.0\"\n" +
+                                $"{gacUtilExe} /nologo /u \"Microsoft.Build.Tasks.Core, Version=15.1.0.0\"\n" +
+                                $"{gacUtilExe} /nologo /u \"Microsoft.Build.Utilities.Core, Version=15.1.0.0\"\n" +
+                                $"{gacUtilExe} /nologo /u \"Microsoft.Build.Framework, Version=15.1.0.0\"\n");
             }
 
-            if(output.BaseStream.CanSeek && output.Peek() == -1)
-            {
-
-            }
             //hacky temporary workaround
             int i = 0;
 
-            // Peek is a strange beast. It returns a -1 if no character can be read or if the stream does not support seeking.
+            // Peek is a strange beast. It returns a -1 if "no character can be read or if the stream does not support seeking."
             // Yet there are cases where you can peek a -1, yet read() returns a valid character.
             // I've tested breakpoints where output.Peek() == -1 && output.BaseStream.Caneek == true, but it never gets hit.
-            
             while (!(i > 2000 && output.Peek() == -1))
             {
                 // We're reading because if you happen to attempt a ReadLine() at the end of the stream, it will lock up.
@@ -93,11 +88,6 @@ namespace Microsoft.Build.UnGAC
 
                 Console.Write(c);
                 i++;
-
-                if (output.BaseStream.CanSeek && output.Peek() == -1)
-                {
-
-                }
             }
 
             proc.Close();
