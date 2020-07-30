61992ab3174f3b9362a679c17fde6b7b0e270813
Jakub Jareš <me@jakubjares.com>
2020-07-30 21:45:58 +0200

    Add modern --blame params to dotnet vstest and dotnet test + dll (#12690) (#12699)
    * Remove unnecessary test
    
    This test is testing CSC behavior. It does not test SDK code.
    The behaivor is also changed due to https://github.com/dotnet/roslyn/pull/42769
    
    * Add --blame options to dotnet vstest and dotnet test + dll
    
    * Uncomment other test cases
    
    * Fix comma
    
    * Refactor to recude code repetition
    
    * Ensure that legacy Blame syntax still works
    
    Co-authored-by: William Li <wul@microsoft.com>
    
    Co-authored-by: William Li <wul@microsoft.com>

--- src/Cli/dotnet/commands/dotnet-test/VSTestArgumentConverter.cs
+++ src/Cli/dotnet/commands/dotnet-test/VSTestArgumentConverter.cs
@@ -6,6 +6,7 @@ namespace Microsoft.DotNet.Cli
     using System;
     using System.Collections.Generic;
     using System.Linq;
+    using static BlameArgs;
 
     /// <summary>
     /// Converts the given arguments to vstest parsable arguments
@@ -62,6 +63,8 @@ namespace Microsoft.DotNet.Cli
             ignoredArgs = new List<string>();
 
             string activeArgument = null;
+            BlameArgs blame = new BlameArgs();
+
             foreach (var arg in args)
             {
                 if (arg == "--")
@@ -76,6 +79,10 @@ namespace Microsoft.DotNet.Cli
                         if (IgnoredArguments.Contains(activeArgument))
                         {
                             ignoredArgs.Add(activeArgument);
+                        }
+                        else if (blame.IsBlameArg(activeArgument, null))
+                        {
+                            // do nothing, we process remaining arguments ourselves
                         }
                         else
                         {
@@ -101,6 +108,12 @@ namespace Microsoft.DotNet.Cli
                             continue;
                         }
 
+                        if (blame.IsBlameArg(argValues[0], argValues[1]))
+                        {
+                            blame.UpdateBlame(argValues[0], argValues[1]);
+                            continue;
+                        }
+
                         // Check if the argument is shortname
                         if (ArgumentMapping.TryGetValue(argValues[0].ToLower(), out var longName))
                         {
@@ -111,10 +124,18 @@ namespace Microsoft.DotNet.Cli
                     }
                     else
                     {
-                        activeArgument = arg.ToLower();
-                        if (ArgumentMapping.TryGetValue(activeArgument, out var value))
+                        if (blame.IsBlameSwitch(arg))
                         {
-                            activeArgument = value;
+                            blame.UpdateBlame(arg, null);
+                            activeArgument = arg;
+                        }
+                        else
+                        {
+                            activeArgument = arg.ToLower();
+                            if (ArgumentMapping.TryGetValue(activeArgument, out var value))
+                            {
+                                activeArgument = value;
+                            }
                         }
                     }
                 }
@@ -129,6 +150,10 @@ namespace Microsoft.DotNet.Cli
                         ignoredArgs.Add(activeArgument);
                         ignoredArgs.Add(arg);
                     }
+                    else if (blame.IsBlameArg(activeArgument, arg))
+                    {
+                        blame.UpdateBlame(activeArgument, arg);
+                    }
                     else
                     {
                         newArgList.Add(string.Concat(activeArgument, ":", arg));
@@ -138,7 +163,14 @@ namespace Microsoft.DotNet.Cli
                 }
                 else
                 {
-                    newArgList.Add(arg);
+                    if (blame.IsBlameArg(arg, null))
+                    {
+                        blame.UpdateBlame(arg, null);
+                    }
+                    else
+                    {
+                        newArgList.Add(arg);
+                    }
                 }
             }
 
@@ -148,10 +180,18 @@ namespace Microsoft.DotNet.Cli
                 {
                     ignoredArgs.Add(activeArgument);
                 }
+                else if( blame.IsBlameArg(activeArgument, null)) {
+                    // do nothing, we process remaining arguments ourselves
+                }
                 else
                 {
                     newArgList.Add(activeArgument);
                 }
+            }
+
+            if (blame.Blame)
+            {
+                blame.AddCombinedBlameArgs(newArgList);
             }
 
             return newArgList;
@@ -170,6 +210,186 @@ namespace Microsoft.DotNet.Cli
                 return;
             }
             newArgList.Add(verbosityString + verbosity);
+        }
+    }
+
+    class BlameArgs
+    {
+        public bool Blame = false;
+        public string LegacyBlame = null;
+
+        public bool CollectCrashDump = false;
+        public string CollectCrashDumpType = null;
+        public bool CollectCrashDumpAlways = false;
+
+        public bool CollectHangDump = false;
+        public string CollectHangDumpType = null;
+        public string CollectHangDumpTimeout = null;
+
+        public static string BlameParam = "--blame";
+        public static string BlameCrashParam = "--blame-crash";
+        public static string BlameCrashDumpTypeParam = "--blame-crash-dump-type";
+        public static string BlameCrashCollectAlwaysParam = "--blame-crash-collect-always";
+        public static string BlameHangParam = "--blame-hang";
+        public static string BlameHangDumpTypeParam = "--blame-hang-dump-type";
+        public static string BlameHangTimeoutParam = "--blame-hang-timeout";
+
+        // parameters that expect arguments
+        private readonly string[] _blameArgList = new string[]{
+            BlameCrashDumpTypeParam,
+
+            BlameHangDumpTypeParam,
+            BlameHangTimeoutParam
+        };
+
+        // parameters that don't expect any arguments
+        private readonly string[] _blameSwitchList = new string[]{
+            BlameParam,
+
+            BlameCrashParam,
+            BlameCrashCollectAlwaysParam,
+
+            BlameHangParam,
+        };
+
+
+        internal bool IsBlameArg(string parameter, string value)
+        {
+            return _blameArgList.Any(p => Eq(p, parameter)) || _blameSwitchList.Any(p => Eq(p, parameter));
+        }
+
+        private bool IsLegacyBlame(string parameter, string value)
+        {
+            // when provided --blame <value>, we do not want to process it any further
+            // most likely a legacy call, and the param is already in the format that vstest.console expects
+            return Eq(BlameParam, parameter) && !string.IsNullOrWhiteSpace(value);
+        }
+
+        internal bool IsBlame(string parameter)
+        {
+            return Eq(BlameParam, parameter);
+
+        }
+
+        internal bool IsBlameSwitch(string parameter)
+        {
+            return _blameSwitchList.Any(p => Eq(p, parameter));
+
+        }
+
+        internal void UpdateBlame(string parameter, string argument)
+        {
+            if (IsLegacyBlame(parameter, argument))
+            {
+                Blame = true;
+                LegacyBlame = argument;
+            }
+
+            if (Eq(parameter, BlameParam))
+            {
+                Blame = true;
+            }
+
+            // Any blame-crash param implies that we collect crash dump
+            if (Eq(parameter, BlameCrashParam))
+            {
+                Blame = true;
+                CollectCrashDump = true;
+            }
+
+            if (Eq(parameter, BlameCrashCollectAlwaysParam))
+            {
+                Blame = true;
+                CollectCrashDump = true;
+                CollectCrashDumpAlways = true;
+            }
+
+            if (Eq(parameter, BlameCrashDumpTypeParam))
+            {
+                Blame = true;
+                CollectCrashDump = true;
+                CollectCrashDumpType = argument;
+            }
+
+            // Any Blame-hang param implies that we collect hang dump
+            if (Eq(parameter, BlameHangParam))
+            {
+                Blame = true;
+                CollectHangDump = true;
+            }
+
+            if (Eq(parameter, BlameHangDumpTypeParam))
+            {
+                Blame = true;
+                CollectHangDump = true;
+                CollectHangDumpType = argument;
+            }
+
+            if (Eq(parameter, BlameHangTimeoutParam))
+            {
+                Blame = true;
+                CollectHangDump = true;
+                CollectHangDumpTimeout = argument;
+            }
+        }
+
+        private bool Eq(string left, string right)
+        {
+            return string.Equals(left, right, StringComparison.OrdinalIgnoreCase);
+        }
+
+        internal void AddCombinedBlameArgs(List<string> newArgList)
+        {
+            if (!Blame)
+                return;
+
+            if (!string.IsNullOrWhiteSpace(LegacyBlame))
+            {
+                // when legacy call is detected don't process
+                // any more parameters
+                newArgList.Add($"--blame:{LegacyBlame}");
+                return;
+            }
+
+            string crashDumpArgs = null;
+            string hangDumpArgs = null;
+
+            if (CollectCrashDump)
+            {
+                crashDumpArgs = "CollectDump";
+                if (CollectCrashDumpAlways)
+                {
+                    crashDumpArgs += ";CollectAlways=true";
+                }
+
+                if (!string.IsNullOrWhiteSpace(CollectCrashDumpType))
+                {
+                    crashDumpArgs += $";DumpType={CollectCrashDumpType}";
+                }
+            }
+
+            if (CollectHangDump)
+            {
+                hangDumpArgs = "CollectHangDump";
+                if (!string.IsNullOrWhiteSpace(CollectHangDumpType))
+                {
+                    hangDumpArgs += $";DumpType={CollectHangDumpType}";
+                }
+
+                if (!string.IsNullOrWhiteSpace(CollectHangDumpTimeout))
+                {
+                    hangDumpArgs += $";TestTimeout={CollectHangDumpTimeout}";
+                }
+            }
+
+            if (CollectCrashDump || CollectHangDump)
+            {
+                newArgList.Add($@"--blame:""{string.Join(";", crashDumpArgs, hangDumpArgs).Trim(';')}""");
+            }
+            else
+            {
+                newArgList.Add("--blame");
+            }
         }
     }
 }
