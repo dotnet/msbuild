79e618c83a57b74200d30d7367092acc0aa548b7
Lachlan Ennis <elachlan@gmail.com>
2020-08-04 06:28:01 +1000

    Update src/Tasks/ResolveComReference.cs
    Co-authored-by: Forgind <Forgind@users.noreply.github.com>

--- src/Tasks/ResolveComReference.cs
+++ src/Tasks/ResolveComReference.cs
@@ -327,7 +327,7 @@ namespace Microsoft.Build.Tasks
 
             _timestampCache = (ResolveComReferenceCache)StateFileBase.DeserializeCache(StateFile, Log, typeof(ResolveComReferenceCache));
 
-            if (_timestampCache == null || (_timestampCache?.ToolPathsMatchCachePaths(_tlbimpPath, _aximpPath) == false))
+            if (_timestampCache?.ToolPathsMatchCachePaths(_tlbimpPath, _aximpPath) != true))
             {
                 if (!Silent)
                 {
