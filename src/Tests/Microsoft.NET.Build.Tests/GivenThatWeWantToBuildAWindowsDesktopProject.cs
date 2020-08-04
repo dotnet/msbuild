049eaaf034977fd848aa51e500a45c370a551a6b
Sarah Oslund <sfoslund@microsoft.com>
2020-08-04 11:49:14 -0700

    Standardize TargetPlatformVersion format

--- src/Tests/Microsoft.NET.Build.Tests/GivenThatWeWantToBuildAWindowsDesktopProject.cs
+++ src/Tests/Microsoft.NET.Build.Tests/GivenThatWeWantToBuildAWindowsDesktopProject.cs
@@ -35,6 +35,25 @@ namespace Microsoft.NET.Build.Tests
                 .HaveStdOutContaining("NETSDK1137");
         }
 
+        [WindowsOnlyFact]
+        public void It_succeeds_if_windows_target_platform_version_has_trailing_zeros()
+        {
+            var testProject = new TestProject()
+            {
+                Name = "ValidWindowsVersion",
+                IsSdkProject = true,
+                TargetFrameworks = "net5.0"
+            };
+            testProject.AdditionalProperties["TargetPlatformIdentifier"] = "windows"; 
+            testProject.AdditionalProperties["TargetPlatformVersion"] = "10.0.18362.0"; // We must set this manually because if we set it in the TFM we remove the trailing zeroes. 
+            var testAsset = _testAssetsManager.CreateTestProject(testProject);
+
+            var buildCommand = new BuildCommand(testAsset);
+            buildCommand.Execute()
+                .Should()
+                .Pass();
+        }
+
         [Fact]
         public void It_fails_if_target_platform_identifier_and_version_are_invalid()
         {
