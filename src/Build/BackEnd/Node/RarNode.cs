356a33a0f500fe6d632a88b056bdb1123f40f7c7
Ondřej Štorc <t-ostorc@microsoft.com>
2020-08-12 11:49:43 +0200

    Build fix

--- src/Build/BackEnd/Node/RarNode.cs
+++ src/Build/BackEnd/Node/RarNode.cs
@@ -1,4 +1,4 @@
-// Copyright (c) Microsoft. All rights reserved.
+﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
@@ -7,9 +7,9 @@ using System.Threading;
 using System.Threading.Tasks;
 
 using Microsoft.Build.BackEnd;
+using Microsoft.Build.Framework;
 using Microsoft.Build.Internal;
 using Microsoft.Build.Tasks.ResolveAssemblyReferences.Server;
-using Microsoft.VisualStudio.Threading;
 
 namespace Microsoft.Build.Execution
 {
@@ -18,7 +18,7 @@ namespace Microsoft.Build.Execution
         private readonly OutOfProcNode _msBuildNode;
 
         private Task _rarTask;
-        private int _rarResult;
+        //private int _rarResult;
 
         private Task _msBuildTask;
         private Exception shutdownException;
@@ -29,19 +29,19 @@ namespace Microsoft.Build.Execution
             _msBuildNode = new OutOfProcNode();
         }
 
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "VSTHRD002:Avoid problematic synchronous waits",
-            Justification = "We need to wait for completion of async method in synchronous method (required by interface)")]
         public NodeEngineShutdownReason Run(bool nodeReuse, bool lowPriority, out Exception shutdownException)
         {
-            var cancellationTokenSource = new CancellationTokenSource();
+            using CancellationTokenSource cts = new CancellationTokenSource();
 
             string pipeName = CommunicationsUtilities.GetRarPipeName(nodeReuse, lowPriority);
             RarController controller = new RarController(pipeName);
 
+            _rarTask = Task.Run(async () => /*_rarResult = */await controller.StartAsync(cts.Token).ConfigureAwait(false));
+            // Exits in 15 mins (by default), if don't have any active connection
             _msBuildTask = Task.Run(() => shutdownReason = _msBuildNode.Run(nodeReuse, lowPriority, specialNode: true, out this.shutdownException), cts.Token);
 
             Task.WaitAny(_msBuildTask, _rarTask);
-            cancellationTokenSource.Cancel();
+            cts.Cancel();
 
             shutdownException = this.shutdownException;
             return shutdownReason;
