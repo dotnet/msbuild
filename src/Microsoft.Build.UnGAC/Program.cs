3857ba1dba570cfca66ecf515fb4c530fbd23c02
Ben Villalobos <bevillal@microsoft.com>
2020-08-19 16:18:40 -0700

    Using unmanaged code.
    Much cleaner!

--- src/Microsoft.Build.UnGAC/Program.cs
+++ src/Microsoft.Build.UnGAC/Program.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Runtime.ExceptionServices;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Microsoft.Build.UnGAC
 {
@@ -15,85 +8,37 @@ namespace Microsoft.Build.UnGAC
         {
             try
             {
-                List<string> allInstancesOfGACUtil = new List<string>();
-                StreamReader output;
-                StreamWriter input;
-
-                Process proc = new Process
+                string[] assembliesToUnGAC =
                 {
-                    StartInfo = new ProcessStartInfo
-                    {
-                        FileName = "C:\\Windows\\System32\\cmd.exe",
-                        Arguments = "/k", // Keep cmd open until we're done
-                        RedirectStandardOutput = true,
-                        RedirectStandardInput = true,
-                        UseShellExecute = false
-                    }
+                    "Microsoft.Build, Version=15.1.0.0",
+                    "Microsoft.Build.Engine, Version=15.1.0.0",
+                    "Microsoft.Build.Framework, Version=15.1.0.0",
+                    "Microsoft.Build.Tasks.Core, Version=15.1.0.0",
+                    "Microsoft.Build.Utilities.Core, Version=15.1.0.0",
+                    "Microsoft.Build.Conversion.Core, Version=15.1.0.0"
                 };
-                proc.Start();
 
-                // Hook into standard input/output of the process
-                output = proc.StandardOutput;
-                input = proc.StandardInput;
+                uint hresult = NativeMethods.CreateAssemblyCache(out IAssemblyCache assemblyCache, 0);
 
-                if (!input.BaseStream.CanWrite)
+                // Most significant bit is set, meaning there was an error in the Hresult.
+                if ((hresult >> 31) == 1)
                 {
-                    proc.Close();
-                    proc.Dispose();
-                    Console.WriteLine("Could not use where.exe to find gacutil.exe. Exiting...");
+                    Console.WriteLine($"Could not successfully call CreateAssemblyCache. HResult: {hresult}");
+                    Console.WriteLine("Exiting without removing assemblies from the GAC...");
                     return;
                 }
 
-                // Use where.exe to find gacutil.exe
-                input.WriteLine("where /F /R \"C:\\Program Files (x86)\\Microsoft SDKs\\Windows\" gacutil.exe");
-
-                // Throw away the first line - it's the command we just used
-                output.ReadLine();
-
-                for(string gacInstance = output.ReadLine(); !string.IsNullOrEmpty(gacInstance); gacInstance = output.ReadLine())
+                for (int i = 0; i < assembliesToUnGAC.Length; i++)
                 {
-                    allInstancesOfGACUtil.Add(gacInstance);
+                    hresult = assemblyCache.UninstallAssembly(0, assembliesToUnGAC[i], IntPtr.Zero, 0);
+
+                    // If we hit an error with an assembly, keep trying the others.
+                    if ((hresult >> 31) == 1)
+                    {
+                        Console.WriteLine($"Could not remove {assembliesToUnGAC[i]} from the GAC. HResult: {hresult}");
+                    }
                 }
 
-                if (allInstancesOfGACUtil.Count == 0)
-                {
-                    proc.Close();
-                    proc.Dispose();
-                    Console.WriteLine("Could not find instances of gacutil. Exiting...");
-                    return;
-                }
-
-                // We've found gacutil, now let's use it.
-                string gacUtilExe = allInstancesOfGACUtil[0];
-
-                if (!input.BaseStream.CanWrite)
-                {
-                    proc.Close();
-                    proc.Dispose();
-                    Console.WriteLine("Could not write gacutil commands. Exiting...");
-                    return;
-                }
-
-                input.WriteLine($"{gacUtilExe} /nologo /u \"MSBuild, Version=15.1.0.0\"\n" +
-                                $"{gacUtilExe} /nologo /u \"Microsoft.Build, Version=15.1.0.0\"\n" +
-                                $"{gacUtilExe} /nologo /u \"Microsoft.Build.Engine, Version=15.1.0.0\"\n" +
-                                $"{gacUtilExe} /nologo /u \"Microsoft.Build.Framework, Version=15.1.0.0\"\n" +
-                                $"{gacUtilExe} /nologo /u \"Microsoft.Build.Tasks.Core, Version=15.1.0.0\"\n" +
-                                $"{gacUtilExe} /nologo /u \"Microsoft.Build.Utilities.Core, Version=15.1.0.0\"\n" +
-                                $"{gacUtilExe} /nologo /u \"Microsoft.Build.Conversion.Core, Version=15.1.0.0\"\n");
-
-                // This prevents output.ReadLine() from locking up below.
-                input.Flush();
-                input.Close();
-
-                // Output everything gacutil returned.
-                for (string s = output.ReadLine(); s != null; s = output.ReadLine())
-                {
-                    Console.WriteLine(s);
-                }
-
-                proc.Close();
-                proc.Dispose();
                 Console.ReadKey();
             }
             catch (Exception e)
