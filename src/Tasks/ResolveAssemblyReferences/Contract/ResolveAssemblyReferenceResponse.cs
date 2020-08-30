ade41d8110a2994e3b4d12db7bf4a6f2105afba2
Ondřej Štorc <t-ostorc@microsoft.com>
2020-08-30 12:16:47 +0200

    WIP

--- src/Tasks/ResolveAssemblyReferences/Contract/ResolveAssemblyReferenceResponse.cs
+++ src/Tasks/ResolveAssemblyReferences/Contract/ResolveAssemblyReferenceResponse.cs
@@ -6,7 +6,7 @@ using MessagePack;
 namespace Microsoft.Build.Tasks.ResolveAssemblyReferences.Contract
 {
     [MessagePackObject]
-    internal sealed class ResolveAssemblyReferenceResponse
+    public sealed class ResolveAssemblyReferenceResponse
     {
         public ResolveAssemblyReferenceResponse()
         {
