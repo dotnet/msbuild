e0c576c751aee0f57aae32ea847fb632fe3caa42
Vlada Shubina <vshubina@microsoft.com>
2020-10-30 19:55:00 +0200

    changed kebab form to use regex solution for Unicode conversion added firstLowerCaseInvariant and firstUpperCaseInvariant forms added new forms to JSON schema added documentation added end-to-end test for new forms

--- test/Microsoft.TemplateEngine.Cli.UnitTests/ValueFormsTests.cs
+++ test/Microsoft.TemplateEngine.Cli.UnitTests/ValueFormsTests.cs
@@ -5,7 +5,7 @@ namespace Microsoft.TemplateEngine.Cli.UnitTests
     public class ValueFormsTests : EndToEndTestBase
     {
         [Theory(DisplayName = nameof(ValueFormsTest))]
-        [InlineData("TestAssets.TemplateWithValueForms --foo Test.Value6", "TemplateWithValueForms.json")]
+        [InlineData("TestAssets.TemplateWithValueForms --foo Test.Value6 --param1 MyPascalTestValue --param2 myCamelTestValue --param3 \"my test text\"", "TemplateWithValueForms.json")]
         [InlineData("TestAssets.TemplateWithDerivedSymbolWithValueForms -n Test.AppSeven", "DerivedSymbolWithValueForms.json")]
         public void ValueFormsTest(string args, params string[] scripts)
         {
