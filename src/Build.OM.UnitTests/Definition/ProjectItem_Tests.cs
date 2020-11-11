c4763c09ec283b029a701243665d49da19b4c607
Nathan Mytelka <Forgind@users.noreply.github.com>
2020-11-10 16:55:38 -0800

    Should ALWAYS be case-insensitive

--- src/Build.OM.UnitTests/Definition/ProjectItem_Tests.cs
+++ src/Build.OM.UnitTests/Definition/ProjectItem_Tests.cs
@@ -3333,28 +3333,15 @@ namespace Microsoft.Build.UnitTests.OM.Definition
 
             IList<ProjectItem> items = ObjectModelHelpers.GetItemsFromFragment(content);
 
-            if (FileUtilities.GetIsFileSystemCaseSensitive())
+            items.ShouldHaveSingleItem();
+
+            var expectedUpdated = new Dictionary<string, string>
             {
-                var expectedUpdated = new Dictionary<string, string>
-                {
-                    {"m1", "m1_contents"},
-                    {"m2", "m2_contents"},
-                };
+                {"m1", "m1_updated"},
+                {"m2", "m2_updated"},
+            };
 
-                ObjectModelHelpers.AssertItemHasMetadata(expectedUpdated, items[0]);
-            }
-            else
-            {
-                items.ShouldHaveSingleItem();
-
-                var expectedUpdated = new Dictionary<string, string>
-                {
-                    {"m1", "m1_updated"},
-                    {"m2", "m2_updated"},
-                };
-
-                ObjectModelHelpers.AssertItemHasMetadata(expectedUpdated, items[0]);
-            }
+            ObjectModelHelpers.AssertItemHasMetadata(expectedUpdated, items[0]);
         }
 
         public static IEnumerable<Object[]> UpdateAndRemoveShouldWorkWithEscapedCharactersTestData
