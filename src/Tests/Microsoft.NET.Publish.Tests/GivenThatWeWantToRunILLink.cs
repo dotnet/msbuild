72240bdb646415a1ae5c184910e12d537579c0c4
Tlakaelel Axayakatl Ceja <tlakaelel.ceja@microsoft.com>
2020-11-19 23:32:46 -0800

    Runtime informational version (#14589)
    * Print info that help recognize which library assemblies are being used.
    * Use darc to introduce MetadataLoadContext version

--- src/Tests/Microsoft.NET.Publish.Tests/GivenThatWeWantToRunILLink.cs
+++ src/Tests/Microsoft.NET.Publish.Tests/GivenThatWeWantToRunILLink.cs
@@ -6,10 +6,11 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Xml.Linq;
+using System.Reflection;
 using System.Reflection.Metadata;
 using System.Reflection.PortableExecutable;
 using System.Runtime.CompilerServices;
-using System.Text.RegularExpressions;
+using System.Text;
 using FluentAssertions;
 using Microsoft.DotNet.Cli.Utils;
 using Microsoft.Extensions.DependencyModel;
@@ -285,12 +286,12 @@ namespace Microsoft.NET.Publish.Tests
         }
 
         [RequiresMSBuildVersionTheory("16.8.0")]
-        [InlineData("net5.0")]
+        [InlineData("net6.0")]
         public void ILLink_verify_analysis_warnings_hello_world_app(string targetFramework)
         {
             var projectName = "AnalysisWarningsOnHelloWorldApp";
             var rid = EnvironmentInfo.GetCompatibleRid(targetFramework);
-            List<string> expectedOutput = new List<string> () {
+            List<string> expectedOutput = new List<string>() {
                     "ILLink : Trim analysis warning IL2059: System.Reflection.RuntimeConstructorInfo.Invoke(Object,BindingFlags,Binder,Object[],CultureInfo",
                     "ILLink : Trim analysis warning IL2070: System.Reflection.RuntimeAssembly.AddPublicNestedTypes(Type,List<Type>,List<Exception>",
                     "ILLink : Trim analysis warning IL2026: System.Reflection.RuntimeModule.ResolveLiteralField(Int32,Type[],Type[]",
@@ -335,24 +336,17 @@ namespace Microsoft.NET.Publish.Tests
                     "ILLink : Trim analysis warning IL2080: System.Resources.ResourceReader.<>c.<InitializeBinaryFormatter>b__6_1(",
                     "ILLink : Trim analysis warning IL2060: System.Resources.ResourceReader.<>c.<InitializeBinaryFormatter>b__6_1(",
                     "ILLink : Trim analysis warning IL2075: System.Diagnostics.Tracing.EventSource.CreateManifestAndDescriptors(Type,String,EventSource,EventManifestOptions",
-                    "ILLink : Trim analysis warning IL2055: System.RuntimeTypeHandle.GetTypeHelper(Type,Type[],IntPtr,Int32",
-                    "ILLink : Trim analysis warning IL2026: System.Reflection.Associates.AssignAssociates(Int32,RuntimeType,RuntimeType",
-                    "ILLink : Trim analysis warning IL2065: System.Reflection.CustomAttribute.AddCustomAttributes(ListBuilder`1&,RuntimeModule,Int32,RuntimeType,Boolean,RuntimeType.ListBuilder<Object>",
-                    "ILLink : Trim analysis warning IL2026: System.Reflection.CustomAttribute.FilterCustomAttributeRecord(MetadataToken,MetadataImport&,RuntimeModule,MetadataToken,RuntimeType,Boolean,ListBuilder`1&,RuntimeType&,IRuntimeMethodInfo&,Boolean&",
-                    "ILLink : Trim analysis warning IL2026: System.Reflection.CustomAttribute.GetAttributeUsage(RuntimeType",
-                    "ILLink : Trim analysis warning IL2075: System.Reflection.CustomAttributeData.CustomAttributeData(RuntimeModule,MetadataToken,ConstArray&",
-                    "ILLink : Trim analysis warning IL2026: System.Reflection.Emit.ModuleBuilder.GetMemberRefToken(MethodBase,IEnumerable<Type>",
-                    "ILLink : Trim analysis warning IL2072: System.Diagnostics.Tracing.NullableTypeInfo.WriteData(TraceLoggingDataCollector,PropertyValue",
-                    "ILLink : Trim analysis warning IL2070: System.RuntimeType.GetMethodBase(RuntimeType,RuntimeMethodHandleInternal",
-                    "ILLink : Trim analysis warning IL2055: System.Reflection.SignatureTypeExtensions.TryMakeGenericType(Type,Type[]",
-                    "ILLink : Trim analysis warning IL2055: System.Reflection.Emit.TypeBuilderInstantiation.Substitute(Type[]"
+                    "ILLink : Trim analysis warning IL2072: System.Diagnostics.Tracing.NullableTypeInfo.WriteData(PropertyValue",
+                    "ILLink : Trim analysis warning IL2026: System.Reflection.Emit.ModuleBuilder.GetGenericMethodBaseDefinition(MethodBase",
+                    "ILLink : Trim analysis warning IL2026: System.Reflection.Emit.ModuleBuilder.GetGenericMethodBaseDefinition(MethodBase",
+                    "ILLink : Trim analysis warning IL2055: System.RuntimeTypeHandle.GetTypeHelper(Type,Type[],IntPtr,Int32"
             };
 
             var testProject = CreateTestProjectForILLinkTesting(targetFramework, projectName);
             var testAsset = _testAssetsManager.CreateTestProject(testProject);
 
-            var result = new PublishCommand(Log, Path.Combine(testAsset.TestRoot, testProject.Name)).Execute($"/p:RuntimeIdentifier={rid}", $"/p:SelfContained=true", "/p:PublishTrimmed=true", "/p:SuppressTrimAnalysisWarnings=false");
-
+            var publishCommand = new PublishCommand(Log, Path.Combine(testAsset.TestRoot, testProject.Name));
+            var result = publishCommand.Execute($"/p:RuntimeIdentifier={rid}", $"/p:SelfContained=true", "/p:PublishTrimmed=true", "/p:SuppressTrimAnalysisWarnings=false");
             result.Should().Pass();
             //This function doesn't use an XML file like the runtime
             //to silence warnings since will make the test to fail only
@@ -360,24 +354,45 @@ namespace Microsoft.NET.Publish.Tests
             //missing warnings also. It doesn't use BeEquivalentTo
             //function that warns for any new or missing warnings
             //since the error experience is not good for a developer
-            var warnings = result.StdOut.Split('\n','\r', ')').Where(line => line.StartsWith("ILLink :"));
+            var warnings = result.StdOut.Split('\n', '\r', ')').Where(line => line.StartsWith("ILLink :"));
             var extraWarnings = warnings.Except(expectedOutput);
             var missingWarnings = expectedOutput.Except(warnings);
 
-            string errorMessage = $"The execution of a hello world app generated a diff in the number of warnings the app produces{Environment.NewLine}{Environment.NewLine}";
+            StringBuilder errorMessage = new StringBuilder();
+
+            if (missingWarnings.Any() || extraWarnings.Any())
+            {
+                // Print additional information to recognize which framework assemblies are being used.
+                errorMessage.Append($"Target framework from test: {targetFramework}{Environment.NewLine}");
+                errorMessage.Append($"Runtime identifier: {rid}{Environment.NewLine}");
+
+                // Get the array of runtime assemblies inside the publish folder.
+                string[] runtimeAssemblies = Directory.GetFiles(publishCommand.GetOutputDirectory(targetFramework: targetFramework, runtimeIdentifier: rid).FullName, "*.dll");
+                var paths = new List<string>(runtimeAssemblies);
+                var resolver = new PathAssemblyResolver(paths);
+                var mlc = new MetadataLoadContext(resolver, "System.Private.CoreLib");
+                using (mlc)
+                {
+                    Assembly assembly = mlc.LoadFromAssemblyPath(Path.Combine(publishCommand.GetOutputDirectory(targetFramework: targetFramework, runtimeIdentifier: rid).FullName, "System.Private.CoreLib.dll"));
+                    string assemblyVersionInfo = (string)assembly.CustomAttributes.Where(ca => ca.AttributeType.Name == "AssemblyInformationalVersionAttribute").Select(ca => ca.ConstructorArguments[0].Value).FirstOrDefault();
+                    errorMessage.Append($"Runtime Assembly Informational Version: {assemblyVersionInfo}{Environment.NewLine}");
+                }
+                errorMessage.Append($"The execution of a hello world app generated a diff in the number of warnings the app produces{Environment.NewLine}{Environment.NewLine}");
+            }
             if (missingWarnings.Any())
             {
-                errorMessage += $"This is a list of missing linker warnings generated with your change using a console app, if you are working on make things linker" +
-                    $" friendly please also submit a PR deleting these warnings:{Environment.NewLine}";
+                errorMessage.Append($"This is a list of missing linker warnings generated with your change using a console app, if you are working on make things linker" +
+                    $" friendly please also submit a PR deleting these warnings:{Environment.NewLine}");
                 foreach (var missingWarning in missingWarnings)
-                    errorMessage += "-  " + missingWarning + Environment.NewLine;
+                    errorMessage.Append("-  " + missingWarning + Environment.NewLine);
             }
-            if (extraWarnings.Any()) { 
-                errorMessage += $"This is a list of extra linker warnings generated with your change using a console app:{Environment.NewLine}";
+            if (extraWarnings.Any())
+            {
+                errorMessage.Append($"This is a list of extra linker warnings generated with your change using a console app:{Environment.NewLine}");
                 foreach (var extraWarning in extraWarnings)
-                    errorMessage += "+  " + extraWarning + Environment.NewLine;
+                    errorMessage.Append("+  " + extraWarning + Environment.NewLine);
             }
-            Assert.True(!missingWarnings.Any() && !extraWarnings.Any(), errorMessage);
+            Assert.True(!missingWarnings.Any() && !extraWarnings.Any(), errorMessage.ToString());
         }
 
         [Theory]
@@ -1105,7 +1120,7 @@ namespace Microsoft.NET.Publish.Tests
         {
             var ns = project.Root.Name.Namespace;
 
-            project.Root.Add (new XElement(ns + "ItemGroup",
+            project.Root.Add(new XElement(ns + "ItemGroup",
                                 new XElement("EmbeddedResource",
                                     new XAttribute("Include", substitutionsFilename),
                                     new XElement("LogicalName", substitutionsFilename))));
@@ -1129,11 +1144,11 @@ namespace Microsoft.NET.Publish.Tests
         {
             var ns = project.Root.Name.Namespace;
 
-            project.Root.Add (new XElement(ns + "ItemGroup",
+            project.Root.Add(new XElement(ns + "ItemGroup",
                                 new XElement("RuntimeHostConfigurationOption",
                                     new XAttribute("Include", "DisableFeature"),
                                     new XAttribute("Value", "true"),
-                                    new XAttribute("Trim", trim.ToString ()))));
+                                    new XAttribute("Trim", trim.ToString()))));
         }
 
         private TestProject CreateTestProjectWithAnalysisWarnings(string targetFramework, string projectName)
@@ -1227,7 +1242,8 @@ public class Program
 }
 ";
 
-            if (referenceProjectName == null) {
+            if (referenceProjectName == null)
+            {
                 return testProject;
             }
 
