f2dc5dfcc3bc7a568937d9bf9cf34b12e6cb606e
David Karlaš <david.karlas@microsoft.com>
2020-12-04 08:54:52 +0100

    Just uncommenting AllProjectsWork.cs to make future changes comperable

--- test/dotnet-new3.UnitTests/AllProjectsWork.cs
+++ test/dotnet-new3.UnitTests/AllProjectsWork.cs
@@ -1,78 +1,78 @@
-﻿//using System;
-//using System.IO;
-//using System.Linq;
-//using System.Reflection;
-//using System.Runtime.CompilerServices;
-//using Microsoft.DotNet.Cli.Utils;
-//using Microsoft.TemplateEngine.Edge;
-//using Microsoft.TemplateEngine.Mocks;
-//using Microsoft.TemplateEngine.TestHelper;
-//using Microsoft.TemplateEngine.Utils;
-//using Xunit;
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using Microsoft.DotNet.Cli.Utils;
+using Microsoft.TemplateEngine.Edge;
+using Microsoft.TemplateEngine.Mocks;
+using Microsoft.TemplateEngine.TestHelper;
+using Microsoft.TemplateEngine.Utils;
+using Xunit;
 
-//namespace DotnetNew3.UnitTests
-//{
-//    public class AllProjectsWork
-//    {
-//        private static readonly Guid _tempDirParent = Guid.NewGuid();
+namespace DotnetNew3.UnitTests
+{
+    public class AllProjectsWork
+    {
+        private static readonly Guid _tempDirParent = Guid.NewGuid();
 
-//        private static string GetWorkingDirectoryName(string suffix, [CallerMemberName]string callerName = "")
-//        {
-//            string relTo = new Uri(typeof(AllProjectsWork).GetTypeInfo().Assembly.CodeBase, UriKind.Absolute).LocalPath;
-//            relTo = Path.GetDirectoryName(relTo);
-//            return Path.Combine(relTo, callerName, _tempDirParent.ToString(), suffix);
-//        }
-                
-//        [Theory]
-//        [InlineData("emptyweb_cs-10", "web")]
-//        [InlineData("mvc_cs-10", "mvc")]
-//        [InlineData("mvc_fs-10", "mvc", "-lang", "F#")]
-//        [InlineData("api_cs-10", "api")]
-//        [InlineData("emptyweb_cs-11", "web", "-f", "1.1")]
-//        [InlineData("mvc_cs-11", "mvc", "-f", "1.1")]
-//        [InlineData("mvc_fs-11", "mvc", "-lang", "F#", "-f", "1.1")]
-//        [InlineData("api_cs-11", "api", "-f", "1.1")]
-//        [InlineData("console_cs-10", "console")]
-//        [InlineData("library_cs-10", "library")]
-//        public void AllWebProjectsRestoreAndBuild(string testName, params string[] args)
-//        {
-//            string workingDir = GetWorkingDirectoryName(testName);
-//            Directory.CreateDirectory(workingDir);
+        private static string GetWorkingDirectoryName(string suffix, [CallerMemberName] string callerName = "")
+        {
+            string relTo = new Uri(typeof(AllProjectsWork).GetTypeInfo().Assembly.CodeBase, UriKind.Absolute).LocalPath;
+            relTo = Path.GetDirectoryName(relTo);
+            return Path.Combine(relTo, callerName, _tempDirParent.ToString(), suffix);
+        }
 
-//            Program.Main(new[] { "--debug:reinit" });
-            
-//            if (string.IsNullOrEmpty(Environment.GetEnvironmentVariable("DN3")))
-//            {
-//                string relTo = new Uri(typeof(Program).GetTypeInfo().Assembly.CodeBase, UriKind.Absolute).LocalPath;
-//                relTo = Path.GetDirectoryName(relTo);
-//                relTo = Path.Combine(relTo, @"..\..\..\..\..\dev");
-//                Environment.SetEnvironmentVariable("DN3", relTo);
-//            }
+        [Theory]
+        [InlineData("emptyweb_cs-10", "web")]
+        [InlineData("mvc_cs-10", "mvc")]
+        [InlineData("mvc_fs-10", "mvc", "-lang", "F#")]
+        [InlineData("api_cs-10", "api")]
+        [InlineData("emptyweb_cs-11", "web", "-f", "1.1")]
+        [InlineData("mvc_cs-11", "mvc", "-f", "1.1")]
+        [InlineData("mvc_fs-11", "mvc", "-lang", "F#", "-f", "1.1")]
+        [InlineData("api_cs-11", "api", "-f", "1.1")]
+        [InlineData("console_cs-10", "console")]
+        [InlineData("library_cs-10", "library")]
+        public void AllWebProjectsRestoreAndBuild(string testName, params string[] args)
+        {
+            string workingDir = GetWorkingDirectoryName(testName);
+            Directory.CreateDirectory(workingDir);
 
-//            CommandResult result = Command.Create("dotnet-new3", args, outputPath: Environment.GetEnvironmentVariable("DN3"))
-//                .WorkingDirectory(workingDir)
-//                .CaptureStdErr()
-//                .CaptureStdOut()
-//                .Execute();
-//            Assert.Equal(0, result.ExitCode);
+            Program.Main(new[] { "--debug:reinit" });
 
-//            result = Command.CreateDotNet("restore", new string[0])
-//                .WorkingDirectory(workingDir)
-//                .CaptureStdErr()
-//                .CaptureStdOut()
-//                .Execute();
-//            Assert.Equal(string.Empty, result.StdErr);
-//            Assert.Equal(0, result.ExitCode);
+            if (string.IsNullOrEmpty(Environment.GetEnvironmentVariable("DN3")))
+            {
+                string relTo = new Uri(typeof(Program).GetTypeInfo().Assembly.CodeBase, UriKind.Absolute).LocalPath;
+                relTo = Path.GetDirectoryName(relTo);
+                relTo = Path.Combine(relTo, @"..\..\..\..\..\dev");
+                Environment.SetEnvironmentVariable("DN3", relTo);
+            }
 
-//            result = Command.CreateDotNet("build", new string[0])
-//                .WorkingDirectory(workingDir)
-//                .CaptureStdErr()
-//                .CaptureStdOut()
-//                .Execute();
-//            Assert.Equal(string.Empty, result.StdErr);
-//            Assert.Equal(0, result.ExitCode);
+            CommandResult result = Command.Create("dotnet-new3", args, outputPath: Environment.GetEnvironmentVariable("DN3"))
+                .WorkingDirectory(workingDir)
+                .CaptureStdErr()
+                .CaptureStdOut()
+                .Execute();
+            Assert.Equal(0, result.ExitCode);
 
-//            Directory.Delete(workingDir, true);
-//        }
-//    }
-//}
+            result = Command.CreateDotNet("restore", new string[0])
+                .WorkingDirectory(workingDir)
+                .CaptureStdErr()
+                .CaptureStdOut()
+                .Execute();
+            Assert.Equal(string.Empty, result.StdErr);
+            Assert.Equal(0, result.ExitCode);
+
+            result = Command.CreateDotNet("build", new string[0])
+                .WorkingDirectory(workingDir)
+                .CaptureStdErr()
+                .CaptureStdOut()
+                .Execute();
+            Assert.Equal(string.Empty, result.StdErr);
+            Assert.Equal(0, result.ExitCode);
+
+            Directory.Delete(workingDir, true);
+        }
+    }
+}
