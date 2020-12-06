791ba899f8697c75eedc511dd367bb1f9957413c
Haiyu Zhu <zhuhaiyu1991@outlook.com>
2020-12-06 15:04:15 +0800

    revert changes in TaskBuilder

--- src/Build/BackEnd/Components/RequestBuilder/TaskBuilder.cs
+++ src/Build/BackEnd/Components/RequestBuilder/TaskBuilder.cs
@@ -866,7 +866,7 @@ namespace Microsoft.Build.BackEnd
                     else if (type == typeof(CircularDependencyException))
                     {
                         _continueOnError = ContinueOnError.ErrorAndStop;
-                        ProjectErrorUtilities.ThrowInvalidProject(taskLoggingContext.Task.Location, "CircularDependencyInTargetGraph", taskLoggingContext.TargetLoggingContext.Target.Name);
+                        ProjectErrorUtilities.ThrowInvalidProject(taskLoggingContext.Task.Location, "CircularDependency", taskLoggingContext.TargetLoggingContext.Target.Name);
                     }
                     else if (type == typeof(InvalidProjectFileException))
                     {
