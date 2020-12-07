1ff34e830630ff7c4b6cb008e85ece4f263ace52
Forgind <Forgind@users.noreply.github.com>
2020-12-07 13:03:18 -0800

    Add timeout to hanging test (#5944)
    GraphConstructionCanLoadEntryPointsFromSolution has been hanging often. Adding a timeout to make that a bit better.

--- src/Build.UnitTests/Graph/GraphLoadedFromSolution_tests.cs
+++ src/Build.UnitTests/Graph/GraphLoadedFromSolution_tests.cs
@@ -194,7 +194,7 @@ namespace Microsoft.Build.Graph.UnitTests
             }
         }
 
-        [Theory]
+        [Theory(Timeout = 20_000)] // Test hangs intermittently: https://github.com/dotnet/msbuild/issues/5520
         [MemberData(nameof(GraphsWithUniformSolutionConfigurations))]
         public void GraphConstructionCanLoadEntryPointsFromSolution(
             Dictionary<int, int[]> edges,
