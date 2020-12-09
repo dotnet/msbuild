f235a6e08f5dfca2d1e86a3ef2d5cf1a4d3b1c71
KevinRansom <codecutter@hotmail.com>
2020-12-08 17:28:53 -0800

    FsiForwardingApp.cs

--- src/Cli/dotnet/commands/dotnet-fsi/FsiForwardingApp.cs
+++ src/Cli/dotnet/commands/dotnet-fsi/FsiForwardingApp.cs
@@ -11,16 +11,45 @@ namespace Microsoft.DotNet.Cli
 {
     public class FsiForwardingApp : ForwardingApp
     {
-        private const string FsiAppName = @"FSharp/fsi.exe";
+        private const string FsiDllName = @"FSharp/fsi.dll";
+        private const string FsiExeName = @"FSharp/fsi.exe";
 
         public FsiForwardingApp(IEnumerable<string> argsToForward)
-            : base(GetFsiExePath(), argsToForward)
+            : base(GetFsiAppPath(), argsToForward)
         {
         }
 
-        private static string GetFsiExePath()
+        private static bool exists(string path)
         {
-            return Path.Combine(AppContext.BaseDirectory, FsiAppName);
+            try
+            {
+                return File.Exists(path);
+            }
+            catch
+            {
+                return false;
+            }
+            
+        }
+
+        /*
+         * FSharp switched from compiling fsi.exe to fsi.dll which will allow us to ship an AppHost version of fsi.exe
+         * The signal that fsi.exe is an apphost fs.exe is the presence of fsi.dll
+         *
+         * So here we look for fsi.dll, if it's found then we will return the path to it, otherwise we return fsi.exe
+         * the reason for using this bridging mechanism is to simplify the coordination between F#/VS and the dotnet sdk
+        */
+        private static string GetFsiAppPath()
+        {
+            var dllPath = Path.Combine(AppContext.BaseDirectory, FsiDllName);
+            if (exists(dllPath))
+            {
+                return dllPath;
+            }
+            else
+            {
+                return Path.Combine(AppContext.BaseDirectory, FsiExeName);
+            }
         }
     }
 }
