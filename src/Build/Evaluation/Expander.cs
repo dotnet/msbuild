bd0254760b03348ddb72f2e02ff522b4a1df8a58
Rainer Sigwald <raines@microsoft.com>
2020-12-08 21:37:11 -0600

    Disambiguate order of operations
    Between an `as` and string concatenations.

--- src/Build/Evaluation/Expander.cs
+++ src/Build/Evaluation/Expander.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) Microsoft. All rights reserved.
+// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
@@ -4935,7 +4935,7 @@ namespace Microsoft.Build.Evaluation
                 }
                 else
                 {
-                    string propertyValue = "\"" + objectInstance as string + "\"";
+                    string propertyValue = $"\"{objectInstance as string}\"";
 
                     if ((_bindingFlags & BindingFlags.InvokeMethod) == BindingFlags.InvokeMethod)
                     {
