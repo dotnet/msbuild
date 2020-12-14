02a2ee7533ab96d80506c7f7383888d34c3829b1
David Karlaš <david.karlas@microsoft.com>
2020-12-14 15:58:56 +0100

    Address PR comments: Move SharedTestOutputHelper to its own file

--- test/dotnet-new3.UnitTests/SharedHomeDirectory.cs
+++ test/dotnet-new3.UnitTests/SharedHomeDirectory.cs
@@ -75,29 +75,5 @@ namespace dotnet_new3.UnitTests
                 .And
                 .NotHaveStdErr();
         }
-
-        /// <summary>
-        /// This is so we can pass ITestOutputHelper to TestCommand constructor
-        /// when calling from SharedHomeDirectory
-        /// </summary>
-        class SharedTestOutputHelper : ITestOutputHelper
-        {
-            private readonly IMessageSink sink;
-
-            public SharedTestOutputHelper(IMessageSink sink)
-            {
-                this.sink = sink;
-            }
-
-            public void WriteLine(string message)
-            {
-                sink.OnMessage(new DiagnosticMessage(message));
-            }
-
-            public void WriteLine(string format, params object[] args)
-            {
-                sink.OnMessage(new DiagnosticMessage(format, args));
-            }
-        }
     }
 }
