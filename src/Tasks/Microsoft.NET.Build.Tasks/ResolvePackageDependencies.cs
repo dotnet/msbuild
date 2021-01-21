dedc5e244607a8bfab99873deb851af1935dedd2
Sarah Oslund <sfoslund@microsoft.com>
2021-01-21 09:20:50 -0800

    PR feedback

--- src/Tasks/Microsoft.NET.Build.Tasks/ResolvePackageDependencies.cs
+++ src/Tasks/Microsoft.NET.Build.Tasks/ResolvePackageDependencies.cs
@@ -150,11 +150,9 @@ namespace Microsoft.NET.Build.Tasks
         /// </summary>
         protected override void ExecuteCore()
         {
-            var targetFrameworkToAliasMap = LockFile.PackageSpec.TargetFrameworks.ToDictionary(tf => ( tf.FrameworkName.DotNetFrameworkName, tf.FrameworkName.DotNetPlatformName ), tf => tf.TargetAlias);
-
             _targetNameToAliasMap = LockFile.Targets.ToDictionary(t => t.Name, t =>
             {
-                var alias = targetFrameworkToAliasMap[(t.TargetFramework.DotNetFrameworkName, t.TargetFramework.DotNetPlatformName)];
+                var alias = LockFile.GetLockFileTargetAlias(t);
                 if (string.IsNullOrEmpty(t.RuntimeIdentifier))
                 {
                     return alias;
