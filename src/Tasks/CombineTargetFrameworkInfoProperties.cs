9ca4269df444fc19a6dbc429b6d1f49a42b7ba4f
Daniel Plaisted <daplaist@microsoft.com>
2021-01-25 22:48:33 -0800

    Use Xml for gathering additional properties from referenced projects

---
+++ src/Tasks/CombineTargetFrameworkInfoProperties.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Build.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Microsoft.Build.Tasks
+{
+    public class CombineTargetFrameworkInfoProperties : TaskExtension
+    {
+        public string RootElementName { get; set; }
+
+        public ITaskItem[] PropertiesAndValues { get; set; }
+
+        [Output]
+        public string Result { get; set; }
+
+        public override bool Execute()
+        {
+            XElement root = new XElement(RootElementName);
+
+            foreach (var item in PropertiesAndValues)
+            {
+                root.Add(new XElement(item.ItemSpec, item.GetMetadata("Value")));
+            }
+
+            Result = root.ToString();
+
+            return true;
+        }
+    }
+}
---
+++ src/Tasks/CombineXmlElements.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Build.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Microsoft.Build.Tasks
+{
+    public class CombineXmlElements : TaskExtension
+    {
+        public string RootElementName { get; set; }
+
+        public ITaskItem [] XmlElements { get; set; }
+
+        [Output]
+        public string Result { get; set; }
+
+        public override bool Execute()
+        {
+            XElement root = new XElement(RootElementName);
+
+            foreach (var item in XmlElements)
+            {
+                root.Add(XElement.Parse(item.ItemSpec));
+            }
+
+            Result = root.ToString();
+
+            return true;
+        }
+    }
+}
