8164bfdf08382dfb6573ecc0ae46216d1756f0c5
Ivan Lieckens (Sitecore) <ivan.lieckens@sitecore.com>
2021-01-28 09:53:16 +0100

    Attempt to fail valid character path tests on MacOS and Linux

--- src/Tasks.UnitTests/Unzip_Tests.cs
+++ src/Tasks.UnitTests/Unzip_Tests.cs
@@ -329,7 +329,7 @@ namespace Microsoft.Build.Tasks.UnitTests
                                       OverwriteReadOnlyFiles = true,
                                       SkipUnchangedFiles = false,
                                       SourceFiles = new ITaskItem[] { new TaskItem(zipArchive.Path) },
-                                      Include = "<BE78A17D30144B549D21F71D5C633F7D>.txt"
+                                      Include = "<BE78A17D30144B549D21F71D5C633F7D/.txt"
                                   };
 
                 unzip.Execute().ShouldBeFalse(() => _mockEngine.Log);
@@ -385,7 +385,7 @@ namespace Microsoft.Build.Tasks.UnitTests
                                       OverwriteReadOnlyFiles = true,
                                       SkipUnchangedFiles = false,
                                       SourceFiles = new ITaskItem[] { new TaskItem(zipArchive.Path) },
-                                      Exclude = "<BE78A17D30144B549D21F71D5C633F7D>.txt"
+                                      Exclude = "<BE78A17D30144B549D21F71D5C633F7D/.txt"
                                   };
 
                 unzip.Execute().ShouldBeFalse(() => _mockEngine.Log);
