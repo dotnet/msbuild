d9adc6b9693dd78bd33a8ec1dcc0973f00235876
Jacques Eloff <joeloff@users.noreply.github.com>
2021-04-05 02:02:56 -0700

    Fix unit tests to work around .NET quirks

--- src/Tests/Microsoft.Win32.Msi.Tests/WindowsInstallerExceptionTests.cs
+++ src/Tests/Microsoft.Win32.Msi.Tests/WindowsInstallerExceptionTests.cs
@@ -9,13 +9,14 @@ namespace Microsoft.Win32.Msi.Tests
     public class WindowsInstallerExceptionTests
     {
         [WindowsOnlyTheory]
-        [InlineData(Error.ACCESS_DENIED, "Access is denied.")]
-        [InlineData(Error.INSTALL_PACKAGE_OPEN_FAILED, "This installation package could not be opened. Verify that the package exists and that you can access it, or contact the application vendor to verify that this is a valid Windows Installer package.")]
+        [InlineData(Error.ACCESS_DENIED, "Access is denied")]
+        [InlineData(Error.INSTALL_PACKAGE_OPEN_FAILED, "This installation package could not be opened. Verify that the package exists and that you can access it, or contact the application vendor to verify that this is a valid Windows Installer package")]
         public void ItContainsValidErrorMessage(uint errorCode, string expectedMessage)
         {
             WindowsInstallerException e = new(unchecked((int)errorCode));
 
-            Assert.Equal(expectedMessage, e.Message);
+            // Exception messages are different between .NET Framework 4.7.2 (no "." at the end) and .NET 6.0 (includes ".")
+            Assert.StartsWith(expectedMessage, e.Message);
         }
     }
 }
