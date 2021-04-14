5dc0b323bb6b590683f4aa69d6e194131074cfdc
Vlada Shubina <vshubina@microsoft.com>
2021-04-14 13:30:24 +0300

    removed unused members

--- src/Microsoft.TemplateEngine.Cli/TemplateSearch/ParameterNameDisposition.cs
+++
@@ -1,9 +0,0 @@
-namespace Microsoft.TemplateEngine.Cli.TemplateSearch
-{
-    // Indicates the matched-ness of a parameter name.
-    internal enum ParameterNameDisposition
-    {
-        Exact,
-        Invalid,
-    };
-}
