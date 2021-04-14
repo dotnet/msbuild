2615576d276799bc208fbf9ad68726ef776d5636
Vlada Shubina <vshubina@microsoft.com>
2021-04-14 14:34:00 +0300

    implementation of MatchInfo for CLI Parameters

---
+++ src/Microsoft.TemplateEngine.Cli/TemplateResolution/ParameterMatchInfo.cs
@@ -0,0 +1,19 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+#nullable enable
+
+using Microsoft.TemplateEngine.Abstractions.TemplateFiltering;
+
+namespace Microsoft.TemplateEngine.Cli.TemplateResolution
+{
+    internal class ParameterMatchInfo : MatchInfo
+    {
+        internal ParameterMatchInfo(string name, string? value, MatchKind kind, string? inputFormat = null) : base (name, value, kind)
+        {
+            InputFormat = inputFormat;
+        }
+
+        public string? InputFormat { get; }
+    }
+}
