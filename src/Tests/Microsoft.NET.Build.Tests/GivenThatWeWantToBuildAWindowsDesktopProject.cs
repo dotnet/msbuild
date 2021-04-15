ff2c5be4f4fd0fdc5deef7cc0fa28aa83cab9590
Sarah Oslund <sfoslund@microsoft.com>
2021-04-15 13:10:18 -0700

    Adding windows as installed workload in MSBuild evaluation

--- src/Tests/Microsoft.NET.Build.Tests/GivenThatWeWantToBuildAWindowsDesktopProject.cs
+++ src/Tests/Microsoft.NET.Build.Tests/GivenThatWeWantToBuildAWindowsDesktopProject.cs
@@ -341,6 +341,37 @@ namespace Microsoft.NET.Build.Tests
             GetPropertyValue(testAsset, "TargetPlatformMoniker").Should().Be("Windows,Version=10.0.19041.0");
         }
 
+        [WindowsOnlyTheory]
+        [InlineData("net5.0", true)]
+        [InlineData("net5.0-windows10.0.19041.0", true)]
+        [InlineData("netcoreapp3.1", false)]
+        [InlineData("net472", false)]
+        public void WindowsWorkloadIsInstalledForNet5AndUp(string targetFramework, bool windowsWorkloadInstalled)
+        {
+            var testProject = new TestProject()
+            {
+                TargetFrameworks = targetFramework
+            };
+            var testAsset = _testAssetsManager.CreateTestProject(testProject);
+
+            var getValueCommand = new GetValuesCommand(testAsset, "SdkSupportedTargetPlatformIdentifier", GetValuesCommand.ValueType.Item);
+            getValueCommand.Execute()
+                .Should()
+                .Pass();
+
+            if (windowsWorkloadInstalled)
+            {
+                getValueCommand.GetValues()
+                    .Should()
+                    .Contain("windows");
+            } 
+            else
+            {
+                getValueCommand.GetValues()
+                    .Should()
+                    .NotContain("windows");
+            }
+        }
 
         private string GetPropertyValue(TestAsset testAsset, string propertyName)
         {
