73454b1f45a9c0865a108545038b64b4c9048bbd
Vlada Shubina <vshubina@microsoft.com>
2021-04-20 17:39:41 +0300

    improved logging in TestHost

--- test/Microsoft.TemplateEngine.TestHelper/EnvironmentSettingsHelper.cs
+++ test/Microsoft.TemplateEngine.TestHelper/EnvironmentSettingsHelper.cs
@@ -1,40 +1,45 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+#nullable enable
+
 using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
 using System.Reflection;
+using System.Runtime.CompilerServices;
 using Microsoft.TemplateEngine.Abstractions;
 using Microsoft.TemplateEngine.Edge;
 using Microsoft.TemplateEngine.Edge.Settings;
-using Microsoft.TemplateEngine.Orchestrator.RunnableProjects;
 using Microsoft.TemplateEngine.Utils;
 
 namespace Microsoft.TemplateEngine.TestHelper
 {
     public class EnvironmentSettingsHelper : IDisposable
     {
-        private List<string> foldersToCleanup = new List<string>();
-        private List<EngineEnvironmentSettings> engineEnvironmentToDispose = new List<EngineEnvironmentSettings>();
+        private readonly List<string> _foldersToCleanup = new List<string>();
+        private readonly List<EngineEnvironmentSettings> _engineEnvironmentToDispose = new List<EngineEnvironmentSettings>();
 
-        public IEngineEnvironmentSettings CreateEnvironment(string locale = null, bool virtualize = false)
+        public IEngineEnvironmentSettings CreateEnvironment(
+            string? locale = null,
+            bool virtualize = false,
+            [CallerMemberName] string hostIdentifier = "",
+            bool loadDefaultGenerator = true)
         {
             if (string.IsNullOrEmpty(locale))
             {
                 locale = "en-US";
             }
-
-            ITemplateEngineHost host = new TestHost
+            List<Assembly> builtIns = new List<Assembly>();
+            if (loadDefaultGenerator)
             {
-                HostIdentifier = "TestRunner",
-                Version = "1.0.0.0",
-                BuiltInComponents = new AssemblyComponentCatalog(new List<Assembly>()
-                {
-                    typeof(Microsoft.TemplateEngine.Orchestrator.RunnableProjects.Abstractions.IMacro).Assembly,   //RunnableProject
-                    typeof(SettingsLoader).Assembly //Edge
-                }),
+                builtIns.Add(typeof(Orchestrator.RunnableProjects.Abstractions.IMacro).Assembly);
+            }
+
+            ITemplateEngineHost host = new TestHost(hostIdentifier: string.IsNullOrWhiteSpace(hostIdentifier) ? "TestRunner" : hostIdentifier)
+            {
+                BuiltInComponents = new AssemblyComponentCatalog(builtIns),
                 FileSystem = new MonitoredFileSystem(new PhysicalFileSystem()),
                 FallbackHostTemplateConfigNames = new[] { "dotnetcli" }
             };
@@ -50,21 +55,23 @@ namespace Microsoft.TemplateEngine.TestHelper
                 var tempateEngineRoot = Path.Combine(CreateTemporaryFolder(), ".templateengine");
                 engineEnvironmentSettings = new EngineEnvironmentSettings(host, (x) => new SettingsLoader(x), tempateEngineRoot);
             }
-            engineEnvironmentToDispose.Add(engineEnvironmentSettings);
+            _engineEnvironmentToDispose.Add(engineEnvironmentSettings);
             return engineEnvironmentSettings;
         }
+
+
 
         public string CreateTemporaryFolder(string name = "")
         {
             string folder = TestUtils.CreateTemporaryFolder(name);
-            foldersToCleanup.Add(folder);
+            _foldersToCleanup.Add(folder);
             return folder;
         }
 
         public void Dispose()
         {
-            engineEnvironmentToDispose.ForEach(e => e.Dispose());
-            foldersToCleanup.ForEach(f => Directory.Delete(f, true));
+            _engineEnvironmentToDispose.ForEach(e => e.Dispose());
+            _foldersToCleanup.ForEach(f => Directory.Delete(f, true));
         }
     }
 }
--- test/Microsoft.TemplateEngine.TestHelper/TestHost.cs
+++ test/Microsoft.TemplateEngine.TestHelper/TestHost.cs
@@ -1,18 +1,25 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+#nullable enable
+
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
 using Microsoft.TemplateEngine.Abstractions;
 using Microsoft.TemplateEngine.Abstractions.PhysicalFileSystem;
 using Microsoft.TemplateEngine.Utils;
 
 namespace Microsoft.TemplateEngine.TestHelper
 {
-    public class TestHost : ITemplateEngineHost
+    internal class TestHost : ITemplateEngineHost
     {
-        public TestHost()
+        public TestHost([CallerMemberName] string hostIdentifier = "", string version = "1.0.0")
         {
+            HostIdentifier = string.IsNullOrWhiteSpace(hostIdentifier) ? "TestRunner" : hostIdentifier;
+            Version = string.IsNullOrWhiteSpace(version) ? "1.0.0" : version;
             BuiltInComponents = new List<KeyValuePair<Guid, Func<Type>>>();
             HostParamDefaults = new Dictionary<string, string>();
             FileSystem = new PhysicalFileSystem();
@@ -24,27 +31,27 @@ namespace Microsoft.TemplateEngine.TestHelper
 
         public delegate void CriticalErrorHandler(string code, string message, string currentFile, long currentPosition);
 
-        public event Action<string, TimeSpan> TimingCompleted;
+        public event Action<string, TimeSpan>? TimingCompleted;
 
-        public event Action<string, object> SymbolUsed;
+        public event Action<string, object>? SymbolUsed;
 
-        public event ParameterErrorHandler ParameterError;
+        public event ParameterErrorHandler? ParameterError;
 
-        public event NonCriticalErrorHandler NonCriticalError;
+        public event NonCriticalErrorHandler? NonCriticalError;
 
-        public event CriticalErrorHandler CriticalError;
+        public event CriticalErrorHandler? CriticalError;
 
-        public event Action<string> MessageReceived;
+        public event Action<string>? MessageReceived;
 
         public Dictionary<string, string> HostParamDefaults { get; set; }
 
         public IPhysicalFileSystem FileSystem { get; set; }
 
-        public string HostIdentifier { get; set; }
+        public string HostIdentifier { get; }
 
-        public IReadOnlyList<string> FallbackHostTemplateConfigNames { get; set; }
+        public IReadOnlyList<string>? FallbackHostTemplateConfigNames { get; set; }
 
-        public string Version { get; set; }
+        public string Version { get; }
 
         public IReadOnlyList<KeyValuePair<Guid, Func<Type>>> BuiltInComponents { get; set; }
 
@@ -55,32 +62,38 @@ namespace Microsoft.TemplateEngine.TestHelper
 
         public void OnTimingCompleted(string label, TimeSpan timing)
         {
+            Console.WriteLine($"[{HostIdentifier}][{nameof(OnTimingCompleted)}][{label}]: completed in {timing.ToString()}");
             TimingCompleted?.Invoke(label, timing);
         }
 
         public void OnSymbolUsed(string symbol, object value)
         {
+            Console.WriteLine($"[{HostIdentifier}][{nameof(OnSymbolUsed)}] symbol {symbol} with value: {value}");
             SymbolUsed?.Invoke(symbol, value);
         }
 
-        public bool OnParameterError(ITemplateParameter parameter, string receivedValue, string message, out string newValue)
+        public bool OnParameterError(ITemplateParameter parameter, string receivedValue, string message, out string? newValue)
         {
             newValue = null;
+            Console.WriteLine($"[{HostIdentifier}][{nameof(OnParameterError)}] {message}; parameter '{parameter.Name}', received value '{receivedValue}'");
             return ParameterError?.Invoke(parameter, receivedValue, message, out newValue) ?? false;
         }
 
         public bool OnNonCriticalError(string code, string message, string currentFile, long currentPosition)
         {
+            Console.WriteLine($"[{HostIdentifier}][{nameof(OnNonCriticalError)}] {message} (code: {code}, current file: {currentFile}, currentPosition: {currentPosition})");
             return NonCriticalError?.Invoke(code, message, currentFile, currentPosition) ?? false;
         }
 
         public void OnCriticalError(string code, string message, string currentFile, long currentPosition)
         {
+            Console.WriteLine($"[{HostIdentifier}][{nameof(OnCriticalError)}] {message} (code: {code}, current file: {currentFile}, currentPosition: {currentPosition})");
             CriticalError?.Invoke(code, message, currentFile, currentPosition);
         }
 
         public void LogMessage(string message)
         {
+            Console.WriteLine($"[{HostIdentifier}][{nameof(LogMessage)}] {message}");
             MessageReceived?.Invoke(message);
         }
 
@@ -91,21 +104,23 @@ namespace Microsoft.TemplateEngine.TestHelper
 
         public bool OnPotentiallyDestructiveChangesDetected(IReadOnlyList<IFileChange> changes, IReadOnlyList<IFileChange> destructiveChanges)
         {
+            var sb = new StringBuilder();
+            sb.Append("Changes: ").AppendLine(string.Join("|", changes.Select(change => $"{change.ChangeKind} in {change.TargetRelativePath}")));
+            sb.Append("Destructive changes: ").AppendLine(string.Join("|", destructiveChanges.Select(change => $"{change.ChangeKind} in {change.TargetRelativePath}")));
+
+            Console.WriteLine($"[{HostIdentifier}][{nameof(OnPotentiallyDestructiveChangesDetected)}] {sb.ToString()}");
             return true;
         }
 
         public bool OnConfirmPartialMatch(string name)
         {
+            Console.WriteLine($"[{HostIdentifier}][{nameof(OnConfirmPartialMatch)}] {name}");
             return true;
         }
 
         public void LogDiagnosticMessage(string message, string category, params string[] details)
         {
-            Console.WriteLine("Diag: " + message + " (" + category + ")");
-            foreach (string detail in details)
-            {
-                Console.WriteLine("      " + detail);
-            }
+            Console.WriteLine($"[{HostIdentifier}][{nameof(LogDiagnosticMessage)}][{category}] {message} (details: {string.Join(";", details)})");
         }
 
         public void LogTiming(string label, TimeSpan duration, int depth)
