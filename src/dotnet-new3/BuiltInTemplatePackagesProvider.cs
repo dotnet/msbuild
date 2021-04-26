9f5e02982b2989235005dbb9e72c35668290be97
Vlada Shubina <vshubina@microsoft.com>
2021-04-26 12:50:23 +0300

    BuiltInTemplatePackagesProvider - read templates to install in provider itself
    some reasons here:
    if we have to store name of "defaultinstall.template.list" in Edge provider itself should be in edge
    I can rework and put this provider in Edge if valuable

--- src/dotnet-new3/BuiltInTemplatePackagesProvider.cs
+++ src/dotnet-new3/BuiltInTemplatePackagesProvider.cs
@@ -10,7 +10,6 @@ using System.Threading.Tasks;
 using Microsoft.TemplateEngine.Abstractions;
 using Microsoft.TemplateEngine.Abstractions.PhysicalFileSystem;
 using Microsoft.TemplateEngine.Abstractions.TemplatePackage;
-using Microsoft.TemplateEngine.Edge;
 using Microsoft.TemplateEngine.Utils;
 
 namespace Dotnet_new3
@@ -52,11 +51,12 @@ namespace Dotnet_new3
             public Task<IReadOnlyList<ITemplatePackage>> GetAllTemplatePackagesAsync(CancellationToken cancellationToken)
             {
                 List<ITemplatePackage> templatePackages = new List<ITemplatePackage>();
-
+                string assemblyLocation = Path.GetDirectoryName(typeof(Program).Assembly.Location);
                 string dn3Path = _settings.Environment.GetEnvironmentVariable("DN3");
+
                 if (string.IsNullOrEmpty(dn3Path))
                 {
-                    string path = typeof(Program).Assembly.Location;
+                    string path = assemblyLocation;
                     while (path != null && !File.Exists(Path.Combine(path, "Microsoft.TemplateEngine.sln")))
                     {
                         path = Path.GetDirectoryName(path);
@@ -69,12 +69,12 @@ namespace Dotnet_new3
                     Environment.SetEnvironmentVariable("DN3", path);
                 }
 
-                Paths paths = new Paths(_settings);
-
-                if (paths.FileExists(paths.Global.DefaultInstallTemplateList))
+                string defaultPackagesFilePath = Path.Combine(assemblyLocation, "defaultinstall.template.list");
+                if (_settings.Host.FileSystem.FileExists(defaultPackagesFilePath))
                 {
                     IFileLastWriteTimeSource fileSystem = _settings.Host.FileSystem as IFileLastWriteTimeSource;
-                    foreach (string sourceLocation in paths.ReadAllText(paths.Global.DefaultInstallTemplateList).Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries))
+                    string packagesToInstall = _settings.Host.FileSystem.ReadAllText(defaultPackagesFilePath);
+                    foreach (string sourceLocation in packagesToInstall.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries))
                     {
                         string expandedPath = Environment.ExpandEnvironmentVariables(sourceLocation).Replace('\\', Path.DirectorySeparatorChar);
                         IEnumerable<string> expandedPaths = InstallRequestPathResolution.ExpandMaskedPath(expandedPath, _settings);
