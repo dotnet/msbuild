3ba96173c8e4be2660734882092adc115677b000
Ladi Prosek <laprosek@microsoft.com>
2021-04-27 17:02:20 +0200

    Switch from changewave 17.0 to 16.10

--- src/Build/Xml/XmlReaderExtension.cs
+++ src/Build/Xml/XmlReaderExtension.cs
@@ -105,7 +105,7 @@ namespace Microsoft.Build.Internal
             string uri = new UriBuilder(Uri.UriSchemeFile, string.Empty) { Path = file }.ToString();
 
             XmlReader reader = null;
-            if (ChangeWaves.AreFeaturesEnabled(ChangeWaves.Wave17_0) && loadAsReadOnly && !_disableReadOnlyLoad)
+            if (ChangeWaves.AreFeaturesEnabled(ChangeWaves.Wave16_10) && loadAsReadOnly && !_disableReadOnlyLoad)
             {
                 // Create an XML reader with IgnoreComments and IgnoreWhitespace set if we know that we won't be asked
                 // to write the DOM back to a file. This is a performance optimization.
