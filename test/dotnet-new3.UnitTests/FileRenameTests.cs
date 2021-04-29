d2f11aaf74a1fed8662bd855a11a3003cfa95588
Vlada Shubina <vshubina@microsoft.com>
2021-04-29 19:29:20 +0300

    fixes dotnet/templating#3094 fileRename doesn't work with "now" generator
    - forces now generator to string datatype
    - fixes FileRenameGenerator to cast object to string for replacement (same as content replacement)
    - remove chars invalid in path in replacement value (except separators as we allow such renames)

---
+++ test/dotnet-new3.UnitTests/FileRenameTests.cs
@@ -0,0 +1,43 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+#nullable enable
+
+using System.IO;
+using System.Linq;
+using Microsoft.NET.TestFramework.Assertions;
+using Microsoft.TemplateEngine.TestHelper;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Dotnet_new3.IntegrationTests
+{
+    public class FileRenameTests
+    {
+        private readonly ITestOutputHelper _log;
+
+        public FileRenameTests(ITestOutputHelper log)
+        {
+            _log = log;
+        }
+
+        [Fact]
+        public void CanUseFileRenameWithNowGenerator()
+        {
+            string home = TestUtils.CreateTemporaryFolder("Home");
+            string workingDirectory = TestUtils.CreateTemporaryFolder();
+            Helpers.InstallTestTemplate("TemplateWithFileRenameDate", _log, workingDirectory, home);
+            new DotnetNewCommand(_log, "TestAssets.TemplateWithFileRenameDate", "--migrationName", "MyTestName")
+                .WithCustomHive(home)
+                .WithWorkingDirectory(workingDirectory)
+                .Execute()
+                .Should()
+                .ExitWith(0)
+                .And.NotHaveStdErr()
+                .And.HaveStdOutContaining("The template \"TestAssets.TemplateWithFileRenameDate\" was created successfully.");
+
+            DirectoryInfo directoryInfo = new DirectoryInfo(workingDirectory);
+            Assert.Matches("\\d{8}_mytestname.cs", directoryInfo.EnumerateFiles().Single().Name);
+        }
+    }
+}
