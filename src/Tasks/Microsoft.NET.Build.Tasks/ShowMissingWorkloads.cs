1a9a8cc9847d7ff67b3386ec89a5f26c4993036e
Daniel Plaisted <daplaist@microsoft.com>
2021-05-16 08:44:03 -0700

    Generate suggestions for workloads to install

--- src/Tasks/Microsoft.NET.Build.Tasks/ShowMissingWorkloads.cs
+++ src/Tasks/Microsoft.NET.Build.Tasks/ShowMissingWorkloads.cs
@@ -9,6 +9,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using Microsoft.NET.Sdk.WorkloadManifestReader;
 
 namespace Microsoft.NET.Build.Tasks
 {
@@ -16,17 +18,46 @@ namespace Microsoft.NET.Build.Tasks
     {
         public ITaskItem[] MissingWorkloadPacks { get; set; }
 
+        public string NetCoreRoot { get; set; }
+
+        public string NETCoreSdkVersion { get; set; }
+
+        public bool GenerateErrorsForMissingWorkloads { get; set; }
+
+        [Output]
+        public ITaskItem[] SuggestedWorkloads { get; set; }
+
         protected override void ExecuteCore()
         {
             if (MissingWorkloadPacks.Any())
             {
-                //  TODO: Once IWorkloadResolver.GetWorkloadSuggestionForMissingPacks is implemented, switch to using that to report recommended workloads to install,
-                //  instead of reporting which workload packs are missing
-                var errorMessage = string.Format(CultureInfo.CurrentCulture,
-                    Strings.WorkloadNotInstalled, string.Join(" ", MissingWorkloadPacks.Select(p => p.ItemSpec)));
+                var workloadManifestProvider = new SdkDirectoryWorkloadManifestProvider(NetCoreRoot, NETCoreSdkVersion);
+                var workloadResolver = WorkloadResolver.Create(workloadManifestProvider, NetCoreRoot, NETCoreSdkVersion);
 
-                Log.LogError(errorMessage);
+                var suggestedWorkloads = workloadResolver.GetWorkloadSuggestionForMissingPacks(MissingWorkloadPacks.Select(item => item.ItemSpec).ToList());
+
+                if (GenerateErrorsForMissingWorkloads)
+                {
+                    var suggestedInstallCommand = "dotnet workload install " + string.Join(" ", suggestedWorkloads.Select(w => w.Id));
+
+                    var errorMessage = string.Format(CultureInfo.CurrentCulture,
+                        Strings.WorkloadNotInstalled, string.Join(" ", suggestedWorkloads.Select(w => w.Id)), suggestedInstallCommand);
+
+                    Log.LogError(errorMessage);
+                }
+
+                SuggestedWorkloads = suggestedWorkloads.Select(suggestedWorkload =>
+                {
+                    var taskItem = new TaskItem(suggestedWorkload.Id);
+                    taskItem.SetMetadata("VisualStudioComponentId", ToSafeId(suggestedWorkload.Id));
+                    return taskItem;
+                }).ToArray();
             }
+        }
+
+        internal static string ToSafeId(string id)
+        {
+            return id.Replace("-", ".").Replace(" ", ".").Replace("_", ".");
         }
     }
 }
