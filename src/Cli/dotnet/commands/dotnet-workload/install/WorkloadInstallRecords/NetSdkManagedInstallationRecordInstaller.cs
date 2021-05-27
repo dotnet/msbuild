b4433aa99e4063ebb9da26b14e097eca17579e5a
Sarah Oslund <sfoslund@microsoft.com>
2021-05-27 16:26:35 -0700

    Implement workload uninstall

--- src/Cli/dotnet/commands/dotnet-workload/install/WorkloadInstallRecords/NetSdkManagedInstallationRecordInstaller.cs
+++ src/Cli/dotnet/commands/dotnet-workload/install/WorkloadInstallRecords/NetSdkManagedInstallationRecordInstaller.cs
@@ -1,6 +1,7 @@
 // Copyright (c) .NET Foundation and contributors. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -65,6 +66,8 @@ namespace Microsoft.DotNet.Workloads.Workload.Install.InstallRecord
             var path = Path.Combine(_workloadMetadataDir, featureBand.ToString(), _installedWorkloadDir, workloadId.ToString());
             if (File.Exists(path))
             {
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
                 File.Delete(path);
             }
         }
