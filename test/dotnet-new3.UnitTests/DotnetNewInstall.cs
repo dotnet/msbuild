a06cf2a72c05afa463063bb4f91d8a5c3b41e3e1
Bekir Ozturk <60651445+bekir-ozturk@users.noreply.github.com>
2021-06-10 18:33:47 +0200

    Localization model validation (#3216)
    * Combine template loding to result in SimpleConfigModel
    
    * refactoring draft
    
    * improved template validation logic in RunnableProjectGenerator
    
    * Symbols and SimpleConfigModel are immutable as possible
    
    # Conflicts:
    #	src/Microsoft.TemplateEngine.Orchestrator.RunnableProjects/RunnableProjectGenerator.cs
    #	src/Microsoft.TemplateEngine.Orchestrator.RunnableProjects/SimpleConfigModel.cs
    
    * Make PostActionModel immutable
    
    * Localization loading and verification
    
    * fixes and many tests
    
    * fixed unix test error
    
    * Addressing PR feedback
    
    * Fix doc errors
    
    * Update test strings for German
    
    Co-authored-by: Vlada Shubina <vshubina@microsoft.com>

--- test/dotnet-new3.UnitTests/DotnetNewInstall.cs
+++ test/dotnet-new3.UnitTests/DotnetNewInstall.cs
@@ -524,10 +524,12 @@ namespace Dotnet_new3.IntegrationTests
                 .Should()
                 .ExitWith(0)
                 .And.NotHaveStdErr()
-                .And.HaveStdOutContaining($"Error: Failed to load template from {invalidTemplatePath}:")
-                .And.HaveStdOutContaining($"Error:     Missing 'identity' in '/template.json'")
-                .And.HaveStdOutContaining($"Error:     Missing 'name' in '/template.json'")
-                .And.HaveStdOutContaining($"Error:     Missing 'shortName' in '/template.json'")
+                .And.HaveStdOutContaining($"Error: Failed to load template from {invalidTemplatePath}")
+                .And.HaveStdOutContaining($"  Missing 'identity'.")
+                .And.HaveStdOutContaining($"  Missing 'name'.")
+                .And.HaveStdOutContaining($"  Missing 'shortName'.")
+                .And.HaveStdOutContaining($"  The template root is outside the specified install source location.")
+                .And.HaveStdOutContaining($"  Source location './' is outside the specified install source location.")
                 .And.HaveStdOutContaining($"No templates were found in the package {invalidTemplatePath}.");
         }
     }
--- test/dotnet-new3.UnitTests/DotnetNewLocaleTests.cs
+++ test/dotnet-new3.UnitTests/DotnetNewLocaleTests.cs
@@ -90,5 +90,178 @@ namespace Dotnet_new3.IntegrationTests
                 .And
                 .HaveStdOutMatching(Regex.Escape(expectedName) + ".*TestAssets.TemplateWithLocalization");
         }
+
+        [Fact]
+        public void SkipsLocalizationOnInstall_WhenInvalidFormat()
+        {
+            var home = TestUtils.CreateTemporaryFolder("Home");
+            var workingDir = TestUtils.CreateTemporaryFolder("Home");
+            var testTemplateLocation = TestUtils.GetTestTemplateLocation("Invalid/Localization/InvalidFormat");
+            new DotnetNewCommand(_log, "-i", testTemplateLocation)
+                .WithCustomHive(home)
+                .WithWorkingDirectory(workingDir)
+                .Execute()
+                .Should()
+                .ExitWith(0)
+                .And
+                .NotHaveStdErr()
+                .And.HaveStdOutContaining("Warning: Failed to read parse localization file").And.HaveStdOutContaining("localize/templatestrings.de-DE.json")
+                .And.HaveStdOutContaining($"Success: {testTemplateLocation} installed the following templates:").And.HaveStdOutContaining("TestAssets.Invalid.Localization.InvalidFormat")
+                .And.HaveStdOutContaining("name in base configuration");
+        }
+
+        [Fact]
+        public void SkipsLocalizationOnInstall_WhenLocalizationValidationFails()
+        {
+            var home = TestUtils.CreateTemporaryFolder("Home");
+            var workingDir = TestUtils.CreateTemporaryFolder("Home");
+            var testTemplateLocation = TestUtils.GetTestTemplateLocation("Invalid/Localization/ValidationFailure");
+
+            var expectedErrors = new[]
+            {
+@$"Warning: [{testTemplateLocation + Path.DirectorySeparatorChar}.template.config/template.json]: id of the post action 'pa2' at index '3' is not unique. Only the first post action that uses this id will be localized.",
+
+@$"Warning: Localization file {testTemplateLocation + Path.DirectorySeparatorChar}.template.config/localize/templatestrings.de-DE.json is not compatible with base configuration {testTemplateLocation + Path.DirectorySeparatorChar}.template.config/template.json, and will be skipped.
+  In localization file under the post action with id 'pa1', there are localized strings for manual instruction(s) with ids 'do-not-exist'. These manual instructions do not exist in the template.json file and should be removed from localization file.
+  Post action(s) with id(s) 'pa0' specified in the localization file do not exist in the template.json file. Remove the localized strings from the localization file.",
+
+@$"Warning: Localization file {testTemplateLocation + Path.DirectorySeparatorChar}.template.config/localize/templatestrings.tr.json is not compatible with base configuration {testTemplateLocation + Path.DirectorySeparatorChar}.template.config/template.json, and will be skipped.
+  Post action(s) with id(s) 'pa6' specified in the localization file do not exist in the template.json file. Remove the localized strings from the localization file."
+  };
+
+            var commandResult = new DotnetNewCommand(_log, "-i", testTemplateLocation)
+                .WithCustomHive(home)
+                .WithWorkingDirectory(workingDir)
+                .Execute();
+
+            commandResult.Should()
+                .ExitWith(0)
+                .And
+                .NotHaveStdErr()
+                .And.HaveStdOutContaining($"Success: {testTemplateLocation} installed the following templates:").And.HaveStdOutContaining("TestAssets.Invalid.Localiation.ValidationFailure");
+            foreach (var error in expectedErrors)
+            {
+                commandResult.Should().HaveStdOutContaining(error);
+            }
+        }
+
+        [Fact]
+        public void SkipsLocalizationOnInstantiate_WhenInvalidFormat()
+        {
+            var home = TestUtils.CreateTemporaryFolder("Home");
+            var workingDir = TestUtils.CreateTemporaryFolder();
+            var validTestTemplateLocation = TestUtils.GetTestTemplateLocation("TemplateWithLocalization");
+            var invalidTestTemplateLocation = TestUtils.GetTestTemplateLocation("Invalid/Localization/InvalidFormat");
+            var tmpTemplateLocation = TestUtils.CreateTemporaryFolder();
+            DirectoryCopy(validTestTemplateLocation, tmpTemplateLocation, copySubDirs: true);
+
+            new DotnetNewCommand(_log, "-i", tmpTemplateLocation)
+                .WithCustomHive(home)
+                .WithWorkingDirectory(workingDir)
+                .WithEnvironmentVariable("DOTNET_CLI_UI_LANGUAGE", "de-DE")
+                .Execute()
+                .Should()
+                .ExitWith(0)
+                .And
+                .NotHaveStdErr()
+                .And.NotHaveStdOutContaining("Warning: Failed to read parse localization file").And.NotHaveStdOutContaining("localize/templatestrings.de-DE.json")
+                .And.HaveStdOutContaining($"Success: {tmpTemplateLocation} installed the following templates:").And.HaveStdOutContaining("TemplateWithLocalization")
+                .And.HaveStdOutContaining("name_de-DE:äÄßöÖüÜ");
+
+            //replace localization with bad file
+            File.Copy(
+                Path.Combine(invalidTestTemplateLocation, ".template.config", "localize", "templatestrings.de-DE.json"),
+                Path.Combine(tmpTemplateLocation, ".template.config", "localize", "templatestrings.de-DE.json"),
+                overwrite: true);
+
+            new DotnetNewCommand(_log, "TestAssets.TemplateWithLocalization")
+                .WithCustomHive(home)
+                .WithWorkingDirectory(workingDir)
+                .WithEnvironmentVariable("DOTNET_CLI_UI_LANGUAGE", "de-DE")
+                .Execute()
+                .Should()
+                .ExitWith(0)
+                .And.HaveStdOutContaining("Warning: Failed to read parse localization file").And.HaveStdOutContaining("localize/templatestrings.de-DE.json")
+                .And.HaveStdOutContaining("Die Vorlage \"name\" wurde erfolgreich erstellt.").And.NotHaveStdOutContaining("name_de-DE:äÄßöÖüÜ");
+        }
+
+        [Fact]
+        public void SkipsLocalizationOnInstantiate_WhenLocalizationValidationFails()
+        {
+            var home = TestUtils.CreateTemporaryFolder("Home");
+            var workingDir = TestUtils.CreateTemporaryFolder();
+            var validTestTemplateLocation = TestUtils.GetTestTemplateLocation("TemplateWithLocalization");
+            var invalidTestTemplateLocation = TestUtils.GetTestTemplateLocation("Invalid/Localization/ValidationFailure");
+            var tmpTemplateLocation = TestUtils.CreateTemporaryFolder();
+            DirectoryCopy(validTestTemplateLocation, tmpTemplateLocation, copySubDirs: true);
+
+            var expectedErrors =
+@$"Warning: Localization file {tmpTemplateLocation + Path.DirectorySeparatorChar}.template.config/localize/templatestrings.de-DE.json is not compatible with base configuration {tmpTemplateLocation + Path.DirectorySeparatorChar}.template.config/template.json, and will be skipped.
+  In localization file under the post action with id 'pa1', there are localized strings for manual instruction(s) with ids 'do-not-exist'. These manual instructions do not exist in the template.json file and should be removed from localization file.";
+
+            new DotnetNewCommand(_log, "-i", tmpTemplateLocation)
+                .WithCustomHive(home)
+                .WithWorkingDirectory(workingDir)
+                .WithEnvironmentVariable("DOTNET_CLI_UI_LANGUAGE", "de-DE")
+                .Execute()
+                .Should()
+                .ExitWith(0)
+                .And
+                .NotHaveStdErr()
+                .And.NotHaveStdOutContaining(expectedErrors)
+                .And.HaveStdOutContaining($"Success: {tmpTemplateLocation} installed the following templates:").And.HaveStdOutContaining("TemplateWithLocalization").And.HaveStdOutContaining("name_de-DE:äÄßöÖüÜ");
+
+            //replace localization with bad file
+            File.Copy(
+                Path.Combine(invalidTestTemplateLocation, ".template.config", "localize", "templatestrings.de-DE.json"),
+                Path.Combine(tmpTemplateLocation, ".template.config", "localize", "templatestrings.de-DE.json"),
+                overwrite: true);
+
+            new DotnetNewCommand(_log, "TestAssets.TemplateWithLocalization")
+                .WithCustomHive(home)
+                .WithWorkingDirectory(workingDir)
+                .WithEnvironmentVariable("DOTNET_CLI_UI_LANGUAGE", "de-DE")
+                .Execute()
+                .Should()
+                .ExitWith(0)
+                .And.HaveStdOutContaining(expectedErrors)
+                .And.HaveStdOutContaining("Die Vorlage \"name\" wurde erfolgreich erstellt.").And.NotHaveStdOutContaining("name_de-DE:äÄßöÖüÜ");
+        }
+
+        private static void DirectoryCopy(string sourceDirName, string destDirName, bool copySubDirs)
+        {
+            // Get the subdirectories for the specified directory.
+            DirectoryInfo dir = new DirectoryInfo(sourceDirName);
+
+            if (!dir.Exists)
+            {
+                throw new DirectoryNotFoundException(
+                    "Source directory does not exist or could not be found: "
+                    + sourceDirName);
+            }
+
+            DirectoryInfo[] dirs = dir.GetDirectories();
+
+            // If the destination directory doesn't exist, create it.       
+            Directory.CreateDirectory(destDirName);
+
+            // Get the files in the directory and copy them to the new location.
+            FileInfo[] files = dir.GetFiles();
+            foreach (FileInfo file in files)
+            {
+                string tempPath = Path.Combine(destDirName, file.Name);
+                file.CopyTo(tempPath, false);
+            }
+
+            // If copying subdirectories, copy them and their contents to new location.
+            if (copySubDirs)
+            {
+                foreach (DirectoryInfo subdir in dirs)
+                {
+                    string tempPath = Path.Combine(destDirName, subdir.Name);
+                    DirectoryCopy(subdir.FullName, tempPath, copySubDirs);
+                }
+            }
+        }
     }
 }
