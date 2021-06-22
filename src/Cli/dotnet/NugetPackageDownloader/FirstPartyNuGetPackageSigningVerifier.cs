66bbe5ac8ff3e2b7c2a79ef5eb518bae14380d3a
William Li <wul@microsoft.com>
2021-06-22 14:32:33 -0700

    Update verify logic

--- src/Cli/dotnet/NugetPackageDownloader/FirstPartyNuGetPackageSigningVerifier.cs
+++ src/Cli/dotnet/NugetPackageDownloader/FirstPartyNuGetPackageSigningVerifier.cs
@@ -3,19 +3,27 @@
 
 using System;
 using System.IO;
+using System.Linq;
 using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
 using System.Threading;
 using Microsoft.Extensions.EnvironmentAbstractions;
 using NuGet.Common;
 using NuGet.Packaging;
+using NuGet.Packaging.Signing;
 
 namespace Microsoft.DotNet.Cli.NuGetPackageDownloader
 {
     internal class FirstPartyNuGetPackageSigningVerifier : IFirstPartyNuGetPackageSigningVerifier
     {
+        private const string FirstPartyCertificateThumbprint = "F404000FB11E61F446529981C7059A76C061631E";
+        private const string UpperFirstPartyCertificateThumbprint = "92C1588E85AF2201CE7915E8538B492F605B80C6";
+        private const string FirstPartyCertificateSubject =
+            "CN=Microsoft Corporation, O=Microsoft Corporation, L=Redmond, S=Washington, C=US";
+
         private DirectoryPath _tempDirectory;
         private ILogger _logger;
+
         public FirstPartyNuGetPackageSigningVerifier(DirectoryPath? tempDirectory = null, ILogger logger = null)
         {
             _tempDirectory = tempDirectory ?? new DirectoryPath(Path.GetTempPath());
@@ -35,8 +43,29 @@ namespace Microsoft.DotNet.Cli.NuGetPackageDownloader
             try
             {
                 packageReader.ExtractFile(".signature.p7s", targetFilePath.Value, _logger);
-                var x509Certificate = X509Certificate2.CreateFromCertFile(targetFilePath.Value);
-                return x509Certificate.Subject.Contains("O=Microsoft Corporation", StringComparison.OrdinalIgnoreCase);
+                using var fs = new FileStream(targetFilePath.Value, FileMode.Open);
+                PrimarySignature primarySignature = PrimarySignature.Load(fs);
+                IX509CertificateChain certificateChain = SignatureUtility.GetCertificateChain(primarySignature);
+
+                if (certificateChain.Count < 2)
+                {
+                    return false;
+                }
+
+                X509Certificate2 firstCert = certificateChain.First();
+                if (firstCert.Thumbprint.Equals(FirstPartyCertificateThumbprint, StringComparison.OrdinalIgnoreCase))
+                {
+                    return true;
+                }
+
+                if (firstCert.Subject.Equals(FirstPartyCertificateSubject, StringComparison.OrdinalIgnoreCase)
+                    && certificateChain[1].Thumbprint.Equals(UpperFirstPartyCertificateThumbprint,
+                        StringComparison.OrdinalIgnoreCase))
+                {
+                    return true;
+                }
+
+                return false;
             }
             catch (FileNotFoundException)
             {
