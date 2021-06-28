895b6f1f1ba17f12ceb10b90d00fcfc2de22fbbf
Ladi Prosek <laprosek@microsoft.com>
2021-06-28 23:36:22 +0200

    Fix unit test on Mac, Linux (attempt #2)

--- src/Build.UnitTests/Globbing/MSBuildGlob_Tests.cs
+++ src/Build.UnitTests/Globbing/MSBuildGlob_Tests.cs
@@ -337,7 +337,7 @@ namespace Microsoft.Build.Engine.UnitTests.Globbing
             @"a/b/\c",
             @"d/e\/*b*/\*.cs",
             @"d\e\\abc/\a.cs",
-            @"d\e\", @"abc\", @"a.cs")]
+            @"d\e\\", @"abc\\", @"a.cs")]
         public void GlobMatchingIgnoresSlashOrientationAndRepetitions(string globRoot, string fileSpec, string stringToMatch,
             string fixedDirectoryPart, string wildcardDirectoryPart, string filenamePart)
         {
@@ -350,10 +350,11 @@ namespace Microsoft.Build.Engine.UnitTests.Globbing
 
             string NormalizeSlashes(string path)
             {
-                return path.Replace(Path.DirectorySeparatorChar == '/' ? '\\' : '/', Path.DirectorySeparatorChar);
+                string normalized = path.Replace(Path.DirectorySeparatorChar == '/' ? '\\' : '/', Path.DirectorySeparatorChar);
+                return NativeMethodsShared.IsWindows ? normalized.Replace("\\\\", "\\") : normalized;
             }
 
-            Assert.Equal(NormalizeSlashes(Path.GetFullPath(Path.Combine(globRoot, fixedDirectoryPart))), result.FixedDirectoryPartMatchGroup);
+            Assert.Equal(NormalizeSlashes(Path.Combine(FileUtilities.NormalizePath(globRoot), fixedDirectoryPart)), result.FixedDirectoryPartMatchGroup);
             Assert.Equal(NormalizeSlashes(wildcardDirectoryPart), result.WildcardDirectoryPartMatchGroup);
             Assert.Equal(NormalizeSlashes(filenamePart), result.FilenamePartMatchGroup);
         }
