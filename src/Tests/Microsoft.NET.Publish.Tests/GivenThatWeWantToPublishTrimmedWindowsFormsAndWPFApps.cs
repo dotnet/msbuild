e2687a3e6e5259d2167173977e64f1e9ab91f51b
Lakshan Fernando <lakshanf@hotmail.com>
2021-06-30 04:02:51 -0700

    Show trimmer warnings for winfowm and wpf (#18617)

--- src/Tests/Microsoft.NET.Publish.Tests/GivenThatWeWantToPublishTrimmedWindowsFormsAndWPFApps.cs
+++ src/Tests/Microsoft.NET.Publish.Tests/GivenThatWeWantToPublishTrimmedWindowsFormsAndWPFApps.cs
@@ -21,21 +21,70 @@ namespace Microsoft.NET.Publish.Tests
         }
 
         [WindowsOnlyFact]
+        public void It_builds_windows_Forms_app_with_warning()
+        {
+            var targetFramework = "net6.0-windows";
+            TestProject testProject = new TestProject()
+            {
+                Name = "WinformsBuildWarnPresentPassTest",
+                TargetFrameworks = targetFramework,
+                IsWinExe=true
+            };
+            testProject.AdditionalProperties["UseWindowsForms"] = "true";
+            testProject.AdditionalProperties["PublishTrimmed"] = "true";
+            var testAsset = _testAssetsManager.CreateTestProject(testProject);
+
+            var buildCommand = new BuildCommand(testAsset);
+            buildCommand.Execute()
+                .Should()
+                .Pass()
+                .And
+                .HaveStdOutContaining("NETSDK1175");
+        }
+
+        [WindowsOnlyFact]
+        public void It_builds_windows_Forms_app_with_warning_suppressed()
+        {
+            var targetFramework = "net6.0-windows";
+            TestProject testProject = new TestProject()
+            {
+                Name = "WinformsBuildWarnSuppressPassTest",
+                TargetFrameworks = targetFramework,
+                IsWinExe = true
+            };
+            testProject.AdditionalProperties["UseWindowsForms"] = "true";
+            testProject.AdditionalProperties["PublishTrimmed"] = "true";
+            testProject.AdditionalProperties["NoWarn"] = "NETSDK1175";
+            var testAsset = _testAssetsManager.CreateTestProject(testProject);
+
+            var buildCommand = new BuildCommand(testAsset);
+            buildCommand.Execute()
+                .Should()
+                .Pass()
+                .And
+                //cannot check for absence of NETSDK1175 since that is used with /nowarn: in some configurations
+                .NotHaveStdOutContaining(Strings.@TrimmingWindowsFormsIsNotSupported);
+        }
+
+
+        [WindowsOnlyFact]
         public void It_publishes_windows_Forms_app_with_warning()
         {
             var targetFramework = "net6.0-windows";
             TestProject testProject = new TestProject()
             {
                 Name = "WinformsWarnPresentPassTest",
-                TargetFrameworks = targetFramework
+                TargetFrameworks = targetFramework,
+                IsWinExe = true
             };
             testProject.AdditionalProperties["UseWindowsForms"] = "true";
             testProject.AdditionalProperties["SelfContained"] = "true";
             testProject.AdditionalProperties["RuntimeIdentifier"] = "win-x64";
+            testProject.AdditionalProperties["PublishTrimmed"] = "true";
             var testAsset = _testAssetsManager.CreateTestProject(testProject);
-            
+
             var publishCommand = new PublishCommand(testAsset);
-            publishCommand.Execute($"/p:PublishTrimmed=true")
+            publishCommand.Execute()
                 .Should()
                 .Pass()
                 .And
@@ -48,19 +97,20 @@ namespace Microsoft.NET.Publish.Tests
             var targetFramework = "net6.0-windows";
             TestProject testProject = new TestProject()
             {
-                Name = "WinformsPassTest",
-                TargetFrameworks = targetFramework
+                Name = "WinformsWarnSuppressedPassTest",
+                TargetFrameworks = targetFramework,
+                IsWinExe = true
             };
-            testProject.IsWinExe = true;
             testProject.AdditionalProperties["UseWindowsForms"] = "true";
             testProject.AdditionalProperties["SelfContained"] = "true";
             testProject.AdditionalProperties["RuntimeIdentifier"] = "win-x64";
+            testProject.AdditionalProperties["PublishTrimmed"] = "true";
             testProject.AdditionalProperties["NoWarn"] = "NETSDK1175";
             testProject.AdditionalProperties["SuppressTrimAnalysisWarnings"] = "false";
             var testAsset = _testAssetsManager.CreateTestProject(testProject);
 
             var publishCommand = new PublishCommand(testAsset);
-            publishCommand.Execute($"/p:PublishTrimmed=true")
+            publishCommand.Execute()
                 .Should()
                 .Pass()
                 .And
@@ -69,22 +119,70 @@ namespace Microsoft.NET.Publish.Tests
         }
 
         [WindowsOnlyFact]
+        public void It_builds_wpf_app_with_warning()
+        {
+            var targetFramework = "net6.0-windows";
+            TestProject testProject = new TestProject()
+            {
+                Name = "WpfWarnPresentPassTest",
+                TargetFrameworks = targetFramework,
+                IsWinExe = true
+            };
+            testProject.AdditionalProperties["UseWPF"] = "true";
+            testProject.AdditionalProperties["PublishTrimmed"] = "true";
+            var testAsset = _testAssetsManager.CreateTestProject(testProject);
+
+            var buildCommand = new BuildCommand(testAsset);
+            buildCommand.Execute()
+                .Should()
+                .Pass()
+                .And
+                .HaveStdOutContaining("NETSDK1168");
+        }
+
+        [WindowsOnlyFact]
+        public void It_builds_wpf_app_with_warning_Suppressed()
+        {
+            var targetFramework = "net6.0-windows";
+            TestProject testProject = new TestProject()
+            {
+                Name = "WpfWarnSuppressedPassTest",
+                TargetFrameworks = targetFramework,
+                IsWinExe = true
+            };
+            testProject.AdditionalProperties["UseWPF"] = "true";
+            testProject.AdditionalProperties["PublishTrimmed"] = "true";
+            testProject.AdditionalProperties["NoWarn"] = "NETSDK1168";
+            var testAsset = _testAssetsManager.CreateTestProject(testProject);
+
+            var buildCommand = new BuildCommand(testAsset);
+            buildCommand.Execute()
+                .Should()
+                .Pass()
+                .And
+                //cannot check for absence of NETSDK1168 since that is used with /nowarn: in some configurations
+                .NotHaveStdOutContaining(Strings.@TrimmingWpfIsNotSupported);
+        }
+
+
+        [WindowsOnlyFact]
         public void It_publishes_wpf_app_with_warning()
         {
             var targetFramework = "net6.0-windows";
             TestProject testProject = new TestProject()
             {
                 Name = "WpfWarnPresentPassTest",
-                TargetFrameworks = targetFramework
+                TargetFrameworks = targetFramework,
+                IsWinExe = true
             };
-            testProject.IsWinExe = true;
             testProject.AdditionalProperties["UseWPF"] = "true";
             testProject.AdditionalProperties["SelfContained"] = "true";
             testProject.AdditionalProperties["RuntimeIdentifier"] = "win-x64";
+            testProject.AdditionalProperties["PublishTrimmed"] = "true";
             var testAsset = _testAssetsManager.CreateTestProject(testProject);
 
             var publishCommand = new PublishCommand(testAsset);
-            publishCommand.Execute($"/p:PublishTrimmed=true")
+            publishCommand.Execute()
                 .Should()
                 .Pass()
                 .And
@@ -98,18 +196,19 @@ namespace Microsoft.NET.Publish.Tests
             TestProject testProject = new TestProject()
             {
                 Name = "WpfPassTest",
-                TargetFrameworks = targetFramework
+                TargetFrameworks = targetFramework,
+                IsWinExe = true
             };
-            testProject.IsWinExe = true;
             testProject.AdditionalProperties["UseWPF"] = "true";
             testProject.AdditionalProperties["SelfContained"] = "true";
             testProject.AdditionalProperties["RuntimeIdentifier"] = "win-x64";
             testProject.AdditionalProperties["NoWarn"] = "NETSDK1168";
-            testProject.AdditionalProperties["SuppressTrimAnalysisWarnings"] = "false";            
+            testProject.AdditionalProperties["SuppressTrimAnalysisWarnings"] = "false";
+            testProject.AdditionalProperties["PublishTrimmed"] = "true";
             var testAsset = _testAssetsManager.CreateTestProject(testProject);
 
             var publishCommand = new PublishCommand(testAsset);
-            publishCommand.Execute($"/p:PublishTrimmed=true")
+            publishCommand.Execute()
                 .Should()
                 .Pass()
                 .And
