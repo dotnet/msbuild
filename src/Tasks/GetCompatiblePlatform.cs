5553b7cf7b18e8cd460994e70dd338d183659a75
Ben Villalobos <4691428+BenVillalobos@users.noreply.github.com>
2021-07-06 10:54:25 -0700

    Make GetCompatiblePlatform its own task. Renamed from GetNearestPlatformTask

---
+++ src/Tasks/GetCompatiblePlatform.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Microsoft.Build.Tasks
+{
+    class GetCompatiblePlatform : TaskExtension
+    {
+        public ITaskItem[] AnnotatedProjects { get; set; }
+
+        public string ParentProjectPlatform { get; set; }
+
+        public string PlatformLookupTable { get; set; }
+
+        [Output]
+        public ITaskItem[] AssignedProjectsWithPlatform { get; set; }
+
+        public override bool Execute()
+        {
+            Dictionary<string, string> translationTable = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
+            if (!string.IsNullOrEmpty(PlatformLookupTable))
+            {
+                foreach (string s in PlatformLookupTable.Split(';'))
+                {
+                    // Minimum translation: a=b
+                    if (s.Length < 3)
+                    {
+                        continue;
+                    }
+                    string key = s.Split('=')[0];
+                    string val = s.Split('=')[1];
+                    translationTable[key] = val;
+                }
+                Log.LogMessage($"Translation Table: {translationTable.Aggregate(new StringBuilder(), (sb, kvp) => sb.Append(kvp.Key + "=" + kvp.Value + ";"), sb => sb.ToString())}");
+            }
+
+            AssignedProjectsWithPlatform = new ITaskItem[AnnotatedProjects.Length];
+            for (int i = 0; i < AnnotatedProjects.Length; i++)
+            {
+                AssignedProjectsWithPlatform[i] = new TaskItem(AnnotatedProjects[i]);
+
+                HashSet<string> childPlatforms = new HashSet<string>();
+                foreach (string s in AssignedProjectsWithPlatform[i].GetMetadata("PlatformOptions").Split(';'))
+                {
+                    if (!string.IsNullOrEmpty(s))
+                    {
+                        childPlatforms.Add(s);
+                    }
+                }
+
+                string buildChildProjectAs = "";
+
+                // Translation table takes priority
+                if (translationTable.ContainsKey(ParentProjectPlatform) &&
+                          childPlatforms.Contains(translationTable[ParentProjectPlatform]))
+                {
+                    buildChildProjectAs = translationTable[ParentProjectPlatform];
+                    Log.LogMessage($"Found {ParentProjectPlatform}={buildChildProjectAs} in the given translation table: {translationTable.ToString()}.");
+                }
+                // AnyCPU if possible
+                else if (childPlatforms.Contains("AnyCPU"))
+                {
+                    buildChildProjectAs = "AnyCPU";
+                    Log.LogMessage($"Defaulting to AnyCPU.");
+                }
+                // Try to prefer matching platforms
+                else if (childPlatforms.Contains(ParentProjectPlatform))
+                {
+                    buildChildProjectAs = ParentProjectPlatform;
+                    Log.LogMessage($"Child and parent have the same platform.");
+                }
+                else
+                {
+                    // Keep it empty, log a warning. Common.CurrentVersion.targets will undefine 
+                    // Platform/PlatformTarget when this is the case.
+                    Log.LogWarning("Could not determine what the child project should be built as.\nChild project will be built normally.");
+                }
+
+                AssignedProjectsWithPlatform[i].SetMetadata("NearestPlatform", buildChildProjectAs);
+                Log.LogMessage($"Project {AssignedProjectsWithPlatform[i].GetMetadata("Identity")} would be built as: {buildChildProjectAs}");
+            }
+
+            return true;
+        }
+    }
+}
