60d33988fbc5f05cb240ea3514264dfb12c9b48f
Don Syme <dsyme@users.noreply.github.com>
2021-07-24 18:09:54 +0100

    Update Program.fs

--- template_feed/Microsoft.DotNet.Common.ProjectTemplates.6.0/content/ConsoleApplication-FSharp/Program.fs
+++ template_feed/Microsoft.DotNet.Common.ProjectTemplates.6.0/content/ConsoleApplication-FSharp/Program.fs
@@ -1,13 +1,4 @@
-﻿// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
+﻿
 
-open System
-
-// Define a function to construct a message to print
-let from whom =
-    sprintf "from %s" whom
-
-[<EntryPoint>]
-let main argv =
-    let message = from "F#" // Call the function
-    printfn "Hello world %s" message
-    0 // return an integer exit code
+// For more information see https://aka.ms/fsharp-console-apps
+printfn "Hello from F#"
