6a38bb8a08e452b85ce6fec930c94748d2274b20
Vlada Shubina <vshubina@microsoft.com>
2021-07-28 10:33:50 +0300

    tests changes

--- test/dotnet-new3.UnitTests/DotnetNewDebugOptions.cs
+++ test/dotnet-new3.UnitTests/DotnetNewDebugOptions.cs
@@ -13,25 +13,6 @@ namespace Dotnet_new3.IntegrationTests
 {
     public class DotnetNewDebugOptions
     {
-        private const string DotnetNewOutput =
-@"The 'dotnet new3' command creates a .NET project based on a template.
-
-Common templates are:
-Template Name        Short Name  Language    Tags          
--------------------  ----------  ----------  --------------
-Class Library        classlib    [C#],F#,VB  Common/Library
-Console Application  console     [C#],F#,VB  Common/Console
-
-An example would be:
-   dotnet new3 console
-
-Display template options with:
-   dotnet new3 console -h
-Display all installed templates with:
-   dotnet new3 --list
-Display templates available on NuGet.org with:
-   dotnet new3 web --search";
-
         private readonly ITestOutputHelper _log;
 
         public DotnetNewDebugOptions(ITestOutputHelper log)
@@ -43,32 +24,48 @@ Display templates available on NuGet.org with:
         public void CanShowBasicInfoWithDebugReinit()
         {
             string home = TestUtils.CreateTemporaryFolder("Home");
-            string workingDirectory = TestUtils.CreateTemporaryFolder();
+            string cacheFilePath = Path.Combine(home, "dotnetcli-preview", "v2.0.0", "templatecache.json");
 
-            new DotnetNewCommand(_log, "--debug:reinit")
+            var commandResult = new DotnetNewCommand(_log)
                 .WithCustomHive(home)
-                .WithWorkingDirectory(workingDirectory)
-                .Execute()
-                .Should()
-                .ExitWith(0)
-                .And.NotHaveStdErr()
-                .And.HaveStdOut(DotnetNewOutput);
+                .Execute();
+
+            commandResult.Should().ExitWith(0).And.NotHaveStdErr();
+            Assert.True(File.Exists(cacheFilePath));
+            DateTime lastUpdateDate = File.GetLastWriteTimeUtc(cacheFilePath);
+
+            var reinitCommandResult = new DotnetNewCommand(_log, "--debug:reinit")
+               .WithCustomHive(home)
+               .Execute();
+
+            reinitCommandResult.Should().ExitWith(0).And.NotHaveStdErr();
+            Assert.Equal(commandResult.StdOut, reinitCommandResult.StdOut);
+            Assert.True(File.Exists(cacheFilePath));
+            Assert.True(lastUpdateDate < File.GetLastWriteTimeUtc(cacheFilePath));
         }
 
         [Fact]
         public void CanShowBasicInfoWithDebugRebuildCache()
         {
             string home = TestUtils.CreateTemporaryFolder("Home");
-            string workingDirectory = TestUtils.CreateTemporaryFolder();
+            string cacheFilePath = Path.Combine(home, "dotnetcli-preview", "v2.0.0", "templatecache.json");
 
-            new DotnetNewCommand(_log, "--debug:rebuildcache")
+            var commandResult = new DotnetNewCommand(_log)
                 .WithCustomHive(home)
-                .WithWorkingDirectory(workingDirectory)
-                .Execute()
-                .Should()
-                .ExitWith(0)
-                .And.NotHaveStdErr()
-                .And.HaveStdOut(DotnetNewOutput);
+                .Execute();
+
+            commandResult.Should().ExitWith(0).And.NotHaveStdErr();
+            Assert.True(File.Exists(cacheFilePath));
+            DateTime lastUpdateDate = File.GetLastWriteTimeUtc(cacheFilePath);
+
+            var reinitCommandResult = new DotnetNewCommand(_log, "--debug:rebuildcache")
+               .WithCustomHive(home)
+               .Execute();
+
+            reinitCommandResult.Should().ExitWith(0).And.NotHaveStdErr();
+            Assert.Equal(commandResult.StdOut, reinitCommandResult.StdOut);
+            Assert.True(File.Exists(cacheFilePath));
+            Assert.True(lastUpdateDate < File.GetLastWriteTimeUtc(cacheFilePath));
         }
     }
 }
