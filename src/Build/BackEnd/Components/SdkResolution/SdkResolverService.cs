a1bd10d5bbaf0e3cf19df723a71995270497579d
Ben Villalobos <4691428+BenVillalobos@users.noreply.github.com>
2021-08-16 18:15:11 -0700

    Promote MSB4242 to error and make a clearer message. ResolveSdk returns early when and logs an error when we fail to resolve an SDK

--- src/Build/BackEnd/Components/SdkResolution/SdkResolverService.cs
+++ src/Build/BackEnd/Components/SdkResolution/SdkResolverService.cs
@@ -124,14 +124,14 @@ namespace Microsoft.Build.BackEnd.SdkResolution
                     // than give them a generic error, we want to give a more specific message.  This exception cannot be caught by
                     // the resolver itself because it is usually thrown before the class is loaded
                     // MSB4243: The NuGet-based SDK resolver failed to run because NuGet assemblies could not be located.  Check your installation of MSBuild or set the environment variable "{0}" to the folder that contains the required NuGet assemblies. {1}
-                    loggingContext.LogWarning(null, new BuildEventFileInfo(sdkReferenceLocation), "CouldNotRunNuGetSdkResolver", MSBuildConstants.NuGetAssemblyPathEnvironmentVariableName, e.Message);
-                    continue;
+                    loggingContext.LogError(new BuildEventFileInfo(sdkReferenceLocation), "CouldNotRunNuGetSdkResolver", MSBuildConstants.NuGetAssemblyPathEnvironmentVariableName, e.Message);
+                    return new SdkResult(sdk, null, null);
                 }
                 catch (Exception e)
                 {
-                    // MSB4242: The SDK resolver "{0}" failed to run. {1}
-                    loggingContext.LogWarning(null, new BuildEventFileInfo(sdkReferenceLocation), "CouldNotRunSdkResolver", sdkResolver.Name, e.Message);
-                    continue;
+                    // MSB4242: The SDK resolver "{0}" failed while attempting to resolve the SDK "{1}": {2}
+                    loggingContext.LogError(new BuildEventFileInfo(sdkReferenceLocation), "CouldNotRunSdkResolver", sdkResolver.Name, sdk.ToString(), e.Message);
+                    return new SdkResult(sdk, null, null);
                 }
 
                 SetResolverState(submissionId, sdkResolver, context.State);
