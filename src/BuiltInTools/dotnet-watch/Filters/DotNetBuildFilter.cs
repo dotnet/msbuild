71e12e51c8e7df77ec95db65d8533b802fac9648
William Li <wul@microsoft.com>
2021-08-19 14:03:37 -0700

    Fix namespace

--- src/BuiltInTools/dotnet-watch/Filters/DotNetBuildFilter.cs
+++ src/BuiltInTools/dotnet-watch/Filters/DotNetBuildFilter.cs
@@ -1,6 +1,8 @@
 ﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.DotNet.Watcher.Internal;
 using Microsoft.Extensions.Tools.Internal;
