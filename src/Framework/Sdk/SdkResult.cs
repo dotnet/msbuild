5e0b0ea218d94e9b382f79775a4e7e189c62dddc
Gordon Hogenson <ghogen@microsoft.com>
2021-09-07 14:49:21 -0700

    Doc comments: fix validation issues in docs build (#6744)
    * Fix doc build validation issues
    
    * fix text/markdown
    
    * Fix comment syntax
    
    * Fix comment syntax closing tag
    
    * fix errors
    
    * fix targetFrameworkFallbackSearchPaths paramref
    
    * try CDATA section
    
    * Update src/Build/Definition/Project.cs
    
    * port comment changes from docs for pronouns

--- src/Framework/Sdk/SdkResult.cs
+++ src/Framework/Sdk/SdkResult.cs
@@ -7,11 +7,10 @@ namespace Microsoft.Build.Framework
 {
     /// <summary>
     ///     An abstract interface class to indicate SDK resolver success or failure.
-    ///     <remarks>
-    ///         Note: Use <see cref="SdkResultFactory" /> to create instances of this class. Do not
-    ///         inherit from this class.
-    ///     </remarks>
     /// </summary>
+    /// <remarks>
+    ///   Note: Use <see cref="Microsoft.Build.Framework.SdkResultFactory"/> to create instances of this class. Do not inherit from this class.
+    /// </remarks>
     public abstract class SdkResult
     {
         //  Explicit backing fields so that implementation in Microsoft.Build.dll can use them for translation
