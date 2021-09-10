5636b4531d4903f4c97e9b56c1da4b65a2a332d4
David Karlaš <david.karlas@microsoft.com>
2021-09-10 10:50:06 +0200

    Added unit tests and fixed Position option to be `nullable`

--- src/Cli/dotnet/commands/dotnet-complete/CompleteCommand.cs
+++ src/Cli/dotnet/commands/dotnet-complete/CompleteCommand.cs
@@ -49,9 +49,9 @@ namespace Microsoft.DotNet.Cli
 
         private static string[] Suggestions(ParseResult complete)
         {
-            var input = complete.ValueForArgument<string>(CompleteCommandParser.PathArgument) ?? string.Empty;
+            var input = complete.ValueForArgument(CompleteCommandParser.PathArgument) ?? string.Empty;
 
-            var position = complete.ValueForOption<int>(CompleteCommandParser.PositionOption);
+            var position = complete.ValueForOption(CompleteCommandParser.PositionOption);
 
             if (position > input.Length)
             {
