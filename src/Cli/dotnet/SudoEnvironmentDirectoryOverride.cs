d38e558fa88d1f1069a47ecd4b80da43b628466f
William Li <wul@microsoft.com>
2021-09-10 15:52:28 -0700

    Fix nuget config not being honored
    Copy user level nuget config to override folder

--- src/Cli/dotnet/SudoEnvironmentDirectoryOverride.cs
+++ src/Cli/dotnet/SudoEnvironmentDirectoryOverride.cs
@@ -4,6 +4,10 @@
 using System;
 using System.CommandLine.Parsing;
 using System.IO;
+using System.Linq;
+using Microsoft.DotNet.Cli.Utils;
+using NuGet.Common;
+using NuGet.Configuration;
 
 namespace Microsoft.DotNet.Cli
 {
@@ -33,7 +37,57 @@ namespace Microsoft.DotNet.Cli
             if (!OperatingSystem.IsWindows() && IsRunningUnderSudo() && IsRunningWorkloadCommand(parseResult))
             {
                 Directory.CreateDirectory(SudoHomeDirectory);
+
+                var homeBeforeOverride = Path.Combine(Environment.GetEnvironmentVariable("HOME"));
                 Environment.SetEnvironmentVariable("HOME", SudoHomeDirectory);
+
+                CopyUserNuGetConfigToOverriddenHome(homeBeforeOverride);
+            }
+        }
+
+        /// <summary>
+        /// To make NuGet honor the user's NuGet config file.
+        /// Copying instead of using the file directoy to avoid existing file being set higher permission
+        /// Try to delete the existing NuGet config file in "/tmp/dotnet_sudo_home/"
+        /// to avoid different user's NuGet config getting mixed.
+        /// </summary>
+        private static void CopyUserNuGetConfigToOverriddenHome(string homeBeforeOverride)
+        {
+            // https://github.com/NuGet/NuGet.Client/blob/dev/src/NuGet.Core/NuGet.Common/PathUtil/NuGetEnvironment.cs#L139
+            // home is cache in NuGet we cannot directly use the call
+            var userSettingsDir = Path.Combine(homeBeforeOverride, ".nuget", "NuGet");
+
+            string userNuGetConfig = Settings.OrderedSettingsFileNames
+                .Select(fileName => Path.Combine(userSettingsDir, fileName))
+                .FirstOrDefault(f => File.Exists(f));
+
+            var overridenSettingsDir = NuGetEnvironment.GetFolderPath(NuGetFolderPath.UserSettingsDirectory);
+            var overridenNugetConfig = Path.Combine(overridenSettingsDir, Settings.DefaultSettingsFileName);
+
+            if (File.Exists(overridenNugetConfig))
+            {
+                try
+                {
+                    FileAccessRetrier.RetryOnIOException(
+                        () => File.Delete(overridenNugetConfig));
+                }
+                catch
+                {
+                    // best effort to remove
+                }
+            }
+
+            if (userNuGetConfig != default)
+            {
+                try
+                {
+                    FileAccessRetrier.RetryOnIOException(
+                        () => File.Copy(userNuGetConfig, overridenNugetConfig, overwrite: true));
+                }
+                catch
+                {
+                    // best effort to copy
+                }
             }
         }
 
