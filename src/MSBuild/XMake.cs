5d6c1e8346ec5f342b72f455a5feb06391c0de3e
Ladi Prosek <laprosek@microsoft.com>
2021-09-22 14:11:04 +0200

    [Debug] Check which OS is used by the pipeline

--- src/MSBuild/XMake.cs
+++ src/MSBuild/XMake.cs
@@ -1550,7 +1550,8 @@ namespace Microsoft.Build.CommandLine
             {
                 // If we're running on any of the unsupported OS's, fail immediately.  This way,
                 // we don't run into some obscure error down the line, totally confusing the user.
-                InitializationException.VerifyThrow(false, "UnsupportedOS");
+                InitializationException.Throw($"Platform {Environment.OSVersion.Platform}, version {Environment.OSVersion.Version}", "");
+                //InitializationException.VerifyThrow(false, "UnsupportedOS");
             }
 #endif
         }
