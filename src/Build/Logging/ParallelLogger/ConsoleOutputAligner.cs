b8fd3caec36408e53bb020a6aeccee827b2d741f
Roman Konecny <rokonecn@microsoft.com>
2021-11-12 10:35:05 +0100

    comment

--- src/Build/Logging/ParallelLogger/ConsoleOutputAligner.cs
+++ src/Build/Logging/ParallelLogger/ConsoleOutputAligner.cs
@@ -54,6 +54,7 @@ namespace Microsoft.Build.BackEnd.Logging
 
             StringBuilder sb = _reusedStringBuilder;
             Debug.Assert(sb.Length == 0, "Something else is using _reusedStringBuilder. Please make sure this instance method is not called concurrently.");
+            // prepare reused StringBuilder instance for new use.
             sb.Length = 0;
             // The string contains new lines, treat each new line as a different string to format and send to the console
             while (j >= 0)
