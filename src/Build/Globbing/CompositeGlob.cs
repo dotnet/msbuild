79c45ab5f24ca8208eaf03ba1369a55d2e23d5b8
Drew Noakes <git@drewnoakes.com>
2021-11-16 09:25:26 +1100

    Eliminate allocation in CompositeGlob.IsMatch
    ImmutableArray<> has a non-allocating enumerator which is used within ImmutableArrayExtensions.Any. By changing the declared type from IEnumerable<> to ImmutableArray<> the non-allocating extension method is used.

--- src/Build/Globbing/CompositeGlob.cs
+++ src/Build/Globbing/CompositeGlob.cs
@@ -13,27 +13,20 @@ namespace Microsoft.Build.Globbing
     /// </summary>
     public class CompositeGlob : IMSBuildGlob
     {
+        private readonly ImmutableArray<IMSBuildGlob> _globs;
+
         /// <summary>
         /// The direct children of this composite
         /// </summary>
-        public IEnumerable<IMSBuildGlob> Globs { get; }
+        public IEnumerable<IMSBuildGlob> Globs => _globs;
 
         /// <summary>
         ///     Constructor
         /// </summary>
         /// <param name="globs">Children globs. Input gets shallow cloned</param>
         public CompositeGlob(IEnumerable<IMSBuildGlob> globs)
-        {
-            // ImmutableArray also does this check, but copied it here just in case they remove it
-            if (globs is ImmutableArray<IMSBuildGlob> immutableGlobs)
-            {
-                Globs = immutableGlobs;
-            }
-            else
-            {
-                Globs = globs.ToImmutableArray();
-            }
-        }
+            : this(globs is ImmutableArray<IMSBuildGlob> immutableGlobs ? immutableGlobs : globs.ToImmutableArray())
+        {}
 
         /// <summary>
         ///     Constructor
@@ -42,13 +35,22 @@ namespace Microsoft.Build.Globbing
         public CompositeGlob(params IMSBuildGlob[] globs) : this(globs.ToImmutableArray())
         {}
 
+        /// <summary>
+        ///     Constructor
+        /// </summary>
+        /// <param name="globs">Children globs.</param>
+        private CompositeGlob(ImmutableArray<IMSBuildGlob> globs)
+        {
+            _globs = globs;
+        }
+
         /// <inheritdoc />
         public bool IsMatch(string stringToMatch)
         {
             // Threadpools are a scarce resource in Visual Studio, do not use them.
             //return Globs.AsParallel().Any(g => g.IsMatch(stringToMatch));
 
-            return Globs.Any(g => g.IsMatch(stringToMatch));
+            return _globs.Any(g => g.IsMatch(stringToMatch));
         }
     }
 }
