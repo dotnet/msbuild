99f27c72d99dd0c086be22e9a6a4ddcccb0c492f
Vlada Shubina <vshubina@microsoft.com>
2021-11-24 10:22:49 +0100

    dotnet new3 complete tests

---
+++ test/dotnet-new3.UnitTests/DotnetNew3CompleteTests.cs
@@ -0,0 +1,58 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+#nullable enable
+
+using ApprovalTests;
+using FluentAssertions;
+using Microsoft.NET.TestFramework.Assertions;
+using Microsoft.TemplateEngine.TestHelper;
+using NuGet.Versioning;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Dotnet_new3.IntegrationTests
+{
+    public class DotnetNew3CompleteTests
+    {
+        private readonly ITestOutputHelper _log;
+
+        public DotnetNew3CompleteTests(ITestOutputHelper log)
+        {
+            _log = log;
+        }
+
+        [Fact]
+        public void CanDoTabCompletion()
+        {
+            string homeDir = TestUtils.CreateTemporaryFolder();
+            var commandResult = new DotnetNewCommand(_log, "complete", $"new3 --debug:custom-hive {homeDir} ")
+                .WithoutCustomHive()
+                .Execute();
+
+            commandResult
+                .Should()
+                .ExitWith(0)
+                .And.NotHaveStdErr();
+
+            Approvals.Verify(commandResult.StdOut);
+        }
+
+#pragma warning disable xUnit1004 // Test methods should not be skipped
+        [Fact(Skip = "not working")]
+#pragma warning restore xUnit1004 // Test methods should not be skipped
+        public void CanDoTabCompletionAtGivenPosition()
+        {
+            string homeDir = TestUtils.CreateTemporaryFolder();
+            var commandResult = new DotnetNewCommand(_log, "complete", $"new3 co --debug:custom-hive {homeDir} --language C#", "--position", "7")
+                .WithoutCustomHive()
+                .Execute();
+
+            commandResult
+                .Should()
+                .ExitWith(0)
+                .And.NotHaveStdErr()
+                .And.HaveStdOut("console");
+        }
+    }
+}
