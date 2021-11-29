8470a1f036ce99b21d51f4f53c171363084ed5e3
Roman Konecny <rokonecn@microsoft.com>
2021-11-29 15:23:34 +0100

    Disable StringBuilderCache assert ballance

--- src/Framework/StringBuilderCache.cs
+++ src/Framework/StringBuilderCache.cs
@@ -31,6 +31,8 @@
 **
 ===========================================================*/
 
+//#define ASSERT_BALANCE
+
 using System;
 using System.Diagnostics;
 using System.Text;
@@ -80,9 +82,11 @@ namespace Microsoft.Build.Framework
         {
             if (sb.Capacity <= MAX_BUILDER_SIZE)
             {
+#if ASSERT_BALANCE
                 // Assert we are not replacing another string builder. That could happen when Acquire is reentered.
                 // User of StringBuilderCache has to make sure that calling method call stacks do not also use StringBuilderCache.
                 Debug.Assert(StringBuilderCache.t_cachedInstance == null, "Unexpected replacing of other StringBuilder.");
+#endif
 
                 StringBuilderCache.t_cachedInstance = sb;
             }
