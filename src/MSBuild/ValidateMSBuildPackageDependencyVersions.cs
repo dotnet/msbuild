f2c30e59d0b15c6c567fa7715ba2fdb240042dc0
Forgind <Forgind@users.noreply.github.com>
2021-12-16 12:24:38 -0800

    Somewhat cleaner version

--- src/MSBuild/ValidateMSBuildPackageDependencyVersions.cs
+++ src/MSBuild/ValidateMSBuildPackageDependencyVersions.cs
@@ -2,6 +2,7 @@ using Microsoft.Build.Framework;
 using Microsoft.Build.Utilities;
 using System;
 using System.IO;
+using System.Linq;
 using System.Reflection;
 using System.Xml;
 namespace MSBuild
@@ -13,68 +14,50 @@ namespace MSBuild
         [Required]
         public string AssemblyPath { get; set; }
 
+        private string[] assembliesToIgnore = { "Microsoft.Build.Conversion.Core", "Microsoft.NET.StringTools.net35", "Microsoft.Build.Engine", "Microsoft.Activities.Build", "XamlBuildTask" };
+
         public override bool Execute()
         {
             XmlDocument doc = new XmlDocument();
             doc.Load(AppConfig);
-            foreach (var topLevelElement in doc.ChildNodes)
+            var runtime = doc.SelectSingleNode("configuration").SelectSingleNode("runtime");
+            foreach (var node in runtime.ChildNodes)
             {
-                if (topLevelElement is XmlElement topLevelXmlElement && topLevelXmlElement.Name.Equals("configuration", StringComparison.OrdinalIgnoreCase))
+                if (node is XmlElement assemblyBinding && assemblyBinding.Name.Equals("assemblyBinding"))
                 {
-                    foreach (var configurationElement in topLevelXmlElement.ChildNodes)
+                    foreach (var assemblyBindingElement in assemblyBinding.ChildNodes)
                     {
-                        if (configurationElement is XmlElement configurationXmlElement && configurationXmlElement.Name.Equals("runtime", StringComparison.OrdinalIgnoreCase))
+                        string name = string.Empty;
+                        string version = string.Empty;
+                        if (assemblyBindingElement is not XmlElement dependentAssemblyElement)
                         {
-                            foreach (var runtimeElement in configurationXmlElement.ChildNodes)
+                            continue;
+                        }
+                        foreach (var dependentAssembly in dependentAssemblyElement.ChildNodes)
+                        {
+                            if (dependentAssembly is XmlElement dependentAssemblyXmlElement)
                             {
-                                if (runtimeElement is XmlElement runtimeXmlElement && runtimeXmlElement.Name.Equals("assemblyBinding", StringComparison.OrdinalIgnoreCase))
+                                if (dependentAssemblyXmlElement.Name.Equals("assemblyIdentity", StringComparison.OrdinalIgnoreCase))
                                 {
-                                    foreach (var assemblyBindingElement in runtimeXmlElement.ChildNodes)
-                                    {
-                                        if (assemblyBindingElement is XmlElement assemblyBindingXmlElement && assemblyBindingXmlElement.Name.Equals("dependentAssembly", StringComparison.OrdinalIgnoreCase))
-                                        {
-                                            string name = string.Empty;
-                                            string version = string.Empty;
-                                            foreach (var dependentAssembly in assemblyBindingXmlElement.ChildNodes)
-                                            {
-                                                if (dependentAssembly is XmlElement dependentAssemblyXmlElement)
-                                                {
-                                                    if (dependentAssemblyXmlElement.Name.Equals("assemblyIdentity", StringComparison.OrdinalIgnoreCase))
-                                                    {
-                                                        foreach (var assemblyIdentityAttribute in dependentAssemblyXmlElement.Attributes)
-                                                        {
-                                                            if (assemblyIdentityAttribute is XmlAttribute assemblyIdentityAttributeXmlElement && assemblyIdentityAttributeXmlElement.Name.Equals("name", StringComparison.OrdinalIgnoreCase))
-                                                            {
-                                                                name = assemblyIdentityAttributeXmlElement.Value;
-                                                            }
-                                                        }
-                                                    }
-                                                    else if (dependentAssemblyXmlElement.Name.Equals("bindingRedirect", StringComparison.OrdinalIgnoreCase))
-                                                    {
-                                                        foreach (var bindingRedirectAttribute in dependentAssemblyXmlElement.Attributes)
-                                                        {
-                                                            if (bindingRedirectAttribute is XmlAttribute bindingRedirectAttributeXmlElement && bindingRedirectAttributeXmlElement.Name.Equals("newVersion", StringComparison.OrdinalIgnoreCase))
-                                                            {
-                                                                version = bindingRedirectAttributeXmlElement.Value;
-                                                            }
-                                                        }
-                                                    }
-                                                }
-                                            }
-                                            if (!string.IsNullOrEmpty(name) && !string.IsNullOrEmpty(version))
-                                            {
-                                                string path = Path.Combine(AssemblyPath, name + ".dll");
-                                                string assemblyVersion = File.Exists(path) ? Assembly.LoadFile(path).GetName().Version.ToString() : version;
-                                                if (!version.Equals(assemblyVersion))
-                                                {
-                                                    if (!(String.Equals(name, "System.ValueTuple", StringComparison.OrdinalIgnoreCase) && String.Equals(version, "4.0.0.0") && String.Equals(assemblyVersion, "4.0.3.0")))
-                                                    {
-                                                        Log.LogError($"Binding redirect for '{name}' redirects to a different version ({version}) than MSBuild ships ({assemblyVersion}).");
-                                                    }
-                                                }
-                                            }
-                                        }
-                                    }
+                                    name = dependentAssemblyXmlElement.GetAttribute("name");
+                                }
+                                else if (dependentAssemblyXmlElement.Name.Equals("bindingRedirect", StringComparison.OrdinalIgnoreCase))
+                                {
+                                    version = dependentAssemblyXmlElement.GetAttribute("newVersion");
+                                }
+                            }
+                        }
+                        if (!string.IsNullOrEmpty(name) && !string.IsNullOrEmpty(version) && !assembliesToIgnore.Contains(name, StringComparer.OrdinalIgnoreCase))
+                        {
+                            string path = Path.Combine(AssemblyPath, name + ".dll");
+                            string assemblyVersion = AssemblyName.GetAssemblyName(path).Version.ToString();
+                            if (!version.Equals(assemblyVersion))
+                            {
+                                // It is unusual to want to redirect down, but in this case it's ok: 4.0.3.0 forwards to 4.0.0.0 in the GAC, so this just removes the need to redistribute a file
+                                // and makes that resolution faster. Still verify that the versions are exactly as in this comment, as that may change.
+                                if (!(String.Equals(name, "System.ValueTuple", StringComparison.OrdinalIgnoreCase) && String.Equals(version, "4.0.0.0") && String.Equals(assemblyVersion, "4.0.3.0")))
+                                {
+                                    Log.LogError($"Binding redirect for '{name}' redirects to a different version ({version}) than MSBuild ships ({assemblyVersion}).");
                                 }
                             }
                         }
