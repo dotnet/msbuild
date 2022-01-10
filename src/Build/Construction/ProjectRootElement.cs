4d7cd15f4afb35ac5c50b74155c3f90ec6263ae8
Lachlan Ennis <2433737+elachlan@users.noreply.github.com>
2022-01-11 02:10:49 +1000

    SA1125 use shorthand for Nullable types (#7201)

--- src/Build/Construction/ProjectRootElement.cs
+++ src/Build/Construction/ProjectRootElement.cs
@@ -252,7 +252,7 @@ namespace Microsoft.Build.Construction
 
         /// <summary>
         /// Initialize a ProjectRootElement instance from an existing document.
-        /// Helper constructor for the <see cref="ReloadFrom(string,bool,System.Nullable{bool})"/>> mehtod which needs to check if the document parses
+        /// Helper constructor for the <see cref="ReloadFrom(string,bool,bool?)"/>> mehtod which needs to check if the document parses
         /// </summary>
         /// <remarks>
         /// Do not make public: we do not wish to expose particular XML API's.
