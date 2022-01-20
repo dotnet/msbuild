09a64c885a6899b92924f5e6501be9b3705e3dfb
Will Godbe <wigodbe@microsoft.com>
2022-01-20 13:19:07 -0800

    Fix BuildFromSource

--- eng/restore-toolset.sh
+++ eng/restore-toolset.sh
@@ -7,7 +7,7 @@ function InitializeCustomSDKToolset {
 
   # The following frameworks and tools are used only for testing.
   # Do not attempt to install them in source build.
-  if [[ "${DotNetBuildFromSource:-}" == "true" ]]; then
+  if [[ "${ArcadeBuildFromSource:-}" == "true" ]]; then
     return
   fi
 
