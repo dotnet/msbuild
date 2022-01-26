baronfel/sdk

set lambda-based handlers for commands

src/Cli/dotnet/commands/dotnet-tool/list/ToolListCommandParser.cs
@@ -33,7 +33,7 @@ namespace Microsoft.DotNet.Cli
             command.AddOption(LocalOption.WithHelpDescription(command, LocalizableStrings.LocalOptionDescription));
             command.AddOption(ToolPathOption.WithHelpDescription(command, LocalizableStrings.ToolPathOptionDescription));
 
-            CommandHandler.Create<ParseResult>((parseResult) => new ToolRestoreCommand(parseResult).Execute());
+            command.SetHandler((parseResult) => new ToolRestoreCommand(parseResult).Execute());
 
             return command;
         }

dotnet/msbuild

src/Cli/dotnet/commands/dotnet-tool/list/ToolListCommandParser.cs
@@ -34,7 +34,7 @@ namespace Microsoft.DotNet.Cli
             command.AddOption(LocalOption.WithHelpDescription(command, LocalizableStrings.LocalOptionDescription));
             command.AddOption(ToolPathOption.WithHelpDescription(command, LocalizableStrings.ToolPathOptionDescription));
 
-            command.Handler = CommandHandler.Create<ParseResult>((parseResult) => new ToolListCommand(parseResult).Execute());
+            command.SetHandler((parseResult) => new ToolListCommand(parseResult).Execute());
 
             return command;
         }