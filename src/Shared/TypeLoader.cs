41e3475e0dc0eea2f20375f3b4c0a7a0235b4777
Forgind <Forgind@users.noreply.github.com>
2022-01-31 14:54:15 -0800

    Create need and reference package

--- src/Shared/TypeLoader.cs
+++ src/Shared/TypeLoader.cs
@@ -290,6 +290,7 @@ namespace Microsoft.Build.Shared
                 ErrorUtilities.VerifyThrowArgumentNull(typeName, nameof(typeName));
 
                 // Only one thread should be doing operations on this instance of the object at a time.
+                MetadataLoadContext metadataLoadContext = new(new PathAssemblyResolver(new string[] {"abc"}));
 
                 Type type = _typeNameToType.GetOrAdd(typeName, (key) =>
                 {
