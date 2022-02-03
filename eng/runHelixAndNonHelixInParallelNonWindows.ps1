ec7009e92e544b54b26fd7f65f8e9c077d71b11d
Matt Mitchell <mmitche@microsoft.com>
2022-02-03 15:28:28 -0800

    Add additional msbuild parameters to non-windows script

--- eng/runHelixAndNonHelixInParallelNonWindows.ps1
+++ eng/runHelixAndNonHelixInParallelNonWindows.ps1
@@ -4,6 +4,7 @@
       [string] $buildSourcesDirectory,
       [string] $customHelixTargetQueue,
       [string] $officialBuildIdArgs,
+      [string] $additionalMSBuildParameters = "",
       [switch] $test
   )
 
@@ -20,7 +21,7 @@
   "-test",
   "-projects", "$buildSourcesDirectory/src/Tests/UnitTests.proj",
   "/bl:$buildSourcesDirectory/artifacts/log/$configuration/TestInHelix.binlog",
-  "/p:_CustomHelixTargetQueue=$customHelixTargetQueue")
+  "/p:_CustomHelixTargetQueue=$customHelixTargetQueue") + $additionalMSBuildParameters.Split(' ')
 
   $runTests = ("&'$PSScriptRoot/runTestsCannotRunOnHelix.sh' $runTestsCannotRunOnHelixArgs", "&'$PSScriptRoot/common/build.sh' $runTestsOnHelixArgs")
 
