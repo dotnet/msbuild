9077e6dd6e3d9c702925a747c4819331bbee32a8
Forgind <Forgind@users.noreply.github.com>
2022-02-14 17:47:29 -0800

    Write to windows event log?

--- src/Shared/Debugging/DebugUtils.cs
+++ src/Shared/Debugging/DebugUtils.cs
@@ -2,10 +2,12 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using Microsoft.Build.Framework;
+using Microsoft.Build.Shared.FileSystem;
 using System;
 using System.Diagnostics;
 using System.IO;
 using System.Security;
+using System.Security.Principal;
 using System.Text.RegularExpressions;
 
 #nullable disable
@@ -46,6 +48,20 @@ namespace Microsoft.Build.Shared.Debugging
                 {
                     Environment.SetEnvironmentVariable("MSBUILDDEBUGPATH", debugDirectory);
                 }
+
+#if NETFRAMEWORK
+                string source = "MSBuild";
+                using (EventLog eventLog = new(source))
+                {
+                    if (!EventLog.SourceExists(source))
+                    {
+                        EventLog.CreateEventSource(source, "MSBuild_Logs");
+                    }
+
+                    eventLog.Source = source;
+                    eventLog.WriteEntry($"Binary logs and other build information will be written to {debugDirectory}. If this is wrong, set the environment variable MSBUILDDEBUGPATH to an appropriate value.");
+                }
+#endif
             }
 
             if (debugDirectory is not null)
