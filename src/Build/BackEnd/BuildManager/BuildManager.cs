def4fbb8675b5b1e3afc6a2613921953a542a9a9
Rainer Sigwald <raines@microsoft.com>
2022-02-16 14:10:15 -0600

    Opt into IncludeEvaluationPropertiesAndItems in NullLogger (#7386)
    This keeps a build with all distribution-aware loggers from vetoing the
    new behavior because a logger (NullLogger) is attached that doesn't indicate
    that it can handle it. It trivially can, so it should!

--- src/Build/BackEnd/BuildManager/BuildManager.cs
+++ src/Build/BackEnd/BuildManager/BuildManager.cs
@@ -3232,6 +3232,25 @@ namespace Microsoft.Build.Execution
             /// </summary>
             public void Initialize(IEventSource eventSource)
             {
+                // The concrete type we get should always be our internal
+                // implementation and up-to-date, but we need to meet the
+                // external contract so can't specify that for the
+                // argument.
+
+                IEventSource4 eventSource4 = (IEventSource4)eventSource;
+
+                // Most checks in LoggingService are "does any attached logger
+                // specifically opt into this new behavior?". As such, the
+                // NullLogger shouldn't opt into them explicitly and should
+                // let other loggers opt in.
+
+                // IncludeEvaluationPropertiesAndItems is different though,
+                // because its check is "do ALL attached loggers opt into
+                // the new behavior?", since the new behavior removes
+                // information from old loggers. So the NullLogger must
+                // opt in to ensure it doesn't accidentally veto the new
+                // behavior.
+                eventSource4.IncludeEvaluationPropertiesAndItems();
             }
 
             /// <summary>
