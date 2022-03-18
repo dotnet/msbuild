eb4aa50ba3ad646ba5df70c677efe40832a498c4
Chet Husk <chusk3@gmail.com>
2022-03-18 15:12:57 -0500

    safer and more complete token grabbing condition

--- src/Cli/dotnet/ParseResultExtensions.cs
+++ src/Cli/dotnet/ParseResultExtensions.cs
@@ -24,7 +24,9 @@ namespace Microsoft.DotNet.Cli
         ///</remarks>
         public static void ShowHelp(this ParseResult parseResult)
         {
-            var tokenList = parseResult.Tokens.TakeWhile(t => t.Type == TokenType.Command).Select(t => t.Value).ToList(); // this can be empty when there's no leading command token
+            // take from the start of the list until we hit an option/--/unparsed token
+            // since commands can have arguments, we must take those as well in order to get accurate help
+            var tokenList = parseResult.Tokens.TakeWhile(token => token.Type == TokenType.Argument || token.Type == TokenType.Command || token.Type == TokenType.Directive).Select(t => t.Value).ToList();
             tokenList.Add("-h");
             Parser.Instance.Parse(tokenList).Invoke();
         }
