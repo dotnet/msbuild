1b5554591915ca8ddba1f84e244ba60007f257cf
Chet Husk <chusk3@gmail.com>
2022-05-04 20:34:29 -0500

    improve AnalysisLevel versioning to match TFM

--- src/Tests/Microsoft.NET.Build.Tests/GivenThatWeWantToFloatWarningLevels.cs
+++ src/Tests/Microsoft.NET.Build.Tests/GivenThatWeWantToFloatWarningLevels.cs
@@ -2,7 +2,6 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System.IO;
-
 using FluentAssertions;
 
 using Microsoft.NET.TestFramework;
@@ -112,6 +111,59 @@ namespace Microsoft.NET.Build.Tests
             var computedWarningLevel = buildCommand.GetValues()[0];
             buildResult.StdErr.Should().Be(string.Empty);
             computedWarningLevel.Should().Be(expectedWarningLevel.ToString());
+        }
+
+        [InlineData(targetFrameworkNet6, "6.0")]
+        [InlineData(targetFrameworkNet7, "7.0")]
+        [InlineData(targetFrameworkNetFramework472, null)]
+        [RequiresMSBuildVersionTheory("16.8")]
+        public void It_defaults_AnalysisLevel_To_The_Current_TFM_When_NotLatestTFM(string tfm, string analysisLevel)
+        {
+            var testProject = new TestProject
+            {
+                Name = "HelloWorld",
+                TargetFrameworks = tfm,
+                IsExe = true,
+                SourceFiles =
+                {
+                    ["Program.cs"] = @"
+                        using System;
+
+                        namespace ConsoleCore
+                        {
+                            class Program
+                            {
+                                static void Main()
+                                {
+                                }
+                            }
+                        }
+                    ",
+                }
+            };
+
+            var testAsset = _testAssetsManager
+                .CreateTestProject(testProject, identifier: "analysisLevelConsoleApp"+tfm, targetExtension: ".csproj");
+
+            var buildCommand = new GetValuesCommand(
+                Log,
+                Path.Combine(testAsset.TestRoot, testProject.Name),
+                tfm, "EffectiveAnalysisLevel")
+            {
+                DependsOnTargets = "Build"
+            };
+            var buildResult = buildCommand.Execute();
+
+                buildResult.StdErr.Should().Be(string.Empty);
+            if (analysisLevel == null)
+            {
+                buildCommand.GetValues().Should().BeEmpty();
+            }
+            else {
+                var computedEffectiveAnalysisLevel = buildCommand.GetValues()[0];
+                computedEffectiveAnalysisLevel.Should().Be(analysisLevel.ToString());
+            }
+            buildResult.StdErr.Should().Be(string.Empty);
         }
 
     }
