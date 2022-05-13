5fc09bebd63921458a45f2c05bc7c3d52ff3b75b
Forgind <Forgind@users.noreply.github.com>
2022-05-12 20:25:44 -0700

    VS broke the build

--- src/MSBuild.UnitTests/MSBuildServer_Tests.cs
+++ src/MSBuild.UnitTests/MSBuildServer_Tests.cs
@@ -4,7 +4,6 @@
 using System;
 using System.Diagnostics;
 using System.Reflection;
-using System.Runtime.Remoting.Messaging;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Build.Framework;
