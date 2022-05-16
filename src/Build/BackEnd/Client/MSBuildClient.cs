2cf13e470af4a978d09e340fa0bc8516665815ce
Forgind <Forgind@users.noreply.github.com>
2022-05-16 16:52:50 -0700

    Some cleanup

--- src/Build/BackEnd/Client/MSBuildClient.cs
+++ src/Build/BackEnd/Client/MSBuildClient.cs
@@ -230,26 +230,13 @@ namespace Microsoft.Build.Execution
 
         private void SupportVT100()
         {
-            const int STD_OUTPUT_HANDLE = -11;
-            const uint DISABLE_NEWLINE_AUTO_RETURN = 0x0008;
-            const uint ENABLE_VIRTUAL_TERMINAL_PROCESSING = 0x0004;
-
-            var stdOut = GetStdHandle(STD_OUTPUT_HANDLE);
-            if (GetConsoleMode(stdOut, out uint consoleMode))
+            IntPtr stdOut = NativeMethodsShared.GetStdHandle(NativeMethodsShared.STD_OUTPUT_HANDLE);
+            if (NativeMethodsShared.GetConsoleMode(stdOut, out uint consoleMode))
             {
-                consoleMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING | DISABLE_NEWLINE_AUTO_RETURN;
-                SetConsoleMode(stdOut, consoleMode);
+                consoleMode |= NativeMethodsShared.ENABLE_VIRTUAL_TERMINAL_PROCESSING | NativeMethodsShared.DISABLE_NEWLINE_AUTO_RETURN;
+                NativeMethodsShared.SetConsoleMode(stdOut, consoleMode);
             }
         }
-
-        [DllImport("kernel32.dll")]
-        private static extern bool GetConsoleMode(IntPtr hConsoleHandle, out uint lpMode);
-
-        [DllImport("kernel32.dll")]
-        private static extern bool SetConsoleMode(IntPtr hConsoleHandle, uint dwMode);
-
-        [DllImport("kernel32.dll", SetLastError = true)]
-        private static extern IntPtr GetStdHandle(int nStdHandle);
 
         private void SendCancelCommand(NamedPipeClientStream nodeStream) => throw new NotImplementedException();
 
