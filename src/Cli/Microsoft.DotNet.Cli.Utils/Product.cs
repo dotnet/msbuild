9478c04bd19dfe043a5b658a92b653cfda34bf0e
Jan Krivanek <jankrivanek@microsoft.com>
2022-05-24 15:04:56 +0200

    Remove the reflection usage to get product version

--- src/Cli/Microsoft.DotNet.Cli.Utils/Product.cs
+++ src/Cli/Microsoft.DotNet.Cli.Utils/Product.cs
@@ -1,8 +1,6 @@
 // Copyright (c) .NET Foundation and contributors. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using System.Reflection;
-
 namespace Microsoft.DotNet.Cli.Utils
 {
     public class Product
@@ -13,11 +11,7 @@ namespace Microsoft.DotNet.Cli.Utils
         private static string GetProductVersion()
         {
             DotnetVersionFile versionFile = DotnetFiles.VersionFileObject;
-            return versionFile.BuildNumber ??
-                   System.Diagnostics.FileVersionInfo.GetVersionInfo(
-                           typeof(Product).GetTypeInfo().Assembly.Location)
-                       .ProductVersion ?? 
-                   string.Empty;
+            return versionFile.BuildNumber ?? string.Empty;
         }
     }
 }
