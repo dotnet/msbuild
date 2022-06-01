f4f534ebf6ed278036699f948697c07e7514d57c
Jan Krivanek <jankrivanek@microsoft.com>
2022-06-01 07:52:27 +0200

    Workload + Sdk version Constraints (#4753)
    * Extract common logic
    
    * Move Constraints abstractions, Extract Constraint base
    
    * Implement Workload and SdkVersion Constraints
    
    * Add Unit test, implement PR feedback
    
    * Support multiple Sdks, Reflect PR comments
    
    * Fix build after merge
    
    * Fix API
    
    * Improve changes
    
    * Improve based on PR comments

--- src/Microsoft.TemplateEngine.Cli/CliTemplateInfo.cs
+++ src/Microsoft.TemplateEngine.Cli/CliTemplateInfo.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using Microsoft.TemplateEngine.Abstractions;
+using Microsoft.TemplateEngine.Abstractions.Constraints;
 using Microsoft.TemplateEngine.Abstractions.TemplatePackage;
 using Microsoft.TemplateEngine.Edge.Settings;
 using Microsoft.TemplateEngine.Utils;
