d4c4bb28c3feb8c8c4a81a90bd36c4a8bbbfe3be
Forgind <Forgind@users.noreply.github.com>
2022-06-07 16:30:27 -0700

    Reorder using statements

--- src/Shared/FileMatcher.cs
+++ src/Shared/FileMatcher.cs
@@ -2,17 +2,17 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
+using System.Buffers;
 using System.Collections.Concurrent;
-using System.IO;
-using System.Text;
-using System.Diagnostics;
-using System.Linq;
-using System.Text.RegularExpressions;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Microsoft.Build.Framework;
 using Microsoft.Build.Shared.FileSystem;
-using System.Buffers;
 
 #nullable disable
 
