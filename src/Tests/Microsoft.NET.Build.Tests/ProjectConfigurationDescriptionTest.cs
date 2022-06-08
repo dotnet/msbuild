b00ae4e042660bdca74da588aed0bc24c646bd67
Rainer Sigwald <raines@microsoft.com>
2022-06-08 10:53:15 -0500

    Copyright header for new test file

--- src/Tests/Microsoft.NET.Build.Tests/ProjectConfigurationDescriptionTest.cs
+++ src/Tests/Microsoft.NET.Build.Tests/ProjectConfigurationDescriptionTest.cs
@@ -1,4 +1,7 @@
-﻿using System.IO;
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.IO;
 using Microsoft.NET.TestFramework;
 using Microsoft.NET.TestFramework.Assertions;
 using Microsoft.NET.TestFramework.Commands;
