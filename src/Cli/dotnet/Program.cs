c8eb0df5b81838a595ed9398e80b5bd169a2bde8
Tom Deseyn <tom.deseyn@gmail.com>
2022-06-09 14:49:11 +0200

    Use const string also for existing check.

--- src/Cli/dotnet/Program.cs
+++ src/Cli/dotnet/Program.cs
@@ -178,7 +178,7 @@ namespace Microsoft.DotNet.Cli
                     bool generateAspNetCertificate =
                         environmentProvider.GetEnvironmentVariableAsBool("DOTNET_GENERATE_ASPNET_CERTIFICATE", defaultValue: true);
                     bool telemetryOptout =
-                      environmentProvider.GetEnvironmentVariableAsBool("DOTNET_CLI_TELEMETRY_OPTOUT", defaultValue: false);
+                      environmentProvider.GetEnvironmentVariableAsBool(TelemetryOptout, defaultValue: false);
                     bool addGlobalToolsToPath =
                         environmentProvider.GetEnvironmentVariableAsBool("DOTNET_ADD_GLOBAL_TOOLS_TO_PATH", defaultValue: true);
                     bool nologo =
