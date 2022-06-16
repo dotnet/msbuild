39a56d0daa531e24aff5f04bcbd03dc20bcbec83
MichalPavlik <mipavlik@microsoft.com>
2022-06-16 19:14:29 +0200

    Moving public types not intended to use externally to "Microsoft.Build.Experimental" namespace. (#7688)

--- src/Build/BackEnd/Client/MSBuildClientExitResult.cs
+++ src/Build/BackEnd/Client/MSBuildClientExitResult.cs
@@ -1,7 +1,7 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-namespace Microsoft.Build.Execution
+namespace Microsoft.Build.Experimental
 {
     /// <summary>
     /// Enumeration of the various ways in which the MSBuildClient execution can exit.
