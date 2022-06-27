2348a1546d98ee28fc7b4df8747f96455abd59f1
AR-May <67507805+AR-May@users.noreply.github.com>
2022-06-27 16:06:28 +0200

    Address PR comment.

--- src/Build/BackEnd/Components/Communications/NodeLauncher.cs
+++ src/Build/BackEnd/Components/Communications/NodeLauncher.cs
@@ -26,28 +26,13 @@ namespace Microsoft.Build.BackEnd
         {
             // Disable MSBuild server for a child process.
             // In case of starting msbuild server it prevents an infinite recurson. In case of starting msbuild node we also do not want this variable to be set.
-            string useMSBuildServerEnvVarValue = Environment.GetEnvironmentVariable(Traits.UseMSBuildServerEnvVarName);
-            try
-            {
-                if (useMSBuildServerEnvVarValue is not null)
-                {
-                    Environment.SetEnvironmentVariable(Traits.UseMSBuildServerEnvVarName, "0");
-                }
-                return PrivateStart(msbuildLocation, commandLineArgs);
-            }
-            finally
-            {
-                if (useMSBuildServerEnvVarValue is not null)
-                {
-                    Environment.SetEnvironmentVariable(Traits.UseMSBuildServerEnvVarName, useMSBuildServerEnvVarValue);
-                }
-            }
+            return DisableMSBuildServer(() => StartInternal(msbuildLocation, commandLineArgs));
         }
 
         /// <summary>
         /// Creates a new MSBuild process
         /// </summary>
-        private Process PrivateStart(string msbuildLocation, string commandLineArgs)
+        private Process StartInternal(string msbuildLocation, string commandLineArgs)
         {
             // Should always have been set already.
             ErrorUtilities.VerifyThrowInternalLength(msbuildLocation, nameof(msbuildLocation));
@@ -200,6 +185,26 @@ namespace Microsoft.Build.BackEnd
 
                 CommunicationsUtilities.Trace("Successfully launched {1} node with PID {0}", childProcessId, exeName);
                 return Process.GetProcessById(childProcessId);
+            }
+        }
+
+        private Process DisableMSBuildServer(Func<Process> func)
+        {
+            string useMSBuildServerEnvVarValue = Environment.GetEnvironmentVariable(Traits.UseMSBuildServerEnvVarName);
+            try
+            {
+                if (useMSBuildServerEnvVarValue is not null)
+                {
+                    Environment.SetEnvironmentVariable(Traits.UseMSBuildServerEnvVarName, "0");
+                }
+                return func();
+            }
+            finally
+            {
+                if (useMSBuildServerEnvVarValue is not null)
+                {
+                    Environment.SetEnvironmentVariable(Traits.UseMSBuildServerEnvVarName, useMSBuildServerEnvVarValue);
+                }
             }
         }
     }
