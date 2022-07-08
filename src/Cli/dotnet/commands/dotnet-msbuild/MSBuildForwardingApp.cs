c780119f5224d36082859029c2bd1084ed2c76ce
Roman Konecny <rokonecn@microsoft.com>
2022-07-08 12:29:27 -0700

    Revert "Fix telemetry session ID env var"
    This reverts commit d90bd2877697c738c6a867e5fc46fd7e91c3c102.

--- src/Cli/dotnet/commands/dotnet-msbuild/MSBuildForwardingApp.cs
+++ src/Cli/dotnet/commands/dotnet-msbuild/MSBuildForwardingApp.cs
@@ -63,6 +63,8 @@ namespace Microsoft.DotNet.Tools.MSBuild
 
         public ProcessStartInfo GetProcessStartInfo()
         {
+            EnvironmentVariable(TelemetrySessionIdEnvironmentVariableName, Telemetry.CurrentSessionId);
+
             return _forwardingAppWithoutLogging.GetProcessStartInfo();
         }
 
@@ -82,10 +84,6 @@ namespace Microsoft.DotNet.Tools.MSBuild
             Console.CancelKeyPress += (sender, e) => { e.Cancel = true; };
 
             int exitCode;
-
-            // Config needed for telemetry logger
-            EnvironmentVariable(TelemetrySessionIdEnvironmentVariableName, Telemetry.CurrentSessionId);
-
             if (_forwardingAppWithoutLogging.ExecuteMSBuildOutOfProc)
             {
                 ProcessStartInfo startInfo = GetProcessStartInfo();
