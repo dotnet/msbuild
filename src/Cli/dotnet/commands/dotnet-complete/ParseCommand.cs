baronfel/sdk

[24068] Remove Legacy -- behavior and use of UnparsedTokens

src/Cli/dotnet/commands/dotnet-complete/ParseCommand.cs
@@ -13,10 +13,10 @@ namespace Microsoft.DotNet.Cli
 
             Console.WriteLine(result.Diagram());
 
-            if (result.UnparsedTokens.Any())
+            if (result.UnmatchedTokens.Any())
             {
-                Console.WriteLine("Unparsed Tokens: ");
-                Console.WriteLine(string.Join(" ", result.UnparsedTokens));
+                Console.WriteLine("UnmatchedTokens Tokens: ");
+                Console.WriteLine(string.Join(" ", result.UnmatchedTokens));
             }
 
             var optionValuesToBeForwarded = result.OptionValuesToBeForwarded(ParseCommandParser.GetCommand());

dotnet/msbuild

src/Cli/dotnet/commands/dotnet-complete/ParseCommand.cs
@@ -17,10 +17,10 @@ namespace Microsoft.DotNet.Cli
             Console.WriteLine(reparsed.Diagram());
 
 
-            if (reparsed.UnparsedTokens.Any())
+            if (reparsed.UnmatchedTokens.Any())
             {
-                Console.WriteLine("Unparsed Tokens: ");
-                Console.WriteLine(string.Join(" ", reparsed.UnparsedTokens));
+                Console.WriteLine("Unmatched Tokens: ");
+                Console.WriteLine(string.Join(" ", reparsed.UnmatchedTokens));
             }
 
             var optionValuesToBeForwarded = reparsed.OptionValuesToBeForwarded(ParseCommandParser.GetCommand());