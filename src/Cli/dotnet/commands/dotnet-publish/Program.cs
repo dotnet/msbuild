814cdaf614b808322439c8185b1049f7fa737876
Noah Gilson <noahgilson@microsoft.com>
2022-07-26 14:00:09 -0700

    [WIP] Find the project and process publishrelease. Works if project provided, elsewise need to discuss dupe-ing MSBuild code

--- src/Cli/dotnet/commands/dotnet-publish/Program.cs
+++ src/Cli/dotnet/commands/dotnet-publish/Program.cs
@@ -5,6 +5,8 @@ using System;
 using System.Collections.Generic;
 using System.CommandLine;
 using System.CommandLine.Parsing;
+using System.Linq;
+using Microsoft.Build.Execution;
 using Microsoft.DotNet.Cli;
 using Microsoft.DotNet.Cli.Utils;
 using Parser = Microsoft.DotNet.Cli.Parser;
@@ -36,8 +38,39 @@ namespace Microsoft.DotNet.Tools.Publish
             var msbuildArgs = new List<string>()
             {
                 "-target:Publish",
-                "-property:_IsPublishing=true"
             };
+
+            // TODO: Move this code to function ProcessPublishConfigurationProperties(ref msbuildArgs) 
+
+            IEnumerable<string> args = parseResult.GetValueForArgument(PublishCommandParser.SlnOrProjectArgument);
+            string releaseMode = null;
+            bool projectIdentified = false;
+            foreach (string potentialProject in args)
+            {
+                // Get project
+                if (!string.IsNullOrEmpty(potentialProject) && potentialProject.Contains("proj")) // Could be bad if test dir has proj in it, need to eval
+                {
+                    var project = new ProjectInstance(potentialProject);
+                    projectIdentified = true;
+
+                    string publishRelease = project.GetPropertyValue("PublishRelease");
+                    if (!string.IsNullOrEmpty(publishRelease))
+                    {
+                        releaseMode = publishRelease == "true" ? "Release" : "Debug";
+                    }
+                }
+            }
+
+            // Get project
+            if(!projectIdentified)
+            {
+                ; // Call XMake::ProcessProjectSwitch to get the targeting project. This code needs to be imported to the SDK. 
+            }
+
+            if (!string.IsNullOrEmpty(releaseMode))
+                msbuildArgs.Add($"-property:configuration={releaseMode}");
+
+            // -----------
 
             CommonOptions.ValidateSelfContainedOptions(parseResult.HasOption(PublishCommandParser.SelfContainedOption),
                 parseResult.HasOption(PublishCommandParser.NoSelfContainedOption));
