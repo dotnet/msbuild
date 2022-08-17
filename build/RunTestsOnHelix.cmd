ba34d87e0e921f08e58c74c468cc2e3a3c0ef163
Marc Paine <marcpop@microsoft.com>
2022-08-17 15:18:09 -0700

    Update RunTestsOnHelix.cmd

--- build/RunTestsOnHelix.cmd
+++ build/RunTestsOnHelix.cmd
@@ -20,7 +20,7 @@ mkdir %TestExecutionDirectory%
 robocopy %HELIX_CORRELATION_PAYLOAD%\t\TestExecutionDirectoryFiles %TestExecutionDirectory%
 
 REM call dotnet new so the first run message doesn't interfere with the first test
-dotnet new
+dotnet new --debug:ephemeral-hive
 REM avoid potetial cocurrency issues when nuget is creating nuget.config
 dotnet nuget list source
 REM We downloaded a special zip of files to the .nuget folder so add that as a source
