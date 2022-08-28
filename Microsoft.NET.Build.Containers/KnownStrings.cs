2f2e5dfccdc63fae88918c12526a896f00f416b1
Chet Husk <chusk3@gmail.com>
2022-08-27 19:15:56 -0500

    move knownstrings to library layer ot help keep in sync

---
+++ Microsoft.NET.Build.Containers/KnownStrings.cs
@@ -0,0 +1,33 @@
+namespace Microsoft.NET.Build.Containers;
+
+public static class KnownStrings
+{
+    public static class Properties
+    {
+        public static string ContainerBaseImage = nameof(ContainerBaseImage);
+        public static string ContainerRegistry = nameof(ContainerRegistry);
+        public static string ContainerImageName = nameof(ContainerImageName);
+        public static string ContainerImageTag = nameof(ContainerImageTag);
+        public static string ContainerImageTags = nameof(ContainerImageTags);
+        public static string ContainerWorkingDirectory = nameof(ContainerWorkingDirectory);
+        public static string ContainerEntrypoint = nameof(ContainerEntrypoint);
+        public static string UseAppHost = nameof(UseAppHost);
+        public static string ContainerLabel = nameof(ContainerLabel);
+        public static string SelfContained = nameof(SelfContained);
+        public static string ContainerPort = nameof(ContainerPort);
+        public static string ContainerEnvironmentVariable = nameof(ContainerEnvironmentVariable);
+
+        public static string ComputeContainerConfig = nameof(ComputeContainerConfig);
+        public static string AssemblyName = nameof(AssemblyName);
+        public static string ContainerBaseRegistry = nameof(ContainerBaseRegistry);
+        public static string ContainerBaseName = nameof(ContainerBaseName);
+        public static string ContainerBaseTag = nameof(ContainerBaseTag);
+    }
+
+    public static class ErrorCodes
+    {
+        public static string CONTAINER001 = nameof(CONTAINER001);
+        public static string CONTAINER004 = nameof(CONTAINER004);
+        public static string CONTAINER005 = nameof(CONTAINER005);
+    }
+}
--- Microsoft.NET.Build.Containers/ParseContainerProperties.cs
+++ Microsoft.NET.Build.Containers/ParseContainerProperties.cs
@@ -115,7 +115,7 @@ public class ParseContainerProperties : Microsoft.Build.Utilities.Task
             else
             {
                 validTags = Array.Empty<string>();
-                Log.LogError(null, "CONTAINER004", "Container.InvalidTag", null, 0, 0, 0, 0, $"Invalid {nameof(ContainerImageTag)} provided: {{0}}. Image tags must be alphanumeric, underscore, hyphen, or period.", ContainerImageTag);
+                Log.LogError(null, KnownStrings.ErrorCodes.CONTAINER004, "Container.InvalidTag", null, 0, 0, 0, 0, $"Invalid {nameof(ContainerImageTag)} provided: {{0}}. Image tags must be alphanumeric, underscore, hyphen, or period.", ContainerImageTag);
             }
         }
         else if (ContainerImageTags.Length != 0 && TryValidateTags(ContainerImageTags, out var valids, out var invalids))
@@ -128,7 +128,7 @@ public class ParseContainerProperties : Microsoft.Build.Utilities.Task
                     { Length: 1 } => ($"Invalid {nameof(ContainerImageTags)} provided: {{0}}. {nameof(ContainerImageTags)} must be a semicolon-delimited list of valid image tags. Image tags must be alphanumeric, underscore, hyphen, or period.", invalids[0]),
                     _ => ($"Invalid {nameof(ContainerImageTags)} provided: {{0}}. {nameof(ContainerImageTags)} must be a semicolon-delimited list of valid image tags. Image tags must be alphanumeric, underscore, hyphen, or period.", String.Join(", ", invalids))
                 };
-                Log.LogError(null, "CONTAINER004", "Container.InvalidTag", null, 0, 0, 0, 0, message, args);
+                Log.LogError(null, KnownStrings.ErrorCodes.CONTAINER004, "Container.InvalidTag", null, 0, 0, 0, 0, message, args);
                 return !Log.HasLoggedErrors;
             }
         }
@@ -163,10 +163,10 @@ public class ParseContainerProperties : Microsoft.Build.Utilities.Task
 
         try
         {
-            if (!ContainerHelpers.NormalizeImageName(ContainerImageName, out string? normalizedImageName))
+            if (!ContainerHelpers.NormalizeImageName(ContainerImageName, out var normalizedImageName))
             {
-                Log.LogMessage(MessageImportance.High, $"'{ContainerImageName}' was not a valid container image name, it was normalized to {normalizedImageName}");
-                NewContainerImageName = normalizedImageName ?? "";
+                Log.LogMessage(null, KnownStrings.ErrorCodes.CONTAINER001, "Container.InvalidImageName", null, 0, 0, 0, 0, MessageImportance.High, "'{0}' was not a valid container image name, it was normalized to '{1}'", nameof(ContainerImageName), normalizedImageName);
+                NewContainerImageName = normalizedImageName;
             }
             else
             {
