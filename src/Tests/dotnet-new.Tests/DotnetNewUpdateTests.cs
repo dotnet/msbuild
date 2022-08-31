776ff66a3bf36b9cf36394bf626dcceb7161c325
Vlada Shubina <vshubina@microsoft.com>
2022-08-31 11:34:19 +0200

    merged update command tests

--- src/Tests/dotnet-new.Tests/DotnetNewUpdateTests.cs
+++ src/Tests/dotnet-new.Tests/DotnetNewUpdateTests.cs
@@ -9,11 +9,11 @@ using Xunit.Abstractions;
 
 namespace Microsoft.DotNet.Cli.New.IntegrationTests
 {
-    public class DotnetNewUpdateCheck : BaseIntegrationTest
+    public class DotnetNewUpdateTests : BaseIntegrationTest
     {
         private readonly ITestOutputHelper _log;
 
-        public DotnetNewUpdateCheck(ITestOutputHelper log) : base(log)
+        public DotnetNewUpdateTests(ITestOutputHelper log) : base(log)
         {
             _log = log;
         }
@@ -24,7 +24,7 @@ namespace Microsoft.DotNet.Cli.New.IntegrationTests
         [InlineData("update --dry-run")]
         public void CanCheckForUpdate(string testCase)
         {
-            var home = CreateTemporaryFolder(folderName: "Home");
+            string home = CreateTemporaryFolder(folderName: "Home");
             new DotnetNewCommand(_log, "install", "Microsoft.DotNet.Common.ProjectTemplates.5.0::5.0.0")
                 .WithCustomHive(home).WithoutBuiltInTemplates()
                 .WithWorkingDirectory(CreateTemporaryFolder())
@@ -61,7 +61,7 @@ namespace Microsoft.DotNet.Cli.New.IntegrationTests
             string nugetFullName = $"{nugetName}::{nugetVersion}";
             string nugetFileName = $"{nugetName}.{nugetVersion}.nupkg";
             string workingDirectory = CreateTemporaryFolder();
-            var home = CreateTemporaryFolder(folderName: "Home");
+            string home = CreateTemporaryFolder(folderName: "Home");
 
             InstallNuGetTemplate(
                 Path.Combine(DotnetNewTestPackagesBasePath, nugetFileName),
@@ -87,7 +87,7 @@ For details on the exit code, refer to https://aka.ms/templating-exit-codes#106"
         [InlineData("update --dry-run")]
         public void DoesNotShowUpdatesWhenAllTemplatesAreUpToDate(string testCase)
         {
-            var home = CreateTemporaryFolder(folderName: "Home");
+            string home = CreateTemporaryFolder(folderName: "Home");
             string workingDirectory = CreateTemporaryFolder();
             InstallTestTemplate("TemplateResolution/DifferentLanguagesGroup/BasicFSharp", _log, home, workingDirectory);
             new DotnetNewCommand(_log, "install", "Microsoft.DotNet.Common.ProjectTemplates.5.0")
@@ -116,7 +116,7 @@ For details on the exit code, refer to https://aka.ms/templating-exit-codes#106"
         [Fact]
         public void PrintInfoOnUpdateOnCreation()
         {
-            var home = CreateTemporaryFolder(folderName: "Home");
+            string home = CreateTemporaryFolder(folderName: "Home");
             new DotnetNewCommand(_log, "install", "Microsoft.DotNet.Common.ProjectTemplates.5.0::5.0.0")
                 .WithCustomHive(home).WithoutBuiltInTemplates()
                 .WithWorkingDirectory(CreateTemporaryFolder())
@@ -146,7 +146,7 @@ For details on the exit code, refer to https://aka.ms/templating-exit-codes#106"
         [Fact]
         public void DoesNotPrintUpdateInfoOnCreation_WhenNoUpdateCheckOption()
         {
-            var home = CreateTemporaryFolder(folderName: "Home");
+            string home = CreateTemporaryFolder(folderName: "Home");
             new DotnetNewCommand(_log, "install", "Microsoft.DotNet.Common.ProjectTemplates.5.0::5.0.0")
                 .WithCustomHive(home).WithoutBuiltInTemplates()
                 .WithWorkingDirectory(CreateTemporaryFolder())
@@ -189,7 +189,7 @@ For details on the exit code, refer to https://aka.ms/templating-exit-codes#106"
         [Fact]
         public void DoesNotPrintUpdateInfoOnCreation_WhenLatestVersionIsInstalled()
         {
-            var home = CreateTemporaryFolder(folderName: "Home");
+            string home = CreateTemporaryFolder(folderName: "Home");
             new DotnetNewCommand(_log, "install", "Microsoft.DotNet.Common.ProjectTemplates.5.0")
                 .WithCustomHive(home).WithoutBuiltInTemplates()
                 .WithWorkingDirectory(CreateTemporaryFolder())
@@ -217,15 +217,130 @@ For details on the exit code, refer to https://aka.ms/templating-exit-codes#106"
         }
 
         [Fact]
-        public void CanShowDeprecationMessage_WhenLegacyCommandIsUsed()
+        public void CanShowDeprecationMessage_WhenLegacyCommandIsUsed_Check()
         {
             const string deprecationMessage =
 @"Warning: use of 'dotnet new --update-check' is deprecated. Use 'dotnet new update --check-only' instead.
 For more information, run: 
    dotnet new update -h";
 
-            var home = CreateTemporaryFolder(folderName: "Home");
-            var commandResult = new DotnetNewCommand(_log, "--update-check")
+            string home = CreateTemporaryFolder(folderName: "Home");
+            Utils.CommandResult commandResult = new DotnetNewCommand(_log, "--update-check")
+                .WithCustomHive(home)
+                .Execute();
+
+            commandResult.Should()
+                .ExitWith(0)
+                .And.NotHaveStdErr();
+
+            Assert.StartsWith(deprecationMessage, commandResult.StdOut);
+        }
+
+        [Fact]
+        public void DoNotShowDeprecationMessage_WhenNewCommandIsUsed_Check()
+        {
+            string home = CreateTemporaryFolder(folderName: "Home");
+            Utils.CommandResult commandResult = new DotnetNewCommand(_log, "update", "--check-only")
+                .WithCustomHive(home)
+                .Execute();
+
+            commandResult.Should()
+                .ExitWith(0)
+                .And.NotHaveStdErr()
+                .And.NotHaveStdOutContaining("Warning")
+                .And.NotHaveStdOutContaining("deprecated");
+        }
+
+        [Theory]
+        [InlineData("--update-apply")]
+        [InlineData("update")]
+        public void CanApplyUpdates(string testCase)
+        {
+            string home = CreateTemporaryFolder(folderName: "Home");
+            new DotnetNewCommand(_log, "install", "Microsoft.DotNet.Common.ProjectTemplates.5.0::5.0.0")
+                .WithCustomHive(home).WithoutBuiltInTemplates()
+                .WithWorkingDirectory(CreateTemporaryFolder())
+                .Execute()
+                .Should()
+                .ExitWith(0)
+                .And
+                .NotHaveStdErr()
+                .And.NotHaveStdOutContaining("Determining projects to restore...")
+                .And.HaveStdOutContaining("console")
+                .And.HaveStdOutContaining("classlib");
+
+            new DotnetNewCommand(_log, "update", "--check-only")
+                .WithCustomHive(home).WithoutBuiltInTemplates()
+                .WithWorkingDirectory(CreateTemporaryFolder())
+                .Execute()
+                .Should()
+                .ExitWith(0)
+                .And
+                .NotHaveStdErr()
+                .And.HaveStdOutContaining("An update for template packages is available:")
+                .And.HaveStdOutMatching("Package\\s+Current\\s+Latest")
+                .And.HaveStdOutMatching("Microsoft.DotNet.Common.ProjectTemplates.5.0\\s+5.0.0\\s+([\\d\\.a-z-])+")
+                .And.HaveStdOutContaining("To update the package use:")
+                .And.HaveStdOutContaining("   dotnet new install <package>::<version>")
+                .And.HaveStdOutMatching("   dotnet new install Microsoft\\.DotNet\\.Common\\.ProjectTemplates\\.5\\.0::([\\d\\.a-z-])+");
+
+            new DotnetNewCommand(_log, testCase)
+                .WithCustomHive(home).WithoutBuiltInTemplates()
+                .WithWorkingDirectory(CreateTemporaryFolder())
+                .Execute()
+                .Should()
+                .ExitWith(0)
+                .And
+                .NotHaveStdErr()
+                .And.HaveStdOutContaining("The following template packages will be updated:")
+                .And.HaveStdOutContaining("Microsoft.DotNet.Common.ProjectTemplates.5.0::5.0.0 was successfully uninstalled")
+                .And.NotHaveStdOutContaining("Microsoft.DotNet.Common.ProjectTemplates.5.0::5.0.0 is already installed and will be replaced with version")
+                .And.HaveStdOutMatching($"^Success: Microsoft\\.DotNet\\.Common\\.ProjectTemplates\\.5\\.0::([\\d\\.a-z-])+ installed the following templates:\\s*$", System.Text.RegularExpressions.RegexOptions.Multiline)
+                .And.HaveStdOutContaining("console")
+                .And.HaveStdOutContaining("Console App");
+        }
+
+        [Theory]
+        [InlineData("--update-apply")]
+        [InlineData("update")]
+        public void DoesNotApplyUpdatesWhenAllTemplatesAreUpToDate(string commandName)
+        {
+            string home = CreateTemporaryFolder(folderName: "Home");
+            string workingDirectory = CreateTemporaryFolder();
+            string templateLocation = InstallTestTemplate("TemplateResolution/DifferentLanguagesGroup/BasicFSharp", _log, home, workingDirectory);
+            new DotnetNewCommand(_log, "install", "Microsoft.DotNet.Common.ProjectTemplates.5.0")
+                .WithCustomHive(home)
+                .WithWorkingDirectory(CreateTemporaryFolder())
+                .Execute()
+                .Should()
+                .ExitWith(0)
+                .And
+                .NotHaveStdErr()
+                .And.NotHaveStdOutContaining("Determining projects to restore...")
+                .And.HaveStdOutContaining("console")
+                .And.HaveStdOutContaining("classlib");
+
+            new DotnetNewCommand(_log, commandName)
+                .WithCustomHive(home)
+                .WithWorkingDirectory(CreateTemporaryFolder())
+                .Execute()
+                .Should()
+                .ExitWith(0)
+                .And
+                .NotHaveStdErr()
+                .And.HaveStdOutContaining("All template packages are up-to-date.");
+        }
+
+        [Fact]
+        public void CanShowDeprecationMessage_WhenLegacyCommandIsUsed()
+        {
+            const string deprecationMessage =
+@"Warning: use of 'dotnet new --update-apply' is deprecated. Use 'dotnet new update' instead.
+For more information, run: 
+   dotnet new update -h";
+
+            string home = CreateTemporaryFolder(folderName: "Home");
+            Utils.CommandResult commandResult = new DotnetNewCommand(_log, "--update-apply")
                 .WithCustomHive(home)
                 .Execute();
 
@@ -239,8 +354,8 @@ For more information, run:
         [Fact]
         public void DoNotShowDeprecationMessage_WhenNewCommandIsUsed()
         {
-            var home = CreateTemporaryFolder(folderName: "Home");
-            var commandResult = new DotnetNewCommand(_log, "update", "--check-only")
+            string home = CreateTemporaryFolder(folderName: "Home");
+            Utils.CommandResult commandResult = new DotnetNewCommand(_log, "update")
                 .WithCustomHive(home)
                 .Execute();
 
