7d4ae9a8a2f401681e3c169b1e1fcc854a545d59
Mackinnon Buck <mackinnon.buck@gmail.com>
2022-09-01 10:03:12 -0700

    Hot reload script injection improvements (#27537)

--- src/Tests/Microsoft.AspNetCore.Watch.BrowserRefresh.Tests/WebSocketScriptInjectionTest.cs
+++ src/Tests/Microsoft.AspNetCore.Watch.BrowserRefresh.Tests/WebSocketScriptInjectionTest.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +10,7 @@ using Xunit;
 
 namespace Microsoft.AspNetCore.Watch.BrowserRefresh
 {
-    public class WebSockerScriptInjectionTest
+    public class WebSocketScriptInjectionTest
     {
         [Fact]
         public async Task TryInjectLiveReloadScriptAsync_DoesNotInjectMarkup_IfInputDoesNotContainBodyTag()
@@ -26,22 +27,23 @@ namespace Microsoft.AspNetCore.Watch.BrowserRefresh
             Assert.Equal(input, stream.ToArray());
         }
 
-        [Fact]
-        public async Task TryInjectLiveReloadScriptAsync_InjectsMarkupIfBodyTagAppearsInTheMiddle()
+        [Theory]
+        [MemberData(nameof(ClosingBodyTagVariations))]
+        public async Task TryInjectLiveReloadScriptAsync_InjectsMarkupIfBodyTagAppearsInTheMiddle(string closingBodyTag)
         {
             // Arrange
             var expected =
 $@"<footer>
     This is the footer
 </footer>
-{WebSocketScriptInjection.InjectedScript}</body>
+{WebSocketScriptInjection.InjectedScript}{closingBodyTag}
 </html>";
             var stream = new MemoryStream();
             var input = Encoding.UTF8.GetBytes(
-@"<footer>
+$@"<footer>
     This is the footer
 </footer>
-</body>
+{closingBodyTag}
 </html>");
 
             // Act
@@ -53,13 +55,14 @@ $@"<footer>
             Assert.Equal(expected, output, ignoreLineEndingDifferences: true);
         }
 
-        [Fact]
-        public async Task TryInjectLiveReloadScriptAsync_WithOffsetBodyTagAppearsInMiddle()
+        [Theory]
+        [MemberData(nameof(ClosingBodyTagVariations))]
+        public async Task TryInjectLiveReloadScriptAsync_WithOffsetBodyTagAppearsInMiddle(string closingBodyTag)
         {
             // Arrange
-            var expected = $"</table>{WebSocketScriptInjection.InjectedScript}</body>";
+            var expected = $"</table>{WebSocketScriptInjection.InjectedScript}{closingBodyTag}";
             var stream = new MemoryStream();
-            var input = Encoding.UTF8.GetBytes("unused</table></body>");
+            var input = Encoding.UTF8.GetBytes($"unused</table>{closingBodyTag}");
 
             // Act
             var result = await WebSocketScriptInjection.TryInjectLiveReloadScriptAsync(stream, input.AsMemory(6));
@@ -70,13 +73,14 @@ $@"<footer>
             Assert.Equal(expected, output);
         }
 
-        [Fact]
-        public async Task TryInjectLiveReloadScriptAsync_WithOffsetBodyTagAppearsAtStartOfOffset()
+        [Theory]
+        [MemberData(nameof(ClosingBodyTagVariations))]
+        public async Task TryInjectLiveReloadScriptAsync_WithOffsetBodyTagAppearsAtStartOfOffset(string closingBodyTag)
         {
             // Arrange
-            var expected = $"{WebSocketScriptInjection.InjectedScript}</body>";
+            var expected = $"{WebSocketScriptInjection.InjectedScript}{closingBodyTag}";
             var stream = new MemoryStream();
-            var input = Encoding.UTF8.GetBytes("unused</body>");
+            var input = Encoding.UTF8.GetBytes($"unused{closingBodyTag}");
 
             // Act
             var result = await WebSocketScriptInjection.TryInjectLiveReloadScriptAsync(stream, input.AsMemory(6));
@@ -87,13 +91,14 @@ $@"<footer>
             Assert.Equal(expected, output);
         }
 
-        [Fact]
-        public async Task TryInjectLiveReloadScriptAsync_InjectsMarkupIfBodyTagAppearsAtTheStartOfOutput()
+        [Theory]
+        [MemberData(nameof(ClosingBodyTagVariations))]
+        public async Task TryInjectLiveReloadScriptAsync_InjectsMarkupIfBodyTagAppearsAtTheStartOfOutput(string closingBodyTag)
         {
             // Arrange
-            var expected = $"{WebSocketScriptInjection.InjectedScript}</body></html>";
+            var expected = $"{WebSocketScriptInjection.InjectedScript}{closingBodyTag}</html>";
             var stream = new MemoryStream();
-            var input = Encoding.UTF8.GetBytes("</body></html>");
+            var input = Encoding.UTF8.GetBytes($"{closingBodyTag}</html>");
 
             // Act
             var result = await WebSocketScriptInjection.TryInjectLiveReloadScriptAsync(stream, input);
@@ -104,13 +109,14 @@ $@"<footer>
             Assert.Equal(expected, output);
         }
 
-        [Fact]
-        public async Task TryInjectLiveReloadScriptAsync_InjectsMarkupIfBodyTagAppearsByItself()
+        [Theory]
+        [MemberData(nameof(ClosingBodyTagVariations))]
+        public async Task TryInjectLiveReloadScriptAsync_InjectsMarkupIfBodyTagAppearsByItself(string closingBodyTag)
         {
             // Arrange
-            var expected = $"{WebSocketScriptInjection.InjectedScript}</body>";
+            var expected = $"{WebSocketScriptInjection.InjectedScript}{closingBodyTag}";
             var stream = new MemoryStream();
-            var input = Encoding.UTF8.GetBytes("</body>");
+            var input = Encoding.UTF8.GetBytes(closingBodyTag);
 
             // Act
             var result = await WebSocketScriptInjection.TryInjectLiveReloadScriptAsync(stream, input);
@@ -121,13 +127,14 @@ $@"<footer>
             Assert.Equal(expected, output);
         }
 
-        [Fact]
-        public async Task TryInjectLiveReloadScriptAsync_MultipleBodyTags()
+        [Theory]
+        [MemberData(nameof(ClosingBodyTagVariations))]
+        public async Task TryInjectLiveReloadScriptAsync_MultipleBodyTags(string closingBodyTag)
         {
             // Arrange
-            var expected = $"<p></body>some text</p>{WebSocketScriptInjection.InjectedScript}</body>";
+            var expected = $"<p>{closingBodyTag}some text</p>{WebSocketScriptInjection.InjectedScript}{closingBodyTag}";
             var stream = new MemoryStream();
-            var input = Encoding.UTF8.GetBytes("abc<p></body>some text</p></body>").AsMemory(3);
+            var input = Encoding.UTF8.GetBytes($"abc<p>{closingBodyTag}some text</p>{closingBodyTag}").AsMemory(3);
 
             // Act
             var result = await WebSocketScriptInjection.TryInjectLiveReloadScriptAsync(stream, input);
@@ -138,55 +145,13 @@ $@"<footer>
             Assert.Equal(expected, output);
         }
 
-        [Fact]
-        public void TryInjectLiveReloadScript_NoBodyTag()
+        public static IEnumerable<object[]> ClosingBodyTagVariations => new[]
         {
-            // Arrange
-            var expected = "<p>Hello world</p>";
-            var stream = new MemoryStream();
-            var input = Encoding.UTF8.GetBytes(expected).AsSpan();
-
-            // Act
-            var result = WebSocketScriptInjection.TryInjectLiveReloadScript(stream, input);
-
-            // Assert
-            Assert.False(result);
-            var output = Encoding.UTF8.GetString(stream.ToArray());
-            Assert.Equal(expected, output);
-        }
-
-        [Fact]
-        public void TryInjectLiveReloadScript_NoOffset()
-        {
-            // Arrange
-            var expected = $"</table>{WebSocketScriptInjection.InjectedScript}</body>";
-            var stream = new MemoryStream();
-            var input = Encoding.UTF8.GetBytes("</table></body>").AsSpan();
-
-            // Act
-            var result = WebSocketScriptInjection.TryInjectLiveReloadScript(stream, input);
-
-            // Assert
-            Assert.True(result);
-            var output = Encoding.UTF8.GetString(stream.ToArray());
-            Assert.Equal(expected, output);
-        }
-
-        [Fact]
-        public void TryInjectLiveReloadScript_WithOffset()
-        {
-            // Arrange
-            var expected = $"</table>{WebSocketScriptInjection.InjectedScript}</body>";
-            var stream = new MemoryStream();
-            var input = Encoding.UTF8.GetBytes("unused</table></body>").AsSpan(6);
-
-            // Act
-            var result = WebSocketScriptInjection.TryInjectLiveReloadScript(stream, input);
-
-            // Assert
-            Assert.True(result);
-            var output = Encoding.UTF8.GetString(stream.ToArray());
-            Assert.Equal(expected, output);
-        }
+            new[] { "</body>" },
+            new[] { "</BoDy>" },
+            new[] { "</  BODY>" },
+            new[] { "</ bodY  >" },
+            new[] { "</\n\n Body\t >" },
+        };
     }
 }
