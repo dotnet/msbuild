48ec3e38787d014a12b1f36d5eb51b5b833caec2
Eric Erhardt <eric.erhardt@microsoft.com>
2022-09-08 16:59:44 -0500

    Remove Jab dependency (#27742)
    The DependencyInjection usage isn't necessary in ApiCompat. It can
    simplified to using Lazy<T> for singleton services, and just creating
    objects directly when necessary.
    
    This removes the dependency on an external library, and simplifies
    source-build, since we no longer need to build another repo.

---
+++ src/ApiCompat/Microsoft.DotNet.ApiCompat.Shared/ApiCompatServiceProvider.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using Microsoft.DotNet.ApiCompatibility;
+using Microsoft.DotNet.ApiCompatibility.Logging;
+using Microsoft.DotNet.ApiCompatibility.Rules;
+using Microsoft.DotNet.ApiCompatibility.Runner;
+
+namespace Microsoft.DotNet.ApiCompat
+{
+    internal sealed class ApiCompatServiceProvider
+    {
+        private readonly Lazy<ISuppressionEngine> _suppressionEngine;
+        private readonly Lazy<ICompatibilityLogger> _compatibilityLogger;
+        private readonly Lazy<IApiCompatRunner> _apiCompatRunner;
+
+        internal ApiCompatServiceProvider(Func<ISuppressionEngine, ICompatibilityLogger> logFactory,
+            Func<ISuppressionEngine> suppressionEngineFactory,
+            Func<ICompatibilityLogger, IRuleFactory> ruleFactory)
+        {
+            _suppressionEngine = new Lazy<ISuppressionEngine>(suppressionEngineFactory);
+            _compatibilityLogger = new Lazy<ICompatibilityLogger>(() => logFactory(SuppressionEngine));
+            _apiCompatRunner = new Lazy<IApiCompatRunner>(() =>
+                new ApiCompatRunner(CompatibilityLogger, SuppressionEngine, new ApiComparerFactory(ruleFactory(CompatibilityLogger)), new AssemblySymbolLoaderFactory()));
+        }
+
+        public ISuppressionEngine SuppressionEngine => _suppressionEngine.Value;
+        public ICompatibilityLogger CompatibilityLogger => _compatibilityLogger.Value;
+        public IApiCompatRunner ApiCompatRunner => _apiCompatRunner.Value;
+    }
+}
