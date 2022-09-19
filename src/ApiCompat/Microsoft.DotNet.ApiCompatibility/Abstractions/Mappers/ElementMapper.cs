0d8aea8a1cac35e67f81de240405e725410dfc8c
Viktor Hofer <viktor.hofer@microsoft.com>
2022-09-19 11:47:33 +0200

    Make mapping infrastructure unit testable, Ignore effectively sealed members and types and add tests (#27962)
    * Make mapping infrastructure unit testable
    
    Define interfaces for the mapping types in order for them to not depend
    on concrete implementations in order for them to be unit test-able.
    
    * Ignore effectively sealed members and types
    
    Don't map effectively sealed members and types so that individual rules
    don't have to special case them.
    
    * Add unit tests for mapper types and eff sealed case
    
    * Update SymbolExtensions.cs

--- src/ApiCompat/Microsoft.DotNet.ApiCompatibility/Abstractions/Mappers/ElementMapper.cs
+++ src/ApiCompat/Microsoft.DotNet.ApiCompatibility/Abstractions/Mappers/ElementMapper.cs
@@ -10,29 +10,23 @@ namespace Microsoft.DotNet.ApiCompatibility.Abstractions
     /// <summary>
     /// Class that represents a mapping in between two objects of type <see cref="T"/>.
     /// </summary>
-    public abstract class ElementMapper<T>
+    public abstract class ElementMapper<T> : IElementMapper<T>
     {
         private IEnumerable<CompatDifference>? _differences;
 
-        /// <summary>
-        /// Property representing the Left hand side of the mapping.
-        /// </summary>
+        /// <inheritdoc />
         public T? Left { get; private set; }
 
-        /// <summary>
-        /// Property representing the Right hand side element(s) of the mapping.
-        /// </summary>
+        /// <inheritdoc />
         public T?[] Right { get; private set; }
 
-        /// <summary>
-        /// The <see cref="MapperSettings"/> used to diff <see cref="Left"/> and <see cref="Right"/>.
-        /// </summary>
+        /// <inheritdoc />
         public MapperSettings Settings { get; }
 
         /// <summary>
         /// The rule runner to perform api comparison checks.
         /// </summary>
-        protected IRuleRunner RuleRunner { get; }
+        protected readonly IRuleRunner RuleRunner;
 
         /// <summary>
         /// Instantiates an object with the provided <see cref="ComparingSettings"/>.
@@ -40,8 +34,8 @@ namespace Microsoft.DotNet.ApiCompatibility.Abstractions
         /// <param name="settings">The settings used to diff the elements in the mapper.</param>
         /// <param name="rightSetSize">The number of elements in the right set to compare.</param>
         public ElementMapper(IRuleRunner ruleRunner,
-            MapperSettings settings = default,
-            int rightSetSize = 1)
+            MapperSettings settings,
+            int rightSetSize)
         {
             if (rightSetSize < 1)
                 throw new ArgumentOutOfRangeException(nameof(rightSetSize), Resources.ShouldBeGreaterThanZero);
@@ -51,12 +45,7 @@ namespace Microsoft.DotNet.ApiCompatibility.Abstractions
             Right = new T[rightSetSize];
         }
 
-        /// <summary>
-        /// Adds an element to the mapping given the <paramref name="side"/> and the <paramref name="setIndex"/>.
-        /// </summary>
-        /// <param name="element">The element to add to the mapping.</param>
-        /// <param name="side">Value representing the side of the mapping.</param>
-        /// <param name="setIndex">Value representing the index the element is added. Only used when adding to <see cref="ElementSide.Right"/>.</param>
+        /// <inheritdoc />
         public virtual void AddElement(T element, ElementSide side, int setIndex = 0)
         {
             if (side == ElementSide.Left)
@@ -72,12 +61,7 @@ namespace Microsoft.DotNet.ApiCompatibility.Abstractions
             }
         }
 
-        /// <summary>
-        /// Returns the element from the specified <see cref="ElementSide"/> and index.
-        /// </summary>
-        /// <param name="side">Value representing the side of the mapping.</param>
-        /// <param name="setIndex">Value representing the index the element is retrieved. Only used when adding to <see cref="ElementSide.Right"/>.</param>
-        /// <returns></returns>
+        /// <inheritdoc />
         public T? GetElement(ElementSide side, int setIndex)
         {
             if (side == ElementSide.Left)
@@ -88,12 +72,7 @@ namespace Microsoft.DotNet.ApiCompatibility.Abstractions
             return Right[setIndex];
         }
 
-        /// <summary>
-        /// Runs the rules found by the rule driver on the element mapper and returns a list of differences.
-        /// </summary>
-        /// <returns>A list containing the list of differences for each possible combination of
-        /// (<see cref="ElementMapper{T}.Left"/>, <see cref="ElementMapper{T}.Right"/>).
-        /// One list of <see cref="CompatDifference"/> per the number of right elements that the <see cref="ElementMapper{T}"/> contains.</returns>
+        /// <inheritdoc />
         public IEnumerable<CompatDifference> GetDifferences()
         {
             return _differences ??= RuleRunner.Run(this);
