05bd0b79abacda1686ad4a780be63b67822dd2b0
dmex <dmex04@gmail.com>
2022-09-20 12:53:53 +1000

    Remove check for numberOfCpus >= 32

--- src/Framework/NativeMethods.cs
+++ src/Framework/NativeMethods.cs
@@ -509,13 +509,7 @@ internal static class NativeMethods
         // .NET Core on Windows returns a core count limited to the current NUMA node
         //     https://github.com/dotnet/runtime/issues/29686
         // so always double-check it.
-        if (IsWindows
-#if NETFRAMEWORK
-            // .NET Framework calls Windows APIs that have a core count limit (32/64 depending on process bitness).
-            // So if we get a high core count on full framework, double-check it.
-            && (numberOfCpus >= 32)
-#endif
-            )
+        if (IsWindows)
         {
             var result = GetLogicalCoreCountOnWindows();
             if (result != -1)
