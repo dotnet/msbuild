0738d32cc87086dbced3e8d3d1896f2a35b275ce
Noah Gilson <noahgilson@microsoft.com>
2022-09-21 11:31:19 -0700

    Fix last remaining test

--- src/Tests/Microsoft.NET.Publish.Tests/GivenThatWeWantToPublishAHelloWorldProject.cs
+++ src/Tests/Microsoft.NET.Publish.Tests/GivenThatWeWantToPublishAHelloWorldProject.cs
@@ -579,7 +579,7 @@ public static class Program
             var publishCommand = new DotnetPublishCommand(Log, helloWorldAsset.TestRoot);
 
             publishCommand
-            .Execute("-f net7.0")
+            .Execute("-f", "net7.0")
             .Should()
             .Pass().And.NotHaveStdErr();
         }
