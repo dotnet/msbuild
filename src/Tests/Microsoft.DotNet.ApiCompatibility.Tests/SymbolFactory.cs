50de3add63fc8921c976730b699b41deaa5d2ca9
Jared Parsons <jaredpparsons@gmail.com>
2022-09-22 10:44:37 -0700

    Fixup

--- src/Tests/Microsoft.DotNet.ApiCompatibility.Tests/SymbolFactory.cs
+++ src/Tests/Microsoft.DotNet.ApiCompatibility.Tests/SymbolFactory.cs
@@ -7,7 +7,6 @@ using System.Collections.Immutable;
 using System.IO;
 using System.Linq;
 using System.Runtime.CompilerServices;
-using System.ServiceModel.Configuration;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.DotNet.ApiCompatibility.Abstractions;
