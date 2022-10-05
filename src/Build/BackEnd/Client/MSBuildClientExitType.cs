073f2ba7595e4b28a4326d9a5591e864b1898086
AR-May <67507805+AR-May@users.noreply.github.com>
2022-10-05 16:47:33 +0200

    Add the try-catch block for mutex connection timeout exception.

--- src/Build/BackEnd/Client/MSBuildClientExitType.cs
+++ src/Build/BackEnd/Client/MSBuildClientExitType.cs
@@ -24,6 +24,13 @@ namespace Microsoft.Build.Experimental
         /// The build stopped unexpectedly, for example,
         /// because a named pipe between the server and the client was unexpectedly closed.
         /// </summary>
-        Unexpected
+        Unexpected,
+        /// <summary>
+        /// The client is not able to identify the server state.
+        /// </summary>
+        /// <remarks>
+        /// This may happen when mutex that is regulating the server state throws.
+        /// </remarks>
+        UnknownServerState
     }
 }
