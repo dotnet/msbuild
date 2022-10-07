43c72ef04a47b8a0ebe264d34320386a0389cde4
Marc Paine <marcpop@microsoft.com>
2022-10-07 11:47:13 -0700

    Set an assembly attribute so we can add an installer test to check for it

--- src/Common/CompileOptions.cs
+++ src/Common/CompileOptions.cs
@@ -1,5 +1,10 @@
 // Copyright (c) .NET Foundation and contributors. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System.Reflection;
+
+#if MICROSOFT_ENABLE_TELEMETRY
+      [assembly: AssemblyMetadata("OfficialBuilder", "Microsoft")]
+#endif
 
 namespace Microsoft.DotNet.Cli
 {
