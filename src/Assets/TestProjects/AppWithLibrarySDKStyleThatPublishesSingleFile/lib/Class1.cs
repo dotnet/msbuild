a80d6b5fa1bca09e9938f1d10120d0c9df94cca4
Noah Gilson <noahgilson@microsoft.com>
2022-10-18 10:10:55 -0700

    Add opt out feature and tests

---
+++ src/Assets/TestProjects/AppWithLibrarySDKStyleThatPublishesSingleFile/lib/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace lib;
+public class Class1
+{
+
+}
---
+++ src/Tests/Microsoft.NET.Publish.Tests/GivenThatWeWantToPublishASingleFileLibrary.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using FluentAssertions;
+using Microsoft.NET.TestFramework;
+using Microsoft.NET.TestFramework.Assertions;
+using Microsoft.NET.TestFramework.Commands;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Microsoft.NET.Publish.Tests
+{
+    public class GivenThatWeWantToPublishASingleFileLibrary : SdkTest
+    {
+        public GivenThatWeWantToPublishASingleFileLibrary(ITestOutputHelper log) : base(log)
+        {
+        }
+
+        [Fact]
+        // Tests regression on https://github.com/dotnet/sdk/pull/28484
+        public void ItPublishesSuccessfullyWithRIDAndPublishSingleFileLibrary()
+        {
+            var testAsset = _testAssetsManager
+                     .CopyTestAsset("AppWithLibrarySDKStyleThatPublishesSingleFile")
+                     .WithSource();
+
+            var publishCommand = new PublishCommand(testAsset);
+            publishCommand.Execute()
+                    .Should()
+                    .Pass();
+        }
+
+    }
+
+}
