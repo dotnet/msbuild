adbcd3baa59b7b0f8b5dde346196617385f9377b
Forgind <Forgind@users.noreply.github.com>
2022-11-09 13:26:47 -0800

    Add tests for user's system configuration
    Currently includes verifying long paths are enabled and they can create symlinks. As more issues come up, we should add to this list.

---
+++ src/Framework.UnitTests/SystemSetup_Tests.cs
@@ -0,0 +1,43 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.IO;
+using Microsoft.Build.UnitTests;
+using Shouldly;
+using Xunit;
+
+
+namespace Microsoft.Build.Framework
+{
+    public sealed class SystemSetup_Tests
+    {
+        [Fact]
+        public void VerifyLongPaths()
+        {
+            NativeMethodsShared.MaxPath.ShouldBeGreaterThan(10000);
+        }
+
+#if NETCOREAPP
+        [Fact]
+        public void VerifySymLinksEnabled()
+        {
+            using TestEnvironment env = TestEnvironment.Create();
+            TransientTestFile file = env.CreateFile("fileName.tmp", "fileContents");
+            string path = Path.Combine(Path.GetTempPath(), "symLink");
+            try
+            {
+                string symLink = File.CreateSymbolicLink(path, file.Path).FullName;
+                string contents = File.ReadAllText(path);
+                contents.ShouldBe("fileContents");
+            }
+            finally
+            {
+                if (File.Exists(path))
+                {
+                    File.Delete(path);
+                }
+            }
+        }
+#endif
+    }
+}
