65dab9781cb345d90c258de627354f502ef768d5
Forgind <Forgind@users.noreply.github.com>
2022-11-14 16:56:41 -0800

    Add osVersion

--- src/Tasks.UnitTests/Copy_Tests.cs
+++ src/Tasks.UnitTests/Copy_Tests.cs
@@ -2397,6 +2397,7 @@ namespace Microsoft.Build.UnitTests
         [Fact]
         public void CopyToDestinationFolderWithSymbolicLinkCheck()
         {
+            Version osVersion = Environment.OSVersion.Version;
             if (NativeMethodsShared.IsWindows && osVersion.Major < 11 (osVersion.Major < 10 || osVersion.Build < 14972))
             {
                 // Symlink creation depends on having a high enough OS version on windows unless you explicitly enable the setting for
