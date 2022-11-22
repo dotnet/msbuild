903295b807417d23a2d86733b1652d2cda82cf8e
Forgind <Forgind@users.noreply.github.com>
2022-11-21 16:55:47 -0800

    Remove version check

--- src/Tasks.UnitTests/Copy_Tests.cs
+++ src/Tasks.UnitTests/Copy_Tests.cs
@@ -2397,14 +2397,6 @@ namespace Microsoft.Build.UnitTests
         [Fact]
         public void CopyToDestinationFolderWithSymbolicLinkCheck()
         {
-            Version osVersion = Environment.OSVersion.Version;
-            if (NativeMethodsShared.IsWindows && osVersion.Major < 11 && (osVersion.Major < 10 || osVersion.Build < 14972))
-            {
-                // Symlink creation depends on having a high enough OS version on windows unless you explicitly enable the setting for
-                // the current user or are running as admin. Skip this test.
-                return;
-            }
-
             string sourceFile = FileUtilities.GetTemporaryFile();
             string temp = Path.GetTempPath();
             string destFolder = Path.Combine(temp, "2A333ED756AF4dc392E728D0F864A398");
