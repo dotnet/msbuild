bc87b690bdf8d1c86645dcfc0462694c1d4c8cf2
Jenny Bai <v-jennybai@microsoft.com>
2022-11-24 23:34:54 -0800

    Remove wrong checks

--- src/Build/Evaluation/ProjectParser.cs
+++ src/Build/Evaluation/ProjectParser.cs
@@ -636,14 +636,10 @@ namespace Microsoft.Build.Construction
                             ProjectErrorUtilities.ThrowInvalidProject(onError.Location, "NodeMustBeLastUnderElement", XMakeElements.onError, XMakeElements.target, childElement.Name);
                         }
 
-                        if (childElement.ChildNodes.Count != 0)
+                        if (childElement.ChildNodes.Count == 1 && childElement.FirstChild.NodeType==XmlNodeType.Text)
                         {
-                            // If the element has inner text and no child elements, then this should be a property and throw invalid child element of <Target>
-                            var firstNode = childElement.FirstChild;
-                            if (firstNode.NodeType == XmlNodeType.Text && firstNode.ChildNodes.Count == 0)
-                            {
-                                ProjectXmlUtilities.ThrowProjectInvalidChildElement(childElement.Name, childElement.ParentNode.Name, childElement.Location);
-                            }
+                            // If the element has inner text and no other child elements except test, then this should be a property and throw invalid child element of <Target>
+                            ProjectXmlUtilities.ThrowProjectInvalidChildElement(childElement.Name, childElement.ParentNode.Name, childElement.Location);
                         }
 
                         child = ParseProjectTaskElement(childElement, target);
