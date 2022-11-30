831c29fd7a585ce7d2b5f9b4cf23358bc1e38b2f
Daniel Plaisted <daplaist@microsoft.com>
2022-11-30 13:52:16 -0500

    Fix test paths

--- src/Tasks/Microsoft.NET.Build.Tasks.UnitTests/GivenThatWeWantToGetDependenciesViaDesignTimeBuild.cs
+++ src/Tasks/Microsoft.NET.Build.Tasks.UnitTests/GivenThatWeWantToGetDependenciesViaDesignTimeBuild.cs
@@ -8,229 +8,32 @@ using System.Linq;
 using System.Reflection;
 using FluentAssertions;
 using Microsoft.Build.Framework;
+using Microsoft.NET.TestFramework;
 using Xunit;
+using Xunit.Abstractions;
 using static Microsoft.NET.Build.Tasks.ResolvePackageAssets;
 
 namespace Microsoft.NET.Build.Tasks.UnitTests
 {
-    public class GivenThatWeWantToGetDependenciesViaDesignTimeBuild
+    public class GivenThatWeWantToGetDependenciesViaDesignTimeBuild : SdkTest
     {
+        public GivenThatWeWantToGetDependenciesViaDesignTimeBuild(ITestOutputHelper log) : base(log)
+        {
+        }
+
         [Fact]
         public void ItShouldIgnoreAllDependenciesWithTypeNotEqualToPackageOrUnresolved()
         {
-            string projectAssetsJsonPath = Path.GetTempFileName();
-            string projectCacheAssetsJsonPath = Path.GetTempFileName();
+            var testRoot = _testAssetsManager.CreateTestDirectory().Path;
+            Log.WriteLine("Test root: " + testRoot);
+
+            string projectAssetsJsonPath = Path.Combine(testRoot, "project.assets.json");
+            string projectCacheAssetsJsonPath = Path.Combine(testRoot, "projectassets.cache");
             // project.assets.json
-            var assetsContent =
-"""
-{
-  "version" : 3,
-  "targets" : {
-    "net6.0" : {
-      "top.package1/1.0.0": {
-        "type": "package",
-        "dependencies": {
-          "dependent.package1": "1.0.0",
-          "dependent.package2": "1.0.0",
-        },
-        "compile": {
-          "lib/netstandard2.1/top.package1.dll": {
-            "related": ".xml"
-          }
-        },
-        "runtime": {
-          "lib/netstandard2.1/top.package1.dll": {
-            "related": ".xml"
-          }
-        }
-      },
-      "top.package2/1.0.0" : {
-        "type" : "unresolved",
-        "compile" : {
-            "lib/netstandard2.0/top.package2.dll" : {
-                "related" : ".pdb;.xml"
-            }
-        },
-        "runtime" : {
-            "lib/netstandard2.0/top.package2.dll" : {
-                "related": ".pdb;.xml"
-            }
-        }
-      },
-      "top.package3/1.0.0" : {
-          "type" : "unknown",
-          "dependencies": {
-              "dependent.package1" : "1.0.1"
-          },
-          "compile" : {
-              "lib/netstandard2.0/top.package3.dll" : {
-                  "related" : ".pdb;.xml"
-              }
-          },
-          "runtime" : {
-              "lib/netstandard2.0/top.package3.dll" : {
-                  "related" : ".pdb;.xml"
-               }
-          }
-      },
-      "dependent.package1/1.0.0": {
-        "type": "package",
-        "dependencies": {
-          "dependent.package3": "1.0.0"
-        },
-        "compile": {
-          "lib/netcoreapp3.1/_._": {
-            "related": ".xml"
-          }
-        },
-        "runtime": {
-          "lib/netcoreapp3.1/dependent.package1.dll": {
-            "related": ".xml"
-          }
-        }
-      },
-      "dependent.package2/1.0.0": {
-        "type": "package",
-        "compile": {
-          "lib/netstandard2.0/dependent.package2.dll": {
-            "related": ".xml"
-          }
-        },
-        "runtime": {
-          "lib/netstandard2.0/dependent.package2.dll": {
-            "related": ".xml"
-          }
-        }
-      },
-      "dependent.package3/1.0.0": {
-          "type": "package",
-          "compile": {
-          "lib/netstandard2.1/dependent.package3.dll": {
-              "related": ".xml"
-          }
-        },
-        "runtime": {
-          "lib/netstandard2.1/dependent.package3.dll": {
-              "related": ".xml"
-          }
-        }
-      }
-    }
-  },
-  "libraries" : {
-    "dependent.package1/1.0.0" : {
-        "sha512" : "xyz",
-        "type" : "package",
-        "path" : "dependent.package1/1.0.0",
-        "files" : [
-          "lib/net461/dependent.package1.dll",
-          "lib/net461/dependent.package1.xml"
-        ]
-    },
-    "dependent.package2/1.0.0" : {
-        "sha512" : "xyz",
-        "type" : "package",
-        "path" : "dependent.package2/1.0.0",
-        "files" : [
-          ".nupkg.metadata"
-        ]
-    },
-    "dependent.package3/1.0.0" : {
-        "sha512" : "xyz",
-        "type" : "package",
-        "path" : "dependent.package3/1.0.0",
-        "files" : [
-          "lib/net472/dependent.package3.dll",
-          "lib/net472/dependent.package3.xml"
-        ]
-    },
-    "top.package1/1.0.0" : {
-        "sha512" : "xyz",
-        "type" : "package",
-        "path" : "top.package1/1.0.0",
-        "files" : [
-            "lib/net20/top.package1.dll",
-            "lib/net20/top.package1.pdb",
-            "lib/net20/top.package1.xml"
-        ]
-    },
-    "top.package2/1.0.0" : {
-        "sha512" : "abc",
-        "type" : "unresolved",
-        "path" : "top.package2/1.0.0",
-        "files" : [
-            "lib/net45/top.package2.dll",
-            "lib/net45/top.package2.pdb",
-            "lib/net45/top.package2.xml"
-        ]
-    },
-    "top.package3/1.0.0" : {
-        "sha512" : "abc",
-        "type" : "unknown",
-        "path" : "top.package3/1.0.0",
-        "files" : [
-           ".nupkg.metadata",
-        ]
-    }
-  },
-  "projectFileDependencyGroups": {
-    "net6.0": [
-      "top.package1 >= 1.0.0",
-      "top.package2 >= 1.0.0",
-      "top.package3 >= 1.0.0"
-    ]
-  },
-  "packageFolders": {
-    "C:\\.nuget\\packages\\" : {}
-  },
-  "project" : {
-      "version" : "1.0.0",
-      "restore": {
-          "projectUniqueName": "C:\\projDir1\\projDir2\\proj.csproj",
-          "projectName": "proj",
-          "projectPath": "C:\\projDir1\\projDir2\\proj.csproj",
-          "packagesPath": "C:\\.nuget\\packages\\",
-          "outputPath": "C:\\projDir1\\projDir2\\obj\\",
-          "projectStyle": "PackageReference",
-          "fallbackFolders": [
-            "C:\\fallbackDir\\NuGetPackages"
-          ],
-          "configFilePaths": [
-            "C:\\configDir\\NuGet.Config",
-            "C:\\configDir2\\Microsoft.VisualStudio.FallbackLocation.config",
-            "C:\\configDir3\\Microsoft.VisualStudio.Offline.config"
-          ],
-          "originalTargetFrameworks": [
-            "net6.0"
-          ],
-          "frameworks": {
-            "net6.0": {
-              "targetAlias": "net6.0",
-              "projectReferences": {}
-            }
-          },
-          "warningProperties": {
-            "warnAsError": [
-              "NU1605"
-            ]
-      }
-    },
-      "frameworks" : {
-          "net6.0": {
-              "targetAlias" : "net6.0",
-              "dependencies" : {
-                  "top.package1" : {
-                      "target" : "Package",
-                      "version" : "[1.0.0, )"
-                   }
-              }
-          }
-      }
-  }
-}
-""";
+            var assetsContent = CreateBasicProjectAssetsFile(testRoot, package2Type: "unresolved", package3Type: "unknown");
+
             File.WriteAllText(projectAssetsJsonPath, assetsContent);
-            var task = InitializeTask(out _);
+            var task = InitializeTask(testRoot, out _);
             task.ProjectAssetsFile = projectAssetsJsonPath;
             task.ProjectAssetsCacheFile = projectCacheAssetsJsonPath;
             task.TargetFramework = "net6.0";
@@ -254,11 +57,14 @@ namespace Microsoft.NET.Build.Tasks.UnitTests
         [Fact]
         public void ItShouldIdentifyDefaultImplicitPackages()
         {
-            string projectAssetsJsonPath = Path.GetTempFileName();
-            string projectCacheAssetsJsonPath = Path.GetTempFileName();
+            var testRoot = _testAssetsManager.CreateTestDirectory().Path;
+            Log.WriteLine("Test root: " + testRoot);
+
+            string projectAssetsJsonPath = Path.Combine(testRoot, "project.assets.json");
+            string projectCacheAssetsJsonPath = Path.Combine(testRoot, "projectassets.cache");
             // project.assets.json
-            File.WriteAllText(projectAssetsJsonPath, CreateBasicProjectAssetsFile());
-            var task = InitializeTask(out _);
+            File.WriteAllText(projectAssetsJsonPath, CreateBasicProjectAssetsFile(testRoot));
+            var task = InitializeTask(testRoot, out _);
             task.ProjectAssetsFile = projectAssetsJsonPath;
             task.ProjectAssetsCacheFile = projectCacheAssetsJsonPath;
             task.TargetFramework = "net6.0";
@@ -286,8 +92,11 @@ namespace Microsoft.NET.Build.Tasks.UnitTests
         [Fact]
         public void ItShouldOnlyReturnPackagesInTheSpecifiedTarget()
         {
-            string projectAssetsJsonPath = Path.GetTempFileName();
-            string projectCacheAssetsJsonPath = Path.GetTempFileName();
+            var testRoot = _testAssetsManager.CreateTestDirectory().Path;
+            Log.WriteLine("Test root: " + testRoot);
+
+            string projectAssetsJsonPath = Path.Combine(testRoot, "project.assets.json");
+            string projectCacheAssetsJsonPath = Path.Combine(testRoot, "projectassets.cache");
             // project.assets.json
             var assetsContent =
 """
@@ -530,8 +339,10 @@ namespace Microsoft.NET.Build.Tasks.UnitTests
   }
 }
 """;
+            assetsContent = assetsContent.Replace(@"C:\\.nuget", $@"{testRoot.Replace("\\", "\\\\")}\\.nuget"); ;
+
             File.WriteAllText(projectAssetsJsonPath, assetsContent);
-            var task = InitializeTask(out _);
+            var task = InitializeTask(testRoot, out _);
             task.ProjectAssetsFile = projectAssetsJsonPath;
             task.ProjectAssetsCacheFile = projectCacheAssetsJsonPath;
             // Set target to verify
@@ -548,11 +359,14 @@ namespace Microsoft.NET.Build.Tasks.UnitTests
         [Fact]
         public void ItShouldOnlyReturnTopLevelPackages()
         {
-            string projectAssetsJsonPath = Path.GetTempFileName();
-            string projectCacheAssetsJsonPath = Path.GetTempFileName();
+            var testRoot = _testAssetsManager.CreateTestDirectory().Path;
+            Log.WriteLine("Test root: " + testRoot);
+
+            string projectAssetsJsonPath = Path.Combine(testRoot, "project.assets.json");
+            string projectCacheAssetsJsonPath = Path.Combine(testRoot, "projectassets.cache");
             // project.assets.json
-            File.WriteAllText(projectAssetsJsonPath, CreateBasicProjectAssetsFile());
-            var task = InitializeTask(out _);
+            File.WriteAllText(projectAssetsJsonPath, CreateBasicProjectAssetsFile(testRoot));
+            var task = InitializeTask(testRoot, out _);
             task.ProjectAssetsFile = projectAssetsJsonPath;
             task.ProjectAssetsCacheFile = projectCacheAssetsJsonPath;
             task.TargetFramework = "net6.0";
@@ -571,9 +385,9 @@ namespace Microsoft.NET.Build.Tasks.UnitTests
             Assert.Equal("top.package3/1.0.0", item3.ItemSpec);
         }
 
-        private string CreateBasicProjectAssetsFile()
+        private string CreateBasicProjectAssetsFile(string testRoot, string package2Type = "package", string package3Type = "package")
         {
-            return
+            var json =
 """
 {
   "version" : 3,
@@ -597,7 +411,7 @@ namespace Microsoft.NET.Build.Tasks.UnitTests
         }
       },
       "top.package2/1.0.0" : {
-        "type" : "package",
+        "type" : "PACKAGE2_TYPE",
         "compile" : {
             "lib/netstandard2.0/top.package2.dll" : {
                 "related" : ".pdb;.xml"
@@ -610,7 +424,7 @@ namespace Microsoft.NET.Build.Tasks.UnitTests
         }
       },
       "top.package3/1.0.0" : {
-          "type" : "package",
+          "type" : "PACKAGE3_TYPE",
           "dependencies": {
               "dependent.package1" : "1.0.1"
           },
@@ -708,7 +522,7 @@ namespace Microsoft.NET.Build.Tasks.UnitTests
     },
     "top.package2/1.0.0" : {
         "sha512" : "abc",
-        "type" : "package",
+        "type" : "PACKAGE2_TYPE",
         "path" : "top.package2/1.0.0",
         "files" : [
             "lib/net45/top.package2.dll",
@@ -718,7 +532,7 @@ namespace Microsoft.NET.Build.Tasks.UnitTests
     },
     "top.package3/1.0.0" : {
         "sha512" : "abc",
-        "type" : "package",
+        "type" : "PACKAGE3_TYPE",
         "path" : "top.package3/1.0.0",
         "files" : [
            ".nupkg.metadata",
@@ -781,8 +595,11 @@ namespace Microsoft.NET.Build.Tasks.UnitTests
   }
 }
 """;
+            return json.Replace("PACKAGE2_TYPE", package2Type)
+                       .Replace("PACKAGE3_TYPE", package3Type)
+                       .Replace(@"C:\\.nuget", $@"{testRoot.Replace("\\", "\\\\")}\\.nuget");
         }
-        private ResolvePackageAssets InitializeTask(out IEnumerable<PropertyInfo> inputProperties)
+        private ResolvePackageAssets InitializeTask(string testRoot, out IEnumerable<PropertyInfo> inputProperties)
         {
             inputProperties = typeof(ResolvePackageAssets)
                 .GetProperties(BindingFlags.DeclaredOnly | BindingFlags.Instance | BindingFlags.Public)
@@ -807,31 +624,31 @@ namespace Microsoft.NET.Build.Tasks.UnitTests
 
             task.BuildEngine = new MockBuildEngine();
 
-            CreateFolders();
+            CreateFolders(testRoot);
 
             return task;
         }
 
-        private void CreateFolders()
+        private void CreateFolders(string testRoot)
         {
-            Directory.CreateDirectory("C:\\.nuget\\packages\\top.package1\\1.0.0\\");
-            Directory.CreateDirectory("C:\\.nuget\\packages\\top.package2\\1.0.0\\");
-            Directory.CreateDirectory("C:\\.nuget\\packages\\top.package3\\1.0.0\\");
-            Directory.CreateDirectory("C:\\.nuget\\packages\\dependent.package1\\1.0.0\\");
-            Directory.CreateDirectory("C:\\.nuget\\packages\\dependent.package2\\1.0.0");
-            Directory.CreateDirectory("C:\\.nuget\\packages\\dependent.package3\\1.0.0\\");
-            using (File.Create("C:\\.nuget\\packages\\top.package1\\1.0.0\\.nupkg.metadata")) { }
-            using (File.Create("C:\\.nuget\\packages\\top.package1\\1.0.0\\top.package1.1.0.0.nupkg.sha512")) { }
-            using (File.Create("C:\\.nuget\\packages\\top.package2\\1.0.0\\.nupkg.metadata")) { }
-            using (File.Create("C:\\.nuget\\packages\\top.package2\\1.0.0\\top.package2.1.0.0.nupkg.sha512")) { }
-            using (File.Create("C:\\.nuget\\packages\\top.package3\\1.0.0\\.nupkg.metadata")) { }
-            using (File.Create("C:\\.nuget\\packages\\top.package3\\1.0.0\\top.package3.1.0.0.nupkg.sha512")) { }
-            using (File.Create("C:\\.nuget\\packages\\dependent.package1\\1.0.0\\.nupkg.metadata")) { }
-            using (File.Create("C:\\.nuget\\packages\\dependent.package1\\1.0.0\\dependent.package1.10.0.nupkg.sha512")) { }
-            using (File.Create("C:\\.nuget\\packages\\dependent.package2\\1.0.0\\.nupkg.metadata")) { }
-            using (File.Create("C:\\.nuget\\packages\\dependent.package2\\1.0.0\\dependent.package2.1.0.0.nupkg.sha512")) { }
-            using (File.Create("C:\\.nuget\\packages\\dependent.package3\\1.0.0\\.nupkg.metadata")) { }
-            using (File.Create("C:\\.nuget\\packages\\dependent.package3\\1.0.0\\dependent.package3.1.0.0.nupkg.sha512")) { }
+            Directory.CreateDirectory($@"{testRoot}\.nuget\packages\top.package1\1.0.0\");
+            Directory.CreateDirectory($@"{testRoot}\.nuget\packages\top.package2\1.0.0\");
+            Directory.CreateDirectory($@"{testRoot}\.nuget\packages\top.package3\1.0.0\");
+            Directory.CreateDirectory($@"{testRoot}\.nuget\packages\dependent.package1\1.0.0\");
+            Directory.CreateDirectory($@"{testRoot}\.nuget\packages\dependent.package2\1.0.0\");
+            Directory.CreateDirectory($@"{testRoot}\.nuget\packages\dependent.package3\1.0.0\");
+            using (File.Create($@"{testRoot}\.nuget\packages\top.package1\1.0.0\.nupkg.metadata")) { }
+            using (File.Create($@"{testRoot}\.nuget\packages\top.package1\1.0.0\top.package1.1.0.0.nupkg.sha512")) { }
+            using (File.Create($@"{testRoot}\.nuget\packages\top.package2\1.0.0\.nupkg.metadata")) { }
+            using (File.Create($@"{testRoot}\.nuget\packages\top.package2\1.0.0\top.package2.1.0.0.nupkg.sha512")) { }
+            using (File.Create($@"{testRoot}\.nuget\packages\top.package3\1.0.0\.nupkg.metadata")) { }
+            using (File.Create($@"{testRoot}\.nuget\packages\top.package3\1.0.0\top.package3.1.0.0.nupkg.sha512")) { }
+            using (File.Create($@"{testRoot}\.nuget\packages\dependent.package1\1.0.0\.nupkg.metadata")) { }
+            using (File.Create($@"{testRoot}\.nuget\packages\dependent.package1\1.0.0\dependent.package1.10.0.nupkg.sha512")) { }
+            using (File.Create($@"{testRoot}\.nuget\packages\dependent.package2\1.0.0\.nupkg.metadata")) { }
+            using (File.Create($@"{testRoot}\.nuget\packages\dependent.package2\1.0.0\dependent.package2.1.0.0.nupkg.sha512")) { }
+            using (File.Create($@"{testRoot}\.nuget\packages\dependent.package3\1.0.0\.nupkg.metadata")) { }
+            using (File.Create($@"{testRoot}\.nuget\packages\dependent.package3\1.0.0\dependent.package3.1.0.0.nupkg.sha512")) { }
 
         }
     }
