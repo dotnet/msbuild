be3728ce3360f54e1cf5f7b7b3110bce8bf94dd4
Noah Gilson <noahgilson@microsoft.com>
2022-12-19 16:51:30 -0800

    [WIP] Add test infra code to create test solutions

---
+++ src/Tests/Microsoft.NET.Publish.Tests/GivenThatWeWantToTestAMultitargetedSolutionWithPublishReleaseOrPackRelease.cs
@@ -0,0 +1,106 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Xml.Linq;
+using Microsoft.DotNet.Cli;
+using Microsoft.DotNet.Cli.Utils;
+using Microsoft.Extensions.DependencyModel;
+using Microsoft.NET.Build.Tasks;
+using Microsoft.NET.TestFramework;
+using Microsoft.NET.TestFramework.Assertions;
+using Microsoft.NET.TestFramework.Commands;
+using Microsoft.NET.TestFramework.ProjectConstruction;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Microsoft.NET.Publish.Tests
+{
+
+    public class GivenThatWeWantToTestAMultitargetedSolutionWithPublishReleaseOrPackRelease : SdkTest
+    {
+
+        public GivenThatWeWantToTestAMultitargetedSolutionWithPublishReleaseOrPackRelease(ITestOutputHelper log) : base(log)
+        {
+
+        }
+
+        internal Tuple<TestSolution, List<TestProject>> Setup(ITestOutputHelper log, string firstProjTfm, string secondProjTfm, string PReleaseProperty, string firstProjPReleaseValue, string secondProjPReleaseValue, [CallerMemberName] string testPath = "")
+        {
+            // Project Setup
+            List<TestProject> testProjects = new List<TestProject>();
+            var _testProject = new TestProject("TestProject")
+            {
+                TargetFrameworks = firstProjTfm,
+                IsExe = true
+            };
+            _testProject.RecordProperties("Configuration", "Optimize", PReleaseProperty);
+            _testProject.AdditionalProperties[PReleaseProperty] = firstProjPReleaseValue;
+            var mainProject = _testAssetsManager.CreateTestProject(_testProject, testPath);
+
+            var _referencedProject = new TestProject("ReferencedProject")
+            {
+                TargetFrameworks = secondProjTfm,
+                IsExe = false
+            };
+            _referencedProject.RecordProperties("Configuration", "Optimize", PReleaseProperty);
+            _testProject.AdditionalProperties[PReleaseProperty] = secondProjPReleaseValue;
+            var secondProject = _testAssetsManager.CreateTestProject(_referencedProject, testPath);
+
+            List<TestAsset> projects = new List<TestAsset> { mainProject, secondProject };
+
+            // Solution Setup
+            var sln = new TestSolution(log, testPath, projects);
+            testProjects.Add(_testProject);
+            testProjects.Add(_referencedProject);
+            return new Tuple<TestSolution, List<TestProject>>(sln, testProjects);
+        }
+
+        [Theory]
+        ["PublishRelease"]
+        ["PackRelease"]
+        public void ItUsesReleaseWithNet8PlusAndNet7WhereNoneDefineTheReleaseProperty(string releaseProperty)
+        {
+            var solutionAndProjects = Setup(Log, "net7.0", ToolsetInfo.CurrentTargetFramework, releaseProperty, "", "");
+            var sln = solutionAndProjects.Item1;
+            var testProjects = solutionAndProjects.Item2;
+
+            var dotnetCommand = new DotnetCommand(Log, releaseProperty == "PublishRelease" ? "publish" : "pack");
+            dotnetCommand.Execute(sln.SolutionPath)
+                .Should()
+                .Pass();
+
+            // Check properties
+
+        }
+
+        [Theory]
+        ["PublishRelease"]
+        ["PackRelease"]
+        public void ItUsesDebugIfNet8PlusDefinesFalseAndNet7DefinesNothing(string releaseProperty)
+        {
+
+        }
+
+        [Theory]
+        ["PublishRelease"]
+        ["PackRelease"]
+        public void ItFailsIfNet7DefinesReleasePropertyFalseButNet8PlusDefinesNone(string releaseProperty)
+        {
+
+        }
+
+        [Theory]
+        ["PublishRelease"]
+        ["PackRelease"]
+        public void ItUsesReleaseIfNet7DefinesReleasePropertyAndNet8PlusDfinesNothing(string releaseProperty)
+        {
+
+        }
+    }
+}
---
+++ src/Tests/Microsoft.NET.TestFramework/ProjectConstruction/TestSolution.cs
@@ -0,0 +1,66 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+//
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.NET.TestFramework.Commands;
+using Xunit.Abstractions;
+using Xunit.Sdk;
+
+namespace Microsoft.NET.TestFramework.ProjectConstruction
+{
+    /// <summary>
+    /// Create an on-disk solution for testing that has given projects.
+    /// This has dependencies on the functionality of .NET new and .NET sln add; if those are broken, tests revolving around solutions will also break.
+    /// </summary>
+    public class TestSolution
+    {
+        public TestSolution(ITestOutputHelper log, string newSolutionPath, List<TestAsset> solutionProjects, [CallerMemberName] string name = null)
+        {
+            Name = name;
+            SolutionPath = Path.Combine(newSolutionPath, name, ".sln");
+            ProjectPaths = new List<string>();
+
+            var slnCreator = new DotnetNewCommand(log, "sln", "-o", $"{newSolutionPath}", "-n", $"{name}");
+            slnCreator.Execute();
+
+            foreach (var project in solutionProjects)
+            {
+                var slnProjectAdder = new DotnetCommand(log, "sln", $"{SolutionPath}", "add", project.Path);
+                slnProjectAdder.Execute();
+                ProjectPaths.Add(project.Path);
+                Projects.Add(project);
+            }
+        }
+
+        /// <summary>
+        /// The FULL path to the newly created, on-disk solution, including the .sln file.
+        /// </summary>
+        public string SolutionPath { get; set; }
+
+        /// <summary>
+        /// The delegated or generated name of the solution.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// A List of the paths of projects that were initially added to the solution.
+        /// </summary>
+        public List<string> ProjectPaths { get; set; }
+
+        internal List<TestAsset> Projects { get; set; }
+
+        public List<string> ProjectProperties()
+        {
+            var properties = testProject.GetPropertyValues(testAsset.TestRoot, targetFramework: targetFramework);
+
+        }
+
+    }
+}
