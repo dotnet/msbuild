6728025bf18cdd17877ccbfd49f8bf4191528023
Jan Krivanek <jankrivanek@microsoft.com>
2022-12-23 12:01:44 +0100

    Adjust tests #2

--- src/Assets/TestPackages/dotnet-new/test_templates/TemplateWithConditionalParameters/Test.cs
+++ src/Assets/TestPackages/dotnet-new/test_templates/TemplateWithConditionalParameters/Test.cs
@@ -2,10 +2,26 @@
 // value of paramA: placeholderA
 // value of paramB: placeholderB
 
+//#if( A_enabled  )
+// paramA is enabled
+//#else
+// paramA is disabled
+//#endif
+
 //#if( paramA )
-// A is enabled
+// paramA is evaluated as true
+//#else
+// paramA is evaluated as false
+//#endif
+
+//#if( B_enabled  )
+// paramB is enabled
+//#else
+// paramB is disabled
 //#endif
 
 //#if( paramB )
-// B is enabled
+// paramB is evaluated as true
+//#else
+// paramB is evaluated as false
 //#endif
