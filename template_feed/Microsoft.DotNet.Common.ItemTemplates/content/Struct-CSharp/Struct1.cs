bb0e8b152664d8a569ac6d9a82850c6a02dd9119
YuliiaKovalova <95473390+YuliiaKovalova@users.noreply.github.com>
2022-12-26 08:34:27 +0100

    add C# class templates (#29655)
    Co-authored-by: v-wuzhai <46013274+v-wuzhai@users.noreply.github.com>

---
+++ template_feed/Microsoft.DotNet.Common.ItemTemplates/content/Struct-CSharp/Struct1.cs
@@ -0,0 +1,19 @@
+﻿#if (ImplicitUsings != "enable")
+using System;
+
+#endif
+#if (csharpFeature_FileScopedNamespaces)
+namespace Company.ClassLibrary1;
+public struct Struct1
+{
+
+}
+#else
+namespace Company.ClassLibrary1
+{
+    public struct Struct1
+    {
+
+    }
+}
+#endif
