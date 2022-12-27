e11b9a8ce556b81f70aba50c685587e58dddd06d
stan-sz <37585349+stan-sz@users.noreply.github.com>
2022-12-27 09:31:37 +0000

    Follow up

--- src/Build/Logging/FancyLogger/FancyLoggerNode.cs
+++ src/Build/Logging/FancyLogger/FancyLoggerNode.cs
@@ -2,12 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 //
 
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.Build.Framework;
 
 namespace Microsoft.Build.Logging.FancyLogger
 { 
