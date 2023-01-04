9ec16b563ac9149405e8a75b1ab20fc67754f89f
YuliiaKovalova <95473390+YuliiaKovalova@users.noreply.github.com>
2023-01-04 11:29:22 +0100

    fix formatting in CliTemplateInfo.cs

--- src/Cli/Microsoft.TemplateEngine.Cli/CliTemplateInfo.cs
+++ src/Cli/Microsoft.TemplateEngine.Cli/CliTemplateInfo.cs
@@ -69,7 +69,7 @@ namespace Microsoft.TemplateEngine.Cli
         public string? HostConfigPlace => _templateInfo.HostConfigPlace;
 
         public string? ThirdPartyNotices => _templateInfo.ThirdPartyNotices;
-        
+
         public bool PreferDefaultName => _templateInfo.PreferDefaultName;
 
         public IReadOnlyDictionary<string, IBaselineInfo> BaselineInfo => _templateInfo.BaselineInfo;
