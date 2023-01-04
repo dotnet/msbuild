ee6ec1c0dad0887227522f016b4e888ae52f1548
YuliiaKovalova <95473390+YuliiaKovalova@users.noreply.github.com>
2023-01-04 16:13:35 +0100

    cover template samples with tests + fix floating issue with test run (#29716)

---
+++ src/Tests/dotnet-new.Tests/Approvals/sample05.multi-project.includetest=true.verified/sample05/sample05.Test/MyTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Xunit;
+using sample05;
+
+namespace sample05.Test
+{
+    public class sample05_UnitTest
+    {
+        [Fact]
+        public void sample05_Test()
+        {
+            var name = Sample.GetName();
+            Assert.Equal("Console and unit test demo",name);
+        }
+    }
+}
