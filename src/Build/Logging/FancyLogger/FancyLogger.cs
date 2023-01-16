6b6879045dfe421da2a79816bef6688eed4c6075
Eduardo Villalpando Mello <t-eduardov@microsoft.com>
2023-01-15 22:32:55 -0800

    [FancyLogger] Clear screen on ctrl-c (#8310)
    Context
    Previously, when using CtrlC with the FancyLogger, the process would end abruptly, causing unexpected behavior such as rendered elements not erasing.
    
    Changes Made
    Added new Console.CancelKeyPress event handler
    
    Testing
    Notes
    Waiting for #8284 to show summary at the end

--- src/Build/Logging/FancyLogger/FancyLogger.cs
+++ src/Build/Logging/FancyLogger/FancyLogger.cs
@@ -43,6 +43,8 @@ namespace Microsoft.Build.Logging.FancyLogger
             eventSource.MessageRaised += new BuildMessageEventHandler(eventSource_MessageRaised);
             eventSource.WarningRaised += new BuildWarningEventHandler(eventSource_WarningRaised);
             eventSource.ErrorRaised += new BuildErrorEventHandler(eventSource_ErrorRaised);
+            // Cancelled
+            Console.CancelKeyPress += new ConsoleCancelEventHandler(console_CancelKeyPressed);
             // Initialize FancyLoggerBuffer
             FancyLoggerBuffer.Initialize();
         }
@@ -145,6 +147,11 @@ namespace Microsoft.Build.Logging.FancyLogger
             node.Log();
         }
 
+        void console_CancelKeyPressed(object? sender, ConsoleCancelEventArgs eventArgs)
+        {
+            // Shutdown logger
+            Shutdown();
+        }
 
         public void Shutdown()
         {
