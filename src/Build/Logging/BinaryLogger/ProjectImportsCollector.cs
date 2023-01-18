98924b86bbc19aff3360f9cc3a2922df6a5c5d6a
Jan Krivanek <krivanek.j@hotmail.com>
2023-01-18 07:36:09 +0100

    Handle symlinks embedding into binlogs - approach #2 (#8306)
    Fixes #6773
    
    Context
    Supersedes #8213 and #8282
    Symlinked files were not embedded into binlog - previous solution was too much focused on symlinks and hence requried nontrivial code to properly distinguish aymlinks with available contnet.
    
    Alternate approach - proceed with adding file as soon as it has available content
    
    Testing
    Preexisting unit test is excercising the scenario. Added a case for empty file - that still should not be added.

--- src/Build/Logging/BinaryLogger/ProjectImportsCollector.cs
+++ src/Build/Logging/BinaryLogger/ProjectImportsCollector.cs
@@ -130,7 +130,7 @@ namespace Microsoft.Build.Logging
                 return;
             }
 
-            if (!NativeMethodsShared.ExistAndHasContent(filePath))
+            if (!File.Exists(filePath))
             {
                 _processedFiles.Add(filePath);
                 return;
@@ -144,8 +144,8 @@ namespace Microsoft.Build.Logging
                 return;
             }
 
-            using Stream entryStream = OpenArchiveEntry(filePath);
             using FileStream content = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.Read | FileShare.Delete);
+            using Stream entryStream = OpenArchiveEntry(filePath);
             content.CopyTo(entryStream);
         }
 
