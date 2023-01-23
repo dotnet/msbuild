45382a4dc2a5726c4ef2214183d644bab3dbd116
Roman Konecny <rokonecn@microsoft.com>
2023-01-23 22:41:08 +0100

    DumpExceptionToFileShouldWriteInTempPathByDefault investigation...

--- src/Build.UnitTests/BackEnd/DebugUtils_tests.cs
+++ src/Build.UnitTests/BackEnd/DebugUtils_tests.cs
@@ -1,23 +1,33 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+
+
+#nullable disable
+
 using System;
 using System.IO;
 using System.Linq;
 using Microsoft.Build.Shared;
+using Microsoft.Build.Shared.Debugging;
 using Shouldly;
 using Xunit;
-
-#nullable disable
-
+using Xunit.Abstractions;
 namespace Microsoft.Build.UnitTests
 {
     public class DebugUtils_Tests
     {
+        private readonly ITestOutputHelper _testOutput;
+
+        public DebugUtils_Tests(ITestOutputHelper testOutput)
+        {
+            _testOutput = testOutput;
+        }
+
         [Fact]
         public void DumpExceptionToFileShouldWriteInTempPathByDefault()
         {
-            Directory.GetFiles(Path.GetTempPath(), "MSBuild_*failure.txt").ShouldBeEmpty();
+            var exceptionFilesBefore = Directory.GetFiles(FileUtilities.TempFileDirectory, "MSBuild_*failure.txt");
 
             string[] exceptionFiles = null;
 
@@ -28,10 +38,14 @@ namespace Microsoft.Build.UnitTests
             }
             finally
             {
-                exceptionFiles.ShouldNotBeNull();
-                exceptionFiles.ShouldHaveSingleItem();
+                _testOutput.WriteLine($"DebugUtils.DebugPath: {DebugUtils.DebugPath}");
+                _testOutput.WriteLine($"Environment.GetEnvironmentVariable(\"MSBUILDDEBUGPATH\"): {Environment.GetEnvironmentVariable("MSBUILDDEBUGPATH")}");
 
-                var exceptionFile = exceptionFiles.First();
+                exceptionFilesBefore.ShouldNotBeNull();
+                exceptionFiles.ShouldNotBeNull();
+                (exceptionFiles.Length - exceptionFilesBefore.Length).ShouldBe(1);
+
+                var exceptionFile = exceptionFiles.Except(exceptionFilesBefore).Single();
                 File.ReadAllText(exceptionFile).ShouldContain("hello world");
                 File.Delete(exceptionFile);
             }
