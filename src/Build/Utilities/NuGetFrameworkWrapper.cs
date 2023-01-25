8154a2415cea22bea9bc3d6e27acaf7ed56f48b4
Matt Mitchell <mmitche@microsoft.com>
2023-01-25 10:00:06 -0800

    Fix usings

--- src/Build/Utilities/NuGetFrameworkWrapper.cs
+++ src/Build/Utilities/NuGetFrameworkWrapper.cs
@@ -2,10 +2,11 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
-using System.IO;
-using System.Reflection;
-using System.Linq;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
 
 using Microsoft.Build.Framework;
 using Microsoft.Build.Shared;
