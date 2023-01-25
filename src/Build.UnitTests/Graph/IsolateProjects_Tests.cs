d57b052f61a378fe73e6e93c04f1f0f62f8ced51
Roman Konecny <rokonecn@microsoft.com>
2023-01-25 12:42:45 +0100

    Revert test changes

--- src/Build.UnitTests/Graph/IsolateProjects_Tests.cs
+++ src/Build.UnitTests/Graph/IsolateProjects_Tests.cs
@@ -17,7 +17,7 @@ using Xunit.Abstractions;
 
 namespace Microsoft.Build.Graph.UnitTests
 {
-    public class IsolateProjectsTests : IDisposable, IClassFixture<IsolateProjectsTests.IsolateProjectsClassFixture>
+    public class IsolateProjectsTests : IDisposable
     {
         private readonly string _project = @"
                 <Project DefaultTargets='BuildSelf'>
@@ -133,6 +133,13 @@ BuildEngine5.BuildProjectFilesInParallel(
             _testOutput = testOutput;
             _env = TestEnvironment.Create(_testOutput, ignoreBuildErrorFiles: true);
 
+            if (NativeMethodsShared.IsOSX)
+            {
+                // OSX links /var into /private, which makes Path.GetTempPath() to return "/var..." but Directory.GetCurrentDirectory to return "/private/var..."
+                // this discrepancy fails the msbuild undeclared reference enforcements due to failed path equality checks
+                _env.SetTempPath(Path.Combine(Directory.GetCurrentDirectory(), Guid.NewGuid().ToString("N")), deleteTempDirectory: true);
+            }
+
             // todo investigate why out of proc builds fail on macos https://github.com/dotnet/msbuild/issues/3915
             var disableInProcNode = !NativeMethodsShared.IsOSX;
 
@@ -148,8 +155,6 @@ BuildEngine5.BuildProjectFilesInParallel(
         {
             _env.Dispose();
         }
-
-
 
         [Theory]
         [InlineData(BuildResultCode.Success, new string[] { })]
@@ -482,28 +487,6 @@ BuildEngine5.BuildProjectFilesInParallel(
                 buildManagerSession.Logger.ErrorCount.ShouldBe(0);
                 // twice for the initial target, once for A, once for DefaultTarget
                 buildManagerSession.Logger.AssertMessageCount("Previously built successfully", 4);
-            }
-        }
-
-        internal class IsolateProjectsClassFixture : IDisposable
-        {
-            private readonly TestEnvironment _classFixtureEnv;
-
-            public IsolateProjectsClassFixture()
-            {
-                _classFixtureEnv = TestEnvironment.Create(output: null, ignoreBuildErrorFiles: true);
-                if (NativeMethodsShared.IsOSX)
-                {
-                    // OSX links /var into /private, which makes Path.GetTempPath() to return "/var..." but Directory.GetCurrentDirectory to return "/private/var..."
-                    // this discrepancy fails the msbuild undeclared reference enforcements due to failed path equality checks
-                    var tempFolder = _classFixtureEnv.CreateFolder(Path.Combine(Directory.GetCurrentDirectory(), Guid.NewGuid().ToString("N")));
-                    _classFixtureEnv.SetTempPath(tempFolder.Path);
-                }
-            }
-
-            public void Dispose()
-            {
-                _classFixtureEnv.Dispose();
             }
         }
     }
