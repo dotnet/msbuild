c0ce586e224bdcb7c36bf41a34d21e4e188f27a8
Noah Gilson <noahgilson@microsoft.com>
2023-01-26 09:17:06 -0800

    Update warning and remove old warning
    Remove the old self-contained warning as it would be redundant with the breaking change warning and only occurred with -r.
    
    Co-authored-by: richlander <rlander@microsoft.com>

--- src/Tests/dotnet-build.Tests/GivenDotnetBuildBuildsCsproj.cs
+++ src/Tests/dotnet-build.Tests/GivenDotnetBuildBuildsCsproj.cs
@@ -179,7 +179,7 @@ namespace Microsoft.DotNet.Cli.Build.Tests
         }
 
         [Fact]
-        public void It_warns_on_rid_without_self_contained_options()
+        public void It_no_longer_warns_on_rid_without_self_contained_options()
         {
             var testInstance = _testAssetsManager.CopyTestAsset("HelloWorld")
                 .WithSource()
@@ -192,7 +192,7 @@ namespace Microsoft.DotNet.Cli.Build.Tests
                .Should()
                .Pass()
                .And
-               .HaveStdOutContaining("NETSDK1179");
+               .NotHaveStdOutContaining("NETSDK1179");
         }
 
         [Fact]
