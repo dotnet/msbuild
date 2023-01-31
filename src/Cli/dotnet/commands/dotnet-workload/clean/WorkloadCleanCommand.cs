5bb3924900159fd9527d4a9db7ed5c1d2752bebb
Noah Gilson <noahgilson@microsoft.com>
2023-01-31 15:35:58 -0800

    Add basic template code for dotnet workload clean

---
+++ src/Cli/dotnet/commands/dotnet-workload/clean/WorkloadCleanCommand.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System.CommandLine;
+using Microsoft.DotNet.Cli;
+
+namespace Microsoft.DotNet.Workloads.Workload.Clean
+{
+    internal class WorkloadCleanCommand : WorkloadCommandBase
+    {
+        private readonly bool _cleanAll;
+
+        public WorkloadCleanCommand(ParseResult parseResult) : base(parseResult)
+        {
+            _cleanAll = parseResult.GetValue(WorkloadCleanCommandParser.CleanAllOption);
+        }
+
+        public override int Execute()
+        {
+            return 0;
+        }
+
+    }
+}
