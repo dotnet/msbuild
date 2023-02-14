a461afcc85d4b0f3d182136ef490be146f1076dc
Jan Krivanek <jankrivanek@microsoft.com>
2023-02-14 16:54:26 +0100

    Add changewave doc info

--- src/Build/Evaluation/Evaluator.cs
+++ src/Build/Evaluation/Evaluator.cs
@@ -8,7 +8,6 @@ using System.Diagnostics;
 using System.Globalization;
 using System.IO;
 using System.Linq;
-using System.Reflection;
 using System.Text;
 using Microsoft.Build.BackEnd;
 using Microsoft.Build.BackEnd.Components.Logging;
