f93b24b5a88a37a3b6b122c7f65f910fcc2ba2f0
Rainer Sigwald <raines@microsoft.com>
2023-02-22 09:11:03 -0600

    Revert "Added support for logging interpolated strings in tasks. (#8424)" (#8479)
    This reverts commit 05f741b98545ab7d2cdef096e7d6ff33dcc2fb9e, which should unblock dotnet/installer#15586.

--- src/Shared/TaskLoggingHelper.cs
+++ src/Shared/TaskLoggingHelper.cs
@@ -239,7 +239,7 @@ namespace Microsoft.Build.Utilities
         }
         #endregion
 
-        #region Message logging method
+        #region Message logging methods
 
         /// <summary>
         /// Returns <see langword="true"/> if the build is configured to log all task inputs.
@@ -257,33 +257,6 @@ namespace Microsoft.Build.Utilities
             return BuildEngine is not IBuildEngine10 buildEngine10
                 || buildEngine10.EngineServices.LogsMessagesOfImportance(importance);
         }
-
-#if NET6_0_OR_GREATER
-
-        /// <summary>
-        /// Logs a message using the specified interpolated string.
-        /// Thread safe.
-        /// </summary>
-        /// <param name="stringHandler">The message interpolated string.</param>
-        public void LogMessage(LogInterpolatedStringHandler stringHandler)
-        {
-            LogMessage(MessageImportance.Normal, stringHandler);
-        }
-
-        /// <summary>
-        /// Logs a message using the specified interpolated string.
-        /// Thread safe.
-        /// </summary>
-        /// <param name="importance">The importance level of the message.</param>
-        /// <param name="stringHandler">The message interpolated string.</param>
-        public void LogMessage(MessageImportance importance, LogInterpolatedStringHandler stringHandler)
-        {
-            if (LogsMessagesOfImportance(importance))
-            {
-                LogMessage(importance, stringHandler.GetFormat(), stringHandler.Arguments);
-            }
-        }
-#endif
 
         /// <summary>
         /// Logs a message using the specified string.
@@ -619,19 +592,6 @@ namespace Microsoft.Build.Utilities
         #endregion
 
         #region Error logging methods
-
-#if NET6_0_OR_GREATER
-
-        /// <summary>
-        /// Logs an error message using the specified interpolated string.
-        /// Thread safe.
-        /// </summary>
-        /// <param name="stringHandler">The message interpolated string.</param>
-        public void LogError(LogInterpolatedStringHandler stringHandler)
-        {
-            LogError(stringHandler.GetFormat(), stringHandler.Arguments);
-        }
-#endif
 
         /// <summary>
         /// Logs an error using the specified string.
@@ -983,19 +943,6 @@ namespace Microsoft.Build.Utilities
         #endregion
 
         #region Warning logging methods
-
-#if NET6_0_OR_GREATER
-
-        /// <summary>
-        /// Logs a warning message using the specified interpolated string.
-        /// Thread safe.
-        /// </summary>
-        /// <param name="stringHandler">The message interpolated string.</param>
-        public void LogWarning(LogInterpolatedStringHandler stringHandler)
-        {
-            LogWarning(stringHandler.GetFormat(), stringHandler.Arguments);
-        }
-#endif
 
         /// <summary>
         /// Logs a warning using the specified string.
