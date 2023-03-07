4acc8fa05d649ea8496b342292f4edda056ac5e2
Andrii Patsula <apatsula@microsoft.com>
2023-03-07 16:40:51 +0100

    GenAPI: add throw exception body for constructors with reference parameters. (#31028)

--- src/Tests/Microsoft.DotNet.GenAPI.Tests/SyntaxRewriterTests.cs
+++ src/Tests/Microsoft.DotNet.GenAPI.Tests/SyntaxRewriterTests.cs
@@ -462,6 +462,32 @@ namespace Microsoft.DotNet.GenAPI.Tests
                 }
                 """);
         }
+
+        [Fact]
+        public void TestConstructorWithReferenceParameters()
+        {
+            CompareSyntaxTree(new BodyBlockCSharpSyntaxRewriter("Not implemented"),
+                original: """
+                namespace A
+                {
+                    public class Foo
+                    {
+                        public Foo(int a) { }
+                        public Foo(int a, out int b) { b = 1; }
+                    }
+                }
+                """,
+                expected: """
+                namespace A
+                {
+                    public class Foo
+                    {
+                        public Foo(int a) { }
+                        public Foo(int a, out int b) { throw new PlatformNotSupportedException("Not implemented"); }
+                    }
+                }
+                """);
+        }
     }
 
     public class TypeForwardAttributeCSharpSyntaxRewriterTests : SyntaxRewriterTests
