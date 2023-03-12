20a5b0866b3aa0495678a0badf6acc704acff133
Jonathan Dodds <jrdodds@me.com>
2023-03-12 18:21:42 -0400

    add new unit tests for WriteCodeFragment for non-existent directory

--- src/Tasks.UnitTests/WriteCodeFragment_Tests.cs
+++ src/Tasks.UnitTests/WriteCodeFragment_Tests.cs
@@ -90,6 +90,30 @@ namespace Microsoft.Build.UnitTests
             string file = Path.Combine(Path.GetTempPath(), "CombineFileDirectory.tmp");
             Assert.Equal(file, task.OutputFile.ItemSpec);
             Assert.True(File.Exists(file));
+
+            File.Delete(task.OutputFile.ItemSpec);
+        }
+
+        /// <summary>
+        /// Combine file and directory
+        /// </summary>
+        [Fact]
+        public void CombineFileDirectoryAndDirectoryDoesNotExist()
+        {
+            // ToDo: Replace "foo" with random unique directory name.
+            TaskItem folder = new TaskItem(Path.Combine(Path.GetTempPath(), "foo" + Path.DirectorySeparatorChar));
+            TaskItem file = new TaskItem("CombineFileDirectory.tmp");
+            string expectedFile = Path.Combine(folder.ItemSpec, file.ItemSpec);
+            WriteCodeFragment task = CreateTask("c#", folder, file, new TaskItem[] { new TaskItem("aa") });
+            MockEngine engine = new MockEngine(true);
+            task.BuildEngine = engine;
+            bool result = task.Execute();
+
+            Assert.True(result);
+            Assert.Equal(expectedFile, task.OutputFile.ItemSpec);
+            Assert.True(File.Exists(expectedFile));
+
+            FileUtilities.DeleteWithoutTrailingBackslash(folder.ItemSpec, true);
         }
 
         /// <summary>
@@ -315,6 +339,27 @@ namespace Microsoft.Build.UnitTests
             Assert.Equal(".cs", task.OutputFile.ItemSpec.Substring(task.OutputFile.ItemSpec.Length - 3));
 
             File.Delete(task.OutputFile.ItemSpec);
+        }
+
+        /// <summary>
+        /// Combine file and directory
+        /// </summary>
+        [Fact]
+        public void ToDirectoryAndDirectoryDoesNotExist()
+        {
+            // ToDo: Replace "foo" with random unique directory name.
+            TaskItem folder = new TaskItem(Path.Combine(Path.GetTempPath(), "foo" + Path.DirectorySeparatorChar));
+            WriteCodeFragment task = CreateTask("c#", folder, null, new TaskItem[] { new TaskItem("System.AssemblyTrademarkAttribute") });
+            MockEngine engine = new MockEngine(true);
+            task.BuildEngine = engine;
+            bool result = task.Execute();
+
+            Assert.True(result);
+            Assert.True(File.Exists(task.OutputFile.ItemSpec));
+            Assert.Equal(folder.ItemSpec, task.OutputFile.ItemSpec.Substring(0, folder.ItemSpec.Length));
+            Assert.Equal(".cs", task.OutputFile.ItemSpec.Substring(task.OutputFile.ItemSpec.Length - 3));
+
+            FileUtilities.DeleteWithoutTrailingBackslash(folder.ItemSpec, true);
         }
 
         /// <summary>
@@ -874,7 +919,7 @@ namespace Microsoft.Build.UnitTests
         }
 
         /// <summary>
-        /// For backward-compatibility, if multiple constructors are found with the same number 
+        /// For backward-compatibility, if multiple constructors are found with the same number
         /// of position arguments that was specified in the metadata, then the constructor that
         /// has strings for every parameter should be used.
         /// </summary>
@@ -985,11 +1030,18 @@ namespace Microsoft.Build.UnitTests
 
         private WriteCodeFragment CreateTask(string language, params TaskItem[] attributes)
         {
-            WriteCodeFragment task = new();
-            task.Language = language;
-            task.OutputDirectory = new TaskItem(Path.GetTempPath());
-            task.AssemblyAttributes = attributes;
-            return task;
+            return CreateTask(language, new TaskItem(Path.GetTempPath()), null, attributes);
+        }
+
+        private WriteCodeFragment CreateTask(string language, TaskItem outputDirectory, TaskItem outputFile, params TaskItem[] attributes)
+        {
+            return new WriteCodeFragment()
+            {
+                Language = language,
+                OutputDirectory = outputDirectory,
+                OutputFile = outputFile,
+                AssemblyAttributes = attributes
+            };
         }
 
         private void ExecuteAndVerifySuccess(WriteCodeFragment task, params string[] expectedAttributes)
