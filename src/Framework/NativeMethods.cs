c6e6cd447ac3944b351247d995f32b4d2010d409
Roman Konecny <rokonecn@microsoft.com>
2023-04-24 12:05:52 +0200

    Fix outputIsScreen on non-windows OS. (#8689)
    Detected while working on #8413
    
    Context
    outputIsScreen is always falls on on non-windows OS causing /tl:auto to reject live logger
    
    Changes Made
    see files
    
    Testing
    locally
    
    Notes
    would affect MSB server VT100 output coloring as well.

--- src/Framework/NativeMethods.cs
+++ src/Framework/NativeMethods.cs
@@ -1531,6 +1531,8 @@ internal static class NativeMethods
         {
             // On posix OSes we expect console always supports VT100 coloring unless it is explicitly marked as "dumb".
             acceptAnsiColorCodes = Environment.GetEnvironmentVariable("TERM") != "dumb";
+            // It wasn't redirected as tested above so we assume output is screen/console
+            outputIsScreen = true; 
         }
         return (acceptAnsiColorCodes, outputIsScreen, originalConsoleMode);
     }
