cf87ef1f3b50bfdf8086c069d85abcfb4d60a9b4
Jenny Bai <v-jennybai@microsoft.com>
2023-04-27 17:05:03 +0800

    Move the log out of the loop

--- src/Tasks/XslTransformation.cs
+++ src/Tasks/XslTransformation.cs
@@ -159,16 +159,17 @@ namespace Microsoft.Build.Tasks
             // Do the transformation.
             try
             {
+                if (UseTrustedSettings)
+                {
+                    Log.LogMessageFromResources(MessageImportance.High, "XslTransform.SecuritySettingsViaUseTrustedSettings");
+                }
+
                 for (int i = 0; i < xmlinput.Count; i++)
                 {
                     using (XmlWriter xmlWriter = XmlWriter.Create(_outputPaths[i].ItemSpec, xslct.OutputSettings))
                     {
                         using (XmlReader xr = xmlinput.CreateReader(i))
                         {
-                            if (UseTrustedSettings)
-                            {
-                                Log.LogMessageFromResources(MessageImportance.High, "XslTransform.SecuritySettingsViaUseTrustedSettings");
-                            }
                             xslct.Transform(xr, arguments, xmlWriter, new XmlUrlResolver());
                         }
 
