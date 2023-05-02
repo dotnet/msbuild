cbdf46eafec4349fec3d31773420a60cc2795c96
Tomáš Matoušek <tmat@users.noreply.github.com>
2023-05-02 11:10:03 -0700

    Fix license headers in all source files (#32141)

--- src/Cli/dotnet/commands/dotnet-new/OptionalWorkloadProvider.cs
+++ src/Cli/dotnet/commands/dotnet-new/OptionalWorkloadProvider.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.DotNet.Cli;
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Microsoft.DotNet.Cli;
 using Microsoft.DotNet.Configurer;
 using Microsoft.TemplateEngine.Abstractions;
 using Microsoft.TemplateEngine.Abstractions.TemplatePackage;
--- src/Tests/Microsoft.NET.TestFramework/Assertions/DependencyContextAssertions.cs
+++ src/Tests/Microsoft.NET.TestFramework/Assertions/DependencyContextAssertions.cs
@@ -1,4 +1,7 @@
-﻿using FluentAssertions;
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using FluentAssertions;
 using FluentAssertions.Execution;
 using Microsoft.DotNet.Cli.Utils;
 using Microsoft.Extensions.DependencyModel;
