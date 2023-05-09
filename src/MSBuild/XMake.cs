28b8bd056b703141c66362cad7c58dce5089b508
Noah Gilson <OTAKUPENGUINOP@GMAIL.COM>
2023-05-09 14:53:07 -0700

    Remove the english language and redirect in the help link
    Co-authored-by: Forgind <12969783+Forgind@users.noreply.github.com>

--- src/MSBuild/XMake.cs
+++ src/MSBuild/XMake.cs
@@ -1677,7 +1677,7 @@ namespace Microsoft.Build.CommandLine
             thisThread.CurrentUICulture = desiredCulture;
 
             // For full framework, both the above and below must be set. This is not true in core, but it is a no op in core.
-            // https://learn.microsoft.com/en-us/dotnet/api/system.globalization.cultureinfo.defaultthreadcurrentculture?redirectedfrom=MSDN&view=net-7.0#remarks
+            // https://learn.microsoft.com/dotnet/api/system.globalization.cultureinfo.defaultthreadcurrentculture#remarks
             CultureInfo.CurrentUICulture = desiredCulture;
             CultureInfo.DefaultThreadCurrentUICulture = desiredCulture;
 
