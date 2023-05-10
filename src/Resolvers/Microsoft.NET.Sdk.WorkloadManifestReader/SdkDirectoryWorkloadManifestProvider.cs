9947c3052a4901b87fd2763e79a2bd9cc460f6ec
Daniel Plaisted <daplaist@microsoft.com>
2023-05-10 19:07:19 -0400

    Support reading side-by-side workload manifests

--- src/Resolvers/Microsoft.NET.Sdk.WorkloadManifestReader/SdkDirectoryWorkloadManifestProvider.cs
+++ src/Resolvers/Microsoft.NET.Sdk.WorkloadManifestReader/SdkDirectoryWorkloadManifestProvider.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.IO;
 using System.Linq;
+using Microsoft.Deployment.DotNet.Releases;
 using Microsoft.DotNet.Cli;
 using Microsoft.DotNet.Workloads.Workload;
 using Microsoft.NET.Sdk.Localization;
@@ -117,20 +118,26 @@ namespace Microsoft.NET.Sdk.WorkloadManifestReader
         public IEnumerable<string> GetManifestDirectories()
         {
             //  Scan manifest directories
-            //  TODO: Handle new format with subfolders for version
             var manifestIdsToDirectories = new Dictionary<string, string>();
+
+            void ProbeDirectory(string manifestDirectory)
+            {
+                (string? id, string? finalManifestDirectory) = ResolveManifestDirectory(manifestDirectory);
+                if (id != null && finalManifestDirectory != null)
+                {
+                    manifestIdsToDirectories.Add(id, finalManifestDirectory);
+                }
+            }
+
             if (_manifestRoots.Length == 1)
             {
                 //  Optimization for common case where test hook to add additional directories isn't being used
-                var manifestDirectory = Path.Combine(_manifestRoots[0], _sdkVersionBand.ToString());
-                if (Directory.Exists(manifestDirectory))
+                var manifestVersionBandDirectory = Path.Combine(_manifestRoots[0], _sdkVersionBand.ToString());
+                if (Directory.Exists(manifestVersionBandDirectory))
                 {
-                    foreach (var workloadManifestDirectory in Directory.EnumerateDirectories(manifestDirectory))
+                    foreach (var workloadManifestDirectory in Directory.EnumerateDirectories(manifestVersionBandDirectory))
                     {
-                        if (!IsManifestIdOutdated(workloadManifestDirectory))
-                        {
-                            manifestIdsToDirectories.Add(Path.GetFileName(workloadManifestDirectory), workloadManifestDirectory);
-                        }
+                        ProbeDirectory(workloadManifestDirectory);
                     }
                 }
             }
@@ -140,10 +147,10 @@ namespace Microsoft.NET.Sdk.WorkloadManifestReader
                 Dictionary<string, string> directoriesWithManifests = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
                 foreach (var manifestRoot in _manifestRoots.Reverse())
                 {
-                    var manifestDirectory = Path.Combine(manifestRoot, _sdkVersionBand.ToString());
-                    if (Directory.Exists(manifestDirectory))
+                    var manifestVersionBandDirectory = Path.Combine(manifestRoot, _sdkVersionBand.ToString());
+                    if (Directory.Exists(manifestVersionBandDirectory))
                     {
-                        foreach (var workloadManifestDirectory in Directory.EnumerateDirectories(manifestDirectory))
+                        foreach (var workloadManifestDirectory in Directory.EnumerateDirectories(manifestVersionBandDirectory))
                         {
                             directoriesWithManifests[Path.GetFileName(workloadManifestDirectory)] = workloadManifestDirectory;
                         }
@@ -152,10 +159,7 @@ namespace Microsoft.NET.Sdk.WorkloadManifestReader
 
                 foreach (var workloadManifestDirectory in directoriesWithManifests.Values)
                 {
-                    if (!IsManifestIdOutdated(workloadManifestDirectory))
-                    {
-                        manifestIdsToDirectories.Add(Path.GetFileName(workloadManifestDirectory), workloadManifestDirectory);
-                    }
+                    ProbeDirectory(workloadManifestDirectory);
                 }
             }
 
@@ -195,6 +199,42 @@ namespace Microsoft.NET.Sdk.WorkloadManifestReader
                 .ToList();
         }
 
+        /// <summary>
+        /// Given a folder that may directly include a WorkloadManifest.json file, or may have the workload manifests in version subfolders, choose the directory
+        /// with the latest workload manifest.
+        /// </summary>
+        private (string? id, string? manifestDirectory) ResolveManifestDirectory(string manifestDirectory)
+        {
+            string manifestId = Path.GetFileName(manifestDirectory);
+            if (_outdatedManifestIds.Contains(manifestId))
+            {
+                return (null, null);
+            }
+
+            var manifestVersionDirectories = Directory.GetDirectories(manifestDirectory)
+                    .Where(dir => File.Exists(Path.Combine(dir, "WorkloadManifest.json")))
+                    .Select(dir =>
+                    {
+                        ReleaseVersion? releaseVersion = null;
+                        ReleaseVersion.TryParse(Path.GetFileName(dir), out releaseVersion);
+                        return (directory: dir, version: releaseVersion);
+                    })
+                    .Where(t => t.version != null)
+                    .OrderByDescending(t => t.version)
+                    .ToList();
+
+            //  Assume that if there are any versioned subfolders, they are higher manifest versions than a workload manifest directly in the specified folder, if it exists
+            if (manifestVersionDirectories.Any())
+            {
+                return (manifestId, manifestVersionDirectories.First().directory);
+            }
+            else if (File.Exists(Path.Combine(manifestDirectory, "WorkloadManifest.json")))
+            {
+                return (manifestId, manifestDirectory);
+            }
+            return (null, null);
+        }
+
         private string FallbackForMissingManifest(string manifestId)
         {
             //  Only use the last manifest root (usually the dotnet folder itself) for fallback
@@ -208,11 +248,21 @@ namespace Microsoft.NET.Sdk.WorkloadManifestReader
                 .Select(dir => Path.GetFileName(dir))
                 .Select(featureBand => new SdkFeatureBand(featureBand))
                 .Where(featureBand => featureBand < _sdkVersionBand || _sdkVersionBand.ToStringWithoutPrerelease().Equals(featureBand.ToString(), StringComparison.Ordinal));
-            var matchingManifestFatureBands = candidateFeatureBands
-                .Where(featureBand => Directory.Exists(Path.Combine(sdkManifestPath, featureBand.ToString(), manifestId)));
-            if (matchingManifestFatureBands.Any())
+
+            var matchingManifestFatureBandsAndResolvedManifestDirectories = candidateFeatureBands
+                //  Calculate path to <FeatureBand>\<ManifestID>
+                .Select(featureBand => (featureBand, manifestDirectory: Path.Combine(sdkManifestPath, featureBand.ToString(), manifestId)))
+                //  Filter out directories that don't exist
+                .Where(t => Directory.Exists(t.manifestDirectory))
+                //  Inside directory, resolve where to find WorkloadManifest.json
+                .Select(t => (t.featureBand, res: ResolveManifestDirectory(t.manifestDirectory)))
+                //  Filter out directories where no WorkloadManifest.json was resolved
+                .Where(t => t.res.id != null && t.res.manifestDirectory != null)
+                .ToList();
+
+            if (matchingManifestFatureBandsAndResolvedManifestDirectories.Any())
             {
-                return Path.Combine(sdkManifestPath, matchingManifestFatureBands.Max()!.ToString(), manifestId);
+                return matchingManifestFatureBandsAndResolvedManifestDirectories.OrderByDescending(t => t.featureBand).First().res.manifestDirectory!;
             }
             else
             {
