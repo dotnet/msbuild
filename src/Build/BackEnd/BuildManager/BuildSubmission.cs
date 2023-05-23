9ee3de60db435e7fff1df3510d141139045f5980
Forgind <Forgind@users.noreply.github.com>
2023-05-22 17:46:37 -0700

    un-public BRD

--- src/Build/BackEnd/BuildManager/BuildSubmission.cs
+++ src/Build/BackEnd/BuildManager/BuildSubmission.cs
@@ -103,7 +103,7 @@ namespace Microsoft.Build.Execution
         /// <summary>
         /// The BuildRequestData being used for this submission.
         /// </summary>
-        public BuildRequestData BuildRequestData { get; }
+        internal BuildRequestData BuildRequestData { get; }
 
         /// <summary>
         /// The build request for execution.
