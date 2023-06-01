118d917a2651319b8fdcdd99503f62016436b135
Forgind <Forgind@users.noreply.github.com>
2023-05-31 17:17:18 -0700

    Fix fall back for ComSpec

--- src/Cli/dotnet/CommandFactory/CommandResolution/WindowsExePreferredCommandSpecFactory.cs
+++ src/Cli/dotnet/CommandFactory/CommandResolution/WindowsExePreferredCommandSpecFactory.cs
@@ -50,7 +50,7 @@ namespace Microsoft.DotNet.CommandFactory
             string command,
             IEnumerable<string> args)
         {
-            var comSpec = Environment.GetEnvironmentVariable("ComSpec") ?? "cmd.exe";
+            var comSpec = Environment.GetEnvironmentVariable("ComSpec") ?? @"C:\WINDOWS\system32\cmd.exe";
 
             // Handle the case where ComSpec is already the command
             if (command.Equals(comSpec, StringComparison.OrdinalIgnoreCase))
