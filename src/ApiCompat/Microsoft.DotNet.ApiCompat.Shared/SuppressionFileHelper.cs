86db944afd9699903751a860de819a01c77b176c
Viktor Hofer <viktor.hofer@microsoft.com>
2023-06-01 12:50:41 +0200

    ApiCompat log breaking changes footer as error (#32956)

--- src/ApiCompat/Microsoft.DotNet.ApiCompat.Shared/SuppressionFileHelper.cs
+++ src/ApiCompat/Microsoft.DotNet.ApiCompat.Shared/SuppressionFileHelper.cs
@@ -1,4 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System;
@@ -45,8 +45,7 @@ namespace Microsoft.DotNet.ApiCompat
             {
                 if (!generateSuppressionFile)
                 {
-                    log.LogMessage(MessageImportance.High,
-                        Resources.BreakingChangesFoundRegenerateSuppressionFileCommandHelp);
+                    log.LogError(Resources.BreakingChangesFoundRegenerateSuppressionFileCommandHelp);
                 }
             }
             else
