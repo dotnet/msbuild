a7cebdec16e17f3a6e62465f8a493bc61564adaf
Ladi Prosek <laprosek@microsoft.com>
2023-06-07 12:52:45 +0200

    Make DependencyFile (PortableLibraryFile's base) serializable as well

--- src/Tasks/DependencyFile.cs
+++ src/Tasks/DependencyFile.cs
@@ -14,7 +14,12 @@ namespace Microsoft.Build.Tasks
     /// <remarks>
     /// Represents a single input to a compilation-style task.
     /// Keeps track of timestamp for later comparison.
+    ///
+    /// Must be serializable because instances may be marshaled cross-AppDomain, see <see cref="ProcessResourceFiles.PortableLibraryCacheInfo"/>.
     /// </remarks>
+#if FEATURE_APPDOMAIN
+    [Serializable]
+#endif
     internal class DependencyFile
     {
         // Filename
