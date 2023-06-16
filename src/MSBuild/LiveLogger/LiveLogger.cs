3e5d243f6b3e5ab4da77467fe6707fbfdd94a6be
Rainer Sigwald <raines@microsoft.com>
2023-06-16 12:20:49 -0500

    Avoid updating lines when colorized
    The root cause of #8781 is that the optimize-output-by-skipping-
    identical-characters logic is not aware of escape sequences, so it goes
    horribly awry when the replacing line differs after the first escape
    sequence.
    
    Instead, apply the previous logic only if there are no escape sequences;
    if there are, replace the entire line.
    
    If this proves to cause too much flicker or perform slowly, we could
    implement escape-sequence-aware truncation, length, and comparison, but
    we can wait for feedback to that effect.

--- src/MSBuild/LiveLogger/LiveLogger.cs
+++ src/MSBuild/LiveLogger/LiveLogger.cs
@@ -740,21 +740,22 @@ internal sealed class LiveLogger : INodeLogger
                     if (!previous.SequenceEqual(needed))
                     {
                         int commonPrefixLen = previous.CommonPrefixLength(needed);
-                        if (commonPrefixLen == 0)
+
+                        if (commonPrefixLen != 0 && needed.Slice(0, commonPrefixLen).IndexOf('\x1b') == -1)
                         {
-                            // whole string
-                            sb.Append(needed);
+                            // no escape codes, so can trivially skip substrings
+                            sb.Append($"{AnsiCodes.CSI}{commonPrefixLen}{AnsiCodes.MoveForward}");
+                            sb.Append(needed.Slice(commonPrefixLen));
                         }
                         else
                         {
-                            // set cursor to different char
-                            sb.Append($"{AnsiCodes.CSI}{commonPrefixLen}{AnsiCodes.MoveForward}");
-                            sb.Append(needed.Slice(commonPrefixLen));
-                            // Shall we clear rest of line
-                            if (needed.Length < previous.Length)
-                            {
-                                sb.Append($"{AnsiCodes.CSI}{AnsiCodes.EraseInLine}");
-                            }
+                            sb.Append(needed);
+                        }
+
+                        // Shall we clear rest of line
+                        if (needed.Length < previous.Length)
+                        {
+                            sb.Append($"{AnsiCodes.CSI}{AnsiCodes.EraseInLine}");
                         }
                     }
                 }
