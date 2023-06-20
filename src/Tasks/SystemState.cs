9e315996391df9e2c9d22b3d3e622a1c88dc97ad
Ladi Prosek <ladi.prosek@gmail.com>
2023-06-20 22:42:38 +0200

    Update src/Tasks/SystemState.cs
    Co-authored-by: AR-May <67507805+AR-May@users.noreply.github.com>

--- src/Tasks/SystemState.cs
+++ src/Tasks/SystemState.cs
@@ -608,7 +608,7 @@ namespace Microsoft.Build.Tasks
         internal void SerializePrecomputedCache(string stateFile, TaskLoggingHelper log)
         {
             // Save a copy of instanceLocalOutgoingFileStateCache so we can restore it later. SerializeCacheByTranslator serializes
-            // instanceLocalFileStateCache by default, so change that to the relativized form, then change it back.
+            // instanceLocalOutgoingFileStateCache by default, so change that to the relativized form, then change it back.
             Dictionary<string, FileState> oldFileStateCache = instanceLocalOutgoingFileStateCache;
             instanceLocalOutgoingFileStateCache = instanceLocalFileStateCache.ToDictionary(kvp => FileUtilities.MakeRelative(Path.GetDirectoryName(stateFile), kvp.Key), kvp => kvp.Value);
 
