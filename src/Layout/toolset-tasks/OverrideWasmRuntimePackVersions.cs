bfe468b3bd08b83b26116af593a2637739ceb037
Marek Fišera <mara@neptuo.com>
2023-07-17 17:05:08 +0200

    [wasm] Override versions of downlevel runtime packs (#32882)

---
+++ src/Layout/toolset-tasks/OverrideWasmRuntimePackVersions.cs
@@ -0,0 +1,48 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Xml.Linq;
+using System.Xml.XPath;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace Microsoft.DotNet.Build.Tasks
+{
+    /// <summary>
+    /// </summary>
+    public sealed class OverrideWasmRuntimePackVersions : Task
+    {
+        [Required]
+        public ITaskItem[] Properties { get; set; }
+
+        [Required]
+        public string WorkloadManifestPath { get; set; }
+
+        public override bool Execute()
+        {
+            var document = XDocument.Load(WorkloadManifestPath);
+            if (document != null)
+            {
+                foreach (var property in Properties)
+                {
+                    string propertyName = property.ItemSpec;
+                    string targetVersion = property.GetMetadata("Version");
+
+                    var xmlProperty = document.Descendants(propertyName).Single();
+                    xmlProperty.Value = targetVersion;
+                }
+
+                document.Save(WorkloadManifestPath);
+
+                return true;
+            }
+            
+            return false;
+        }
+    }
+}
