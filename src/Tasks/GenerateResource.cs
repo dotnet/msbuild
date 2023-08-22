ed09e36432551833fda3546773e185cb29d80e73
Felix Huang <yuehuang@microsoft.com>
2023-08-22 14:52:05 -0700

    .

--- src/Tasks/GenerateResource.cs
+++ src/Tasks/GenerateResource.cs
@@ -3613,7 +3613,8 @@ namespace Microsoft.Build.Tasks
                         name.Length--;
                     }
                     ch = sr.Read(); // move past =
-                                    // If it exists, move past the first space after the equals sign.
+
+                    // If it exists, move past the first space after the equals sign.
                     if (ch == ' ')
                     {
                         ch = sr.Read();
@@ -3754,10 +3755,11 @@ namespace Microsoft.Build.Tasks
                     // In that case, the first time we catch an exception indicating that the XML written to disk is malformed,
                     // specifically an InvalidOperationException: "Token EndElement in state Error would result in an invalid XML document."
                     try { writer.Dispose(); }
-                    catch (Exception) { } // We agressively catch all exception types since we already have one we will throw.
-                                          // The second time we catch the out of disk space exception.
+                    catch (Exception) { } // We aggressively catch all exception types since we already have one we will throw.
+
+                    // The second time we catch the out of disk space exception.
                     try { writer.Dispose(); }
-                    catch (Exception) { } // We agressively catch all exception types since we already have one we will throw.
+                    catch (Exception) { } // We aggressively catch all exception types since we already have one we will throw.
                     throw capturedException; // In the event of a full disk, this is an out of disk space IOException.
                 }
             }
