3f8212ad348e37fc516d3d0c5291abca529770a2
Roman Konecny <rokonecn@microsoft.com>
2023-09-06 11:33:10 +0200

    Enable nullabel for ExtendedDataFields and change it into immutable.

--- src/Build/Logging/BinaryLogger/ExtendedDataFields.cs
+++ src/Build/Logging/BinaryLogger/ExtendedDataFields.cs
@@ -1,14 +1,20 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-#nullable disable
 using System.Collections.Generic;
 
 namespace Microsoft.Build.Logging;
 
 internal class ExtendedDataFields
 {
-    public string ExtendedType { get; set; }
-    public IDictionary<string, string> ExtendedMetadata { get; set; }
-    public string ExtendedData { get; set; }
+    public ExtendedDataFields(string extendedType, IDictionary<string, string>? extendedMetadata, string? extendedData)
+    {
+        ExtendedType = extendedType;
+        ExtendedMetadata = extendedMetadata;
+        ExtendedData = extendedData;
+    }
+
+    public string ExtendedType { get; }
+    public IDictionary<string, string?>? ExtendedMetadata { get; }
+    public string? ExtendedData { get; }
 }
