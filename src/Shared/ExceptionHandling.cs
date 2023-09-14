b2444c77eedaa33facf2f9e3916d3a9e18543c4a
Mariana Dematte <magarces@microsoft.com>
2023-09-14 23:20:03 +0200

    Addressed PR comments

--- src/Shared/ExceptionHandling.cs
+++ src/Shared/ExceptionHandling.cs
@@ -167,7 +167,7 @@ namespace Microsoft.Build.Shared
         internal static bool IsXmlException(Exception e)
         {
             return e is XmlException
-#if NETFRAMEWORK
+#if FEATURE_SECURITY_PERMISSIONS
                 || e is System.Security.XmlSyntaxException
 #endif
                 || e is XmlSchemaException
