33c5745554409984a1fc7fc97f1a4d9131652d21
Tarek Mahmoud Sayed <tarekms@microsoft.com>
2023-09-14 15:44:14 -0700

    Metrics switch support in the SDK

--- src/Tests/Microsoft.NET.Publish.Tests/GivenThatWeWantToPublishAProjectWithAllFeatures.cs
+++ src/Tests/Microsoft.NET.Publish.Tests/GivenThatWeWantToPublishAProjectWithAllFeatures.cs
@@ -61,6 +61,7 @@ namespace Microsoft.NET.Publish.Tests
             ""System.ComponentModel.TypeConverter.EnableUnsafeBinaryFormatterInDesigntimeLicenseContextSerialization"": false,
             ""System.Diagnostics.Debugger.IsSupported"": true,
             ""System.Diagnostics.Tracing.EventSource.IsSupported"": false,
+            ""System.Diagnostics.Metrics.Meter.IsSupported"": false,
             ""System.Globalization.Invariant"": true,
             ""System.Globalization.PredefinedCulturesOnly"": true,
             ""System.GC.Concurrent"": false,
@@ -101,7 +102,7 @@ namespace Microsoft.NET.Publish.Tests
             baselineConfigJsonObject["runtimeOptions"]["tfm"] = targetFramework;
             baselineConfigJsonObject["runtimeOptions"]["framework"]["version"] =
                 targetFramework == "net6.0" ? "6.0.0" : "1.1.2";
-            
+
             runtimeConfigJsonObject
                 .Should()
                 .BeEquivalentTo(baselineConfigJsonObject);
