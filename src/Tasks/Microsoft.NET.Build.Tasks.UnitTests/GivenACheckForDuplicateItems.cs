c9a7e276ec89f64e259e6385270d47c850da83ce
stan-sz <37585349+stan-sz@users.noreply.github.com>
2023-09-25 11:38:09 +0200

    Add UT
    Add unit test

---
+++ src/Tasks/Microsoft.NET.Build.Tasks.UnitTests/GivenACheckForDuplicateItems.cs
@@ -0,0 +1,59 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using FluentAssertions;
+using Microsoft.Build.Framework;
+using NuGet.Packaging.Core;
+using NuGet.Versioning;
+using Xunit;
+
+namespace Microsoft.NET.Build.Tasks.UnitTests
+{
+    public class GivenACheckForDuplicateItems
+    {
+        [Fact]
+        public void CheckForNoDuplicateItems()
+        {
+            var compile = new[]
+            {
+                new TaskItem("foo.cs"),
+                new TaskItem("bar.cs"),
+            };
+
+            // execute task
+            var task = new CheckForDuplicateItems()
+            {
+                Items = compile,
+                ItemName = "Compile",
+                PropertyNameToDisableDefaultItems = "PropertyNameToDisableDefaultItems",
+                MoreInformationLink = "MoreInformationLink"
+            };
+            task.Execute().Should().BeTrue();
+
+            task.DeduplicatedItems.Length.Should().Be(0);
+        }
+
+        [Fact]
+        public void CheckForDuplicateItems()
+        {
+            var compile = new[]
+            {
+                new TaskItem("foo.cs"),
+                new TaskItem("FOO.cs"),
+            };
+
+            // execute task
+            var task = new CheckForDuplicateItems()
+            {
+                Items = compile,
+                ItemName = "Compile",
+                PropertyNameToDisableDefaultItems = "PropertyNameToDisableDefaultItems",
+                MoreInformationLink = "MoreInformationLink"
+            };
+            task.Execute().Should().BeFalse();
+
+            task.DeduplicatedItems.Length.Should().Be(1);
+        }
+
+    }
+}
