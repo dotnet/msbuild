0eb79f5cffd3965fbe0d285902edab9bc0dec8d5
YuliiaKovalova <ykovalova@microsoft.com>
2023-10-11 11:00:34 +0200

    fix test failure

--- src/Tasks.UnitTests/GetAssembliesMetadata_Tests.cs
+++ src/Tasks.UnitTests/GetAssembliesMetadata_Tests.cs
@@ -37,7 +37,7 @@ namespace Microsoft.Build.Tasks.UnitTests
             t.AssembliesMetadata[0].GetMetadata("RuntimeVersion").ShouldBe("v4.0.30319");
             t.AssembliesMetadata[0].GetMetadata("MajorVersion").ShouldBe("1");
             t.AssembliesMetadata[0].GetMetadata("MinorVersion").ShouldBe("2");
-            t.AssembliesMetadata[0].GetMetadata("PeKind").ShouldBe("1");
+            t.AssembliesMetadata[0].GetMetadata("PeKind").ShouldBe("3");
             t.AssembliesMetadata[0].GetMetadata("Guid").ShouldBe("a48efb66-2596-4c6a-87ab-c8a765e54429");
             t.AssembliesMetadata[0].GetMetadata("BuildNumber").ShouldBe("3");
             t.AssembliesMetadata[0].GetMetadata("Description").ShouldBe("description for com");
@@ -45,7 +45,6 @@ namespace Microsoft.Build.Tasks.UnitTests
             t.AssembliesMetadata[0].GetMetadata("TargetFrameworkMoniker").ShouldBe(".NETFramework,Version=v4.7.2");
             t.AssembliesMetadata[0].GetMetadata("DefaultAlias").ShouldBe("Custom_COM");
             t.AssembliesMetadata[0].GetMetadata("PublicKey").ShouldBeEmpty();
-            t.AssembliesMetadata[0].GetMetadata("PublicKeyLength").ShouldBe("0");
         }
 
         [Fact]
@@ -73,7 +72,6 @@ namespace Microsoft.Build.Tasks.UnitTests
             t.AssembliesMetadata[0].GetMetadata("TargetFrameworkMoniker").ShouldBeEmpty();
             t.AssembliesMetadata[0].GetMetadata("DefaultAlias").ShouldBe("mscorlib");
             t.AssembliesMetadata[0].GetMetadata("PublicKey").ShouldBe("00000000000000000400000000000000");
-            t.AssembliesMetadata[0].GetMetadata("PublicKeyLength").ShouldBe("16");
         }
     }
 }
