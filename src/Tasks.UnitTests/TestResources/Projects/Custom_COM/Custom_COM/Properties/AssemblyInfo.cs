537d0f13d6c980e0df5549a8747b1b864ffe12c0
YuliiaKovalova <ykovalova@microsoft.com>
2023-10-27 19:09:56 +0200

    add missed header

--- src/Tasks.UnitTests/TestResources/Projects/Custom_COM/Custom_COM/Properties/AssemblyInfo.cs
+++ src/Tasks.UnitTests/TestResources/Projects/Custom_COM/Custom_COM/Properties/AssemblyInfo.cs
@@ -1,4 +1,7 @@
-﻿using System.Resources;
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Resources;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
