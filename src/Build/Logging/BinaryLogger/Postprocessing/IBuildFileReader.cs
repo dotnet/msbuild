9b2cbc29f0f580dc4b2ce0967ed320e04c563054
Jan Krivanek <jankrivanek@microsoft.com>
2023-10-30 16:31:35 +0100

    Clarified comment

--- src/Build/Logging/BinaryLogger/Postprocessing/IBuildFileReader.cs
+++ src/Build/Logging/BinaryLogger/Postprocessing/IBuildFileReader.cs
@@ -9,8 +9,8 @@ public interface IBuildFileReader
 {
     /// <summary>
     /// An event that allows the caller to be notified when an embedded file is encountered in the binary log.
-    /// When subscriber is OK with greedy reading entire content of the file, it can simplify subscribing to this event,
-    /// by using handler with same signature as handler for <see cref="IBuildEventStringsReader.StringReadDone"/> and wrapping it via
+    /// When subscriber is OK with greedy reading entire content of the file and is interested only in the individual strings (e.g. for sensitive data redaction purposes),
+    ///  it can simplify subscribing to this event, by using handler with same signature as handler for <see cref="IBuildEventStringsReader.StringReadDone"/> and wrapping it via
     /// <see cref="ArchiveFileEventArgsExtensions.ToArchiveFileHandler"/> extension.
     ///
     /// <example>
