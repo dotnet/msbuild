330f842a9958deb25b022553b522f543bd75b060
Jan Krivanek <jankrivanek@microsoft.com>
2023-10-30 16:40:26 +0100

    Improve documenting comments

--- src/Build/Logging/BinaryLogger/Postprocessing/ArchiveFileEventArgsExtensions.cs
+++ src/Build/Logging/BinaryLogger/Postprocessing/ArchiveFileEventArgsExtensions.cs
@@ -7,6 +7,27 @@ namespace Microsoft.Build.Logging;
 
 public static class ArchiveFileEventArgsExtensions
 {
+    /// <summary>
+    /// Helper method that allows to subscribe to <see cref="IBuildFileReader.ArchiveFileEncountered"/> event via <see cref="IBuildEventStringsReader.StringReadDone"/> event handler.
+    ///
+    /// This applies only when subscriber is OK with greedy reading entire content of the file and is interested only in the individual strings (e.g. for sensitive data redaction purposes),
+    ///  without distinction what each individual string means (e.g. they do not care about distinction between path and content or between individual files - they just need all textual data).
+    ///
+    /// <example>
+    /// <code>
+    /// private void OnStringReadDone(StringReadEventArgs e)
+    /// {
+    ///     e.StringToBeUsed = e.StringToBeUsed.Replace("foo", "bar");
+    /// }
+    ///
+    /// private void SubscribeToEvents()
+    /// {
+    ///     reader.StringReadDone += OnStringReadDone;
+    ///     reader.ArchiveFileEncountered += ((Action&lt;StringReadEventArgs&gt;)OnStringReadDone).ToArchiveFileHandler();
+    /// }
+    /// </code>
+    /// </example>
+    /// </summary>
     public static Action<ArchiveFileEventArgs> ToArchiveFileHandler(this Action<StringReadEventArgs> stringHandler)
     {
         return args =>
