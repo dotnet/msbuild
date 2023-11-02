dd0c00fe6ab77cc57a2ef790fa2c75511a606116
David Federman <dfederm@microsoft.com>
2023-11-02 09:11:26 -0700

    Removed unused using

--- src/Build/Graph/ProjectGraph.cs
+++ src/Build/Graph/ProjectGraph.cs
@@ -8,7 +8,6 @@ using System.Collections.Immutable;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Linq.Expressions;
 using System.Text;
 using System.Threading;
 using Microsoft.Build.Evaluation;
