6433fed983b18f4b66d44ddd46a74ff764947c33
Mariana Dematté <magarces@microsoft.com>
2023-12-21 19:32:20 +0100

    Commit some stuff that should not go

--- src/MSBuild/XMake.cs
+++ src/MSBuild/XMake.cs
@@ -633,7 +633,6 @@ namespace Microsoft.Build.CommandLine
             string[] commandLine)
 #endif
         {
-            Debugger.Launch();
             DebuggerLaunchCheck();
 
             // Initialize new build telemetry and record start of this build, if not initialized already
