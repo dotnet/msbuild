3d2b1616c151a448eaa183708f2ef379dad1e8cc
Jakub Jareš <me@jakubjares.com>
2024-02-16 08:38:36 +0100

    Document node status ctors, to make it more obvious what the parameters are for

--- src/MSBuild/TerminalLogger/NodeStatus.cs
+++ src/MSBuild/TerminalLogger/NodeStatus.cs
@@ -18,6 +18,13 @@ internal class NodeStatus
     public string Target { get; }
     public StopwatchAbstraction Stopwatch { get; }
 
+    /// <summary>
+    /// Status of a node that is currently doing work.
+    /// </summary>
+    /// <param name="project">The project that is written on left side.</param>
+    /// <param name="targetFramework">Target framework that is colorized and written on left side after project.</param>
+    /// <param name="target">The currently running work, usually the currently running target. Written on right.</param>
+    /// <param name="stopwatch">Duration of the current step. Written on right after target.</param>
     public NodeStatus(string project, string? targetFramework, string target, StopwatchAbstraction stopwatch)
     {
         Debug.Assert(!target.Contains("\x1B"), "Target should not contain any escape codes, if you want to colorize target use the other constructor.");
@@ -27,6 +34,15 @@ internal class NodeStatus
         Stopwatch = stopwatch;
     }
 
+    /// <summary>
+    /// Status of a node that is currently doing work.
+    /// </summary>
+    /// <param name="project">The project that is written on left side.</param>
+    /// <param name="targetFramework">Target framework that is colorized and written on left side after project.</param>
+    /// <param name="targetPrefixColor">Color for the status of the currently running work written on right.</param>
+    /// <param name="targetPrefix">Colorized status for the currently running work, written on right, before target, and separated by 1 space from it.</param>
+    /// <param name="target">The currently running work, usually the currently runnig target. Written on right.</param>
+    /// <param name="stopwatch">Duration of the current step. Written on right after target.</param>
     public NodeStatus(string project, string? targetFramework, TerminalColor targetPrefixColor, string targetPrefix, string target, StopwatchAbstraction stopwatch)
     {
         Project = project;
