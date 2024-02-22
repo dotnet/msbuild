a5e8f1564ab03c847df3b2b4916b1eb872ee6911
Surayya Huseyn Zada <114938397+surayya-MS@users.noreply.github.com>
2024-02-22 14:38:39 +0100

    Update src/Tasks.UnitTests/RoslynCodeTaskFactory_Tests.cs
    Co-authored-by: Rainer Sigwald <raines@microsoft.com>

--- src/Tasks.UnitTests/RoslynCodeTaskFactory_Tests.cs
+++ src/Tasks.UnitTests/RoslynCodeTaskFactory_Tests.cs
@@ -759,7 +759,7 @@ namespace InlineTask
             // Can't just compare `Name` because `ZipArchive` does not handle unix directory separators well
             // thus producing garbled fully qualified paths in the actual .ProjectImports.zip entries
             zipArchive.Entries.ShouldContain(zE => zE.Name.EndsWith($"{taskName}.cs"),
-                "");
+                $"Binlog's embedded files didn't have the expected {taskName}.cs.");
         }
 
 #if !FEATURE_RUN_EXE_IN_TESTS
