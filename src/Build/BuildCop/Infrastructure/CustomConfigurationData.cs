bddef4cd1f7c6b0e3b772437840ade284f9466f5
Jan Krivanek <jankrivanek@microsoft.com>
2024-02-29 18:45:21 +0100

    Troubleshoot test, comment

--- src/Build/BuildCop/Infrastructure/CustomConfigurationData.cs
+++ src/Build/BuildCop/Infrastructure/CustomConfigurationData.cs
@@ -10,13 +10,32 @@ using System.Threading.Tasks;
 
 namespace Microsoft.Build.Experimental.BuildCop;
 
+/// <summary>
+/// Holder for the key-value pairs of unstructured data from .editorconfig file,
+///  that were attribute to a particular rule, but were not recognized by the infrastructure.
+/// The configuration data that is recognized by the infrastructure is passed as <see cref="BuildAnalyzerConfiguration"/>.
+/// </summary>
+/// <param name="ruleId"></param>
 public class CustomConfigurationData(string ruleId)
 {
     public static CustomConfigurationData Null { get; } = new(string.Empty);
 
     public static bool NotNull(CustomConfigurationData data) => !Null.Equals(data);
 
+    /// <summary>
+    /// Identifier of the rule that the configuration data is for.
+    /// </summary>
     public string RuleId { get; init; } = ruleId;
+
+    /// <summary>
+    /// Key-value pairs of unstructured data from .editorconfig file.
+    /// E.g. if in editorconfig file we'd have:
+    /// [*.csrpoj]
+    /// build_analyzer.microsoft.BC0101.name_of_targets_to_restrict = "Build,CoreCompile,ResolveAssemblyReferences"
+    ///
+    /// the ConfigurationData would be:
+    /// "name_of_targets_to_restrict" -> "Build,CoreCompile,ResolveAssemblyReferences"
+    /// </summary>
     public IReadOnlyDictionary<string, string>? ConfigurationData { get; init; }
 
     public override bool Equals(object? obj)
