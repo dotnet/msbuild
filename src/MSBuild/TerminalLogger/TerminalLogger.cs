adf8171f1fd1773828a525ffd28f152d6b5e86c6
Michal Pavlik <mipavlik@microsoft.com>
2024-03-18 13:21:43 +0100

    Resolving conflicts

--- src/MSBuild/TerminalLogger/TerminalLogger.cs
+++ src/MSBuild/TerminalLogger/TerminalLogger.cs
@@ -5,7 +5,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Text.RegularExpressions;
 using System.Threading;
 using Microsoft.Build.Framework;
 using Microsoft.Build.Shared;
