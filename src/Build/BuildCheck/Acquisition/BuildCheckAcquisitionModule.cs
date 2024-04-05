1d7c4c635287a08e383a4f4486d7c4e949327a9e
Jan Krivanek <jankrivanek@microsoft.com>
2024-04-05 18:01:21 +0200

    Apply suggestions from code review
    Co-authored-by: Mariana Dematte <magarces@microsoft.com>

--- src/Build/BuildCheck/Acquisition/BuildCheckAcquisitionModule.cs
+++ src/Build/BuildCheck/Acquisition/BuildCheckAcquisitionModule.cs
@@ -16,7 +16,7 @@ internal class BuildCheckAcquisitionModule
     private static T Construct<T>() where T : new() => new();
     public BuildAnalyzerFactory CreateBuildAnalyzerFactory(AnalyzerAcquisitionData analyzerAcquisitionData)
     {
-        // Acquisition module - https://github.com/orgs/dotnet/projects/373/views/1?pane=issue&itemId=52643036
+        // Acquisition module - https://github.com/dotnet/msbuild/issues/9633
         return Construct<SharedOutputPathAnalyzer>;
     }
 }
