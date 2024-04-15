f744eb5b855c02c7a55490de6a16c688931fa618
Jan Krivanek <jankrivanek@microsoft.com>
2024-04-15 20:57:01 +0200

    Analyzers prototype proposal (#9735)
    * Initial demonstration version
    
    * Make analyzer test use bootstrap properly (#9733)
    
    * Hook analyzers stats stub
    
    * Fix unit tests by explicitly opting into analysis
    
    * Disable build acceleration for MSBuild.Bootstrap
    
    * Make EndToEndTests disposable
    
    * Support running Analyzers.UnitTests from stage1
    
    * Fix MSBuild.dll casing
    
    * Don't run netfx Analyzer.UnitTests in Windows Core builds
    
    * Fix Analyzers.UnitTests on Mac
    
    * Renaming changes
    
    * Renaming for clarity (#9754)
    
    * Removing unnecessary types
    
    * Code move
    
    * Adjust namespaces
    
    * Simplify TestEnvironments in EndToEndTests
    
    * Support for per-project configuration, Acquisition mounting, etc
    
    * Add more comments
    
    * Grace handle double initialization attempts
    
    * Fix tests
    
    * Troubleshoot test, comment
    
    * Reflect PR comments
    
    * Fix build
    
    * Fix build - proper multitargeting on core builds
    
    * Adjust API naming and exposure
    
    * Add forgotten acquisition data sending
    
    * Renaming BuildCop to BuildCheck (#9893)
    
    We decided to rename the analyzer project from BuildCop to BuildCheck. This PR is just reflecting that on the code.
    
    * Renamed a few files missed (#9900)
    
    * Add template for custom analyzers (#9789)
    
    * Run tests against just-built bootstrap environment
    
    * Reflecting PR comments
    
    * Add test without analysis
    
    * Rename tests project
    
    * Force case renaming
    
    * Simplify GlobalInstance initialization
    
    * Remove multiple registrations checking
    
    * Reflect on PR feedback
    
    * Apply suggestions from code review
    
    Co-authored-by: Mariana Dematte <magarces@microsoft.com>
    
    * Reflect on PR comments
    
    * Update src/Build/BuildCheck/API/BuildAnalyzerRule.cs
    
    Co-authored-by: Farhad Alizada <104755925+f-alizada@users.noreply.github.com>
    
    * Reflect PR comments
    
    ---------
    
    Co-authored-by: Ladi Prosek <laprosek@microsoft.com>
    Co-authored-by: Mariana Dematte <magarces@microsoft.com>
    Co-authored-by: YuliiaKovalova <95473390+YuliiaKovalova@users.noreply.github.com>
    Co-authored-by: Farhad Alizada <104755925+f-alizada@users.noreply.github.com>

---
+++ src/Build/BuildCheck/Infrastructure/BuildEventsProcessor.cs
@@ -0,0 +1,86 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Diagnostics.Tracing;
+using System.IO;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using Microsoft.Build.BackEnd;
+using Microsoft.Build.BackEnd.Components.Caching;
+using Microsoft.Build.BackEnd.Logging;
+using Microsoft.Build.BuildCheck.Analyzers;
+using Microsoft.Build.BuildCheck.Logging;
+using Microsoft.Build.Collections;
+using Microsoft.Build.Construction;
+using Microsoft.Build.Evaluation;
+using Microsoft.Build.Experimental.BuildCheck;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Shared;
+
+namespace Microsoft.Build.BuildCheck.Infrastructure;
+
+internal class BuildEventsProcessor(BuildCheckCentralContext buildCheckCentralContext)
+{
+    private readonly SimpleProjectRootElementCache _cache = new SimpleProjectRootElementCache();
+    private readonly BuildCheckCentralContext _buildCheckCentralContext = buildCheckCentralContext;
+
+    // This requires MSBUILDLOGPROPERTIESANDITEMSAFTEREVALUATION set to 1
+    internal void ProcessEvaluationFinishedEventArgs(
+        AnalyzerLoggingContext buildAnalysisContext,
+        ProjectEvaluationFinishedEventArgs evaluationFinishedEventArgs)
+    {
+        Dictionary<string, string> propertiesLookup = new Dictionary<string, string>();
+        Internal.Utilities.EnumerateProperties(evaluationFinishedEventArgs.Properties, propertiesLookup,
+            static (dict, kvp) => dict.Add(kvp.Key, kvp.Value));
+
+        EvaluatedPropertiesAnalysisData analysisData =
+            new(evaluationFinishedEventArgs.ProjectFile!, propertiesLookup);
+
+        _buildCheckCentralContext.RunEvaluatedPropertiesActions(analysisData, buildAnalysisContext, ReportResult);
+
+        if (_buildCheckCentralContext.HasParsedItemsActions)
+        {
+            ProjectRootElement xml = ProjectRootElement.OpenProjectOrSolution(
+                evaluationFinishedEventArgs.ProjectFile!, /*unused*/
+                null, /*unused*/null, _cache, false /*Not explicitly loaded - unused*/);
+
+            ParsedItemsAnalysisData itemsAnalysisData = new(evaluationFinishedEventArgs.ProjectFile!,
+                new ItemsHolder(xml.Items, xml.ItemGroups));
+
+            _buildCheckCentralContext.RunParsedItemsActions(itemsAnalysisData, buildAnalysisContext, ReportResult);
+        }
+    }
+
+    private static void ReportResult(
+        BuildAnalyzerWrapper analyzerWrapper,
+        LoggingContext loggingContext,
+        BuildAnalyzerConfigurationInternal[] configPerRule,
+        BuildCheckResult result)
+    {
+        if (!analyzerWrapper.BuildAnalyzer.SupportedRules.Contains(result.BuildAnalyzerRule))
+        {
+            loggingContext.LogErrorFromText(null, null, null,
+                BuildEventFileInfo.Empty,
+                $"The analyzer '{analyzerWrapper.BuildAnalyzer.FriendlyName}' reported a result for a rule '{result.BuildAnalyzerRule.Id}' that it does not support.");
+            return;
+        }
+
+        BuildAnalyzerConfigurationInternal config = configPerRule.Length == 1
+            ? configPerRule[0]
+            : configPerRule.First(r =>
+                r.RuleId.Equals(result.BuildAnalyzerRule.Id, StringComparison.CurrentCultureIgnoreCase));
+
+        if (!config.IsEnabled)
+        {
+            return;
+        }
+
+        BuildEventArgs eventArgs = result.ToEventArgs(config.Severity);
+        eventArgs.BuildEventContext = loggingContext.BuildEventContext;
+        loggingContext.LogBuildEvent(eventArgs);
+    }
+}
---
+++ src/Build/BuildCheck/Infrastructure/NullBuildCheckManager.cs
@@ -0,0 +1,51 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Build.BackEnd.Logging;
+using Microsoft.Build.BuildCheck.Acquisition;
+using Microsoft.Build.BuildCheck.Logging;
+using Microsoft.Build.Experimental.BuildCheck;
+using Microsoft.Build.Framework;
+
+namespace Microsoft.Build.BuildCheck.Infrastructure;
+
+internal class NullBuildCheckManager : IBuildCheckManager
+{
+    public void Shutdown() { }
+
+    public void ProcessEvaluationFinishedEventArgs(AnalyzerLoggingContext buildAnalysisContext,
+        ProjectEvaluationFinishedEventArgs projectEvaluationFinishedEventArgs)
+    { }
+
+    public void SetDataSource(BuildCheckDataSource buildCheckDataSource) { }
+    public void ProcessAnalyzerAcquisition(AnalyzerAcquisitionData acquisitionData) { }
+
+    public Dictionary<string, TimeSpan> CreateTracingStats() => throw new NotImplementedException();
+
+    public void FinalizeProcessing(LoggingContext loggingContext)
+    { }
+
+    public void StartProjectEvaluation(BuildCheckDataSource buildCheckDataSource, BuildEventContext buildEventContext,
+        string fullPath)
+    { }
+
+    public void EndProjectEvaluation(BuildCheckDataSource buildCheckDataSource, BuildEventContext buildEventContext)
+    { }
+
+    public void StartProjectRequest(BuildCheckDataSource buildCheckDataSource, BuildEventContext buildEventContext)
+    { }
+
+    public void EndProjectRequest(BuildCheckDataSource buildCheckDataSource, BuildEventContext buildEventContext)
+    { }
+
+    public void YieldProject(BuildCheckDataSource buildCheckDataSource, BuildEventContext buildEventContext)
+    { }
+
+    public void ResumeProject(BuildCheckDataSource buildCheckDataSource, BuildEventContext buildEventContext)
+    { }
+}
