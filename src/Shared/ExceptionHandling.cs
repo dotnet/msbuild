9eb3a0a9509387d75707109033315038de692989
YuliiaKovalova <95473390+YuliiaKovalova@users.noreply.github.com>
2024-05-24 17:11:58 +0200

    Update src/Shared/ExceptionHandling.cs
    Co-authored-by: Rainer Sigwald <raines@microsoft.com>

--- src/Shared/ExceptionHandling.cs
+++ src/Shared/ExceptionHandling.cs
@@ -374,7 +374,8 @@ namespace Microsoft.Build.Shared
             }
             
             // Some customers experience exceptions such as 'OutOfMemory' errors when msbuild attempts to log errors to a local file.
-            // This catch helps to prevent the application from crashing.
+            // This catch helps to prevent the application from crashing in this best-effort dump-diagnostics path,
+            // but doesn't prevent the overall crash from going to Watson.
             catch
             {
             }
