9580be36d336fe6ea96f6f2a3d36efd96193e4ec
Jan Krivanek <jankrivanek@microsoft.com>
2024-06-11 12:52:50 +0200

    Restructure the code, add exception for sealing OM

--- src/Build/BackEnd/BuildManager/BuildSubmission.cs
+++ src/Build/BackEnd/BuildManager/BuildSubmission.cs
@@ -20,89 +20,6 @@ namespace Microsoft.Build.Execution
         where TRequestData : BuildRequestDataBase
         where TResultData : BuildResultBase;
 
-    public abstract class BuildSubmissionBase
-    {
-        /// <summary>
-        /// The completion event.
-        /// </summary>
-        protected readonly ManualResetEvent CompletionEvent;
-
-        /// <summary>
-        /// Flag indicating if logging is done.
-        /// </summary>
-        internal bool LoggingCompleted { get; private set; }
-
-        /// <summary>
-        /// True if it has been invoked
-        /// </summary>
-        protected int CompletionInvoked;
-
-        //
-        // Unfortunately covariant overrides are not available for .NET 472,
-        //  so we have to use two set of properties for derived classes.
-        internal abstract BuildRequestDataBase BuildRequestDataBase { get; }
-
-        internal abstract BuildResultBase? BuildResultBase { get; }
-
-        /// <summary>
-        /// Constructor
-        /// </summary>
-        protected internal BuildSubmissionBase(BuildManager buildManager, int submissionId)
-        {
-            ErrorUtilities.VerifyThrowArgumentNull(buildManager, nameof(buildManager));
-
-            BuildManager = buildManager;
-            SubmissionId = submissionId;
-            CompletionEvent = new ManualResetEvent(false);
-            LoggingCompleted = false;
-            CompletionInvoked = 0;
-        }
-
-        /// <summary>
-        /// The BuildManager with which this submission is associated.
-        /// </summary>
-        public BuildManager BuildManager { get; }
-
-        /// <summary>
-        /// An ID uniquely identifying this request from among other submissions within the same build.
-        /// </summary>
-        public int SubmissionId { get; }
-
-        /// <summary>
-        /// The asynchronous context provided to <see cref="BuildSubmission{TRequestData,TResultData}.ExecuteAsync(BuildSubmissionCompleteCallback&lt;TRequestData, TResultData&gt;, object)"/>, if any.
-        /// </summary>
-        public object? AsyncContext { get; protected set; }
-
-        /// <summary>
-        /// A <see cref="System.Threading.WaitHandle"/> which will be signalled when the build is complete.  Valid after <see cref="BuildSubmission{TRequestData,TResultData}.Execute()"/> or <see cref="BuildSubmission{TRequestData,TResultData}.ExecuteAsync(BuildSubmissionCompleteCallback&lt;TRequestData, TResultData&gt;, object)"/> returns, otherwise null.
-        /// </summary>
-        public WaitHandle WaitHandle => CompletionEvent;
-
-        /// <summary>
-        /// Returns true if this submission is complete.
-        /// </summary>
-        public bool IsCompleted => WaitHandle.WaitOne(new TimeSpan(0));
-
-        /// <summary>
-        /// Whether the build has started.
-        /// </summary>
-        internal bool IsStarted { get; set; }
-
-        /// <summary>
-        /// Indicates that all logging events for this submission are complete.
-        /// </summary>
-        internal void CompleteLogging()
-        {
-            LoggingCompleted = true;
-            CheckForCompletion();
-        }
-
-        protected internal virtual void OnCompletition() { }
-        protected internal abstract void CheckForCompletion();
-
-        internal abstract BuildResultBase CompleteResultsWithException(Exception exception);
-    }
-
     public abstract class BuildSubmission<TRequestData, TResultData> : BuildSubmissionBase
         where TRequestData : BuildRequestDataBase
         where TResultData : BuildResultBase
@@ -131,7 +48,7 @@ namespace Microsoft.Build.Execution
         /// <summary>
         /// The results of the build per graph node.  Valid only after WaitHandle has become signalled.
         /// </summary>
-        public TResultData? BuildResult { get; private set; }
+        public TResultData? BuildResult { get; set; }
 
         /// <summary>
         /// The BuildRequestData being used for this submission.
