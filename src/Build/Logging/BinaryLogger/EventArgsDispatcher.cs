4c08a5eec0f15459d205e92d10775fb01a09870a
Surayya Huseyn Zada <shuseynzada@microsoft.com>
2024-06-13 12:55:37 +0200

    renamed BuildEventArgsDispatcher file to EventArgsDispatcher according to the class name

--- src/Build/Logging/BinaryLogger/EventArgsDispatcher.cs
+++ src/Build/Logging/BinaryLogger/EventArgsDispatcher.cs
@@ -106,6 +106,24 @@ namespace Microsoft.Build.Logging
         /// </summary>
         public void Dispatch(BuildEventArgs buildEvent)
         {
+            BuildWarningEventArgs warningEvent = null;
+            BuildErrorEventArgs errorEvent = null;
+            BuildMessageEventArgs messageEvent = null;
+
+            if ((warningEvent = buildEvent as BuildWarningEventArgs) != null &&
+                warningEvent.ProjectFile == null)
+            {
+            }
+            else if ((errorEvent = buildEvent as BuildErrorEventArgs) != null &&
+                errorEvent.ProjectFile == null)
+            {
+            }
+            else if ((messageEvent = buildEvent as BuildMessageEventArgs) != null &&
+                messageEvent.ProjectFile == null)
+            {
+            }
+
+
             if (buildEvent is BuildMessageEventArgs buildMessageEventArgs)
             {
                 MessageRaised?.Invoke(null, buildMessageEventArgs);
