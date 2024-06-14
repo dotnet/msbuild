78fcbe81fe13d45414d490f8c49a38b2f92b983f
MichalPavlik <michalpavlik@outlook.com>
2024-06-14 11:44:17 +0200

    Removed unnecessary header

--- src/Framework/IEventSource5.cs
+++ src/Framework/IEventSource5.cs
@@ -1,9 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-
-#nullable disable
-
-namespace Microsoft.Build.Framework
+﻿namespace Microsoft.Build.Framework
 {
     /// <summary>
     /// This interface defines the events raised by the build engine.
