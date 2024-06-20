3c12d57711574faa505d423b5005f2bbc3ba506f
AR-May <67507805+AR-May@users.noreply.github.com>
2024-06-20 16:52:17 +0200

    Disable CustomAnalyzerTest (#10278)

--- src/BuildCheck.UnitTests/EndToEndTests.cs
+++ src/BuildCheck.UnitTests/EndToEndTests.cs
@@ -187,7 +187,7 @@ public class EndToEndTests : IDisposable
         _env.SetEnvironmentVariable("MSBUILDLOGPROPERTIESANDITEMSAFTEREVALUATION", "1");
     }
 
-    [Theory]
+    [Theory(Skip = "https://github.com/dotnet/msbuild/issues/10277")]
     [InlineData("AnalysisCandidate", new[] { "CustomRule1", "CustomRule2" })]
     [InlineData("AnalysisCandidateWithMultipleAnalyzersInjected", new[] { "CustomRule1", "CustomRule2", "CustomRule3" }, true)]
     public void CustomAnalyzerTest(string analysisCandidate, string[] expectedRegisteredRules, bool expectedRejectedAnalyzers = false)
