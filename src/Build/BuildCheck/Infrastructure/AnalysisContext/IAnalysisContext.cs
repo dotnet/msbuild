965ad711826bfa6196fee6d9d09c385aa5de5a15
Surayya Huseyn Zada <shuseynzada@microsoft.com>
2024-06-21 13:59:50 +0200

    add summary for IAnalysisContext

--- src/Build/BuildCheck/Infrastructure/AnalysisContext/IAnalysisContext.cs
+++ src/Build/BuildCheck/Infrastructure/AnalysisContext/IAnalysisContext.cs
@@ -12,17 +12,32 @@ using Microsoft.Build.Shared;
 namespace Microsoft.Build.Experimental.BuildCheck;
 
 /// <summary>
-/// Interface for dispatching <see cref="BuildEventContext"/>.
+/// Interface that contains an instance of <see cref="BuildEventContext"/> and methods to dispatch it.
 /// </summary>
 internal interface IAnalysisContext
 {
+    /// <summary>
+    /// Instance of <see cref="BuildEventContext"/>.
+    /// </summary>
     BuildEventContext BuildEventContext { get; }
 
+    /// <summary>
+    /// Dispatch the instance of <see cref="BuildEventContext"/> as a comment.
+    /// </summary>
     void DispatchAsComment(MessageImportance importance, string messageResourceName, params object?[] messageArgs);
 
+    /// <summary>
+    /// Dispatch a <see cref="BuildEventArgs"/>.
+    /// </summary>
     void DispatchBuildEvent(BuildEventArgs buildEvent);
 
+    /// <summary>
+    /// Dispatch the instance of <see cref="BuildEventContext"/> as an error message.
+    /// </summary>
     void DispatchAsErrorFromText(string? subcategoryResourceName, string? errorCode, string? helpKeyword, BuildEventFileInfo file, string message);
 
+    /// <summary>
+    /// Dispatch the instance of <see cref="BuildEventContext"/> as a comment with provided text for the message.
+    /// </summary>
     void DispatchAsCommentFromText(MessageImportance importance, string message);
 }
