3c1b95814a757ad1b5eed9d40f7a04e50fc3a676
Surayya Huseyn Zada <shuseynzada@microsoft.com>
2024-06-27 17:28:53 +0200

    small fix

--- src/MSBuild.UnitTests/TerminalLogger_Tests.cs
+++ src/MSBuild.UnitTests/TerminalLogger_Tests.cs
@@ -6,7 +6,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
-using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading;
