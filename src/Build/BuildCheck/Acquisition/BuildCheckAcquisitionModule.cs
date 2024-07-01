f489f0d9607dd9c69aa2fc43d907a1f08b19f4d5
YuliiaKovalova <95473390+YuliiaKovalova@users.noreply.github.com>
2024-07-01 18:49:46 +0200

    fix compilation error

--- src/Build/BuildCheck/Acquisition/BuildCheckAcquisitionModule.cs
+++ src/Build/BuildCheck/Acquisition/BuildCheckAcquisitionModule.cs
@@ -46,8 +46,7 @@ internal class BuildCheckAcquisitionModule : IBuildCheckAcquisitionModule
             foreach (Type analyzerCandidate in analyzerTypes)
             {
                 analyzersFactories.Add(() => (BuildAnalyzer)Activator.CreateInstance(analyzerCandidate)!);
-
-                _loggingService.LogComment(buildEventContext, MessageImportance.Normal, "CustomAnalyzerRegistered", analyzerCandidate.Name, analyzerCandidate.Assembly);
+                analysisContext.DispatchAsComment(MessageImportance.Normal, "CustomAnalyzerRegistered", analyzerCandidate.Name, analyzerCandidate.Assembly);
             }
 
             if (availableTypes.Count != analyzerTypes.Count)
