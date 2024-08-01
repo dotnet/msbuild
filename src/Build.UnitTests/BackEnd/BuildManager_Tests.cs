7020f32c6809f04859da73058ae199d1587f1088
Farhad Alizada <falizada@microsoft.com>
2024-08-01 13:03:19 +0200

    To cleanup the envvar use null

--- src/Build.UnitTests/BackEnd/BuildManager_Tests.cs
+++ src/Build.UnitTests/BackEnd/BuildManager_Tests.cs
@@ -1516,7 +1516,7 @@ namespace Microsoft.Build.UnitTests.BackEnd
         /// <summary>
         /// A canceled build
         /// </summary>
-        [Fact(Timeout = 20_000)]
+        [Fact]
         public void CancelledBuild()
         {
             Console.WriteLine("Starting CancelledBuild test that is known to hang.");
