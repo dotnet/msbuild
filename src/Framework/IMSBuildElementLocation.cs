9664265acdcdf0010e0156b2eb8048025420bcb4
MichalPavlik <michalpavlik@outlook.com>
2024-08-28 14:15:20 +0200

    Fixed build

--- src/Framework/IMSBuildElementLocation.cs
+++ src/Framework/IMSBuildElementLocation.cs
@@ -1,7 +1,7 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-namespace Microsoft.Build.Experimental.BuildCheck;
+namespace Microsoft.Build.Shared;
 
 /// <summary>
 /// Represents the location information for error reporting purposes.  This is normally used to
