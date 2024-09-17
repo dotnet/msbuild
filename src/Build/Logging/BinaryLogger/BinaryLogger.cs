988196b1c64418351f46da7d3a9ff403560a4270
MichalPavlik <michalpavlik@outlook.com>
2024-09-17 14:03:55 +0200

    Fix for some tests which can fail due the environment setup (#10653)
    * Using invariant culture to make these tests stable
    
    * Removing env variable by using null (change in .net 9)

--- src/Build/Logging/BinaryLogger/BinaryLogger.cs
+++ src/Build/Logging/BinaryLogger/BinaryLogger.cs
@@ -311,7 +311,7 @@ namespace Microsoft.Build.Logging
         public void Shutdown()
         {
             Environment.SetEnvironmentVariable("MSBUILDTARGETOUTPUTLOGGING", _initialTargetOutputLogging);
-            Environment.SetEnvironmentVariable("MSBUILDLOGIMPORTS", _initialLogImports ? "1" : "");
+            Environment.SetEnvironmentVariable("MSBUILDLOGIMPORTS", _initialLogImports ? "1" : null);
             Environment.SetEnvironmentVariable("MSBUILDBINARYLOGGERENABLED", _initialIsBinaryLoggerEnabled);
 
             Traits.Instance.EscapeHatches.LogProjectImports = _initialLogImports;
