12981aafd4457d053120717518770aaf937cad55
YuliiaKovalova <ykovalova@microsoft.com>
2024-10-07 13:24:28 +0200

    cleanup

--- src/BuildCheck.UnitTests/MockBuildCheckRegistrationContext.cs
+++ src/BuildCheck.UnitTests/MockBuildCheckRegistrationContext.cs
@@ -3,7 +3,6 @@
 
 using System;
 using System.Collections.Generic;
-using Microsoft.Build.BuildCheck.OM;
 using Microsoft.Build.Experimental.BuildCheck;
 using Microsoft.Build.Experimental.BuildCheck.Infrastructure;
 
