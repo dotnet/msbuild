7e8b35bc2a3a38290998bad74d0fffb170ff0e27
Jan Krivanek <jankrivanek@microsoft.com>
2024-10-31 13:19:33 +0100

    Move the extensions codefile

--- src/Build/Logging/BuildEventArgsExtensions.cs
+++ src/Build/Logging/BuildEventArgsExtensions.cs
@@ -1,14 +1,9 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using Microsoft.Build.Evaluation;
 using Microsoft.Build.Framework;
-using static Microsoft.Build.Internal.Utilities;
 
 namespace Microsoft.Build.Logging;
 
