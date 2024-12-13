4fb0f98cd2d31c9ad24104ed667462f7d0b5680f
YuliiaKovalova <95473390+YuliiaKovalova@users.noreply.github.com>
2024-12-13 19:00:28 +0100

    Disable TestBuildCheckTemplate

--- src/BuildCheck.UnitTests/EndToEndTests.cs
+++ src/BuildCheck.UnitTests/EndToEndTests.cs
@@ -1,4 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System;
@@ -867,7 +867,7 @@ public class EndToEndTests : IDisposable
     }
 
 #if NET
-    [Fact]
+    [Fact(Skip = "To unblock: https://github.com/dotnet/msbuild/issues/11090")]
     public void TestBuildCheckTemplate()
     {
         TransientTestFolder workFolder = _env.CreateFolder(createFolder: true);
