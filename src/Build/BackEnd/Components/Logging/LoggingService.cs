8632fff105b904ea7ff350f165d1393dfae55dd5
YuliiaKovalova <95473390+YuliiaKovalova@users.noreply.github.com>
2025-01-31 11:11:25 +0100

    Remove nullable reference type operator in LoggingService.cs

--- src/Build/BackEnd/Components/Logging/LoggingService.cs
+++ src/Build/BackEnd/Components/Logging/LoggingService.cs
@@ -1,4 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System;
@@ -877,7 +877,7 @@ namespace Microsoft.Build.BackEnd.Logging
 
                 _buildEngineDataRouter = (buildComponentHost.GetComponent(BuildComponentType.BuildCheckManagerProvider) as IBuildCheckManagerProvider)?.BuildEngineDataRouter;
 
-                _buildCheckEnabled = buildComponentHost.BuildParameters!.IsBuildCheckEnabled;
+                _buildCheckEnabled = buildComponentHost.BuildParameters.IsBuildCheckEnabled;
             }
         }
 
