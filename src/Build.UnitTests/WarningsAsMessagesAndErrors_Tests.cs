798ab8acbc96ef8feb18ff77acb0b20953886ba3
Tomas Bartonek <tom.bartonek@volny.cz>
2025-02-10 09:41:34 +0100

    Remove unused import in test file

--- src/Build.UnitTests/WarningsAsMessagesAndErrors_Tests.cs
+++ src/Build.UnitTests/WarningsAsMessagesAndErrors_Tests.cs
@@ -1,4 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System;
@@ -9,7 +9,6 @@ using Microsoft.Build.UnitTests;
 using Shouldly;
 using Xunit;
 using Xunit.Abstractions;
-using static System.Net.WebRequestMethods;
 
 #nullable disable
 
