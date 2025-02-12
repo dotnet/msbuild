fbf50d3d52094703443c5750b4bc31d219e6af03
Christian Castaneda <chcasta@microsoft.com>
2025-02-11 19:28:45 -0800

    Use FileSystems.Default

--- src/Build/BackEnd/Components/Communications/RarNodeLauncher.cs
+++ src/Build/BackEnd/Components/Communications/RarNodeLauncher.cs
@@ -1,12 +1,15 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.Collections;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.IO.Pipes;
 using System.Linq;
 using Microsoft.Build.Internal;
 using Microsoft.Build.Shared;
+using Microsoft.Build.Shared.FileSystem;
 
 namespace Microsoft.Build.BackEnd
 {
@@ -55,7 +58,7 @@ namespace Microsoft.Build.BackEnd
             // If the node is running, we will find it in the list of named pipes.
             // TODO: Adapt for non-Windows platforms.
             const string NamedPipeRoot = @"\\.\pipe\";
-            string[] pipeNames = Directory.GetFiles(NamedPipeRoot);
+            IEnumerable<string> pipeNames = FileSystems.Default.EnumerateFiles(NamedPipeRoot);
 
             return pipeNames.Contains(Path.Combine(NamedPipeRoot, _pipeName));
         }
