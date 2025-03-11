1f645545c5fb868a240986568674723fd62ac2bf
Rainer Sigwald <raines@microsoft.com>
2025-03-11 16:53:04 -0500

    Revert "IsAsciiHexDigit behind a changewave (to be super conservative)"
    This reverts commit aa508b87d8c4a6b5560647a223f78fc8197a1bd5.
    
    No need to worry about speed on this path so let's just match existing behavior.

--- src/Build/Evaluation/Conditionals/CharacterUtilities.cs
+++ src/Build/Evaluation/Conditionals/CharacterUtilities.cs
@@ -3,8 +3,6 @@
 
 #nullable disable
 
-using Microsoft.Build.Framework;
-
 namespace Microsoft.Build.Evaluation
 {
     internal static class CharacterUtilities
@@ -26,18 +24,7 @@ namespace Microsoft.Build.Evaluation
 
         internal static bool IsHexDigit(char candidate)
         {
-            if (ChangeWaves.AreFeaturesEnabled(ChangeWaves.Wave17_14))
-            {
-#if NET
-                return char.IsAsciiHexDigit(candidate);
-#else
-                return (candidate - '0' <= '9' - '0') || ((uint)((candidate | 0x20) - 'a') <= 'f' - 'a');
-#endif
-            }
-            else
-            {
-                return char.IsDigit(candidate) || ((uint)((candidate | 0x20) - 'a') <= 'f' - 'a');
-            }
+            return char.IsDigit(candidate) || ((uint)((candidate | 0x20) - 'a') <= 'f' - 'a');
         }
     }
 }
