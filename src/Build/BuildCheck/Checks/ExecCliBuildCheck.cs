661a78322a723ca94ff3d3ccff0652e980c230ad
Ilia Shuliatikov <ilia.shuliatikov@gmail.com>
2025-03-21 19:42:06 +0100

    Fix logic with several separated commands when the build command is the last one

--- src/Build/BuildCheck/Checks/ExecCliBuildCheck.cs
+++ src/Build/BuildCheck/Checks/ExecCliBuildCheck.cs
@@ -1,4 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System;
@@ -75,7 +75,7 @@ internal sealed class ExecCliBuildCheck : Check
 
                 if (nextSeparatorIndex == -1)
                 {
-                    if (TryGetMatchingKnownBuildCommand(commandSpan, out var knownBuildCommand))
+                    if (TryGetMatchingKnownBuildCommand(commandSpan.Slice(start), out var knownBuildCommand))
                     {
                         context.ReportResult(BuildCheckResult.CreateBuiltIn(
                             SupportedRule,
