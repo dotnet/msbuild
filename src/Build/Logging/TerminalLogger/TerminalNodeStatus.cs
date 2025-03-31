86c337d97aeb523f199a092550f342d6befcdc58
Jenny Bai <v-jennybai@microsoft.com>
2025-03-31 11:25:41 +0800

    Fix IDE0005 error

--- src/Build/Logging/TerminalLogger/TerminalNodeStatus.cs
+++ src/Build/Logging/TerminalLogger/TerminalNodeStatus.cs
@@ -1,7 +1,9 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+#if DEBUG
 using System;
+#endif
 using Microsoft.Build.Framework.Logging;
 
 namespace Microsoft.Build.Logging;
