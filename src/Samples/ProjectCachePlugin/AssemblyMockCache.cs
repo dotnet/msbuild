24dc1a8655caea0c59ad8948c7d67881cee9518a
Jan Provazník <janprovaznik@microsoft.com>
2025-04-07 16:04:26 +0200

    fix: Sample was pinned to older version without the namespace change

--- src/Samples/ProjectCachePlugin/AssemblyMockCache.cs
+++ src/Samples/ProjectCachePlugin/AssemblyMockCache.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Build.Execution;
-using Microsoft.Build.ProjectCache;
+using Microsoft.Build.Experimental.ProjectCache;
 using Microsoft.Build.Framework;
 using Microsoft.Build.Graph;
 using Shouldly;
