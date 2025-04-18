96881801d67ff3cc05fae17125a76810a8a1c179
Jenny Bai <v-jennybai@microsoft.com>
2025-04-18 09:06:07 +0800

    Fix DefaultChannelsRequired parameter type issue

--- eng/common/post-build/publish-using-darc.ps1
+++ eng/common/post-build/publish-using-darc.ps1
@@ -37,7 +37,7 @@ try {
 
   if ($DefaultChannelsRequired) {
     $optionalParams.Add("--default-channels-required") | Out-Null
-    $optionalParams.Add("$True") | Out-Null
+    $optionalParams.Add(1) | Out-Null
   }
 
   & $darc add-build-to-channel `
