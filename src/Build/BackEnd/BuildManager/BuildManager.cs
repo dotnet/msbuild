4734a3be7d247ea6dbdf6161227476f72ce292a9
Surayya Huseyn Zada <shuseynzada@microsoft.com>
2025-04-28 16:58:30 +0200

    fix attaching binlog

--- src/Build/BackEnd/BuildManager/BuildManager.cs
+++ src/Build/BackEnd/BuildManager/BuildManager.cs
@@ -678,7 +678,7 @@ namespace Microsoft.Build.Execution
 
                 var logger = new BinaryLogger { Parameters = binlogPath };
 
-                return (loggers ?? [logger]);
+                return (loggers ?? []).Concat([logger]);
             }
 
             void InitializeCaches()
