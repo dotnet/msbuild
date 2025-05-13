1563b84803d45266c6116eec0fbaebc9bec691dd
Linh Nguyen <huulinhnguyen99@gmail.com>
2025-05-13 09:00:46 +0700

    Added the code have been deleted by mistake

--- src/MSBuild/XMake.cs
+++ src/MSBuild/XMake.cs
@@ -1045,6 +1045,13 @@ namespace Microsoft.Build.CommandLine
 
                 exitType = ExitType.Unexpected;
             }
+            catch (PathTooLongException e)
+            {
+                Console.WriteLine(
+                    $"{e.Message}{(e.InnerException != null ? $" {e.InnerException.Message}" : string.Empty)}");
+
+                exitType = ExitType.Unexpected;
+            }
             // handle fatal errors
             catch (Exception e)
             {
