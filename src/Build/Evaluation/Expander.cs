2a93726da6019f3bbb2ed987a15a6b7c9ff4b932
Eric Arndt <erarndt@microsoft.com>
2025-05-16 12:25:16 -0700

    Fix typo

--- src/Build/Evaluation/Expander.cs
+++ src/Build/Evaluation/Expander.cs
@@ -1863,7 +1863,7 @@ namespace Microsoft.Build.Evaluation
 
                 List<ExpressionShredder.ItemExpressionCapture> matches;
 
-                // PERF: you migh be tempted to switch this to call Contains(), but there is no string.Contains(char) overload
+                // PERF: you might be tempted to switch this to call Contains(), but there is no string.Contains(char) overload
                 // in framework. Instead, you'll call the LINQ overload and allocate an enumerator. We could use the
                 // string.Contains(string) overload instead, but it's slighly slower and you'll get a suggestion in
                 // newer .NET to use string.Contains(char) where that overload actually exists.
