2dc7fef4b24bba603c2d03231eb473cd0372b569
Christian Castaneda <chcasta@microsoft.com>
2025-05-20 16:30:53 -0700

    Disposal comment

--- src/Tasks/AssemblyDependency/ResolveAssemblyReference.cs
+++ src/Tasks/AssemblyDependency/ResolveAssemblyReference.cs
@@ -3255,7 +3255,7 @@ namespace Microsoft.Build.Tasks
             {
                 try
                 {
-#pragma warning disable CA2000 // Dispose objects before losing scope
+#pragma warning disable CA2000 // The OutOfProcRarClient is disposable but its disposal is handled by RegisterTaskObject.
                     _ = OutOfProcRarClient.GetInstance(buildEngine10).Execute(this);
 #pragma warning restore CA2000 // Dispose objects before losing scope
                     CommunicationsUtilities.Trace("RAR out-of-proc test connection completed. Executing task in-proc.");
