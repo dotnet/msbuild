93a9bbc9de0465e9c82544c0bcc7ca0c2984563e
huulinh99 <huulinhnguyen99@gmail.com>
2025-05-29 13:16:53 +0700

    Add method System.IO.Directory::Exists

--- src/Build/Resources/Constants.cs
+++ src/Build/Resources/Constants.cs
@@ -325,6 +325,7 @@ namespace Microsoft.Build.Internal
                         availableStaticMethods.TryAdd("System.IO.Directory::GetLastWriteTime", directoryType);
                         availableStaticMethods.TryAdd("System.IO.Directory::GetParent", directoryType);
                         availableStaticMethods.TryAdd("System.IO.Directory::Exists", directoryType);
+
                         availableStaticMethods.TryAdd("System.IO.File::Exists", fileType);
                         availableStaticMethods.TryAdd("System.IO.File::GetCreationTime", fileType);
                         availableStaticMethods.TryAdd("System.IO.File::GetAttributes", fileType);
