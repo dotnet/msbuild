890fc5ab28224aa398baf5b3e9f1df8ef0292c65
Christian Castaneda <chcasta@microsoft.com>
2025-06-06 17:56:29 -0700

    Perf: Avoid repeat stream allocations in read loop

--- src/Build/BackEnd/Components/Communications/NodeProviderOutOfProcBase.cs
+++ src/Build/BackEnd/Components/Communications/NodeProviderOutOfProcBase.cs
@@ -531,7 +531,7 @@ namespace Microsoft.Build.BackEnd
         /// <summary>
         /// Class which wraps up the communications infrastructure for a given node.
         /// </summary>
-        internal class NodeContext
+        internal sealed class NodeContext
         {
             private enum ExitPacketState
             {
@@ -541,18 +541,17 @@ namespace Microsoft.Build.BackEnd
             }
 
             // The pipe(s) used to communicate with the node.
-            private Stream _clientToServerStream;
-            private Stream _serverToClientStream;
+            private readonly Stream _clientToServerStream;
 
             /// <summary>
             /// The factory used to create packets from data read off the pipe.
             /// </summary>
-            private INodePacketFactory _packetFactory;
+            private readonly INodePacketFactory _packetFactory;
 
             /// <summary>
             /// The node id assigned by the node provider.
             /// </summary>
-            private int _nodeId;
+            private readonly int _nodeId;
 
             /// <summary>
             /// The node process.
@@ -564,24 +563,28 @@ namespace Microsoft.Build.BackEnd
             /// <summary>
             /// An array used to store the header byte for each packet when read.
             /// </summary>
-            private byte[] _headerByte;
+            private readonly byte[] _headerByte;
 
             /// <summary>
             /// A buffer typically big enough to handle a packet body.
             /// We use this as a convenient way to manage and cache a byte[] that's resized
             /// automatically to fit our payload.
             /// </summary>
-            private MemoryStream _readBufferMemoryStream;
+            private readonly MemoryStream _readBufferMemoryStream;
 
             /// <summary>
             /// A reusable buffer for writing packets.
             /// </summary>
-            private MemoryStream _writeBufferMemoryStream;
+            private readonly MemoryStream _writeBufferMemoryStream;
+
+            private readonly ITranslator _readTranslator;
+
+            private readonly ITranslator _writeTranslator;
 
             /// <summary>
             /// A queue used for enqueuing packets to write to the stream asynchronously.
             /// </summary>
-            private ConcurrentQueue<INodePacket> _packetWriteQueue = new ConcurrentQueue<INodePacket>();
+            private readonly ConcurrentQueue<INodePacket> _packetWriteQueue = new ConcurrentQueue<INodePacket>();
 
             /// <summary>
             /// A task representing the last packet write, so we can chain packet writes one after another.
@@ -593,17 +596,12 @@ namespace Microsoft.Build.BackEnd
             /// <summary>
             /// Delegate called when the context terminates.
             /// </summary>
-            private NodeContextTerminateDelegate _terminateDelegate;
+            private readonly NodeContextTerminateDelegate _terminateDelegate;
 
             /// <summary>
             /// Tracks the state of the packet sent to terminate the node.
             /// </summary>
             private ExitPacketState _exitPacketState;
-
-            /// <summary>
-            /// Per node read buffers
-            /// </summary>
-            private BinaryReaderFactory _binaryReaderFactory;
 
             /// <summary>
             /// Constructor.
@@ -615,13 +613,13 @@ namespace Microsoft.Build.BackEnd
                 _nodeId = nodeId;
                 _process = process;
                 _clientToServerStream = nodePipe;
-                _serverToClientStream = nodePipe;
                 _packetFactory = factory;
                 _headerByte = new byte[5]; // 1 for the packet type, 4 for the body length
                 _readBufferMemoryStream = new MemoryStream();
                 _writeBufferMemoryStream = new MemoryStream();
+                _readTranslator = BinaryTranslator.GetReadTranslator(_readBufferMemoryStream, InterningBinaryReader.CreateSharedBuffer());
+                _writeTranslator = BinaryTranslator.GetWriteTranslator(_writeBufferMemoryStream);
                 _terminateDelegate = terminateDelegate;
-                _binaryReaderFactory = InterningBinaryReader.CreateSharedBuffer();
             }
 
             /// <summary>
@@ -688,7 +686,7 @@ namespace Microsoft.Build.BackEnd
                     }
 
                     // Read and route the packet.
-                    if (!ReadAndRoutePacket(packetType, packetData, packetLength))
+                    if (!ReadAndRoutePacket(packetType))
                     {
                         return;
                     }
@@ -753,7 +751,7 @@ namespace Microsoft.Build.BackEnd
                             // clear the buffer but keep the underlying capacity to avoid reallocations
                             writeStream.SetLength(0);
 
-                            ITranslator writeTranslator = BinaryTranslator.GetWriteTranslator(writeStream);
+                            ITranslator writeTranslator = context._writeTranslator;
                             try
                             {
                                 writeStream.WriteByte((byte)packet.Type);
@@ -774,7 +772,7 @@ namespace Microsoft.Build.BackEnd
                                 {
                                     int lengthToWrite = Math.Min(writeStreamLength - i, MaxPacketWriteSize);
 #pragma warning disable CA1835 // Prefer the 'Memory'-based overloads for 'ReadAsync' and 'WriteAsync'
-                                    await context._serverToClientStream.WriteAsync(writeStreamBuffer, i, lengthToWrite, CancellationToken.None);
+                                    await context._clientToServerStream.WriteAsync(writeStreamBuffer, i, lengthToWrite, CancellationToken.None);
 #pragma warning restore CA1835 // Prefer the 'Memory'-based overloads for 'ReadAsync' and 'WriteAsync'
                                 }
 
@@ -819,10 +817,6 @@ namespace Microsoft.Build.BackEnd
             private void Close()
             {
                 _clientToServerStream.Dispose();
-                if (!object.ReferenceEquals(_clientToServerStream, _serverToClientStream))
-                {
-                    _serverToClientStream.Dispose();
-                }
                 _terminateDelegate(_nodeId);
             }
 
@@ -969,17 +963,14 @@ namespace Microsoft.Build.BackEnd
                 return true;
             }
 
-            private bool ReadAndRoutePacket(NodePacketType packetType, byte[] packetData, int packetLength)
+            private bool ReadAndRoutePacket(NodePacketType packetType)
             {
                 try
                 {
                     // The buffer is publicly visible so that InterningBinaryReader doesn't have to copy to an intermediate buffer.
                     // Since the buffer is publicly visible dispose right away to discourage outsiders from holding a reference to it.
-                    using (var packetStream = new MemoryStream(packetData, 0, packetLength, /*writeable*/ false, /*bufferIsPubliclyVisible*/ true))
-                    {
-                        ITranslator readTranslator = BinaryTranslator.GetReadTranslator(packetStream, _binaryReaderFactory);
-                        _packetFactory.DeserializeAndRoutePacket(_nodeId, packetType, readTranslator);
-                    }
+                    _readBufferMemoryStream.Position = 0;
+                    _packetFactory.DeserializeAndRoutePacket(_nodeId, packetType, _readTranslator);
                 }
                 catch (IOException e)
                 {
@@ -1032,7 +1023,7 @@ namespace Microsoft.Build.BackEnd
                 }
 
                 // Read and route the packet.
-                if (!ReadAndRoutePacket(packetType, packetData, packetLength))
+                if (!ReadAndRoutePacket(packetType))
                 {
                     return;
                 }
