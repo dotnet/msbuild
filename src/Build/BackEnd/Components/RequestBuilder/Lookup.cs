41c1bddb4c1de2c655cb501629b81ab199281bc6
Tevin Stanley <tevinstanley@microsoft.com>
2025-06-09 17:31:29 -0700

    use underlying dictionary to be able to use struct enumerator rather than getting boxed

--- src/Build/BackEnd/Components/RequestBuilder/Lookup.cs
+++ src/Build/BackEnd/Components/RequestBuilder/Lookup.cs
@@ -1156,7 +1156,7 @@ namespace Microsoft.Build.BackEnd
             /// <summary>
             /// Provides an enumeration of the explicit metadata modifications.
             /// </summary>
-            public IEnumerable<KeyValuePair<string, MetadataModification>> ExplicitModifications
+            public Dictionary<string, MetadataModification> ExplicitModifications
             {
                 get { return _modifications; }
             }
