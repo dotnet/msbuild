a5ee430612527b6ac1bad1b40e1836f6e64a79f0
YuliiaKovalova <95473390+YuliiaKovalova@users.noreply.github.com>
2025-06-19 08:32:06 +0200

    Fix for Build Requests with Different Flags Not Correctly Scheduled (#11862)

--- src/Build/BackEnd/BuildManager/BuildManager.cs
+++ src/Build/BackEnd/BuildManager/BuildManager.cs
@@ -2390,8 +2390,8 @@ namespace Microsoft.Build.Execution
                 }
             }
 
-            IEnumerable<ScheduleResponse> response = _scheduler!.ReportRequestBlocked(node, blocker);
-            PerformSchedulingActions(response);
+            IEnumerable<ScheduleResponse> responses = _scheduler!.ReportRequestBlocked(node, blocker);
+            PerformSchedulingActions(responses);
         }
 
         /// <summary>
