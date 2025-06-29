98f79db5ce2d38232ff441ee3e737d1e713ffc67
Chet Husk <chusk3@gmail.com>
2025-06-29 00:09:28 -0500

    Get SourceRoot handling working again. NOTE that this requires target output logging. Will need to flow this through in a structured way.

--- src/Build/Logging/TerminalLogger/ForwardingTerminalLogger.cs
+++ src/Build/Logging/TerminalLogger/ForwardingTerminalLogger.cs
@@ -1,7 +1,6 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System;
 using Microsoft.Build.Framework;
 
 namespace Microsoft.Build.Logging;
@@ -85,18 +84,6 @@ public sealed partial class ForwardingTerminalLogger : IForwardingLogger
         if (e.BuildEventContext is null)
         {
             return;
-        }
-
-        if (e is TaskParameterEventArgs taskArgs)
-        {
-            if (taskArgs.Kind == TaskParameterMessageKind.AddItem)
-            {
-                if (taskArgs.ItemType.Equals("SourceRoot", StringComparison.OrdinalIgnoreCase))
-                {
-                    BuildEventRedirector?.ForwardEvent(taskArgs);
-                    return;
-                }
-            }
         }
 
         if (e.Message is not null &&
