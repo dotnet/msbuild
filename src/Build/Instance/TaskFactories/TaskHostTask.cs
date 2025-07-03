b1d543a20f07e95d1585998170083c735afa2729
YuliiaKovalova <ykovalova@microsoft.com>
2025-07-03 14:06:06 +0200

    move to net10

--- src/Build/Instance/TaskFactories/TaskHostTask.cs
+++ src/Build/Instance/TaskFactories/TaskHostTask.cs
@@ -266,6 +266,7 @@ namespace Microsoft.Build.BackEnd
 
             TaskHostConfiguration hostConfiguration =
                 new TaskHostConfiguration(
+                        runtime,
                         _buildComponentHost.BuildParameters.NodeId,
                         NativeMethodsShared.GetCurrentDirectory(),
                         CommunicationsUtilities.GetEnvironmentVariables(),
@@ -581,10 +582,19 @@ namespace Microsoft.Build.BackEnd
         /// </summary>
         private void LogErrorUnableToCreateTaskHost(HandshakeOptions requiredContext, string runtime, string architecture, NodeFailedToLaunchException e)
         {
-            string msbuildLocation = NodeProviderOutOfProcTaskHost.GetMSBuildLocationFromHostContext(requiredContext) ??
+            string taskHostLocation = NodeProviderOutOfProcTaskHost.GetMSBuildExecutablePathForNonNETRuntimes(requiredContext);
+#if NETFRAMEWORK
+            if (Handshake.IsHandshakeOptionEnabled(requiredContext, HandshakeOptions.NET))
+            {
+                taskHostLocation = NodeProviderOutOfProcTaskHost.GetMSBuildLocationForNETRuntime(requiredContext).MSBuildAssemblyPath;
+            }
+#endif
+            string msbuildLocation = taskHostLocation ??
                 // We don't know the path -- probably we're trying to get a 64-bit assembly on a
                 // 32-bit machine.  At least give them the exe name to look for, though ...
-                ((requiredContext & HandshakeOptions.CLR2) == HandshakeOptions.CLR2 ? "MSBuildTaskHost.exe" : "MSBuild.exe");
+                ((requiredContext & HandshakeOptions.CLR2) == HandshakeOptions.CLR2 
+                ? "MSBuildTaskHost.exe" 
+                : NodeProviderOutOfProcTaskHost.GetTaskHostNameFromHostContext(requiredContext));
 
             if (e == null)
             {
