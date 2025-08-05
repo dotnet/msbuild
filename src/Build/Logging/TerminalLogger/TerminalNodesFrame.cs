d3e1c2e79078cb215f19cd18d90332affb51ce04
Chet Husk <chusk3@gmail.com>
2025-08-05 17:58:38 -0500

    Detect and use RIDs in project build reporting

--- src/Build/Logging/TerminalLogger/TerminalNodesFrame.cs
+++ src/Build/Logging/TerminalLogger/TerminalNodesFrame.cs
@@ -51,6 +51,7 @@ internal sealed class TerminalNodesFrame
 
         string project = status.Project;
         string? targetFramework = status.TargetFramework;
+        string? runtimeIdentifier = status.RuntimeIdentifier;
         string target = status.Target;
         string? targetPrefix = status.TargetPrefix;
         TerminalColor targetPrefixColor = status.TargetPrefixColor;
@@ -60,7 +61,7 @@ internal sealed class TerminalNodesFrame
             ? targetPrefix!.Length + 1 + target.Length
             : target.Length;
 
-        int renderedWidth = Length(durationString, project, targetFramework, targetWithoutAnsiLength);
+        int renderedWidth = Length(durationString, project, targetFramework, runtimeIdentifier, targetWithoutAnsiLength);
 
         if (renderedWidth > Width)
         {
@@ -82,12 +83,13 @@ internal sealed class TerminalNodesFrame
         }
 
         var renderedTarget = !string.IsNullOrWhiteSpace(targetPrefix) ? $"{AnsiCodes.Colorize(targetPrefix, targetPrefixColor)} {target}" : target;
-        return $"{TerminalLogger.Indentation}{project}{(targetFramework is null ? string.Empty : " ")}{AnsiCodes.Colorize(targetFramework, TerminalLogger.TargetFrameworkColor)} {AnsiCodes.SetCursorHorizontal(MaxColumn)}{AnsiCodes.MoveCursorBackward(targetWithoutAnsiLength + durationString.Length + 1)}{renderedTarget} {durationString}".AsSpan();
+        return $"{TerminalLogger.Indentation}{project}{(targetFramework is null ? string.Empty : " ")}{AnsiCodes.Colorize(targetFramework, TerminalLogger.TargetFrameworkColor)} {AnsiCodes.Colorize(runtimeIdentifier, TerminalLogger.RuntimeIdentifierColor)} {AnsiCodes.SetCursorHorizontal(MaxColumn)}{AnsiCodes.MoveCursorBackward(targetWithoutAnsiLength + durationString.Length + 1)}{renderedTarget} {durationString}".AsSpan();
 
-        static int Length(string durationString, string project, string? targetFramework, int targetWithoutAnsiLength) =>
+        static int Length(string durationString, string project, string? targetFramework, string? runtimeIdentifier, int targetWithoutAnsiLength) =>
                 TerminalLogger.Indentation.Length +
                 project.Length + 1 +
                 (targetFramework?.Length ?? -1) + 1 +
+                (runtimeIdentifier?.Length ?? -1) + 1 +
                 targetWithoutAnsiLength + 1 +
                 durationString.Length;
     }
