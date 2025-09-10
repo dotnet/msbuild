323664b817621d45a6d49e9252c3599d1463231d
YuliiaKovalova <ykovalova@microsoft.com>
2025-09-10 14:15:16 +0200

    fix TaskAssemblyLocationMismatch message

--- src/Build.UnitTests/NetTaskHost_E2E_Tests.cs
+++ src/Build.UnitTests/NetTaskHost_E2E_Tests.cs
@@ -4,6 +4,7 @@
 using System;
 using System.IO;
 using Microsoft.Build.Internal;
+using Microsoft.Build.Shared;
 using Microsoft.Build.UnitTests;
 using Microsoft.Build.UnitTests.Shared;
 using Shouldly;
@@ -14,6 +15,8 @@ namespace Microsoft.Build.Engine.UnitTests
 {
     public class NetTaskHost_E2E_Tests
     {
+        private const string LatestDotNetCoreForMSBuild = "net10.0";
+
         private static string AssemblyLocation { get; } = Path.Combine(Path.GetDirectoryName(typeof(NetTaskHost_E2E_Tests).Assembly.Location) ?? AppContext.BaseDirectory);
 
         private static string TestAssetsRootPath { get; } = Path.Combine(AssemblyLocation, "TestAssets");
@@ -34,7 +37,7 @@ namespace Microsoft.Build.Engine.UnitTests
             // Environment variables needed to discover the .NET Core SDK from bootstrap folder.
             string testProjectPath = Path.Combine(TestAssetsRootPath, "ExampleNetTask", "TestNetTask", "TestNetTask.csproj");
 
-            string testTaskOutput = RunnerUtilities.ExecBootstrapedMSBuild($"{testProjectPath} -restore -v:n", out bool successTestTask);
+            string testTaskOutput = RunnerUtilities.ExecBootstrapedMSBuild($"{testProjectPath} -restore -v:n", out bool successTestTask, timeoutMilliseconds: 30000000);
 
             if (!successTestTask)
             {
@@ -44,6 +47,15 @@ namespace Microsoft.Build.Engine.UnitTests
             successTestTask.ShouldBeTrue();
             testTaskOutput.ShouldContain($"The task is executed in process: dotnet");
             testTaskOutput.ShouldContain($"Process path: {bootstrapCorePath}", customMessage: testTaskOutput);
+
+            var customTaskAssemblyLocation = Path.GetFullPath(Path.Combine(AssemblyLocation, "..", LatestDotNetCoreForMSBuild, "ExampleTask.dll"));
+            var resource = ResourceUtilities.FormatResourceStringIgnoreCodeAndKeyword(
+               "TaskAssemblyLocationMismatch",
+
+               // Microsoft.Build.dll represents TaskHostTask wrapper for the custom task here.
+               Path.Combine(RunnerUtilities.BootstrapRootPath, "net472", "MSBuild", "Current", "Bin", "Microsoft.Build.dll"),
+               customTaskAssemblyLocation);
+            testTaskOutput.ShouldNotContain(resource);
         }
     }
 }
