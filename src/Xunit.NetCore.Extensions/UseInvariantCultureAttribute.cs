3019f6d49d9a222627d689c65e2650a72d00392f
MichalPavlik <michalpavlik@outlook.com>
2025-10-23 11:01:41 +0200

    Introduces attribute to set invariant culture for tests

---
+++ src/Xunit.NetCore.Extensions/UseInvariantCultureAttribute.cs
@@ -0,0 +1,37 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit.Sdk;
+
+namespace Xunit.NetCore.Extensions
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
+    public class UseInvariantCultureAttribute : BeforeAfterTestAttribute
+    {
+        private CultureInfo originalCulture = CultureInfo.CurrentCulture;
+        private CultureInfo originalUICulture = CultureInfo.CurrentUICulture    ;
+
+        public override void Before(MethodInfo methodUnderTest)
+        {
+            originalCulture = Thread.CurrentThread.CurrentCulture;
+            originalUICulture = Thread.CurrentThread.CurrentUICulture;
+
+            Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+            Thread.CurrentThread.CurrentUICulture = CultureInfo.InvariantCulture;
+        }
+
+        public override void After(MethodInfo methodUnderTest)
+        {
+            Thread.CurrentThread.CurrentCulture = originalCulture;
+            Thread.CurrentThread.CurrentUICulture = originalUICulture;
+        }
+    }
+}
