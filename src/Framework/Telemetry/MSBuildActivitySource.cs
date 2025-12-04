494ee525f1d6df31a99a17cbfcbe8b81054130c9
YuliiaKovalova <ykovalova@microsoft.com>
2025-12-04 11:20:51 +0100

    namespace

--- src/Framework/Telemetry/MSBuildActivitySource.cs
+++ src/Framework/Telemetry/MSBuildActivitySource.cs
@@ -1,9 +1,10 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System.Diagnostics;
 #if NETFRAMEWORK
 using Microsoft.VisualStudio.Telemetry;
+#else
+using System.Diagnostics;
 #endif
 
 namespace Microsoft.Build.Framework.Telemetry
