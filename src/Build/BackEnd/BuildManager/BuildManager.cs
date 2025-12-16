b9b82dd26b488d1a424955a2682fe5a65f6b418a
Chet Husk <chusk3@gmail.com>
2025-12-16 17:06:51 -0600

    Fix this spot too

--- src/Build/BackEnd/BuildManager/BuildManager.cs
+++ src/Build/BackEnd/BuildManager/BuildManager.cs
@@ -2558,7 +2558,7 @@ namespace Microsoft.Build.Execution
                     BuildEventContext buildEventContext = _projectStartedEvents.TryGetValue(result.SubmissionId, out BuildEventArgs? buildEventArgs)
                         ? buildEventArgs.BuildEventContext!
                         : BuildEventContext.CreateInitial(result.SubmissionId, node)
-                            .WithEvaluationId(configuration.Project?.EvaluationId ?? BuildEventContext.InvalidEvaluationId)
+                            .WithEvaluationId(configuration.ProjectEvaluationId)
                             .WithProjectInstanceId(configuration.ConfigurationId);
                     try
                     {
