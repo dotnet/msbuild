c3af4ebdad90fb7fd1e017904e967d3c00fe16e4
YuliiaKovalova <ykovalova@microsoft.com>
2026-01-02 20:05:23 +0100

    remove extra change

--- src/Build.UnitTests/BackEnd/BuildManager_Tests.cs
+++ src/Build.UnitTests/BackEnd/BuildManager_Tests.cs
@@ -1801,7 +1801,6 @@ namespace Microsoft.Build.UnitTests.BackEnd
  </Target>
 </Project>
 ");
-            _env.SetEnvironmentVariable("MSBUILDNOINPROCNODE", "1");
             Project project = CreateProject(contents, MSBuildDefaultToolsVersion, _projectCollection, true);
             ProjectInstance instance = _buildManager.GetProjectInstanceForBuild(project);
             _buildManager.BeginBuild(_parameters);
