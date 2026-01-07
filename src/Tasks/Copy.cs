65f0d01c7222956b4e295736d53babd00a2cd91c
Jan Provazník <janprovaznik@microsoft.com>
2026-01-07 12:39:42 +0100

    revert wrong change

--- src/Tasks/Copy.cs
+++ src/Tasks/Copy.cs
@@ -378,7 +378,7 @@ namespace Microsoft.Build.Tasks
                 // Do not log a fake command line as well, as it's superfluous, and also potentially expensive
                 Log.LogMessage(MessageImportance.Normal, FileComment, sourceFileState.FileNameFullPath, destinationFileState.FileNameFullPath);
 
-                File.Copy(sourceFileState.FileNameFullPath, destinationFileState.FileNameFullPath, true);
+                File.Copy(sourceFileState.Name, destinationFileState.Name, true);
             }
 
             // If the destinationFile file exists, then make sure it's read-write.
