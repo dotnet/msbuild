3db473bb781b243940e999a875b4e658ba3e60dc
Rainer Sigwald <raines@microsoft.com>
2026-01-30 09:38:36 -0600

    Remove redundant #nullable disable from 21 Framework files
    These files are already nullable-clean and don't need the directive.

--- src/Framework/XamlTypes/ProjectSchemaDefinitions.cs
+++ src/Framework/XamlTypes/ProjectSchemaDefinitions.cs
@@ -1,4 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System;
@@ -6,8 +6,6 @@ using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Windows.Markup;
-
-#nullable disable
 
 namespace Microsoft.Build.Framework.XamlTypes
 {
