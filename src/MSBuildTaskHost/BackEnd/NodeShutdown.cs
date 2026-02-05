ea0e75a25da54b31e914ead42504d0575d8ee978
Dustin Campbell <dustin@teamcampbell.org>
2026-02-05 14:38:39 -0800

    MSBuildTaskHost: Clean up NodeShutdown
    - File-scoped namespace
    - Expression-bodied members
    - Enable nullability

--- src/MSBuildTaskHost/BackEnd/NodeShutdown.cs
+++ src/MSBuildTaskHost/BackEnd/NodeShutdown.cs
@@ -3,121 +3,92 @@
 
 using System;
 
-#nullable disable
+namespace Microsoft.Build.TaskHost.BackEnd;
 
-namespace Microsoft.Build.TaskHost.BackEnd
+/// <summary>
+/// Reasons why the node shut down.
+/// </summary>
+internal enum NodeShutdownReason
 {
     /// <summary>
-    /// Reasons why the node shut down.
+    /// The node shut down because it was requested to shut down.
     /// </summary>
-    internal enum NodeShutdownReason
+    Requested,
+
+    /// <summary>
+    /// The node shut down because of an error.
+    /// </summary>
+    Error,
+
+    /// <summary>
+    /// The node shut down because the connection failed.
+    /// </summary>
+    ConnectionFailed,
+}
+
+/// <summary>
+/// Implementation of INodePacket for the packet informing the build manager than a node has shut down.
+/// This is the last packet the BuildManager will receive from a Node, and as such can be used to trigger
+/// any appropriate cleanup behavior.
+/// </summary>
+internal sealed class NodeShutdown : INodePacket
+{
+    /// <summary>
+    /// The reason the node shut down.
+    /// </summary>
+    private NodeShutdownReason _reason;
+
+    /// <summary>
+    /// The exception - if any.
+    /// </summary>
+    private Exception? _exception;
+
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    public NodeShutdown(NodeShutdownReason reason)
+        : this(reason, exception: null)
     {
-        /// <summary>
-        /// The node shut down because it was requested to shut down.
-        /// </summary>
-        Requested,
+    }
 
-        /// <summary>
-        /// The node shut down because of an error.
-        /// </summary>
-        Error,
+    public NodeShutdown(NodeShutdownReason reason, Exception? exception)
+    {
+        _reason = reason;
+        _exception = exception;
+    }
 
-        /// <summary>
-        /// The node shut down because the connection failed.
-        /// </summary>
-        ConnectionFailed,
+    private NodeShutdown()
+    {
     }
 
     /// <summary>
-    /// Implementation of INodePacket for the packet informing the build manager than a node has shut down.
-    /// This is the last packet the BuildManager will receive from a Node, and as such can be used to trigger
-    /// any appropriate cleanup behavior.
+    /// Gets the packet type.
     /// </summary>
-    internal class NodeShutdown : INodePacket
+    public NodePacketType Type => NodePacketType.NodeShutdown;
+
+    /// <summary>
+    /// Gets the reason for shutting down.
+    /// </summary>
+    public NodeShutdownReason Reason => _reason;
+
+    /// <summary>
+    /// Gets the exception, if any.
+    /// </summary>
+    public Exception? Exception => _exception;
+
+    /// <summary>
+    /// Serializes or deserializes a packet.
+    /// </summary>
+    public void Translate(ITranslator translator)
     {
-        /// <summary>
-        /// The reason the node shut down.
-        /// </summary>
-        private NodeShutdownReason _reason;
+        translator.TranslateEnum(ref _reason, (int)_reason);
+        translator.TranslateException(ref _exception);
+    }
 
-        /// <summary>
-        /// The exception - if any.
-        /// </summary>
-        private Exception _exception;
-
-        /// <summary>
-        /// Constructor
-        /// </summary>
-        public NodeShutdown(NodeShutdownReason reason)
-            : this(reason, null)
-        {
-        }
-
-        /// <summary>
-        /// Constructor
-        /// </summary>
-        public NodeShutdown(NodeShutdownReason reason, Exception e)
-        {
-            _reason = reason;
-            _exception = e;
-        }
-
-        /// <summary>
-        /// Constructor for deserialization
-        /// </summary>
-        private NodeShutdown()
-        {
-        }
-
-        #region INodePacket Members
-
-        /// <summary>
-        /// Returns the packet type.
-        /// </summary>
-        public NodePacketType Type
-        {
-            get { return NodePacketType.NodeShutdown; }
-        }
-
-        #endregion
-
-        /// <summary>
-        /// The reason for shutting down.
-        /// </summary>
-        public NodeShutdownReason Reason
-        {
-            get { return _reason; }
-        }
-
-        /// <summary>
-        /// The exception, if any.
-        /// </summary>
-        public Exception Exception
-        {
-            get { return _exception; }
-        }
-
-        #region INodePacketTranslatable Members
-
-        /// <summary>
-        /// Serializes or deserializes a packet.
-        /// </summary>
-        public void Translate(ITranslator translator)
-        {
-            translator.TranslateEnum(ref _reason, (int)_reason);
-            translator.TranslateException(ref _exception);
-        }
-
-        /// <summary>
-        /// Factory method for deserialization
-        /// </summary>
-        internal static NodeShutdown FactoryForDeserialization(ITranslator translator)
-        {
-            NodeShutdown shutdown = new NodeShutdown();
-            shutdown.Translate(translator);
-            return shutdown;
-        }
-
-        #endregion
+    internal static NodeShutdown FactoryForDeserialization(ITranslator translator)
+    {
+        var shutdown = new NodeShutdown();
+        shutdown.Translate(translator);
+        return shutdown;
     }
 }
