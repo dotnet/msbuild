f64ad9d39b4073dd62acc8ff0c736d1ae585138b
Dustin Campbell <dustin@teamcampbell.org>
2026-02-05 15:39:50 -0800

    MSBuildTaskHost: Clean up TaskParameterTypeVerifier
    - File-scoped namespace
    - Expression-bodied members
    - Enable nullability

--- src/MSBuildTaskHost/BackEnd/TaskParameterTypeVerifier.cs
+++ src/MSBuildTaskHost/BackEnd/TaskParameterTypeVerifier.cs
@@ -4,68 +4,52 @@
 using System;
 using Microsoft.Build.Framework;
 
-#nullable disable
+namespace Microsoft.Build.TaskHost.BackEnd;
 
-namespace Microsoft.Build.TaskHost.BackEnd
+/// <summary>
+/// Provide a class which can verify the correct type for both input and output parameters.
+/// </summary>
+internal static class TaskParameterTypeVerifier
 {
     /// <summary>
-    /// Provide a class which can verify the correct type for both input and output parameters.
+    /// Is the parameter type a valid scalar input value.
     /// </summary>
-    internal static class TaskParameterTypeVerifier
-    {
-        /// <summary>
-        /// Is the parameter type a valid scalar input value
-        /// </summary>
-        internal static bool IsValidScalarInputParameter(Type parameterType) =>
-            parameterType.IsValueType || parameterType == typeof(string) || parameterType == typeof(ITaskItem);
+    internal static bool IsValidScalarInputParameter(Type parameterType)
+        => parameterType.IsValueType || parameterType == typeof(string) || parameterType == typeof(ITaskItem);
 
-        /// <summary>
-        /// Is the passed in parameterType a valid vector input parameter
-        /// </summary>
-        internal static bool IsValidVectorInputParameter(Type parameterType)
-        {
-            bool result = (parameterType.IsArray && parameterType.GetElementType().IsValueType) ||
-                        parameterType == typeof(string[]) ||
-                        parameterType == typeof(ITaskItem[]);
-            return result;
-        }
+    /// <summary>
+    /// Is the passed in parameterType a valid vector input parameter.
+    /// </summary>
+    internal static bool IsValidVectorInputParameter(Type parameterType)
+        => (parameterType.IsArray && parameterType.GetElementType().IsValueType) ||
+            parameterType == typeof(string[]) ||
+            parameterType == typeof(ITaskItem[]);
 
-        /// <summary>
-        /// Is the passed in value type assignable to an ITask or Itask[] object
-        /// </summary>
-        internal static bool IsAssignableToITask(Type parameterType)
-        {
-            bool result = typeof(ITaskItem[]).IsAssignableFrom(parameterType) ||    /* ITaskItem array or derived type, or */
-                          typeof(ITaskItem).IsAssignableFrom(parameterType);        /* ITaskItem or derived type */
-            return result;
-        }
+    /// <summary>
+    /// Is the passed in value type assignable to an ITask or ITask[] object.
+    /// </summary>
+    internal static bool IsAssignableToITask(Type parameterType)
+        => typeof(ITaskItem[]).IsAssignableFrom(parameterType) || // ITaskItem array or derived type, or
+           typeof(ITaskItem).IsAssignableFrom(parameterType);     // ITaskItem or derived type
 
-        /// <summary>
-        /// Is the passed parameter a valid value type output parameter
-        /// </summary>
-        internal static bool IsValueTypeOutputParameter(Type parameterType)
-        {
-            bool result = (parameterType.IsArray && parameterType.GetElementType().IsValueType) ||    /* array of value types, or */
-                          parameterType == typeof(string[]) ||                                        /* string array, or */
-                          parameterType.IsValueType ||                                                /* value type, or */
-                          parameterType == typeof(string);                                            /* string */
-            return result;
-        }
+    /// <summary>
+    /// Is the passed parameter a valid value type output parameter.
+    /// </summary>
+    internal static bool IsValueTypeOutputParameter(Type parameterType)
+        => (parameterType.IsArray && parameterType.GetElementType().IsValueType) || // array of value types, or
+            parameterType == typeof(string[]) ||                                    // string array, or 
+            parameterType.IsValueType ||                                            // value type, or
+            parameterType == typeof(string);                                        // string
 
-        /// <summary>
-        /// Is the parameter type a valid scalar or value type input parameter
-        /// </summary>
-        internal static bool IsValidInputParameter(Type parameterType)
-        {
-            return IsValidScalarInputParameter(parameterType) || IsValidVectorInputParameter(parameterType);
-        }
+    /// <summary>
+    /// Is the parameter type a valid scalar or value type input parameter.
+    /// </summary>
+    internal static bool IsValidInputParameter(Type parameterType)
+        => IsValidScalarInputParameter(parameterType) || IsValidVectorInputParameter(parameterType);
 
-        /// <summary>
-        /// Is the parameter type a valid scalar or value type output parameter
-        /// </summary>
-        internal static bool IsValidOutputParameter(Type parameterType)
-        {
-            return IsValueTypeOutputParameter(parameterType) || IsAssignableToITask(parameterType);
-        }
-    }
+    /// <summary>
+    /// Is the parameter type a valid scalar or value type output parameter.
+    /// </summary>
+    internal static bool IsValidOutputParameter(Type parameterType)
+        => IsValueTypeOutputParameter(parameterType) || IsAssignableToITask(parameterType);
 }
