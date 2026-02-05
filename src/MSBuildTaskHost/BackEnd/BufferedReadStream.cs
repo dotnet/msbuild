f07f4ce9597eadc323cf3e614723ffa5e5f11edd
Dustin Campbell <dustin@teamcampbell.org>
2026-02-05 10:20:14 -0800

    MSBuildTaskHost: Clean up BufferedReadStream
    - File-scoped namespace
    - Expression-bodied members
    - Primary constructor
    - Enable nullability

--- src/MSBuildTaskHost/BackEnd/BufferedReadStream.cs
+++ src/MSBuildTaskHost/BackEnd/BufferedReadStream.cs
@@ -5,145 +5,121 @@ using System;
 using System.IO;
 using System.IO.Pipes;
 
-#nullable disable
+namespace Microsoft.Build.TaskHost.BackEnd;
 
-namespace Microsoft.Build.TaskHost.BackEnd
+internal sealed class BufferedReadStream(NamedPipeServerStream innerStream) : Stream
 {
-    internal class BufferedReadStream : Stream
+    private const int BUFFER_SIZE = 1024;
+
+    private readonly NamedPipeServerStream _innerStream = innerStream;
+    private readonly byte[] _buffer = new byte[BUFFER_SIZE];
+
+    // The number of bytes in the buffer that have been read from the underlying stream but not read by consumers of this stream
+    private int _currentlyBufferedByteCount = 0;
+    private int _currentIndexInBuffer;
+
+    public override bool CanRead => _innerStream.CanRead;
+
+    public override bool CanSeek => false;
+
+    public override bool CanWrite => _innerStream.CanWrite;
+
+    public override long Length => _innerStream.Length;
+
+    public override long Position
     {
-        private const int BUFFER_SIZE = 1024;
-        private NamedPipeServerStream _innerStream;
-        private byte[] _buffer;
+        get => throw new NotSupportedException();
+        set => throw new NotSupportedException();
+    }
 
-        // The number of bytes in the buffer that have been read from the underlying stream but not read by consumers of this stream
-        private int _currentlyBufferedByteCount;
-        private int _currentIndexInBuffer;
-
-        public BufferedReadStream(NamedPipeServerStream innerStream)
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing)
         {
-            _innerStream = innerStream;
-            _buffer = new byte[BUFFER_SIZE];
-
-            _currentlyBufferedByteCount = 0;
+            _innerStream.Dispose();
         }
 
-        public override bool CanRead { get { return _innerStream.CanRead; } }
+        base.Dispose(disposing);
+    }
 
-        public override bool CanSeek { get { return false; } }
+    public override void Flush()
+        => _innerStream.Flush();
 
-        public override bool CanWrite { get { return _innerStream.CanWrite; } }
-
-        public override long Length { get { return _innerStream.Length; } }
-
-        public override long Position
+    public override int ReadByte()
+    {
+        if (_currentlyBufferedByteCount > 0)
         {
-            get { throw new NotSupportedException(); }
-            set { throw new NotSupportedException(); }
+            int ret = _buffer[_currentIndexInBuffer];
+            _currentIndexInBuffer++;
+            _currentlyBufferedByteCount--;
+            return ret;
         }
-
-        public override void Flush()
+        else
         {
-            _innerStream.Flush();
-        }
-
-        public override int ReadByte()
-        {
-            if (_currentlyBufferedByteCount > 0)
-            {
-                int ret = _buffer[_currentIndexInBuffer];
-                _currentIndexInBuffer++;
-                _currentlyBufferedByteCount--;
-                return ret;
-            }
-            else
-            {
-                // Let the base class handle it, which will end up calling the Read() method
-                return base.ReadByte();
-            }
-        }
-
-        public override int Read(byte[] buffer, int offset, int count)
-        {
-            if (count > BUFFER_SIZE)
-            {
-                // Trying to read more data than the buffer can hold
-                int alreadyCopied = 0;
-                if (_currentlyBufferedByteCount > 0)
-                {
-                    Array.Copy(_buffer, _currentIndexInBuffer, buffer, offset, _currentlyBufferedByteCount);
-                    alreadyCopied = _currentlyBufferedByteCount;
-                    _currentIndexInBuffer = 0;
-                    _currentlyBufferedByteCount = 0;
-                }
-                int innerReadCount = _innerStream.Read(buffer, offset + alreadyCopied, count - alreadyCopied);
-                return innerReadCount + alreadyCopied;
-            }
-            else if (count <= _currentlyBufferedByteCount)
-            {
-                // Enough data buffered to satisfy read request
-                Array.Copy(_buffer, _currentIndexInBuffer, buffer, offset, count);
-                _currentIndexInBuffer += count;
-                _currentlyBufferedByteCount -= count;
-                return count;
-            }
-            else
-            {
-                // Need to read more data
-                int alreadyCopied = 0;
-                if (_currentlyBufferedByteCount > 0)
-                {
-                    Array.Copy(_buffer, _currentIndexInBuffer, buffer, offset, _currentlyBufferedByteCount);
-                    alreadyCopied = _currentlyBufferedByteCount;
-                    _currentIndexInBuffer = 0;
-                    _currentlyBufferedByteCount = 0;
-                }
-
-                int innerReadCount = _innerStream.Read(_buffer, 0, BUFFER_SIZE);
-                _currentIndexInBuffer = 0;
-                _currentlyBufferedByteCount = innerReadCount;
-
-                int remainingCopyCount;
-
-                if (alreadyCopied + innerReadCount >= count)
-                {
-                    remainingCopyCount = count - alreadyCopied;
-                }
-                else
-                {
-                    remainingCopyCount = innerReadCount;
-                }
-
-                Array.Copy(_buffer, 0, buffer, offset + alreadyCopied, remainingCopyCount);
-                _currentIndexInBuffer += remainingCopyCount;
-                _currentlyBufferedByteCount -= remainingCopyCount;
-
-                return alreadyCopied + remainingCopyCount;
-            }
-        }
-
-        public override long Seek(long offset, SeekOrigin origin)
-        {
-            throw new NotSupportedException();
-        }
-
-        public override void SetLength(long value)
-        {
-            throw new NotSupportedException();
-        }
-
-        public override void Write(byte[] buffer, int offset, int count)
-        {
-            _innerStream.Write(buffer, offset, count);
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                _innerStream.Dispose();
-            }
-
-            base.Dispose(disposing);
+            // Let the base class handle it, which will end up calling the Read() method
+            return base.ReadByte();
         }
     }
+
+    public override int Read(byte[] buffer, int offset, int count)
+    {
+        if (count > BUFFER_SIZE)
+        {
+            // Trying to read more data than the buffer can hold
+            int alreadyCopied = 0;
+            if (_currentlyBufferedByteCount > 0)
+            {
+                Array.Copy(_buffer, _currentIndexInBuffer, buffer, offset, _currentlyBufferedByteCount);
+                alreadyCopied = _currentlyBufferedByteCount;
+                _currentIndexInBuffer = 0;
+                _currentlyBufferedByteCount = 0;
+            }
+
+            int innerReadCount = _innerStream.Read(buffer, offset + alreadyCopied, count - alreadyCopied);
+            return innerReadCount + alreadyCopied;
+        }
+        else if (count <= _currentlyBufferedByteCount)
+        {
+            // Enough data buffered to satisfy read request
+            Array.Copy(_buffer, _currentIndexInBuffer, buffer, offset, count);
+            _currentIndexInBuffer += count;
+            _currentlyBufferedByteCount -= count;
+            return count;
+        }
+        else
+        {
+            // Need to read more data
+            int alreadyCopied = 0;
+            if (_currentlyBufferedByteCount > 0)
+            {
+                Array.Copy(_buffer, _currentIndexInBuffer, buffer, offset, _currentlyBufferedByteCount);
+                alreadyCopied = _currentlyBufferedByteCount;
+                _currentIndexInBuffer = 0;
+                _currentlyBufferedByteCount = 0;
+            }
+
+            int innerReadCount = _innerStream.Read(_buffer, 0, BUFFER_SIZE);
+            _currentIndexInBuffer = 0;
+            _currentlyBufferedByteCount = innerReadCount;
+
+            int remainingCopyCount = alreadyCopied + innerReadCount >= count
+                ? count - alreadyCopied
+                : innerReadCount;
+
+            Array.Copy(_buffer, 0, buffer, offset + alreadyCopied, remainingCopyCount);
+            _currentIndexInBuffer += remainingCopyCount;
+            _currentlyBufferedByteCount -= remainingCopyCount;
+
+            return alreadyCopied + remainingCopyCount;
+        }
+    }
+
+    public override long Seek(long offset, SeekOrigin origin)
+        => throw new NotSupportedException();
+
+    public override void SetLength(long value)
+        => throw new NotSupportedException();
+
+    public override void Write(byte[] buffer, int offset, int count)
+        => _innerStream.Write(buffer, offset, count);
 }
