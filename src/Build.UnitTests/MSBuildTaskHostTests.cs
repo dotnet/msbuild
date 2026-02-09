b0c015fd7a467e37817492d4b8db5cd3a62492c7
Dustin Campbell <dustin@teamcampbell.org>
2026-02-09 10:54:12 -0800

    Add test that builds .NET Framework 3.5 WinForms project

---
+++ src/Build.UnitTests/MSBuildTaskHostTests.cs
@@ -0,0 +1,57 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using Microsoft.Build.UnitTests;
+using Microsoft.Build.UnitTests.Shared;
+using Shouldly;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Microsoft.Build.Engine.UnitTests;
+
+public class MSBuildTaskHostTests(ITestOutputHelper testOutput) : IDisposable
+{
+    private static string AssemblyLocation
+        => field ??= Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ?? AppContext.BaseDirectory);
+
+    private static string TestAssetsRootPath
+        => field ??= Path.Combine(AssemblyLocation, "TestAssets");
+
+    private readonly TestEnvironment _environment = TestEnvironment.Create(testOutput);
+
+    public void Dispose()
+        => _environment.Dispose();
+
+    [WindowsNet35OnlyFact]
+    public void CompileNet35WinFormsApp()
+    {
+        TransientTestFolder testFolder = _environment.CreateFolder(createFolder: true);
+
+        CopyFilesRecursively(Path.Combine(TestAssetsRootPath, "Net35WinFormsApp"), testFolder.Path);
+        string projectFilePath = Path.Combine(testFolder.Path, "TestNet35WinForms.csproj");
+
+        string output = RunnerUtilities.ExecBootstrapedMSBuild(projectFilePath, out bool success, outputHelper: testOutput);
+        success.ShouldBeTrue();
+
+        output.ShouldContain("Build succeeded.");
+    }
+
+    private static void CopyFilesRecursively(string sourcePath, string targetPath)
+    {
+        // First Create all directories
+        foreach (string dirPath in Directory.GetDirectories(sourcePath, "*", SearchOption.AllDirectories))
+        {
+            Directory.CreateDirectory(dirPath.Replace(sourcePath, targetPath));
+        }
+
+        // Then copy all the files & Replaces any files with the same name
+        foreach (string newPath in Directory.GetFiles(sourcePath, "*", SearchOption.AllDirectories))
+        {
+            File.Copy(newPath, newPath.Replace(sourcePath, targetPath), overwrite: true);
+        }
+    }
+}
---
+++ src/UnitTests.Shared/WindowsNet35OnlyFactAttribute.cs
@@ -0,0 +1,27 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Runtime.InteropServices;
+using Microsoft.Build.Shared;
+using Xunit;
+
+namespace Microsoft.Build.UnitTests.Shared;
+
+public class WindowsNet35OnlyFactAttribute : FactAttribute
+{
+    private const string Message = "This test only runs on Windows under .NET Framework when .NET Framework 3.5 is installed.";
+
+    public WindowsNet35OnlyFactAttribute(string? additionalMessage = null)
+    {
+        if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ||
+            !RuntimeInformation.FrameworkDescription.StartsWith(".NET Framework", StringComparison.OrdinalIgnoreCase) ||
+            FrameworkLocationHelper.GetPathToDotNetFrameworkV35(DotNetFrameworkArchitecture.Current) == null)
+        {
+            Skip = SkipMessage(additionalMessage);
+        }
+    }
+
+    private static string SkipMessage(string? additionalMessage = null)
+        => !string.IsNullOrWhiteSpace(additionalMessage) ? $"{Message} {additionalMessage}" : Message;
+}
