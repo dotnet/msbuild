37abd6b93caa2b1772914153330528d8cc215bff
Dustin Campbell <dustin@teamcampbell.org>
2026-02-23 10:20:52 -0800

    Add MSBuild.Benchmarks project
    - Update BenchmarkDotNet to version: 0.13.12.
    - Add BenchmarkDotNet.Diagnostics.Windows package for capturing ETW traces.
    - Add project to contain benchmarks written against Microsoft.Build, Microsoft.Build.Framework, Microsoft.Build.Tasks, or Microsoft.Build.Utilities

---
+++ src/MSBuild.Benchmarks/Extensions.cs
@@ -0,0 +1,32 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Reports;
+
+namespace MSBuild.Benchmarks;
+
+internal static class Extensions
+{
+    public static int ToExitCode(this IEnumerable<Summary> summaries)
+    {
+        // an empty summary means that initial filtering and validation did not allow to run
+        if (!summaries.Any())
+        {
+            return 1;
+        }
+
+        // if anything has failed, it's an error
+        return summaries.Any(HasAnyErrors) ? 1 : 0;
+    }
+
+    public static bool HasAnyErrors(this Summary summary)
+        => summary.HasCriticalValidationErrors || summary.Reports.Any(report => report.HasAnyErrors());
+
+    public static bool HasAnyErrors(this BenchmarkReport report)
+        => !report.BuildResult.IsBuildSuccess || !report.AllMeasurements.Any();
+}
---
+++ src/MSBuild.Benchmarks/Program.cs
@@ -0,0 +1,76 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Diagnostics;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Diagnostics.Windows;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+using Perfolizer.Horology;
+using static MSBuild.Benchmarks.Extensions;
+
+var argList = new List<string>(args);
+
+ParseAndRemoveBooleanParameter(argList, "--collect-etw", out bool collectEtw);
+ParseAndRemoveBooleanParameter(argList, "--disable-ngen", out bool disableNGen);
+ParseAndRemoveBooleanParameter(argList, "--disable-inlining", out bool disableJitInlining);
+
+return BenchmarkSwitcher
+    .FromAssembly(typeof(Program).Assembly)
+    .Run([.. argList], GetConfig(collectEtw, disableNGen, disableJitInlining))
+    .ToExitCode();
+
+static IConfig GetConfig(bool collectEtw, bool disableNGen, bool disableJitInlining)
+{
+    if (Debugger.IsAttached)
+    {
+        return new DebugInProcessConfig();
+    }
+
+    IConfig config = DefaultConfig.Instance;
+
+    if (collectEtw)
+    {
+        config = config.AddDiagnoser(new EtwProfiler());
+    }
+
+    // Use a mutator for settings that should apply to all jobs
+    // (default or CLI-specified like --job short).
+    Job overrides = new Job()
+        .DontEnforcePowerPlan();
+
+    if (disableNGen)
+    {
+        overrides = overrides
+            .WithEnvironmentVariable("COMPlus_ZapDisable", "1")
+            .WithEnvironmentVariable("COMPlus_ReadyToRun", "0")
+            .WithEnvironmentVariable("DOTNET_ReadyToRun", "0");
+    }
+
+    if (disableJitInlining)
+    {
+        overrides = overrides
+            .WithEnvironmentVariable("COMPlus_JitNoInline", "1")
+            .WithEnvironmentVariable("DOTNET_JitNoInline", "1");
+    }
+
+    config = config.AddJob(overrides.AsMutator());
+
+    return config;
+}
+
+static void ParseAndRemoveBooleanParameter(List<string> argsList, string parameter, out bool parameterValue)
+{
+    int parameterIndex = argsList.IndexOf(parameter);
+
+    if (parameterIndex != -1)
+    {
+        argsList.RemoveAt(parameterIndex);
+
+        parameterValue = true;
+    }
+    else
+    {
+        parameterValue = false;
+    }
+}
