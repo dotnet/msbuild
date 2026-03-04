a4b28122cd89f7b6a03ce15c42cc2acf9838a132
AR-May <67507805+AR-May@users.noreply.github.com>
2026-03-04 15:53:36 +0100

    Enable MT by default for testing

--- src/Framework/Traits.cs
+++ src/Framework/Traits.cs
@@ -148,7 +148,7 @@ namespace Microsoft.Build.Framework
         /// This environment variable is used for easier opt-in to multithreaded mode for testing and dogfooding purposes. 
         /// It is not intended to be a long-term escape hatch, and it should be removed once multithreaded mode is stable.
         /// </remarks>
-        public readonly bool UseMultiThreadedMode = Environment.GetEnvironmentVariable("MSBUILDUSEMULTITHREADEDMODE") == "1";
+        public readonly bool UseMultiThreadedMode = true;
 
         /// <summary>
         /// Name of environment variable for logging arguments (e.g., -bl, -check).
