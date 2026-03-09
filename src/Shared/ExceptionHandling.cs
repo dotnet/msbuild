002592ce8f3302d51c8ab319715b6e5d97bc0527
Dustin Campbell <dustin@teamcampbell.org>
2026-03-09 10:11:41 -0700

    Move exception dumping members from ExceptionHandling to DebugUtils
    This change moves all of the implementations of exception dumping members from ExceptionHandling to DebugUtils. Member stubs have been left in ExceptionHandling that delegate to the implementations in DebugUtils. This will be cleaned up in a later commit.
    
    Note that references to the BUILDINGAPPXTASKS conditional compilation symbol have been removed from ExceptionHandling. BUILDINGAPPXTASKS isn't set anywhere within the msbuild repo.

--- src/Shared/ExceptionHandling.cs
+++ src/Shared/ExceptionHandling.cs
@@ -3,96 +3,40 @@
 
 #nullable disable
 
-#if BUILDINGAPPXTASKS
-namespace Microsoft.Build.AppxPackage.Shared
-#else
 using System;
-using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
-using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Security;
-using System.Text;
 using System.Threading;
 using System.Xml;
-using Microsoft.Build.Shared.FileSystem;
 using System.Xml.Schema;
 using System.Runtime.Serialization;
-#if !MICROSOFT_BUILD_ENGINE_OM_UNITTESTS
 using Microsoft.Build.Shared.Debugging;
-using Microsoft.Build.Framework.Telemetry;
-#endif
+
 using Microsoft.Build.Framework;
 
 namespace Microsoft.Build.Shared
-#endif
 {
     /// <summary>
     /// Utility methods for classifying and handling exceptions.
     /// </summary>
     internal static class ExceptionHandling
     {
-        private static readonly string s_debugDumpPath = GetDebugDumpPath();
-
-        /// <summary>
-        /// Gets the location of the directory used for diagnostic log files.
-        /// </summary>
-        /// <returns></returns>
-        private static string GetDebugDumpPath()
+        /// <inheritdoc cref="DebugUtils.ResetDebugDumpPathInRunningTests"/>
+        internal static bool ResetDebugDumpPathInRunningTests
         {
-            string debugPath =
-
-#if MICROSOFT_BUILD_ENGINE_OM_UNITTESTS
-                Environment.GetEnvironmentVariable("MSBUILDDEBUGPATH");
-#else
-                DebugUtils.DebugPath;
-#endif
-
-            return !string.IsNullOrEmpty(debugPath)
-                    ? debugPath
-                    : FileUtilities.TempFileDirectory;
+            get => DebugUtils.ResetDebugDumpPathInRunningTests;
+            set => DebugUtils.ResetDebugDumpPathInRunningTests = value;
         }
 
-        private static string s_debugDumpPathInRunningTests = GetDebugDumpPath();
-        internal static bool ResetDebugDumpPathInRunningTests = false;
+        /// <inheritdoc cref="DebugUtils.DebugDumpPath"/>
+        internal static string DebugDumpPath => DebugUtils.DebugDumpPath;
 
-        /// <summary>
-        /// The directory used for diagnostic log files.
-        /// </summary>
-        internal static string DebugDumpPath
-        {
-            get
-            {
-                if (BuildEnvironmentHelper.Instance.RunningTests)
-                {
-                    if (ResetDebugDumpPathInRunningTests)
-                    {
-                        s_debugDumpPathInRunningTests = GetDebugDumpPath();
-                        // reset dump file name so new one is created in new path
-                        s_dumpFileName = null;
-                        ResetDebugDumpPathInRunningTests = false;
-                    }
+        /// <inheritdoc cref="DebugUtils.DumpFilePath"/>
+        internal static string DumpFilePath => DebugUtils.DumpFilePath;
 
-                    return s_debugDumpPathInRunningTests;
-                }
-
-                return s_debugDumpPath;
-            }
-        }
-
-        /// <summary>
-        /// The file used for diagnostic log files.
-        /// </summary>
-        internal static string DumpFilePath => s_dumpFileName;
-
-#if !BUILDINGAPPXTASKS
-        /// <summary>
-        /// The filename that exceptions will be dumped to
-        /// </summary>
-        private static string s_dumpFileName;
-#endif
         /// <summary>
         /// If the given exception is "ignorable under some circumstances" return false.
         /// Otherwise it's "really bad", and return true.
@@ -108,10 +52,7 @@ namespace Microsoft.Build.Shared
              || e is ThreadInterruptedException
              || e is AccessViolationException
              || e is CriticalTaskException
-#if !BUILDINGAPPXTASKS
-             || e is InternalErrorException
-#endif
-             )
+             || e is InternalErrorException)
             {
                 // Ideally we would include NullReferenceException, because it should only ever be thrown by CLR (use ArgumentNullException for arguments)
                 // but we should handle it if tasks and loggers throw it.
@@ -175,7 +116,7 @@ namespace Microsoft.Build.Shared
         {
             return e is XmlException
 #if FEATURE_SECURITY_PERMISSIONS
-                || e is System.Security.XmlSyntaxException
+                || e is XmlSyntaxException
 #endif
                 || e is XmlSchemaException
                 || e is UriFormatException; // XmlTextReader for example uses this under the covers
@@ -212,8 +153,6 @@ namespace Microsoft.Build.Shared
                 Column = column
             };
         }
-
-#if !BUILDINGAPPXTASKS
 
         /// <summary>
         /// If the given exception is file IO related or Xml related return false.
@@ -319,111 +258,18 @@ namespace Microsoft.Build.Shared
             return true;
         }
 
-        /// <summary>
-        /// Dump any unhandled exceptions to a file so they can be diagnosed
-        /// </summary>
+        /// <inheritdoc cref="DebugUtils.UnhandledExceptionHandler"/>
         [SuppressMessage("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode", Justification = "It is called by the CLR")]
         internal static void UnhandledExceptionHandler(object sender, UnhandledExceptionEventArgs e)
-        {
-            Exception ex = (Exception)e.ExceptionObject;
-            DumpExceptionToFile(ex);
-#if !MICROSOFT_BUILD_ENGINE_OM_UNITTESTS
-            RecordCrashTelemetryForUnhandledException(ex);
-#endif
-        }
+            => DebugUtils.UnhandledExceptionHandler(sender, e);
 
-#if !MICROSOFT_BUILD_ENGINE_OM_UNITTESTS
-        /// <summary>
-        /// Records and immediately flushes crash telemetry for an unhandled exception.
-        /// Best effort - must never throw, as the process is already crashing.
-        /// </summary>
-        [System.Runtime.CompilerServices.MethodImpl(System.Runtime.CompilerServices.MethodImplOptions.NoInlining)]
-        private static void RecordCrashTelemetryForUnhandledException(Exception ex)
-        {
-            CrashTelemetryRecorder.RecordAndFlushCrashTelemetry(
-                ex,
-                exitType: CrashExitType.UnhandledException,
-                isUnhandled: true,
-                isCritical: IsCriticalException(ex));
-        }
-#endif
-
-        /// <summary>
-        /// Dump the exception information to a file
-        /// </summary>
+        /// <inheritdoc cref="DebugUtils.DumpExceptionToFile"/>
         internal static void DumpExceptionToFile(Exception ex)
-        {
-            try
-            {
-                // Locking on a type is not recommended.  However, we are doing it here to be extra cautious about compatibility because
-                //  this method previously had a [MethodImpl(MethodImplOptions.Synchronized)] attribute, which does lock on the type when
-                //  applied to a static method.
-                lock (typeof(ExceptionHandling))
-                {
-                    if (s_dumpFileName == null)
-                    {
-                        Guid guid = Guid.NewGuid();
+            => DebugUtils.DumpExceptionToFile(ex);
 
-                        // For some reason we get Watson buckets because GetTempPath gives us a folder here that doesn't exist.
-                        // Either because %TMP% is misdefined, or because they deleted the temp folder during the build.
-                        // If this throws, no sense catching it, we can't log it now, and we're here
-                        // because we're a child node with no console to log to, so die
-                        Directory.CreateDirectory(DebugDumpPath);
-
-                        var pid = EnvironmentUtilities.CurrentProcessId;
-                        // This naming pattern is assumed in ReadAnyExceptionFromFile
-                        s_dumpFileName = Path.Combine(DebugDumpPath, $"MSBuild_pid-{pid}_{guid:n}.failure.txt");
-
-                        using (StreamWriter writer = FileUtilities.OpenWrite(s_dumpFileName, append: true))
-                        {
-                            writer.WriteLine("UNHANDLED EXCEPTIONS FROM PROCESS {0}:", pid);
-                            writer.WriteLine("=====================");
-                        }
-                    }
-
-                    using (StreamWriter writer = FileUtilities.OpenWrite(s_dumpFileName, append: true))
-                    {
-                        // "G" format is, e.g., 6/15/2008 9:15:07 PM
-                        writer.WriteLine(DateTime.Now.ToString("G", CultureInfo.CurrentCulture));
-                        writer.WriteLine(ex.ToString());
-                        writer.WriteLine("===================");
-                    }
-                }
-            }
-
-            // Some customers experience exceptions such as 'OutOfMemory' errors when msbuild attempts to log errors to a local file.
-            // This catch helps to prevent the application from crashing in this best-effort dump-diagnostics path,
-            // but doesn't prevent the overall crash from going to Watson.
-            catch
-            {
-            }
-        }
-
-        /// <summary>
-        /// Returns the content of any exception dump files modified
-        /// since the provided time, otherwise returns an empty string.
-        /// </summary>
+        /// <inheritdoc cref="DebugUtils.ReadAnyExceptionFromFile"/>
         internal static string ReadAnyExceptionFromFile(DateTime fromTimeUtc)
-        {
-            var builder = new StringBuilder();
-            IEnumerable<string> files = FileSystems.Default.EnumerateFiles(DebugDumpPath, "MSBuild*failure.txt");
-
-            foreach (string file in files)
-            {
-                if (FileSystems.Default.GetLastWriteTimeUtc(file) >= fromTimeUtc)
-                {
-                    builder.Append(Environment.NewLine);
-                    builder.Append(file);
-                    builder.Append(':');
-                    builder.Append(Environment.NewLine);
-                    builder.Append(FileSystems.Default.ReadFileAllText(file));
-                    builder.Append(Environment.NewLine);
-                }
-            }
-
-            return builder.ToString();
-        }
-#endif
+            => DebugUtils.ReadAnyExceptionFromFile(fromTimeUtc);
 
         /// <summary> Line and column pair. </summary>
         internal struct LineAndColumn
