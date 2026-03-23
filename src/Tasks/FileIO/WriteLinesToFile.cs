d1f27e526bd3bdae5bde5e45680a5fd20b80b10b
SACHIN SHARMA <sachinsharma31261@gmail.com>
2026-03-22 17:37:45 -0700

    Robustify WriteLinesToFile transactional save mode

--- src/Tasks/FileIO/WriteLinesToFile.cs
+++ src/Tasks/FileIO/WriteLinesToFile.cs
@@ -1,4 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System;
@@ -226,11 +226,11 @@ namespace Microsoft.Build.Tasks
                 catch (IOException)
                 {
                     // Replace failed (likely file is locked). Retry a few times with small delay.
-                    for (int retry = 1; retry < 3; retry++)
+                    for (int retry = 1; retry < 10; retry++)
                     {
                         try
                         {
-                            System.Threading.Thread.Sleep(10);
+                            System.Threading.Thread.Sleep(retry * 10);
                             System.IO.File.Replace(temporaryFilePath, filePath, null, true);
                             temporaryFilePath = null; // Mark as successfully replaced
                             return !Log.HasLoggedErrors;
