5e79eb71456c27ea1cd75a54a5e09fd9cbbe3e45
Youssef1313 <youssefvictor00@gmail.com>
2026-04-01 22:49:29 +0200

    Remove duplicate test cases
    The deleted test methods are already tested in MultiThreadableTaskCodeFixProviderTests which covers both the analyzer and the codefix

--- src/ThreadSafeTaskAnalyzer.Tests/MultiThreadableTaskCodeFixProviderTests.cs
+++ src/ThreadSafeTaskAnalyzer.Tests/MultiThreadableTaskCodeFixProviderTests.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp.Testing;
 using Microsoft.CodeAnalysis.Testing;
 using Xunit;
@@ -42,7 +43,7 @@ public class MultiThreadableTaskCodeFixProviderTests
         DiagnosticIds.FilePathRequiresAbsolute => new DiagnosticResult(DiagnosticDescriptors.FilePathRequiresAbsolute),
         DiagnosticIds.PotentialIssue => new DiagnosticResult(DiagnosticDescriptors.PotentialIssue),
         DiagnosticIds.TransitiveUnsafeCall => new DiagnosticResult(DiagnosticDescriptors.TransitiveUnsafeCall),
-        _ => new DiagnosticResult(id, Microsoft.CodeAnalysis.DiagnosticSeverity.Warning),
+        _ => new DiagnosticResult(id, DiagnosticSeverity.Warning),
     };
 
     [Fact]
