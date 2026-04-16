627ec372628996aace0f2915f19cdb2ddda72040
Dustin Campbell <dustin@teamcampbell.org>
2026-04-16 15:45:00 -0700

    Remove errant space character

--- src/Build/BackEnd/BuildManager/BuildManager.cs
+++ src/Build/BackEnd/BuildManager/BuildManager.cs
@@ -1,4 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System;
@@ -2393,7 +2393,7 @@ namespace Microsoft.Build.Execution
         {
             if (_buildManagerState != requiredState)
             {
-                ErrorUtilities.ThrowInternalError($"Expected state {requiredState}, actual state { _buildManagerState}");
+                ErrorUtilities.ThrowInternalError($"Expected state {requiredState}, actual state {_buildManagerState}");
             }
         }
 
