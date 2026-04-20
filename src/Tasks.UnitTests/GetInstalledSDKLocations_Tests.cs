4c5adc5bdec9ecbc01e6665eb1136c70b135dad8
Veronika Ovsyannikova <veronikao@microsoft.com>
2026-04-20 15:16:02 +0200

    Delete unused import

--- src/Tasks.UnitTests/GetInstalledSDKLocations_Tests.cs
+++ src/Tasks.UnitTests/GetInstalledSDKLocations_Tests.cs
@@ -4,7 +4,6 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
-using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.IO;
 using Microsoft.Build.Framework;
