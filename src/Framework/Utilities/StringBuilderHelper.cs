bd5fe3b8b7f7394c71df6acde9d63c4049517b85
Dustin Campbell <dustin@teamcampbell.org>
2026-04-20 15:32:57 -0700

    Add more XML doc comments to StringBuilderHelper

--- src/Framework/Utilities/StringBuilderHelper.cs
+++ src/Framework/Utilities/StringBuilderHelper.cs
@@ -10,6 +10,11 @@ namespace Microsoft.Build.Framework.Utilities;
 /// <summary>
 ///  Helper struct to use with custom interpolated string handlers.
 /// </summary>
+/// <remarks>
+///  Acquires a <see cref="StringBuilder"/> from <see cref="StringBuilderCache"/> on construction.
+///  The builder is returned to the cache when <see cref="GetFormattedText"/> is called, which
+///  produces the final string and releases the underlying <see cref="StringBuilder"/>.
+/// </remarks>
 internal ref struct StringBuilderHelper(int capacity)
 {
     private StringBuilder? _builder = StringBuilderCache.Acquire(capacity);
@@ -24,6 +29,10 @@ internal ref struct StringBuilderHelper(int capacity)
         where TValue : IFormattable
         => _builder?.Append(value?.ToString(format, formatProvider: null));
 
+    /// <summary>
+    ///  Returns the formatted string and releases the underlying <see cref="StringBuilder"/>
+    ///  back to the <see cref="StringBuilderCache"/>. Subsequent calls return <see cref="string.Empty"/>.
+    /// </summary>
     public string GetFormattedText()
         => Interlocked.Exchange(ref _builder, null) is { } builder
             ? StringBuilderCache.GetStringAndRelease(builder)
