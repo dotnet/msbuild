7299b72f561f69687797ecefceb1174236976bbc
Aleš Prokop <alesprokop@microsoft.com>
2026-04-22 08:43:14 +0200

    Added clickable links into the build summary.

---
+++ src/Framework/LoggerRegisteredEventArgs.cs
@@ -0,0 +1,53 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Collections.Generic;
+
+namespace Microsoft.Build.Framework
+{
+    /// <summary>
+    /// Arguments for the logger registered event, indicating that a logger has been registered.
+    /// </summary>
+    [Serializable]
+    public class LoggerRegisteredEventArgs : BuildMessageEventArgs
+    {
+        public LoggerRegisteredEventArgs()
+        {
+        }
+        /// <summary>
+        /// Initialize a new instance of the LoggerRegisteredEventArgs class.
+        /// </summary>
+        /// <param name="loggerName">The name of the logger.</param>
+        /// <param name="outputFilePath">The full path to the log output file.</param>
+        /// <param name="verbosity">The verbosity level of the logger.</param>
+        /// <param name="additionalOutputFilePaths">Additional output file paths for the logger.</param>
+        public LoggerRegisteredEventArgs(string loggerName, string? outputFilePath, LoggerVerbosity? verbosity, IReadOnlyList<string>? additionalOutputFilePaths) : base()
+        {
+            OutputFilePath = outputFilePath;
+            LoggerName = loggerName;
+            Verbosity = verbosity;
+            AdditionalOutputFilePaths = additionalOutputFilePaths;
+        }
+
+        /// <summary>
+        /// The name of the logger.
+        /// </summary>
+        public string LoggerName { get; set; } = string.Empty;
+
+        /// <summary>
+        /// The verbosity level of the logger.
+        /// </summary>
+        public LoggerVerbosity? Verbosity { get; set; }
+
+        /// <summary>
+        /// The full path to the log output file.
+        /// </summary>
+        public string? OutputFilePath { get; set; }
+
+        /// <summary>
+        /// Additional output file paths for the logger.
+        /// </summary>
+        public IReadOnlyList<string>? AdditionalOutputFilePaths { get; set; }
+    }
+}
