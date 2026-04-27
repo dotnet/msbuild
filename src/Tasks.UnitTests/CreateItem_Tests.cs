c122bc45d34c3efe9d629a7c47ca135df6a4ba6e
Jan Kratochvil <kratochvil.jan@outlook.com>
2026-04-27 14:03:56 +0200

    Removes unused using

--- src/Tasks.UnitTests/CreateItem_Tests.cs
+++ src/Tasks.UnitTests/CreateItem_Tests.cs
@@ -4,7 +4,6 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using Microsoft.Build.Execution;
 using Microsoft.Build.Framework;
 using Microsoft.Build.Tasks;
