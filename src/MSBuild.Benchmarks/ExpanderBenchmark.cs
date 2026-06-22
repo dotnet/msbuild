155ef334b9da15e5bbafffb8127f2148b03a654f
Dustin Campbell <dustin@teamcampbell.org>
2026-06-22 08:34:52 -0700

    Remove unused using directive

--- src/MSBuild.Benchmarks/ExpanderBenchmark.cs
+++ src/MSBuild.Benchmarks/ExpanderBenchmark.cs
@@ -6,7 +6,6 @@ using Microsoft.Build.Collections;
 using Microsoft.Build.Construction;
 using Microsoft.Build.Evaluation;
 using Microsoft.Build.Execution;
-using Microsoft.Build.Internal;
 using Microsoft.Build.Shared;
 using Microsoft.Build.Shared.FileSystem;
 
