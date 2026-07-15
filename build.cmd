00addfc1b646a04c04735fa1517fb290990c73de
Viktor Hofer <7412651+ViktorHofer@users.noreply.github.com>
2026-07-15 21:57:23 +0200

    Bring back MSBUILDDEBUGONSTART

--- build.cmd
+++ build.cmd
@@ -5,5 +5,12 @@ set _args=%*
 if "%~1"=="-?" set _args=-help
 if "%~1"=="/?" set _args=-help
 
+if defined MSBUILDDEBUGONSTART_HARD goto build
+if not defined MSBUILDDEBUGONSTART goto build
+if %MSBUILDDEBUGONSTART% == 0 goto build
+set MSBUILDDEBUGONSTART=
+echo To debug the build, define a value for MSBUILDDEBUGONSTART_HARD.
+
+:build
 powershell -ExecutionPolicy ByPass -NoProfile -Command "& '%~dp0eng\build.ps1'" -restore -build %_args%
 exit /b %ERRORLEVEL%
